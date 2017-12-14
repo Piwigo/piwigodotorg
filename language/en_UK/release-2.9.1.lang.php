654c2eed888d9779a66eb1beb7d4b22cc5aa6221
MaximeBOURMAUD <maxime85710@gmail.com>
2017-12-14 18:01:34 +0100

    Add release 2.9.1 - release 2.9.2

---
+++ language/en_UK/release-2.9.1.lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['porg_issue_707'] = 'number of views, hide decimals when less than 1k';
+$lang['porg_issue_706'] = '[security] identification should redirect within Piwigo only';
+$lang['porg_issue_705'] = 'user list backend, check input parameter';
+$lang['porg_issue_701'] = 'The album synchronise link reaches the page to synchronise ./galleries/ even if the album is located in another folder';
+$lang['porg_issue_699'] = 'File inclusion in administration';
+$lang['porg_issue_696'] = 'PIWIGO API - pwg.categories.delete doesn\'t work properly.';
+$lang['porg_issue_693'] = 'Batch downloader failing using pclzip and php 7.1';
+$lang['porg_issue_685'] = 'batch manager, delete photo does not work with Safari';
+$lang['porg_issue_683'] = 'disk usage, hide decimals when more than 100GB';
+$lang['porg_issue_679'] = 'admin dashboard, need trigger for plugins';
+$lang['porg_issue_674'] = '[php7.1] user edit box, [] operator not supported for strings';
+$lang['porg_issue_667'] = 'Bug Report:  Cross Site Scripting(XSS) in page parameter of admin.php';
+$lang['porg_issue_663'] = 'SQL error in tour 2.9';
+?>
---
+++ language/en_UK/release-2.9.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['porg_issue_789'] = 'ability to avoid error message on log in page';
+$lang['porg_issue_787'] = 'unexpected \"action\" for password.php displays errors';
+$lang['porg_issue_777'] = 'WS categories getImages, TotalCount';
+$lang['porg_issue_767'] = 'Publishing via Shotwell 0.22.0 fails with Piwigo 2.9.1';
+$lang['porg_issue_760'] = 'Date creation not refreshed when changing it from picture_modify';
+$lang['porg_issue_755'] = 'Sync\'ing the metadata send a date creation incorrect for Mysql 5.7 ';
+$lang['porg_issue_751'] = 'Cannot run install/piwigo_structure-mysql.sql twice';
+$lang['porg_issue_747'] = 'PHP notice when changing admin page\'s theme';
+$lang['porg_issue_739'] = '[search] when a search on word also matches tags';
+$lang['porg_issue_732'] = 'rename transliterate function ';
+$lang['porg_issue_731'] = 'dashboard, missing number of photos';
+$lang['porg_issue_726'] = 'trigger on album creation';
+$lang['porg_issue_725'] = 'protect pwg.images.setInfo from HTML';
+$lang['porg_issue_724'] = 'Bug Report: SQL injection in page cat_options';
+$lang['porg_issue_723'] = ' when requesting an private id, the permalink is revealed';
+$lang['porg_issue_721'] = 'Bug Report: Set administrator private album to public via CSRF & Incorrect Permissions';
+$lang['porg_issue_713'] = 'update notification mail in french/english mix';
+$lang['porg_issue_710'] = 'input parameter \"redirect_decoded\" is not valid';
+$lang['porg_issue_678'] = 'PHP 7 RSS error';
+?>
