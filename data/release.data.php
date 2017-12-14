d4922188e07719b4de52f9d2b0c09cce0fb01ad1
MaximeBOURMAUD <maxime85710@gmail.com>
2017-12-14 10:13:52 +0100

    Add data for release (release_on, md5sum, summary, features, bugs, know_issues, languages)

---
+++ data/release.data.php
@@ -0,0 +1,30 @@
+<?php
+
+$porg_releases = array(
+    '2.9.2' => array(
+      'released_on' => '2017-10-06',
+      'md5sum' => '8eb7df8d35b5a73d73223eaa9a8509ca',
+      'summary' => array('bug fixes (including security)'),
+      'features' => array(),
+      'bugs' => array('789', '787', '777', '767', '760', '755', '751', '747', '739', '732', '731', '726', '725', '724', '723', '721', '713', '710', '678'),
+      'known_issues' => array(),
+      'languages' => array(
+        'new' => array(),
+        'updated' => array(),
+    ),
+  ),
+    '2.9.1' => array(
+      'released_on' => '2017-06-15',
+      'md5sum' => 'd7e64ca32cb1695762254aecae033181',
+      'summary' => array('bug fixes (including security)'),
+      'features' => array(),
+      'bugs' => array(),
+      'known_issues' => array(),
+      'languages' => array(
+        'new' => array(),
+        'updated' => array(),
+      ),
+    ),
+);
+
+?>
