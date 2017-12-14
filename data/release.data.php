dada940ddcc06918d31aafdf40f89b4054a2d28e
MaximeBOURMAUD <maxime85710@gmail.com>
2017-12-14 11:16:42 +0100

    Review indentation

--- data/release.data.php
+++ data/release.data.php
@@ -1,30 +1,44 @@
 <?php
 
 $porg_releases = array(
-    '2.9.2' => array(
-      'released_on' => '2017-10-06',
-      'md5sum' => '8eb7df8d35b5a73d73223eaa9a8509ca',
-      'summary' => array('bug fixes (including security)'),
-      'features' => array(),
-      'bugs' => array('789', '787', '777', '767', '760', '755', '751', '747', '739', '732', '731', '726', '725', '724', '723', '721', '713', '710', '678'),
-      'known_issues' => array(),
-      'languages' => array(
-        'new' => array(),
-        'updated' => array(),
+  '2.9.2' => array(
+    'released_on' => '2017-10-06',
+    'md5sum' => '8eb7df8d35b5a73d73223eaa9a8509ca',
+    'summary' => array('bug fixes (including security)'),
+    'features' => array(),
+    'bugs' => array(789, 787, 777, 767, 760, 755, 751, 747, 739, 732, 731, 726, 725, 724, 723, 721, 713, 710, 678),
+    'known_issues' => array(),
+    'languages' => array(
+      'new' => array(),
+      'updated' => array('ca_ES', 'en_GB', 'eo_EO', 'eu_ES', 'fi_FI', 'fr_CA', 'hu_HU', 'km_KH', 'pt_PT', 'ro_RO', 'sk_SK', 'sl_SI', 'sr_RS', 'sv_SE', 'vi_VN', 'zh_CN', 'zh_HK'),
     ),
   ),
-    '2.9.1' => array(
-      'released_on' => '2017-06-15',
-      'md5sum' => 'd7e64ca32cb1695762254aecae033181',
-      'summary' => array('bug fixes (including security)'),
-      'features' => array(),
-      'bugs' => array(),
-      'known_issues' => array(),
-      'languages' => array(
-        'new' => array(),
-        'updated' => array(),
-      ),
+
+  '2.9.1' => array(
+    'released_on' => '2017-06-15',
+    'md5sum' => 'd7e64ca32cb1695762254aecae033181',
+    'summary' => array('bug fixes (including security)'),
+    'features' => array(),
+    'bugs' => array(707, 706, 705, 701, 699, 696, 693, 685, 683, 679, 674, 667, 663),
+    'known_issues' => array(710),
+    'languages' => array(
+      'new' => array(),
+      'updated' => array(),
     ),
+  ),
+
+  '2.9.0' => array(
+    'released_on' => '2017-06-15',
+    'md5sum' => '4ac32ff84ca8ae7d37aee19694b31ea9',
+    'summary' => array('Modernized design', 'update notification'),
+    'features' => array(),
+    'bugs' => array(),
+    'known_issues' => array(),
+    'languages' => array(
+      'new' => array(),
+      'updated' => array(),
+    ),
+  ),
 );
 
 ?>
