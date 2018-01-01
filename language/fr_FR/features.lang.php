1d71db325ee17e9e1b8a7975c3b7c146ab0538e2
plegall <plg@piwigo.org>
2018-01-01 17:42:20 +0100

    [features] translate in fr_FR

---
+++ language/fr_FR/features.lang.php
@@ -0,0 +1,44 @@
+<?php
+
+$lang['porg_features_desc1'] = 'Piwigo propose un large éventail de fonctionnalités pour n\'importe quelle galerie photo.';
+$lang['porg_features_desc2'] = 'Nombre d\'entre elles sont décrites sur cette page.';
+$lang['porg_features_desc3'] = 'Des nouveautés sont ajoutées au fil du temps, selon nos idées et les discussions avec les utilisateurs.';
+$lang['Upload your photos'] = 'Transférez vos photos';
+$lang['porg_features_upload_desc'] = 'Vous pouvez transférer vos photos avec le formulaire web, un client FTP,<br> une application distante comme digiKam, Shotwell, Lightroom ou encore une application mobile.';
+$lang['Batch Manager'] = 'Gestion par lot';
+$lang['porg_features_batch_manager_desc'] = 'Filtrez vos photos à partir de votre collection, faites une sélection et appliquez une action par lot : changer l\'auteur, ajouter des tags, associer à un nouvel album, définir les coordonnées géographiques...';
+$lang['Organize with Album'] = 'Organisez par albums';
+$lang['Browse by Dates'] = 'Naviguez par dates';
+$lang['Privacy on your Photos'] = 'Gérez les permissions';
+$lang['porg_features_organize_desc1'] = 'Distribuez vos photos parmi vos albums.';
+$lang['porg_features_organize_desc2'] = 'L\'arborescence d\'albums a une profondeur illimitée.';
+$lang['porg_features_organize_desc3'] = 'Chaque photo peut être associée à plusieurs albums.';
+$lang['porg_features_browse_date_desc'] = 'Les appareils photos numériques enregistrent la date dans chaque photo et Piwigo utilise cette date pour afficher votre collection sous forme de calendrier, navigable jour par jour.';
+$lang['porg_features_privacy_desc1'] = 'Rendez vos photos privées puis décidez qui peut les voir.';
+$lang['porg_features_privacy_desc2'] = 'Vous pouvez définir les permissions sur les albums ou les photoso, par utilisateur ou par groupe.';
+$lang['Geolocate your photos on a map'] = 'Géolocalisez vos photos sur une carte';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo peut lire les coordonnées GPS latitude et longitude à partir des métadonnées.';
+$lang['porg_features_geolocate_desc2'] = 'Puis, grâce au plugin Google Maps ou OpenStreetMap, Piwigo peut afficher vos photos sur une carte interactive.';
+$lang['porg_features_plugins_desc1'] = 'Changez d\'apparance avec les thèmes.';
+$lang['porg_features_plugins_desc2'] = 'Ajoutez des fonctionnalités avec les plugins.';
+$lang['porg_features_plugins_desc3'] = 'Les extensions s\'installent en quelques clics.';
+$lang['porg_features_plugins_desc4'] = '<strong>350 extensions</strong> disponibles, vers l\'infini et au-delà !';
+$lang['Full screen slideshow'] = 'Diaporama plein écran';
+$lang['porg_features_slideshow_desc'] = 'Avec le plugin Fotorama, ou certains thèmes comme Bootstrap Darkroom, faites l\'expérience du diaporama plein écran.';
+$lang['Easy installation and updates'] = 'Installation et mises à jour simplifiées';
+$lang['A great place for your visitors'] = 'Des outils pour captiver vos utilisateurs';
+$lang['Import automatic your metadata'] = 'Import automatique des métadonnées';
+$lang['porg_features_easy_install_desc1'] = 'Nous avons conçu Piwigo pour qu\'il soit super simple à installer, avec un minimum de prérequis.';
+$lang['porg_features_easy_install_desc2'] = 'De même pour les mises à jour, un clic suffit !';
+$lang['porg_features_great_place_desc'] = 'Vos visiteurs peuvent laisser des commentaires, noter les photos, lancer des recherches et être notifié des nouveautés par email.';
+$lang['porg_features_metadata_desc'] = 'Par exemple une description IPTC peut être importée dans les propriétés de la photo, affichées à côté de la photo... et recherchable !';
+$lang['Users management'] = 'Gestion des utilisateurs';
+$lang['porg_features_user_management_desc1'] = 'Les administrateurs peuvent créer des utilisateurs, des groupes, gérer les permissions et envoyer des notifications.';
+$lang['porg_features_user_management_desc2'] = 'Plusieurs administrateurs peuvent travailler simultanément dans un même Piwigo.';
+$lang['porg_features_user_management_desc3'] = 'Parfait pour votre équipe !';
+$lang['API'] = 'API';
+$lang['porg_features_api_desc1'] = 'L\'API web de Piwigo permet aux développeurs de contrôler Piwigo depuis une autre applications : afficher des miniatures dans un billet WordPress, publier depuis Lightroom, synchroniser un répertoire avec un script Perl...';
+$lang['porg_features_api_desc2'] = 'C\'est aussi simple que d\'envoyer des requêtes HTTP et recevoir des réponses en JSON :-)';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'De nombreuses options pour que Piwigo réponde à votre besoin';
+
+?>
