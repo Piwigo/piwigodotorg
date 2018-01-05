310bc286ab5d376d9fd65daf862aceff30c6d888
plegall <plg@piwigo.org>
2018-01-05 19:54:36 +0100

    [get-piwigo] translate FR

---
+++ language/fr_FR/get_piwigo.lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['porg_get_piwigo_desc1'] = 'Il y a deux façons d\'utiliser Piwigo.';
+$lang['porg_get_piwigo_desc2'] = 'Soit vous l\'hébergez vous-mêmes, par exemple avec OVH ou o2switch, soit directement sur Piwigo.com.';
+$lang['porg_get_piwigo_desc3'] = 'Dans les deux cas vous gardez un contrôle total sur vos données.';
+$lang['Self-hosted Piwigo'] = 'Héberger son Piwigo';
+$lang['porg_get_piwigo_self_hosted_desc1'] = 'Faites tourner Piwigo sur votre propre hébergement, disposant de PHP et MySQL.';
+$lang['porg_get_piwigo_self_hosted_desc2'] = 'Utilisez Piwigo gratuitement mais prévoyez de gérer consciencieusement les mises à jour pour garder votre application sécurisée et rapide.';
+$lang['download_piwigo'] = 'Télécharger<br>Piwigo';
+$lang['Install Guide'] = 'Guide d\'installation';
+$lang['NetInstall'] = 'NetInstall';
+$lang['Upgrade Guide'] = 'Guide de mise à jour';
+$lang['Cloud-hosted Piwigo'] = 'Piwigo.com, clés en main';
+$lang['porg_get_piwigo_cloud_hosted_desc1'] = 'Oubliez les tâches techniques si vous choisissez Piwigo.com.';
+$lang['porg_get_piwigo_cloud_hosted_desc2'] = 'Installation, mises à jours, sauvegardes sont gérées automatiquement sur une infrastructure dédiée et optimisée pour Piwigo.';
+$lang['porg_get_piwigo_cloud_hosted_desc3'] = 'Inclus un support dédié.';
+$lang['Enterprises'] = 'Entreprises';
+$lang['Infrastructure'] = 'Infrastructure';
+$lang['Blog'] = 'Blog';
+$lang['Piwigo Mobile App'] = 'Application Mobile Piwigo';
+$lang['porg_get_piwigo_mobile_desc1'] = 'Vous utilisez votre iPhone comme appareil photo ? Publiez vos photos directement sur Piwigo.';
+$lang['porg_get_piwigo_mobile_desc2'] = 'Vous pouvez aussi naviguer dans votre galerie en plein écran, créer/éditer/déplacer vos albums ou éditer les propriétés de vos photos... du bout des doigts !';
+//$lang[''] = '';
+//$lang[''] = '';
+
+?>
