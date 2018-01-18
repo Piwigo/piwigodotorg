fb0b0b49f5a83254fe20110c72237a7c4ec4dfb5
plegall <plg@piwigo.org>
2018-01-18 15:27:14 +0100

    [release] translate FR

---
+++ language/en_UK/release.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['Release note'] = 'Release note';
+$lang['Download Piwigo %s'] = 'Download Piwigo %s';
+$lang['Released on %s'] = 'Released on %s';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'The list of major changes is described on <a href="%s">Piwigo %s</a> release note';
+$lang['Bugs fixed'] = 'Bugs fixed';
+$lang['Known issues'] = 'Known issues';
+$lang['Languages'] = 'Languages';
+$lang['New language:'] = 'New language:';
+$lang['Updated languages'] = 'Updated languages';
+$lang['Featured added'] = 'Featured added';
+$lang['Upgrade'] = 'Upgrade';
+$lang['Automatic upgrade'] = 'Automatic upgrade';
+$lang['Manual Upgrade'] = 'Manual Upgrade';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'We recommend the <b>automatic upgrade</b>.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.';
+$lang['No database upgrade is required.'] = 'No database upgrade is required.';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.';
+?>
---
+++ language/fr_FR/release.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['Release note'] = 'Notes de version';
+$lang['Download Piwigo %s'] = 'Télécharger Piwigo %s';
+$lang['Released on %s'] = 'Sortie le %s';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'La liste des changements majeurs est décrite sur les <a href="%s">notes de la version %s</a>.';
+$lang['Bugs fixed'] = 'Bugs corrigés';
+$lang['Known issues'] = 'Problèmes connus';
+$lang['Languages'] = 'Langues';
+$lang['New language:'] = 'Nouvelles langues :';
+$lang['Updated languages'] = 'Langues mises à jour';
+$lang['Featured added'] = 'Fonctionnalités ajoutées';
+$lang['Upgrade'] = 'Mise à jour';
+$lang['Automatic upgrade'] = 'Mise à jour automatique';
+$lang['Manual Upgrade'] = 'Mise à jour manuelle';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Nous recommandons la <b>mise à jour automatiquea</b>.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Si vous utilisez la <b>version 2.2+</b>, Piwigo vous indiquera quels sont vos plugins qui ne sont pas encore compatibles avec la version %s.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Si vous utilisez actuellement <b>Piwigo %s</b>, vous pouvez télécharger l\'archive <b>%s_to_%s.zip</b> qui ne contient que les fichiers nouveaux ou modifiés.';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Une fois les fichiers extraits, transférez-les par FTP sur votre site par dessus votre installation %s.';
+$lang['No database upgrade is required.'] = 'Votre base de données n\'a pas besoin de mise à jour.';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Si vous utilisez une version plus ancienne que la %s et que vous ne souhaitez pas profiter de la mise à jour automatique, suivez la <b>mise à jour manuelle</b>.';
+?>
