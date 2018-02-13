ee505bd653876d58df081640bf76f21950be2305
plegall <plg@piwigo.org>
2018-02-13 10:07:39 +0100

    [release] FR translations for releases 2.9.1+2.9.2

---
+++ language/fr_FR/release-2.9.1.lang.php
@@ -0,0 +1,17 @@
+<?php
+$lang['porg_issue_707'] = 'nombre de visites, cacher les décimales si moins de 1k';
+$lang['porg_issue_706'] = 'identification devrait rediriger uniquement sur Piwigo';
+$lang['porg_issue_705'] = 'gestion des utilisateurs côté serveur, vérifier les paramètres d\'entrée';
+$lang['porg_issue_701'] = 'le lien de synchronisation d\'un album pointe sur la synchro de ./galleries/ même s\'il s\'agit d\'un autre répertoire';
+$lang['porg_issue_699'] = 'File inclusion dans l\'administration';
+$lang['porg_issue_696'] = 'pwg.categories.delete ne fonctionne pas correctement';
+$lang['porg_issue_693'] = 'Batch downloader plante sur pclzip avec php 7.1';
+$lang['porg_issue_685'] = 'Gestion par lot, la suppression de photos ne fonctionne pas sur Safari';
+$lang['porg_issue_683'] = 'Utilisation disque, cacher les décimales si plus de 100GB';
+$lang['porg_issue_679'] = 'tableau de bord, besoin de trigger pour les plugins';
+$lang['porg_issue_674'] = '[php7.1] popin édition utilisateur, [] operator not supported for strings';
+$lang['porg_issue_667'] = 'Cross Site Scripting(XSS) sur le paramètre "page" de admin.php';
+$lang['porg_issue_663'] = 'Erreur SQL sur la visite guidée 2.9';
+
+$lang['porg_issue_710'] = 'le paramètre d\'entrée \"redirect_decoded\" n\'est pas valide';
+?>
---
+++ language/fr_FR/release-2.9.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['porg_issue_789'] = 'possibilité d\'éviter le message d\'erreur sur la page d\'identification';
+$lang['porg_issue_787'] = 'password.php : une \"action\" non valide affiche une erreur';
+$lang['porg_issue_777'] = 'pwg.categories.getImages, TotalCount';
+$lang['porg_issue_767'] = 'Publier via Shotwell 0.22.0 plante avec Piwigo 2.9.1';
+$lang['porg_issue_760'] = 'La date de création non mise à jour lorsqu\'elle est modifiée sur la page d\'admin';
+$lang['porg_issue_755'] = 'Synchroniser les métadonnées envoie une date de création incorrecte à Mysql 5.7';
+$lang['porg_issue_751'] = 'Impossible d\'exécuter install/piwigo_structure-mysql.sql 2 fois';
+$lang['porg_issue_747'] = 'notice PHP lorsque l\'on change le thème d\'administration';
+$lang['porg_issue_739'] = '[recherche] lorsqu\'une recherche par mot correspond aussi à des tags';
+$lang['porg_issue_732'] = 'renommer la fonction transliterate';
+$lang['porg_issue_731'] = 'tableau de bord, le nombre de photos est absent';
+$lang['porg_issue_726'] = 'trigger à la création d\'un album';
+$lang['porg_issue_725'] = 'protéger pwg.images.setInfo de l\'injection HTML';
+$lang['porg_issue_724'] = 'SQL injection sur la page cat_options';
+$lang['porg_issue_723'] = 'lorsque l\'on demande un album privé, le permalien est révélé';
+$lang['porg_issue_721'] = 'CSRF : passer un album en public';
+$lang['porg_issue_713'] = 'l\'email de notification de mise à jour disponible mélange anglais et français';
+$lang['porg_issue_710'] = 'le paramètre d\'entrée \"redirect_decoded\" n\'est pas valide';
+$lang['porg_issue_678'] = 'Erreur RSS avec PHP 7';
+?>
