6f9167ac87ba6322a566e1c56f472461c96c01f7
plegall <plg@piwigo.org>
2018-02-15 15:57:36 +0100

    [newsletter] translate FR

---
+++ language/fr_FR/newsletters.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['porg_newsletters_desc1'] = 'Gardez le contact avec le projet Piwigo.';
+$lang['porg_newsletters_desc2'] = 'Vous recevrez un email lorsqu\'une nouvelle version sera disponible ou à propos d\'évènements relatifs au projet.';
+$lang['porg_newsletters_subscribe_title'] = 'Abonnez vous à notre newsletter et restez à jour !';
+$lang['Enter your email here'] = 'Votre adresse email';
+$lang['Sign up'] = 'S\'abonner';
+
+?>
