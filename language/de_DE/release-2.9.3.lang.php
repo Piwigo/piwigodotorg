c45c250df7ee40e57a15533b9bb721671aaaa548
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-04 21:08:56 +0100

    Add de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_735'] = 'Fähigkeit einen Authentifizierungsschlüssel über das API zu bekommen';
+$lang['porg_issue_822'] = 'XXS+CSRF Attacke auf Konfigurationsseiten';
+$lang['porg_issue_823'] = 'SQL-Injection in admin/users';
+$lang['porg_issue_824'] = 'Releasebuilder unter MacOS nicht erfolgreich';
+$lang['porg_issue_825'] = 'SQL-Injection im Batchmanager';
+$lang['porg_issue_826'] = 'SQL-Injection in Konfigurationseinstellungen';
+$lang['porg_issue_838'] = '[Batch Manager] löschen von gesamtem Satz erzeugt inkorrekte Fehlermeldung';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - SQL-Injection in Verwaltungsoberfläche';
