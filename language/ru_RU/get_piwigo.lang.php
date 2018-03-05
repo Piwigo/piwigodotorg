b1ede8b961851b9682d8964255e197847357cca2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:47:10 +0100

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/get_piwigo.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_piwigo_desc1'] = 'Получить Piwigo возможно двумя способами.';
+$lang['Enterprises'] = 'Организации';
+$lang['porg_get_piwigo_mobile_desc2'] = 'Можно просматривать всю Вашу галерею в полноэкранном режиме, создавать, изменять, переносить Ваши альбомы или редактировать свойства изображений... всего лишь прикоснувшись кончиками пальцев! :)';
+$lang['porg_get_piwigo_self_hosted_desc2'] = 'Для обеспечения безопасности и должной скорости работы регулярно обновляйте Piwigo с каждым новым релизом. Это, конечно, бесплатно. ';
+$lang['NetInstall'] = 'NetInstall';
+$lang['porg_get_piwigo_desc2'] = 'Можно использовать собственный хостинг, например OVH или BlueHost, либо применяйте облачный сервис Piwigo.com';
+$lang['porg_get_piwigo_cloud_hosted_desc2'] = 'Установка, обновление, резервное копирование полностью управляется инфраструктурой, созданной специально для Piwigo.';
+$lang['porg_get_piwigo_cloud_hosted_desc3'] = 'Вы также получите поддержку от создателей Piwigo.';
+$lang['porg_get_piwigo_self_hosted_desc1'] = 'Запуск Piwigo на Вашем собственном веб-хостинге обеспечат PHP и базы данных MySQL.';
+$lang['porg_get_piwigo_desc3'] = 'В обоих случаях за Вами сохраняются все права собственности на изображения.';
+$lang['porg_get_piwigo_mobile_desc1'] = 'Используете iPhone в качестве камеры? Публикуйте изображения в Piwigo прямо со смартфона.';
+$lang['porg_get_piwigo_cloud_hosted_desc1'] = 'Забудьте про технические вопросы, если Вы начали с piwigo.com';
+$lang['Piwigo Mobile App'] = 'Мобильное приложение Piwigo';
+$lang['Blog'] = 'Блог';
+$lang['Cloud-hosted Piwigo'] = 'Облачный Piwigo';
+$lang['Infrastructure'] = 'Инфраструктура';
+$lang['Install Guide'] = 'Руководство по установке';
+$lang['Self-hosted Piwigo'] = 'Piwigo на собственном хостинге';
+$lang['Upgrade Guide'] = 'Руководство по обновлению';
+$lang['download_piwigo'] = 'Скачать<br>Piwigo';
