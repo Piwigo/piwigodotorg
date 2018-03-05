e75fc783819a5dec340280001a8450fd3ddfaa5e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:47:18 +0100

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,122 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['About'] = 'О';
+$lang['Batch Management'] = 'Пакетное управление';
+$lang['Discover'] = 'Откройте для себя';
+$lang['porg_get_started_desc'] = 'Готовы начать управление своими изображениями с помощью Piwigo? Присоединяйтесь к нашему сообществу!';
+$lang['language_tr_TR'] = 'Турецкий';
+$lang['language_uk_UA'] = 'Украинский';
+$lang['language_vi_VN'] = 'Вьетнамский';
+$lang['language_zh_CN'] = 'Китайский';
+$lang['language_zh_HK'] = 'Гонконгский';
+$lang['language_zh_TW'] = 'Китайский традиционный';
+$lang['language_he_IL'] = 'Иврит';
+$lang['language_hr_HR'] = 'Хорватский';
+$lang['language_hu_HU'] = 'Венгерский';
+$lang['language_id_ID'] = 'Инденезийский';
+$lang['language_nn_NO'] = 'Норвежский';
+$lang['language_pl_PL'] = 'Польский';
+$lang['language_pt_BR'] = 'Португальский Бразильский';
+$lang['language_pt_PT'] = 'Португальский';
+$lang['language_ro_RO'] = 'Румынский';
+$lang['language_ru_RU'] = 'Русский';
+$lang['language_sh_RS'] = 'Сербский';
+$lang['language_sk_SK'] = 'Словацкий';
+$lang['language_sl_SI'] = 'Словенский';
+$lang['language_sr_RS'] = 'Сербский';
+$lang['language_sv_SE'] = 'Шведский';
+$lang['language_ta_IN'] = 'Тамильский';
+$lang['language_th_TH'] = 'Тайландский';
+$lang['language_is_IS'] = 'Исландский';
+$lang['language_it_IT'] = 'Итальянский';
+$lang['language_ja_JP'] = 'Японский';
+$lang['language_ka_GE'] = 'Грузинский';
+$lang['language_km_KH'] = 'Камбоджийский';
+$lang['language_kn_IN'] = 'Каннада';
+$lang['language_ko_KR'] = 'Корейский';
+$lang['language_lt_LT'] = 'Литовский';
+$lang['language_lv_LV'] = 'Латвийский';
+$lang['language_mn_MN'] = 'Монгольский';
+$lang['language_ms_MY'] = 'Малазийский';
+$lang['language_nb_NO'] = 'Норвежский';
+$lang['language_nl_NL'] = 'Датский';
+$lang['Donate'] = 'Пожертвовать';
+$lang['Extensions'] = 'Расширения';
+$lang['Get Involved'] = 'Принять участие';
+$lang['Get started with Piwigo'] = 'Начало работы с Piwigo';
+$lang['High Volume'] = 'Верхний уровень';
+$lang['Open Source'] = 'Открытый исходный код';
+$lang['Showcases'] = 'Витрины';
+$lang['Sustainable'] = 'Устойчивый';
+$lang['[piwigo.org contact form, %s] %s contacted you on %s'] = '[piwigo.org contact form, %s] %s связь через %s';
+$lang['language_es_MX'] = 'Мексиканский';
+$lang['language_et_EE'] = 'Эстонский';
+$lang['language_eu_ES'] = 'Баскский';
+$lang['language_fa_IR'] = 'Персидский';
+$lang['language_fi_FI'] = 'Финский';
+$lang['language_fr_CA'] = 'Французский Канадский';
+$lang['language_fr_FR'] = 'Французский';
+$lang['language_gl_ES'] = 'Гальский';
+$lang['language_dv_MV'] = 'Мальдивский';
+$lang['language_el_GR'] = 'Греческий';
+$lang['language_en_GB'] = 'Английский Британский';
+$lang['language_en_UK'] = 'Английский';
+$lang['language_en_US'] = 'Английский Американский';
+$lang['language_eo_EO'] = 'Эсперанто';
+$lang['language_es_AR'] = 'Аргентинский';
+$lang['language_es_ES'] = 'Испанский';
+$lang['Changelogs'] = 'Список изменений';
+$lang['language_ar_SA'] = 'Арабский/Саудовская Аравия';
+$lang['language_bg_BG'] = 'Болгарский';
+$lang['language_br_FR'] = 'Бретань';
+$lang['language_ca_ES'] = 'Каталонский';
+$lang['language_cs_CZ'] = 'Чешский';
+$lang['language_da_DK'] = 'Датский';
+$lang['language_de_DE'] = 'Немецкий';
+$lang['Testimonials'] = 'Отзывы';
+$lang['Themes'] = 'Темы';
+$lang['Themes & Plugins'] = 'Темы и плагины';
+$lang['What is Piwigo?'] = 'Что такое Piwigo?';
+$lang['What\'s new?'] = 'Что нового?';
+$lang['language_af_ZA'] = 'Африкаанс';
+$lang['language_ar_EG'] = 'Арабский/Египет';
+$lang['Latest news'] = 'Свежие новости';
+$lang['News'] = 'Новости';
+$lang['Newsletters'] = 'Объявления';
+$lang['Organisations'] = 'Организации';
+$lang['Plugins'] = 'Плагины';
+$lang['Press'] = 'Пресса';
+$lang['Professionals'] = 'Профессионалы';
+$lang['See more'] = 'Подробнее';
+$lang['Support'] = 'Поддержка';
+$lang['About us'] = 'О нас';
+$lang['Contact'] = 'Контакты';
+$lang['Demo'] = 'Демо';
+$lang['Documentation'] = 'Документация';
+$lang['Features'] = 'Особенности';
+$lang['Forum'] = 'Форум';
+$lang['Get Piwigo'] = 'Получить Piwigo';
+$lang['Get Started'] = 'Начать';
+$lang['Home'] = 'Домой';
+$lang['Individuals'] = 'Частные лица';
+$lang['Languages'] = 'Языки';
