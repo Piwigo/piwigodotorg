ad95711446bda57594b478f9b232bb5c48929d09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:46:31 +0100

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_735'] = 'ability to get an authentication key through API';
+$lang['porg_issue_822'] = 'используйте xss+csrf attack на странице конфигурации';
+$lang['porg_issue_823'] = 'SQL injection in admin/users';
+$lang['porg_issue_824'] = 'release builder failing under MacOS X';
+$lang['porg_issue_825'] = 'SQL injection on Batch Manager, unit mode';
+$lang['porg_issue_826'] = 'SQL injection in configuration setting';
+$lang['porg_issue_838'] = '[Batch Manager] delete \"all set\" produces an incorrect error message';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - SQL injection in administration panel';
