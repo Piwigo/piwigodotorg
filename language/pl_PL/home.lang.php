63f21b7d6c7d18744d08910fa98ca00cc50f0db0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 06:05:44 +0100

    Add pl_PL, thanks to Sauron

---
+++ language/pl_PL/home.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_home_desc2'] = 'Zaprojektowany dla organizacji, zespołów i osób indywidualnych.';
+$lang['porg_home_open_source_desc'] = 'Kod źródłowy jest dostępny, edytowalny, audytowalny i rozszerzalny dzięki systemowi wtyczek i tematów.';
+$lang['porg_home_title'] = 'Zarządzaj swoja kolekcją zdjęć z Piwigo';
+$lang['Albums Hierarchy'] = 'Hierarchia Albumów';
+$lang['Permissions Control'] = 'Kontrola Uprawnień';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Zrządzaj swoją kolekcją zdjęć';
+$lang['Start discovering Piwigo'] = 'Rozpocznij poznawanie Piwigo';
+$lang['View more showcases'] = 'Pokaż więcej przykładów';
+$lang['Who are Piwigo users?'] = 'Kim są użytkownicy Piwigo?';
+$lang['porg_home_desc1'] = 'Piwigo jest aplikacją "open source" służąca do przechowywania zdjęć w sieci.';
