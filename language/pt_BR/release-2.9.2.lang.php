3e4ae24501b8fe62856d1b26ef0b1c67cf49b33e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:46:39 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.9.2.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_678'] = 'Erro RSS PHP7';
+$lang['porg_issue_710'] = 'O parâmetro de entrada \"redirect_decoded\" não é válido';
+$lang['porg_issue_713'] = 'atualize o correio de notificação em francês/inglês';
+$lang['porg_issue_721'] = 'Relatório de erro: Configuração do álbum privado do administrador para público via CSRF e Permissões Incorretas';
+$lang['porg_issue_723'] = 'ao solicitar um ID privado, o link permanente é revelado';
+$lang['porg_issue_724'] = 'Relatório de erro: Injeção de SQL na página cat_options';
+$lang['porg_issue_725'] = 'proteja pwg.images.setInfo do HTML';
+$lang['porg_issue_726'] = 'gatilho na criação de álbuns';
+$lang['porg_issue_731'] = 'painel de controle, falta de número de fotos';
+$lang['porg_issue_732'] = 'renomear função transliterada';
+$lang['porg_issue_739'] = '[search] quando uma pesquisa por palavra também corresponde a etiquetas';
+$lang['porg_issue_747'] = 'Aviso PHP ao alterar o tema da página de administração';
+$lang['porg_issue_751'] = 'Não é possível executar install/piwigo_structure-mysql.sql duas vezes';
+$lang['porg_issue_755'] = 'Sincronizar os metadados envia uma criação de data incorreta para Mysql 5.7';
+$lang['porg_issue_760'] = 'A criação de data não foi atualizada quando da mudança para picture_modify';
+$lang['porg_issue_767'] = 'Publicação via Shotwell 0.22.0 falha com Piwigo 2.9.1';
+$lang['porg_issue_777'] = 'Categorias WS getImages, TotalCount';
+$lang['porg_issue_787'] = 'inesperada \"action\" para password.php exibe erros';
+$lang['porg_issue_789'] = 'capacidade de evitar mensagens de erro na página de logon';
