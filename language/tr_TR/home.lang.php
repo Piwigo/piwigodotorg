c9a60f99a03eae9093aaf6662a3de9bf0293d793
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 06:05:30 +0100

    Add tr_TR, thanks to LazBoy

---
+++ language/tr_TR/home.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_home_high_volume_desc'] = 'Piwigo, sıra binlerce veya hatta yüz binlerce fotoğrafın sınıflandırılmasına geldiğinde öne çıkıyor, parlıyor.';
+$lang['porg_home_organisation_title'] = 'Binlerce organizasyon ve milyonlarca kişi Piwigo\'yu kullanmayı çok seviyor';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Fotoğraf kolleksiyonunuzu yönetin';
+$lang['porg_home_desc2'] = 'Organizasyonlar, takımlar ve kişisel kullanımlar için geliştirilmiştir.';
+$lang['porg_home_open_source_desc'] = 'Mevcut açık kaynak kodu, eklentiler ve temalar sayesinde düzenlenebilir, denetlenebilir ve genişletilebilir.';
+$lang['porg_home_sustainable_desc'] = 'Piwigi 2002\'de doğduğundan beri 15 yıldan fazla süredir kullanıcılarına destek vermekte. Her zaman gelişmekte!';
+$lang['View more showcases'] = 'Daha fazla örnek göster';
+$lang['porg_home_title'] = 'Fotoğraf kolleksiyonunuzu Piwigo ile yönetin';
+$lang['Albums Hierarchy'] = 'Albüm Hiyerarşisi';
+$lang['Permissions Control'] = 'İzin Kontrolleri';
+$lang['Start discovering Piwigo'] = 'Piwigo\'yu keşfetmeye başla';
+$lang['Who are Piwigo users?'] = 'Piwigo kullanıcıları kimler?';
+$lang['porg_home_desc1'] = 'Piwigo, web üzerinde çalışan açık kaynak kodlu fotoğraf galerisi programıdır.';
