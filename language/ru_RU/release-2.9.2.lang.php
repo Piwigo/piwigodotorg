1d61718ddfb6c12128b3f767ef739b940a9fa102
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:46:47 +0100

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/release-2.9.2.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_755'] = 'Синхронизация метаданных с датой создания, некорректной для Mysql 5.7';
+$lang['porg_issue_760'] = 'Дата создания не обновлена после изменения picture_modify';
+$lang['porg_issue_787'] = 'отображение ошибки  \"action\" for password.php';
+$lang['porg_issue_713'] = 'обновите адрес e-mail (смесь английского и французского)';
+$lang['porg_issue_710'] = 'ввод неправильного параметра \"redirect_decoded\"';
+$lang['porg_issue_724'] = 'Bug Report: SQL injection in page cat_options';
+$lang['porg_issue_725'] = 'protect pwg.images.setInfo from HTML';
+$lang['porg_issue_739'] = '[search] когда искомое слово совпадает с тэгом';
+$lang['porg_issue_678'] = 'Ошибка RSS PHP 7';
+$lang['porg_issue_721'] = 'Сообщение об ошибке: Администратор установил частный просмотр альбома для общественности через CSRF и ограничения';
+$lang['porg_issue_723'] = 'при запросе частного id открывается постоянная ссылка';
+$lang['porg_issue_726'] = 'триггер на создание альбома';
+$lang['porg_issue_731'] = 'число отсутствующих изображений, на приборной панели';
+$lang['porg_issue_732'] = 'переименование функции';
+$lang['porg_issue_747'] = 'Уведомление PHP при изменении темы страницы админа';
+$lang['porg_issue_751'] = 'Нельзя запустить install/piwigo_structure-mysql.sql дважды';
+$lang['porg_issue_767'] = 'При публикации через Shotwell 0.22.0 происходит сбой с Piwigo 2.9.1';
+$lang['porg_issue_777'] = 'Категории getImages WS, TotalCount';
+$lang['porg_issue_789'] = 'возможност избежать сообщения об ошибке на странице входа';
