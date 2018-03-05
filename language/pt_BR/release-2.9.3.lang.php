986b7abf94d54f03130ffa6858ef8357d6638e35
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:46:22 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_735'] = 'capacidade de obter uma chave de autenticação através da API';
+$lang['porg_issue_822'] = 'use xss+csrf acesso em páginas de configuração';
+$lang['porg_issue_823'] = 'Injeção SQL em admin/users';
+$lang['porg_issue_824'] = 'gerador de lançamento falhando no MacOS X';
+$lang['porg_issue_825'] = 'Injeção SQL no Gerenciador de lote, modo unidade';
+$lang['porg_issue_826'] = 'Injeção SQL na configuração';
+$lang['porg_issue_838'] = '[Batch Manager] delete \"all set\" produz uma mensagem de erro incorreta';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - Injeção de SQL no painel de administração';
