517ae5a84f361cdd4d2016a748d3dfc434fe9c9d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:46:06 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/urls.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_doc_urls']['install'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:install';
+$lang['porg_doc_urls']['requirements'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:install:before';
+$lang['porg_doc_urls']['upgrade'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade';
+$lang['porg_doc_urls']['upgrade_auto'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade:upgrade_automatic';
+$lang['porg_doc_urls']['upgrade_manual'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade:upgrade_manual';
+$lang['porg_urls']['about-us'] = 'Sobre nós';
+$lang['porg_urls']['changelogs'] = 'changelogs';
+$lang['porg_urls']['coding-activity'] = 'atividade de codificação';
+$lang['porg_urls']['contact'] = 'contato';
+$lang['porg_urls']['features'] = 'características';
+$lang['porg_urls']['get-involved'] = 'envolver-se';
+$lang['porg_urls']['get-piwigo'] = 'obter o piwigo';
+$lang['porg_urls']['get-started'] = 'iniciar';
+$lang['porg_urls']['news'] = 'notícias';
+$lang['porg_urls']['newsletters'] = 'boletim de notícias';
+$lang['porg_urls']['press'] = 'imprensa';
+$lang['porg_urls']['release'] = 'lançamento';
+$lang['porg_urls']['testimonials'] = 'depoimentos';
+$lang['porg_urls']['what-is-piwigo'] = 'O que é o piwigo';
