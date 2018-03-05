00f0281482f9017e32a0039e6022bd0c02664466
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-05 18:47:02 +0100

    Add nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/get_piwigo.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_piwigo_mobile_desc2'] = 'U kunt ook in de volledige schermmodus door uw hele galerij bladeren, uw albums maken / bewerken / verplaatsen of foto-eigenschappen bewerken ... met uw vingertoppen!';
+$lang['porg_get_piwigo_self_hosted_desc1'] = 'Run Piwigo op uw eigen webhosting, met PHP en een MySQL-database.';
+$lang['porg_get_piwigo_self_hosted_desc2'] = 'Piwigo zelf kost niets, maar u moet Piwigo bijwerken wanneer er nieuwe releases beschikbaar zijn om het veilig en snel te houden.';
+$lang['Cloud-hosted Piwigo'] = 'Cloud-gehoste Piwigo';
+$lang['Enterprises'] = 'Bedrijven';
+$lang['Infrastructure'] = 'Infrastructuur';
+$lang['Install Guide'] = 'Installatiehandleiding';
+$lang['NetInstall'] = 'NetInstall';
+$lang['Piwigo Mobile App'] = 'Piwigo Mobiele App';
+$lang['Self-hosted Piwigo'] = 'Zelf gehoste Piwigo';
+$lang['Upgrade Guide'] = 'Upgradehandleiding';
+$lang['porg_get_piwigo_cloud_hosted_desc1'] = 'Vergeet technische taken als u besluit om met Piwigo.com in zee te gaan.';
+$lang['porg_get_piwigo_cloud_hosted_desc2'] = 'Installatie, updates en back-ups worden volledig beheerd via een speciale infrastructuur, op maat gemaakt voor Piwigo.';
+$lang['porg_get_piwigo_cloud_hosted_desc3'] = 'U krijgt ook toegewijde steun van de makers van Piwigo';
+$lang['porg_get_piwigo_desc1'] = 'Er zijn twee manieren om Piwigo te verkrijgen.';
+$lang['porg_get_piwigo_desc2'] = 'Of u host zelf, bijvoorbeeld met OVH of BlueHost, of u kunt de cloudservice van Piwigo.com gebruiken.';
+$lang['porg_get_piwigo_desc3'] = 'In beide gevallen blijft u eigenaar van uw gegevens.';
+$lang['porg_get_piwigo_mobile_desc1'] = 'Gebruikt u uw iPhone als camera? Publiceer uw foto\'s rechtstreeks van uw smartphone naar uw Piwigo.';
+$lang['Blog'] = 'Blog';
+$lang['download_piwigo'] = 'Download<br>Piwigo';
