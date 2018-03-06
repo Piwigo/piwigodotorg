0d1293481c61bfe2d6ba95f3a8d0e7f74adafefa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-06 22:08:12 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.9.2.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_678'] = 'PHP 7 RSS Fehler';
+$lang['porg_issue_710'] = 'Eingabeparameter \"redirect_decoded\" ist ungültig';
+$lang['porg_issue_713'] = 'Updatemail in Französisch/Deutsch-Mix';
+$lang['porg_issue_721'] = 'Bug Report: Privates Album kann durch CSRF & fehlerhafte Berechtigungen auf öffentlich gesetzt werden';
+$lang['porg_issue_723'] = 'beim Anfordern einer privaten ID wird der Permalink gezeigt';
+$lang['porg_issue_724'] = 'Bug Report: SQL-Injektion auf Seite cat-options';
+$lang['porg_issue_725'] = 'HTML in pwg.images.setInfo verhindern';
+$lang['porg_issue_726'] = 'Trigger bei Albumerstellung';
+$lang['porg_issue_731'] = 'Dashboard, fehlende Nummer von Fotos';
+$lang['porg_issue_732'] = 'Übersetzungfunktion umbenannt';
+$lang['porg_issue_739'] = '[Suche] wenn eine Suche nach Worten auch auf Tags passt ';
+$lang['porg_issue_747'] = 'PHP Notice beim Ändern des Themas für die Administrationsseiten';
+$lang['porg_issue_751'] = 'install/piwigo_structure-mysql.sql kann nicht zweimal ausgeführt werden';
+$lang['porg_issue_755'] = 'Metadaten Synchronisation sendet Erstellungsdatum fehlerhaft an MySQL 5.7';
+$lang['porg_issue_760'] = 'Erstellungsdatum wird nicht aktualisiert nach der Änderung in picture_modify';
+$lang['porg_issue_767'] = 'Veröffentlichung mit ShotWell 0.22.0 auf Piwigo 2.9.1 schlägt fehl';
+$lang['porg_issue_777'] = 'WS Kategorien getImages, Gesamtanzahl';
+$lang['porg_issue_787'] = 'unerwartete \"action\" für password.php Anzeigefehler';
+$lang['porg_issue_789'] = 'Fähigkeit Fehler auf der Login-Seite zu unterdrücken';
