0d4597f8851ea97244ae1a4da6bb0d39eb11a452
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-06 22:08:27 +0100

    Add da_DK, thanks to Kaare

---
+++ language/da_DK/home.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Albums Hierarchy'] = 'Albumhierarki';
+$lang['Permissions Control'] = 'Rettighedskontrol';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Hold styr på din fotosamling';
+$lang['Start discovering Piwigo'] = 'Begynd at gå på opdagelse i Piwigo';
+$lang['View more showcases'] = 'Se flere præsentationer';
+$lang['Who are Piwigo users?'] = 'Hvem er Piwigos brugere?';
+$lang['porg_home_desc1'] = 'Piwigo er open source-fotogallerisoftware til nettet.';
+$lang['porg_home_desc2'] = 'Designet til organisatorer, grupper og enkeltpersoner.';
+$lang['porg_home_high_volume_desc'] = 'Piwigo brillierer når det gælder om at klassificere tusinder eller endda hundredtusinder af fotografier.';
+$lang['porg_home_open_source_desc'] = 'Kildekoden er tilgængelig, redigerbar, auditerbar og udvidbar takket være plugins og temaer.';
+$lang['porg_home_organisation_title'] = 'Tusinder af organisationer og millioner af enkeltpersoner elsker at benytte Piwigo';
+$lang['porg_home_sustainable_desc'] = 'Siden sin fødsel i 2002 har Piwigo hjulpet sine brugere i flere end 15 år. Altid under udvikling!';
+$lang['porg_home_title'] = 'Hold styr på din fotosamling med Piwigo';
