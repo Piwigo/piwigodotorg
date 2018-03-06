1708975a83d1467483b02e82b6947a6bddb0c507
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-06 22:08:05 +0100

    Add da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.9.2.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_678'] = 'RSS-fejl i PHP 7';
+$lang['porg_issue_710'] = 'inddataparameteret \"redirect_decoded\" er ikke gyldigt';
+$lang['porg_issue_713'] = 'mail med opdateringsnotifikation på en blanding af fransk og engelsk';
+$lang['porg_issue_721'] = 'Fejlrapport: Administrators opsætning af privat album som offentligt gennem CSRF og ukorrekte rettigheder';
+$lang['porg_issue_723'] = ' når der bedes om en privat id, afsløres permalinket';
+$lang['porg_issue_724'] = 'Fejlrapport: SQL-indsprøjtning i siden cat_options';
+$lang['porg_issue_725'] = 'beskytter pwg.images.setinfo fra HTML';
+$lang['porg_issue_726'] = 'udløser albumoprettelse';
+$lang['porg_issue_731'] = 'instrumentbræt, manglende antal fotografier';
+$lang['porg_issue_732'] = 'omdøber translitterationsfunktion';
+$lang['porg_issue_739'] = '[søgning] når der søges efter ord matches der også på tags';
+$lang['porg_issue_747'] = 'PHP-besked når administratorsidens tema ændres';
+$lang['porg_issue_751'] = 'Kan ikke køre install/piwigo_structure-mysql.sql to gange';
+$lang['porg_issue_755'] = 'Synkronisering af metadata sender en ukorrekt oprettelsedato i MySQL 5.7';
+$lang['porg_issue_760'] = 'Oprettelsesdato genopfriskes ikke når den ændres fra picture_modify';
+$lang['porg_issue_767'] = 'Udgivelse via Shotwell 0.22.0 fejler med Piwigo 2.9.1';
+$lang['porg_issue_777'] = 'WS-kategorier getImages, TotalCount';
+$lang['porg_issue_787'] = 'uventet \"action\" i password.php viser fejl';
+$lang['porg_issue_789'] = 'mulighed for at undgå fejlmeddelelser på loginside';
