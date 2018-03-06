0dfa962d62f471f0ffe4a48f45ea15a93b11645f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-06 22:07:50 +0100

    Add da_DK, thanks to Kaare

---
+++ language/da_DK/urls.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_doc_urls']['install'] = 'http://da.piwigo.org/doc/doku.php?id=brugerdokumentation:lær:installering';
+$lang['porg_doc_urls']['requirements'] = 'http://da.piwigo.org/doc/doku.php?id=brugerdokumentation:lær:installering:før';
+$lang['porg_doc_urls']['upgrade'] = 'http://da.piwigo.org/doc/doku.php?id=brugerdokumentation:lær:opgradering';
+$lang['porg_doc_urls']['upgrade_auto'] = 'http://da.piwigo.org/doc/doku.php?id=brugerdokumentation:lær:opgrader:opgrader_automatisk';
+$lang['porg_doc_urls']['upgrade_manual'] = 'http://da.piwigo.org/doc/doku.php?id=brugerdokumentation:lær:opgrader:opgrader_manuelt';
+$lang['porg_urls']['about-us'] = 'om-os';
+$lang['porg_urls']['changelogs'] = 'aendringslogger';
+$lang['porg_urls']['coding-activity'] = 'kodeaktivitet';
+$lang['porg_urls']['contact'] = 'kontakt';
+$lang['porg_urls']['features'] = 'funktioner';
+$lang['porg_urls']['get-involved'] = 'bliv-involveret';
+$lang['porg_urls']['get-piwigo'] = 'hent-piwigo';
+$lang['porg_urls']['get-started'] = 'kom-i-gang';
+$lang['porg_urls']['news'] = 'nyheder';
+$lang['porg_urls']['newsletters'] = 'nyhedsbrev';
+$lang['porg_urls']['press'] = 'presse';
+$lang['porg_urls']['release'] = 'udgivelse';
+$lang['porg_urls']['testimonials'] = 'anbefalinger';
+$lang['porg_urls']['what-is-piwigo'] = 'hvad-er-piwigo';
