97154b622bb45f3853e11a36b95895e8226b870c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-06 22:07:43 +0100

    Add da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,122 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['language_ar_EG'] = 'Arabisk/Ægypten';
+$lang['language_zh_HK'] = 'Hongkong';
+$lang['language_zh_TW'] = 'Traditionelt kinesisk';
+$lang['language_ms_MY'] = 'Malaysisk';
+$lang['language_nb_NO'] = 'Norsk';
+$lang['language_nl_NL'] = 'Hollandsk';
+$lang['language_nn_NO'] = 'Norsk';
+$lang['language_pl_PL'] = 'Polsk';
+$lang['language_pt_BR'] = 'Portugisisk Brasilien';
+$lang['language_pt_PT'] = 'Portugisisk';
+$lang['language_ro_RO'] = 'Romænsk';
+$lang['language_ru_RU'] = 'Russisk';
+$lang['language_sh_RS'] = 'Serbisk';
+$lang['language_sk_SK'] = 'Slovakisk';
+$lang['language_sl_SI'] = 'Slovensk';
+$lang['language_sr_RS'] = 'Serbisk';
+$lang['language_sv_SE'] = 'Svensk';
+$lang['language_ta_IN'] = 'Tamil';
+$lang['language_th_TH'] = 'Thai';
+$lang['language_tr_TR'] = 'Tyrkisk';
+$lang['language_uk_UA'] = 'Ukrainsk';
+$lang['language_vi_VN'] = 'Vietnamesisk';
+$lang['language_zh_CN'] = 'Kinesisk';
+$lang['language_dv_MV'] = 'Dhivehi-Maldiverne';
+$lang['language_fa_IR'] = 'Persisk';
+$lang['language_fi_FI'] = 'Finsk';
+$lang['language_fr_CA'] = 'Fransk canadisk';
+$lang['language_fr_FR'] = 'Fransk';
+$lang['language_gl_ES'] = 'Galisisk';
+$lang['language_he_IL'] = 'Hebræisk';
+$lang['language_hr_HR'] = 'Kroatisk';
+$lang['language_hu_HU'] = 'Ungarsk';
+$lang['language_id_ID'] = 'Indonesisk';
+$lang['language_is_IS'] = 'Islandsk';
+$lang['language_it_IT'] = 'Italiensk';
+$lang['language_ja_JP'] = 'Japansk';
+$lang['language_ka_GE'] = 'Georgisk';
+$lang['language_km_KH'] = 'Khmer';
+$lang['language_kn_IN'] = 'Kannada';
+$lang['language_ko_KR'] = 'Koreansk';
+$lang['language_lt_LT'] = 'Litauisk';
+$lang['language_lv_LV'] = 'Lettisk';
+$lang['language_mn_MN'] = 'Mongolsk';
+$lang['language_af_ZA'] = 'Afrikaans';
+$lang['language_ar_SA'] = 'Arabisk/Saudi';
+$lang['language_bg_BG'] = 'Bulgarsk';
+$lang['language_br_FR'] = 'Bretonsk';
+$lang['language_ca_ES'] = 'Catalansk';
+$lang['language_cs_CZ'] = 'Tjekkisk';
+$lang['language_da_DK'] = 'Dansk';
+$lang['language_de_DE'] = 'Tysk';
+$lang['language_el_GR'] = 'Græsk';
+$lang['language_en_GB'] = 'Britisk engelsk';
+$lang['language_en_UK'] = 'Engelsk';
+$lang['language_en_US'] = 'Amerikansk engelsk';
+$lang['language_eo_EO'] = 'Esperanto';
+$lang['language_es_AR'] = 'Argentina';
+$lang['language_es_ES'] = 'Spansk';
+$lang['language_es_MX'] = 'Mexicansk';
+$lang['language_et_EE'] = 'Estisk';
+$lang['language_eu_ES'] = 'Baskisk';
+$lang['porg_get_started_desc'] = 'Er du klar til at gå i gang med at organisere dine fotografier med Piwigo? Bliv en del af vores fællesskab!';
+$lang['Latest news'] = 'Seneste nyt';
+$lang['News'] = 'Nyheder';
+$lang['Newsletters'] = 'Nybedsbreve';
+$lang['Open Source'] = 'Open Source';
+$lang['Organisations'] = 'Organisationer';
+$lang['Plugins'] = 'Plugins';
+$lang['Press'] = 'Presse';
+$lang['Professionals'] = 'Professionelle';
+$lang['See more'] = 'Se mere';
+$lang['Showcases'] = 'Præsentationer';
+$lang['Support'] = 'Support';
+$lang['Sustainable'] = 'Bæredygtig';
+$lang['Testimonials'] = 'Anmeldelser';
+$lang['Themes'] = 'Temaer';
+$lang['Themes & Plugins'] = 'Temaer og plugins';
+$lang['What is Piwigo?'] = 'Hvad er Piwigo?';
+$lang['What\'s new?'] = 'Hvad er nyt?';
+$lang['[piwigo.org contact form, %s] %s contacted you on %s'] = '[piwigo.org-kontaktformular, %s] %s kontaktede dig om %s';
+$lang['About us'] = 'Om os';
+$lang['Batch Management'] = 'Batchhåndtering';
+$lang['Changelogs'] = 'Ændringslogger';
+$lang['Contact'] = 'Kontakt';
+$lang['Demo'] = 'Demo';
+$lang['Discover'] = 'Opdag';
+$lang['Documentation'] = 'Dokumentation';
+$lang['Donate'] = 'Donér';
+$lang['Extensions'] = 'Udvidelser';
+$lang['Features'] = 'Funktioner';
+$lang['Forum'] = 'Forum';
+$lang['Get Involved'] = 'Bliv involveret';
+$lang['Get Piwigo'] = 'Hent Piwigo';
+$lang['Get Started'] = 'Kom i gang';
+$lang['Get started with Piwigo'] = 'Kom i gang med Piwigo';
+$lang['High Volume'] = 'Stor mængde';
+$lang['Home'] = 'Hjem';
+$lang['Individuals'] = 'Enkeltpersoner';
+$lang['Languages'] = 'Sprog';
+$lang['About'] = 'Om';
