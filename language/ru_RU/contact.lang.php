7f62caf50112f39e3a094cd84b1a0d82e2b2759c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-07 11:22:42 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Category'] = 'Категория';
+$lang['Contact us'] = 'Связь с нами';
+$lang['Partnership'] = 'Сотрудничество';
+$lang['Press inquiry'] = 'Запрос';
+$lang['Security report'] = 'Отчет безопасности';
+$lang['Send message'] = 'Отправка сообщения';
+$lang['Testimonial'] = 'Отзыв';
+$lang['Your email address'] = 'Ваш e-mail';
+$lang['Your message'] = 'Ваше ссобщение';
+$lang['porg_contact_desc1'] = 'Вы можете отправить сообщение, используя контактную форму ниже.';
+$lang['porg_contact_desc2'] = 'Пожалуйста, используйте это главным образом для замечаний по сайту.';
+$lang['porg_contact_desc3'] = 'Мы предпочитаем использовать форумы поддержки для любой обратной связи, это облегчает отслеживание группой.';
+$lang['porg_contact_error_email'] = 'Упс, адрес-то неверный...';
+$lang['porg_contact_error_message'] = 'Упс, с Вашим сообщением что-то пошло не так...';
