926d16665d558e46d7faf8bc9ec03eb1d6409ed1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-07 11:22:34 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/features.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_browse_date_desc'] = 'Цифровые фотоаппараты сохраняют дату в фотографии и Piwigo использует эти данные, чтобы показать Вашу коллекцию фото в календаре и позволить Вам рассмотреть изображения, созданные в конкретные дни.';
+$lang['porg_features_desc3'] = 'Новые особенности добавляются Piwigo на постоянной основе, на базе идей команды и обсуждений с коллективом пользователей.';
+$lang['porg_features_geolocate_desc2'] = 'Затем, с плагином Google Maps или OpenStreetMap, Piwigo может показывать Ваши изображения на интерактивной карте.';
+$lang['porg_features_user_management_desc1'] = 'Администраторы могут подклоючать пользователей, создавать группы, управлять разрешениями и отправлять уведомления.';
+$lang['porg_features_user_management_desc2'] = 'Одновременно в Piwigo могут работать сразу несколько администраторов.';
+$lang['porg_features_user_management_desc3'] = 'Идеально подходит для Вашей команды!';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Множество параметров конфигурации позволяют сделать Piwigo полностью соответствующим Вашим нуждам.';
+$lang['porg_features_organize_desc1'] = 'Размещайте Ваши изображения по собственным альбомам.';
+$lang['porg_features_organize_desc2'] = 'Дерево альбомов имеет неограниченную глубину.';
+$lang['porg_features_organize_desc3'] = 'Каждое изображение может принадлежать к нескольким альбомам.';
+$lang['porg_features_plugins_desc1'] = 'Изменение внешнего вида с помощью тем.';
+$lang['porg_features_plugins_desc2'] = 'Добавление функций с помощью плагинов.';
+$lang['porg_features_plugins_desc3'] = 'Для установки расширений понадобится несколько кликов мышкой.';
+$lang['porg_features_plugins_desc4'] = '<span class="bold">350 расширений</span> доступно сразу, и их число продолжает расти!';
+$lang['porg_features_privacy_desc1'] = 'Сделайте Ваши изображения приватными - решите сами, кто сможет их видеть.';
+$lang['porg_features_privacy_desc2'] = 'Можно устанавливать разрешения и ограничени на альбомы и фотографии, для групп или отдельных пользователей.';
+$lang['porg_features_desc1'] = 'Piwigo предлагает широкий набор функций для любой фото-галереи.';
+$lang['porg_features_desc2'] = 'Ниже перечислены некоторые из доступных функций.';
+$lang['porg_features_easy_install_desc1'] = 'Мы разработали Piwigo так, чтобы это было суперпросто в установке и с минимальным требованиям.';
+$lang['porg_features_easy_install_desc2'] = 'К примеру, обновление происходит по нажатию единственной кнопки!';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo может читать GPS-координаты (широту  и долготу) из имеющихся метаданных.';
+$lang['A great place for your visitors'] = 'Прекрасное место для Ваших посетителей';
+$lang['API'] = 'API';
+$lang['Batch Manager'] = 'Пакетный менеджер';
+$lang['Browse by Dates'] = 'Поиск по датам';
+$lang['Easy installation and updates'] = 'Простота установки и обновления';
+$lang['Full screen slideshow'] = 'Полноэранный режим слайдшоу';
+$lang['Geolocate your photos on a map'] = 'Геолокация Вашиз изображений на карте';
+$lang['Import automatic your metadata'] = 'Автоматический импорт Ваших метаданных';
+$lang['Privacy on your Photos'] = 'Конфиденциальность Ваших изображений';
+$lang['Upload your photos'] = 'Загрузка Ваших изображений';
+$lang['Users management'] = 'Управление пользователями';
+$lang['porg_features_api_desc2'] = 'Столь же просто, как HTTP-запросы и JSON-ответы.';
