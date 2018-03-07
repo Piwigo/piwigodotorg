0c57904f83cba9c87bbfd8e7f11c2eff501585b8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-07 06:10:36 +0100

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/get_piwigo.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_piwigo_desc3'] = 'W obu przypadkach zachowujesz pełne prawa własności do swoich danych.';
+$lang['Blog'] = 'Blog';
+$lang['Cloud-hosted Piwigo'] = 'Piwigo w Chmurze';
+$lang['Enterprises'] = 'Przedsiębiorstwa';
+$lang['Infrastructure'] = 'Infrastruktura';
+$lang['Install Guide'] = 'Podręcznik instalacji';
+$lang['NetInstall'] = 'Instalacja sieciowa';
+$lang['Piwigo Mobile App'] = 'Aplikacja Mobilna Piwigo';
+$lang['Upgrade Guide'] = 'Podręcznik Aktualizacji';
+$lang['porg_get_piwigo_self_hosted_desc1'] = 'Uruchom Piwigo na swoich serwerze WWW z obsługą PHP i bazy danych MySQL.';
