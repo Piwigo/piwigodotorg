6a3b73f2b2114f5e5ebea58ae060b8b468a81691
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-07 21:31:38 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Category'] = 'Kategorie';
+$lang['Contact us'] = 'Schreibe uns';
+$lang['Partnership'] = 'Partnerschaft';
+$lang['Press inquiry'] = 'Presseanfragen';
+$lang['Security report'] = 'Sicherheitsbericht';
+$lang['Send message'] = 'Nachricht senden';
+$lang['Testimonial'] = 'Referenzen';
+$lang['Your email address'] = 'Deine E-Mailadresse';
+$lang['Your message'] = 'Deine Nachricht';
+$lang['porg_contact_desc1'] = 'Du kannst uns eine mit dem Kontaktformular eine Nachricht schicken.';
+$lang['porg_contact_desc2'] = 'Bitte benutze dies hauptsächlich für Bemerkungen zur Webseite.';
+$lang['porg_contact_desc3'] = 'Wir präferieren das Supportforum für Feedback. Das vereinfacht die Nachverfolgung für das Team.';
+$lang['porg_contact_error_email'] = 'Ups, diese E-mailadresse ist ungültig.';
+$lang['porg_contact_error_message'] = 'Ups, deine Nachricht scheint falsch zu sein.';
