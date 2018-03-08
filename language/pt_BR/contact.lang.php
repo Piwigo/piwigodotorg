9aa9a5d2440f2d0ba2895e08351ff9332003e332
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-08 20:22:51 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Category'] = 'Categoria';
+$lang['Contact us'] = 'Contate-Nos';
+$lang['Partnership'] = 'Parceria';
+$lang['Press inquiry'] = 'Inquérito à imprensa';
+$lang['Security report'] = 'Relatório de segurança';
+$lang['Send message'] = 'Enviar mensagem';
+$lang['Testimonial'] = 'Testemunho';
+$lang['Your email address'] = 'Seu endereço de email';
+$lang['Your message'] = 'Sua mensagem';
+$lang['porg_contact_desc1'] = 'Você pode deixar uma mensagem usando o formulário de contato abaixo.';
+$lang['porg_contact_desc2'] = 'Use isso principalmente para comentários no site.';
+$lang['porg_contact_desc3'] = 'Preferimos usar os fóruns de suporte para qualquer comentário, facilita o rastreamento pela equipe.';
+$lang['porg_contact_error_email'] = 'Ups, esse endereço de e-mail é inválido';
+$lang['porg_contact_error_message'] = 'Por favor, sua mensagem parece estar errada';
