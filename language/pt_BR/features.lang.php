1dab024d92aa53410ccaf240e84db89551552cc0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-08 20:22:43 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/features.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_easy_install_desc1'] = 'Criamos o Piwigo para ser muito fácil de instalar, com os requisitos mínimos.';
+$lang['porg_features_easy_install_desc2'] = 'Então, as atualizações com um único botão para clicar!';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo pode ler a latitude e a longitude do GPS a partir de metadados incorporados.';
+$lang['porg_features_geolocate_desc2'] = 'Então, com o plugin Google Maps ou OpenStreetMap, o Piwigo pode exibir suas fotos em um mapa interativo.';
+$lang['porg_features_great_place_desc'] = 'Seus visitantes podem publicar comentários, dar classificações, marcar fotos como favoritas, realizar buscas e receber notificações de notícias por e-mail.';
+$lang['porg_features_metadata_desc'] = 'Por exemplo, uma descrição IPTC pode ser importada para propriedades de fotos, exibida ao lado da foto ... e pesquisável!';
+$lang['porg_features_organize_desc1'] = 'Distribua suas fotos entre seus próprios álbuns.';
+$lang['porg_features_organize_desc2'] = 'A árvore do álbum tem profundidade ilimitada.';
+$lang['porg_features_organize_desc3'] = 'Cada foto pode pertencer a vários álbuns.';
+$lang['porg_features_plugins_desc1'] = 'Mude a aparência com os temas.';
+$lang['porg_features_plugins_desc2'] = 'Adicione recursos com plugins.';
+$lang['porg_features_plugins_desc3'] = 'As extensões requerem apenas alguns cliques para se instalar.';
+$lang['porg_features_plugins_desc4'] = '<span class="bold">350 extensões</span> disponíveis e crescendo!';
+$lang['porg_features_privacy_desc1'] = 'Faça suas fotos privadas e decida quem pode ver cada uma delas.';
+$lang['porg_features_privacy_desc2'] = 'Você pode definir permissões em álbuns e fotos, para grupos ou usuários individuais.';
+$lang['porg_features_slideshow_desc'] = 'Com o plugin Fotorama, ou temas específicos como Bootstrap Darkroom, experimente a apresentação de slides em tela cheia.';
+$lang['porg_features_upload_desc'] = 'Você pode adicionar fotos com o formulário da Web, qualquer cliente de FTP ou uma aplicação de desktop, como digiKam, Shotwell, Lightroom ou<br>aplicações móveis.';
+$lang['porg_features_user_management_desc1'] = 'Os administradores podem criar usuários, grupos, gerenciar permissões e enviar notificações.';
+$lang['porg_features_user_management_desc2'] = 'Vários administradores podem trabalhar ao mesmo tempo no Piwigo.';
+$lang['porg_features_user_management_desc3'] = 'Perfeito para sua equipe!';
+$lang['A great place for your visitors'] = 'Um ótimo lugar para seus visitantes';
+$lang['API'] = 'API';
+$lang['Batch Manager'] = 'Gerente de Lote';
+$lang['Browse by Dates'] = 'Procurar por Datas';
+$lang['Easy installation and updates'] = 'Fácil instalação e atualizações';
+$lang['Full screen slideshow'] = 'Apresentação de slides em tela cheia';
+$lang['Geolocate your photos on a map'] = 'Geolocalize suas fotos em um mapa';
+$lang['Import automatic your metadata'] = 'Importe automaticamente seus metadados';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Muitas configurações para fazer Piwigo atender às suas próprias necessidades';
+$lang['Organize with Album'] = 'Organize com um Álbum';
+$lang['Privacy on your Photos'] = 'Privacidade em suas fotos';
+$lang['Upload your photos'] = 'Envie suas fotos';
+$lang['Users management'] = 'Gerenciamento de usuários';
+$lang['porg_features_api_desc1'] = 'A API da web de Piwigo torna mais fácil para os desenvolvedores executar ações de outras aplicações: exibir miniaturas em uma publicação no blog do WordPress, publicar fotos da Lightroom, sincronizar uma pasta local com um script Perl ...';
+$lang['porg_features_api_desc2'] = 'Tão simples como pedidos HTTP e respostas JSON.';
+$lang['porg_features_batch_manager_desc'] = 'Filtre fotos da sua coleção, faça uma seleção e aplique ações em lote: altere o autor, adicione algumas etiquetas, associe a um novo álbum, configure a localização geográfica ...';
+$lang['porg_features_browse_date_desc'] = 's câmeras digitais armazenam a data na foto e Piwigo usa essa data para exibir sua coleção de fotos em um calendário e permitem que você navegue de um dia para o outro.';
+$lang['porg_features_desc1'] = 'Piwigo oferece um amplo conjunto de recursos para qualquer galeria de fotos.';
+$lang['porg_features_desc2'] = 'Muitos dos recursos disponíveis estão listados abaixo.';
+$lang['porg_features_desc3'] = 'Novos recursos são adicionados ao Piwigo de forma contínua, por causa da codificação de idéias e discussões da equipe com a comunidade de usuários.';
