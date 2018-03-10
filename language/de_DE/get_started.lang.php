37e776c4e1684c0d91558fd14316141d9ffa1266
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-10 12:18:50 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/get_started.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_started_desc1'] = 'Wie Sie ihr selbstgehostetes Piwigo zum Beispiel auf ihrem eigenen Host laufen lassen können.';
+$lang['porg_get_started_desc2'] = 'Was müssen Sie vorbereiten, was herunterladen und wie installieren?';
+$lang['Installation guide'] = 'Installationsanleitung';
+$lang['Requirements'] = 'Voraussetzungen';
+$lang['Still got some unanswered questions ?'] = 'Sind noch Fragen offen?';
+$lang['porg_get_started_requirement_desc1'] = 'Was sind die technischen Voraussetzungen um Piwigo auf Ihrem eigenen Host laufen zu lassen?';
+$lang['porg_get_started_requirement_desc2'] = 'PHP, Datenbank, Webserver, Bildverarbeitung... Hier die Details.';
