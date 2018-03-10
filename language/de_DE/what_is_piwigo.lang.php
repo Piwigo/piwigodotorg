680cb38fd5e8e43e7bd4adda1e82e57e68643953
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-10 12:19:13 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/what_is_piwigo.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo kann auch <em>as a service</em> auf Piwigo<span style="color:#ff3363">.com</span> betrieben werden: in wenigen Sekunden kann ein Zugang angelegt werden um Piwigo zu testen ohne sich um technische Details zu kümmern.';
+$lang['porg_whatispiwigo_how_to_desc1'] = 'Sie können Piwigo von Piwigo<span style="color:#f70;">.org</span> herunterladen und auf ihrem Webhost mit dem passenden technischen Veraussetzungen installieren.';
+$lang['porg_whatispiwigo_organisation_desc2'] = 'Im <em>Digital Asset Management</em> Bereich fokussiert sich Piwigo auf einfache Benutzung, Erschwinglichkeit, Performance und Nachhaltigkeit.';
+$lang['porg_whatispiwigo_individual_desc1'] = 'Das Familienalbum der letzten 20 Jahre verwalten, die Sammlung der europäischen Spinnen organisieren, die besten Landschaftsfotos eines Hobbyfotografen zeigen...';
+$lang['porg_whatispiwigo_use_desc2'] = 'Von mittelgroßen Galerien<br> bis riesigen Bildbanken kann Piwigo Ihr Leben einfacher machen sowie Zeit und Geld sparen.';
+$lang['porg_whatispiwigo_gnu_desc3'] = 'Open-Source oder freie Software bedeutet, dass Sie den Quelltext unter bestimmten Bedingungen modifizieren und weiterverteilen können.';
+$lang['porg_whatispiwigo_gnu_desc5'] = 'Der Unterschied zwischen den beiden Bedeutung von frei wird oft als  "frei wie in Redefreiheit, nicht wie in Freibier" beschrieben.';
+$lang['porg_whatispiwigo_individual_desc2'] = 'Dies sind ein paar Beispiele, wie Piwigo benutzt wird.';
+$lang['porg_whatispiwigo_desc2'] = 'Mit seinen umfangreichen Features werden Fotogallerien überall auf der Welt betrieben.';
+$lang['porg_whatispiwigo_gnu_desc2'] = 'Open-Source heißt nicht nur, das der Quelltext eingesehen werden kann - es hat auch politische und philosophische Implikationen.';
+$lang['porg_whatispiwigo_organisation_desc1'] = 'Öffentliche Hand, Firmen, Stiftungen... alle müssen ihre eigene Sammlung verwalten.';
+$lang['porg_whatispiwigo_sustainable_desc1'] = 'Seit 2002 seiner Geburt in 2002 hat Piwigo klar den <em>Erwachsenstatus</em> erreicht.';
+$lang['porg_whatispiwigo_use_desc1'] = 'Unter den Piwigo-Benutzern gibt es eine Vielzahl von verschiedenen Anwendungen.';
+$lang['porg_whatispiwigo_professional_desc1'] = 'Professionelle Fotografen müssen ihre Arbeit ihren Kunden (öffenbtlich oder privat) zeigen.';
+$lang['porg_whatispiwigo_sustainable_desc2'] = 'Natürlich werden Verbesserungen gemacht, um aktuelle Technologien zu verwenden!';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo ist eine Webapplikation um Fotos und andere Medien zu verwalten.';
+$lang['porg_whatispiwigo_high_volume_desc1'] = '500.000 Fotos in der Sammlung? Kein Problem für Piwigo.';
+$lang['porg_whatispiwigo_high_volume_desc2'] = 'Blitzschnelle Geschwindigkeit ist selbstverständlich.';
+$lang['porg_whatispiwigo_how_to_desc2'] = 'Piwigo ist kostenlos, Sie bezahlen nur Ihren Hoster.';
+$lang['porg_whatispiwigo_how_to_desc4'] = 'Ein Piwigo.com account verursacht Hostingkosten.';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo war und wird immer freie Software bleiben.';
+$lang['porg_whatispiwigo_open_source_desc2'] = 'Ein Quelltext, den man lesen und verstehen kann, ist ein Quelltext, dem man vertrauen kann.';
+$lang['porg_whatispiwigo_professional_desc2'] = 'Piwigo erlaubt fortgeschrittene Zugriffrechte.';
+$lang['porg_whatispiwigo_professional_desc3'] = 'Kunden können einzelne Fotos oder ganze Alben herunterladen.';
+$lang['Amateurs'] = 'Amateure';
+$lang['Batch Downloader'] = 'Stapeldownloader';
+$lang['Collections'] = 'Sammlungen';
+$lang['Families'] = 'Familien';
+$lang['Fansites'] = 'Fanseiten';
+$lang['GNU License'] = 'GNU Lizenz';
+$lang['How to use Piwigo ?'] = 'Wie benutze ich Piwigo?';
+$lang['Lightning Fast'] = 'Blitzschnell';
+$lang['Multiple Administrators'] = 'Mehrere Administratoren';
+$lang['Share with Clients'] = 'Teilen Sie mit Kunden';
+$lang['What can I use Piwigo for ?'] = 'Wofür kann ich Piwigo benutzen?';
+$lang['Why choosing Piwigo ?'] = 'Warum Piwigo wählen?';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'The GNU Public License (GPL) ist eine Open-Source-Lizenz.';
+$lang['porg_whatispiwigo_gnu_desc4'] = 'Frei bezieht sich nicht auf den Preis, sondern auf Freiheit. ';
+$lang['porg_whatispiwigo_gnu_desc6'] = 'Die GPL ist frei wie die Rede.';
