c89e5de65e70430ddb37303235135413ef1095ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-10 15:32:14 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/get_started.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Installation guide'] = 'guia de instalação';
+$lang['Requirements'] = 'Requisitos';
+$lang['Still got some unanswered questions ?'] = 'Ainda tem algumas perguntas sem resposta?';
+$lang['porg_get_started_desc1'] = 'Como executar sua auto-hospedagem Piwigo, ou seja, em sua próprio servidor.';
+$lang['porg_get_started_desc2'] = 'O que você precisa para preparar, o que baixar, como instalar.';
+$lang['porg_get_started_download_desc'] = 'Baixe o pacote Piwigo, incluindo todos os arquivos, ou apenas o script NetInstall, que irá baixar o pacote diretamente para o seu servidor.';
+$lang['porg_get_started_guide_desc'] = 'Saiba como instalar o Piwigo em seu servidor, desde credenciais no banco de dados, o que baixar, até a primeira conexão em sua biblioteca de fotos.';
+$lang['porg_get_started_question_desc1'] = 'Você pode obter alguma ajuda no <a href="%s">fórum</a> ou encontrar suas próprias respostas na <a href="%s">documentação</a>';
+$lang['porg_get_started_requirement_desc1'] = 'Quais são os pré-requisitos técnicos para executar Piwigo em seu próprio servidor?';
+$lang['porg_get_started_requirement_desc2'] = 'PHP, banco de dados, servidor web, processamento de imagem ... Encontre todos os detalhes.';
