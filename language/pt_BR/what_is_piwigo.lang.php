b5818c09bad3070765b5777968962cb06c4ebfae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-10 15:32:07 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/what_is_piwigo.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_high_volume_desc1'] = '500.000 fotos em sua coleção? Não é um problema para Piwigo.';
+$lang['porg_whatispiwigo_high_volume_desc2'] = 'Os desempenhos rápidos são incluídos, por padrão.';
+$lang['porg_whatispiwigo_how_to_desc1'] = 'Você pode baixar o Piwigo da Piwigo<span style="color:#f70;">.org</span> e instalá-lo em seu servidor na web, com os requisitos técnicos apropriados.';
+$lang['porg_whatispiwigo_how_to_desc2'] = 'Usar o Piwigo é gratuito, você só paga seu provedor de hospedagem.';
+$lang['porg_whatispiwigo_how_to_desc3'] = 'iwigo também vem <em>como um serviço</em> no Piwigo <span style ="color:#ff3363">.com</span>: leva apenas alguns segundos para abrir uma conta de teste, use o Piwigo e esqueça detalhes técnicos.';
+$lang['porg_whatispiwigo_how_to_desc4'] = 'A conta Piwigo.com envolve taxas de hospedagem.';
+$lang['porg_whatispiwigo_individual_desc1'] = 'Gerenciando álbuns familiares nos últimos 20 anos, organizando uma coleção de aranhas européias, mostrando suas melhores fotos de paisagens como fotógrafo amador...';
+$lang['porg_whatispiwigo_individual_desc2'] = 'Estes são alguns exemplos de como Piwigo é usado por sua comunidade.';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo sempre foi, e sempre será um software livre.';
+$lang['porg_whatispiwigo_open_source_desc2'] = 'Um código que você pode ler e entender é um código em que você pode confiar.';
+$lang['porg_whatispiwigo_organisation_desc1'] = 'Setor público, empresas, fundações ... todos eles têm que gerenciar sua própria coleção.';
+$lang['porg_whatispiwigo_organisation_desc2'] = 'No campo <em>Gestão de Ativos Digitais</em>, Piwigo se concentra na facilidade de uso, acessibilidade, desempenho e sustentabilidade.';
+$lang['porg_whatispiwigo_professional_desc1'] = 'Os fotógrafos profissionais precisam mostrar seu trabalho aos seus clientes, publicamente ou em particular.';
+$lang['porg_whatispiwigo_professional_desc2'] = 'O Piwigo fornece controle avançado de permissões.';
+$lang['porg_whatispiwigo_professional_desc3'] = 'Os clientes podem baixar fotos uma a uma ou álbuns inteiros.';
+$lang['porg_whatispiwigo_sustainable_desc1'] = 'Nascido em 2002, Piwigo atingiu claramente o nível de <em>maturidade</em>.';
+$lang['porg_whatispiwigo_sustainable_desc2'] = 'Claro que continua evoluindo para combinar com técnicas atuais!';
+$lang['porg_whatispiwigo_use_desc1'] = 'Existe uma grande variedade de usos entre a comunidade de usuários de Piwigo.';
+$lang['porg_whatispiwigo_use_desc2'] = 'De galerias de tamanho médio para bancos de imagens enormes, Piwigo pode facilitar sua vida e poupar tempo e esforço.';
+$lang['Amateurs'] = 'Amadores';
+$lang['Batch Downloader'] = 'Baixar Arquivo em Lote';
+$lang['Collections'] = 'Coleções';
+$lang['Families'] = 'Famílias';
+$lang['Fansites'] = 'Sites de fãs';
+$lang['GNU License'] = 'Licença GNU';
+$lang['How to use Piwigo ?'] = 'Como usar o Piwigo?';
+$lang['Lightning Fast'] = 'À velocidade de um relâmpago';
+$lang['Multiple Administrators'] = 'Múltiplos Administradores';
+$lang['Share with Clients'] = 'Compartilhe com clientes';
+$lang['What can I use Piwigo for ?'] = 'Para o que posso usar o Piwigo?';
+$lang['Why choosing Piwigo ?'] = 'Por que escolher o Piwigo?';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo é uma aplicação web para gerenciar sua coleção de fotos e outros mídeas.';
+$lang['porg_whatispiwigo_desc2'] = 'Incorporando recursos poderosos, gerencia galerias de fotos ao redor do mundo.';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'A GNU General Public License, ou GPL, é uma licença de código aberto.';
+$lang['porg_whatispiwigo_gnu_desc2'] = 'A fonte aberta não significa apenas que você pode ver o código-fonte - também tem implicações políticas e filosóficas.';
+$lang['porg_whatispiwigo_gnu_desc3'] = 'Fonte aberta, ou "Software livre", significa que você pode modificar e redistribuir o código fonte sob certas condições.';
+$lang['porg_whatispiwigo_gnu_desc4'] = 'Livre não se refere ao preço, refere-se à liberdade.';
+$lang['porg_whatispiwigo_gnu_desc5'] = 'A diferença entre os dois significados de livre é muitas vezes caracterizada como "Livre como na fala versus livre como na cerveja".';
+$lang['porg_whatispiwigo_gnu_desc6'] = 'A GPL é gratuita como na fala.';
