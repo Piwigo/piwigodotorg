34a652389f89e313f38e33ad96067e09ece9cf15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-10 15:32:22 +0100

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_822'] = 'użycie ataku xss+csrf na strony konfiguracji';
+$lang['porg_issue_823'] = '"SQL injection" w trybie administratora/użytkownika';
+$lang['porg_issue_824'] = 'automat tworzący wydania nie działał pod MacOS X';
+$lang['porg_issue_838'] = '[Zarządca Wsadowy] kasowanie "całego setu" zgłaszało nieprawidłowy komunikat błędu ';
+$lang['porg_issue_735'] = 'możliwość pobrania klucza autoryzacyjnego przez API';
+$lang['porg_issue_825'] = '"SQL injection" w managerze wsadowym';
+$lang['porg_issue_826'] = '"SQL injection" w ustawieniach konfiguracji';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - "SQL injection" w panelu administracyjnym';
