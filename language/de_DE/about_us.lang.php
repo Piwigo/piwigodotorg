1482d859a3ea7eaf94b4f68cb87ec5a871bf3784
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-10 12:19:06 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/about_us.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Recruitement'] = 'Personalaufbau';
+$lang['porg_about_us_launch_desc'] = 'Piwigo aus der Cloud für jedermann.';
+$lang['porg_about_us_recruitement_desc'] = 'Das Team wird größer und die Programmieraktivitäten wachsen schneller.';
+$lang['porg_about_us_service_desc1'] = 'Einige Programmierer oder Themenersteller werden von Kunden bezahlt um neue Features zu implementieren.';
+$lang['porg_about_us_service_desc2'] = 'Meistens stehen diese Arbeiten auch der gesamten Piwigogemeinschaft zur Verfügung.';
+$lang['Get involved!'] = 'Machen Sie mit!';
+$lang['porg_about_us_desc2'] = 'Wenn Sie an unsere Open-Source-Mission glauben, hoffen wir, dass sie mitmachen!';
+$lang['porg_about_us_donation_desc2'] = 'Das Geld wird benutzt um Kosten wie Hosting, Domainnamen oder Appstoresubscriptions zu bezahlen.';
+$lang['porg_about_us_dotcom_desc1'] = 'Erwirtschaftete Gewinne machen es möglich Zeit für Piwigo aufzuwenden.';
+$lang['porg_about_us_dotcom_desc2'] = 'Pierrick der Schöpfer von Piwigo zum Beispiel wird dank Piwigo.com bezahlt und kann an Piwigo.org arbeiten.';
+$lang['porg_about_us_join_desc1'] = 'Das Piwigo-Projekt braucht verschiedene Fähigkeiten wie Programmierer, Designer, Autoren für Dokumentation, Helfer.';
+$lang['porg_about_us_join_desc2'] = 'Wenn Sie an unsere Mission glauben, werden Sie Teil davon :-)';
+$lang['Forum messages'] = 'Forennachrichten';
+$lang['Piwigo moves to GitHub'] = 'Piwigo zieht zu GitHub um.';
+$lang['Piwigo.com is launched'] = 'Piwigo.com wird gestartet.';
+$lang['Years old'] = 'Jahre alt';
+$lang['You?'] = 'Sie?';
+$lang['porg_about_us_apps_desc'] = 'Piwigo veröffentlicht Apps für iPhone/iPad und Android.';
+$lang['porg_about_us_beginning_desc1'] = 'Verfügbar auf free.fr mit einer Ein-Klick-Installation.';
+$lang['porg_about_us_birth_desc'] = 'Nach Monaten der Vorbereitung wird Version 1.0.0 veröffentlicht.';
+$lang['Arrivals of Plugins'] = 'Einführung von Plugins';
+$lang['Beginning'] = 'Anfang';
+$lang['Birth'] = 'Geburt';
+$lang['Business Model'] = 'Geschäftsmodell';
+$lang['Donations'] = 'Spenden';
+$lang['Downloads'] = 'Downloads';
+$lang['Mobile apps'] = 'Handyapps';
+$lang['Our Mission'] = 'Unsere Mission';
+$lang['PhpWebGallery becomes Piwigo'] = 'PhpWebGallery wird Piwigo';
+$lang['Piwigo.com'] = 'Piwigo.com';
+$lang['Take part in our adventure'] = 'Werden Sie Teil unseres Abenteuers';
+$lang['Want to be part of the adventure?'] = 'Wollen Sie Teil des Abenteuers sein?';
