2976c83377d00a5f049f1f4b0250a5dfaee5f10b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-11 16:59:46 +0100

    Update pl_PL, thanks to T00kie

---
+++ language/pl_PL/features.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_desc1'] = 'Piwigo oferuje bogaty zestaw funkcji dla każdej galerii.';
+$lang['porg_features_desc2'] = 'Poniżej lista wielu dostępnych funkcji';
+$lang['porg_features_desc3'] = 'Nowe funkcje dodawane są na bieżąco dzięki współpracy developerów i użytkowników.';
+$lang['porg_features_easy_install_desc1'] = 'Zaprojektowaliśmy Piwigo tak aby instalacja była banalna i niewymagająca.';
+$lang['porg_features_easy_install_desc2'] = 'Aktualizujesz tylko jednym kliknięciem!';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo jest w stanie czytać szerokość i wysokość geograficzną z metadanych.';
+$lang['porg_features_geolocate_desc2'] = 'Aby, używając wtyczki Google Mapa lub OpenStreetMap, wyświetlać Twoje zdjęcia na interaktywnej mapie.';
+$lang['porg_features_great_place_desc'] = 'Twoi goście mogą komentować, oceniać zdjęcia, zaznaczać jako ulubione, wyszukiwać oraz być powiadamianym mailowo o zmianach.';
+$lang['porg_features_browse_date_desc'] = 'Aparaty cyfrowe dodają znacznik daty do robionych zdjęć, Piwigo używa tych dat do wyświetlania twojej kolekcji w kalendarzu i ułatwia przeglądanie zdjęć dzień po dniu.';
+$lang['porg_features_batch_manager_desc'] = 'Filtrowanie zdjęć z twojej kolekcji, masowe operacje poprzez wielokrotnie zaznaczenie, zmiana autora, dodawanie tagów, przypisywanie do albumu, ustawianie geolokalizacji...';
+$lang['porg_features_api_desc2'] = 'Prosto jak zapytania HTTP i odpowiedzi JSON';
+$lang['porg_features_api_desc1'] = 'Interfejs API Piwigo ułatwia użytkownikom interakcje z innymi aplikacjami: wyświetlanie miniaturek w WordPressie, publikowanie zdjęć w Lightroomie, synchronizację folderów za pomocą skryptu Perl...';
+$lang['Users management'] = 'Zarządzanie użytkownikami';
+$lang['Upload your photos'] = 'Wrzucanie zdjęć';
+$lang['Privacy on your Photos'] = 'Ustawienia prywatności twoich zdjęć';
+$lang['Organize with Album'] = 'Porządkowanie wg albumów';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Wiele ustawień konfiguracyjnych aby dopasować Piwigo do twoich potrzeb';
+$lang['Import automatic your metadata'] = 'Automatyczny import twoich metadat';
+$lang['Geolocate your photos on a map'] = 'Geolokalizacja twoich zdjęć na mapie';
+$lang['Full screen slideshow'] = 'Pełnoekranowy pokaz slajdów';
+$lang['Easy installation and updates'] = 'Łatwa instalacja i aktualizacje';
+$lang['Batch Manager'] = 'Menadżer wsadowy';
+$lang['Browse by Dates'] = 'Przeglądaj wg dat';
+$lang['API'] = 'API';
+$lang['A great place for your visitors'] = 'Wspaniałe miejsce dla Twoich gości';
