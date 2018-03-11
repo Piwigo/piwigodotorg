14d9a53fc7dc0a4e3264a96ad0ebc9ddaa9764e6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-11 16:58:33 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/get_piwigo.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Blog'] = 'Blog';
+$lang['Cloud-hosted Piwigo'] = 'Piwigo.com, chiavi in mano';
+$lang['Enterprises'] = 'Aziende';
+$lang['Infrastructure'] = 'Infrastruttura';
+$lang['Install Guide'] = 'Guida all\'installazione';
+$lang['NetInstall'] = 'NetInstall';
+$lang['Piwigo Mobile App'] = 'Applicazione mobile Piwigo';
+$lang['Self-hosted Piwigo'] = 'Web hosting per Piwigo';
+$lang['Upgrade Guide'] = 'Guida all\'aggiornamento';
+$lang['download_piwigo'] = 'Scaricare<br>Piwigo';
+$lang['porg_get_piwigo_cloud_hosted_desc1'] = 'Dimentica le attività tecniche se scegli Piwigo.com.';
+$lang['porg_get_piwigo_cloud_hosted_desc2'] = 'Installazione, aggiornamenti, backup sono gestiti automaticamente su un\'infrastruttura dedicata ed ottimizzata per Piwigo.';
+$lang['porg_get_piwigo_cloud_hosted_desc3'] = 'Incluso un supporto dedicato.';
+$lang['porg_get_piwigo_desc1'] = 'Ci sono due modi per usare Piwigo.';
+$lang['porg_get_piwigo_desc2'] = 'O usi un web hosting personale, ad esempio con OVH o o2switch, o direttamente su Piwigo.com.';
+$lang['porg_get_piwigo_desc3'] = 'In entrambi i casi hai il pieno controllo sui tuoi dati.';
+$lang['porg_get_piwigo_mobile_desc1'] = 'Usi il tuo iPhone come fotocamera? Pubblica le tue foto direttamente su Piwigo.';
+$lang['porg_get_piwigo_mobile_desc2'] = 'Puoi anche navigare nella tua galleria a schermo intero, creare/modificare/spostare i tuoi album o modificare le proprietà delle tue foto ... con la punta delle dita!';
+$lang['porg_get_piwigo_self_hosted_desc1'] = 'Esegui Piwigo sul tuo hosting con PHP e MySQL.';
+$lang['porg_get_piwigo_self_hosted_desc2'] = 'Usa Piwigo gratuitamente ma pianifica di gestire attentamente gli aggiornamenti per mantenere la tua applicazione sicura e veloce.';
