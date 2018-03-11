865341525530d58834b2dd6416d213280bf4ac2c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-11 16:59:25 +0100

    Update tr_TR, thanks to Nakre

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['language_tr_TR'] = 'Türkçe';
+$lang['What is Piwigo?'] = 'Piwigo nedir?';
+$lang['What\'s new?'] = 'Yenilikler nelerdir?';
+$lang['Latest news'] = 'Son haberler';
+$lang['News'] = 'Haberler';
+$lang['Newsletters'] = 'Haber Bülteni';
+$lang['Open Source'] = 'Açık Kaynak';
+$lang['Plugins'] = 'Eklentiler';
+$lang['See more'] = 'Daha Fazlası';
+$lang['Support'] = 'Yardım';
+$lang['Themes'] = 'Temalar';
+$lang['About'] = 'Hakkında';
+$lang['About us'] = 'Hakkımızda';
+$lang['Changelogs'] = 'Yenilikler';
+$lang['Contact'] = 'İletişim';
+$lang['Demo'] = 'Demo';
+$lang['Discover'] = 'Keşfet';
+$lang['Donate'] = 'Bağış';
+$lang['Extensions'] = 'Eklentiler';
+$lang['Features'] = 'Özellikler';
+$lang['Forum'] = 'Forum';
+$lang['Get Piwigo'] = 'Piwigo İndir';
+$lang['Get Started'] = 'Başlangıç';
+$lang['Home'] = 'Anasayfa';
+$lang['Languages'] = 'Diller';
