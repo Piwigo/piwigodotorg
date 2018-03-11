8a6fb3f94f63fab3610242b37869bfdd7facff71
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-11 16:58:09 +0100

    Update tr_TR, thanks to katpatuka, Nakre

---
+++ language/tr_TR/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Partnership'] = 'Ortaklık';
+$lang['Press inquiry'] = 'Basın sorgusu';
+$lang['Security report'] = 'Güvenlik raporu';
+$lang['Testimonial'] = 'Değerlendirme';
+$lang['porg_contact_desc2'] = 'Lütfen bunu esas olarak web sitesinin yorumları için kullanın.';
+$lang['porg_contact_desc3'] = 'Geri bildirimler için destek forumları kullanmayı tercih ederiz cünkü bu yöntem ekibimiz tarafından izleme işini kolaylaştırır.';
+$lang['Category'] = 'Kategori';
+$lang['Contact us'] = 'İletişim';
+$lang['Send message'] = 'Mesaj gönder';
+$lang['Your email address'] = 'Eposta adresiniz';
+$lang['Your message'] = 'Mesajınız';
+$lang['porg_contact_desc1'] = 'Aşağıdaki form ile mesaj gönderebilirsiniz';
+$lang['porg_contact_error_email'] = 'Bu eposta adresi geçersizdir';
+$lang['porg_contact_error_message'] = 'Mesajınız geçersizdir';
