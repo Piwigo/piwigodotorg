c2899da728437d42b8aaafcacacaa3440bc0c1d9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-11 16:58:55 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_735'] = 'possibilità di recuperare una chiave di autoidentificazione tramite l\'API Web';
+$lang['porg_issue_822'] = 'difetto di sicurezza xss+csrf nelle pagine di configurazione';
+$lang['porg_issue_823'] = 'iniezione SQL sulla gestione degli utenti';
+$lang['porg_issue_824'] = 'lo script di generazione della release si arresta in modo anomalo su MacOS X';
+$lang['porg_issue_825'] = 'iniezione SQL sulla gestione per lotti, in modalità unitaria';
+$lang['porg_issue_826'] = 'iniezione SQL nella pagina di configurazione';
+$lang['porg_issue_838'] = '[Gestione per lotti] eliminare \"tutto il lotto\" genera un messaggio di errore errato';
+$lang['porg_issue_839'] = 'iniezione SQL sul pannello di amministrazione';
