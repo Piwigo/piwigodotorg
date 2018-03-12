9945e1c671bb87392ff0ab59e645948621a23c0a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:38:37 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-2.9.3.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_735'] = '
+mogelijkheid om een ​​authenticatiesleutel te krijgen via AP';
+$lang['porg_issue_822'] = 'gebruik xss + csrf aanval op configuratiepagina\'s';
+$lang['porg_issue_823'] = 'SQL-injectie in admin / gebruikers';
+$lang['porg_issue_824'] = '
+release builder mislukt onder MacOS X';
+$lang['porg_issue_825'] = '
+SQL-injectie op Batch Manager, unit-modus';
+$lang['porg_issue_826'] = 'SQL-injectie in configuratie-instelling';
+$lang['porg_issue_838'] = '
+[Batchbeheer] delete \ "all set \" levert een onjuiste foutmelding op';
+$lang['porg_issue_839'] = '
+Piwigo v2.9.2 - SQL-injectie in beheerspaneel';
