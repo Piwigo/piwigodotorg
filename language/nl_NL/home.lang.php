f233a4f8dbd6fa1cf00eb48da91321fb0ad906d5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:38:45 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/home.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Albums Hierarchy'] = 'Hierarchie Albums';
+$lang['Permissions Control'] = 'Controle toestemming';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo-beheer van jouw foto-collectie';
+$lang['Start discovering Piwigo'] = 'Start het verkennen van Piwigo';
+$lang['View more showcases'] = '
+Bekijk meer vitrines';
+$lang['Who are Piwigo users?'] = 'Wie zijn Piwigo-gebruikers?';
+$lang['porg_home_desc1'] = 'Piwigo is open source foto-galerij-software voor het internet.';
+$lang['porg_home_desc2'] = 'Ontworpen voor organisaties, teams en individuelen';
+$lang['porg_home_high_volume_desc'] = '
+Piwigo schittert als het gaat om het classificeren van duizenden of zelfs honderdduizenden foto\'s.';
+$lang['porg_home_open_source_desc'] = '
+Broncode is beschikbaar, bewerkbaar, controleerbaar en uitbreidbaar dankzij plug-ins en thema\'s.';
+$lang['porg_home_organisation_title'] = '
+Duizenden organisaties en miljoenen mensen gebruiken Piwigo graag.';
+$lang['porg_home_sustainable_desc'] = '
+Piwigo werd in 2002 geboren en ondersteunt al meer dan 15 jaar haar gebruikers. Altijd evoluerend!';
+$lang['porg_home_title'] = 'Beheer je fotocollectie met Piwigo';
