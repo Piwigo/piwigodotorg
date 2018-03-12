41ef3b9cf15f2b9585939b6e602905adf0bd5fa2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:37:00 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_contact_desc3'] = 'Alle andre tilbagemeldinger foretrækker vi at få gennem supportforummet, da det dermed er lettere at holde styr på henvendelserne.';
+$lang['Category'] = 'Kategori';
+$lang['Contact us'] = 'Kontakt os';
+$lang['Partnership'] = 'Partnerskab';
+$lang['Press inquiry'] = 'Pressehenvendelser';
+$lang['Security report'] = 'Sikkerhedsrapport';
+$lang['Send message'] = 'Send besked';
+$lang['Testimonial'] = 'Anmeldelser';
+$lang['Your email address'] = 'Din mailadresse';
+$lang['Your message'] = 'Din besked';
+$lang['porg_contact_desc1'] = 'Du kan sende en besked gennem kontaktformularen herunder.';
+$lang['porg_contact_desc2'] = 'Funktionen er primært beregnet til kommentarer vedrørende hjemmesiden.';
+$lang['porg_contact_error_email'] = 'Ups, den mailadresse er ugyldig';
+$lang['porg_contact_error_message'] = 'Ups, din besked lader til at være forkert';
