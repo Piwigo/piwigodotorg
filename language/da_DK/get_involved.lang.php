50bee06135c910ac9237faffc4afe42c000253a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:37:15 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/get_involved.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_involved_desc2'] = 'Der er talrige måder at bidrage på, afhængigt af dine færdigheder og den tid, du ønsker at bruge på projektet.';
+$lang['porg_get_involved_dev_plugins_desc'] = 'Har du brug for en funktion, som endnu ikke findes i Piwigo? Lav en plugin, udgiv den og gør den tilgængelig for fællessskabet!';
+$lang['porg_get_involved_donate_desc1'] = 'Ønsker du at hjælpe, men har ikke tiden eller de tekniske færdigheder?';
+$lang['porg_get_involved_donate_desc2'] = 'Ønsker du at sikre at Piwigo infrastruktur forbliver stærk og ved godt helbred?';
+$lang['porg_get_involved_donate_desc3'] = 'Hvorfor ikke overveje en månedlig donation?';
+$lang['porg_get_involved_donate_desc4'] = 'De penge, der indsamles af Piwigo Association, anvendes til at betale direkte omkostninger, så som hosting af piwigo.org, domænenavne, tilmelding til markedsplads (Apple AppStore)...';
+$lang['porg_get_involved_donate_desc5'] = 'Betaling med';
+$lang['porg_get_involved_grateful_desc1'] = 'Tak for dit bidrag og din involvering.';
+$lang['porg_get_involved_grateful_desc2'] = 'Vi sætter altid pris på alle arbejde og hjælp til udviklingen af Piwigo og dets fællesskab!';
+$lang['porg_get_involved_how_to_code_desc'] = 'Vælg issues på Github, lav en fork af Piwigo...<br>... og indsend pull-rerequests.';
+$lang['porg_get_involved_improvment_desc'] = 'Brug forummet til at dele dine ideer<br>til hvordan Piwigo kan forbedres.';
+$lang['porg_get_involved_recommend_desc'] = '... til kommunikationsafdelingen i din virksomhed<br> or på flere websteder.';
+$lang['porg_get_involved_security_desc1'] = 'Hjælp os med at opbygge det mest sikre program til fotohåndtering.';
+$lang['porg_get_involved_security_desc2'] = 'Hvis du har fundet et potentielt sikkerhedproblem i Piwigo-software eller en mobilapplikation, så rapporter det her.';
+$lang['porg_get_involved_translate_desc1'] = 'Oversæt Piwigo til dit sprog, ret hvad der mangler, er forkert eller kan forbedres.';
+$lang['porg_get_involved_translate_desc2'] = 'Det gælder både selve Piwigo og udvidelserne.';
+$lang['Bugtracker'] = 'Fejlsporing';
+$lang['Coding documentation'] = 'Kodedokumentation';
+$lang['Create a report'] = 'Opret en rapport';
+$lang['Develop plugins'] = 'Udvikling af plugins';
+$lang['Donate to Piwigo'] = 'Doner til Piwigo';
+$lang['Help the Community'] = 'Hjælp fællesskabet';
+$lang['Make this a monthly gift'] = 'Giv en månedlig gave';
+$lang['Recommend Piwigo'] = 'Anbefal Piwigo';
+$lang['Security Report'] = 'Sikkerhedsrapport';
+$lang['Submit bugs'] = 'Indsend fejl';
+$lang['Submit product improvment'] = 'Indsend produktforbedringer';
+$lang['Translate'] = 'Oversæt';
+$lang['Translation tool'] = 'Oversættelsesværktøj';
+$lang['We are grateful !'] = 'Vi er taknemmelige!';
+$lang['You know how to code?'] = 'Kan du programmere?';
+$lang['Your Gift'] = 'Din gave';
+$lang['porg_get_involved_bugs_desc1'] = 'Har du fundet en fejl i Piwigo? Rapporter den i vores fejlsporingssystem... eller i forumet, hvis du ikke er sikker.';
+$lang['porg_get_involved_bugs_desc2'] = 'Beskriv hvordan fejlen kan genskabes.';
+$lang['porg_get_involved_community_desc'] = 'Hjælp andre brugere i forummet<br>og mød andre Piwigo-brugere.';
+$lang['porg_get_involved_desc1'] = 'Piwigo er opbygget af et aktivt fællesskab af brugere og udviklere, der sammen udvikler et fantastisk program til fotohåndtering.';
