5887cdd36ed69c693305d2dcac352c56c8e4113c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:35:52 +0100

    Update en_UK, thanks to Kaare

--- language/en_UK/what_is_piwigo.lang.php
+++ language/en_UK/what_is_piwigo.lang.php
@@ -2,7 +2,7 @@
 
 $lang['porg_whatispiwigo_desc1'] = 'Piwigo is a web application to manage your collection of photos, and other medias.';
 $lang['porg_whatispiwigo_desc2'] = 'Embedding powerful features, it powers photo galleries all around the world.';
-$lang['What can I use Piwigo for ?'] = 'What can I use Piwigo for ?';
+$lang['What can I use Piwigo for ?'] = 'What can I use Piwigo for?';
 $lang['porg_whatispiwigo_use_desc1'] = 'There is a wide variety of usages among Piwigo users community.';
 $lang['porg_whatispiwigo_use_desc2'] = 'From medium sized galleries<br> to huge image banks, Piwigo can make your life easier and save you time and effort.';
 $lang['porg_whatispiwigo_organisation_desc1'] = 'Public sector, companies, foundations... all of them have to manage their own collection.';
@@ -20,14 +20,14 @@ $lang['Amateurs'] = 'Amateurs';
 $lang['Families'] = 'Families';
 $lang['Collections'] = 'Collections';
 $lang['Fansites'] = 'Fansites';
-$lang['Why choosing Piwigo ?'] = 'Why choosing Piwigo ?';
+$lang['Why choosing Piwigo ?'] = 'Why choosing Piwigo?';
 $lang['porg_whatispiwigo_high_volume_desc1'] = '500,000 photos in your collection? Not a problem for Piwigo.';
 $lang['porg_whatispiwigo_high_volume_desc2'] = 'Lightning fast performances is included, by default.';
 $lang['porg_whatispiwigo_sustainable_desc1'] = 'Born in 2002, Piwigo has clearly reached the <em>maturity</em> level.';
 $lang['porg_whatispiwigo_sustainable_desc2'] = 'Of course it keeps evolving to match current techs!';
 $lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo has always been, and will always be a free software.';
 $lang['porg_whatispiwigo_open_source_desc2'] = 'A code you can read and understand is a code you can trust.';
-$lang['How to use Piwigo ?'] = 'How to use Piwigo ?';
+$lang['How to use Piwigo ?'] = 'How to use Piwigo?';
 $lang['porg_whatispiwigo_how_to_desc1'] = 'You can download Piwigo from Piwigo<span style="color:#f70;">.org</span> and install it on your web hosting, with the appropriate technical requirements.';
 $lang['porg_whatispiwigo_how_to_desc2'] = 'Using Piwigo is free, you only pay your hosting provider.';
 $lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo also comes <em>as a service</em> on Piwigo<span style="color:#ff3363">.com</span>: take a few seconds to open a trial account, use Piwigo and forget about technical details.';
@@ -39,5 +39,3 @@ $lang['porg_whatispiwigo_gnu_desc3'] = 'Open source, or "Free Software", means y
 $lang['porg_whatispiwigo_gnu_desc4'] = 'Free doesn\'t refer to the price, it refers to freedom.';
 $lang['porg_whatispiwigo_gnu_desc5'] = ' The difference between the two meanings of free is often characterized as "Free as in speech vs. free as in beer."';
 $lang['porg_whatispiwigo_gnu_desc6'] = ' The GPL is free as in speech.';
-
-?>
