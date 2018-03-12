6b368d5d1a08a3ddaa2450606ba5eee53ca643e1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:38:53 +0100

    Add hu_HU, thanks to bugge

---
+++ language/hu_HU/home.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Albums Hierarchy'] = 'Aldumok rangsora';
+$lang['Permissions Control'] = 'Hozzáférési beállítások';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Kezelheti fotógyűjteményét';
+$lang['Start discovering Piwigo'] = 'Indulhat a Piwigo felfedezése';
+$lang['View more showcases'] = 'További kirakatok megtekintése';
+$lang['Who are Piwigo users?'] = 'Ki a Piwigo felhasználó?';
+$lang['porg_home_desc1'] = 'Piwigo egy nyílt forráskódú internetes kép galléria. ';
+$lang['porg_home_desc2'] = 'Ajánlott cégeknek, csapatoknak és magánszemélyeknek.';
+$lang['porg_home_high_volume_desc'] = 'Piwigoban könnyen kezelhetsz akár több ezer vagy több százezer fényképet is. ';
+$lang['porg_home_open_source_desc'] = 'A forráskód elérhető, szerkeszthető, ellenőrizhető és bővíthető a kiegészítőknek és témáknak köszönhetően. ';
+$lang['porg_home_organisation_title'] = 'Több ezer cég, és több millió magánszemély szereti és használja a Piwigo rendszerét.';
+$lang['porg_home_sustainable_desc'] = '2002-ben kezdődött a Piwigo fejlesztése és több, mint 15 éve használják már világszerte. Mindig megújul és fejlődik. ';
+$lang['porg_home_title'] = 'Kezelje Ön is a kép gyűjteményét a Piwigoval. ';
