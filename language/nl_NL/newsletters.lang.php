6cfe034cda7e541ac2c530f770de33162fe51b7c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:38:14 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/newsletters.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Enter your email here'] = 'Geef hier je e-mailadres op';
+$lang['Sign up'] = 'Aanmelden';
+$lang['porg_newsletters_desc1'] = 'Blijf op de hoogte van Piwigo-nieuws';
+$lang['porg_newsletters_desc2'] = 'U ontvangt e-mails wanneer een nieuwe release beschikbaar is en wanneer er belangrijke gebeurtenissen met het project gebeuren.';
+$lang['porg_newsletters_subscribe_title'] = '
+Abonneer je op onze nieuwsbrief en blijf op de hoogte!';
