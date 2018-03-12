d4de4cfbb999b121f6ffcf59c035f130a966a199
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:37:08 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Category'] = 'Categorie';
+$lang['Contact us'] = 'Contact';
+$lang['Partnership'] = 'Partnerschap';
+$lang['Press inquiry'] = 'Klik op onderzoek';
+$lang['Security report'] = 'Veiligheid verslag';
+$lang['Send message'] = 'Stuur bericht';
+$lang['Testimonial'] = 'Verklaring';
+$lang['Your email address'] = 'Jouw email-adres';
+$lang['Your message'] = 'Jouw bericht';
+$lang['porg_contact_desc1'] = 'Je kunt een bericht achterlaten door gebruik te maken van onderstaand formulier.';
+$lang['porg_contact_desc2'] = 'Gebruik dit hoofdzakelijk voor website-opmerkingen.';
+$lang['porg_contact_desc3'] = 'We prefereren het gebruik van ondersteuningsforums voor elke feedback. Het vergemakkelijkt het zoeken door het team.';
+$lang['porg_contact_error_email'] = 'Oeps, dat email-adres schijnt verkeerd te zijn';
+$lang['porg_contact_error_message'] = 'Oeps, jouw bericht schijnt verkeerd te zijn';
