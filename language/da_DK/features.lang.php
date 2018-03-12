ee93c9bfb5e02f0121c731136d24b558c48eae44
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:36:44 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/features.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_easy_install_desc1'] = 'Vi har designet Piwigo til at være meget let at installere, med et minimum af krav.';
+$lang['porg_features_easy_install_desc2'] = 'Det gælder også opdatering, med en enkelt knap at klikke på!';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo kan læse GPS-bredde og -længdegrader fra indlejrede metadata.';
+$lang['porg_features_geolocate_desc2'] = 'Dernæst, med plugins til Google Maps eller OpenStreetMap, kan Piwigo vise dine fotografier på et interaktivt kort.';
+$lang['porg_features_great_place_desc'] = 'Dine besøgende kan skrive indlæg, give bedømmelser, markere fotografier som favoritter, udføre søgninger og pr. mail få besked om nyheder.';
+$lang['porg_features_metadata_desc'] = 'Eksempelvis kan en IPTC-beskrivelse importeres til fotoegenskaberne, vises ved siden af fotografiet... og være søgbar!';
+$lang['porg_features_organize_desc1'] = 'Distribuer dine fotografier blandt dine egne albummer.';
+$lang['porg_features_organize_desc2'] = 'Albumstræet har uendelig dybde.';
+$lang['porg_features_organize_desc3'] = 'Hvert fotografi kan tilhøre flere albummer.';
+$lang['porg_features_plugins_desc1'] = 'Udseendet kan ændres med teamer.';
+$lang['porg_features_plugins_desc2'] = 'TIlføj funktioner med plugins.';
+$lang['porg_features_plugins_desc3'] = 'Udvidelser installeres med blot nogle få klik.';
+$lang['porg_features_plugins_desc4'] = '<span class="bold">350 udvidelser</span> er tilgængelige, og flere kommer til!';
+$lang['porg_features_privacy_desc1'] = 'Gør dine fotografier private, og beslut hvem der skal kunne se hvert enkelt af dem.';
+$lang['porg_features_privacy_desc2'] = 'Du kan opsætte rettigheder på albummer og fotografier, for grupper og enkelte brugere.';
+$lang['porg_features_slideshow_desc'] = 'Med plguin\'en Fotorama, eller specifikke temaer så som Bootstrap Darkroom, kan man opleve fuldskærmsslideshow.';
+$lang['porg_features_upload_desc'] = 'Du kan tilføje fotografier med webformularen, enhver FTP-klient<br>eller en desktopapplikation så som digiKam, Lightroom<br>eller mobilapplikationer.';
+$lang['porg_features_user_management_desc1'] = 'Administratorer kan oprette brugere, grupper, håndtere rettigheder og sende beskeder.';
+$lang['porg_features_user_management_desc2'] = 'Flere administratorer kan arbejde på samme tid i Piwigo.';
+$lang['porg_features_user_management_desc3'] = 'Perfekt til dit hold!';
+$lang['A great place for your visitors'] = 'Et fremragende sted for dine besøgende';
+$lang['API'] = 'API';
+$lang['Batch Manager'] = 'Batch Manager';
+$lang['Browse by Dates'] = 'Gennemse efter datoer';
+$lang['Easy installation and updates'] = 'Let installering og opdatering';
+$lang['Full screen slideshow'] = 'Fuldskærmsslideshow';
+$lang['Geolocate your photos on a map'] = 'Geolokaliser dine fotografier på et kort';
+$lang['Import automatic your metadata'] = 'Importer automatisk dine metadata';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Mange indstillinger gør det muligt at tilpasse Piwogo til dit eget behov';
+$lang['Organize with Album'] = 'Organiser med album';
+$lang['Privacy on your Photos'] = 'Privatlivsbeskyttelse af dine fotografier';
+$lang['Upload your photos'] = 'Upload dine fotografier';
+$lang['Users management'] = 'Brugeradministration';
+$lang['porg_features_api_desc1'] = 'Piwigos web-API gør det let for udviklere at udføre handlinger fra andre applikationer: visning af miniaturebilleder i et WordPress-blogindlæg, udgivelse af fotografier fra Lightroom, synkronisering af en lokal mappe med et Perl-script...';
+$lang['porg_features_api_desc2'] = 'Så enkelt som HTTP-forespørgsler og JSON-svar.';
+$lang['porg_features_batch_manager_desc'] = 'Filtrer fotografier fra din samling, foretag et valg og udfør flere handlinger på én gang (batch): Ændring af forfatter, tilføj nogle tags, tilknyt til et nyt album, opsæt geolokalisering...';
+$lang['porg_features_browse_date_desc'] = 'Digitalkameraer gemmer datoen i fotografiet, og Piwigo bruger denne dato til at vise din fotosamling i en kalender, og lader dig gennemse fra dag til dag.';
+$lang['porg_features_desc1'] = 'Piwigo tilbyder et bredt udvalg af funktioner, anvendelige i et ethvert fotogalleri.';
+$lang['porg_features_desc2'] = 'Mange af de tilgængelige funktioner er opremset herunder.';
+$lang['porg_features_desc3'] = 'Nye funktioner føjes løbende til Piwigo, på grund af kodeholdets ideer og diskussioner med brugerfællesskabet.';
