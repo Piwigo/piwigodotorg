b2abb630d55254cb706eaebe12a55dbc0277478a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 06:21:35 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/about_us.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_about_us_desc2'] = 'Als je in onze open source-missie gelooft, hopen we dat je je bij ons wilt aansluiten!';
+$lang['porg_about_us_desc1'] = 'We zijn een gemeenschap van medewerkers van over de hele wereld.';
+$lang['porg_about_us_contributors_desc'] = 'Medewerkers van over de hele wereld werken aan alle onderdelen van het Piwigo-ecosysteem!';
+$lang['porg_about_us_business_desc1'] = 'Hoewel geanimeerd door een grotendeels vrijwillige gemeenschap van bijdragers, is een project zoals Piwigo niet vrij om webservers, domeinnamen, ontwikkeltijd of websitebeheer te draaien: ';
+$lang['porg_about_us_business_desc2'] = '
+Hoe is het project op geldniveau levensvatbaar?';
+$lang['porg_about_us_beginning_desc2'] = 'Zal veel bijdragers aantrekken';
+$lang['porg_about_us_birth_desc'] = 'Na maanden van voorbereiding is versie 1.0.0 uitgebracht';
+$lang['porg_about_us_beginning_desc1'] = '
+Beschikbaar op Free.fr met een installatie met 1 klik.';
+$lang['porg_about_us_apps_desc'] = '
+Piwigo komt als mobiele apps voor iPhone / iPad en Android';
+$lang['You?'] = 'Jij?';
+$lang['Years old'] = 'Jaar oud';
+$lang['porg_about_us_service_desc1'] = 'Enkele programmeurs en thema ontwikkelaars zijn betaald door klanten om nieuwe toepassingen te ontwikkelen';
+$lang['porg_about_us_service_desc2'] = 'Meestal zijn deze toepassingen in zijn geheel ook beschikbaar voor de Piwigo gemeenschap';
+$lang['porg_about_us_webgallery_desc'] = 'Nieuwe naam, versie 2.0 maar hetzelfde project';
+$lang['porg_about_us_recruitement_desc'] = 'Het team wordt groter en programmeer activiteit groeit sterk';
+$lang['porg_about_us_plugins_desc2'] = 'Nieuwe toepassingen en nieuwe bijdragers';
+$lang['porg_about_us_plugins_desc1'] = 'Belangrijke technische en mentaliteitsverandering';
+$lang['porg_about_us_mission_desc'] = 'De Piwigo-missie is om de beste open source foto-manager voor het web te bouwen met een sterke focus op gebruikerservaring met technologie';
+$lang['porg_about_us_launch_desc'] = 'Cloud-hosting klaar voor gebruik Piwigo voor iedereen';
+$lang['Arrivals of Plugins'] = 'Binnengekomen plugins';
+$lang['Beginning'] = 'Begin';
+$lang['Birth'] = 'Geboren';
+$lang['Business Model'] = 'Business Model';
+$lang['Donations'] = 'Donaties';
+$lang['Downloads'] = 'Downloads';
+$lang['Forum messages'] = 'Forum berichten';
+$lang['Get involved!'] = 'Doe mee';
+$lang['Mobile apps'] = 'Mobile apps';
+$lang['Our Mission'] = 'Onze Missie';
+$lang['PhpWebGallery becomes Piwigo'] = 'PhpWebGallery wordt Piwigo';
+$lang['Piwigo contributors'] = 'Piwigo bijdragers';
+$lang['Piwigo moves to GitHub'] = 'Piwigo gaat naar GitHub';
+$lang['Piwigo.com'] = 'Piwigo.com';
+$lang['Piwigo.com is launched'] = 'Piwigo.com is gelanceerd';
+$lang['Recruitement'] = 'Werving';
+$lang['Releases'] = 'Edities';
+$lang['Services'] = 'Diensten';
+$lang['Take part in our adventure'] = 'Neem deel aan ons avontuur';
+$lang['Want to be part of the adventure?'] = 'Wil je een onderdeel worden van het avontuur?';
