f12bbe50dafe23035bf2fedbcddd3e73909ec38f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 06:21:47 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/urls.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_urls']['release'] = 'sürüm';
+$lang['porg_urls']['about-us'] = 'hakkımızda';
+$lang['porg_urls']['changelogs'] = 'değişiklik-kayıtları';
+$lang['porg_urls']['coding-activity'] = 'yazılım-faaliyeti';
+$lang['porg_urls']['contact'] = 'iletişim';
+$lang['porg_urls']['features'] = 'özellikler';
+$lang['porg_urls']['get-involved'] = 'katılımda-bulun';
+$lang['porg_urls']['get-piwigo'] = 'piwigo-indir';
+$lang['porg_urls']['get-started'] = 'başlamak';
+$lang['porg_urls']['news'] = 'haberler';
+$lang['porg_urls']['newsletters'] = 'bülten';
+$lang['porg_urls']['press'] = 'basın';
+$lang['porg_urls']['testimonials'] = 'değerlendirmeler';
+$lang['porg_urls']['what-is-piwigo'] = 'piwigo-nedir';
