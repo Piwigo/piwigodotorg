29cd6054d64477456613a551143901af56e891eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:38:29 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_838'] = '[Batch Manager] delete \"all set\" yanlış hata mesajı üretmesi';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - Yönetim panelinde SQL enjeksiyonu';
+$lang['porg_issue_826'] = 'Yapılandırma ayarlarında SQL enjeksiyonu';
+$lang['porg_issue_825'] = 'Toplu İş Yöneticisi\'nde SQL enjeksiyonu, birim modu';
+$lang['porg_issue_824'] = 'MacOS X altında sürüm oluşturma başarısızlığı';
+$lang['porg_issue_823'] = 'Yönetici/kullanıcılar için SQL enjeksiyonu';
+$lang['porg_issue_822'] = 'yapılandırma sayfalarında xss+csrf saldırısı kullanma';
+$lang['porg_issue_735'] = 'API aracılığıyla kimlik doğrulama anahtarı alma yeteneği';
