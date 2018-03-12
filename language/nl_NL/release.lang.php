ab3610ddda3ae679ecdde76fc48ddcb22d8d26aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:36:52 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'De lijst met belangrijke wijzigingen wordt beschreven in de release-opmerking van <a href="%s"> Piwigo%s </a>';
+$lang['Released on %s'] = 'Uitgegeven op %s';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Nadat je de bestanden hebt uitgepakt, breng je ze over naar jouw webserver met een FTP-cliënt via de installatie van Piwigo%s.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Als je <b> versie 2.2 + </ b> gebruikt, zal Piwigo je vóór de upgrade vertellen welke plug-ins mogelijk niet compatibel zijn met Piwigo%s.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Als je <b> Piwigo%s </ b> gebruikt, kunt je ook het archief <b>%s_to_%s.zip </ b> downloaden dat alle nieuwe en gewijzigde bestanden bevat.';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Als je een oudere versie dan %s gebruikt en de automatische upgrade niet wilt gebruiken, volg dan de <b> handmatige upgrade </ b>.';
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = 'Je kunt het md5sum van je gedownloade bestand vergelijken met het officiële md5sum op piwigo.org.';
+$lang['Automatic upgrade'] = 'Automatische upgrade';
+$lang['Bugs fixed'] = 'Fouten hersteld';
+$lang['Download Piwigo %s'] = 'Download Piwigo%s';
+$lang['Featured added'] = 'Uitgelicht toegevoegd';
+$lang['Known issues'] = 'Bekende problemen';
+$lang['Languages'] = 'Talen';
+$lang['Manual Upgrade'] = 'Handmatige upgrade';
+$lang['New language:'] = 'Nieuwe taal:';
+$lang['No database upgrade is required.'] = '
+Er is geen database-upgrade vereist.';
+$lang['Release note'] = 'Uitgave-opmerking';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = '
+De MD5-som is een handige tool om ervoor te zorgen dat jouw bestand niet beschadigd is.';
+$lang['Updated languages'] = 'Geupdate talen';
+$lang['Upgrade'] = 'Upgrade';
+$lang['We recommend the <b>automatic upgrade</b>.'] = '
+We raden de <b> automatische upgrade </ b> aan.';
