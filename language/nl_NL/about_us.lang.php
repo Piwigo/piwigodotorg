2f2d08df407cc43052f7ad57401c71c258c079e3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:36:29 +0100

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/about_us.lang.php
+++ language/nl_NL/about_us.lang.php
@@ -24,24 +24,21 @@ $lang['porg_about_us_desc2'] = 'Als je in onze open source-missie gelooft, hopen
 $lang['porg_about_us_desc1'] = 'We zijn een gemeenschap van medewerkers van over de hele wereld.';
 $lang['porg_about_us_contributors_desc'] = 'Medewerkers van over de hele wereld werken aan alle onderdelen van het Piwigo-ecosysteem!';
 $lang['porg_about_us_business_desc1'] = 'Hoewel geanimeerd door een grotendeels vrijwillige gemeenschap van bijdragers, is een project zoals Piwigo niet vrij om webservers, domeinnamen, ontwikkeltijd of websitebeheer te draaien: ';
-$lang['porg_about_us_business_desc2'] = '
-Hoe is het project op geldniveau levensvatbaar?';
+$lang['porg_about_us_business_desc2'] = 'Hoe is het project op geldniveau levensvatbaar?';
 $lang['porg_about_us_beginning_desc2'] = 'Zal veel bijdragers aantrekken';
 $lang['porg_about_us_birth_desc'] = 'Na maanden van voorbereiding is versie 1.0.0 uitgebracht';
-$lang['porg_about_us_beginning_desc1'] = '
-Beschikbaar op Free.fr met een installatie met 1 klik.';
-$lang['porg_about_us_apps_desc'] = '
-Piwigo komt als mobiele apps voor iPhone / iPad en Android';
+$lang['porg_about_us_beginning_desc1'] = 'Beschikbaar op Free.fr met een installatie met 1 klik.';
+$lang['porg_about_us_apps_desc'] = 'Piwigo komt als mobiele apps voor iPhone / iPad en Android';
 $lang['You?'] = 'Jij?';
 $lang['Years old'] = 'Jaar oud';
-$lang['porg_about_us_service_desc1'] = 'Enkele programmeurs en thema ontwikkelaars zijn betaald door klanten om nieuwe toepassingen te ontwikkelen';
-$lang['porg_about_us_service_desc2'] = 'Meestal zijn deze toepassingen in zijn geheel ook beschikbaar voor de Piwigo gemeenschap';
-$lang['porg_about_us_webgallery_desc'] = 'Nieuwe naam, versie 2.0 maar hetzelfde project';
-$lang['porg_about_us_recruitement_desc'] = 'Het team wordt groter en programmeer activiteit groeit sterk';
-$lang['porg_about_us_plugins_desc2'] = 'Nieuwe toepassingen en nieuwe bijdragers';
-$lang['porg_about_us_plugins_desc1'] = 'Belangrijke technische en mentaliteitsverandering';
-$lang['porg_about_us_mission_desc'] = 'De Piwigo-missie is om de beste open source foto-manager voor het web te bouwen met een sterke focus op gebruikerservaring met technologie';
-$lang['porg_about_us_launch_desc'] = 'Cloud-hosting klaar voor gebruik Piwigo voor iedereen';
+$lang['porg_about_us_service_desc1'] = 'Enkele programmeurs en thema ontwikkelaars zijn betaald door klanten om nieuwe toepassingen te ontwikkelen.';
+$lang['porg_about_us_service_desc2'] = 'Meestal zijn deze toepassingen in zijn geheel ook beschikbaar voor de Piwigo gemeenschap.';
+$lang['porg_about_us_webgallery_desc'] = 'Nieuwe naam, versie 2.0 maar hetzelfde project.';
+$lang['porg_about_us_recruitement_desc'] = 'Het team wordt groter en programmeer activiteit groeit sterk.';
+$lang['porg_about_us_plugins_desc2'] = 'Nieuwe toepassingen en nieuwe bijdragers.';
+$lang['porg_about_us_plugins_desc1'] = 'Belangrijke technische en mentaliteitsverandering.';
+$lang['porg_about_us_mission_desc'] = 'De Piwigo-missie is om de beste open source foto-manager voor het web te bouwen met een sterke focus op gebruikerservaring met technologie.';
+$lang['porg_about_us_launch_desc'] = 'Cloud-hosting, klaar-voor-gebruik Piwigo voor iedereen.';
 $lang['Arrivals of Plugins'] = 'Binnengekomen plugins';
 $lang['Beginning'] = 'Begin';
 $lang['Birth'] = 'Geboren';
@@ -62,3 +59,15 @@ $lang['Releases'] = 'Edities';
 $lang['Services'] = 'Diensten';
 $lang['Take part in our adventure'] = 'Neem deel aan ons avontuur';
 $lang['Want to be part of the adventure?'] = 'Wil je een onderdeel worden van het avontuur?';
+$lang['porg_about_us_donation_desc3'] = '
+<a href="%s"> Uw financiële bijdrage </a> zou uw betrokkenheid bij het project onderstrepen.';
+$lang['porg_about_us_dotcom_desc1'] = '
+De gegenereerde winsten maken het mogelijk om enige tijd op Piwigo door te brengen';
+$lang['porg_about_us_dotcom_desc2'] = 'Bijvoorbeeld Pierrick, maker van Piwigo en nu dankzij Piwigo.com betaald, kan werken op Piwigo.org.';
+$lang['porg_about_us_dotcom_desc3'] = 'De twee projecten onderhouden dus een visieuze cirkel: het succes van de een leidt tot het succes van de ander en omgekeerd.';
+$lang['porg_about_us_github_desc'] = 'Piwigo sluit zich officieel aan bij de Github-gemeenschap van programmeurs.';
+$lang['porg_about_us_join_desc1'] = '
+Het Piwigo-project heeft verschillende vaardigheden nodig, zoals programmeurs, ontwerpers, documentatie-schrijvers en helpers.';
+$lang['porg_about_us_join_desc2'] = 'Als je in onze missie gelooft, maak er dan deel van uit :-)';
+$lang['porg_about_us_donation_desc2'] = 'Dit geld wordt gebruikt om directe kosten te betalen, zoals hosting van webservers, domeinnamen of abonnementen op mobiele app-stores.';
+$lang['porg_about_us_donation_desc1'] = 'De Piwigo-vereniging verzamelt donaties van haar gemeenschap van gebruikers.';
