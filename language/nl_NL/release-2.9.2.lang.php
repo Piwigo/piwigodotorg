b9f1adcb4103a82a857f93524ebbf5baacd13877
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:37:37 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-2.9.2.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_678'] = 'PHP7RSS fout';
+$lang['porg_issue_710'] = 'Ingegeven parameter\"redirect_decoded." is niet geldig';
+$lang['porg_issue_713'] = '
+update kennisgeving mail in Franse / Engelse mix';
+$lang['porg_issue_721'] = '
+Bugrapport: stel privé-album van beheerder in op openbaar via CSRF & onjuiste machtigingen';
+$lang['porg_issue_723'] = 'bij het aanvragen van een privé-ID wordt de permalink onthuld';
+$lang['porg_issue_724'] = '
+Bugrapport: SQL-injectie in pagina cat_options';
+$lang['porg_issue_725'] = 'bescherm pwg.images.setInfo vanuit HTML';
+$lang['porg_issue_726'] = 'activeer het maken van albums';
+$lang['porg_issue_731'] = '
+dashboard, ontbrekend aantal foto\'s';
+$lang['porg_issue_732'] = 'hernoem functieomschrijving';
+$lang['porg_issue_739'] = '
+[zoeken] wanneer een zoekopdracht op woord ook overeenkomt met tags';
+$lang['porg_issue_747'] = 'PHP-melding bij het wijzigen van het thema van de admin-pagina';
+$lang['porg_issue_751'] = '
+Kan installatie / piwigo_structure-mysql.sql niet tweemaal uitvoeren';
+$lang['porg_issue_755'] = '
+Het synchroniseren van de metadata stuurt een datumcreatie die incorrect is voor Mysql 5.7';
+$lang['porg_issue_760'] = '
+Het maken van de datum wordt niet vernieuwd als het wordt gewijzigd van picture_modify';
+$lang['porg_issue_767'] = '
+Publiceren via Shotwell 0.22.0 mislukt met Piwigo 2.9.1';
+$lang['porg_issue_777'] = '
+WS-categorieën getImages, TotalCount';
+$lang['porg_issue_787'] = '
+onverwachte \ "actie \" voor wachtwoord.php toont fouten';
+$lang['porg_issue_789'] = '
+mogelijkheid om foutmeldingen te voorkomen op inlogpagina';
