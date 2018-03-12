341032d706352d39db48b596700028341bcad640
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:35:59 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/what_is_piwigo.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_high_volume_desc1'] = '500.000 fotografier i din samling? Intet problem for Piwigo.';
+$lang['porg_whatispiwigo_high_volume_desc2'] = 'Lynhurtig ydeevne medfølger, som standard.';
+$lang['porg_whatispiwigo_how_to_desc1'] = 'Du kan hente Piwigo fra Piwigo<span style="color:#f70;">.org</span>, og installere det hos din egen webhosting, under de tilhørende tekniske krav.';
+$lang['porg_whatispiwigo_how_to_desc2'] = 'Piwigo kan frit anvendes, du betaler kun til din hostingleverandør.';
+$lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo leveres også <em>som en service</em> på Piwigo<span style="color:#ff3363">.com</span>: brug et par øjeblikke til at åbne en prøvekonto, benyt Piwigo og glem alt om tekniske detaljer.';
+$lang['porg_whatispiwigo_how_to_desc4'] = 'En konto på Piwigo.com betyder hostingudgifter.';
+$lang['porg_whatispiwigo_individual_desc1'] = 'Håndtering af et familiealbum over de sidste 20 år, organisering af en samling europæiske edderkopper, viser dine bedste landskabsfotografier som fotoamatør...';
+$lang['porg_whatispiwigo_individual_desc2'] = 'er blot nogle få eksempler på hvordan Piwigo anvendes af dets fællesskab.';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo har altid været, og vil altid være, fri software.';
+$lang['porg_whatispiwigo_open_source_desc2'] = 'Kode som man kan læse og forstå, er kan man kan stole på.';
+$lang['porg_whatispiwigo_organisation_desc1'] = 'Offentlig sektor, virksomheder, fonde... alle er nødt til at håndtere deres egen samling.';
+$lang['porg_whatispiwigo_organisation_desc2'] = 'Inden for området <em>Digital Asset Management</em>, fokuserer Piwigo på letanvendelighed, prisbillighed, ydeevne og bæredygtighed.';
+$lang['porg_whatispiwigo_professional_desc1'] = 'Professionalle fotografer er nødt til at fremvise deres arbejde til klienter, offentligt eller privat.';
+$lang['porg_whatispiwigo_professional_desc2'] = 'Piwigo indeholder avanceret rettighedskontrol.';
+$lang['porg_whatispiwigo_professional_desc3'] = 'Klienter kan hente fotografier et efter et eller hele albummer.';
+$lang['porg_whatispiwigo_sustainable_desc1'] = 'Siden sin fødsel i 2002, har Piwigo tydeligt bevist sit niveau af <em>modenhed</em>.';
+$lang['porg_whatispiwigo_sustainable_desc2'] = 'Selvfølgelig fortsætter udviklingen for at følge med aktuelle teknologier!';
+$lang['porg_whatispiwigo_use_desc1'] = 'I Piwigos fællesskab er der et bredt udvalg af anvendelsesområder.';
+$lang['porg_whatispiwigo_use_desc2'] = 'Fra mellemstore gallerier<br> til enorme billedbanker, kan Piwigo gøre dit liv lettere, og spare dig for tid og arbejde.';
+$lang['Amateurs'] = 'Amatører';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Collections'] = 'Samlinger';
+$lang['Families'] = 'Familier';
+$lang['Fansites'] = 'Fansteder';
+$lang['GNU License'] = 'GNU-licens';
+$lang['How to use Piwigo ?'] = 'Hvordan anvendes Piwigo?';
+$lang['Lightning Fast'] = 'Lynhurtig';
+$lang['Multiple Administrators'] = 'Flere administratorer';
+$lang['Share with Clients'] = 'Del med klienter';
+$lang['What can I use Piwigo for ?'] = 'Hvad kan jeg bruge Piwigo til?';
+$lang['Why choosing Piwigo ?'] = 'Hvorfor vælge Piwigo?';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo er en webappliaktion til håndtering af din fotosamling, samt andre medier.';
+$lang['porg_whatispiwigo_desc2'] = 'Indeholdende ydedygtige funktioner, drives fotogallerier over hele verden.';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'GNU General Public License, eller GPL, er en open source-licens.';
+$lang['porg_whatispiwigo_gnu_desc2'] = 'Open source betyder ikke bare, at du kan se kildekoden – det har også politisk og filosofisk betydning.';
+$lang['porg_whatispiwigo_gnu_desc3'] = 'Open source, eller "fri software", betyder at du frit kan ændre og videredistribuere kildekoden under visse betingelser.';
+$lang['porg_whatispiwigo_gnu_desc4'] = 'Frit tilgængelig hentyder til ikke prisen, men til frihed.';
+$lang['porg_whatispiwigo_gnu_desc5'] = 'Forskellen mellem de to betydninger af fri karakteriseres ofte som "fri som i fri tale vs. fri som i frit tilgængelig øl."';
+$lang['porg_whatispiwigo_gnu_desc6'] = 'GPL er fri som i fri tale.';
