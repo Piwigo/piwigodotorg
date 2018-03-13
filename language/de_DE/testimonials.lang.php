9ce7eb3eed39a2dd276d173af3ae9847322a3d3a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:16:16 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/testimonials.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_testimonials_desc1'] = 'Entdecken Sie, was Piwigo-Benutzer zu sagen haben!';
+$lang['porg_testimonials_desc2'] = 'Auf dieser Seite können Sie Referenzen von lebensechten Erfahrungen mit Piwigo lesen.';
+$lang['porg_testimonials_share_desc1'] = 'Wenn Sie eine Geschichte zu erzählen haben, <a href="%s">teilen Sie sie.</a';
+$lang['porg_testimonials_share_title'] = 'Wir sind stolz so eine Gemeinschaft zu haben!';
