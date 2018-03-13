1928771e93aed6e51ad0cae512b5d2aca18f1282
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:14:33 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.8.1.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_434'] = 'Konstruktor von LocalSiteReader veraltet';
+$lang['porg_issue_436'] = 'Aufruf der undefinierten Funktion Logger::warning in i.php';
+$lang['porg_issue_439'] = 'eingebaute Dokumentation für Mehrfach-Formate aktualisiert';
+$lang['porg_issue_440'] = 'Suche (smartpocket Thema) ergibt keine Ergebnisse';
+$lang['porg_issue_442'] = 'Aufruf der undefinierten Funktion set_cat_status()';
+$lang['porg_issue_443'] = 'Fehler beim Erzeugen von Tabellen mit MySQL 5.7';
+$lang['porg_issue_466'] = '[Batch Manager] neues Album wird beim Verschieben von Fotos nicht angezeigt';
+$lang['porg_issue_470'] = 'Zufälligkeit von generate_key erhöht. Schwachstelle gemeldet von <a href="https://www.danclifford.eu/">Dan Clifford</a>. Vielen Dank an ihn!';
