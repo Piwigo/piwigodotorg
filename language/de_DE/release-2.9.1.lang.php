c798e8ce4376d7c16feed37e19f9d9ae74ea14ab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:15:21 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.9.1.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_663'] = 'SQL Fehler in Tour 2.9';
+$lang['porg_issue_667'] = 'Bug Report: Cross Site Scripting(XSS) in Seitenparameter von admin.php';
+$lang['porg_issue_674'] = '[php7.1] Benutzer-bearbeiten-Box, [] operator not supported for strings';
+$lang['porg_issue_679'] = 'Admin Dashboard, Trigger für Plugins benötigt';
+$lang['porg_issue_683'] = 'Speicherbedarf, Stellen verstecken, wenn mehr als 100GB';
+$lang['porg_issue_685'] = 'Batch Manager, Fotos löschen funktioniert nicht mit Safari';
+$lang['porg_issue_693'] = 'Batch Downloader schlägt mit pclzip und PHP 7.1 fehl';
+$lang['porg_issue_696'] = 'PIWIGO API - pwg.categories.delete funktioniert nicht richtig';
+$lang['porg_issue_699'] = 'Dateininklusion in Administration';
+$lang['porg_issue_701'] = 'Der Album-Synchronisieren-Link führt auf die Seite um ./galleries/ zu synchronisieren, auch wenn das Album in einem anderen Ordner liegt';
+$lang['porg_issue_705'] = 'Benutzerliste Backend, Prüfen der Eingabeparameter';
+$lang['porg_issue_706'] = '[Sicherheit] Identifikation sollte nur innerhalb von Piwigo umleiten ';
+$lang['porg_issue_707'] = 'Anzahl der Besucher, Stellen verstecken wenn kleiner als 1k';
+$lang['porg_issue_710'] = 'Eingabeparameter \"redirect_decoded\" ist ungültig';
