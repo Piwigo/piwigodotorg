11f78df5231a53067b32dc1ed5843fae1e0b7fde
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:16:01 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/changelogs.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_changelogs_desc1'] = 'Hier ist die Liste aller stabilen Piwigo-Releases.';
+$lang['porg_changelogs_desc2'] = 'Benutzen Sie unser neustes Release: Sie bekommen besseren Support und alle bekannten Sicherheitslücken sind geschlossen.';
+$lang['porg_changelogs_technicals_desc1'] = 'Wenn Sie eine technische Sicht auf die Releases brauchen, gehen Sie zur Seite <a href="%s">Branches und Releases</a>.';
