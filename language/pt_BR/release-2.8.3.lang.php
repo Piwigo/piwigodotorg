0149e86ef87138475b09821930b44071c806d466
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 14:12:29 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.8.3.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_306'] = '[language] Outro arquivo de idioma que não seja plugin.lang não está carregando; corrigido por <a href="http://piwigo.org/forum/profile.php?id=4851">LucMorizur</a>';
+$lang['porg_issue_376'] = '[MySQL 5.7] A cláusula ORDER BY não está na lista SELECT, corrigido por <a href="http://piwigo.org/forum/profile.php?id=22630">Mercury_LABBS</a>';
+$lang['porg_issue_449'] = 'Falha no envio de vídeo, corrigido por <a href="https://github.com/vvinogradsky">vvinogradsky</a> (Lightroom exporta para plugin Piwigo author) e <a href="https://github.com/dkm">dkm</a>';
+$lang['porg_issue_542'] = 'O novo usuário nem sempre deve usar o idioma do navegador';
+$lang['porg_issue_546'] = 'Bug em reestabelecer a senha por email';
+$lang['porg_issue_547'] = 'aumento das verificações no formato url; relatado por Nicolas Bidron de <a href="https://www.nccgroup.trust">NCC Group Security Advisory </a>';
+$lang['porg_issue_548'] = 'pesquisa rápida e comentário HTML; relatado por Nicolas Bidron de <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
