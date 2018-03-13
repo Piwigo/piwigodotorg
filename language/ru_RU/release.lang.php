73908a358a1e99f5221058dd16a99546b69f7436
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 12:25:54 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Если у Вас версия старше, чем %s и Вы не хотите обновлять её автоматически, то перейдите к <b>ручному обновлению</b>.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Если Вы используете <b>Piwigo %s</b>, Вы можете скачать архив <b>%s_to_%s.zip</b>, содержащий все новые и измененные файлы.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Если Вы используете <b>версию 2.2+</b>, то перед установкой Piwigo подскажет Вам, какие плагины не совместимы с Piwigo %s.';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'После извлечения файлов перенесите их на Ваш веб-сервер с помощью FTP-клиента для установки Piwigo %s поверх имеющейся версии.';
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = 'Вы можете сравнить md5sum загруженного файла с официальной md5sum на piwigo.org.';
+$lang['Automatic upgrade'] = 'Автоматическое обновление';
+$lang['Bugs fixed'] = 'Исправленные баги';
+$lang['Download Piwigo %s'] = 'Скачать Piwigo %s';
+$lang['Featured added'] = 'Добавленные функции';
+$lang['Known issues'] = 'Известные проблемы';
+$lang['Languages'] = 'Языки';
+$lang['Manual Upgrade'] = 'Ручное обновление';
+$lang['New language:'] = 'Новый язык:';
+$lang['No database upgrade is required.'] = 'Обновление базы данных не требуется.';
+$lang['Release note'] = 'Примечание к релизу';
+$lang['Released on %s'] = 'Релиз от %s';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'Контрольная сумма MD5 является удобным инструментом, чтобы убедиться, что Ваш файл не поврежден.';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'Список основных изменений содержится в заметках к релизу <a href="%s">Piwigo %s</a>';
+$lang['Updated languages'] = 'Обновленные языки';
+$lang['Upgrade'] = 'Обновление';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Мы рекомендуем <b>автоматическое обновление</b>.';
