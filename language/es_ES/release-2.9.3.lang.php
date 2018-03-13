ce31958aea646df972dcff1a89922260e9bc1e08
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 14:12:13 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_838'] = '[Editor en lote] borrar \"all set\" produce un mensaje de error incorrecto';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - Inyección SQL en el panel de administración';
+$lang['porg_issue_824'] = 'liberar el editor del a versión en MacOS X';
+$lang['porg_issue_825'] = 'inyección de SQL en el administrador en lote, modo unitario';
+$lang['porg_issue_826'] = 'inyección de SQL en los ajustes de configuración';
+$lang['porg_issue_735'] = 'posibilidad de obtener una llave de autorización a través de la API';
+$lang['porg_issue_822'] = 'usar ataque xss+csrf en la página de configuración';
+$lang['porg_issue_823'] = 'Inyección de SQL en la administración/usuarios';
