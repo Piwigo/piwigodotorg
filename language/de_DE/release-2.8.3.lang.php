da94751b2bd241ddb720f985f5ea3ef5903d8246
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:14:25 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.8.3.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_306'] = '[Sprache] Andere Sprachdateien als plugin.lang werden nicht geladen, behoben von <a href="http://piwigo.org/forum/profile.php?id=4851">LucMorizur</a>';
+$lang['porg_issue_376'] = '[MySQL 5.7] ORDER BY Anweisung ist nicht in SELECT Liste, behoben von <a href="http://piwigo.org/forum/profile.php?id=22630">Mercury_LABBS</a>';
+$lang['porg_issue_449'] = 'Video hochladen schlägt fehl, behoben von <a href="https://github.com/vvinogradsky">vvinogradsky</a> (Lightroom export to Piwigo Pluginautor) und <a href="https://github.com/dkm">dkm</a>';
+$lang['porg_issue_542'] = 'Neue Benutzer sollten nicht immer die Browsersprache benutzen';
+$lang['porg_issue_546'] = 'Fehler in der Passwort-Reset-Email';
+$lang['porg_issue_547'] = 'Prüfungen des URL Formats verstärkt: gemeldet von Nicolas Bidron vom <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
+$lang['porg_issue_548'] = 'Schnellsuche und HTML Kommentar: gemeldet von Nicolas Bidron vom <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
