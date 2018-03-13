4910a6b0ee0736fd1f9f32da7efb27f8b7aefea0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 12:24:52 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-2.8.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_572'] = 'CVE-2016-10084 File Inclusion Attack, reported by <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_573'] = 'CVE-2016-10085 File Inclusion Attack #2, reported by <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_574'] = 'File Inclusion with Possible RCE, reported by <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_575'] = 'CVE-2016-10083 Cross Site Scripting, reported by <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_595'] = 'update PHPMailer to 5.2.21';
+$lang['porg_issue_599'] = 'fix PHPMailer smtp class loading when using SMTP transport';
