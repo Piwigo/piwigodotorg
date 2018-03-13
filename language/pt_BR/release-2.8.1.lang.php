b2168ea557f694518b96cd00622f037e4e04a9c2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 14:12:44 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.8.1.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_436'] = 'Chamada para um método indefinido Logger::warning em i.php';
+$lang['porg_issue_439'] = 'atualize a documentação embutida para descrever o formato múltiplo';
+$lang['porg_issue_440'] = 'Pesquisa (tema do smartpocket) não retorna resultados';
+$lang['porg_issue_442'] = 'Chamada para a função indefinida set_cat_status()';
+$lang['porg_issue_443'] = 'Falha na criação de tabelas com o MySQL 5.7';
+$lang['porg_issue_466'] = '[Batch Manager] novo álbum não listado ao mover fotos';
+$lang['porg_issue_470'] = 'Aumento de aleatoriedade em generate_key. Vulnerabilidade comunicada por <a href="https://www.danclifford.eu/">Dan Clifford</a>. Muito obrigado por ele!.';
+$lang['porg_issue_434'] = 'Construtor obsoleto para LocalSiteReader';
