b04a0b91cb0af6f95463091a5eeddcddce135354
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 09:37:25 +0100

    Update en_UK, thanks to Kaare

--- language/en_UK/changelogs.lang.php
+++ language/en_UK/changelogs.lang.php
@@ -1,7 +1,5 @@
 <?php
 
-$lang['porg_changelogs_technicals_desc1'] = 'If you need technicals view of the releases go to the <a href="%s">Branches and Releases</a> page.';
-$lang['porg_changelogs_desc1'] = 'Here is the list of all Piwigo stable releases.';
-$lang['porg_changelogs_desc2'] = 'Use our latest release: you will find better support and it will contain all know security bugs fixed.';
-
-?>
+$lang['porg_changelogs_technicals_desc1'] = 'If you need technical views of the releases go to the <a href="%s">Branches and Releases</a> page.';
+$lang['porg_changelogs_desc1'] = 'Here is the list of all stable Piwigo releases.';
+$lang['porg_changelogs_desc2'] = 'Use our latest release: you will get better support and all known  security bugs will be fixed.';
