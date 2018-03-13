fd69367308c510626bd41d5c785b93cf6a50c07f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 09:35:46 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/get_started.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_started_download_desc'] = 'Загрузите полный пакет Piwigo, включая все файлы, или только скрипт NetInstall, который будет сам скачивать весь пакет непосредственно на Ваш хостинг.';
+$lang['porg_get_started_guide_desc'] = 'Узнайте, как установить Piwigo на Ваш хостинг, от загрузки и учет ных данных базы данных до первого подключения к Вашей библиотеке изображений.';
+$lang['porg_get_started_question_desc1'] = 'Вы можете получить помощь <a href="%s">на форуме</a> или найти ответы в <a href="%s">документации</a>';
+$lang['Still got some unanswered questions ?'] = 'Все еще есть некоторые вопросы?';
+$lang['porg_get_started_desc1'] = 'Как запустить Ваш собственный хостинг Piwigo.';
+$lang['porg_get_started_desc2'] = 'Что нужно подготовить, что скачать, как установить.';
+$lang['porg_get_started_requirement_desc1'] = 'Каковы технические тонкости для запуска Piwigo на собственном хостинге?';
+$lang['Installation guide'] = 'Инструкция по установка';
+$lang['Requirements'] = 'Требования';
+$lang['porg_get_started_requirement_desc2'] = 'PHP, база данных, веб-сервер, обработка изображений... Все детали.';
