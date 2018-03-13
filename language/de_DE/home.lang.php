72419af1b0b53386a52f9f976fe3f04462609565
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 09:37:03 +0100

    Update de_DE, thanks to e-dschungel

--- language/de_DE/home.lang.php
+++ language/de_DE/home.lang.php
@@ -22,14 +22,14 @@
 // +-----------------------------------------------------------------------+
 $lang['Albums Hierarchy'] = 'Albenhierachie';
 $lang['Permissions Control'] = 'Zugriffskontrolle';
-$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Organisiere deine Fotosammlung';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Organisieren Sie Ihre Fotosammlung';
 $lang['Start discovering Piwigo'] = 'Entdecke Piwigo';
 $lang['View more showcases'] = 'mehr Showcases ansehen';
 $lang['Who are Piwigo users?'] = 'Wer benutzt Piwigo?';
 $lang['porg_home_desc1'] = 'Piwigo ist eine Open-Source Fotogallerie für das Web.';
 $lang['porg_home_desc2'] = 'Entwickelt für Organisationen, Teams und Privatpersonen.';
-$lang['porg_home_high_volume_desc'] = 'Piwigo glänzt wenn es darum geht tausende oder sogar hunderttausend Fotos to klassifizieren.';
+$lang['porg_home_high_volume_desc'] = 'Piwigo glänzt, wenn es darum geht tausende oder sogar hunderttausende Fotos zu klassifizieren.';
 $lang['porg_home_open_source_desc'] = 'Der Quellcode ist verfügbar, auditierbar und dank Plugins und Themen erweiterbar.';
 $lang['porg_home_organisation_title'] = 'Tausende von Organisationen und Millionen von Privatpersonen lieben es Piwigo zu benutzen';
-$lang['porg_home_sustainable_desc'] = 'Geboren im Jahr 2002 hat Piwigo seine Nutzer seid mehr als 15 Jahren unterstützt. Und sich verbessert!';
-$lang['porg_home_title'] = 'Organisiere deine Fotosammlung mit Piwigo';
+$lang['porg_home_sustainable_desc'] = 'Geboren im Jahr 2002 hat Piwigo seine Nutzer seit mehr als 15 Jahren unterstützt. Und sich verbessert!';
+$lang['porg_home_title'] = 'Organisieren Sie Ihre Fotosammlung mit Piwigo';
