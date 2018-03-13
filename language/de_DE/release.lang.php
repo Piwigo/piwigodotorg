a6fd33c62d7e4129599cfd8c4773a7357dbba54a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:15:37 +0100

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Wenn Sie die Dateien entpackt haben, laden Sie sie mit einem FTP-Programm auf Ihren Webserver und überschreiben Sie Ihre bestehende Piwigo-%s-Installation.';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Wenn Sie eine Version älter als %s benutzen und nicht das automatische Upgrade benutzen wollen, können Sie das manuelle Upgrade benutzen.';
+$lang['Release note'] = 'Release Note';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'Eine Liste der größeren Änderungen finden Sie in den <a href="%s">Piwigo %s</a> Release Notes.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Wenn Sie <b>Piwigo %s</b> benutzen, können Sie auch das <b>%s_to_%s.zip</b> Archiv herunterladen, das alle neuen und geänderten Dateien enthält.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Wenn Sie <b>Version 2.2+</b> benutzen, wird Piwigo Ihnen vor dem Upgrade mitteilen, welche Plugins möglicherweise nicht kompatibel mit Piwigo %s sind.';
+$lang['Download Piwigo %s'] = 'Piwigo %s herunterladen';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'Der MD5-Hash ist ein komfortabler Weg sicherzustellen, dass Ihre Datei nicht korrupt ist.';
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = 'Sie können den MD5-Hashwert Ihrer heruntergeladen Datei mit dem offiziellen Wert auf piwigo.org vergleichen. ';
+$lang['Automatic upgrade'] = 'Automatisches Upgrade';
+$lang['Bugs fixed'] = 'Behobene Bugs';
+$lang['Featured added'] = 'neue Features';
+$lang['Known issues'] = 'Bekannte Probleme';
+$lang['Languages'] = 'Sprachen';
+$lang['Manual Upgrade'] = 'Manuelles Upgrade';
+$lang['New language:'] = 'Neue Sprache:';
+$lang['No database upgrade is required.'] = 'Ein Datenbankupdate ist nicht erforderlich.';
+$lang['Released on %s'] = 'Veröffentlicht am %s';
+$lang['Updated languages'] = 'Aktualisierte Sprachen';
+$lang['Upgrade'] = 'Upgrade';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Wir empfehlen das <b>automatische Upgrade</b>.';
