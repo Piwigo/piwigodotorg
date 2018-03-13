7d1b9fad2dbbd648bdf6f3e0e0109c4abbf9d88f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 09:37:48 +0100

    Update en_UK, thanks to Kaare

--- language/en_UK/news.lang.php
+++ language/en_UK/news.lang.php
@@ -1,6 +1,4 @@
 <?php
 
-$lang['porg_news_desc1'] = 'Find out about Piwigo latest news here.';
+$lang['porg_news_desc1'] = 'Find out about the latest Piwigo news here.';
 $lang['porg_news_desc2'] = ' Upcoming events, new releases, beta versions and more...';
-
-?>
