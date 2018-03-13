f724e4600dd01f729fec74009e1b4ce737986db6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 09:35:31 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/get_involved.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_involved_grateful_desc1'] = 'Благодарим Вас за вклад и участие.';
+$lang['porg_get_involved_improvment_desc'] = 'Используйте форум, чтобы поделиться своими идеями <br>о том, как улучшить Piwigo.';
+$lang['porg_get_involved_security_desc1'] = 'Помогите нам создать наиболее безопасный менеджер изображений.';
+$lang['porg_get_involved_translate_desc1'] = 'Перевод Piwigo на Ваш язык, исправление ошибок и дополнение отсутствующих разделов.';
+$lang['Bugtracker'] = 'Bugtracker';
+$lang['Coding documentation'] = 'Создание документации';
+$lang['Create a report'] = 'Создание отчета';
+$lang['Develop plugins'] = 'Развитие плагинов';
+$lang['Donate to Piwigo'] = 'Помощь Piwigo';
+$lang['Help the Community'] = 'Помощь сообществу';
+$lang['Make this a monthly gift'] = 'Сделайте это ежемесячным подарком';
+$lang['Recommend Piwigo'] = 'Рекомендовать Piwigo';
+$lang['Security Report'] = 'Сообщение о безопасности';
+$lang['Submit bugs'] = 'Сообщение об ошибках и багах';
+$lang['Submit product improvment'] = 'Предложение по улучшению';
+$lang['Translate'] = 'Перевод';
+$lang['Translation tool'] = 'Инструмент переводчика';
+$lang['We are grateful !'] = 'Мы благодарны!';
+$lang['Your Gift'] = 'Ваш подарок';
