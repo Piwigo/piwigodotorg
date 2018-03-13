fba7eed42f164ef5c41ea97c07ed21001a14a9f7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:16:24 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/testimonials.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_testimonials_desc1'] = 'Lad os finde ud af, hvad Piwigos brugere har at sige!';
+$lang['porg_testimonials_desc2'] = 'På denne side kan du læse ægte anmeldelser baseret på folks egne oplevelser med Piwigo.';
+$lang['porg_testimonials_share_desc1'] = 'Hvis du har en historie at fortælle, så <a href="%s">del den</a>';
+$lang['porg_testimonials_share_title'] = 'Vi er stole over at have et sådant fællesskab!';
