8556103b47f1c248e464d4349bc6ca5812b18e49
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 12:25:38 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/newsletters.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Enter your email here'] = 'Укажите здесь свой e-mail';
+$lang['Sign up'] = 'Подписаться';
+$lang['porg_newsletters_desc1'] = 'Следите за новостями Piwigo.';
+$lang['porg_newsletters_desc2'] = 'Вы будете получать электронную почту, когда будет доступен новый релиз и когда в проекте происходят основные события.';
+$lang['porg_newsletters_subscribe_title'] = 'Подпишитесь на нашу рассылку и будьте всегда в курсе!';
