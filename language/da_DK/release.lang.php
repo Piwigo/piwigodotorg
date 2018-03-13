3ac6d26a9701f7e890372818ed6d33560a4070f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-13 21:15:45 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = 'Du kan sammenligne den hentede fils md5sum med den officielle md5sum, der findes på piwigo.org.';
+$lang['Automatic upgrade'] = 'Automatisk opgradering';
+$lang['Bugs fixed'] = 'Fejl rettet';
+$lang['Download Piwigo %s'] = 'Hent Piwigo %s';
+$lang['Featured added'] = 'Funktion tilføjet';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Hvis du benytter en version ældre end %s, og du ikke ønsker at anvende den automatiske opgradering, så følg den <b>manuelle opgradering</b>.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Hvis du benytter <b>Piwigo %s</b>, kan du også hente arkivet <b>%s_to_%s.zip</b>, som indeholder alle nye og ændrede filer.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Hvis du benytter <b>version 2.2+</b>, vil Piwigo før opgraderingen fortælle dig hvilke plugins, som måske ikke er kompatible med Piwigo %s.';
+$lang['Known issues'] = 'Kendte problemer';
+$lang['Languages'] = 'Sprog';
+$lang['Manual Upgrade'] = 'Manuel opgradering';
+$lang['New language:'] = 'Nyt sprog:';
+$lang['No database upgrade is required.'] = 'Ingen databaseopgradering påkrævet.';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Når du har udpakket filerne, skal de overføres til din webserver med en FTP-klient, oven i din Piwigo %s-installation.';
+$lang['Release note'] = 'Udgivelsesbemærkning';
+$lang['Released on %s'] = 'Udgivet den %s';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'MD5-summen er en bekvem måde at sikre sig, at ens fil ikke er blevet ødelagt.';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'Listen over større ændringer er beskrevet udgivelsesbemærkningerne til <a href="%s">Piwigo %s</a>';
+$lang['Updated languages'] = 'Opdaterede sprog';
+$lang['Upgrade'] = 'Opgrader';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Vi anbefaler den <b>automatiske opgradering</b>.';
