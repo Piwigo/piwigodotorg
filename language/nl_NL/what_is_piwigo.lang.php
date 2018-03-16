59ffccee46ffbbb2e04b6a8b2a68c19b67292098
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-16 16:19:56 +0100

    Update nl_NL, thanks to watertoren5044, navisence

---
+++ language/nl_NL/what_is_piwigo.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_high_volume_desc1'] = '500.000 foto\'s in je verzameling? Geen probleem voor Piwigo.';
+$lang['porg_whatispiwigo_high_volume_desc2'] = 'Bliksemsnelle uitvoeringen zijn standaard inbegrepen.';
+$lang['porg_whatispiwigo_how_to_desc1'] = 'U kunt Piwigo downloaden van Piwigo <span style = "color: # f70;">. Org </ span> en installeren met de juiste technische vereisten op uw webhosting, .';
+$lang['porg_whatispiwigo_how_to_desc2'] = 'Het gebruik van Piwigo is gratis, u betaalt alleen uw hostingprovider.';
+$lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo komt ook <em> als een service </ em> op Piwigo <span style = "color: # ff3363">. Com </ span>: neem een paar seconden om een proefaccount te openen, gebruik Piwigo en denk niet technische details.';
+$lang['porg_whatispiwigo_how_to_desc4'] = 'Een Piwigo.com-account bevat hostingkosten.';
+$lang['porg_whatispiwigo_individual_desc1'] = 'Een familiealbum beheren van de afgelopen 20 jaar, een verzameling Europese spinnen organiseren, je beste landschapsfoto\'s laten zien als amateurfotograaf ...';
+$lang['porg_whatispiwigo_individual_desc2'] = 'dit zijn een paar voorbeelden van hoe Piwigo wordt gebruikt door zijn gemeenschap.';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo was en is altijd gratis software.';
+$lang['porg_whatispiwigo_open_source_desc2'] = 'Een code die u kunt lezen en begrijpen, is een code die u kunt vertrouwen.';
+$lang['porg_whatispiwigo_organisation_desc1'] = 'Publieke sector, bedrijven, stichtingen ... allemaal moeten ze hun eigen verzameling beheren.';
+$lang['porg_whatispiwigo_organisation_desc2'] = 'In het veld <em> Digital Asset Management </ em> focust Piwigo op gebruiksgemak, betaalbaarheid, prestaties en duurzaamheid.';
+$lang['porg_whatispiwigo_professional_desc1'] = 'Professionele fotografen moeten hun werk aan hun klanten kunnen laten zien, openbaar of privé.';
+$lang['porg_whatispiwigo_professional_desc2'] = 'Piwigo biedt geavanceerde machtigingencontrole.';
+$lang['porg_whatispiwigo_professional_desc3'] = 'Klanten kunnen foto\'s afzonderlijk of hele albums downloaden.';
+$lang['porg_whatispiwigo_sustainable_desc1'] = 'Piwigo is in 2002 ontstaan en heeft duidelijk het niveau <em> rijpheid </ em> bereikt.';
+$lang['porg_whatispiwigo_sustainable_desc2'] = 'Natuurlijk blijft het zich ontwikkelen om te passen bij de huidige technologie!';
+$lang['porg_whatispiwigo_use_desc1'] = 'Er is een groot aantal verschillende toepassingen door de gebruikers  van Piwigo.';
+$lang['porg_whatispiwigo_use_desc2'] = 'Van middelgrote galerijen tot grote beeldbanken: Piwigo kan je leven eenvoudiger maken en je tijd en moeite besparen.';
+$lang['Amateurs'] = 'Liefhebbers';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Collections'] = 'Verzamelingen';
+$lang['Families'] = 'Families/gezinnen';
+$lang['Fansites'] = 'Fan-sites';
+$lang['GNU License'] = 'GNU licensie';
+$lang['How to use Piwigo ?'] = 'Hoe Piwigo te gebruiken?';
+$lang['Lightning Fast'] = 'Bliksemsnel';
+$lang['Multiple Administrators'] = 'Meerdere beheerders';
+$lang['Share with Clients'] = 'Deel met klanten';
+$lang['What can I use Piwigo for ?'] = 'Waar kan ik Piwigo voor gebruiken?';
+$lang['Why choosing Piwigo ?'] = 'Waarom Piwigo kiezen?';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo is een webtoepassing om uw verzameling foto\'s en andere media te beheren.';
+$lang['porg_whatispiwigo_desc2'] = 'Door krachtige functies te integreren, worden fotogalerijen over de hele wereld krachtiger.';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'De GNU General Public License of GPL is een open source-licentie.';
+$lang['porg_whatispiwigo_gnu_desc2'] = 'Open source betekent niet alleen dat je de broncode kunt bekijken - het heeft ook politieke en filosofische implicaties.';
+$lang['porg_whatispiwigo_gnu_desc3'] = 'Open source, of \'Vrije Software\', betekent dat u de broncode onder bepaalde voorwaarden kunt wijzigen en herdistribueren.';
+$lang['porg_whatispiwigo_gnu_desc4'] = 'Vrij verwijst niet naar de prijs, het verwijst naar vrijheid.';
+$lang['porg_whatispiwigo_gnu_desc5'] = 'Het verschil tussen de twee betekenissen van gratis wordt vaak gekenmerkt als "Vrij als in spraak versus vrij als in bier."';
+$lang['porg_whatispiwigo_gnu_desc6'] = 'De GPL is gratis zoals in spraak.';
