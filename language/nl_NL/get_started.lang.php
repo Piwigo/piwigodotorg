443d970a6b6c269e613cb9534ba3c04d9e9f4f06
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-16 16:20:04 +0100

    Update nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/get_started.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_started_download_desc'] = 'Download Piwigo-pakket inclusief alle bestanden, of alleen het NetInstall-script, dat het pakket rechtstreeks op uw hosting zal downloaden.';
+$lang['porg_get_started_guide_desc'] = 'Leer hoe u Piwigo op uw hosting kunt installeren, van download- en database-inloggegevens tot de eerste verbinding met uw fotobibliotheek.';
+$lang['porg_get_started_question_desc1'] = 'U kunt hulp krijgen op het <a href="%s">forum</a> of uw eigen antwoorden vinden in de <a href="%s">documentatie</a>';
+$lang['porg_get_started_requirement_desc1'] = 'Wat zijn de technische vereisten om Piwigo op uw eigen hosting te laten draaien?';
+$lang['porg_get_started_requirement_desc2'] = 'PHP, database, webserver, beeldverwerking ... Vind alle details.';
+$lang['Installation guide'] = 'Installatiegids';
+$lang['Requirements'] = 'Vereisten';
+$lang['Still got some unanswered questions ?'] = 'Heeft u nog steeds onbeantwoorde vragen?';
+$lang['porg_get_started_desc1'] = 'Hoe u uw self-hosting Piwigo kunt runnen, dat wil zeggen op uw eigen hosting.';
+$lang['porg_get_started_desc2'] = 'Wat u moet voorbereiden, wat u moet downloaden, hoe u het moet installeren.';
