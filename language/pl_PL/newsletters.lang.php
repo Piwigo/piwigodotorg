98a3f6cdccc90a0713f8b44e9b374dfcf9e3aa1c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-19 06:23:32 +0100

    Update pl_PL, thanks to mjp

---
+++ language/pl_PL/newsletters.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_newsletters_desc1'] = 'Bądź w kontakcie z projektem Piwigo.';
+$lang['porg_newsletters_desc2'] = 'Będziesz otrzymywać maile jeżeli będzie dostępna nowa wersja oraz gdy w projekcie będą działy się ważniejsze rzeczy.';
+$lang['porg_newsletters_subscribe_title'] = 'Zapisz się do newsletter\'a z ogłoszeniami Piwigo.';
+$lang['Enter your email here'] = 'Podaj swój adres email';
+$lang['Sign up'] = 'Zarejestruj się';
