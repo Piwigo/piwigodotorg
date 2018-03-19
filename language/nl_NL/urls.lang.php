70697814e2c38b0cc94ed31fedfceb0c084f186c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-19 06:23:41 +0100

    Update nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/urls.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_urls']['about-us'] = 'over ons';
+$lang['porg_urls']['changelogs'] = 'wijzigingen';
+$lang['porg_urls']['coding-activity'] = 'coderings-activiteit';
+$lang['porg_urls']['contact'] = 'contact';
+$lang['porg_urls']['features'] = 'kenmerken';
+$lang['porg_urls']['get-involved'] = 'raak betrokken';
+$lang['porg_urls']['get-piwigo'] = 'verkrijg piwigo';
+$lang['porg_urls']['get-started'] = 'begin';
+$lang['porg_urls']['news'] = 'nieuws';
+$lang['porg_urls']['newsletters'] = 'nieuwsbrief';
+$lang['porg_urls']['press'] = 'pers';
+$lang['porg_urls']['release'] = 'release';
+$lang['porg_urls']['testimonials'] = 'getuigenissen';
+$lang['porg_urls']['what-is-piwigo'] = 'wat is piwigo';
