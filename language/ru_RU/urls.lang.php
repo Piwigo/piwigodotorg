5331a15ed36848e2d8560eb5b87322bcbbf4e683
plegall <plg@piwigo.org>
2018-03-27 14:27:01 +0200

    RU: guides have been moved to russian wiki

--- language/ru_RU/urls.lang.php
+++ language/ru_RU/urls.lang.php
@@ -20,11 +20,11 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['porg_doc_urls']['install'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:install';
-$lang['porg_doc_urls']['requirements'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:install:before';
-$lang['porg_doc_urls']['upgrade'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade';
-$lang['porg_doc_urls']['upgrade_auto'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade:upgrade_automatic';
-$lang['porg_doc_urls']['upgrade_manual'] = 'http://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade:upgrade_manual';
+$lang['porg_doc_urls']['install'] = 'http://ru.piwigo.org/doc/doku.php?id=установка';
+$lang['porg_doc_urls']['requirements'] = 'http://ru.piwigo.org/doc/doku.php?id=установка:требования';
+$lang['porg_doc_urls']['upgrade'] = 'http://ru.piwigo.org/doc/doku.php?id=руководство_по_обновлению';
+$lang['porg_doc_urls']['upgrade_auto'] = 'http://ru.piwigo.org/doc/doku.php?id=руководство_по_обновлению:автоматическое_обновление';
+$lang['porg_doc_urls']['upgrade_manual'] = 'http://ru.piwigo.org/doc/doku.php?id=руководство_по_обновлению:обновление_в_ручном_режиме';
 $lang['porg_urls']['coding-activity'] = 'Программирование';
 $lang['porg_urls']['about-us'] = 'О-нас';
 $lang['porg_urls']['changelogs'] = 'Список-изменений';
