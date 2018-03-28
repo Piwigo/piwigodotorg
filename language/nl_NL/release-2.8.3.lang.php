fad5ed998111e176064bc7bb482dd206d10487f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-28 15:39:44 +0200

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/release-2.8.3.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_548'] = 'snel zoeken en HTML commentaar; gerapporteerd door Nicolas Bidron van <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
+$lang['porg_issue_547'] = 'meer controles op url formaat; gerapporteerd door Nicolas Bidron van <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
+$lang['porg_issue_306'] = '[taal] Ander taalbestand dan plugin.lang wordt niet geladen; hersteld door <a href="http://piwigo.org/forum/profile.php?id=4851">LucMorizur</a>';
+$lang['porg_issue_376'] = '[MySQL 5.7] ORDER BY statement is niet in SELECT lijst, hersteld door <a href="http://piwigo.org/forum/profile.php?id=22630">Mercury_LABBS</a>';
+$lang['porg_issue_449'] = 'Video opladen faalt, hersteld door <a href="https://github.com/vvinogradsky">vvinogradsky</a> (Lightroom export to Piwigo plugin author) en <a href="https://github.com/dkm">dkm</a>';
+$lang['porg_issue_542'] = 'Nieuwe gebruiker hoeft niet steeds de taal van de browser te gebruiken';
+$lang['porg_issue_546'] = 'Bug bij reset paswoord email';
