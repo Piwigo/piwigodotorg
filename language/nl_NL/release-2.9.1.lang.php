c9fd39d1dd8f0e769692a9738c1ade99a7be2f87
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-28 11:43:09 +0200

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/release-2.9.1.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_696'] = 'PIWIGO API - pwg.categories.delete werkt niet naar behoren.';
+$lang['porg_issue_693'] = 'Batch downloader faalt bij gebruik van pclzip and php 7.1';
+$lang['porg_issue_685'] = 'batch manager, verwijder foto werkt niet met Safari';
+$lang['porg_issue_683'] = 'disk gebruik, verberg decimalen indien meer dan 100GB';
+$lang['porg_issue_679'] = 'admin dashboard, heeft trigger nodig voor plugins';
+$lang['porg_issue_674'] = '[php7.1] gebruiker edit box, [] operator niet ondersteund voor strings';
+$lang['porg_issue_667'] = 'Bug melding: Cross Site Scripting(XSS) in pagina parameter van admin.php';
+$lang['porg_issue_663'] = 'SQL fout in rondleiding 2.9';
