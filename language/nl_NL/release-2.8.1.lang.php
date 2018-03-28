c33cb3d1ebd7616c1b32a1200bb09cf501446af9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-28 11:43:17 +0200

    Update nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/release-2.8.1.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_434'] = 'Verouderde constructor voor LocalSiteReader';
+$lang['porg_issue_436'] = 'Aanvraag voor ongedefinieerde methode Logger::warning in i.php';
+$lang['porg_issue_439'] = 'werk de bijgesloten documentatie, over het beschrijven van meerdere indelingen, bij';
+$lang['porg_issue_440'] = 'Zoeken (smartpocket-thema) geeft geen resultaten';
+$lang['porg_issue_442'] = 'Aanvraag voor ongedefinieerde functie set_cat_status ()';
+$lang['porg_issue_443'] = 'Fouten bij het maken van tabellen met MySQL 5.7';
+$lang['porg_issue_466'] = '[Batch Manager] nieuw album niet vermeld bij het verplaatsen van foto\'s';
+$lang['porg_issue_470'] = 'Vergroot de willekeurigheid van generate_key. Kwetsbaarheid gerapporteerd door <a href="https://www.danclifford.eu/"> Dan Clifford</a>. Hartelijk dank!.';
