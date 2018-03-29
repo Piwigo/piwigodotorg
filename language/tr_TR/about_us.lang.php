124ea31cb8dd0d87a9fba78bdded5a174bbeb4cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-29 19:44:40 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/about_us.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Years old'] = 'yaşında';
+$lang['You?'] = 'Sen?';
+$lang['Arrivals of Plugins'] = 'Eklentilerin gelişi';
+$lang['Beginning'] = 'Başlarken';
+$lang['Birth'] = 'Doğum';
+$lang['Business Model'] = 'Ticaret Modeli';
+$lang['Donations'] = 'Bağışlar';
+$lang['Downloads'] = 'İndirmeler';
+$lang['Forum messages'] = 'Forum mesajları';
+$lang['Get involved!'] = 'Sende katıl!';
+$lang['Mobile apps'] = 'Mobil apps';
+$lang['Our Mission'] = 'Misyonumuz';
+$lang['PhpWebGallery becomes Piwigo'] = 'PhpWebGallery\'den Piwigo oluşyor';
+$lang['Piwigo contributors'] = 'Piwigo\'nun katkıda bulunanlar';
+$lang['Piwigo moves to GitHub'] = 'Piwigo Github\'a taşınıyor';
+$lang['Piwigo.com'] = 'Piwigo.com';
+$lang['Piwigo.com is launched'] = 'Piwigo.com başlatıldı';
+$lang['Recruitement'] = 'İşlendirme';
+$lang['Releases'] = 'Sürümler';
+$lang['Services'] = 'Servisler';
+$lang['Take part in our adventure'] = 'Maceramıza katıl';
+$lang['Want to be part of the adventure?'] = 'Maceranın bir parçası olmak ister misin?';
