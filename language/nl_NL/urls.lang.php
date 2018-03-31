253dd054e1054b17282cb2f39e5d1b96fe6596a3
plegall <plg@piwigo.org>
2018-03-31 14:50:53 +0200

    NL: add hyphens in urls

--- language/nl_NL/urls.lang.php
+++ language/nl_NL/urls.lang.php
@@ -1,18 +1,18 @@
 <?php
-$lang['porg_urls']['about-us'] = 'over ons';
+$lang['porg_urls']['about-us'] = 'over-ons';
 $lang['porg_urls']['changelogs'] = 'wijzigingen';
 $lang['porg_urls']['coding-activity'] = 'coderings-activiteit';
 $lang['porg_urls']['contact'] = 'contact';
 $lang['porg_urls']['features'] = 'kenmerken';
-$lang['porg_urls']['get-involved'] = 'raak betrokken';
-$lang['porg_urls']['get-piwigo'] = 'verkrijg piwigo';
+$lang['porg_urls']['get-involved'] = 'raak-betrokken';
+$lang['porg_urls']['get-piwigo'] = 'verkrijg-piwigo';
 $lang['porg_urls']['get-started'] = 'begin';
 $lang['porg_urls']['news'] = 'nieuws';
 $lang['porg_urls']['newsletters'] = 'nieuwsbrief';
 $lang['porg_urls']['press'] = 'pers';
 $lang['porg_urls']['release'] = 'release';
 $lang['porg_urls']['testimonials'] = 'getuigenissen';
-$lang['porg_urls']['what-is-piwigo'] = 'wat is piwigo';
+$lang['porg_urls']['what-is-piwigo'] = 'wat-is-piwigo';
 
 $lang['porg_doc_urls']['requirements'] = 'http://nl.piwigo.org/doc/doku.php?id=installatie:vereisten';
 $lang['porg_doc_urls']['install'] = 'http://nl.piwigo.org/doc/doku.php?id=installatie';
