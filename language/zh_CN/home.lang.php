efe36d1e1490e0559098f005750e7f6765a2af34
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-01 09:26:27 +0200

    Add zh_CN, thanks to dennisyan

---
+++ language/zh_CN/home.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Albums Hierarchy'] = '相册层级';
+$lang['Permissions Control'] = '权限管理';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - 管理您的图像收藏';
+$lang['Start discovering Piwigo'] = '了解 Piwigo';
+$lang['View more showcases'] = '查看更多实例';
+$lang['Who are Piwigo users?'] = 'Piwigo 的用户都是谁？';
+$lang['porg_home_desc1'] = 'Piwigo 是网络端的开源相册程序。';
+$lang['porg_home_desc2'] = '专为组织、团队与个人设计。';
+$lang['porg_home_high_volume_desc'] = '当需要分类上万乃至数十万的照片时，Piwigo 的表现出类拔萃。';
+$lang['porg_home_open_source_desc'] = '由于插件和主题的存在，源代码是可取得、可编辑、可审计、且可扩展的。';
+$lang['porg_home_organisation_title'] = 'Piwigo 受到了数万组织和数百万个人的喜爱';
+$lang['porg_home_sustainable_desc'] = '始于2002，Piwigo 对用户的支持已超过了15年。我们永远在进步！';
+$lang['porg_home_title'] = '用 Piwigo 管理您的图像收藏';
