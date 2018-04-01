3bea397cece8f4d29711e6165dbb392c795ad438
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-01 18:03:24 +0200

    Update es_ES, thanks to JamworkS

---
+++ language/es_ES/features.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_api_desc1'] = 'La API de Piwigo lo pone fácil para que los desarrolladores efectúen acciones desde otras aplicaciones: mostrar miniaturas en una publicación de un blog de Wordpress, publicar una foto desde Lightroom, sincronizar un carpeta local con un script de Perl...';
+$lang['porg_features_metadata_desc'] = 'Por ejemplo una descripción IPTC puede ser importada en las propiedades de la foto, mostrada junto con la foto ... ¡y es buscable!';
+$lang['porg_features_upload_desc'] = 'Puedes añadir fotos con el formulario web, cualquier cliente FTP o <br>una aplicación como digiKam, Shotwell, Lightroom o<br> aplicaciones móviles.';
+$lang['porg_features_user_management_desc1'] = 'Los administradores pueden crear usuarios, grupos, cambiar permisos y enviar notificaciones.';
+$lang['porg_features_user_management_desc2'] = 'Varios administradores pueden trabajar a la misma vez con Piwigo.';
+$lang['porg_features_user_management_desc3'] = '¡Perfecto para tu equipo!';
+$lang['porg_features_privacy_desc1'] = 'Haz tus fotos privadas y decide quién las puede ver.';
+$lang['porg_features_privacy_desc2'] = 'Puedes dar permisos en álbumes y fotos, para grupos o usuarios individuales.';
+$lang['porg_features_slideshow_desc'] = 'Con el plugin Fotorama, o el tema específico como Bootstrap Darkroom, puedes experimentar presentaciones a pantalla completa.';
+$lang['porg_features_organize_desc2'] = 'El árbol de álbumes tiene profundidad ilimitada.';
+$lang['porg_features_organize_desc3'] = 'Cada foto puede pertener a varios álbumes a la vez.';
+$lang['porg_features_plugins_desc1'] = 'Cambia la apariencia con temas.';
+$lang['porg_features_plugins_desc2'] = 'Añade características con plugins.';
+$lang['porg_features_plugins_desc3'] = 'Las extensiones requieren de tan sólo unos clicks para ser instaladas.';
+$lang['porg_features_plugins_desc4'] = '¡<span class="bold">350 extensiones</span> disponibles, y creciendo!';
+$lang['porg_features_organize_desc1'] = 'Distribuye tus fotos entre tus álbums.';
+$lang['porg_features_great_place_desc'] = 'Tus visitantes pueden publicar comentarios, votar, marcar fotos como favoritas, efectuar búsquedas y ser notificados de las novedades vía email.';
+$lang['porg_features_geolocate_desc2'] = 'Entonces, con el plugin para Google Maps o OpenStreetMap, Piwigo puede mostrar tus fotos en un mapa interactivo.';
+$lang['porg_features_easy_install_desc2'] = '¡Así cómo las actualizaciones con un simple clic de ratón¡';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo puede leer la latitud y longitud GPS de los metadatos almacenados.';
+$lang['Full screen slideshow'] = 'Presentación a pantalla completa';
+$lang['porg_features_easy_install_desc1'] = 'Hemos diseñado Piwigo para que sea super fácil de instalar, con los mínimos requerimientos.';
+$lang['Batch Manager'] = 'Administrador de Lotes';
+$lang['A great place for your visitors'] = 'Un gran lugar para tus visitas';
+$lang['API'] = 'API';
+$lang['Browse by Dates'] = 'Navegar por Fechas';
+$lang['Easy installation and updates'] = 'Fácil instalación y actualizaciones';
+$lang['Geolocate your photos on a map'] = 'Geolocaliza tus fotos en un mapa';
+$lang['Import automatic your metadata'] = 'Importa automáticamente tus metadatos';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Muchos ajustes de configuración para hacer que Piwigo se adapte a tus necesidades';
+$lang['Organize with Album'] = 'Organizar con Album';
+$lang['Privacy on your Photos'] = 'Privacidad en tus fotos';
+$lang['Upload your photos'] = 'Sube tus fotos';
+$lang['Users management'] = 'Administración de Usuarios';
+$lang['porg_features_api_desc2'] = 'Tan simple como respuestas de HTTP y JSON';
+$lang['porg_features_batch_manager_desc'] = 'Filtra fotos de tu colección, haz una selección y aplica las acciones en lote: cambia el autor, añade etiquetas, asocia a un álbum, añade geolocalización...';
+$lang['porg_features_browse_date_desc'] = 'Las cámaras digitales almacenan la fecha en la foto y Piwigo la usa para mostrar tu colección de fotos en un calendario y deja que navegues día a día.';
+$lang['porg_features_desc1'] = 'Piwigo ofrece un gran rango de características para cualquier galería de fotos.';
+$lang['porg_features_desc2'] = 'A continuación listamos muchas de las características disponibles.';
+$lang['porg_features_desc3'] = 'Nuevas características son añadidadas a Piwigo continuamente, gracias a ideas del equipo de programación e interacciones con la comunidad de usuarios.';
