b9371004d9075bf1c209b771dd7e7b19811ca7ce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-01 09:50:42 +0200

    Update es_ES, thanks to JamworkS

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,123 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Individuals'] = 'Personal';
+$lang['[piwigo.org contact form, %s] %s contacted you on %s'] = '[formulario de contacto de piwigo.org, %s] %s te ha contactado en %s';
+$lang['language_kn_IN'] = 'Canarés';
+$lang['language_ta_IN'] = 'Tamil';
+$lang['language_zh_HK'] = 'Hong-Kong';
+$lang['language_lv_LV'] = 'Letón';
+$lang['language_mn_MN'] = 'Mongolés';
+$lang['language_ar_EG'] = 'Árabe Egipcio';
+$lang['language_br_FR'] = 'Bretón';
+$lang['language_dv_MV'] = 'Maldivo';
+$lang['language_id_ID'] = 'Indonesio';
+$lang['language_zh_TW'] = 'Chino Tradicional';
+$lang['yesterday'] = 'ayer';
+$lang['porg_get_started_desc'] = '¿Listo para comenzar a administrar tus fotos con Piwigo? ¡Únete a nuestra comunidad!';
+$lang['language_et_EE'] = 'Estonio';
+$lang['language_sk_SK'] = 'Eslovaco';
+$lang['language_sl_SI'] = 'Esloveno';
+$lang['language_sr_RS'] = 'Serbio';
+$lang['language_sv_SE'] = 'Sueco';
+$lang['language_th_TH'] = 'Tailandés';
+$lang['language_tr_TR'] = 'Turco';
+$lang['language_uk_UA'] = 'Ucraniano';
+$lang['language_vi_VN'] = 'Vietnamita';
+$lang['language_zh_CN'] = 'Chino';
+$lang['language_ms_MY'] = 'Malayo';
+$lang['language_nb_NO'] = 'Noruego';
+$lang['language_nl_NL'] = 'Holandés';
+$lang['language_nn_NO'] = 'Noruego';
+$lang['language_pl_PL'] = 'Polaco';
+$lang['language_pt_BR'] = 'Portugués Brasileño';
+$lang['language_pt_PT'] = 'Portugués';
+$lang['language_ro_RO'] = 'Rumano';
+$lang['language_ru_RU'] = 'Ruso';
+$lang['language_sh_RS'] = 'Serbio';
+$lang['language_hr_HR'] = 'Croata';
+$lang['language_hu_HU'] = 'Húngaro';
+$lang['language_is_IS'] = 'Islandés';
+$lang['language_it_IT'] = 'Italiano';
+$lang['language_ja_JP'] = 'Japonés';
+$lang['language_ka_GE'] = 'Georgiano';
+$lang['language_km_KH'] = 'Camboyés';
+$lang['language_ko_KR'] = 'Koreano';
+$lang['language_lt_LT'] = 'Lituano';
+$lang['language_gl_ES'] = 'Gallego';
+$lang['language_he_IL'] = 'Hebreo';
+$lang['language_ar_SA'] = 'Árabe Saudita';
+$lang['language_el_GR'] = 'Griego';
+$lang['language_en_GB'] = 'Inglés Británico';
+$lang['language_en_UK'] = 'Inglés';
+$lang['language_en_US'] = 'Inglés Americano';
+$lang['language_eo_EO'] = 'Esperanto';
+$lang['language_es_AR'] = 'Español Argentino';
+$lang['language_es_ES'] = 'Español';
+$lang['language_es_MX'] = 'Español Mexicano';
+$lang['language_eu_ES'] = 'Vasco';
+$lang['language_fa_IR'] = 'Persa';
+$lang['language_fi_FI'] = 'Finlandés';
+$lang['language_fr_CA'] = 'Francés Canadiense';
+$lang['language_fr_FR'] = 'Francés';
+$lang['What\'s new?'] = '¿Qué hay nuevo?';
+$lang['language_af_ZA'] = 'Afrikaans';
+$lang['language_bg_BG'] = 'Búlgaro';
+$lang['language_ca_ES'] = 'Catalán';
+$lang['language_cs_CZ'] = 'Checo';
+$lang['language_da_DK'] = 'Danés';
+$lang['language_de_DE'] = 'Alemán';
+$lang['Newsletters'] = 'Publicaciones';
+$lang['Get Involved'] = 'Colabora';
+$lang['Get started with Piwigo'] = 'Comienza con Piwigo';
+$lang['Get Started'] = 'Comienza';
+$lang['Showcases'] = 'Escaparate';
+$lang['See more'] = 'Descubre más';
+$lang['Latest news'] = 'Últimas noticias';
+$lang['News'] = 'Noticias';
+$lang['Open Source'] = 'Código Abierto';
+$lang['Organisations'] = 'Organizaciones';
+$lang['Plugins'] = 'Plugins';
+$lang['Press'] = 'Prensa';
+$lang['Professionals'] = 'Profesionales';
+$lang['Support'] = 'Soporte';
+$lang['Sustainable'] = 'Sostenible';
+$lang['Testimonials'] = 'Testimonios';
+$lang['Themes'] = 'Temas';
+$lang['Themes & Plugins'] = 'Temas & Plugins';
+$lang['What is Piwigo?'] = '¿Qué es Piwigo?';
+$lang['About'] = 'Acerca de';
+$lang['About us'] = 'Sobre nosotros';
+$lang['Batch Management'] = 'Proceso por Lotes';
+$lang['Changelogs'] = 'Logs de cambios';
+$lang['Contact'] = 'Contacto';
+$lang['Demo'] = 'Demostración';
+$lang['Discover'] = 'Descubrir';
+$lang['Documentation'] = 'Documentación';
+$lang['Donate'] = 'Donar';
+$lang['Extensions'] = 'Extensiones';
+$lang['Features'] = 'Características';
+$lang['Forum'] = 'Foro';
+$lang['Get Piwigo'] = 'Consigue Piwigo';
+$lang['High Volume'] = 'Alto Volumen';
+$lang['Home'] = 'Inicio';
+$lang['Languages'] = 'Idiomas';
