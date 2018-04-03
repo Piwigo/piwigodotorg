1bcd116bb72864335a3f4cbe004d2c0613a7806b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-03 13:51:50 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-2.9.2.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_755'] = 'Meta verileri senkronize etmek, Mysql 5.7 için bir tarih oluşturma hatası gönderir';
+$lang['porg_issue_767'] = 'Piwigo 2.9.1\'da Shotwell 0.22.0 ile yayınlama hatası';
+$lang['porg_issue_760'] = 'Tarih oluşturma, picture_modify öğesi değiştirilirken yenilenmemesi';
+$lang['porg_issue_723'] = 'özel bir kimlik istendiğinde, permalink ortaya çıkması';
+$lang['porg_issue_724'] = 'Hata Raporu: cat_options sayfasında SQL enjeksiyonu';
+$lang['porg_issue_721'] = 'Hata Raporu: CSRF ve Yanlış İzinler aracılığıyla yönetici özel albümünü herkese açık olarak ayarlama';
+$lang['porg_issue_787'] = 'password.php için beklenmedik \"action\" hata gösterimleri';
+$lang['porg_issue_777'] = 'WS kategorileri getImages, TotalCount';
+$lang['porg_issue_789'] = 'oturum açma sayfasında hata mesajını önleyebilme seçeneği';
+$lang['porg_issue_751'] = 'install/piwigo_structure-mysql.sql iki kez çalıştırılamaz';
+$lang['porg_issue_747'] = 'yönetici sayfa teması değişikliğinde PHP bilgilendirmesi';
+$lang['porg_issue_739'] = '[arama] arama kelimesi etiketlerle eşleştiğinde';
+$lang['porg_issue_732'] = 'transliterate işlevini yeniden adlandırma';
+$lang['porg_issue_731'] = 'genel pano, eksik fotoğraf sayısı';
+$lang['porg_issue_726'] = 'albüm oluşturmada tetikleme';
+$lang['porg_issue_725'] = 'HTML\'den pwg.images.setInfo koruma';
+$lang['porg_issue_678'] = 'PHP 7 RSS hatası';
+$lang['porg_issue_710'] = '\"redirect_decoded\" girdi parametresi geçersiz';
+$lang['porg_issue_713'] = 'güncelleme bildiri eposta içeriği Fransızca/İngilizce karışımı';
