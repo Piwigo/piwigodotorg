234c5e2893811f0e857e07280bb6aff4da1ed3e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-07 20:34:15 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/get_involved.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Bugtracker'] = 'Hata takipçi';
+$lang['Coding documentation'] = 'Kodlama belgemesi';
+$lang['Create a report'] = 'Rapor oluştur';
+$lang['Develop plugins'] = 'Eklentiler oluştur';
+$lang['Donate to Piwigo'] = 'Piwigo\'ya bağış yap';
+$lang['Help the Community'] = 'Topluluğa yardım et';
+$lang['Make this a monthly gift'] = 'Bunu aylık bir hediye yap';
+$lang['Recommend Piwigo'] = 'Piwigo tavsiye et';
+$lang['Security Report'] = 'Güvenlik Raporu';
+$lang['Submit bugs'] = 'Hatalar bildir';
+$lang['Submit product improvment'] = 'Ürün geliştirmesi gönder';
+$lang['Translate'] = 'Tercüme et';
+$lang['Translation tool'] = 'Çeviri aracı';
+$lang['We are grateful !'] = 'Minnettarız!';
+$lang['You know how to code?'] = 'Kodlama biliyor musun?';
+$lang['Your Gift'] = 'Hediyen';
