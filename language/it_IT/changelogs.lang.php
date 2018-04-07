29290fbd0324fb95d51b5056c3ade91f02816b40
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-07 20:33:17 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/changelogs.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_changelogs_technicals_desc1'] = 'Se preferisci avere una visione più tecnica delle versioni,<br> vai alla pagina <a href="%s">Branches and Releases</a> nella documentazione in lingua inglese.';
+$lang['porg_changelogs_desc2'] = 'Usa la versione più recente. Include le ultime correzioni di sicurezza e otterrai il supporto più facilmente.';
+$lang['porg_changelogs_desc1'] = 'Ecco la lista di tutte le versioni di Piwigo stabili.';
