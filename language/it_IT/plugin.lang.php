c45d447a7c2e30ddabfb1fe7d04651cdea084beb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-07 20:33:56 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,123 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['language_id_ID'] = 'Bahasa Indonesia';
+$lang['language_ka_GE'] = 'Georgiano';
+$lang['language_pt_BR'] = 'Portoghese brasiliano';
+$lang['language_pt_PT'] = 'Portoghese';
+$lang['language_ro_RO'] = 'Rumeno';
+$lang['language_ru_RU'] = 'Russo';
+$lang['language_sh_RS'] = 'Serbo';
+$lang['language_sk_SK'] = 'Slovacco';
+$lang['language_sl_SI'] = 'Sloveno';
+$lang['language_sr_RS'] = 'Serbo';
+$lang['language_sv_SE'] = 'Svedese';
+$lang['language_ta_IN'] = 'Tamil';
+$lang['language_th_TH'] = 'Thai';
+$lang['language_tr_TR'] = 'Turco';
+$lang['language_uk_UA'] = 'Ucraino';
+$lang['language_vi_VN'] = 'Vietnamita';
+$lang['language_zh_CN'] = 'Cinese';
+$lang['language_zh_HK'] = 'Hong Kong';
+$lang['language_zh_TW'] = 'Cinese tradizionale';
+$lang['porg_get_started_desc'] = 'Pronto a gestire le tue foto grazie a Piwigo? Partecipa alla nostra comunità!';
+$lang['yesterday'] = 'ieri';
+$lang['language_fr_CA'] = 'Quebecchese';
+$lang['language_fr_FR'] = 'Francese';
+$lang['language_gl_ES'] = 'Gallego';
+$lang['language_he_IL'] = 'Ebraico';
+$lang['language_hr_HR'] = 'Croato';
+$lang['language_hu_HU'] = 'Ungherese';
+$lang['language_is_IS'] = 'Islandese';
+$lang['language_it_IT'] = 'Italiano';
+$lang['language_ja_JP'] = 'Giapponese';
+$lang['language_km_KH'] = 'Khmer';
+$lang['language_kn_IN'] = 'Kannada';
+$lang['language_ko_KR'] = 'Coreano';
+$lang['language_lt_LT'] = 'Lituano';
+$lang['language_lv_LV'] = 'Lettone';
+$lang['language_mn_MN'] = 'Mongolo';
+$lang['language_ms_MY'] = 'Malese';
+$lang['language_nb_NO'] = 'Norvegese';
+$lang['language_nl_NL'] = 'Olandese';
+$lang['language_nn_NO'] = 'Norvegese';
+$lang['language_pl_PL'] = 'Polacco';
+$lang['language_ar_SA'] = 'Arabo Saudita';
+$lang['language_bg_BG'] = 'Bulgaro';
+$lang['language_br_FR'] = 'Bretone';
+$lang['language_ca_ES'] = 'Catalano';
+$lang['language_cs_CZ'] = 'Ceco';
+$lang['language_da_DK'] = 'Danese';
+$lang['language_de_DE'] = 'Tedesco';
+$lang['language_dv_MV'] = 'Dhivehi Maldive';
+$lang['language_el_GR'] = 'Greco';
+$lang['language_en_GB'] = 'Inglese britannico';
+$lang['language_en_UK'] = 'Inglese';
+$lang['language_en_US'] = 'Inglese americano';
+$lang['language_eo_EO'] = 'Esperanto';
+$lang['language_es_AR'] = 'Argentino';
+$lang['language_es_ES'] = 'Spagnolo';
+$lang['language_es_MX'] = 'Messicano';
+$lang['language_et_EE'] = 'Estone';
+$lang['language_eu_ES'] = 'Basco';
+$lang['language_fa_IR'] = 'Persiano';
+$lang['language_fi_FI'] = 'Finlandese';
+$lang['[piwigo.org contact form, %s] %s contacted you on %s'] = '[piwigo.org Modulo di contatto, %s] %s ti ha contattato su %s';
+$lang['Latest news'] = 'Ultime notizie';
+$lang['News'] = 'Notizie';
+$lang['Newsletters'] = 'Newsletter';
+$lang['Open Source'] = 'Opensource';
+$lang['Organisations'] = 'Organizzazioni';
+$lang['Plugins'] = 'Plugins';
+$lang['Press'] = 'Stampa';
+$lang['Professionals'] = 'Professionisti';
+$lang['See more'] = 'Vedi altro';
+$lang['Showcases'] = 'Elenco';
+$lang['Support'] = 'Supporto';
+$lang['Sustainable'] = 'Perenne';
+$lang['Testimonials'] = 'Testimonianze';
+$lang['Themes'] = 'Temi';
+$lang['Themes & Plugins'] = 'Temi e plugin';
+$lang['What is Piwigo?'] = 'Piwigo, che cos\'è?';
+$lang['What\'s new?'] = 'Cosa c\'è di nuovo?';
+$lang['language_af_ZA'] = 'Afrikaans';
+$lang['language_ar_EG'] = 'Arabo egiziano';
+$lang['About us'] = 'Chi siamo';
+$lang['Batch Management'] = 'Gestione batch';
+$lang['Changelogs'] = 'Versioni';
+$lang['Contact'] = 'Contatto';
+$lang['Demo'] = 'Demo';
+$lang['Discover'] = 'Scoprire';
+$lang['Documentation'] = 'Documentazione';
+$lang['Donate'] = 'Fai una donazione';
+$lang['Extensions'] = 'Estensioni';
+$lang['Features'] = 'Caratteristiche';
+$lang['Forum'] = 'Forum';
+$lang['Get Involved'] = 'Contribuire';
+$lang['Get Piwigo'] = 'Ottenere Piwigo';
+$lang['Get Started'] = 'Avvio';
+$lang['Get started with Piwigo'] = 'Inizia con Piwigo';
+$lang['High Volume'] = 'Forte volumetria';
+$lang['Home'] = 'Home';
+$lang['Individuals'] = 'Individui';
+$lang['Languages'] = 'Lingue';
+$lang['About'] = 'About';
