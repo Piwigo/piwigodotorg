e1fc0f9fff2cd8f232e7a57290df9590c9c07a5c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-09 21:06:35 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.9.1.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_663'] = 'SQL-fejl i udflugt 2.9';
+$lang['porg_issue_667'] = 'Fejlrapport: Cross Site Scripting(XSS) i pageparameter i admin.php';
+$lang['porg_issue_674'] = '[php7.1] brugerredigeringsboks, []-operator er ikke understøttet ved strenge';
+$lang['porg_issue_679'] = 'admininstrumentbræt, trigger er nødvendig til plugins';
+$lang['porg_issue_683'] = 'diskforbrug, skjul decimaler ved mere end 100GB';
+$lang['porg_issue_685'] = 'batch manager, slet fotografier virker ikke med Safari';
+$lang['porg_issue_693'] = 'Batch downloader fejler ved brug af pclzip og php 7.1';
+$lang['porg_issue_696'] = 'PIWIGO-API - pwg.categories.delete fungerer ikke korrekt.';
+$lang['porg_issue_699'] = 'Filmedtagelse i administration';
+$lang['porg_issue_701'] = 'Albumsynkroniseringslinket peger på synkroniseringssiden  ./galleries/ selv hvis albummet er placeret i en anden mappe';
+$lang['porg_issue_705'] = 'brugerlistebackend, tjek inputparameter';
+$lang['porg_issue_706'] = '[sikkerhed] identifikation bør kun viderestille indenfor Piwigo';
+$lang['porg_issue_707'] = 'antal visninger, skjul decimaler hvis mindre end 1k';
+$lang['porg_issue_710'] = 'inputparameter \"redirect_decoded\" er ikke gyldigt';
