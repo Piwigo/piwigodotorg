fccea9a23dbac502fa35307be73ab4fe8e3f14c5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-12 22:48:51 +0200

    Update es_ES, thanks to JamworkS

---
+++ language/es_ES/about_us.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_about_us_business_desc1'] = 'Aunque animado por una gran comunidad de colaboradores voluntarios, un proyecto como Piwigo no funciona gratuitamente: servidores web, nombres de dominio, tiempo de desarrollo o administración web.';
+$lang['porg_about_us_mission_desc'] = 'La misión de Piwigo es crear el mejor Administrador de Fotografías para la web con un gran foco en la experiencia de usuario sobre tecnología.';
+$lang['porg_about_us_join_desc1'] = 'El proyecto Piwigo necesita de varias habilidades, como programadores, diseñadores, escritores de documentación, ayudantes.';
+$lang['porg_about_us_service_desc2'] = 'La mayor parte del tiempo, estas creaciones están disponibles a la totalidad de la comunidad Piwigo.';
+$lang['Arrivals of Plugins'] = 'Nuevos Plugins';
+$lang['porg_about_us_webgallery_desc'] = 'Nuevo nombre, versión 2.0 pero mismo proyecto.';
+$lang['Releases'] = 'Lanzamientos';
+$lang['porg_about_us_join_desc2'] = 'Si crees en nuestra misión, sé parte de ella :-)';
+$lang['porg_about_us_launch_desc'] = 'Hosting en la nube, listo para usar por todo el mundo.';
+$lang['porg_about_us_plugins_desc1'] = 'Grandes cambios tanto técnicos como de mentalidad.';
+$lang['porg_about_us_plugins_desc2'] = 'Nuevas funcionalidades y colaboradores.';
+$lang['porg_about_us_recruitement_desc'] = 'El equipo se hace más grande y la actividad en programación crece enormemente.';
+$lang['porg_about_us_service_desc1'] = 'Algunos programadores y creadores de temas son pagados por los usuarios para crear nuevas funcionalidades.';
+$lang['Years old'] = 'Años';
+$lang['You?'] = '¿Tú?';
+$lang['porg_about_us_apps_desc'] = 'Piwigo dispone de versiones para iPhone y Android.';
+$lang['porg_about_us_beginning_desc1'] = 'Disponible en Free.fr como Instalación rápida (un sólo click).';
+$lang['porg_about_us_beginning_desc2'] = 'Atraerá a muchos colaboradores.';
+$lang['porg_about_us_birth_desc'] = 'Después de meses de preparación, la versión 1.0.0 fue lanzada.';
+$lang['porg_about_us_business_desc2'] = '¿Cómo se financia este proyecto?';
+$lang['porg_about_us_contributors_desc'] = '¡Colaboradores de todo el mundo, trabajando en cada parte del Ecosistema de Piwigo!';
+$lang['porg_about_us_desc1'] = 'Somos una comunidad de colaboradores de todo el mundo.';
+$lang['porg_about_us_desc2'] = 'Si crees en la misión del Open Source, ¡esperamos que te unas a nosotros!';
+$lang['porg_about_us_donation_desc1'] = 'La Asociación Piwigo recoge donaciones de su comunidad de usuarios.';
+$lang['porg_about_us_donation_desc2'] = 'El dinero es usado para pagar los gastos de los servidores web, nombres de dominio o suscripciones a las Store de aplicaciones móviles.';
+$lang['porg_about_us_donation_desc3'] = '<a href="%s">Tu contribución financiera</a> marcará tu compromiso con el proyecto.';
+$lang['porg_about_us_dotcom_desc1'] = 'Los beneficios generados hacen posible que podamos dedicarle tiempo a Piwigo.';
+$lang['porg_about_us_dotcom_desc2'] = 'Por ejemplo Pierrick, creador de Piwigo y ahora empleado de Piwigo.com, puede ahora trabajar en Piwigo.org.';
+$lang['porg_about_us_dotcom_desc3'] = 'Los dos proyectos mantienen un círculo vicioso: el éxito de uno lleva al éxito del otro y viceversa.';
+$lang['porg_about_us_github_desc'] = 'Piwigo oficialmente se une a la comunidad de programadores de Github.';
+$lang['Beginning'] = 'Comienzo';
+$lang['Birth'] = 'Nacimiento';
+$lang['Business Model'] = 'Modelo de Negocio';
+$lang['Donations'] = 'Donaciones';
+$lang['Downloads'] = 'Descargas';
+$lang['Forum messages'] = 'Mensajes de los Foros';
+$lang['Get involved!'] = 'Involúcrate';
+$lang['Mobile apps'] = 'Apps Móviles';
+$lang['Our Mission'] = 'Nuestra misión';
+$lang['PhpWebGallery becomes Piwigo'] = 'PhpWebGallery se convierte en Piwigo';
+$lang['Piwigo contributors'] = 'Colaboradores de Piwigo';
+$lang['Piwigo moves to GitHub'] = 'Piwigo se muda a GitHub';
+$lang['Piwigo.com'] = 'Piwigo.com';
+$lang['Piwigo.com is launched'] = 'Lanzamos Piwigo.com';
+$lang['Recruitement'] = 'Contratación';
+$lang['Services'] = 'Servicios';
+$lang['Take part in our adventure'] = 'Toma parte en nuestra aventura';
+$lang['Want to be part of the adventure?'] = '¿Quieres ser parte de la aventura?';
