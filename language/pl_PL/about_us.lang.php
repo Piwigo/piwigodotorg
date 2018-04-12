73d9da4c320def71cda85a0849c2190ba789ea30
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-12 14:59:26 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/about_us.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Beginning'] = 'Początek';
+$lang['Birth'] = 'Narodziny';
+$lang['Business Model'] = 'Model biznesowy';
+$lang['Donations'] = 'Darowizny';
+$lang['Downloads'] = 'Pobrania';
+$lang['Forum messages'] = 'Wiadomości z forum';
+$lang['Get involved!'] = 'Zaangażuj się!';
+$lang['Mobile apps'] = 'Aplikacje mobilne';
+$lang['Our Mission'] = 'Nasza Misja';
+$lang['Recruitement'] = 'Rekrutacja';
+$lang['Releases'] = 'Wydania';
+$lang['Services'] = 'Usługi';
+$lang['Take part in our adventure'] = 'Weź udział w naszej przygodzie';
