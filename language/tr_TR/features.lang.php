f831c7a0265c99a52a629da2bc95102c8d9a599e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-17 11:15:31 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/features.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_easy_install_desc1'] = 'Piwigo\'u minimum gereksinimle, kurulumu çok kolay olacak şekilde tasarladık.';
+$lang['porg_features_easy_install_desc2'] = 'Güncellemeler, tek bir düğme basarak kolay yüklenir!';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo gömülü meta veriden GPS enlem ve boylam okuyabilirsiniz.';
+$lang['porg_features_organize_desc1'] = 'Fotoğraflarınızı kendi albümleriniz arasında dağıtın.';
+$lang['porg_features_organize_desc2'] = 'Albüm ağacının sınırsız derinliği var.';
+$lang['porg_features_slideshow_desc'] = 'Fotorama eklentisi veya Bootstrap Darkroom gibi belirli temalar ile, tam ekran slayt gösterisi sunabılırsınız.';
+$lang['porg_features_upload_desc'] = 'Web formu, herhangi bir FTP istemcisi veya digiKam, Shotwell, Lightroom<br>veya mobil uygulamalar gibi bir masaüstü uygulaması ile<br>fotoğraf ekleyebilirsiniz.';
+$lang['porg_features_user_management_desc1'] = 'Yöneticiler, kullanıcılar, gruplar oluşturabilir, izinleri yönetebilir ve bildirimleri gönderebilir.';
+$lang['porg_features_user_management_desc2'] = 'Birkaç yönetici aynı anda Piwigo üzerinde çalışabilir.';
+$lang['porg_features_user_management_desc3'] = 'Takımın için mükemmel!';
+$lang['porg_features_organize_desc3'] = 'Her fotoğraf birkaç albüme ait olabilir.';
+$lang['porg_features_plugins_desc1'] = 'Temalarla görünümünü değiştirin.';
+$lang['porg_features_plugins_desc2'] = 'Eklentileri ile özellikler ekleyin.';
+$lang['porg_features_plugins_desc3'] = 'Uzantıları birkaç tıklama ile yüklenebilir.';
+$lang['porg_features_plugins_desc4'] = '<span class="bold">350 uzantı</span> mevcuttur ve onların sayı büyüyor!';
+$lang['porg_features_privacy_desc1'] = 'Fotoğraflarınızı kişiye özel yapın ve her birini kimin görebileceğine karar verin.';
+$lang['porg_features_privacy_desc2'] = 'Albümler ve fotoğraflar, gruplar veya tek tek kullanıcılar için izinler ayarlayabilirsiniz.';
+$lang['A great place for your visitors'] = 'Ziyaretçileriniz için harika bir yer';
+$lang['API'] = 'API';
+$lang['Batch Manager'] = 'Toplu İş Yöneticisi';
+$lang['Browse by Dates'] = 'Tarihe göre gözat';
+$lang['Easy installation and updates'] = 'Kolay kurulum ve güncellemeler';
+$lang['Full screen slideshow'] = 'Tam ekran slayt gösterisi';
+$lang['Geolocate your photos on a map'] = 'Fotoğraflarınızı harita üzerinde buldurunuz';
+$lang['Import automatic your metadata'] = 'Otomatik olarak meta verilerinizi içe aktarın';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Piwigo kendi ihtiyaçlarınıza göre birçok yapılandırma ayarları';
+$lang['Organize with Album'] = 'Albüm ile Düzenle';
+$lang['Privacy on your Photos'] = 'Fotoğraflarınızdaki Kişiye Özellik';
+$lang['Upload your photos'] = 'Fotoğraflarınızı yükleyin';
+$lang['Users management'] = 'Kullanıcı yönetimi';
+$lang['porg_features_desc1'] = 'Piwigo bir fotoğraf galerisi için özellikleri geniş olan bir altyapı sunuyor.';
+$lang['porg_features_desc2'] = 'Mevcut özelliklerin çoğu aşağıda listelenmiştir.';
