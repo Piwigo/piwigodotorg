53952e7c9637cd4b3d5097236fd2f153251e2496
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-17 11:15:42 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/release.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Released on %s'] = 'Wydane na %s';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'Funkcja skrótu MD5 jest wygodnym narzędzie do zabezpieczenia Twoich plików przed uszkodzeniem.';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Rekomendujemy <b>automatyczne aktualizacje</b>.';
+$lang['Automatic upgrade'] = 'Automatyczna aktualizacja';
+$lang['Bugs fixed'] = 'Poprawione błędy';
+$lang['Download Piwigo %s'] = 'Pobierz Piwigo %s';
+$lang['Featured added'] = 'Dodane funkcjonalności';
+$lang['Known issues'] = 'Znane problemy';
+$lang['Languages'] = 'Języki';
+$lang['Manual Upgrade'] = 'Ręczna aktualizacja';
+$lang['New language:'] = 'Nowy język:';
+$lang['No database upgrade is required.'] = 'Aktualizacja bazy danych nie jest wymagana.';
+$lang['Release note'] = 'Informacje o wydaniu';
+$lang['Updated languages'] = 'Aktualizuj języki';
+$lang['Upgrade'] = 'Aktualizacja';
