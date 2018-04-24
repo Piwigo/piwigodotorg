8529e135fa210be43e3a591289c3f0c3ca0d42e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-24 06:08:25 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.8.2.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_270'] = '[skabelon] Manglende password.tpl i temaet smartpocket';
+$lang['porg_issue_415'] = 'Downloadlink fejler ved filer der ikke er billeder';
+$lang['porg_issue_430'] = '[Batch Manager] rediger forfatter/titel kan anvende placeholder\'en';
+$lang['porg_issue_476'] = 'HTTP-viderestilling ved forbudt gæsteadgang';
+$lang['porg_issue_479'] = 'Der er ingen passende CSPRNG';
