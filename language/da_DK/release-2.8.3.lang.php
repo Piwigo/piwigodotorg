492c6750e923e84b3cbefd65dcc57856cf64b8aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-24 06:08:55 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.8.3.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_306'] = '[sprog] Anden sprogfil end plugin.lang indlæses ikke; rettet af <a href="http://piwigo.org/forum/profile.php?id=4851">LucMorizur</a>';
+$lang['porg_issue_376'] = '[MySQL 5.7] ORDER BY-clause er ikke i SELECT-listen, rettet af <a href="http://piwigo.org/forum/profile.php?id=22630">Mercury_LABBS</a>';
+$lang['porg_issue_449'] = 'Videoupload fejler, rettet af <a href="https://github.com/vvinogradsky">vvinogradsky</a> (forfatter af plugin\'en til eksport fra Lightroom til Piwigo) og <a href="https://github.com/dkm">dkm</a>';
+$lang['porg_issue_542'] = 'Ny bruger bør ikke altid anvende browserens sprog';
+$lang['porg_issue_546'] = 'Fejl ved nulstilling af adgangskode';
+$lang['porg_issue_547'] = 'Forøg kontroller af URL-format; rapporteret af Nicolas Bidron fra <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
+$lang['porg_issue_548'] = 'Hurtigsøgning og HTML-kommentar; rapporteret af Nicolas Bidron fra <a href="https://www.nccgroup.trust">NCC Group Security Advisory</a>';
