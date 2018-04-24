fd0786fcd73257c705321e6ab8d6a67be93c8ed6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-24 06:08:35 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.8.1.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_434'] = 'Udfaset constructor til LocalSiteReader';
+$lang['porg_issue_436'] = 'Kald af udefineret metode Logger::warning i i.php';
+$lang['porg_issue_439'] = 'Opdaterer indlejret dokumentation til at beskrive adskillige formater';
+$lang['porg_issue_440'] = 'Søgning (temaet smartpocket) giver ingen resultater';
+$lang['porg_issue_442'] = 'Kald af udefineret funktion set_cat_status() ';
+$lang['porg_issue_443'] = 'Det lykkes ikke at oprette tabeller med MySQL 5.7';
+$lang['porg_issue_466'] = '[Batch Manager] Nyt album vises ikke ved flytning af fotografier';
+$lang['porg_issue_470'] = 'Forøg tilfældighed ved generate_key. Sårbarhed rapporteret af  <a href="https://www.danclifford.eu/">Dan Clifford</a>. Stor tak til ham!';
