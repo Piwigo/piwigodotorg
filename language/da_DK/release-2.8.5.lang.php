a8418c2d06310790b626d1c4a35f19c8026e3449
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-24 06:09:06 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.8.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_572'] = 'CVE-2016-10084 filindlæsningsangreb, rapporteret af <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_573'] = 'CVE-2016-10085 filindlæsningsangreb #2, rapporteret af <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_574'] = 'Filindlæsning med mulig RCE, rapporteret af <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_575'] = 'CVE-2016-10083 Udførelse af scripts på tværs af websteder, rapporteret af <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_595'] = 'Opdaterer PHPMailer til 5.2.21';
+$lang['porg_issue_599'] = 'Retter PHPMailers SMTP-klasseindlæsning, når der anvendes en SMTP-transport';
