9fb707f4243f8c7b8fa043a5a9261bbfbbd6de54
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-24 06:09:16 +0200

    Update hu_HU, thanks to asicu.com

---
+++ language/hu_HU/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Category'] = 'Kategória';
+$lang['Contact us'] = 'Kapcsolat';
+$lang['Partnership'] = 'Partnerség';
+$lang['Press inquiry'] = 'Újság-ügyi kérdések';
+$lang['Security report'] = 'Biztonsági jelentés';
+$lang['Send message'] = 'Üzenet küldése';
+$lang['Testimonial'] = 'Beszámolók';
+$lang['Your email address'] = 'E-mail címed';
+$lang['Your message'] = 'Üzeneted';
+$lang['porg_contact_desc1'] = 'Hagyhatsz üzenetet az alábbi kapcsolati úrlap segítségével';
+$lang['porg_contact_desc2'] = 'Ezt főleg honlap-ügyi megjegyzésekre használd';
+$lang['porg_contact_desc3'] = 'Nekünk jobb, ha a Fórumot használod minden hozzászólásra, csapatunknak könnyebb a nyomkövetés így.';
+$lang['porg_contact_error_email'] = 'Hoppá! Ez egy érvénytelen e-mail cím!';
+$lang['porg_contact_error_message'] = 'Hoppá! Úgy tűnik, hibás az üzeneted';
