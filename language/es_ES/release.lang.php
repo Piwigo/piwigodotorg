2c4c9e9a181ac16aae753ce367872b391ab622d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-05-07 06:32:47 +0200

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = 'Puede comparar el md5sum de su archivo descargado con el md5sum oficial provisto en piwigo.org.';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'La lista de cambios mayores se describen en <a href="%s">la nota de Prensa de Piwigo %s</a>';
+$lang['Automatic upgrade'] = 'Actualización automática';
+$lang['Bugs fixed'] = 'Errores solucionados';
+$lang['Download Piwigo %s'] = 'Descarga Piwigo %s';
+$lang['Featured added'] = 'Funciones añadidas';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Si estas corriendo una versión anterior a %s y no quieres usar el actualizador automático, sigue el <b>proceso manual</b>.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Si estás corriendo <b>Piwigo %s</b> también puedes descargar el archivo <b>%s_a_%s.zip</b> que contiene los ficheros nuevos y modificados.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Si estás corriendo <b>la versión 2.2+</b>, Piwigo te dirá que plugins pueden no ser compatibles con Piwigo %s antes de la actualización.';
+$lang['Known issues'] = 'Errores conocidos';
+$lang['Languages'] = 'Idiomas';
+$lang['Manual Upgrade'] = 'Actualización manual';
+$lang['New language:'] = 'Nuevo idioma:';
+$lang['No database upgrade is required.'] = 'No se requiere actualizar la base de datos.';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Una vez que hayas extraido los archivos, transfiérelos a tu web con un cliente FTP sobre la instalación %s de Piwigo.';
+$lang['Release note'] = 'Nota de actualizacion';
+$lang['Released on %s'] = 'Actualizado el %s';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'El sum MD5 es una herramienta útil que sirve para estar seguro de que tu archivo no está dañado.';
+$lang['Updated languages'] = 'Idiomas actualizados';
+$lang['Upgrade'] = 'Actualización';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Recomendamos la <b>actualización automática</b>.';
