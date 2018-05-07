0800dddf200940388946fbbab2eb907cfd618e55
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-05-07 06:32:57 +0200

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/newsletters.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Enter your email here'] = 'Introduce tu email aquí';
+$lang['Sign up'] = 'Regístrate';
+$lang['porg_newsletters_desc1'] = 'Estate atento a las novedades de Piwigo';
+$lang['porg_newsletters_desc2'] = 'Recebirás un email cuando haya una nueva versión disponible y cuando ocurran eventos importantes al proyecto.';
+$lang['porg_newsletters_subscribe_title'] = '¡Suscríbete al proyecto y estate al día de las novedades!';
