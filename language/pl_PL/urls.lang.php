8156f2b5e7fd06abbeb7e23ffc815714ec9348a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-05-09 21:03:35 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/urls.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_urls']['changelogs'] = 'dziennik-zmian';
+$lang['porg_urls']['get-involved'] = 'zaangazuj-sie';
+$lang['porg_urls']['get-piwigo'] = 'pobierz-piwigo';
+$lang['porg_urls']['testimonials'] = 'referencje';
+$lang['porg_urls']['about-us'] = 'o-nas';
+$lang['porg_urls']['contact'] = 'kontakt';
+$lang['porg_urls']['features'] = 'właściwości';
+$lang['porg_urls']['get-started'] = 'rozpocznij';
+$lang['porg_urls']['news'] = 'nowosci';
+$lang['porg_urls']['newsletters'] = 'newsletter';
+$lang['porg_urls']['press'] = 'prasa';
+$lang['porg_urls']['release'] = 'wydania';
+$lang['porg_urls']['what-is-piwigo'] = 'czym-jest-piwigo';
