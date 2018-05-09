7c113910bbdff4edd861252224be0127d5e33601
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-05-09 06:12:54 +0200

    Add zh_CN, thanks to dennisyan

---
+++ language/zh_CN/release-2.9.3.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_735'] = '能够通过 API 获取验证密钥';
+$lang['porg_issue_822'] = '在设置页使用 xss+csrf 攻击';
+$lang['porg_issue_823'] = '对管理员/用户进行 SQL 注入';
+$lang['porg_issue_824'] = '在 MacOS X 下发布失败';
+$lang['porg_issue_825'] = '对批量管理、单独管理的 SQL 注入';
+$lang['porg_issue_826'] = '对设置的 SQL 注入';
+$lang['porg_issue_838'] = '[批量管理] 删除 \"all set\" 产生一个错误的报错信息';
+$lang['porg_issue_839'] = 'Piwigo v2.9.2 - 对后台管理界面的 SQL 注入';
