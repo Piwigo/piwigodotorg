0b65f32a78ade79ffc07cdc5a97e141a1dd3d2f7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-05-09 06:12:43 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/contact.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Category'] = '类别';
+$lang['Testimonial'] = '感言';
+$lang['porg_contact_desc2'] = '请将留言内容主要用于评论网站。';
+$lang['porg_contact_desc3'] = '我们推荐使用支持论坛进行使用反馈，便于团队跟进。';
+$lang['Contact us'] = '联系我们';
+$lang['Partnership'] = '合作关系';
+$lang['Press inquiry'] = '媒体调查';
+$lang['Security report'] = '安全报告';
+$lang['Send message'] = '发送留言';
+$lang['Your email address'] = '您的email地址';
+$lang['Your message'] = '您的留言';
+$lang['porg_contact_desc1'] = '您可以使用下面的联系表单留言。';
+$lang['porg_contact_error_email'] = '抱歉，该email地址无效';
+$lang['porg_contact_error_message'] = '抱歉，您的留言似乎出了问题';
