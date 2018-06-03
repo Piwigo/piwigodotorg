22fce9675940ba1f7162b6985820d79248ec10a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-06-03 18:39:42 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/what_is_piwigo.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_how_to_desc2'] = 'L\'uso di Piwigo è gratuito, paghi solo il tuo provider.';
+$lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo è anche disponibile <em>come servizio</em> su Piwigo<span style="color:#ff3363">.com</span>: apri un account di prova in pochi secondi, usa Piwigo e dimentica i prerequisiti tecnici.';
+$lang['porg_whatispiwigo_how_to_desc4'] = 'Piwigo.com è un\'offerta a pagamento perché include un servizio di hosting.';
+$lang['porg_whatispiwigo_individual_desc1'] = 'Gestisci l\'album fotografico della famiglia negli ultimi 20 anni, categorizza una raccolta di foto di ragni europei, mostra le tue migliori foto di paesaggi come fotografo dilettante ...';
+$lang['porg_whatispiwigo_individual_desc2'] = 'sono alcuni esempi dell\'uso di Piwigo da parte della sua comunità.';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo è sempre stato e resterà un software libero.';
+$lang['porg_whatispiwigo_open_source_desc2'] = 'Il codice sorgente accessibile è un codice di cui ti fidi.';
+$lang['porg_whatispiwigo_organisation_desc1'] = 'Settore pubblico, aziende, fondazioni ... tutti hanno bisogno di gestire la propria collezione di foto.';
+$lang['porg_whatispiwigo_organisation_desc2'] = 'Nel campo del <em>Digital Asset Management</em> (DAM), Piwigo si concentra sulla facilità d\'uso, la convenienza, le prestazioni e la perennità.';
+$lang['porg_whatispiwigo_professional_desc1'] = 'Les photographes pro ont besoin de montrer leur travail à leurs clients, publiquement ou en privé.';
+$lang['porg_whatispiwigo_professional_desc2'] = 'Piwigo fornisce un sistema avanzato di gestione delle autorizzazioni.';
+$lang['porg_whatispiwigo_professional_desc3'] = 'I clienti possono scaricare le foto una alla volta o un album completo.';
+$lang['porg_whatispiwigo_sustainable_desc1'] = 'Nato nel 2002, Piwigo ha chiaramente raggiunto la fase di <em>maturità</em>.';
+$lang['porg_whatispiwigo_sustainable_desc2'] = 'Certo, continua ad evolversi con le tecnologie di oggi!';
+$lang['porg_whatispiwigo_use_desc1'] = 'Esiste un\'ampia varietà di usi tra i membri della comunità di Piwigo.';
+$lang['porg_whatispiwigo_use_desc2'] = 'Dalla galleria di medie dimensioni alle enormi librerie di foto, Piwigo può semplificarti la vita risparmiando tempo ed energia.';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Collections'] = 'Collezioni';
+$lang['Families'] = 'Famiglie';
+$lang['Fansites'] = 'Siti di fan';
+$lang['GNU License'] = 'Licenza GNU';
+$lang['How to use Piwigo ?'] = 'Come usare Piwigo?';
+$lang['Lightning Fast'] = 'Veloce come un fulmine';
+$lang['Multiple Administrators'] = 'Amministratori Multipli';
+$lang['Share with Clients'] = 'Condivisione con i clienti';
+$lang['What can I use Piwigo for ?'] = 'Piwigo, per quali usi?';
+$lang['Why choosing Piwigo ?'] = 'Pourquoi choisir Piwigo ?';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo è un\'applicazione web per gestire la tua collezione di foto e altri media.';
+$lang['porg_whatispiwigo_desc2'] = 'Con potenti funzionalità, gestisce gallerie in tutto il mondo.';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'La GNU General Public License, o GPL, è una licenza open source.';
+$lang['porg_whatispiwigo_gnu_desc2'] = 'Opensource non significa solo che puoi vedere il codice sorgente - ci sono anche implicazioni politiche e filosofiche.';
+$lang['porg_whatispiwigo_gnu_desc3'] = 'L\'Opensource o "Software Libero" significa che sei libero di modificare e ridistribuire il codice sorgente a determinate condizioni.';
+$lang['porg_whatispiwigo_high_volume_desc1'] = '500.000 foto nella tua collezione? Non è un problema per Piwigo.';
+$lang['porg_whatispiwigo_high_volume_desc2'] = 'Prestazioni di fascia alta incluse, di default';
+$lang['porg_whatispiwigo_how_to_desc1'] = 'Puoi scaricare Piwigo da Piwigo<span style="color:#f70;">.org</span> e installarlo sul tuo web hosting, con i prerequisiti tecnici necessari.';
+$lang['Amateurs'] = 'Dilettanti';
