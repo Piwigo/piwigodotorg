dae3755cb20b2fe36e6c8fb1b757a36c763d9517
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-06-21 06:29:04 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-2.9.1.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_710'] = 'girdi parametresi \"redirect_decoded\" geçerli değil';
+$lang['porg_issue_707'] = 'görüntülenme sayısı, 1k\'dan az olduğunda ondalık notları gizler';
+$lang['porg_issue_706'] = '[güvenlik] kimliği sadece Piwigo\'da yönlendirilmelidir';
+$lang['porg_issue_705'] = 'kullanıcı listesi arka uç, giriş parametresini kontrol et';
+$lang['porg_issue_701'] = 'Albüm eşitleme bağlantısı, ./galleries/ adresini eşitlemek için albüm başka bir klasörde olsa bile sayfaya ulaşır';
+$lang['porg_issue_699'] = 'Yönetim kısmına dosya iliştirme';
+$lang['porg_issue_696'] = 'PIWIGO API - pwg.categories.delete düzgün çalışmıyor';
+$lang['porg_issue_693'] = 'pclzip ve php 7.1 \'de yığın olarak indirme hatalı';
+$lang['porg_issue_685'] = 'yığın yönetimi, Safari\'de fotoğraf silme çalışmıyor';
+$lang['porg_issue_683'] = 'disk kullanımı, 100 GB\'den fazla olduğunda ondalık gizleniyor';
+$lang['porg_issue_679'] = 'Yönetici kontrol paneli, eklentiler için tetiklemeye ihtiyaç var';
+$lang['porg_issue_674'] = '[php7.1] kullanıcı düzenleme penceresi, [] operatör dizeler için desteklenmiyor';
+$lang['porg_issue_667'] = 'Hata Raporu: admin.php sayfa parametresinde çapraz site komut (XSS)';
+$lang['porg_issue_663'] = '2.9 turunda SQL hatası';
