7325472f646942cd7fdc4ef9118ade92e63408b3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-07 04:54:57 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/get_piwigo.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_piwigo_mobile_desc2'] = '您还可以全屏浏览整个图库，创建、编辑、移动相册或编辑照片属性... 只需要用指尖触动！';
+$lang['porg_get_piwigo_self_hosted_desc1'] = '在支持 PHP 和 MySQL 数据库的您自己的网络服务器上运行 Piwigo。';
+$lang['porg_get_piwigo_self_hosted_desc2'] = 'Piwigo 本身是免费的，但您需要在有新版本发布时进行更新，以保持程序的安全性和运行的流畅度。';
+$lang['porg_get_piwigo_cloud_hosted_desc3'] = '您还能得到 Piwigo 设计者的专门支持。';
+$lang['porg_get_piwigo_desc1'] = '您可以通过两种方式使用 Piwigo。';
+$lang['porg_get_piwigo_desc2'] = '您可以将 Piwigo 托管于诸如 OVH、BlueHost 等服务器上，也可以使用 Piwigo.com 的云服务。';
+$lang['porg_get_piwigo_desc3'] = '两种途径您对您的数据都拥有完全的所有权。';
+$lang['porg_get_piwigo_mobile_desc1'] = '经常使用您的 iPhone 拍照吗？您可以直接将照片从智能手机发布到您的 Piwigo。';
+$lang['Blog'] = 'Blog';
+$lang['Cloud-hosted Piwigo'] = '云托管的 Piwigo';
+$lang['Download<br>Piwigo %s'] = '下载<br>Piwigo %s';
+$lang['Enterprises'] = '企业';
+$lang['Infrastructure'] = '基础架构';
+$lang['Install Guide'] = '安装指南';
+$lang['NetInstall'] = '在线安装';
+$lang['Piwigo Mobile App'] = 'Piwigo 移动端 App';
+$lang['Self-hosted Piwigo'] = '自托管 Piwigo';
+$lang['Upgrade Guide'] = '升级指南';
+$lang['porg_get_piwigo_cloud_hosted_desc1'] = '使用 Piwigo.com，你无需为技术问题费心。';
+$lang['porg_get_piwigo_cloud_hosted_desc2'] = '安装、升级、备份完全在专为 Piwigo 定制的基础架构上进行。';
