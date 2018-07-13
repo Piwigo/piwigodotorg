81677e42f4589abb678e93e2fb0e1ad8d1af8e8e
plegall <plg@piwigo.org>
2018-07-13 18:16:54 +0200

    [FR] translate release note 2.9.4

---
+++ language/fr_FR/release-2.9.4.lang.php
@@ -0,0 +1,17 @@
+<?php
+$lang['porg_issue_894'] = 'Correction erreur de syntaxe';
+$lang['porg_issue_891'] = 'Les liens vers piwigo.org doivent être en HTTPS';
+$lang['porg_issue_890'] = 'Contenu mixte HTTP/HTTPS sur la page [Admin > Photos > Ajouter > Applications]';
+$lang['porg_issue_889'] = 'Mise à jour de Smarty pour PHP 7.2';
+$lang['porg_issue_888'] = 'Mise à jour de Cssmin pour PHP 7.2';
+$lang['porg_issue_887'] = 'PHP 7.2, avertissement sur la gestion des utilisateurs';
+$lang['porg_issue_877'] = 'conflit potentiel sur le champ website_url avec une table utilisateurs tierce';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 casse le plugin Colored Tags';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_844'] = 'Lien manquant dans le fil d\'Ariane pour les albums récents';
+$lang['porg_issue_834'] = 'compatibilité PHP 7.2';
+$lang['porg_issue_748'] = 'Liste utilisateurs inaccessible';
+$lang['porg_issue_596'] = 'PHP 7.1 et template.class.php incompatibles';
+$lang['porg_issue_258'] = '[albums] gestion par lot et filtre sur album supprimé';
+?>
