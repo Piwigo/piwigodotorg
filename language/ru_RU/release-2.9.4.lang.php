e07ce04b1884f2d8d5a119c21f8ef49679c95f20
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-19 14:03:19 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-2.9.4.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_258'] = '[albums] Batch-managing a since-deleted album gives SQL error';
+$lang['porg_issue_748'] = 'Unable to view users - 403 on user_list_backend.php';
+$lang['porg_issue_844'] = 'Missing link tag in /recent_cats breadcrumb';
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 breaks Colored Tags plugin';
+$lang['porg_issue_866'] = 'Include pwg_token in user list POST request (Fixes #748)';
+$lang['porg_issue_877'] = 'conflict on website_url column with external user table';
+$lang['porg_issue_887'] = 'PHP 7.2, warning on user manager';
+$lang['porg_issue_890'] = 'http/https mixed content on [Admin > Photos > Add > Applications]';
+$lang['porg_issue_891'] = 'link to piwigo.org should now be in HTTPS';
+$lang['porg_issue_897'] = '[2.9.4] unexpected T_FUNCTION in functions_mysqli.inc.php on line 403';
+$lang['porg_issue_596'] = 'PHP 7.1 and template.class.php incompatability';
+$lang['porg_issue_834'] = 'Initial work to support PHP 7.2';
+$lang['porg_issue_894'] = 'Fix syntax error';
+$lang['porg_issue_888'] = 'Fix for Cssmin с PHP7.2 и выше';
+$lang['porg_issue_889'] = 'Fix for Smarty and PHP7.2';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_846'] = 'Fix issue #844';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
