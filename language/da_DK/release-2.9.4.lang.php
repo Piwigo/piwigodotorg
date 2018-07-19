00ade48ee20607d75eb6760d62215d28a28ca005
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-19 14:03:28 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.9.4.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_258'] = '[albums] Batchhåndtering af et slettet album giver SQL-fejl';
+$lang['porg_issue_596'] = 'Inkompatibilitet mellem PHP 7.1 og template.class.php';
+$lang['porg_issue_748'] = 'Ikke muligt at se brugere - fejl 403 i user_list_backend.php';
+$lang['porg_issue_834'] = 'Indledende arbejde i understøttelse afPHP 7.2';
+$lang['porg_issue_844'] = 'Manglende linktag i /recent_cats breadcrumb';
+$lang['porg_issue_846'] = 'Retter fejl #844';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_853'] = 'Plugin\'en Colored Tags virker ikke med Piwigo 2.9.3';
+$lang['porg_issue_866'] = 'Medtager pwg_token i POST-forespørgsel af brugerliste (retter #748)';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_877'] = 'Konflikt i website_url-kolonne med ekstern brugertabel';
+$lang['porg_issue_887'] = 'PHP 7.2, advarsel i brugerhåndtering';
+$lang['porg_issue_888'] = 'Rettelse af Cssmin med PHP 7.2 og højere';
+$lang['porg_issue_889'] = 'Rettelse til Smarty og PHP 7.2';
+$lang['porg_issue_890'] = 'Blandet http-/https-indhold på [Admin > Fotografier > Tilføj > Applikationer]';
+$lang['porg_issue_891'] = 'Link til piwigo.org skulle nu være med HTTPS';
+$lang['porg_issue_894'] = 'Retter syntaksfejl';
+$lang['porg_issue_897'] = '[2.9.4] uventet T_FUNCTION i functions_mysqli.inc.php på linje 403';
