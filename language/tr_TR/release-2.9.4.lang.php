17a387c78e083cb904bc747cb044439ff72e2709
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-19 14:03:10 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-2.9.4.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_866'] = 'Pwg_token kullanıcı listesine POST isteğini dahil etme (Düzeltmeler #748)';
+$lang['porg_issue_258'] = '[albums] Silinen albümde toplu yönetimde SQL hatası veriyor';
+$lang['porg_issue_897'] = '[2.9.4] functions_mysqli.inc.php satır 403 \'te beklenmeyen T_FUNCTION';
+$lang['porg_issue_890'] = 'http/https karışık içerik [Admin > Photos > Add > Applications]';
+$lang['porg_issue_891'] = 'piwigo.org bağlantıları artık HTTPS olacak';
+$lang['porg_issue_877'] = 'website_url kolonu ile kullanıcı tablosu arasında çakışma';
+$lang['porg_issue_844'] = '/recent_cats breadcrumb içerisinde eksik bağlantı etiketi';
+$lang['porg_issue_888'] = 'php7.2 ve üzeri sürümler için Cssmin düzeltmesi';
+$lang['porg_issue_887'] = 'PHP 7.2, kullanıcı yönetiminde uyarı';
+$lang['porg_issue_889'] = 'Smarty ve PHP7.2 için düzeltmeler';
+$lang['porg_issue_894'] = 'Söz dizimi hatası düzeltmesi';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 \'Colored Tags\' eklantisini kırıyor';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_846'] = '#844 numaralı sorunun çözümü';
+$lang['porg_issue_834'] = 'PHP 7.2 desteği için başlangıç çalışmaları';
+$lang['porg_issue_748'] = 'user_list_backend.php üzerinde \'kullanıcılar görüntülenemiyor - 403\'';
+$lang['porg_issue_596'] = 'PHP 7.1 ve template.class.php uyumsuzluğu';
