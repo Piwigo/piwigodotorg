fe16e52ffecdbf5a195687e35958553ae445a357
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-19 21:11:59 +0200

    Update nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/release-2.9.4.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_834'] = 'Eerste werk ter ondersteuning van PHP 7.2';
+$lang['porg_issue_866'] = 'Neem Pwg_token op in gebruikerslijst POST-aanvraag (Fixes # 748)';
+$lang['porg_issue_877'] = 'conflict op de website_url kolom met externe gebruikerstabel';
+$lang['porg_issue_887'] = 'PHP7.2, waarschuwing voor gebruikersbeheerder';
+$lang['porg_issue_894'] = 'Los syntax-fout op';
+$lang['porg_issue_888'] = 'Oplossing voor Cssmin met php7.2 en hoger';
+$lang['porg_issue_889'] = 'Oplossing voor Smarty en PHP7.2';
+$lang['porg_issue_890'] = 'http/https gemengde inhoud bij [Beheerder> Foto\'s> Toevoegen> Toepassingen]';
+$lang['porg_issue_846'] = 'Oplossing kweste #844';
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 heeft problemen met de plugin Colored Tags';
+$lang['porg_issue_258'] = '[albums] Batch-beheer van een sinds-verwijderd album geeft SQL-fout';
+$lang['porg_issue_596'] = 'PHP 7.1 en template.class.php incompatibiliteit';
+$lang['porg_issue_748'] = 'Kan gebruikers niet bekijken - 403-fout in user_list_backend.php';
+$lang['porg_issue_844'] = 'Ontbrekende link-tag in /recent_cats breadcrumb';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_891'] = 'link naar piwigo.org zou nu in HTTPS moeten staan';
+$lang['porg_issue_897'] = '[2.9.4] onverwachte T_FUNCTION in functions_mysqli.inc.php in regel 403';
