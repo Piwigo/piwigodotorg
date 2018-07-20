3056935717e2013b8c3c7b427c9421d941b307a0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-20 10:18:39 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/release-2.9.4.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_889'] = 'Poprawka dla Smarty i PHP7.2';
+$lang['porg_issue_891'] = 'Odnośnik do piwigo.org powinien teraz być jako HTTPS';
+$lang['porg_issue_894'] = 'Poprawka błędu składni';
+$lang['porg_issue_897'] = '[2.9.4] Niespodziewana T_FUNCTION w functions_mysqli.inc.php w linii 403';
+$lang['porg_issue_596'] = 'Niekompatybilności z PHP 7.1 i template.class.php';
+$lang['porg_issue_748'] = 'Nie można pokazać użytkowników - błąd 403 w user_list_backend.php';
+$lang['porg_issue_834'] = 'Wstępne prace do wsparcia PHP 7.1';
