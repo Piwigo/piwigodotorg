91020a70d6c9d5588c05b8f711a80422ade8300f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-21 07:47:41 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/release-2.9.4.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_889'] = '针对 Smarty 与 PHP7.2 的修正';
+$lang['porg_issue_890'] = '[管理面板 > 图片 > 添加 > 应用程序]中的 http/https 混合内容';
+$lang['porg_issue_897'] = '[2.9.4] functions_mysqli.inc.php 页 line 403 中的 意外 T_FUNCTION';
+$lang['porg_issue_888'] = '修正 php7.2 及以上的 Cssmin';
+$lang['porg_issue_887'] = 'PHP 7.2，在用户管理中有警告提醒';
+$lang['porg_issue_844'] = '/recent_cats breadcrumb 中有缺失的链接标签';
+$lang['porg_issue_866'] = '在用户列表 POST request 中加入 pwg_token (修正 #748)';
+$lang['porg_issue_748'] = '无法查看用户 - 403 on user_list_backend.php';
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 破坏 Colored Tags 插件';
+$lang['porg_issue_877'] = 'website_url 列与外部用户表（external user table）的冲突';
+$lang['porg_issue_891'] = '链接到 piwigo.org 现须用 HTTPS';
+$lang['porg_issue_596'] = 'PHP 7.1 与 template.class.php 不兼容';
+$lang['porg_issue_834'] = '支持 PHP 7.2 的前期工作';
+$lang['porg_issue_846'] = '修正问题 #844';
+$lang['porg_issue_258'] = '[albums] Batch-managing a since-deleted album gives SQL error';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_894'] = '修复语法错误';
