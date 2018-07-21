604bf2a73e4e64e25eda0e93cd85bb4b581b6c22
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-21 07:47:32 +0200

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.9.4.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 funktioniert nicht mit Colored Tags Plugins';
+$lang['porg_issue_877'] = 'Konflikt zwischen website_url Spalte und externer Benutzer-Tabelle';
+$lang['porg_issue_258'] = '[Alben] Batchverwaltung eines inzwischen gelöschten Albums erzeugt SQL-Fehler';
+$lang['porg_issue_748'] = 'Benutzer nicht sichtbar - 403 in user_list_backend.php';
+$lang['porg_issue_834'] = 'Unterstützung für PHP 7.2 begonnen';
+$lang['porg_issue_866'] = 'pwg_token in Benutzerliste POST Anfrage eingefügt (fixt #748)';
+$lang['porg_issue_844'] = 'Fehlender Link-Tag in /recent_cats Breadcrumb';
+$lang['porg_issue_888'] = 'Fix für Cssmin unter PHP 7.2 und höher';
+$lang['porg_issue_889'] = 'Fix für Smarty und PHP 7.2';
+$lang['porg_issue_890'] = 'HTTP/HTTPS mixed content in [Admin > Fotos > Hinzufügen > Applikationen]';
+$lang['porg_issue_897'] = '[2.9.4] unexpected T_FUNCTION in functions_mysqli.inc.php in Zeile 403';
+$lang['porg_issue_846'] = 'Issue #844 behoben';
+$lang['porg_issue_887'] = 'PHP 7.2 Warnung im Usermanager';
+$lang['porg_issue_891'] = 'Link zu piwigo.org sollte jetzt HTTPS sein';
+$lang['porg_issue_894'] = 'Syntaxfehler behoben';
+$lang['porg_issue_596'] = 'Inkompatibilität zwischen template.class.php und PHP 7.1';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
