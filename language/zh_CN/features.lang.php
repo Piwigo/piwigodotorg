1061c665c18ca802d4adee0becbc4400530de0af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-10-08 09:25:50 +0200

    Update zh_CN, thanks to dennisyan,

---
+++ language/zh_CN/features.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_batch_manager_desc'] = '过滤图库中的图片，批量进行选择并执行操作：修改作者、添加一些标签、关联到一个新的相册、设置地理位置……';
+$lang['porg_features_api_desc1'] = 'Piwigo web API使开发人员可以很容易地从其他应用程序执行操作：在WordPress博客文章中显示缩略图、从Lightroom发布照片、用Perl脚本同步本地文件夹……';
+$lang['porg_features_browse_date_desc'] = '数码相机会将日期存储在照片中，Piwigo 会利用日期信息在日历上显示您的照片集，让您可以按日期浏览。';
+$lang['porg_features_desc3'] = '基于编码团队的想法与用户社区的讨论，Piwigo 得以不断地添加新的特性。';
+$lang['porg_features_metadata_desc'] = '例如，IPTC描述可以导入到照片属性中，显示在照片旁边…… 而且还能被搜索到！';
+$lang['porg_features_geolocate_desc2'] = '然后，依靠 Google Maps 或 OpenStreetMap，Piwigo 可以在一张互动地图上显示您拍的照片。';
+$lang['porg_features_great_place_desc'] = '您的访客可以发表评论、打分、标记喜欢的照片、进行搜索并通过电子邮件获得通知。';
+$lang['porg_features_easy_install_desc1'] = 'Piwigo 安装极为简单，对系统的要求极少。';
+$lang['porg_features_organize_desc1'] = '在你自己的相册中发布你的照片。';
+$lang['porg_features_desc1'] = 'Piwigo 为任何图库提供丰富的功能。';
+$lang['porg_features_desc2'] = '许多特色功能罗列如下。';
+$lang['porg_features_easy_install_desc2'] = '升级更新一键搞定！';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo 能够从照片的元数据中读取GPS经纬度。';
+$lang['porg_features_organize_desc2'] = '相册层级没有限制。';
+$lang['porg_features_organize_desc3'] = '一张图片可归属到多个相册。';
+$lang['porg_features_plugins_desc1'] = '选用主题改变图库与相册的外观。';
+$lang['porg_features_plugins_desc2'] = '利用插件增加更多的功能。';
+$lang['porg_features_plugins_desc3'] = '扩展只需点击几次鼠标即可完成安装。';
+$lang['porg_features_plugins_desc4'] = '<span class="bold">现有 350 个扩展</span> 可用，并且仍在增加！';
+$lang['porg_features_api_desc2'] = '就像HTTP请求和JSON响应一样简单。';
+$lang['porg_features_user_management_desc1'] = '管理员可以创建用户、组，管理权限并发送通知。';
+$lang['porg_features_user_management_desc2'] = '数位管理员能够同时管理 Piwigo。';
+$lang['porg_features_user_management_desc3'] = '非常适合您的团队！';
+$lang['porg_features_privacy_desc1'] = '给照片添加私密属性并决定谁可以看到它们。';
+$lang['porg_features_privacy_desc2'] = '可以按用户组或单个用户设定相册与图片的访问权限。';
+$lang['porg_features_slideshow_desc'] = '使用 Fotorama 插件，或特定的主题（如 Bootstrap Darkroom）,您可以实现全屏幻灯片浏览。';
+$lang['porg_features_upload_desc'] = '您可以通过web表单添加图片，任何FTP客户端或<br>digiKam, Shotwell, Lightroom 等桌面应用或<br>移动端app。';
+$lang['A great place for your visitors'] = '是您的访问者的好地方';
+$lang['Batch Manager'] = '批量管理';
+$lang['Browse by Dates'] = '按日期浏览';
+$lang['Easy installation and updates'] = '安装及更新简单';
+$lang['Full screen slideshow'] = '全屏幻灯片浏览';
+$lang['Geolocate your photos on a map'] = '在地图上定位照片拍摄地点';
+$lang['Import automatic your metadata'] = '自动导入元数据';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = '许多设置让 Piwigo 满足您的个性需求';
+$lang['Organize with Album'] = '相册管理';
+$lang['Privacy on your Photos'] = '照片的权限';
+$lang['Upload your photos'] = '更新照片';
+$lang['Users management'] = '用户管理';
+$lang['API'] = 'API';
