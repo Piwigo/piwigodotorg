f62bc4e5b22681034034f213078a2d23255a30a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-10-27 10:20:30 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-2.9.4.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_866'] = 'Include pwg_token nella lista degli utenti POST request (Risoluzione #748)';
+$lang['porg_issue_897'] = '[2.9.4] T_FUNCTION imprevisto in functions_mysqli.inc.php sulla riga 403';
+$lang['porg_issue_902'] = '[2.9.4] users list no longer paginated or filtered';
+$lang['porg_issue_846'] = 'Risolve problema #844';
+$lang['porg_issue_877'] = 'Potenziale conflitto nel campo website_url con una tabella utente di terze parti';
+$lang['porg_issue_887'] = 'PHP 7.2, avviso sulla gestione degli utenti';
+$lang['porg_issue_888'] = 'Aggiornamento di Cssmin per PHP 7.2';
+$lang['porg_issue_889'] = 'Aggiornamento di Smarty per PHP 7.2';
+$lang['porg_issue_890'] = 'Contenuti misti HTTP / HTTPS nella pagina [Admin > Foto > Aggiungi > Applicazioni]';
+$lang['porg_issue_891'] = 'I collegamenti a piwigo.org devono essere in HTTPS';
+$lang['porg_issue_894'] = 'Correzione errore di sintassi';
+$lang['porg_issue_853'] = 'Piwigo 2.9.3 rompe il plugin Colored Tags';
+$lang['porg_issue_872'] = 'CVE-2018-7724';
+$lang['porg_issue_844'] = 'Link mancante nel breadcrumb per gli album recenti';
+$lang['porg_issue_847'] = 'CVE-2018-5692';
+$lang['porg_issue_834'] = 'Compatibilità PHP 7.2';
+$lang['porg_issue_748'] = 'Elenco degli utenti inaccessibile';
+$lang['porg_issue_258'] = '[album] gestione batch e filtro su album cancellato';
+$lang['porg_issue_596'] = 'PHP 7.1 e template.class.php incompatibile';
