827f37275700b2483984ee8f36d661e2d3d20bbb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-10-27 10:20:41 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/features.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_features_user_management_desc2'] = 'Diversi amministratori possono lavorare contemporaneamente nello stesso Piwigo.';
+$lang['porg_features_user_management_desc3'] = 'Perfetto per la tua squadra!';
+$lang['porg_features_user_management_desc1'] = 'Gli amministratori possono creare utenti, gruppi, gestire le autorizzazioni e inviare notifiche.';
+$lang['porg_features_upload_desc'] = 'È possibile trasferire le foto con il modulo Web, un client FTP,<br> un\'applicazione remota come digiKam, Shotwell, Lightroom o un\'applicazione mobile.';
+$lang['porg_features_slideshow_desc'] = 'Con il plug-in Fotorama o alcuni temi come Bootstrap Darkroom, sperimenta la presentazione a schermo intero.';
+$lang['porg_features_privacy_desc2'] = 'Puoi impostare permessi sull\' album o la foto, per utente o un gruppo.';
+$lang['porg_features_privacy_desc1'] = 'Rendi le tue foto private e decidi chi può vederle.';
+$lang['porg_features_plugins_desc4'] = '<strong>350 estensioni</strong> disponibili, all\'infinito e oltre!';
+$lang['porg_features_plugins_desc3'] = 'Le estensioni s\'installano in pochi clic.';
+$lang['porg_features_plugins_desc2'] = 'Aggiungi funzionalità con plug-in.';
+$lang['porg_features_organize_desc3'] = 'Ogni foto può essere associata a più album.';
+$lang['porg_features_plugins_desc1'] = 'Cambia aspetto con i temi.';
+$lang['porg_features_organize_desc2'] = 'L\'albero degli album ha una profondità illimitata.';
+$lang['porg_features_organize_desc1'] = 'Distribuisci le tue foto tra i tuoi album.';
+$lang['porg_features_geolocate_desc2'] = 'Quindi, grazie al plug-in Google Maps o OpenStreetMap, Piwigo può visualizzare le tue foto su una mappa interattiva.';
+$lang['porg_features_easy_install_desc1'] = 'Abbiamo progettato Piwigo per essere semplicissimo da installare, con un minimo di prerequisiti.';
+$lang['porg_features_easy_install_desc2'] = 'Allo stesso modo per gli aggiornamenti, basta un clic!';
+$lang['porg_features_geolocate_desc1'] = 'Piwigo può leggere le coordinate GPS latitudine e longitudine dai metadati.';
+$lang['porg_features_great_place_desc'] = 'I visitatori possono lasciare commenti, votare foto, avviare ricerche e ricevere notifiche via e-mail.';
+$lang['porg_features_metadata_desc'] = 'Ad esempio una descrizione IPTC può essere importata nelle proprietà della foto, visualizzata accanto alla foto ... e può essere ricercata!';
+$lang['porg_features_browse_date_desc'] = 'Le fotocamere digitali registrano la data in ogni foto e Piwigo usa quella data per visualizzare la tua collezione come un calendario, navigabile giorno per giorno.';
+$lang['porg_features_desc1'] = 'Piwigo offre una vasta gamma di funzionalità per qualsiasi galleria fotografica.';
+$lang['porg_features_desc2'] = 'Molte di queste sono descritte in questa pagina.';
+$lang['porg_features_desc3'] = 'Nuove funzionalità sono aggiunte nel tempo, a seconda delle nostre idee e discussioni con gli utenti.';
+$lang['porg_features_batch_manager_desc'] = 'Filtra le tue foto direttamente dalla collezione di foto, fai una selezione e applica un\'azione à tutto il lotto : cambia l\'autore, aggiungi dei tag, associa a un nuovo album, imposta le coordinate geografiche ...';
+$lang['Organize with Album'] = 'Organizza per album';
+$lang['Privacy on your Photos'] = 'Gestisci le autorizzazioni';
+$lang['Upload your photos'] = 'Carica le tue foto';
+$lang['Users management'] = 'Gestione degli utenti';
+$lang['porg_features_api_desc1'] = 'L\'API web di Piwigo consente agli sviluppatori di controllare Piwigo da un\'altra applicazione: visualizzare le miniature in un post WordPress, pubblicare da Lightroom, sincronizzare una directory con uno script Perl ...';
+$lang['porg_features_api_desc2'] = 'È semplice come inviare richieste HTTP e ricevere risposte JSON :-)';
+$lang['Many configuration settings to make Piwigo fit your own needs'] = 'Molte opzioni in modo che Piwigo soddisfi le tue esigenze';
+$lang['Import automatic your metadata'] = 'Importazione automatica dei metadati';
+$lang['Geolocate your photos on a map'] = 'Geolocalizza le tue foto su una mappa';
+$lang['Full screen slideshow'] = 'Presentazione a schermo intero';
+$lang['Browse by Dates'] = 'Esplorare per data';
+$lang['Easy installation and updates'] = 'Installazione e aggiornamenti semplificati';
+$lang['Batch Manager'] = 'Gestione batch';
+$lang['API'] = 'API';
+$lang['A great place for your visitors'] = 'Strumenti per sedurre i tuoi utenti';
