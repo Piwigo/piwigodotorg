1ae1fad149816b57491d6742c3773a560b97bd8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-10-29 07:45:18 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/get_started.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_started_requirement_desc2'] = 'PHP, database, web server, elaborazione delle immagini ... Vedi dettagli.';
+$lang['porg_get_started_question_desc1'] = 'Puoi ottenere assistenza nel <a href="%s">forum</a> o trovare delle risposte nella <a href="%s">documentazione</a>.';
+$lang['porg_get_started_requirement_desc1'] = 'Quali sono i prerequisiti tecnici per l\'esecuzione di Piwigo sul proprio hosting?';
+$lang['porg_get_started_desc2'] = 'Cosa bisognerà preparare, scaricare, installare.';
+$lang['porg_get_started_download_desc'] = 'Cosa bisognerà preparare, scaricare, installare.';
+$lang['porg_get_started_guide_desc'] = 'Scopri come installare Piwigo: dal download alla prima connessione della libreria fotografica alle impostazioni del database.';
+$lang['Still got some unanswered questions ?'] = 'Domande ancora in sospeso?';
+$lang['porg_get_started_desc1'] = 'Come fare funzionare il tuo Piwigo sul tuo hosting.';
+$lang['Requirements'] = 'Prerequisiti';
+$lang['Installation guide'] = 'Guida all\'installazione';
