a4ec1eb19e8c65e7e3c21aef7a7529df71526518
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-10-29 07:46:09 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'Il md5sum è uno strumento utile per verificare che un file non sia corrotto.';
+$lang['Updated languages'] = 'Lingue aggiornate';
+$lang['Upgrade'] = 'Aggiornamento';
+$lang['We recommend the <b>automatic upgrade</b>.'] = 'Raccomandiamo l\'<b>aggiornamento automatico</b>.';
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = 'Puoi confrontare il md5sum del file scaricato con il md5sum ufficiale fornito qui su piwigo.org.';
+$lang['Automatic upgrade'] = 'Aggiornamento automatico';
+$lang['Bugs fixed'] = 'Bug corretti';
+$lang['Download Piwigo %s'] = 'Scarica Piwigo %s';
+$lang['Featured added'] = 'Funzionalità aggiunte';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = 'Se stai utilizzando una versione precedente a %s e non vuoi approfittare dell\'aggiornamento automatico, segui l\'<b>aggiornamento manuale</b>.';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = 'Se stai utilizzando <b>Piwigo %s</b>, puoi scaricare l\'archivio <b>%s_to_%s.zip</b> che contiene solo file nuovi o modificati.';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = 'Se usi la <b>versione 2.2+</b>, Piwigo ti dirà quali plugin non sono ancora compatibili con la versione %s.';
+$lang['Known issues'] = 'Problemi noti';
+$lang['Languages'] = 'lingue';
+$lang['Manual Upgrade'] = 'Aggiornamento manuale';
+$lang['New language:'] = 'Nuove lingue:';
+$lang['No database upgrade is required.'] = 'Il tuo database non ha bisogno di un aggiornamento.';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = 'Una volta estratti i file, caricali tramite FTP sul tuo sito sulla tua installazione %s.';
+$lang['Release note'] = 'Note della versione';
+$lang['Released on %s'] = 'Uscita il %s';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = 'L\'elenco delle principali modifiche è descritto nelle <a href="%s">note della versione %s</a>';
