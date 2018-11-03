1f0439c17e805414bcb59490dba8ed0ee13266a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-03 19:19:00 +0100

    Update de_DE, thanks to cryopad

---
+++ language/de_DE/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,75 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_privacy_mobile_storage_5'] = 'Sie können den Zwischenspeicher abschalten oder leeren, was jedoch Konsequenzen für die Verfügbarkeit und Funktionalität der Anwendungen haben kann.';
+$lang['porg_privacy_mobile_storage_4'] = 'Wir verwenden nur Mechanismen zur Zwischenspeicherung um die Anwendungen anpassen zu können und die Nutzung zu verbessern.';
+$lang['porg_privacy_mobile_storage_3'] = 'Wenn Sie die Anwendungen verwenden werde keine persönlichen Daten durch Tracking Technologie erfasst.';
+$lang['porg_privacy_mobile_storage_2'] = 'Es werden keine Daten erfasst, übertragen oder gespeichert die nicht für diese beabsichtigte Funktionalität erforderlich sind.';
+$lang['porg_privacy_mobile_storage_1'] = 'Die Anwendungen speichern nur Daten um ihre Bestimmung zu erfüllen: Zugriff auf eine Piwigo-Gallerie, welche auf einem entfernten Webserver betrieben wird zu ermöglichen.';
+$lang['porg_privacy_mobile_security_4'] = 'Daher können wir keine absolute Sicherheit für Ihre persönlichen Daten garantieren.';
+$lang['porg_privacy_mobile_security_3'] = 'Jegliche Information im Internet kann potentiell abfangen oder von Unbefugten missbraucht werden.';
+$lang['porg_privacy_mobile_security_2'] = 'Obwohl wir Maßnahmen um die persönlichen Daten zu schützen getroffen haben, weisen wir darauf hin, dass trotz unserer Bemühungen, keine Sicherheitsmaßnahme perfekt oder unüberwindbar ist und keine Methode der Datenübertragung absolute Sicherheit gegen Abhören oder sonstigen Missbrauch garantieren kann.';
+$lang['porg_privacy_mobile_security_1'] = 'Wir verwenden technische Sicherheitsmaßnahmen um Ihre persönlichen Daten zu schützen.';
+$lang['porg_privacy_mobile_protection_8'] = 'Es liegt in der Verantwortung des Benutzers zu entscheiden, ob diese privaten Daten übertragen werden.';
+$lang['porg_privacy_mobile_protection_7'] = 'Im den Einstellungen bieten die Anwendungen eine Option zum Entfernen dieser Daten aus Bilddateien vor dem Upload.';
+$lang['porg_privacy_mobile_protection_6'] = 'Beim Hochladen von Bildern auf eine Piwigo Gallerie können private Daten wie GPS-Koordinaten oder Kommentare enthalten sein.';
+$lang['porg_privacy_mobile_protection_3'] = 'Wir empfehlen dringend HTTPS zu verwenden um die Daten während der Übertragung vor unberechtigtem Zugriff zu schützen. Die Anwendungen versuchen eine verschlüsselte Verbindung über HTTPS zu verwenden, wenn der Benutzer zum ersten Mal eine Verbindung zu einer Piwigo-Gallerie herstellt.';
+$lang['porg_privacy_mobile_protection_2'] = 'Sie sind dafür verantwortlich die gespeicherten Daten vor unbefugtem Zugriff zu schützen.';
+$lang['porg_privacy_mobile_protection_5'] = 'Falls die Anwendungen HTTPS nicht verwenden können schalten sie auf HTTP um und die Daten sind bei der Übertragung nicht verschlüsselt.';
+$lang['porg_privacy_mobile_protection_4'] = 'Für die Aktualisierung von Zertifikaten und Bibliotheken für den HTTPS-Zugriff ist der Betreiber des Piwigo Servers verantwortlich.';
+$lang['porg_privacy_mobile_protection_1'] = 'Die Anwendungen speichern Daten lokal auf dem Gerät auf dem sie ausgeführt werden.';
+$lang['porg_privacy_mobile_intro_9'] = 'Wir behalten uns vor, diese Datenschutzerklärung jederzeit und aus beliebigem Grund zu ändern.';
+$lang['porg_privacy_mobile_intro_8'] = 'Wenn Sie mit den Bedingungen dieser Datenschutzerklärung nicht einverstanden sind, verwenden Sie die Anwendungen nicht.';
+$lang['porg_privacy_mobile_intro_7'] = 'Bitte lesen Sie diese Datenschutzrichtlinie sorgfältig.';
+$lang['porg_privacy_mobile_intro_6'] = 'Unsere Namen können aus den Metadaten in den Git-Repositories und in den Abschnitten über Mitwirkende in den Apps entnommen werden.';
+$lang['porg_privacy_mobile_intro_5'] = 'Diese Datenschutzerklärung erläutert wie wir Ihre Daten erheben, verarbeiten, übertragen und schützen wenn Sie unsere mobilen Anwendungen ("Apps") verwenden.';
+$lang['porg_privacy_mobile_intro_4'] = 'Die Entwickler von Piwigo-Android und Piwigo-Mobile ("wir", "uns") respektieren die Privatsphäre unserer Nutzer ("Nutzer", "Sie").';
+$lang['porg_privacy_mobile_intro_3'] = 'Konsultieren Sie die Datenschutzerklärung der entsprechenden Webseiten, bevor Sie ein Konto in Piwigo-Android oder Piwigo-Mobile konfigurieren um die App mit einer Gallerie zu verbinden.';
+$lang['porg_privacy_mobile_intro_2'] = 'Diese Datenschutzerklärung gilt für diese nativen Android- und iOS-Anwendungen, welche eine Verbindung zu Piwigo-Gallerie-Seiten aufbauen, die außerhalb der Kontrolle der Autoren sind.';
+$lang['porg_privacy_mobile_intro_14'] = 'Wir sind nicht verantwortlich für irgendwelche Daten, die von solchen Dritten gesammelt werden.';
+$lang['porg_privacy_mobile_intro_13'] = 'Diese Datenschutzrichtlinie gilt nicht für den Online- / mobilen Store eines Drittanbieters, von dem aus Sie die Anwendungen installieren oder Zahlungen ausführen, einschließlich virtueller In-App-Elemente, die möglicherweise auch Daten über Sie erfassen und verwenden.';
+$lang['porg_privacy_mobile_intro_12'] = 'Indem Sie die Apps nach der Veröffentlichung einer Überarbeitung weiterhin nutzen bestätigen Sie die Aktualisierung zur Kenntnis genommen zu haben und akzeptieren die überarbeitete Datenschutzerklärung.';
+$lang['porg_privacy_mobile_intro_11'] = 'Wir empfehlen Ihnen diese Datenschutzerklärung regelmäßig zu überprüfen um über Neuerungen informiert zu bleiben.';
+$lang['porg_privacy_mobile_intro_10'] = 'Wir benachrichtigt Sie über Änderungen in dieser Datenschutzerklärung indem wir das letzte Aktualisierungsdatum ändern.';
+$lang['porg_privacy_mobile_intro_1'] = '<a href="%s" target="_blank">Piwigo-Android</a> und <a href="%s" target="_blank">Piwigo-Mobile</a> sind kostenlose, freie, Open Source Programme (Apps) um auf eine Piwigo-Gallerie zuzugreifen.';
+$lang['porg_privacy_mobile_data_piwigo_2'] = 'Darüber hinaus werden alle Fotos, Videos und sonstige Dateien, die zum Download/Upload ausgewählt werden und die eingegebenen Inhalte gespeichert, verarbeitet und an die konfigurierten Galerie-Websites übertragen.';
+$lang['porg_privacy_mobile_data_piwigo_1'] = 'Kontoanmeldeinformationen werden von den Anwendungen gespeichert und an die konfigurierten Websites zur Anmeldung bei einer oder mehreren Piwigo Galerien übertragen.';
+$lang['porg_privacy_mobile_data_geo_2'] = 'Wenn Sie den Zugriff oder die Berechtigungen ändern möchten, können Sie in Ihren Geräteeinstellungen tun.';
+$lang['porg_privacy_mobile_data_geo_1'] = 'Wir erbitten Zugriff oder die Erlaubnis auf Standort-Informationen Ihres mobilen Gerätes zuzugreifen und diese zu verfolgen, entweder kontinuierlich oder während der Verwendung der Anwendungen, um standortbasierte Dienste und Funktionen bereitzustellen.';
+$lang['porg_privacy_mobile_data_device_4'] = 'Neben den Daten, die der Benutzer explizit an die Entwickler überträgt werden keine Inhalte gespeichert, übertragen oder sonst von uns verarbeitet.';
+$lang['porg_privacy_mobile_data_device_3'] = 'Bei der Kontaktaufnahme mit dem Support-Team, können Geräteinformationen wie Ihre Geräte-ID, Modell und Hersteller, Version Ihres Betriebssystems, E-Mail-Adresse, Land, Ort und andere Daten, die Sie bereitstellen möchten an das Entwicklungsteam unserer Anwendunge nübermittelt werden.';
+$lang['porg_privacy_mobile_data_device_2'] = 'Falls Sie unseren Zugriff oder die Berechtigungen verändern möchten so können Sie dies in den Geräteeinstellungen tun, bedenken Sie jedoch, dass einige Berechtigungen nötig sind um Medien Ihres Gerätes in eine Piwigo-Gallerie hochzuladen oder um Medien aus einer Piwigo-Gallerie auf Ihrem Gerät zu speichern.';
+$lang['porg_privacy_mobile_data_device_1'] = 'Die Anwendungen können Zugriff auf verschiedene Funktionen Ihres Gerätes erbitten. Dies umfasst unter anderem die Fotogallerie und Dateiablage ihres Gerätes, die Android Kontoverwaltung, den Apple Schlüsselbund, entfernte und "Cloud"-basierte Speicher, Standortdienste und weitere Funktionen.';
+$lang['porg_privacy_mobile_contact_1'] = 'Bei Fragen oder Anmerkungen zu dieser Datenschutzerklärung kontaktieren Sie uns bitte über:';
+$lang['porg_privacy_mobile_banner'] = 'Hier finden Sie die Datenschutzbestimmungen für Piwigo-Mobilanwendungen, sowohl für iOS (z. B. iPhone, iPod oder iPad) als auch für Android (im Wesentlichen alle anderen Smartphones).';
+$lang['or'] = 'oder';
+$lang['Why does the mobile app store data?'] = 'Warum speichert die mobile Anwendung Daten?';
+$lang['What data is processed and stored?'] = 'Welche Daten werden verarbeitet und gespeichert?';
+$lang['Security of your information'] = 'Sicherheit Ihrer Daten';
+$lang['Privacy Policy for Mobile Apps'] = 'Datenschutzerklärung für Android- und iOS-Anwendungen';
+$lang['Piwigo Gallery Data'] = 'Piwigo Galleriedaten';
+$lang['Mobile Device Data'] = 'Geräteinformationen';
+$lang['Mobile Device Access'] = 'Zugriff auf Ihr Mobilgerät';
+$lang['Last updated on %s'] = 'Letzte Aktualisierung: %s';
+$lang['How is the data protected?'] = 'Wie sind Ihre Daten geschützt?';
+$lang['Geo-Location Information'] = 'Standortinformationen';
+$lang['Contact us'] = 'Kontakt';
