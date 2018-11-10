b44cd8bb30256e14ebbe7ffb1a28ded08682c858
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-10 08:44:57 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_privacy_mobile_intro_7'] = 'Пожалуйста, внимательно ознакомьтесь с Политикой конфиденциальности.';
+$lang['porg_privacy_mobile_intro_8'] = 'Если Вы не согласны с положениями этой Политики конфиденциальности, пожалуйста, воздержитесь от использования приложения.';
+$lang['Contact us'] = 'Наши контакты';
+$lang['Geo-Location Information'] = 'Информация о геолокации';
+$lang['How is the data protected?'] = 'Какие данные защищены?';
+$lang['Last updated on %s'] = 'Последнее обновление %s';
+$lang['Mobile Device Access'] = 'Доступ к мобильному устройству';
+$lang['Mobile Device Data'] = 'Данные мобильного устройства';
+$lang['Piwigo Gallery Data'] = 'Данные галереи Piwigo';
+$lang['Privacy Policy for Mobile Apps'] = 'Политика конфиденциальности для мобильных приложений';
+$lang['Security of your information'] = 'Защита Вашей информации';
+$lang['What data is processed and stored?'] = 'Какие данные обрабатываются и хранятся?';
+$lang['Why does the mobile app store data?'] = 'Зачем приложения хранят данные?';
+$lang['or'] = 'или ';
+$lang['porg_privacy_mobile_contact_1'] = 'Если у Вас есть вопросы или комментарии относительно этой Политики конфиденциальности, пожалуйста, свяжитесь с нами: ';
