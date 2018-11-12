fcd2affc4d2d0a11644b32d773abec4588806e1f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-12 21:27:54 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Contact us'] = 'Contáctanos';
+$lang['Geo-Location Information'] = 'Información de geolocalización';
+$lang['How is the data protected?'] = '¿Cómo se protegen los datos?';
+$lang['Last updated on %s'] = 'Última actualización en %s';
+$lang['Mobile Device Access'] = 'Acceso desde dispositivo móvil';
+$lang['Mobile Device Data'] = 'Datos del dispositivo móvil';
+$lang['Piwigo Gallery Data'] = 'Datos de la galería de Piwigo';
