2c870c05a572e8a708dfa45ced96f3356b911959
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-12-19 19:04:27 +0100

    Add id_ID, thanks to cryopad

---
+++ language/id_ID/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,75 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_privacy_mobile_banner'] = 'Di sini mengikuti kebijakan privasi untuk aplikasi seluler Piwigo, pada iOS (yaitu iPhone, iPod atau iPad) dan Android (pada dasarnya semua smartphone lainnya).';
+$lang['porg_privacy_mobile_contact_1'] = 'Jika Anda memiliki pertanyaan atau komentar tentang kebijakan privasi ini, silakan hubungi kami:';
+$lang['porg_privacy_mobile_storage_5'] = 'Anda dapat menonaktifkan atau menghapus cache, tetapi ketahuilah bahwa tindakan tersebut dapat memengaruhi ketersediaan dan fungsionalitas Aplikasi.';
+$lang['porg_privacy_mobile_storage_4'] = 'Kami hanya menggunakan mekanisme cache untuk membantu menyesuaikan Aplikasi dan meningkatkan pengalaman Anda.';
+$lang['porg_privacy_mobile_storage_3'] = 'Saat Anda mengakses Aplikasi, informasi pribadi Anda tidak dikumpulkan melalui penggunaan teknologi pelacakan.';
+$lang['porg_privacy_mobile_storage_2'] = 'Tidak ada data yang dikumpulkan, dikirim atau disimpan yang tidak diperlukan untuk fungsi yang dimaksudkan.';
+$lang['porg_privacy_mobile_storage_1'] = 'Aplikasi hanya menyimpan data untuk memenuhi satu tujuan: mengakses galeri Piwigo yang dihosting di server web jarak jauh.';
+$lang['porg_privacy_mobile_security_4'] = 'Oleh karena itu, kami tidak dapat menjamin keamanan lengkap jika Anda memberikan informasi pribadi.';
+$lang['porg_privacy_mobile_security_3'] = 'Setiap informasi yang diungkapkan secara online rentan terhadap intersepsi dan penyalahgunaan oleh pihak yang tidak berwenang.';
+$lang['porg_privacy_mobile_security_2'] = 'Meskipun kami telah mengambil langkah-langkah yang wajar untuk mengamankan informasi pribadi yang Anda berikan kepada kami, harap perhatikan bahwa terlepas dari upaya kami, tidak ada tindakan keamanan yang sempurna atau tidak dapat ditembus, dan tidak ada metode transmisi data yang dapat dijamin terhadap setiap intersepsi atau jenis penyalahgunaan lainnya.';
+$lang['porg_privacy_mobile_security_1'] = 'Kami menggunakan langkah-langkah keamanan teknis untuk membantu melindungi informasi pribadi Anda.';
+$lang['porg_privacy_mobile_protection_8'] = 'Ini adalah tanggung jawab pengguna untuk memutuskan apakah data pribadi tersebut akan diunggah.';
+$lang['porg_privacy_mobile_protection_7'] = 'Aplikasi menyediakan opsi untuk menghapus data ini dari file gambar sebelum diunggah di bagian Pengaturan.';
+$lang['porg_privacy_mobile_protection_6'] = 'Saat Anda mengunggah gambar ke galeri Piwigo, gambar mungkin berisi data pribadi seperti koordinat GPS atau komentar.';
+$lang['porg_privacy_mobile_protection_5'] = 'Ketika Aplikasi tidak dapat menggunakan HTTPS, mereka beralih ke HTTP dan data tidak dienkripsi selama transmisi.';
+$lang['porg_privacy_mobile_protection_4'] = 'Menyimpan sertifikat dan pustaka untuk akses HTTPS hingga saat ini berada di bawah tanggung jawab operator server Piwigo.';
+$lang['porg_privacy_mobile_protection_3'] = 'Kami sangat menyarankan untuk menggunakan HTTPS untuk melindungi data selama transmisi dan Aplikasi mencoba menggunakan koneksi terenkripsi melalui HTTPS saat pertama kali pengguna membuat koneksi dengan galeri Piwigo.';
+$lang['porg_privacy_mobile_protection_2'] = 'Anda bertanggung jawab untuk melindungi data yang disimpan dari akses yang tidak sah.';
+$lang['porg_privacy_mobile_protection_1'] = 'Aplikasi menyimpan data secara lokal di dalam perangkat.';
+$lang['porg_privacy_mobile_intro_9'] = 'Kami berhak mengubah Kebijakan Privasi ini kapanpun dan dengan alasan apapun.';
+$lang['porg_privacy_mobile_intro_8'] = 'Jika anda tidak menyetujui Kebijakan Privasi ini, anda tidak dapat mengakses Aplikasi.';
+$lang['porg_privacy_mobile_intro_7'] = 'Dimohon untuk membaca Kebijakan privasi ini dengan seksama.';
+$lang['porg_privacy_mobile_intro_6'] = 'Nama kami dapat ditemukan dari data meta di repositori git dan bagian Ucapan Terima Kasih dalam Aplikasi.';
+$lang['porg_privacy_mobile_intro_5'] = 'Kebijakan Privasi ini menjelaskan bagaimana kami mengumpulkan, menggunakan, mengungkapkan, dan menjaga informasi Anda ketika Anda menggunakan aplikasi mobile kami.';
+$lang['porg_privacy_mobile_intro_4'] = 'Pengembang Piwigo-Android dan Piwigo-Mobile, kami menghormati privasi pengguna kami.';
+$lang['porg_privacy_mobile_intro_3'] = 'Periksa kebijakan privasi dari situs yang sesuai sebelum mengonfigurasi akun di Piwigo-Android atau Piwigo-Mobile untuk terhubung ke galeri.';
+$lang['porg_privacy_mobile_intro_2'] = 'Kebijakan privasi ini berlaku untuk aplikasi asli Android dan iOS, yang menghubungkan ke situs Piwigo di luar kendali pencipta.';
+$lang['porg_privacy_mobile_intro_14'] = 'Kami tidak bertanggung jawab atas segala data yang dikumpulkan oleh pihak ketiga tersebut.';
+$lang['porg_privacy_mobile_intro_13'] = 'Kebijakan Privasi ini tidak berlaku untuk toko daring/seluler pihak ketiga tempat Anda menginstal Aplikasi atau melakukan pembayaran, termasuk item virtual dalam aplikasi, yang juga dapat mengumpulkan dan menggunakan data tentang Anda.';
+$lang['porg_privacy_mobile_intro_12'] = 'Anda akan dianggap telah mengetahui, tunduk, dan telah menerima perubahan dalam Kebijakan Privasi yang direvisi dengan terus menggunakan Aplikasi setelah tanggal revisi Kebijakan Privasi tersebut diposting.';
+$lang['porg_privacy_mobile_intro_11'] = 'Anda dianjurkan secara berkala untuk meninjau Kebijakan Privasi ini untuk tetap mendapat informasi tentang pembaruan.';
+$lang['porg_privacy_mobile_intro_10'] = 'Kami akan memberitahu anda mengenai perubahan dengan memperbarui tanggal "Terakhir diperbarui" pada Kebijakan Privasi ini.';
+$lang['porg_privacy_mobile_intro_1'] = '<a href="%s" target="_blank">Piwigo-Android</a> dan <a href="%s" target="_blank">Piwigo-Mobile</a> adalah aplikasi gratis dan open source untuk galeri Piwigo.';
+$lang['porg_privacy_mobile_data_piwigo_2'] = 'Selain itu, semua foto, video, dan file lain yang dipilih untuk diunduh / diunggah dan konten yang dimasukkan disimpan, diproses, dan dikirimkan ke situs galeri yang dikonfigurasikan.';
+$lang['porg_privacy_mobile_data_piwigo_1'] = 'Kredensial akun disimpan oleh Aplikasi dan diserahkan ke situs yang dikonfigurasi untuk terhubung ke satu atau beberapa galeri Piwigo.';
+$lang['porg_privacy_mobile_data_geo_2'] = 'Jika anda ingin mengubah akses atau izin kami, anda dapat mengubahnya pada pengaturan perangkat Anda.';
+$lang['porg_privacy_mobile_data_geo_1'] = 'Kami dapat meminta akses atau izin untuk melacak informasi berbasis lokasi dari perangkat seluler Anda, baik secara terus menerus atau hanya ketika Anda menggunakan aplikasi, untuk menyediakan layanan berbasi lokasi.';
+$lang['porg_privacy_mobile_data_device_4'] = 'Selain data yang secara eksplisit dibagikan oleh pengguna dengan pengembang, konten tidak akan disimpan, dikirim, atau diproses oleh kami.';
+$lang['porg_privacy_mobile_data_device_3'] = 'Saat menghubungi tim dukungan, informasi perangkat seperti ID perangkat seluler Anda, model, dan pabrikan, versi sistem operasi Anda, email, negara, lokasi, dan data lain yang Anda berikan dapat dikirimkan ke tim pengembangan Aplikasi.';
+$lang['porg_privacy_mobile_data_device_2'] = 'Jika anda ingin mengubah akses atau izin kami, anda dapat mengubahnya pada pengaturan ponsel anda tetapi berhati-hatilah bahwa beberapa akses dibutuhkan untuk mengunggah file media dari ponsel anda ke galeri Piwigo atau untuk menyimpan file media Piwigo di ponsel Anda.';
+$lang['porg_privacy_mobile_data_device_1'] = 'Aplikasi dapat meminta akses atau izin untuk fitur tertentu dari ponsel anda, termasuk perpustakaan foto dan penyimpanan media ponsel Anda, Pengelola Akun Android, Apple Keychain, folder penyimpanan asing dan berbasis cloud, Geolokalisasi, dan fitur lainnya.';
+$lang['or'] = 'atau';
+$lang['Why does the mobile app store data?'] = 'Mengapa aplikasi menyimpan data?';
+$lang['What data is processed and stored?'] = 'Data apa yang diolah dan disimpan?';
+$lang['Security of your information'] = 'Keamanan Informasi Anda';
+$lang['Privacy Policy for Mobile Apps'] = 'Kebijakan Privasi untuk Aplikasi Seluler';
+$lang['Piwigo Gallery Data'] = 'Data Galeri Piwigo';
+$lang['Mobile Device Access'] = 'Akses Perangkat Mobile';
+$lang['Mobile Device Data'] = 'Data Perangkat Seluler';
+$lang['Last updated on %s'] = 'Diperbarui %s';
+$lang['How is the data protected?'] = 'Bagaimana data diamankan?';
+$lang['Geo-Location Information'] = 'Informasi Geo-lokasi';
+$lang['Contact us'] = 'Hubungi kami';
