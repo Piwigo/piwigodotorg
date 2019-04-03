76bf57b90d4f273d9b780a5d363367aa28e44582
plegall <plg@piwigo.org>
2019-04-03 14:54:31 +0200

    FR : contact page was not translated yet

---
+++ language/fr_FR/contact.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['Contact us'] = 'Contactez nous';
+$lang['porg_contact_desc1'] = 'Vous pouvez nous laisser un message grâce au formulaire ci-dessous.';
+$lang['porg_contact_desc2'] = 'Utilisez le pour des demandes, questions ou remarques privées.';
+$lang['porg_contact_desc3'] = 'Merci d\'utiliser le forum pour tout ce qui peut se discuter publiquement, cela simplifie le suivi pour l\'équipe.';
+$lang['Your email address'] = 'Votre adresse email';
+$lang['porg_contact_error_email'] = 'Oups, cette adresse email n\'est pas valide';
+$lang['Category'] = 'Catégorie';
+$lang['Press inquiry'] = 'Relation Presse';
+$lang['Partnership'] = 'Partenariat';
+$lang['Security report'] = 'Rapport de faille de sécurité';
+$lang['Testimonial'] = 'Témoignage';
+$lang['Your message'] = 'Votre message';
+$lang['porg_contact_error_message'] = 'Oups, il y a un problème avec votre message';
+$lang['Send message'] = 'Envoyer le message';
+
+?>
