9ffab8777b2f397ad1a35b28c57c4560b469a4e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-17 06:21:32 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-2.9.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_902'] = '[2.9.4] к списку пользователей больше не применяются постраничная разбивка или фильтры';
+$lang['porg_issue_945'] = 'pwg_log не проверяет, если  $page[\'section\'] является частью перечисления в таблице';
+$lang['porg_issue_946'] = 'Неправильно обработаны странные разделы полей в pwg_log';
+$lang['porg_issue_972'] = '[albums] проблема с производительностью на странице редактирования, в случаях, когда слишком много изображений';
+$lang['porg_issue_988'] = 'strpos () из-за устаревания не рекомендуется в php 7.3';
+$lang['porg_issue_998'] = 'У Piwigo 2.9 происходит сбой при удаленной синхронизации';
