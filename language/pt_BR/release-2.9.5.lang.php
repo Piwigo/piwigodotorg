5af2f53dd8297024ef0f9433915a73692a6b1274
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-17 06:21:41 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.9.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_998'] = 'Piwigo 2.9 falha com a Sincronização Remota Piwigo';
+$lang['porg_issue_988'] = 'strpos() é obsoleto em php 7.3';
+$lang['porg_issue_972'] = 'problema de desempenho de edição na página [albums], quando existem muitas fotos';
+$lang['porg_issue_946'] = 'Manipulação incorreta de estranhos campos de seção em pwg_log';
+$lang['porg_issue_945'] = 'pwg_log não está checando se $page[\'section\'] é parte do enum na tabela';
+$lang['porg_issue_902'] = '[2.9.4] lista de usuários não mais paginados ou filtrados';
