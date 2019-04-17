35c7f5f0f925aacdfe6250cd8f63c105900878f1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-17 18:34:07 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-2.9.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_945'] = 'eğer $page[\'section\'] komutu tablonun içindeki enumun parçası ise  pwg_log kontrol etmiyor';
+$lang['porg_issue_902'] = '[2.9.4] kullanıcı listesi artık sayfalanmış veya filtrelenmiş değil';
+$lang['porg_issue_972'] = '[albums] çok fotoğraf olduğunda düzenleme sayfasında performans hataları';
+$lang['porg_issue_946'] = 'pwg_log\'da garip bölüm alanlarının hatalı işlenmesi';
+$lang['porg_issue_988'] = 'strpos() komutu php 7.3 \'de kaldırılmıştır';
+$lang['porg_issue_998'] = 'Piwigo 2.9, Piwigo Remote Sync ile başarısız (çalışmıyor)';
