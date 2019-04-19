6d665e451b5215cbef50b13af7f1027050d88d17
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-19 06:27:09 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/release-2.9.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_946'] = 'Niepoprawną obsługę niektórych pól w pwg_log';
+$lang['porg_issue_998'] = 'Piwigo 2.9 nie działało poprawnie z Piwigo Remote Sync';
+$lang['porg_issue_902'] = '[2.9.4] lista użytkowników nie jest już stronicowana lub filtrowana';
+$lang['porg_issue_945'] = 'pwg_log nie sprawdza już, czy dana $page[\'section\'] jest częścią enum w tabeli';
+$lang['porg_issue_972'] = '[albumy] problemy wydajnościowe na stronie edycji, gdy było za dużo zdjęć';
+$lang['porg_issue_988'] = 'strpos() jest wycofane w php 7.3';
