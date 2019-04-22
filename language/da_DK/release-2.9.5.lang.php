2ea324aedb3830a9796f8676043dae04b8f162cd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-22 08:58:07 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.9.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_902'] = '[2.9.4] Brugerlisten er ikke længere sideinddelt eller filtreret';
+$lang['porg_issue_945'] = 'pwg_log kontrollerer ikke hvorvidt en given $page[\'section\'] er del af enum\'en i tabellen';
+$lang['porg_issue_946'] = 'Forkert håndtering af underlige section-felter i pwg_log';
+$lang['porg_issue_972'] = '[albummer] Problemer med ydeevnen på redigeringssiden, når der er for mange fotografier';
+$lang['porg_issue_988'] = 'strpos() er udfaset i php 7.3';
+$lang['porg_issue_998'] = 'Piwigo 2.9 fejler med Piwigo Remote Sync';
