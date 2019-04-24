284c88e7e1aaaf446c9a967be139c29a78d4edcf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-24 05:54:57 +0200

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.9.5.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_902'] = '[2.9.4] Benutzerliste wird nicht mehr gefiltert oder in Seiten unterteilt';
+$lang['porg_issue_945'] = 'pwg_log überprüft nicht, ob angegebener Wert von $page[\'section\'] in dem Enum der Tabelle enthalten ist';
+$lang['porg_issue_946'] = 'Falsche Behandlung von seltsamen section-Feldern in pwg_log';
+$lang['porg_issue_972'] = '[Alben] Performanzprobleme auf der Bearbeitungsseite mit zu vielen Fotos';
+$lang['porg_issue_988'] = 'strpos() ist veraltet in PHP 7.3';
+$lang['porg_issue_998'] = 'Piwigo 2.9 scheitert mit Piwigo Remote Sync';
