af01ec2c2e971af45d1600b6bf4f856a4f0b7f7b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-05-18 08:29:28 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/release.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can compare the md5sum of your downloaded file with the official md5sum provided on piwigo.org.'] = '您可以将所下载文件的 md5sum 与官方在 piwigo.org 上提供的 md5sum 进行比较。';
+$lang['Once you have extracted the files, transfer them onto your web server with a FTP client over your Piwigo %s installation.'] = '在解压缩文件后，使用FTP客户端将其上传到您的web服务器上，over your Piwigo %s installation.';
+$lang['Release note'] = '版本更新说明';
+$lang['Released on %s'] = '于 %s 发布';
+$lang['The MD5 sum is a convenient tool to make sure that your file is not corrupted.'] = 'MD5 验证 (MD5 sum) 是确保文件未被损坏的一个方便工具。';
+$lang['The list of major changes is described on <a href="%s">Piwigo %s</a> release note'] = '主要变更见 <a href="%s">Piwigo %s</a> 版本更新说明';
+$lang['Updated languages'] = '更新的语言';
+$lang['Upgrade'] = '升级';
+$lang['We recommend the <b>automatic upgrade</b>.'] = '我们建议 <b>自动升级</b>。';
+$lang['Automatic upgrade'] = '自动升级';
+$lang['Bugs fixed'] = '错误修正';
+$lang['Download Piwigo %s'] = '下载 Piwigo %s';
+$lang['Featured added'] = '增加特性';
+$lang['If you are running a version older than %s and do not want to use the automatic upgrade, then follow the <b>manual upgrade</b>.'] = '如果您运行的版本早于 %s 并且不想使用自动升级，那么请 <b>手动升级</b>。';
+$lang['If you\'re running <b>Piwigo %s</b> you can also download the <b>%s_to_%s.zip</b> archive that contains all new and modified files.'] = '如果您运行的是 <b>Piwigo %s</b> 您也可以下载 <b>%s_to_%s.zip</b> 存档，它包含了所有全新的和更新的文件。';
+$lang['If you\'re running <b>version 2.2+</b>, Piwigo will tell you which plugins may be not compatible with Piwigo %s before upgrade.'] = '如果您运行的是 <b>version 2.2+</b>, Piwigo 将在升级前告诉您哪些插件可能与 Piwigo %s 不能兼容。';
+$lang['Known issues'] = '已知问题';
+$lang['Languages'] = '语言';
+$lang['Manual Upgrade'] = '手动升级';
+$lang['New language:'] = '新语言：';
+$lang['No database upgrade is required.'] = '无需数据库更新。';
