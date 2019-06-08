0b76ff4909248cf46a29d6959e116cdd4628e94f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-06-08 07:43:43 +0200

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.9.2.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_755'] = 'La sincronización de los metadatos envía una creación de fecha incorrecta para Mysql 5.7';
+$lang['porg_issue_760'] = 'La creación de la fecha no se actualizó al cambiarla de picture_modify';
+$lang['porg_issue_767'] = 'La publicación a través de Shotwell 0.22.0 falla con Piwigo 2.9.1';
+$lang['porg_issue_777'] = 'Categorías WS getImages, TotalCount';
+$lang['porg_issue_787'] = '"acción" inesperada para password.php muestra errores';
+$lang['porg_issue_789'] = 'Posibilidad de evitar mensaje de error en la página de inicio de sesión';
+$lang['porg_issue_726'] = 'lanzarlo en la creación del álbum';
+$lang['porg_issue_731'] = 'Tablero de control, falta el número de fotos';
+$lang['porg_issue_732'] = 'renombrar función transliterar';
+$lang['porg_issue_739'] = '[búsqueda] cuando una búsqueda en word también coincide con las etiquetas';
+$lang['porg_issue_747'] = 'Aviso de PHP al cambiar el tema de la página de administrador';
+$lang['porg_issue_751'] = 'No se puede ejecutar install / piwigo_structure-mysql.sql dos veces';
+$lang['porg_issue_678'] = 'PHP 7 RSS error';
+$lang['porg_issue_710'] = 'el valor de entrada \"redirect_decoded\"  no es válido';
+$lang['porg_issue_713'] = 'actualiza las notificaciones por email en francés/inglés';
+$lang['porg_issue_721'] = 'Informe de error: configure el álbum privado del administrador como público a través de CSRF y permisos incorrectos';
+$lang['porg_issue_723'] = 'Al solicitar un ID privado, se muestra el enlace permanente.';
+$lang['porg_issue_724'] = 'Informe de error: inyección de SQL en la página cat_options';
+$lang['porg_issue_725'] = 'proteger pwg.images.setInfo de HTML';
