0ca30b71127c0a57d0d3e8eb224a5c58512e5742
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-07-16 17:23:07 +0200

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.9.1.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_696'] = 'API de PIWIGO: pwg.categories.delete no funciona correctamente.';
+$lang['porg_issue_699'] = 'Inclusión de archivos en la administración.';
+$lang['porg_issue_701'] = 'El enlace de sincronización del álbum llega a la página para sincronizar ./galleries/ incluso si el álbum se encuentra en otra carpeta';
+$lang['porg_issue_705'] = 'backend de la lista de usuarios, compruebe el parámetro de entrada';
+$lang['porg_issue_706'] = 'La identificación [de seguridad] debe redireccionar solo dentro de Piwigo';
+$lang['porg_issue_707'] = 'número de vistas, oculta decimales cuando menos de 1k';
+$lang['porg_issue_710'] = 'el parámetro de entrada \ "redirect_decoded \" no es válido';
+$lang['porg_issue_663'] = 'Error de SQL en el tour 2.9';
+$lang['porg_issue_667'] = 'Informe de error: Cross Site Scripting (XSS) en el parámetro de la página de admin.php';
+$lang['porg_issue_674'] = '[php7.1] cuadro de edición de usuario, [] operador no admitido para cadenas';
+$lang['porg_issue_679'] = 'Panel de administración, necesita desencadenador para complementos';
+$lang['porg_issue_683'] = 'el uso del disco, oculta decimales cuando hay más de 100 GB';
+$lang['porg_issue_685'] = 'administrador de lotes, eliminar foto no funciona con Safari';
+$lang['porg_issue_693'] = 'Fallo en la descarga de lotes utilizando pclzip y php 7.1';
