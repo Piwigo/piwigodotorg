8a1b6ff245a9b16d0149ee813086318a6e36c3f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-07-16 17:23:20 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-2.9.1.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_693'] = 'Downloader batch non funziona con pclzip e php 7.1';
+$lang['porg_issue_696'] = 'pwg.categories.delete non funziona correttamente';
+$lang['porg_issue_699'] = 'File inclusion in amministrazione';
+$lang['porg_issue_701'] = 'il link di sincronizzazione di un album punta sulla sincronizzazione di ./galleries/ anche se si tratta di un\'altra directory';
+$lang['porg_issue_705'] = 'gestione degli utenti lato server, controllare le impostazioni di input';
+$lang['porg_issue_706'] = 'l\'identificazione dovrebbe reindirizzare solo su Piwigo';
+$lang['porg_issue_707'] = 'numero di visite, nascondere i decimali se inferiore a 1k';
+$lang['porg_issue_710'] = 'il parametro di input \"redirect_decoded\" non è valido';
+$lang['porg_issue_685'] = 'Gestione batch, l\'eliminazione delle foto non funziona su Safari';
+$lang['porg_issue_683'] = 'Utilizzo del disco, nascondere i decimali se più di 100 GB';
+$lang['porg_issue_679'] = 'dashboard, necessario attivare i plug-in';
+$lang['porg_issue_667'] = 'Cross Site Scripting (XSS) sul parametro "pagina" di admin.php';
+$lang['porg_issue_674'] = '[php7.1] popin user edition, [] operatore non supportato per le stringhe';
+$lang['porg_issue_663'] = 'Errore SQL durante la visita guidata 2.9';
