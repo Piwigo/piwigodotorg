63cb967161a8b758bcd3757dad99633838b7b9c8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-07-24 05:44:28 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/what_is_piwigo.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_gnu_desc2'] = '开源并不仅仅意味着可以查看源代码——它还具有政治和哲学意义。';
+$lang['porg_whatispiwigo_gnu_desc3'] = '开放源码，或“自由软件”，意味着您可以在特定条件下自由修改和重新发布源代码。';
+$lang['porg_whatispiwigo_gnu_desc4'] = '"Free" 指的不是价格，而是自由。';
+$lang['porg_whatispiwigo_gnu_desc5'] = '"Free" 的两种含义之间的区别，通常可以描述为“言论自由（free speech） 与 免费啤酒（free beer）”两者间的区别。';
+$lang['porg_whatispiwigo_gnu_desc6'] = 'GPL 的 free 同言论自由的 free 是一样的。';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo 一直是，也将永远是一个自由软件。';
+$lang['porg_whatispiwigo_open_source_desc2'] = '您可以阅读和理解的代码是您可以信任的代码。';
+$lang['porg_whatispiwigo_organisation_desc1'] = '公共部门、公司、基金会……等，都有管理自己的图片资料的需要。';
+$lang['porg_whatispiwigo_organisation_desc2'] = '在<em>数字资产管理<em>领域，Piwigo 专注于易用性、承受能力、性能与可持续性。';
+$lang['porg_whatispiwigo_professional_desc1'] = '专业摄影师需要向客户公开或私下展示他们的作品。';
+$lang['porg_whatispiwigo_individual_desc2'] = '这里有 Piwigo 的粉丝们如何使用它的一些范例。';
+$lang['porg_whatispiwigo_individual_desc1'] = '以20年的时间跨度建立一个家庭影集，管理维护一个欧洲蜘蛛种类的图片收藏，作为摄影爱好者展示自己最棒的风光摄影作品……';
+$lang['porg_whatispiwigo_high_volume_desc1'] = '拥有超过50万张的图片收藏？对 Piwigo 来说小菜一碟。';
+$lang['porg_whatispiwigo_high_volume_desc2'] = '快如闪电是它的天性。';
+$lang['porg_whatispiwigo_how_to_desc1'] = '您可以从 Piwigo<span style="color:#f70;">.org</span> 下载 Piwigo 并安装在符合一定技术要求的主机上。';
+$lang['porg_whatispiwigo_how_to_desc2'] = 'Piwigo 是免费的，您只需承担主机的费用。';
+$lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo 本身也在 Piwigo<span style="color:#ff3363">.com</span> 提供<em>图库服务</em>：花几秒钟开设一个试用帐号，无需知道技术细节便可开始使用 Piwigo。';
+$lang['porg_whatispiwigo_how_to_desc4'] = '在 Piwigo.com 上建立相册需要支付主机费用。';
+$lang['Amateurs'] = '业余玩家';
+$lang['Families'] = '家庭';
+$lang['porg_whatispiwigo_professional_desc2'] = 'Piwigo 提供了高级的权限设置。';
+$lang['porg_whatispiwigo_professional_desc3'] = '用户可以下载单张图片或整个相册。';
+$lang['porg_whatispiwigo_sustainable_desc1'] = '诞生于2002年，Piwigo 早已发展<em>成熟</em>。';
+$lang['porg_whatispiwigo_sustainable_desc2'] = '当然它还将继续紧跟技术潮流！';
+$lang['porg_whatispiwigo_use_desc1'] = 'Piwigo 的用户群将它用在了各个方面。';
+$lang['porg_whatispiwigo_use_desc2'] = '从中型图片集<br> 到巨大的图库，Piwigo 都能替您省时省力，轻松搞定工作。';
+$lang['Batch Downloader'] = '批量下载器';
+$lang['Collections'] = '收藏';
+$lang['Fansites'] = '粉丝网站';
+$lang['GNU License'] = 'GNU 许可证';
+$lang['How to use Piwigo ?'] = '怎样使用 Piwigo';
+$lang['Lightning Fast'] = '闪电般的迅捷';
+$lang['Multiple Administrators'] = '多管理员';
+$lang['Share with Clients'] = '与客户分享';
+$lang['What can I use Piwigo for ?'] = '我能用 Piwigo 做什么？';
+$lang['Why choosing Piwigo ?'] = '为何选择 Piwigo ？';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo 是一款可以用来管理您的图片集与其他媒体的网站程序。';
+$lang['porg_whatispiwigo_desc2'] = '内置强大功能，全世界都在使用它管理图片库。';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'GNU 通用公共许可证，或 GPL，是一种开源许可证。';
