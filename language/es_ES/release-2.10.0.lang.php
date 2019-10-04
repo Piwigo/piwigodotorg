1df45da11971dac5f771e53a7fea10da700bbe03
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-04 05:44:57 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-2.10.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['a major update, focused on design and user experience'] = 'una actualización importante, orientada al diseño y la experiencia de usuario';
+$lang['porg_v2100_activity_title'] = 'Las acciones de administración se registran';
+$lang['porg_v2100_activity_s4'] = 'Más adelante podremos mostrarla.';
+$lang['porg_v2100_activity_s3'] = 'De momento, Piwigo solamente registra la actividad.';
+$lang['porg_v2100_activity_s2'] = 'Por ejemplo, si añade/edita/suprime una foto/álbum/etiqueta/usuario/grupo o si inicia o finaliza sesión, Piwigo registrará quién llevó a cabo la acción y cuándo.';
+$lang['new'] = 'nuevo';
+$lang['porg_v2100_activity_s1'] = 'Ahora, muchas acciones realizadas en la administración se almacenan en una tabla <code>activity</code> en la base de datos.';
+$lang['Technical features'] = 'Características técnicas';
