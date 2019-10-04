23348f51ad4b1672e49c8eaf50a6b1870b4403c2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-04 05:45:12 +0200

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.10.0.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v2100_activity_s1'] = 'Viele Verwaltungs-Aktionen werden in einer <code>activity</code> Tabelle in der Datenbank gespeichert.';
+$lang['porg_v2100_android9_s5'] = 'Das bedeutet es wird automatisch verwendet, wenn Piwigo feststellt, dass der Benutzer ein mobiles Endgerät benutzt.';
+$lang['porg_v2100_dates_s2'] = 'Piwigo kann die Datumswerte der Fotos anzeigen, die mit einem Album verknüpft sind.  ';
+$lang['porg_v2100_headers_s6'] = 'Der angenehme Nebeneffekt ist, dass wir jetzt eine aufgeräumte COPYING-Datei mit Details zum Copyright mit Daten und Trademarks + eine LICENSE-Datei mit der vollständigen GPLv2 Lizenz haben.';
+$lang['porg_v2100_help_c1'] = 'Die Hilfe auf der Verwaltungsseite öffnet sich jetzt als Popin (internes Widget) anstelle eines altmodischen Popups.';
+$lang['porg_v2100_intro3'] = 'Die Reise ist noch nicht vorbei, aber wir sind stolz auf die Arbeit, die unsere talentierten Design bis jetzt gemacht haben.';
+$lang['porg_v2100_headers_s5'] = 'Das Hauptziel war die Jahreszahl aus den Headern zu entfernen, weil dies ein Update aller PHP-Dateien einmal im Jahr erfordert ohne dass es eine Änderung des Programmcodes gibt.';
+$lang['porg_v2100_intro1'] = 'Wir freuen uns das neuste Piwigo-Update zu präsentieren.';
+$lang['porg_v2100_help_title'] = 'Die Hilfe auf den Adminseiten öffnet sich als Popin';
+$lang['porg_v2100_hannah_s2'] = 'Ich begann im Januar 2019 als Praktikantin für Piwigo zu arbeiten. Mein Hauptziel war das Aussehen von Piwigo zu modernisieren. ';
+$lang['porg_v2100_hannah_s3'] = 'Beim Brainsorming mit Pierrick haben wir entschieden mit dem Auffrischen des Standardthemes zu beginnen.';
+$lang['porg_v2100_headers_title'] = 'Die Quellcode-Header wurden vereinfacht';
+$lang['porg_v2100_headers_s3'] = 'Anstelle von diesem:';
+$lang['porg_v2100_headers_s4'] = 'haben wir nun diesen vereinfachten Header:';
+$lang['porg_v2100_dates_c2'] = 'In der Galerie wird die Datumsspanne neben dem Albumtitel angezeigt. ';
+$lang['porg_v2100_headers_s1'] = 'Wir haben die Quellcode-Header in allen PHP-Dateien vereinfacht.';
+$lang['porg_v2100_headers_s2'] = 'Anstatt ein volles Copyright mit Jahr und einer Zusammenfassung der GPLv2-Lizenz, sagen wir dem Leser die COPYING-Datei zu öffnen';
+$lang['porg_v2100_android9_s1'] = 'Wir wissen, dass Android 10 vor wenigen Tagen veröffentlicht wurde, aber nur wenige Geräte benutzen heutzutage schon Android 9.';
+$lang['porg_v2100_dates_s1'] = 'Auch dies ist kein neues Feature, wir haben es nur einfacher zu aktivieren gemacht.';
+$lang['porg_v2100_dup_s1'] = 'Der Batch-Manager hilft doppelte Fotos zu finden.';
+$lang['porg_v2100_dup_s2'] = 'Dieses Feature ist nicht neu, aber wir haben es für Piwigo 2.10 verbessert.';
+$lang['porg_v2100_dup_title'] = 'Dubletten bekommen eine bessere Sortierung';
+$lang['porg_v2100_hannah_s1'] = 'Mein Name ist Hannah. Ich studiere gerade User eXperience (UX) Design am ActivDesign in Rennes (Frankreich).';
+$lang['porg_v2100_comments_s1'] = 'Keine Panik, wir entfernen dieses Feature nicht, wir haben es nur standardmäßig ausgeschaltet.';
+$lang['porg_v2100_darkmode_s2'] = 'Dieses Farbschema wurde für Benutzer entwickelt, die in Umgebungen mit wenig Licht arbeiten.';
+$lang['porg_v2100_darkmode_s4'] = 'Wir wollen Piwigo angenehm für Ihre Augen machen!';
+$lang['porg_v2100_dashboard_c1'] = 'Das Verwaltungs-Dashboard kommt jetzt in Farbe!';
+$lang['porg_v2100_dates_s3'] = 'Bitte beachten Sie, dass alle Themes dies unterstützen.';
+$lang['porg_v2100_dates_title'] = 'Datum des Albums anzeigen';
+$lang['a major update, focused on design and user experience'] = 'ein größeres Update mit dem Fokus auf Design und Benutzererlebnis';
+$lang['porg_v2100_albums_c1'] = 'Der Album-Manger wurde mit großen Aktionsbuttons aufgefrischt.';
+$lang['porg_v2100_android9_s6'] = 'Sie können SmartPocket auch deaktivieren, wenn Sie lieber immer Modus benutzen.';
+$lang['porg_v2100_api_s6'] = 'akzeptiert jetzt auch ein leeres Passwort (wie der Weblogin)';
+$lang['porg_v2100_activity_title'] = 'Admin-Aktionen werden geloggt';
+$lang['porg_v2100_darkmode_title'] = 'Neuer Dark Mode';
+$lang['porg_v2100_dashboard_title'] = 'Das Dashboard bekommt eine Auffrischung';
+$lang['porg_v2100_comments_s2'] = 'Sie fragen wahrscheinlich "Warum eine existierende Funktion ausschalten?"';
+$lang['porg_v2100_comments_s3'] = 'Weil zu viele sichtbare Features Leute verschrecken könnten.';
+$lang['porg_v2100_comments_s5'] = 'Auf Piwigo.com, wo wir tausende Piwigos hosten, haben wir interessante Statistiken über die benutzten Funktionen gesehen.';
+$lang['porg_v2100_comments_s6'] = 'Was Benutzerkommentare betrifft, haben nur 16% der Piwigo-Installationen mehr als 10 Kommentare.';
+$lang['porg_v2100_comments_title'] = 'Wir haben Benutzerkommentare standardmäßig deaktiviert';
+$lang['porg_v2100_darkmode_s1'] = 'Die Verwaltungoberfläche im neuen Dark Mode.';
+$lang['porg_v2100_api_s3'] = 'fügt ein Foto zu den Benutzerfavoriten hinzu';
+$lang['porg_v2100_api_s4'] = 'entfernt ein Foto von den Benutzerfavoriten';
+$lang['porg_v2100_api_s5'] = 'gibt eine Liste von Benutzerfavoriten zurück ';
+$lang['porg_v2100_comments_s4'] = 'Wir müssen klug abwägen, was am Wichtigsten für die Mehrheit ist.';
+$lang['porg_v2100_api_s1'] = 'setzt die Checksumme, wenn sie fehlt';
+$lang['porg_v2100_android9_s2'] = 'Der mobile Theme SmartPocket wurde unter Android 9 nicht korrekt geladen. ';
+$lang['porg_v2100_android9_title'] = 'Der Theme SmartPocket ist jetzt mit Android 9 kompatibel';
+$lang['porg_v2100_api_title'] = 'API Verbesserungen';
+$lang['Technical features'] = 'Technische Features';
+$lang['porg_v2100_activity_s3'] = 'Im Moment loggt Piwigo diese Aktivitäten nur.';
+$lang['porg_v2100_activity_s4'] = 'Später werden wir sie anzeigen können.';
+$lang['new'] = 'neu';
+$lang['porg_v2100_albums_title'] = 'Album-Manager, größer und einfacher';
+$lang['porg_v2100_android9_s3'] = 'Das ist in Piwigo 2.10 behoben.';
