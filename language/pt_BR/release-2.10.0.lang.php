2a5665f3d97f57329897afbf7ec2770ea7360f74
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-05 18:07:42 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.10.0.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v2100_api_s3'] = '
+adiciona uma foto aos favoritos do usuário.';
+$lang['porg_v2100_api_s2'] = '
+altera a ordem de classificação de um álbum entre seus irmãos. Você também pode reordenar todo o conjunto de álbuns de uma só vez.';
+$lang['porg_v2100_api_s1'] = '
+define a soma de verificação das fotos em falta.';
+$lang['porg_v2100_android9_title'] = '
+O tema SmartPocket agora é compatível com Android 9';
+$lang['porg_v2100_android9_s6'] = '
+Você pode desativar o SmartPocket se preferir sempre usar o Modus.';
+$lang['porg_v2100_android9_s5'] = '
+Isso significa que será usado automaticamente se o Piwigo detectar que o visitante está usando um dispositivo móvel.';
+$lang['porg_v2100_android9_s4'] = '
+Falando sobre o SmartPocket ... mesmo que o Modus, o novo tema padrão do Piwigo 2.10 seja responsivo, mantemos o SmartPocket ativado por padrão.';
+$lang['porg_v2100_android9_s3'] = 'Isso foi corrigido no Piwigo 2.10.';
+$lang['porg_v2100_android9_s2'] = '
+O tema móvel, SmartPocket, não foi carregado corretamente com o Android 9.';
+$lang['porg_v2100_android9_s1'] = '
+Sabemos que o Android 10 foi anunciado há alguns dias atrás, mas apenas alguns dispositivos estão executando o Android 9 atualmente.';
+$lang['porg_v2100_albums_title'] = '
+Gerente Álbum, maior e mais simples';
+$lang['porg_v2100_albums_c1'] = '
+O gerenciador de álbuns foi atualizado com grandes botões de ação.';
+$lang['porg_v2100_activity_title'] = '
+Ações de administrador são registradas';
+$lang['porg_v2100_activity_s4'] = 'Mais tarde, poderemos mostrá-las.';
+$lang['porg_v2100_activity_s3'] = '
+Por enquanto, Piwigo apenas registra essa atividade.';
+$lang['porg_v2100_activity_s2'] = '
+Por exemplo, se você adicionar/editar/excluir uma foto/álbum/etiqueta/ usuário/grupo ou se fizer login/logout, o Piwigo registrará quem fez a ação e quando.';
+$lang['porg_v2100_activity_s1'] = 'Muitas ações que você executa na administração agora são armazenadas em uma tabela <code>activity</code> no banco de dados.';
+$lang['new'] = 'novo';
+$lang['a major update, focused on design and user experience'] = 'uma grande atualização, focada no design e na experiência do usuário';
+$lang['Technical features'] = 'Características técnicas';
