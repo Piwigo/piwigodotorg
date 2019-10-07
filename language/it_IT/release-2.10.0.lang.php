de401befd6bfab9ae3d3e0557308d5063a48cb32
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-07 16:42:02 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-2.10.0.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v2100_comments_s4'] = 'Dobbiamo selezionare attentamente ciò che è più importante per la maggioranza.';
+$lang['porg_v2100_dates_title'] = 'Visualizza le date di ogni album';
+$lang['porg_v2100_dup_s2'] = 'Questa funzionalità non è nuova, ma è stata migliorata nella 2.10.';
+$lang['porg_v2100_dup_title'] = 'Miglior ordinamento dei duplicati';
+$lang['porg_v2100_darkmode_title'] = 'Nuovo tema scuro';
+$lang['porg_v2100_dashboard_c1'] = 'Il dashboard dell\'amministrazione prende dei colori!';
+$lang['porg_v2100_dashboard_title'] = 'Un dashboard ringiovanito';
+$lang['porg_v2100_dates_c1'] = 'Non è più necessario passare via la configurazione locale per attivarlo, basta selezionare una casella.';
+$lang['porg_v2100_dates_c2'] = 'Nella galleria, il periodo di data dell\'album viene visualizzato accanto al titolo dell\'album.';
+$lang['porg_v2100_dates_s1'] = 'Anche in questo caso, non è una nuova funzionalità, ma diventa più facile da attivare.';
+$lang['porg_v2100_dates_s2'] = 'Piwigo può visualizzare le date delle foto associate all\'album.';
+$lang['porg_v2100_dates_s3'] = 'Nota che non tutti i temi supportano questa funzionalità.';
+$lang['porg_v2100_android9_title'] = 'Tema Smartpocket compatibile con Android 9';
+$lang['porg_v2100_api_s1'] = 'calcola e registra il checksum per le foto che non ne hanno.';
+$lang['porg_v2100_api_s3'] = 'aggiungere una foto ai preferiti dell\'utente.';
+$lang['porg_v2100_api_s4'] = 'rimuovere una foto dai preferiti dell\'utente.';
+$lang['porg_v2100_api_s5'] = 'restituisce l\'elenco dei preferiti dell\'utente.';
+$lang['porg_v2100_api_title'] = 'Miglioramenti sulle API';
+$lang['porg_v2100_comments_title'] = 'Disattivazione dei commenti predefiniti';
+$lang['porg_v2100_darkmode_s4'] = 'Vogliamo che Piwigo sia gradevole per i tuoi occhi!';
+$lang['porg_v2100_albums_c1'] = 'Il gestore degli album è stato riprogettato con grandi pulsanti per le azioni.';
+$lang['porg_v2100_albums_title'] = 'Gestore degli album, più grande e più semplice';
+$lang['porg_v2100_android9_s1'] = 'Sappiamo che Android 10 è stato rilasciato pochi giorni fa, ma al momento pochi telefoni / tablet utilizzano Android 10.';
+$lang['porg_v2100_android9_s2'] = 'Il tema mobile, SmartPocket, non si caricava correttamente con Android 9.';
+$lang['porg_v2100_android9_s3'] = 'È stato corretto con Piwigo 2.10.';
+$lang['porg_v2100_android9_s4'] = 'Parlando di SmartPocket ... anche se Modus, il nuovo tema predefinito in 2.10, è <em>responsive</em>, manteniamo SmartPocket abilitato di default.';
+$lang['porg_v2100_android9_s5'] = 'Ciò significa che verrà caricato automaticamente se Piwigo rileva un telefono.';
+$lang['porg_v2100_android9_s6'] = 'Puoi disattivare SmartPocket se preferisci utilizzare Modus sistematicamente.';
+$lang['Technical features'] = 'Evoluzioni tecniche';
+$lang['a major update, focused on design and user experience'] = 'aggiornamento importante, centrato sul design e l\'esperienza utente';
+$lang['new'] = 'nuovo';
+$lang['porg_v2100_activity_s1'] = 'Molte delle azioni eseguite vengono ora archiviate in una nuova tabella <code>activity</code> nel database del vostro Piwigo.';
+$lang['porg_v2100_activity_s2'] = 'Ad esempio aggiungendo / modificando / eliminando un album / foto / tag / utente / gruppo o se accedi / sconnetti, Piwigo registra chi ha fatto cosa e quando.';
+$lang['porg_v2100_activity_s3'] = 'Per il momento, Piwigo registra solo l\'attività.';
+$lang['porg_v2100_activity_s4'] = 'Più tardi, il tuo Piwigo sarà in grado di presentarti questa attività.';
+$lang['porg_v2100_activity_title'] = 'Le attività di amministrazione sono registrate';
