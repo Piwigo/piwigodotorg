96359f09d30ec7b31b38fddbf24787b95ff774aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-07 16:41:49 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-2.10.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Technical features'] = 'Característiques tècniques';
+$lang['a major update, focused on design and user experience'] = 'Una actualització important, centrada en el disseny i l\'experiència d\'usuari';
+$lang['new'] = 'nou';
+$lang['porg_v2100_activity_s1'] = 'Moltes accions que dueu a terme a l\'administració ara s\'emmagatzemen en una taula <code>activity</code> a la base de dades.';
