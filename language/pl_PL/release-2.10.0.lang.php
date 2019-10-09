ae07f2f8d5278d655b0197b7fea78ba28b0be8b9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-09 06:10:44 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/release-2.10.0.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v2100_api_title'] = 'udoskonalenia API';
+$lang['a major update, focused on design and user experience'] = 'znacząca aktualizacja, skupiająca się na wyglądzie o interakcji z użytkownikiem';
+$lang['new'] = 'nowy';
+$lang['porg_v2100_activity_s3'] = 'Na razie, Piwigo jedynie odnotowuje te działania.';
+$lang['porg_v2100_activity_s4'] = 'Później będziemy w stanie je pokazać.';
+$lang['porg_v2100_activity_title'] = 'Działania Administratora zostają odnotowane';
+$lang['porg_v2100_android9_s3'] = 'Jest to naprawione w Piwigo 2.10.';
