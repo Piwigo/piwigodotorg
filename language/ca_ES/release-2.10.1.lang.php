d5fd62d14d66de22e1820bae6b0ac80728c93fc4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-14 22:09:54 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-2.10.1.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1089'] = 'Jshrink utilitza una funció obsoleta que trenca el javascript';
+$lang['porg_issue_1090'] = 'L\'advertència sobre la manca de sumes de comprovació pot alarmar els usuaris';
+$lang['porg_issue_1092'] = 'Error: Error de sintaxi, expressió no reconeguda: input[name=pwg_token';
+$lang['porg_issue_1097'] = '[gestor de processament per lots] La mida de bloc del càlcul de la suma de comprovació hauria de romandre inalterada';
+$lang['porg_issue_1098'] = '[gestor de processament per lots] Càlcul de suma de comprovació, reduir la mida de bloc';
+$lang['porg_issue_1099'] = '[batch manager] TypeError: associated_categories és null';
