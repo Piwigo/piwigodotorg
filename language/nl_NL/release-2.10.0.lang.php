59350c0b2f57e92283fd403e33163319340b15ed
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-15 18:23:14 +0200

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/release-2.10.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v2100_activity_s1'] = 'Veel handelingen die uitgevoerd worden in de beheersomgving worden nu geregistreerd in een <code>activiteits</code> tabel in de database.';
+$lang['a major update, focused on design and user experience'] = 'een grote update met aandacht voor uitzicht en gebruikerservaring';
+$lang['Technical features'] = 'Technische eigenschappen';
+$lang['porg_v2100_api_s3'] = 'voegt een foto to aan de favorieten van de gebruiker.';
+$lang['porg_v2100_android9_s3'] = 'Dit is opgelost in Piwigo 2.10.';
+$lang['porg_v2100_activity_s3'] = 'Voorlopig registreert Piwigo gewoon de handeling.';
+$lang['porg_v2100_activity_title'] = 'Handelingen van de beheerder worden geregistreerd.';
+$lang['porg_v2100_activity_s4'] = 'Later zullen we dit kunnen tonen.';
+$lang['porg_v2100_activity_s2'] = 'Bijvoorbeeld, als je een foto/album/tag/gebruiker/groep toevoegt/bewerkt/verwijdert of als je in- of uitlogt, registreert Piwigo wie wanneer de handeling uitvoerde.';
+$lang['new'] = 'nieuw';
