fd955cb211bbaf1725770cec5bc609f676675fce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-16 11:20:38 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/release-2.10.0.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Technical features'] = 'Teknik özellikler';
+$lang['a major update, focused on design and user experience'] = 'tasarım ve kullanıcı deneyimine odaklanan önemli bir güncelleme';
+$lang['new'] = 'yeni';
