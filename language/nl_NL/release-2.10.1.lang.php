4bad73b0ebbca9e5a40dbd7efd6603799f9cfbe9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-27 21:22:44 +0100

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/release-2.10.1.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1089'] = 'Jshrink gebruikt verouderde functie, breekt Javascript';
+$lang['porg_issue_1090'] = 'ontbrekende controlesommen waarschuwing op het dashboard kan gebruikers bang maken';
+$lang['porg_issue_1092'] = 'Fout: syntaxisfout, niet-herkende uitdrukking: invoer [naam = pwg_token';
+$lang['porg_issue_1097'] = '[batch manager] controlesom berekening blokkengrootte moet hetzelfde blijven';
+$lang['porg_issue_1098'] = '[batchmanager] controlesom-berekening, verklein blokgrootte';
+$lang['porg_issue_1099'] = '[batchmanager] TypeError: bijbehorende_categorieën is nul';
