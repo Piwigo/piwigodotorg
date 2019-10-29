7ecb59de3347f57f83af032c8fafc3c5893e5025
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-29 22:15:31 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.10.1.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1089'] = 'Jshrink usa função obsoleta, e quebra o javascript';
+$lang['porg_issue_1090'] = 'aviso de somas de verificação ausentes no painel pode assustar os usuários';
+$lang['porg_issue_1092'] = 'Erro:Erro de sintaxe, expressão não reconhecida: input[name= pwg_token';
+$lang['porg_issue_1097'] = 'o tamanho do bloco da computação de soma de verificação [gerenciador de lote] deve permanecer o mesmo';
+$lang['porg_issue_1098'] = 'computação de soma de verificação [gerente de lote], reduza o tamanho do bloco';
+$lang['porg_issue_1099'] = '[gerenciador de lote] TypeError: Associated_categories é nulo';
