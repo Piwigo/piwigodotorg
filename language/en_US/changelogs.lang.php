24ff0f4e1108d4f22452ed11341c593b9df24789
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-11-10 07:07:18 +0100

    Add en_US, thanks to Pat2309

---
+++ language/en_US/changelogs.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Admin redesign'] = 'Admin redesign';
+$lang['Dark mode'] = 'Dark mode';
+$lang['Default theme Modus'] = 'Default theme Modus';
+$lang['Read more'] = 'Read more';
+$lang['porg_changelogs_desc1'] = 'Here is the list of all stable Piwigo releases.';
+$lang['porg_changelogs_desc2'] = 'Use our latest release: you will get better support and all known  security bugs will be fixed.';
+$lang['porg_changelogs_technicals_desc1'] = 'If you need technical views of the releases go to the <a href="%s">Branches and Releases</a> page.';
