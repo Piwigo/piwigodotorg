8e8c2de93f6fe982634e75db9a5a9db1ebed3d3a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-11-10 07:06:23 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-2.10.1.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1099'] = '[batch manager] TypeError: associated_categories ma wartość null';
+$lang['porg_issue_1098'] = '[batch manager] obliczanie sumy kontrolnej, zmniejsz rozmiar bloku';
+$lang['porg_issue_1097'] = '[batch manager] wielkość bloku obliczeniowego sumy kontrolnej powinna pozostać taka sama';
+$lang['porg_issue_1092'] = 'Błąd: Błąd składni, nierozpoznane wyrażenie: input[name=pwg_token';
+$lang['porg_issue_1090'] = 'ostrzeżenie o brakujących sumach kontrolnych na panelu może przestraszyć użytkowników';
+$lang['porg_issue_1089'] = 'Jshrink używa przestarzałej funkcji, która zakłóca działanie javascript';
