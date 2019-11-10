7a04a88c66b2402a958dd77a0d218ad2b4011a18
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-11-10 07:06:35 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.8.6.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_599'] = 'solucionada la carga de PHPMailer smtp class cuando se usa el transporte SMTP';
+$lang['porg_issue_600'] = 'Cross Site Scripting en la carga de imágenes, reportado por <a href="https://github.com/boogyman2">boogyman2</a>';
+$lang['porg_issue_602'] = 'Token de seccion inválido[look_like_gbo2]';
