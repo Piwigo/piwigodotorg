65ff4ef57526e4204944f92452151a21452630cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-12-01 09:04:25 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/home.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Albums Hierarchy'] = 'Jerarquia d\'àlbums';
+$lang['Discover Piwigo'] = 'Descobriu Piwigo';
+$lang['Explore more features'] = 'Exploreu més característiques';
+$lang['Permissions Control'] = 'Control de permisos';
+$lang['Piwigo - Manage your photo collection'] = 'Piwigo - Gestioneu la vostra col·lecció de fotografies';
+$lang['Powerful features for your photo gallery'] = 'Característiques potents per a la vostra galeria de fotos';
+$lang['Start discovering Piwigo'] = 'Comenceu a descobrir Piwigo';
+$lang['View more showcases'] = 'Veieu més casos d\'exemple';
+$lang['Who are Piwigo users?'] = 'Qui són els usuaris de Piwigo?';
+$lang['Why choose Piwigo?'] = 'Per què escollir Piwigo?';
+$lang['porg_home_desc1'] = 'Piwigo és un programari de galeria fotogràfica de codi obert per a la web';
+$lang['porg_home_desc2'] = 'Dissenyat per a organitzacions, equips i persones individuals';
+$lang['porg_home_high_volume_desc'] = 'Piwigo destaca quan es tracta de classificar milers o fins i tot centenars de milers de fotos.';
+$lang['porg_home_open_source_desc'] = 'El codi font està disponible, és editable, auditable i expandible gràcies als temes i els connectors.';
+$lang['porg_home_organisation_title'] = 'Milers d\'organitzacions i milions de persones individuals utilitzen Piwigo i els hi encanta';
+$lang['porg_home_sustainable_desc'] = 'Nascut l\'any 2002, Piwigo ha donat suport als seus usuaris durant més de %d anys. Sempre evolucionant!';
+$lang['porg_home_title'] = 'Gestioneu la vostra col·lecció de fotos amb Piwigo';
+$lang['porg_love_title'] = 'Els hi encanta Piwigo';
+$lang['porg_showcase_title'] = 'Precioses galeries fotogràfiques creades amb Piwigo';
+$lang['version %s'] = 'versió %s';
