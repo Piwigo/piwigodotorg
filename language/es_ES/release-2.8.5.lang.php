980a78fcd1a64a30e328bb3c9c0eb2bf52c2b09b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-12-27 12:20:19 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.8.5.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_572'] = 'CVE-2016-10084 File Inclusion Attack, reportado por <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_573'] = 'CVE-2016-10085 File Inclusion Attack #2, reportado por <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_574'] = 'File Inclusion with Possible RCE, reportado por <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_575'] = 'CVE-2016-10083 Cross Site Scripting, reportado por <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_595'] = 'actualización de PHPMailer a 5.2.21';
+$lang['porg_issue_599'] = 'arreglar la carga de la clase smtp de PHPMailer cuando se utiliza el transporte SMTP';
