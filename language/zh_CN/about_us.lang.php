630f12aefef5873420e08657c40954e3b80d525b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-01-02 15:16:14 +0100

    Update zh_CN, thanks to LuRenJiasWorld

---
+++ language/zh_CN/about_us.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_about_us_recruitement_desc'] = '团队越来越大，开发效率随之提高。';
+$lang['porg_about_us_dotcom_desc1'] = '从这一平台中的收益将会让部分人能全职为Piwigo项目工作。';
+$lang['porg_about_us_dotcom_desc2'] = '例如Piwigo的创始人Pierrick就依靠Piwigo.com的支持才能一直投入时间于Piwigo.org项目。';
+$lang['porg_about_us_dotcom_desc3'] = '两个项目在很多方面都是相通的：如果某个平台开发出了先进的技术，那么另一个平台也会尽快跟进。';
+$lang['porg_about_us_github_desc'] = 'Piwigo正式加入了GitHub，这是专为全世界程序员而设计的社区。';
+$lang['porg_about_us_join_desc1'] = 'Piwigo项目需要有各种各样技能的人们参与其中，例如开发者、设计师、文书，顾问等。';
+$lang['porg_about_us_join_desc2'] = '如果你认同我们的目标，那么请加入我们吧！';
+$lang['porg_about_us_launch_desc'] = '一个运行在云端，开箱即用的Piwigo。';
+$lang['Years old'] = '年';
+$lang['You?'] = '你？';
+$lang['porg_about_us_apps_desc'] = 'Piwigo为iPhone/iPad以及Android开发的客户端正式上线。';
+$lang['porg_about_us_beginning_desc1'] = '在Free.fr平台上建立一键安装脚本。';
+$lang['porg_about_us_beginning_desc2'] = '吸引了大量的贡献者们。';
+$lang['porg_about_us_birth_desc'] = '在几个月的准备后，第一个版本1.0.0正式发布。';
+$lang['porg_about_us_business_desc1'] = '尽管Piwigo由众多的志愿者们共同维护，但其运营并非完全免费——服务器、域名、网站维护、开发工作等均需要大量开支。';
+$lang['porg_about_us_business_desc2'] = '这一项目如何赚到钱？';
+$lang['porg_about_us_contributors_desc'] = '来自全世界的志愿者们为Piwigo生态系统添砖加瓦。';
+$lang['porg_about_us_desc1'] = '社区中的贡献者们来自全球。';
+$lang['porg_about_us_desc2'] = '如果你认同我们的工作，那么欢迎你加入我们！';
+$lang['porg_about_us_donation_desc1'] = 'Piwigo组织会收集社区中用户们的捐赠。';
+$lang['porg_about_us_donation_desc2'] = '捐赠款项会被直接用于支付服务器、域名、以及移动平台应用商店订阅等费用。';
+$lang['porg_about_us_donation_desc3'] = '<a href="%s">你的捐赠</a>将会对本项目产生巨大帮助。';
+$lang['Arrivals of Plugins'] = '正式引入插件功能';
+$lang['Beginning'] = '起步';
+$lang['Birth'] = '诞生';
+$lang['Business Model'] = '商业模式';
+$lang['Donations'] = '捐赠';
+$lang['Downloads'] = '下载';
+$lang['Forum messages'] = '社区帖子';
+$lang['Get involved!'] = '参与其中！';
+$lang['Mobile apps'] = '移动APP';
+$lang['Our Mission'] = '我们的目标';
+$lang['PhpWebGallery becomes Piwigo'] = 'PhpWebGallery更名为Piwigo';
+$lang['Piwigo contributors'] = 'Piwigo的贡献者们';
+$lang['Piwigo moves to GitHub'] = 'Piwigo开始使用GitHub';
+$lang['Piwigo.com'] = 'Piwigo.com';
+$lang['Piwigo.com is launched'] = 'Piwigo.com正式上线';
+$lang['Recruitement'] = '招募全职开发者';
+$lang['Releases'] = '版本发布';
+$lang['Services'] = '提供服务';
+$lang['Take part in our adventure'] = '和我们一起努力';
+$lang['Want to be part of the adventure?'] = '想加入我们吗？';
+$lang['porg_about_us_mission_desc'] = 'Piwigo的目标是开发出全世界最好的开源图片管理系统，在技术和用户体验上都做到极致。';
+$lang['porg_about_us_plugins_desc1'] = '改变了设计理念和技术架构。';
+$lang['porg_about_us_plugins_desc2'] = '越来越多的新功能与开发者加入其中。';
+$lang['porg_about_us_service_desc1'] = '一部分开发者和设计师们依靠为客户提供新的功能或界面获得收入。';
+$lang['porg_about_us_service_desc2'] = '但绝大多数情况下，这些插件和主题也可以在Piwigo社区供所有Piwigo用户使用。';
+$lang['porg_about_us_webgallery_desc'] = '软件版本更新到了2.0，同时使用了新的名称。';
