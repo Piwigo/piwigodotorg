cbc2a60e767fcbc01c73b3d4ff95532993aef773
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-01-25 10:13:20 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.8.3.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_306'] = '[idioma] El archivo de otro idioma que no sea plugin.lang no está cargado; corregido por <a href="http://piwigo.org/forum/profile.php?id=4851"> LucMorizur </a>';
+$lang['porg_issue_376'] = '[MySQL 5.7] La cláusula ORDER BY no está en la lista SELECT, corregida por <a href="http://piwigo.org/forum/profile.php?id=22630"> Mercury_LABBS </a>';
+$lang['porg_issue_449'] = 'La carga de vídeo falla, corregida por <a href="https://github.com/vvinogradsky"> vvinogradsky </a> (autor del plugin Exportador de Lightroom) y <a href = "https://github.com/dkm "> dkm </a>';
+$lang['porg_issue_542'] = 'El nuevo usuario no siempre debe usar el idioma del navegador';
+$lang['porg_issue_546'] = 'Error en restablecer contraseña por correo electrónico';
+$lang['porg_issue_547'] = 'aumentar las verificaciones en el formato de URL; informado por Nicolas Bidron de <a href="https://www.nccgroup.trust"> Grupo de seguridad NCC </a>';
+$lang['porg_issue_548'] = 'búsqueda rápida y comentario HTML; informado por Nicolas Bidron de <a href="https://www.nccgroup.trust"> Grupo de seguridad NCC </a>';
