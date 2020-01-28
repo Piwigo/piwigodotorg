636c8c619670e477f5e3e400a7c26fa68c2cdc13
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-01-28 07:17:09 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.8.2.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_270'] = '[plantilla] Falta la contraseña.tpl en el tema smartpocket';
+$lang['porg_issue_415'] = 'el enlace de descarga falla en archivos que no son de imagen';
+$lang['porg_issue_430'] = '[Batch Manager] editar autor / título puede usar el marcador de posición';
+$lang['porg_issue_476'] = 'redireccionamiento http para acceso de invitado prohibido';
+$lang['porg_issue_479'] = 'No hay CSPRNG adecuado';
