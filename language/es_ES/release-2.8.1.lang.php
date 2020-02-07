b22fc663a3d276e203b08287298f35769618386f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-02-07 10:43:39 +0100

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/release-2.8.1.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_434'] = 'Constructor obsoleto para LocalSiteReader';
+$lang['porg_issue_436'] = 'Llamada al método indefinido Logger :: advertencia en i.php';
+$lang['porg_issue_439'] = 'actualizar la documentación de inserción para describir múltiples formatos';
+$lang['porg_issue_440'] = 'La búsqueda (tema de smartpocket) no devuelve resultados';
+$lang['porg_issue_442'] = 'Llamada a la función indefinida set_cat_status ()';
+$lang['porg_issue_443'] = 'Error al crear tablas con MySQL 5.7';
+$lang['porg_issue_466'] = 'El nuevo álbum de [Batch Manager] no aparece al mover fotos';
+$lang['porg_issue_470'] = 'Aumenta la aleatoriedad en generate_key. Vulnerabilidad reportada por <a href="https://www.danclifford.eu/"> Dan Clifford </a>. Muchas gracias a él!';
