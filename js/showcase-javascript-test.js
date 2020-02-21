a7bb8198e9352cad6d63cdf3cbd81ac9c3f28d3d
Hannah <lilypickle@hotmail.fr>
2020-02-21 15:08:39 +0100

    javascript document with the different settings for the plugin on the showcases page

---
+++ js/showcase-javascript-test.js
@@ -0,0 +1,18 @@
+mixitup('#mix-wrapper', {
+	load: {
+	  sort: 'order:asc'
+	},
+	  animation: {
+	  effects: 'fade',
+	  duration: 500
+	},
+	classNames: {
+	  block: 'programs',
+	  elementFilter: 'filter-btn',
+	  elementSort: 'sort-btn'
+	},
+	selectors: {
+	  target: '.mix-target'
+	}
+	});
+
