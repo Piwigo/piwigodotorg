2b3a1eabb8dff5b520ecd1c12f48dbab0f9635af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-02-25 05:58:22 +0100

    Update zh_CN, thanks to LuRenJiasWorld

---
+++ language/zh_CN/changelogs.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Admin redesign'] = '重新设计了管理页面';
+$lang['Dark mode'] = '支持暗色模式';
+$lang['Default theme Modus'] = '更新了默认主题';
+$lang['Read more'] = '阅读更多';
+$lang['porg_changelogs_desc1'] = '以下为所有Piwigo稳定版的列表：';
+$lang['porg_changelogs_desc2'] = '使用最新版本，你可以获得更好的支持、更少的BUG与安全隐患';
+$lang['porg_changelogs_technicals_desc1'] = '如果你想了解每个版本的差异，欢迎访问<a href="%s">分支和版本</a> 页面.';
