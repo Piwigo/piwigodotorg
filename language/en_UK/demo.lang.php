6d6e203b1c399b1f82eb768bd046988b33460465
Hannah <lilypickle@hotmail.fr>
2020-03-17 15:32:58 +0100

    Demo page FR text translation (EN translation typo corrected)

--- language/en_UK/demo.lang.php
+++ language/en_UK/demo.lang.php
@@ -8,7 +8,7 @@ $lang['porg_demo_1_text'] = 'This Piwigo demo gallery is based on our default th
 $lang['porg_demo_2_title'] = 'Demo n°2';
 $lang['porg_demo_2_text'] = 'Our second Piwigo demo gallery uses our bootstrap darkroom theme. The color theme here has be personnalised to be accorded with the jumbotron image we have activated with the theme.';
 $lang['porg_demo_3_title'] = 'Demo n°3';
-$lang['porg_demo_3_text'] = 'For the third Piwigo demo gallery, we are using Modus width a personnalised skin. in the banner we have simply added our logo with the title of the gallery and used the colors of this specific logo in the gallery.';
+$lang['porg_demo_3_text'] = 'For the third Piwigo demo gallery, we are using Modus with a personnalised skin. in the banner we have simply added our logo with the title of the gallery and used the colors of this specific logo in the gallery.';
 $lang['porg_demo_4_title'] = 'Demo n°4';
 $lang['porg_demo_4_text'] = 'The fourth demo gallery uses Bootstrap darkroom with a hero image. The skin used is Cyborg but the blue from this theme we have changed to the colors of our hero image.';
 
---
+++ language/fr_FR/demo.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['porg_demo_page_title'] = 'Piwigo démos';
+$lang['porg_demo_into_text'] = 'Découvrez nos galeries de démonstration piwigo. Jetez un coup d\'œil et découvrez comment nos galeries sont personnalisables et découvrez de quoi notre technologie est capable.';
+$lang['porg_demo_video_support'] = 'Votre navigateur ne prend pas en charge le tag vidéo.';
+$lang['porg_demo_1_title'] = 'Demo n°1';
+$lang['porg_demo_1_text'] = 'Cette galerie de démonstration Piwigo est basée sur notre thème par défaut: Modus, avec l\'habillage Journal. Pour cette démo, nous avons ajouté une bannière et un bloc d\'introduction en utilisant le plugin PWG stuff.';
+$lang['porg_demo_2_title'] = 'Demo n°2';
+$lang['porg_demo_2_text'] = 'Notre deuxième galerie de démonstration Piwigo utilise notre thème bootstrap darkroom. Le thème de couleur ici est personnalisé pour être accordé avec l\'image du jumbotron que nous avons activé avec le thème.';
+$lang['porg_demo_3_title'] = 'Demo n°3';
+$lang['porg_demo_3_text'] = 'Pour la troisième galerie de démonstration Piwigo, nous utilisons Modus avec un skin personnalisé. Dans la bannière, nous avons simplement ajouté notre logo avec le titre de la galerie et utilisé les couleurs de ce logo spécifique dans la galerie.';
+$lang['porg_demo_4_title'] = 'Demo n°4';
+$lang['porg_demo_4_text'] = 'La quatrième galerie de démonstration utilise Bootstratp Darkroom avec une image héro. Le skin utilisée est Cyborg mais le bleu dans ce thème nous l\'avons changé pour l\'accorder avec notre image héro.';
+
+
+?>
