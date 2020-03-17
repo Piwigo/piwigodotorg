493c213d8326f921b7cb0c17b12e52f19271a4c7
Hannah <lilypickle@hotmail.fr>
2020-03-17 15:17:51 +0100

    Demo page EN text translation

---
+++ language/en_UK/demo.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['porg_demo_page_title'] = 'Piwigo demos';
+$lang['porg_demo_into_text'] = 'Discover our piwigo demo galleries. Have a look through them and see how personalisable our galleries our and find out what our technology is capable of.';
+$lang['porg_demo_video_support'] = 'Your browser does not support the video tag.';
+$lang['porg_demo_1_title'] = 'Demo n°1';
+$lang['porg_demo_1_text'] = 'This Piwigo demo gallery is based on our default theme: Modus, with the Newspaper skin. For this demo we have added a banner and an introduction block using the PWG stuff plugin.';
+$lang['porg_demo_2_title'] = 'Demo n°2';
+$lang['porg_demo_2_text'] = 'Our second Piwigo demo gallery uses our bootstrap darkroom theme. The color theme here has be personnalised to be accorded with the jumbotron image we have activated with the theme.';
+$lang['porg_demo_3_title'] = 'Demo n°3';
+$lang['porg_demo_3_text'] = 'For the third Piwigo demo gallery, we are using Modus width a personnalised skin. in the banner we have simply added our logo with the title of the gallery and used the colors of this specific logo in the gallery.';
+$lang['porg_demo_4_title'] = 'Demo n°4';
+$lang['porg_demo_4_text'] = 'The fourth demo gallery uses Bootstrap darkroom with a hero image. The skin used is Cyborg but the blue from this theme we have changed to the colors of our hero image.';
+
+
+?>
