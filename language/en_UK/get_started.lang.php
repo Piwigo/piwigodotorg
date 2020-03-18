c20f20a338a2f4aec644e88c0615910d9a8ed135
Hannah <lilypickle@hotmail.fr>
2020-03-18 14:45:21 +0100

    Get started page FR and EN missing translations

--- language/en_UK/get_started.lang.php
+++ language/en_UK/get_started.lang.php
@@ -3,10 +3,15 @@
 $lang['porg_get_started_desc1'] = 'How to run your self-hosting Piwigo, ie on your own hosting.';
 $lang['porg_get_started_desc2'] = 'What you need to prepare, what to download, how to install.';
 $lang['Requirements'] = 'Requirements';
+$lang['Discover Requirements'] = 'Discover the requirements';
 $lang['porg_get_started_requirement_desc1'] = 'What are the technical prerequisites to run Piwigo on your own hosting?';
 $lang['porg_get_started_requirement_desc2'] = 'PHP, database, web server, image processing... Find all details.';
+$lang['get-started-download'] = 'Download';
 $lang['porg_get_started_download_desc'] = 'Download Piwigo package including all files, or only the NetInstall script, which will download the package directly on your hosting.';
-$lang['Installation guide'] = 'Installation guide';
+$lang['Get-now'] = 'Get Piwigo now';
+$lang['Installation'] = 'Installation';
 $lang['porg_get_started_guide_desc'] = 'Learn how to install Piwigo on your hosting, from download and database credentials to first connection in your photo library.';
 $lang['Still got some unanswered questions ?'] = 'Still got some unanswered questions?';
 $lang['porg_get_started_question_desc1'] = 'You can get some help on the <a href="%s">forum</a> or find your own answers in the <a href="%s">documentation</a>';
+
+?>
