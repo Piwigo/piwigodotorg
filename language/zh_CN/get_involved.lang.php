e2696074d0081433ff85ae46e4aafcd2794c2af9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-24 07:37:39 +0100

    Update zh_CN, thanks to LuRenJiasWorld

---
+++ language/zh_CN/get_involved.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_involved_desc2'] = '贡献到Piwigo的方法多种多样，你可以根据你的能力或你的时间来自由决定。';
+$lang['porg_get_involved_dev_plugins_desc'] = 'Piwigo未能提供你所需要的功能？你可以将其开发成插件，发布到我们的插件市场，这样更多和你有相同需求的用户就能使用到你的作品！';
+$lang['porg_get_involved_donate_desc1'] = '想要帮助我们，但没有时间也没有足够的技术经验？';
+$lang['porg_get_involved_donate_desc2'] = '希望Piwigo项目能够健康且长久运营？';
+$lang['porg_get_involved_donate_desc3'] = '你可以考虑用金钱的方式赞助我们！';
+$lang['porg_get_involved_donate_desc4'] = 'Piwigo团队会将所有收集到的赞助费用直接用于piwigo.org服务器、域名、应用商店订阅(苹果AppStore)等地方。';
+$lang['porg_get_involved_donate_desc5'] = '支付方式：';
+$lang['porg_get_involved_grateful_desc1'] = '感谢你的无私贡献和积极参与，';
+$lang['porg_get_involved_grateful_desc2'] = '如果没有像你一样参与到Piwigo开发和社区中的用户们，Piwigo项目是无法像现在这样庞大的！';
+$lang['porg_get_involved_how_to_code_desc'] = '在GitHub上解决issues、对Piwigo进行二次开发... <br> ... 也可以提交pull-requests';
+$lang['porg_get_involved_improvment_desc'] = '使用社区功能来分享你的点子<br>或是如何让Piwigo变得更好';
+$lang['porg_get_involved_recommend_desc'] = '将Piwigo分享给你身边的人或集体<br>也可以分享到各大网站';
+$lang['porg_get_involved_security_desc1'] = '帮助我们来构建最安全的照片管理软件吧！';
+$lang['porg_get_involved_security_desc2'] = '如果你发现了Piwigo服务器或客户端中潜在的安全漏洞，请报告给我们。';
+$lang['porg_get_involved_translate_desc1'] = '将Piwigo翻译成你所使用的语言，或是对已有的翻译进行勘误与改善。';
+$lang['porg_get_involved_translate_desc2'] = 'Piwigo的核心和插件都可以被翻译。';
+$lang['Bugtracker'] = '故障跟踪系统';
+$lang['Coding documentation'] = '开发文档';
+$lang['Create a report'] = '报告安全问题';
+$lang['Develop plugins'] = '开发插件';
+$lang['Donate to Piwigo'] = '赞助Piwigo';
+$lang['Help the Community'] = '在社区提供帮助';
+$lang['Make this a monthly gift'] = '每月自动扣款';
+$lang['Recommend Piwigo'] = '推广Piwigo';
+$lang['Security Report'] = '安全报告';
+$lang['Submit bugs'] = '提交故障';
+$lang['Submit product improvment'] = '提交改善意见';
+$lang['Translate'] = '翻译';
+$lang['Translation tool'] = '翻译工具';
+$lang['We are grateful !'] = '真诚感谢你的支持！';
+$lang['You know how to code?'] = '有软件开发经验？';
+$lang['Your Gift'] = '你的捐赠';
+$lang['porg_get_involved_bugs_desc1'] = '在使用Piwigo的过程中遇到了故障？欢迎在我们的故障跟踪系统中报告！如果你不确定这是否是一个BUG，还可以在社区中先问一问。';
+$lang['porg_get_involved_bugs_desc2'] = '你需要描述这一BUG的·复现过程。';
+$lang['porg_get_involved_community_desc'] = '认识其他Piwigo用户<br>为社区的用户们提供帮助';
+$lang['porg_get_involved_desc1'] = 'Piwigo由一群活跃在社区中的用户和开发者们共同构建而成，力求创造一个完美的照片管理软件。';
