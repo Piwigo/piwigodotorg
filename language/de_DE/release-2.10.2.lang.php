3c5794ec06842f6713cb0ea25141a79d31d6d0fa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-29 10:03:58 +0200

    Update de_DE, thanks to e-dschungel

---
+++ language/de_DE/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1130'] = '[PHP-7.4] \"Trying to access array offset on value of type null\"';
+$lang['porg_issue_1133'] = '[admin menubar] Einträge nicht erreichbar, wenn die Höhe zu klein ist';
+$lang['porg_issue_1114'] = 'Admin, Fotoupload in Anwendungs-Tab';
+$lang['porg_issue_1134'] = '[PHP 7.4] array/string offset access deprecation Warnung';
+$lang['porg_issue_1150'] = '[group manager] Stored XSS Schwachstelle';
+$lang['porg_issue_1168'] = '[API] stored XSS mit pwg.images.setInfo';
+$lang['porg_issue_1095'] = 'groups ist ein reserviertes Schlüsselwort in MySQL 8';
+$lang['porg_issue_1102'] = 'Anzahl der Tags im Menü begrenzen';
+$lang['porg_issue_1112'] = 'Adminheader in zwei Zeilen auf kleineren Bildschirmen';
+$lang['porg_issue_1139'] = '[PHP 7.4] count() Warnung behoben';
+$lang['porg_issue_1119'] = 'Seite zum Passwortzurücksetzen nicht erreichbar, wenn noch kein Foto hinzugefügt';
+$lang['porg_issue_1151'] = '[PHP 7.4] Fixes zur Kompatibiltät';
+$lang['porg_issue_1155'] = '[PHP 7.4] Verwendung von implode deprecated';
+$lang['porg_issue_1162'] = '[users] Filtern nach Gruppe';
