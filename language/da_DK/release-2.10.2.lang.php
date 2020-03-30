766bd909d9db53a8da06732b8e01b59e9163c38d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-30 08:06:27 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1095'] = 'groups er et reserveret nøgleord i mysql 8';
+$lang['porg_issue_1102'] = 'begræns antallet af tags i menu';
+$lang['porg_issue_1112'] = 'Adminheader, to linjer på mindre skærme';
+$lang['porg_issue_1114'] = 'Admin, applikationsfane til upload af fotografier';
+$lang['porg_issue_1119'] = 'Siden til nulstilling af adgangskode var ikke tilgængelig når der endnu ikke var tilføjet et fotografi';
+$lang['porg_issue_1130'] = '[PHP-7.4] Prøver at tilgå arrayoffset med værdi af typen null';
+$lang['porg_issue_1133'] = '[admin-menubjælke] utilgængelige elementer når højden er for lav';
+$lang['porg_issue_1134'] = '[PHP 7.4] advarsel om at array-/strengoffset udfases';
+$lang['porg_issue_1139'] = '[PHP 7.4] Fix count()-advarsel';
+$lang['porg_issue_1150'] = '[gruppehåndtering] gemt XSS-sårbarhed';
+$lang['porg_issue_1151'] = '[PHP 7.4] kompabilitetsrettelser';
+$lang['porg_issue_1155'] = '[PHP 7.4] anvendelse af udfaset implode';
+$lang['porg_issue_1162'] = '[brugere] gruppefiltrering';
+$lang['porg_issue_1168'] = '[API] gemt XSS med pwg.images.setInfo';
