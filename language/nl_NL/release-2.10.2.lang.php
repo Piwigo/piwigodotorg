c2d5dd968b557fba205e2e10bbeba8bedfb39373
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-30 08:06:57 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1095'] = 'msql 8 groepen is een gereserveerd wachtwoord';
+$lang['porg_issue_1102'] = 'beperk het aantal tags in het menu';
+$lang['porg_issue_1112'] = 'Beheerder koptekst, twee regels op kleinere schermen';
+$lang['porg_issue_1114'] = 'Beheerder, tabblad upload foto-applicaties';
+$lang['porg_issue_1119'] = 'De pagina voor het opnieuw instellen van het wachtwoord is niet bereikbaar als er nog geen foto is toegevoegd';
+$lang['porg_issue_1130'] = '[PHP-7.4] \ "Probeert toegang te krijgen tot array-offset op waarde van type nul \"';
+$lang['porg_issue_1133'] = '[beheerder menubalk] onbereikbare items als de hoogte te klein is';
+$lang['porg_issue_1134'] = '[PHP 7.4] waarschuwing voor beëindiging van toegang tot array / string';
+$lang['porg_issue_1139'] = '[PHP 7.4] waarschuwing herstel telling () ';
+$lang['porg_issue_1150'] = '[groepsbeheerder] Opgeslagen XSS-kwetsbaarheid';
+$lang['porg_issue_1151'] = '[PHP 7.4] reparaties voor compatibiliteit';
+$lang['porg_issue_1155'] = '[PHP 7.4] Vervallen gebruik van implode';
+$lang['porg_issue_1162'] = '[gebruikers] filteren op groep';
+$lang['porg_issue_1168'] = '[API] heeft XSS opgeslagen met pwg.images.setInfo';
