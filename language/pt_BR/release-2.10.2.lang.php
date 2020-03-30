93f20eb360614e89dcf832785459438530daf011
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-30 17:01:28 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1095'] = 'mysql 8 groups é uma palavra-chave reservada';
+$lang['porg_issue_1102'] = 'limite o número de guias no menu';
+$lang['porg_issue_1112'] = 'Guia do Admin, duas linhas em telas menores';
+$lang['porg_issue_1114'] = 'Admin, faça o envio da guia de aplicativos de fotos';
+$lang['porg_issue_1119'] = 'A página de redefinição de senha não está acessível quando nenhuma foto ainda não foi adicionada';
+$lang['porg_issue_1130'] = '[PHP-7.4] \ "Tentando acessar o deslocamento da matriz no valor do tipo nulo \"';
+$lang['porg_issue_1133'] = '[admin menubar] itens inacessíveis quando a altura é muito pequena';
+$lang['porg_issue_1134'] = '[PHP 7.4] aviso de reprovação de acesso de deslocamento de matriz/ sequência';
+$lang['porg_issue_1139'] = '[PHP 7.4] Aviso de correção de contagem()';
+$lang['porg_issue_1150'] = '[gerente de grupo] Vulnerabilidade XSS armazenada';
+$lang['porg_issue_1151'] = '[PHP 7.4] correções para compatibilidade';
+$lang['porg_issue_1155'] = '[PHP 7.4] Uso preterido de implodir';
+$lang['porg_issue_1162'] = '[usuários] filtrar por grupo';
+$lang['porg_issue_1168'] = '[API] armazenou XSS com pwg.images.setInfo';
