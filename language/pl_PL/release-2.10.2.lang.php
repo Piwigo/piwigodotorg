b12a68197ac582e73c37ab029142b56e5d66bbb9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-30 08:06:41 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-2.10.2.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1095'] = 'groups jest zarezerwowanym słowem w mysql 8';
