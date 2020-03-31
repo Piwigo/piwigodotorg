a0cffc0b8c1c01e7ca146927ae174541ed4ed0bf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-31 08:08:03 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1095'] = 'La palabra groups está reservada en mysql 8';
+$lang['porg_issue_1102'] = 'limitar el númer de etiquetas en el menú';
+$lang['porg_issue_1112'] = 'Cabecera de administrador, dos líneas en pantallas más pequeñas';
+$lang['porg_issue_1114'] = 'Administrador, pestaña de aplicaciones de carga de fotos';
+$lang['porg_issue_1119'] = 'Página de restablecimiento de contraseña no alcanzable cuando aún no se ha añadido ninguna foto';
+$lang['porg_issue_1130'] = '[PHP-7.4] \"Intentando acceder a un desplazamiento de matriz con un valor de tipo null\"';
+$lang['porg_issue_1133'] = '[barra de menús del administrador] elementos inalcanzables cuando la altura es demasiado baja';
+$lang['porg_issue_1134'] = '[PHP 7.4] advertencia de obsolescencia del acceso por desplazamiento en matrices/cadenas';
+$lang['porg_issue_1139'] = '[PHP 7.4] Corregir la advertencia de count()';
+$lang['porg_issue_1150'] = '[gestor de grupos] vulnerabilidad en el XSS almacenado';
+$lang['porg_issue_1151'] = '[PHP 7.4] correcciones para compatibilidad';
+$lang['porg_issue_1155'] = '[PHP 7.4] Uso obsoleto de implode';
+$lang['porg_issue_1162'] = '[usuarios] filtrar por grupo';
+$lang['porg_issue_1168'] = '[API] XSS almacenado con pwg.images.setInfo';
