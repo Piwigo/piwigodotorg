6a85e4cd4e6101e39b4d270929f44104ac52f5ec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-04 08:17:26 +0200

    Update pl_PL, thanks to mjp

---
+++ language/pl_PL/get_involved.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_get_involved_security_desc1'] = 'Pomóż nam tworzyć najbezpieczniejszy system zarządzania zdjęciami.';
+$lang['porg_get_involved_donate_desc5'] = 'Płać za pomocą';
+$lang['porg_get_involved_grateful_desc1'] = 'Dziękujemy za wsparcie i Twój udział.';
+$lang['Bugtracker'] = 'Śledzenie błędów';
+$lang['Create a report'] = 'Utwórz raport';
+$lang['Develop plugins'] = 'Stwórz dodatek';
+$lang['Donate to Piwigo'] = 'Wspomóż Piwigo';
+$lang['Help the Community'] = 'Pomóż Wspólnocie';
+$lang['Make this a monthly gift'] = 'Utwórz comiesięczną dotację';
+$lang['Recommend Piwigo'] = 'Polecaj Piwigo';
+$lang['Security Report'] = 'Raport bezpieczeństwa';
+$lang['Submit bugs'] = 'Wyślij błędy';
+$lang['Submit product improvment'] = 'Wyślij poprawkę';
+$lang['Translate'] = 'Tłumacz';
+$lang['Translation tool'] = 'Narzędzia do tłumaczenia';
+$lang['We are grateful !'] = 'Dziękujemy!';
+$lang['You know how to code?'] = 'Znasz się na programowaniu?';
+$lang['Your Gift'] = 'Twoja dotacja';
+$lang['porg_get_involved_bugs_desc1'] = 'Znalazłeś błąd w Piwigo? Wyślij go w naszym systemie śledzenia błędów... lub podziel się na forum jeżeli nie jesteś pewien.';
+$lang['porg_get_involved_bugs_desc2'] = 'Opisz jak powtórzyć sytuację błędu';
