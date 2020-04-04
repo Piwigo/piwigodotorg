98f894a567c407c2804f4f8cecea0aa868d5b73f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-04 08:17:56 +0200

    Update tr_TR, thanks to Nakre

---
+++ language/tr_TR/release-2.10.1.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1089'] = 'Jshrink kullanımdan kaldırılmış işlevi kullanır, javascript\'i bozmaktadır';
+$lang['porg_issue_1090'] = 'kontrol panelinde eksik sağlama toplamı uyarısı kullanıcıları korkutabilir';
+$lang['porg_issue_1092'] = 'Hata: Syntax error, unrecognized expression: input[name=pwg_token';
+$lang['porg_issue_1097'] = '[batch manager] checksum computation blocksize should stay the same';
+$lang['porg_issue_1098'] = '[batch manager] checksum computation, reduce block size';
+$lang['porg_issue_1099'] = '[batch manager] TypeError: associated_categories is null';
