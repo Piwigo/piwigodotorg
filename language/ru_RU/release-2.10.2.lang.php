471a8f05b568dc58fa88054a91d200cd73f27f1f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-04 14:25:56 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1095'] = 'mysql 8 groups is a reserved keyword';
+$lang['porg_issue_1102'] = 'ограничено число тэгов меню';
+$lang['porg_issue_1112'] = 'Admin Header, two lines on smaller screens';
+$lang['porg_issue_1114'] = 'Admin, upload photos applications tab';
+$lang['porg_issue_1119'] = 'Страница сброса пароля недоступна, пока изображение еще не добавлено';
+$lang['porg_issue_1130'] = '[PHP-7.4] \"Trying to access array offset on value of type null\"';
+$lang['porg_issue_1133'] = '[admin menubar] недоступные предметы, когда высота слишком мала';
+$lang['porg_issue_1134'] = '[PHP 7.4] array/string offset access deprecation warning';
+$lang['porg_issue_1139'] = '[PHP 7.4] Fix count() warning';
+$lang['porg_issue_1150'] = '[group manager] Stored XSS vulnerability';
+$lang['porg_issue_1151'] = '[PHP 7.4] fixes for compatibility';
+$lang['porg_issue_1155'] = '[PHP 7.4] Deprecated usage of implode';
+$lang['porg_issue_1162'] = '[users] фильтр по группам';
+$lang['porg_issue_1168'] = '[API] stored XSS with pwg.images.setInfo';
