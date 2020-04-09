aa72f85b932396df56de02a86c80812920cab39e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-09 08:21:52 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-2.10.2.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1151'] = '[PHP 7.4] correzioni di compatibilità';
+$lang['porg_issue_1155'] = '[PHP 7.4] Uso obsoleto della funzione di implosione';
+$lang['porg_issue_1162'] = '[utenti] filtraggio per gruppo';
+$lang['porg_issue_1168'] = '[API] XSS persistente su pwg.images.setInfo';
+$lang['porg_issue_1150'] = '[gruppi] XSS persistente';
+$lang['porg_issue_1139'] = '[PHP 7.4] correzione avviso su count()';
+$lang['porg_issue_1134'] = '[PHP 7.4] avviso \"array/string offset access deprecation\"
+';
+$lang['porg_issue_1133'] = '[admin] menu inaccessibili quando la finestra è troppo piccola';
+$lang['porg_issue_1130'] = '[PHP-7.4] \"Trying to access array offset on value of type null\"';
+$lang['porg_issue_1114'] = '[admin] ritorna l\'app Android';
+$lang['porg_issue_1119'] = 'Pagina di reimpostazione della password non raggiungibile quando non è stata ancora aggiunta alcuna foto';
+$lang['porg_issue_1112'] = '[admin] header, evita l\'interruzione di riga';
+$lang['porg_issue_1102'] = 'limitare il numero di tag nel menu';
+$lang['porg_issue_1095'] = 'La parola \"gruppi\" è riservata su mysql versione 8';
