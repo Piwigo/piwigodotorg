106ac27950fe16bf245a5ee808698dc1a8da5e3a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-16 08:49:49 +0200

    Update pl_PL, thanks to yareq

--- language/pl_PL/get_started.lang.php
+++ language/pl_PL/get_started.lang.php
@@ -7,7 +7,6 @@
 // +-----------------------------------------------------------------------+
 $lang['porg_get_started_guide_desc'] = 'Dowiedz się jak zainstalować Piwigo na swoim serwerze: od pobrania i poświadczeń bazy danych, do pierwszego połączenia z biblioteką zdjęć.';
 $lang['Still got some unanswered questions ?'] = 'Nadal nie znasz wszystkich odpowiedzi?';
-$lang['Installation guide'] = 'Instrukcja instalacji';
 $lang['Requirements'] = 'Wymagania';
 $lang['porg_get_started_desc1'] = 'Jak uruchomić Piwigo na własnym serwerze.';
 $lang['porg_get_started_desc2'] = 'Co musisz przygotować, co pobrać i jak zainstalować.';
@@ -15,3 +14,7 @@ $lang['porg_get_started_download_desc'] = 'Pobierz pakiet Piwigo zawierający ws
 $lang['porg_get_started_question_desc1'] = 'Możesz otrzymać pomoc na <a href="%s">forum</a> lub poszukać odpowiedzi w <a href="%s">documentacji</a>';
 $lang['porg_get_started_requirement_desc1'] = 'Jakie są wymagania techniczne do uruchomienia Piwigo na własnym serwerze?';
 $lang['porg_get_started_requirement_desc2'] = 'PHP, baza danych, serwer WWW, procesory obrazu ... Znajdź wszystkie szczegóły.';
+$lang['get-started-download'] = 'Pobierz';
+$lang['Installation'] = 'Instalacja';
+$lang['Get-now'] = 'Pobierz Piwigo teraz';
+$lang['Discover Requirements'] = 'Odkryj wymagania';
