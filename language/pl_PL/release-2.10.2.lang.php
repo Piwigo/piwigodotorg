731db2d03cbbc8faaef032d8f58e361f0ac235fa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-16 08:50:03 +0200

    Update pl_PL, thanks to yareq

--- language/pl_PL/release-2.10.2.lang.php
+++ language/pl_PL/release-2.10.2.lang.php
@@ -6,3 +6,16 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['porg_issue_1095'] = 'groups jest zarezerwowanym słowem w mysql 8';
+$lang['porg_issue_1112'] = 'Admin: dwie linie nagłówka na mniejszych ekranach';
+$lang['porg_issue_1134'] = '[PHP 7.4] ostrzeżenie o rezygnacji z dostępu do przesunięcia tablicy / łańcucha';
+$lang['porg_issue_1139'] = '[PHP 7.4] Poprawka ostrzeżenia count()';
+$lang['porg_issue_1150'] = '[menedżer grup] Podatność Stored XSS';
+$lang['porg_issue_1151'] = '[PHP 7.4] poprawki zgodności';
+$lang['porg_issue_1155'] = '[PHP 7.4] Przestarzałe użycie implode';
+$lang['porg_issue_1162'] = '[użytkownicy] filtrowanie według grup';
+$lang['porg_issue_1168'] = '[API] Stored XSS w pwg.images.setInfo';
+$lang['porg_issue_1130'] = '[PHP-7.4] \"Trying to access array offset on value of type null\"';
+$lang['porg_issue_1133'] = '[menu admin] niedostępne pozycje przy zbyt małej wysokości';
+$lang['porg_issue_1114'] = 'Admin: zakładka aplikacji na stronie wgrywania zdjęć';
+$lang['porg_issue_1119'] = 'Niedostępna strona resetowania hasła, jeśli nie dodano jeszcze zdjęcia';
+$lang['porg_issue_1102'] = 'Ograniczenie liczby znaczników w menu';
