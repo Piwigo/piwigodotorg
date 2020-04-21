0232e5907f75ca3df4c682b2a8579f81423927e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-21 09:44:52 +0200

    Update zh_CN, thanks to LuRenJiasWorld

---
+++ language/zh_CN/release-2.9.5.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_902'] = '[2.9.4]用户列表不再支持分页或过滤';
+$lang['porg_issue_945'] = 'pwg_log不再检查$page["section"]的值是否在已有字段列表中';
+$lang['porg_issue_946'] = '修复了pwg_log中对于陌生字段的不当处理';
+$lang['porg_issue_972'] = '[相册]解决了当照片过多时修订页面的性能问题';
+$lang['porg_issue_988'] = '避免使用strpos()函数在PHP7.3环境下的Deprecated警告';
+$lang['porg_issue_998'] = '修复了Piwigo2.9与Piwigo Remote Sync的不兼容问题';
