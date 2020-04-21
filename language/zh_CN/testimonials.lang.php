f3372fef5cdbffa938f468228d5a801dad07cb7e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-21 09:45:06 +0200

    Update zh_CN, thanks to LuRenJiasWorld

---
+++ language/zh_CN/testimonials.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_testimonials_desc1'] = '来看看用户们如何评价Piwigo！';
+$lang['porg_testimonials_desc2'] = '你可以在这里看到很多经典案例，了解用户们是如何使用Piwigo的。';
+$lang['porg_testimonials_share_desc1'] = '同样欢迎你<a href="%s">分享</a>你与Piwigo的故事';
+$lang['porg_testimonials_share_title'] = '我们为拥有这样庞大的社区而感到由衷自豪！';
