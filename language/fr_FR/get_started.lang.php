251a8e354d3cc56317d178a1308822569eb7f49a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-23 11:15:55 +0200

    Update fr_FR, thanks to ddtddt

--- language/fr_FR/get_started.lang.php
+++ language/fr_FR/get_started.lang.php
@@ -8,10 +8,9 @@ $lang['porg_get_started_requirement_desc1'] = 'Quels sont les prérequis techniq
 $lang['porg_get_started_requirement_desc2'] = 'PHP, base de données, serveur web, traitement d\'image... Voir les détails.';
 $lang['get-started-download'] = 'Télécharger';
 $lang['porg_get_started_download_desc'] = 'Télécharger le paquet Piwigo incluant tous les fichiers, ou simplement le script NetInstall, qui s\'occupera de télécharger le paquet directement sur votre hébergement.';
-$lang['Installation guide'] = 'Guide d\'installation';
 $lang['Get-now'] = 'Obtenir Piwigo';
 $lang['porg_get_started_guide_desc'] = 'Apprenez à installer Piwigo : du téléchargement à la première connexion sur votre photothèque en passant par le paramètrage de la base de données.';
 $lang['Still got some unanswered questions ?'] = 'Encore des questions en suspens ?';
 $lang['porg_get_started_question_desc1'] = 'Vous pouvez obtenir de l\'aide sur le <a href="%s">forum</a> ou trouver vos propres réponses dans la <a href="%s">documentation</a>';
 
-?>
+$lang['Installation'] = 'Installation';
