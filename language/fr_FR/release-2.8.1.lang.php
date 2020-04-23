6939cb45fb0e5b00f8bf9b42173423089f70d87e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-23 11:15:42 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-2.8.1.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_436'] = 'Appel  à une méthode non définie : Logger::warning in i.php';
+$lang['porg_issue_442'] = 'Appel à une fonction non définie : set_cat_status() ';
+$lang['porg_issue_470'] = 'Augmenté le caractère aléatoire de generate_key. Une vulnérabilité a été signalée par <a href="https://www.danclifford.eu/">Dan Clifford</a>. Un grand merci à lui.';
+$lang['porg_issue_466'] = '[Gestion par lot] nouvel album non listé quand on déplace des photos';
+$lang['porg_issue_443'] = 'Échec de la création de tables avec MySQL 5.7';
+$lang['porg_issue_440'] = 'La recherche (avec le thème smartpocket) ne renvoie aucun résultat';
+$lang['porg_issue_434'] = 'Constructeur obsolète pour LocalSiteReader';
+$lang['porg_issue_439'] = 'mise à jour de la documentation intégrée pour décrire le format multiple';
