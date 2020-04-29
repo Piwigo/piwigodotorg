9e5c31173b889bbbc62e87dd4ee36e858ccd989c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-29 13:28:20 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-2.8.6.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_602'] = 'jeton de section non valide [look_like_gbo2]';
+$lang['porg_issue_600'] = 'Attaque par scripts intersites lors du téléversement d\'images, signalé par <a href="https://github.com/boogyman2">boogyman2</a>';
+$lang['porg_issue_599'] = 'Correction de la classe smtp de PHPMailer se chargeant lors de l\'utilisation du protocole SMTP';
