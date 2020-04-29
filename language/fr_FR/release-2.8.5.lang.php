2ca75cb64fbec00c8e6bee5924def69fb2211eae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-29 13:28:34 +0200

    Update fr_FR, thanks to laurent.duretz

---
+++ language/fr_FR/release-2.8.5.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_572'] = 'CVE-2016-10084 File Inclusion Attack, reporté par <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_574'] = 'File Inclusion with Possible RCE, reporté par <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_573'] = 'CVE-2016-10085 File Inclusion Attack #2, reporté par <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_575'] = 'CVE-2016-10083 Cross Site Scripting, reporté par <a href="https://github.com/Shinkurt">Shinkurt</a>';
+$lang['porg_issue_599'] = 'fixer le chargement de la classe PHPMailer smtp quand on utilise le protocole SMTP';
+$lang['porg_issue_595'] = 'mettre à jour PHPMailer en 5.2.21';
