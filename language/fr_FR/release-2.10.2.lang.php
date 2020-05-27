8493b02a47d7d198a16aefe86f59609ab288b513
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-05-27 15:02:40 +0200

    Update fr_FR, thanks to plg

--- language/fr_FR/release-2.10.2.lang.php
+++ language/fr_FR/release-2.10.2.lang.php
@@ -4,13 +4,12 @@ $lang['porg_issue_1162'] = '[utilisateurs] filtrage par groupe';
 $lang['porg_issue_1150'] = '[groupes] XSS persistente';
 $lang['porg_issue_1133'] = '[admin] menus inaccessibles quand la fenêtre est trop petite';
 $lang['porg_issue_1119'] = 'La page de réinitialisation de mot de passe est inaccessible si aucune photo dans la galerie';
-$lang['porg_issue_1114'] = '[admin] retour de l\'application Android';
+$lang['porg_issue_1114'] = '[admin] retour de l\'app mobile Android';
 $lang['porg_issue_1112'] = '[admin] en-tête, éviter le retour à la ligne';
-$lang['porg_issue_1102'] = 'limiter le nombre de tags dans le menu';
+$lang['porg_issue_1102'] = 'limiter le nombre de mots-clef dans le menu';
 $lang['porg_issue_1095'] = 'Le mot \"groups\" est réservé sur mysql version 8';
 $lang['porg_issue_1155'] = '[PHP 7.4] Usage déprécié de la fonction implode';
 $lang['porg_issue_1151'] = '[PHP 7.4] corrections de compatibilité';
 $lang['porg_issue_1139'] = '[PHP 7.4] correction avertissement sur count()';
 $lang['porg_issue_1134'] = '[PHP 7.4] avertissement \"array/string offset access deprecation\"';
 $lang['porg_issue_1130'] = '[PHP-7.4] \"Trying to access array offset on value of type null\"';
-?>
