0f3be0a03098880d2df39cbdb2f73c6c2ba44434
Hannah <lilypickle@hotmail.fr>
2020-05-28 14:14:29 +0200

    EN text for requirements page

---
+++ language/en_UK/requirements.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['porg_guides'] = 'Guides';
+$lang['porg_guides_installation_instruction_title'] = 'Piwigo installation instructions';
+$lang['porg_guides_requirements'] = 'Requirements';
+$lang['porg_guides_requirements_paragraph'] = 'For a professional photo gallery solution with "all inclusive" service (installation, hosting, backup) or if you simply want to try Piwigo without installation, you can create a free trial account at Piwigo.com .';
+
+?>
