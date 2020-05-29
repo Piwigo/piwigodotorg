3cb5853fac8e2d8468709b8cbfda4a4486f0f74d
Hannah <lilypickle@hotmail.fr>
2020-05-29 10:17:45 +0200

    EN language file for netinstall page

---
+++ language/en_UK/netinstall.lang.php
@@ -0,0 +1,47 @@
+<?php
+
+$lang['porg_guides'] = 'Guides';
+$lang['porg_guides_installation_instruction_title'] = 'Piwigo installation instructions';
+$lang['porg_guides_netinstall'] = 'Netinstall';
+$lang['porg_guides_netinstall_step1'] = 'Step 1 - Download the NetInstall script';
+$lang['porg_guides_netinstall_step2'] = 'Step 2 - Upload the NetInstall script to your web server';
+$lang['porg_guides_netinstall_step3'] = 'Step 3 - Configuration';
+$lang['porg_guides_netinstall_step4'] = 'Step 4 - First connection';
+$lang['porg_guides_netinstall_step4'] = 'Step 4 - First connection';
+$lang['porg_guides_netinstall_paragraph1'] = 'With NetInstall you only have to upload a single file to your web server. This script then loads the Piwigo archive from piwigo.org onto your web server, unpacks it and starts the installation process.';
+$lang['porg_guides_netinstall_info1'] = 'The NetInstall process does not work on every server, depending on which features your hoster provides. If it doesn\'t work for you, just switch to manual installation.';
+$lang['porg_guides_netinstall_step1_paragraph1'] = 'Download netinstall';
+$lang['porg_guides_netinstall_info2'] = 'The Piwigo team recommends FileZilla as an FTP client because, just like Piwigo, it is free and compatible with Windows and Linux.';
+$lang['porg_guides_netinstall_step2_paragraph1'] = 'Start FileZilla and fill in the access data that your hoster gave you:';
+$lang['porg_guides_netinstall_step2_list1'] = 'Host';
+$lang['porg_guides_netinstall_step2_list2'] = 'Username';
+$lang['porg_guides_netinstall_step2_list3'] = 'Password';
+$lang['porg_guides_netinstall_step2_paragraph2'] = 'Click the Quickconnect button. You are now connected to your web server.';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Select piwigo-netinstall.php on your hard drive and upload it to the web server.';
+$lang['porg_guides_netinstall_image_caption1'] = 'Connect upload';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'When you have uploaded the file, call it up in your browser. For example, the address should be';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Select a folder name for your photo gallery on your website. By default, Piwigo is installed in the piwigo folder . If you leave the field empty, Piwigo is automatically installed in the folder in which the NetInstall script is located.';
+$lang['porg_guides_netinstall_image_caption2'] = 'Choose a folder name for your photo gallery';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Click "Retrieve and unzip Piwigo" to start the action, and then "Install Piwigo now"';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo was downloaded successfully';
+$lang['porg_guides_netinstall_step3_paragraph4'] = 'Now we come to the MySQL database settings and the webmaster access data to administer the gallery';
+$lang['porg_guides_netinstall_image_caption4'] = 'Installation side';
+$lang['porg_guides_netinstall_step3_paragraph5'] = 'Enter the MySQL access data that you received from your host';
+$lang['porg_guides_netinstall_step3_list1'] = 'MySQL host';
+$lang['porg_guides_netinstall_step3_list2'] = 'User';
+$lang['porg_guides_netinstall_step3_list2_span'] = 'Attention, each hoster has different FTP settings and different ways to connect to the MySQL database.';
+$lang['porg_guides_netinstall_step3_list3'] = 'Password';
+$lang['porg_guides_netinstall_step3_list4'] = 'Name of the database';
+$lang['porg_guides_netinstall_step3_list5'] = 'A table prefix';
+$lang['porg_guides_netinstall_step3_list5_span'] = 'Most hosts only provide one database per customer, but you can create as many tables in a database as you want. In order to avoid conflicts with other applications that use the same database or if you work with several Piwigo installations, a prefix is​used for the tables. By default this is called "piwigo_". You can change this prefix, but only alphanumeric characters may be used.';
+$lang['porg_guides_netinstall_step3_paragraph6'] = 'The following is required to create the webmaster account';
+$lang['porg_guides_netinstall_step3_list6'] = 'A user name of your choice';
+$lang['porg_guides_netinstall_step3_list7'] = 'A password which has to be entered twice for verification';
+$lang['porg_guides_netinstall_step3_list8'] = 'Your email address so users can contact you';
+$lang['porg_guides_netinstall_step3_paragraph7'] = 'Now click "Start the installation" to begin.';
+$lang['porg_guides_netinstall_step3_paragraph8'] = 'You will be informed whether the installation has been carried out correctly or whether errors have occurred.';
+$lang['porg_guides_netinstall_image_caption5'] = 'Correct installation';
+$lang['porg_guides_netinstall_step3_paragraph9'] = 'When the installation is complete, you can go to your gallery. When you log in with your webmaster account, you can access the administration pages.';
+$lang['porg_guides_netinstall_image_caption6'] = 'Piwigo is installed';
+
+?>
