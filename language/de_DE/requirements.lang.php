97bb4c35db3288c898cb6af5fbf26dc4e491d57e
Hannah <lilypickle@hotmail.fr>
2020-06-07 23:01:48 +0200

    Requirements lang file
    FR and DE lang file

---
+++ language/de_DE/requirements.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['porg_guides_requirements_paragraph'] = 'Für eine professionelle Foto-Galerie-Lösung mit „all inclusive“ Service (Installation, Hosting, Sicherung) oder wenn Sie Piwigo einfach ohne Installation ausprobieren wollen, <a href="%s">können Sie einen kostenlosen Probeaccount auf Piwigo.com erstellen./a>.';
+$lang['porg_guides_requirements_minimal_first'] = 'Einen Webserver wie Nginx oder Apache';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 oder eine entsprechende MariaDB';
+
+?>
---
+++ language/fr_FR/requirements.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['porg_guides_requirements_paragraph'] = 'Piwigo a besoin d\'un hébergement web pour fonctionner. Pour une solution de galerie photo “clef en main” (installation, hébergement, mises à jour, sauvegardes…), ou si vous souhaitez simplement essayer Piwigo sans l\'installer, <a href="%s">ouvrez un compte d\'essai gratuit sur Piwigo.com.</a>.';
+$lang['porg_guides_requirements_minimal'] = 'Le minimum requis';
+$lang['porg_guides_requirements_minimal_first'] = 'un serveur web comme Apache ou Nginx';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (ou l\'équivalent avec MariaDB)';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (version 7.2 recommandée, pour ses performances)';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Une bibliothèque graphique: ImageMagick est recommandé pour ses performances et sa qualité d\'image mais GD, souvent fourni avec PHP, peut également faire l\'affaire.';
+$lang['porg_guides_requirements_minimal_fifth'] = 'un logiciel client FTP sera nécessaire pour télécharger les fichiers (netinstall ou package complet): l\'équipe de Piwigo recommande FileZilla comme logiciel client FTP, car il est gratuit comme Piwigo et compatible avec Windows, Mac et Linux.';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Assez d\'espace disque pour vos photos: en plus des photos que vous téléchargez, Piwigo stockera les "tailles multiples" dans un répertoire cache de votre serveur.';
+$lang['porg_guides_requirements_optional'] = 'Prérequis optionnels';
+$lang['porg_guides_requirements_optional_first'] = 'exiftool est requis pour le plugin Write Metadata et d\'autres plugins qui manipulent les métadonnées EXIF/IPTC';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg est requis pour le plugin VideoJS lors de la création du “poster” (image représentant la vidéo)';
+$lang['porg_guides_requirements_optional_third'] = 'jpegtran est recommandé pour le plugin RotateImage pour tourner les images sans perte de qualité';
+$lang['porg_guides_requirements_optional_third'] = 'pdftoppm sera recommandé à partir de Piwigo 2.10 pour générer les images représentant les PDFs (au lieu d\'ImageMagick pour cette tâche précise)';
+
+?>
