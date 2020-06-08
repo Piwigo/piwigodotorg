8bfae6d5431ce15ad647aeb5f1e9aca124051fce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-08 17:28:27 +0200

    Update fr_FR, thanks to plg

--- language/fr_FR/release-2.10.1.lang.php
+++ language/fr_FR/release-2.10.1.lang.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 $lang['porg_issue_1090'] = 'l\'alerte sur les checksums manquant devrait effrayer les utilisateurs';
 $lang['porg_issue_1089'] = 'Jshrink utilise des fonctions dépréciées et arrête le javascript ';
-$lang['porg_issue_1092'] = 'Erruer: Erreur de syntaxe, expression non reconnue : input[name=pwg_token';
+$lang['porg_issue_1092'] = 'Erreur: Erreur de syntaxe, expression non reconnue : input[name=pwg_token';
 $lang['porg_issue_1099'] = '[batch manager] TypeError : associated_categories est null';
 $lang['porg_issue_1098'] = '[batch manager] calcul du checksum, réduire la taille du block';
-$lang['porg_issue_1097'] = '[batch manager] calcul du checksum, la taille du block devrait être la même';
+$lang['porg_issue_1097'] = '[batch manager] calcul du checksum, la taille du bloc devrait être la même';
