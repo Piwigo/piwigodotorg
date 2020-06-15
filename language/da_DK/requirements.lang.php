96ae4207fc1aeeab10451c67d1a2434a75972e22
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-15 21:14:46 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/requirements.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_requirements_minimal_first'] = 'En webserver som Nginx eller Apache';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Et grafisk bibliotek: ImageMagick anbefales pga. dets ydeevne og billedkvalitet, men GD, som ofte installeres med PHP, klarer det også.';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (eller tilsvarende med MariaDB)';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Tilstrækkelig diskplads til dine billeder: ud over de billeder, du overfører, gemmer Piwigo også forskellige størrelser i din servers cachemappe.';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (7.0 eller 7.1 anbefales)';
+$lang['porg_guides_requirements_optional'] = 'Valgfrie krav';
+$lang['porg_guides_requirements_optional_first'] = 'exiftool kræves af plugin\'en Write Metadata og enhver anden plugin, som håndterer EXIF-/IPTC-metadata';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg er krævet af plugin\'en VideoJS for at oprette videoplakaten';
+$lang['porg_guides_requirements_optional_third'] = 'pdftoppm anbefales for at generere en plakat fra PDF-filer (i stedet for ImageMagick)';
+$lang['porg_guides_requirements_paragraph'] = 'Piwigo kræver webhosting for at fungere. For en fotogalleriløsning med alt hvad der følger med (installering, hosting, sikkerhedskopier), eller hvis du blot ønsker at prøve Piwigo uden at installere, kan du overveje at <a href="%s">åbne en gratis prøvekonto på Piwigo.com</a>.';
+$lang['porg_guides_requirements_minimal'] = 'Minimumskrav';
+$lang['porg_guides_requirements_minimal_fifth'] = 'et FTP-klientprogram er krævet for at overføre filerne  (netinstall eller komplet pakke): Piwigo-holdet anbefaler  FileZilla som FTP-klientprogram, fordi det som Piwigo er frit tilgængeligt, og er kompatibelt med Windows, Mac og Linux.';
