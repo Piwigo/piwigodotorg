474901e27cb5adec3b9d58e054c2e0eb1e9ace21
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-15 21:14:31 +0200

    Update en_UK, thanks to Kaare

--- language/en_UK/requirements.lang.php
+++ language/en_UK/requirements.lang.php
@@ -7,12 +7,10 @@ $lang['porg_guides_requirements_minimal_first'] = 'A web server like Nginx or Ap
 $lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (or the equivalent with MariaDB)';
 $lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (recommended 7.0 or 7.1)';
 $lang['porg_guides_requirements_minimal_fourth'] = 'A graphic library: ImageMagick is recommended for its performances and image quality but GD, often bundled with PHP, can also do the job.';
-$lang['porg_guides_requirements_minimal_fifth'] = 'an FTP client software will be required to upload the files (netinstall or full package) : Piwigo team recommends FileZilla as FTP client software, because it is free as Piwigo is, and compatible with Windows, Mac and Linux.';
-$lang['porg_guides_requirements_minimal_sixth'] = 'Enough disk space for your pictures : in addition to the photos you upload, Piwigo will store the "multiple sizes" in a cache directory of your server.';
+$lang['porg_guides_requirements_minimal_fifth'] = 'an FTP client software will be required to upload the files (netinstall or full package): Piwigo team recommends FileZilla as FTP client software, because it is free as Piwigo is, and compatible with Windows, Mac and Linux.';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Enough disk space for your pictures: in addition to the photos you upload, Piwigo will store the "multiple sizes" in a cache directory of your server.';
 $lang['porg_guides_requirements_optional'] = 'Optional Requirements';
 $lang['porg_guides_requirements_optional_first'] = 'exiftool is required for plugin Write Metadata or any other plugin dealing with EXIF/IPTC metadata';
 $lang['porg_guides_requirements_optional_second'] = 'ffmpeg is required for plugin VideoJS to create video poster';
 $lang['porg_guides_requirements_optional_third'] = 'jpegtran is recommended for plugin RotateImage to rotate images with no compression';
 $lang['porg_guides_requirements_optional_third'] = 'pdftoppm will be recommended to generate poster from PDF files (instead of ImageMagick)';
-
-?>
