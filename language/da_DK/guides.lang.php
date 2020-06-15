4f46e68c0613394b9ebbb36ae24ac0788b0b2c44
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-15 21:15:16 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = 'Velkommen til Piwigo-vejledninger. Her finder du de grundlæggende oplysninger om hvordan man installerer og opdaterer din Piwigo. Nogen hjælp er også tilgængelig direkte i din Piwigo med “Hjælp mig!”-siderne i administrationspanelet eller over alt hvor du ser et grafisk spørgsmålstegn. En wiki dækker desuden udviklerdokumentationen.';
