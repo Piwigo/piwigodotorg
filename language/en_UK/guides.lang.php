7e53b7017b8ee37f682a92c5294c13f01e9bb225
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-15 21:15:01 +0200

    Update en_UK, thanks to Kaare

--- language/en_UK/guides.lang.php
+++ language/en_UK/guides.lang.php
@@ -1,5 +1,3 @@
 <?php
 
-$lang['porg_guides_intro'] = 'Welcome to Piwigo Guides. Here you will find the basics about how to install and update your Piwigo. Some help is also provided directly in your Piwigo with “Help me!” pages of the administration pannel or anywhere you see a graphical question mark. A wiki also covers the developer documentation.';
-
-?>
+$lang['porg_guides_intro'] = 'Welcome to Piwigo Guides. Here you will find the basics about how to install and update your Piwigo. Some help is also provided directly in your Piwigo with “Help me!” pages of the administration panel or anywhere you see a graphical question mark. A wiki also covers the developer documentation.';
