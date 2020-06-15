983addf15e6dc9ce63fea2bfb5b230ad9d1b7475
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-15 21:15:30 +0200

    Update pl_PL, thanks to mjp

---
+++ language/pl_PL/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = 'Witamy w Piwigo Guides. W tym miejscu znajdziesz podstawowe informacje o instalacji i uaktualnianiu Piwigo. Dodatkowe informacje są również dostępne bezpośrednio w zakładkach "Pomoc" w panelu administracyjnym oraz wszędzie gdzie pojawia się ikona znaku zapytania. Ponadto informacje dla deweloperów są udokumentowane na wiki. ';
