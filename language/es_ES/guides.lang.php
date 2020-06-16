17dfef892fce61e2a86a80298d31a132f8bb0650
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-16 06:41:17 +0200

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = 'Bienvenido a las Guías Piwigo. Aquí encontrará lo básico sobre cómo instalar y actualizar su Piwigo. También encontrará ayuda directamente en su Piwigo en las páginas "¡Ayúdame!" del panel de administración o en cualquier lugar donde vea un signo de interrogación gráfico. Un wiki también cubre la documentación del desarrollador.';
