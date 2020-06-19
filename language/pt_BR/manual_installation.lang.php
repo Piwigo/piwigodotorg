1cea296a1e4cd5c58808a102c86c9f73b6763a66
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-19 14:19:50 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_image_caption1'] = 'Conectar e transferir';
+$lang['porg_guides_manual_installation_info3'] = 'Você também pode instalar o Piwigo na raiz do site, o diretório "fotos" não é obrigatório. Qualquer que seja o nome do diretório que você escolher, é recomendável evitar mostrar o número da versão do Piwigo.';
+$lang['porg_guides_manual_installation_step1'] = 'Etapa 1 - Faça o download do arquivo completo';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'No processo de instalação manual, você baixa o arquivo completo do Piwigo, extrai seu conteúdo e o transfere para o seu servidor web.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Faça o download do arquivo completo</a> e descompacte-o.';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Transfira o conteúdo do arquivo para o seu servidor Web com qualquer cliente FTP.';
+$lang['porg_guides_manual_installation_step2'] = 'Etapa 2 - Enviar o conteúdo do arquivo';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1. Crie um diretório "fotos" no seu servidor web.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2.Na unidade de disco rígido, abra o diretório extraído “piwigo”.';
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3. Selecione todos os arquivos e transfira-os para o seu servidor no diretório "fotos".';
