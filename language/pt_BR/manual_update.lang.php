d9c6b85d37fae90d951ec315d8a0c29a91fc816f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-19 18:09:12 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/manual_update.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_step7_caption1'] = 'Iniciando a página de atualização.';
+$lang['porg_guides_manual_update_step7_caption2'] = 'Atualizar página de memorando.';
+$lang['porg_guides_manual_update_step7_paragraph1'] = 'Iniciando atualização';
+$lang['porg_guides_manual_update_step7_paragraph2'] = 'No seu navegador da web, abra o script "upgrade.php" e siga o guia, http://example.com/photos/upgrade.php';
+$lang['porg_guides_manual_update_step7_paragraph3'] = 'Para evitar qualquer atualização por outro visitante, você está convidado a entrar';
+$lang['porg_guides_manual_update_step7_paragraph4'] = 'Sua versão anterior foi identificada e você obterá um resumo das operações de atualização;';
+$lang['porg_guides_manual_update_step7_paragraph5'] = 'Os plug-ins ativos antes da atualização serão desativados para o status inativo, pois eles podem falhar no trabalho com a nova versão e precisam de sua própria atualização específica.';
+$lang['porg_guides_manual_update_step8'] = 'Etapa 8 - Verifique o resultado da atualização';
+$lang['porg_guides_manual_update_step8_paragraph1'] = 'Seus primeiros controles podem demorar, porque você não está ciente de todas as alterações';
+$lang['porg_guides_manual_update_step8_paragraph2'] = 'Você verá uma lista de todos os seus plugins anteriores; alguns deles foram totalmente integrados no núcleo (como o Plugins Manager), outros agora estão distribuídos (como o LocalFiles Editor), mas qualquer plug-in desativado foi desativado por um bom motivo.';
+$lang['porg_guides_manual_update_step8_paragraph3'] = 'Tente encontrar uma atualização primeiro; as guias de plug-ins podem ajudá-lo.';
+$lang['porg_guides_manual_update_step8_paragraph4'] = 'Para temas, mantenha uma página de administrador aberta no seu navegador, caso você precise reverter qualquer um dos seus testes.';
+$lang['porg_guides_manual_update_step8_paragraph5'] = 'Não esqueça que seus membros ou visitantes podem ter um tema selecionado específico que não é compatível com seu novo lançamento. Provavelmente, é melhor redefinir o tema deles na página de usuários Admin.';
+$lang['porg_guides_manual_update_step9'] = 'Etapa 9 - Desbloqueie sua galeria';
+$lang['porg_guides_manual_update_step9_paragraph1'] = 'Você começa a se sentir confortável com seu último lançamento, não se esqueça de desbloquear sua galeria para dar acesso aos visitantes';
+$lang['porg_guides_manual_update_time_1'] = '3-5min';
+$lang['porg_guides_manual_update_time_2'] = '5 min';
+$lang['porg_guides_manual_update_time_3'] = '1 min';
+$lang['porg_guides_manual_update_time_4'] = '2 min';
+$lang['porg_guides_manual_update_time_5'] = '15-120 min';
+$lang['porg_guides_manual_update_step3_paragraph1'] = 'Se você estiver executando o Piwigo 2.1 ou superior, pule esta etapa.';
+$lang['porg_guides_manual_update_step3_paragraph2'] = 'Faça backup apenas de seus arquivos personalizados ou de todos os arquivos';
+$lang['porg_guides_manual_update_step4'] = 'Etapa 4 - Preparação da galeria';
+$lang['porg_guides_manual_update_step4_caption1'] = 'Bloquear Galeria';
+$lang['porg_guides_manual_update_step4_paragraph1'] = 'Bloquear a galeria';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Com o Piwigo 2.3 ou versão anterior: Configuração>Geral>Bloquear  Galeria. Para Piwigo 2.4+: Ferramentas>Manutenção.';
+$lang['porg_guides_manual_update_step4_paragraph3'] = 'A partir de agora, os usuários não administradores de qualquer versão da galeria deverão ver algo como: “A galeria está bloqueada para manutenção. Por favor, volte mais tarde.".';
+$lang['porg_guides_manual_update_step5'] = 'Etapa 5 - Limpeza';
+$lang['porg_guides_manual_update_step5_list1'] = 'Galerias';
+$lang['porg_guides_manual_update_step5_list2'] = 'Enviar';
+$lang['porg_guides_manual_update_step5_list3'] = 'Plugins';
+$lang['porg_guides_manual_update_step5_list4'] = 'Temas';
+$lang['porg_guides_manual_update_step5_list5'] = 'Extenção-template';
+$lang['porg_guides_manual_update_step5_list6'] = 'Local';
+$lang['porg_guides_manual_update_step5_list7'] = '_data';
+$lang['porg_guides_manual_update_step5_paragraph1'] = 'Remova todos os arquivos da sua instalação atual do Piwigo, com as seguintes exceções, NÃO exclua esses diretórios:';
+$lang['porg_guides_manual_update_step6'] = 'Etapa 6 - Envio FTP';
+$lang['porg_guides_manual_update_step6_paragraph1'] = 'Use seu cliente FTP padrão para enviar a nova versão, ou seja, o conteúdo do diretório “piwigo” (extraído na etapa 1 e atualizado na etapa 3), no diretório de instalação anterior do Piwigo.';
+$lang['porg_guides_manual_update_step6_paragraph2'] = 'Verifique se o seu cliente FTP não encontrou nenhum erro.';
+$lang['porg_guides_manual_update_step7'] = 'Etapa 7 - Atualização do Banco de Dados';
+$lang['porg_guides_manual_update_info1'] = 'Por favor, verifique os <a href="%s">Requisitos</a> atuais do Piwigo. Especialmente se a sua configuração Php/ MySQL NÃO for suficiente, não avance, pois o processo de atualização falhará.';
+$lang['porg_guides_manual_update_info2'] = 'Nosso conselho: desmarque "Inserções estendidas" ou reduza "Comprimento máximo das consultas criadas".';
+$lang['porg_guides_manual_update_paragraph1'] = 'Este procedimento é compatível com qualquer versão maior ou igual a 1.4. Para uma versão mais antiga, peça ajuda no fórum.';
+$lang['porg_guides_manual_update_step1'] = 'Etapa 1 - Preparação do pacote';
+$lang['porg_guides_manual_update_step10'] = 'Etapa 10 - Limpeza pós-atualização';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Nada';
+$lang['porg_guides_manual_update_step10_paragraph2'] = 'Nada precisa ser removido após qualquer atualização. O "upgrade.php" em si não deve ser removido. Lembre-se de que a mensagem “Não é necessário fazer atualização” e o processo de logon estão protegendo sua galeria.';
+$lang['porg_guides_manual_update_step1_list1'] = '<a href="%s">Faça o download do último arquivo de lançamento do Piwigo</a>';
+$lang['porg_guides_manual_update_step1_list2'] = 'No seu computador, extraia o diretório "piwigo".';
+$lang['porg_guides_manual_update_step1_list3'] = 'Remova o diretório “piwigo/local” padrão.';
+$lang['porg_guides_manual_update_step1_paragraph1'] = 'Faça backup de suas tabelas atuais';
+$lang['porg_guides_manual_update_step1_paragraph2'] = 'Se você estiver usando uma versão da família PhpWebGallery 1.7 ou posterior, poderá usar o plug-in Backup do DB. Os usuários de versões anteriores devem ter que fazer backup de suas tabelas por outros meios, por exemplo, com o phpMyAdmin.';
+$lang['porg_guides_manual_update_step1_paragraph3'] = 'De qualquer forma, verifique se o backup está completo e foi concluído com êxito (o resultado às vezes é truncado devido aos limites do servidor).';
+$lang['porg_guides_manual_update_step2'] = 'Etapa 2 - Preparação do banco de dados (recomendado)';
+$lang['porg_guides_manual_update_step2_caption1'] = 'Opções recomendadas pelo PhpMyAdmin.';
+$lang['porg_guides_manual_update_step3'] = 'Etapa 3 - Preparação do servidor de arquivos';
+$lang['porg_guides_manual_update_step3_list1'] = 'Baixe a ferramenta de extensão atualização Prepare 2.1';
+$lang['porg_guides_manual_update_step3_list2'] = 'extraia o script prep21up.php e transfira-o para a raiz da sua instalação do Piwigo';
+$lang['porg_guides_manual_update_step3_list3'] = 'abra prep21up.php no seu navegador da web http://example.com/photos/prep21up.php e você receberá um arquivo "upgrade21.zip"';
+$lang['porg_guides_manual_update_step3_list4'] = 'No seu computador, extraia o diretório "local" de "upgrade21.zip" para o diretório "piwigo" (extraído durante a etapa 1).';
