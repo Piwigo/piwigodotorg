fa902434984f0324f3979e098dbace6d147b4827
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-19 18:08:56 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/netinstall.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_image_caption1'] = 'Conectar para envio';
+$lang['porg_guides_netinstall_image_caption2'] = 'Escolha um nome de pasta para sua galeria de fotos';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo está instalado';
+$lang['porg_guides_netinstall_info1'] = 'O processo NetInstall pode não funcionar no seu servidor web, dependendo do nível de recursos fornecidos. Se você não conseguir fazê-lo funcionar, basta alternar para o <a href="%s">guia de instalação manual</a>.';
+$lang['porg_guides_netinstall_paragraph1'] = 'O NetInstall requer a transferência de um único arquivo no seu servidor web. O script que ele contém fará o download do arquivo Piwigo em piwigo.org, extrairá seu conteúdo e iniciará o instalador.';
+$lang['porg_guides_netinstall_step1'] = 'Etapa 1 - Faça o download do script NetInstall';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">Faça o download do script NetInstall.</a>';
+$lang['porg_guides_netinstall_step2'] = 'Etapa 2 - Transferir o script NetInstall';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Selecione o arquivo piwigo-netinstall.php na sua unidade de disco rígido e transfira-o para o servidor web';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'Depois que o script NetInstall for transferido, acesse seu endereço na web com um navegador, por exemplo';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Escolha um nome de diretório para sua galeria de fotos no seu site. Por padrão, o Piwigo será instalado no diretório piwigo. Se você deixar o campo Destino vazio, o Piwigo será instalado no mesmo local em que você transferiu o script NetInstall.';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Execute a ação "Recuperar e descompactar o Piwigo" e, em seguida, "Instalar o Piwigo agora"';
