443569f47358680c102cf7ae61931f13cc5e7824
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-19 14:20:21 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/automatic_update.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_paragraph1'] = 'Este é o método recomendado para atualizar o Piwigo para a versão mais recente. Tão simples que você não tem mais desculpa para esquecer ou adiar suas atualizações. Realmente leva menos de 1 minuto para executar a versão mais recente e mais segura do Piwigo.';
+$lang['porg_guides_automatic_update_paragraph10'] = 'Baixe o plug-in Piwigo Auto Atualização e descompacte-o no diretório "plugins"';
+$lang['porg_guides_automatic_update_paragraph11'] = 'Vá para Administração>Especiais>Plugins>Administração e clique na ação "instalar" na frente do plug-in "Piwigo AutoUpgrade" e, em seguida, na ação "Ativar".';
+$lang['porg_guides_automatic_update_paragraph12'] = 'Vá para Administração>Especiais>Plugins>Piwigo AutoUpgrade e siga o guia, você pode optar por atualizar para a última versão do mesmo "ramo" ou para a versão mais recente do Piwigo.';
+$lang['porg_guides_automatic_update_paragraph13'] = 'Se você possui uma versão anterior, siga a atualização manual.';
+$lang['porg_guides_automatic_update_paragraph3'] = 'Vá para Administração>Ferramentas>Atualizações e siga o guia';
+$lang['porg_guides_automatic_update_paragraph4'] = 'Vá para Administração>Plugins>Gerenciar>Outros plugins disponíveis e instale o Piwigo Auto Upgrade. Se o plug-in já estiver instalado, verifique se você está executando a revisão mais recente na tela Administração> Plug-ins>Gerenciar>Verificar atualizações.';
+$lang['porg_guides_automatic_update_paragraph5'] = 'Vá para Administração>Plugins>Gerenciar>lista Plugins para instalar e ativar esse novo plug-in.';
+$lang['porg_guides_automatic_update_paragraph6'] = 'Vá para Administração>Plugins>Piwigo AutoUpgrade e siga o guia.';
+$lang['porg_guides_automatic_update_paragraph7'] = 'Administração>Especiais>Plugins>Outros plugins e execute uma instalação automática do Piwigo Auto Atualizar. Se o plug-in já estiver instalado, verifique se você está executando a revisão mais recente.';
+$lang['porg_guides_automatic_update_paragraph8'] = 'Vá para Administração>Especiais>Plugins para instalar e ativar este novo plugin';
+$lang['porg_guides_automatic_update_paragraph9'] = 'Vá para Administração>Especiais>Plugins>Piwigo Atualizção e siga o guia, você pode optar por atualizar para a última versão do mesmo "ramo" ou para a versão mais recente do Piwigo.';
+$lang['porg_guides_automatic_update_step1'] = 'Um único passo';
+$lang['porg_guides_automatic_update_time_1'] = '30 segundos';
+$lang['porg_guides_automatic_update_button2'] = 'Se você estiver executando o Piwigo 2.1.x ou 2.2.x';
+$lang['porg_guides_automatic_update_button3'] = 'Se você estiver executando o Piwigo 2.0.x';
+$lang['porg_guides_automatic_update_button4'] = 'Se você estiver executando o Piwigo 1.7.x';
+$lang['porg_guides_automatic_update_button5'] = 'Se você estiver usando uma versão anterior à 1.7.x';
+$lang['porg_guides_automatic_update_caption1'] = 'Sim, apenas 2 cliques';
+$lang['porg_guides_automatic_update_info1'] = 'Sempre faça um backup do seu banco de dados e arquivos. O melhor é fazer com que sejam feitos automaticamente regularmente. Se algo de ruim acontecer durante a atualização, você poderá recuperar um backup.';
