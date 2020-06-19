de34aa690f2fad70c6c33c002bb14dc1b89fd10b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-19 14:21:25 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/requirements.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_requirements_minimal'] = 'Minimum Requirements';
+$lang['porg_guides_requirements_minimal_fifth'] = 'een FTP-clientsoftware is vereist om de bestanden te uploaden (netinstall of volledig pakket): het Piwigo-team beveelt FileZilla aan als FTP-clientsoftware, omdat het gratis is net als Piwigo en compatibel is met Windows, Mac en Linux.';
+$lang['porg_guides_requirements_minimal_first'] = 'Een webserver als Nginx of Apache';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Een grafische bibliotheek: ImageMagick wordt aanbevolen vanwege zijn prestaties en beeldkwaliteit, maar GD, vaak gebundeld met PHP, kan ook het werk doen';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (of het equivalent met MariaDB)';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Voldoende schijfruimte voor uw foto\'s: naast de foto\'s die u uploadt, slaat Piwigo de "meerdere formaten" op in een cachemap van uw server';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (aanbevolen 7.0 of 7.1)';
+$lang['porg_guides_requirements_optional'] = 'Optionele vereisten';
+$lang['porg_guides_requirements_optional_first'] = 'exiftool is vereist voor plug-in Schrijf metadata of een andere plug-in die te maken heeft met EXIF ​​/ IPTC-metadata';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg is vereist voor plug-in VideoJS om een ​​videoposter te maken';
+$lang['porg_guides_requirements_optional_third'] = 'pdftoppm wordt aanbevolen om een ​​poster van PDF-bestanden te genereren (in plaats van ImageMagick)';
+$lang['porg_guides_requirements_paragraph'] = 'Piwigo vereist een webhosting. Voor een fotogalerijoplossing met alles inbegrepen (installatie, hosting, back-ups), of als u Piwigo gewoon zonder installatie wilt proberen, kunt u overwegen om <a href="%s"> een gratis proefaccount te openen op Piwigo.com < / a>';
