7ba5815a518d541e9f0d9ceeffa43f707426cb3e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-20 14:44:18 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/netinstall.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_image_caption1'] = 'Verbind upload';
+$lang['porg_guides_netinstall_image_caption2'] = 'Kies een mapnaam voor jouw fotogalerij';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo is geïnstalleerd';
+$lang['porg_guides_netinstall_info1'] = 'Het NetInstall-proces werkt mogelijk niet op jouw webserver, afhankelijk van het niveau van de geboden functies. Als je het niet kunt laten werken, ga dan gewoon naar de <a href="%s"> handmatige installatiehandleiding </a>';
+$lang['porg_guides_netinstall_paragraph1'] = 'NetInstall vereist de overdracht van een enkel bestand op jouw webserver. Het script dat het bevat, downloadt het Piwigo-archief van piwigo.org, haalt de inhoud eruit en start het installatieprogramma';
+$lang['porg_guides_netinstall_step1'] = 'Stap 1: download het Netinstallatie-script';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">Download het Netinstallatiescript</a>';
+$lang['porg_guides_netinstall_step2'] = 'Stap 2 - Breng het NetInstall-script over';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Selecteer het Piwigo-netinstall.php-bestand op uw harde schijf en breng het over naar de webserver';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'Zodra het NetInstall-script is overgedragen, ga je bijvoorbeeld naar het webadres met een webbrowser';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Kies een mapnaam voor jouw fotogalerij op jouw website. Standaard wordt Piwigo geïnstalleerd in de directory Piwigo. Als je het veld Bestemming leeg laat, wordt Piwigo geïnstalleerd op dezelfde plaats waar je het NetInstall-script hebt geplaatst.';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Voer de actie "Ophalen en uitpakken Piwigo" uit en vervolgens "Installeer Piwigo nu"';
