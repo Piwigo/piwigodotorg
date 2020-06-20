da095d2839646f3f8c9231da7936108dca4c74c8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-20 14:44:03 +0200

    Update pl_PL, thanks to mjp

---
+++ language/pl_PL/netinstall.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_info1'] = 'Netinstalator może nie działać na twoim serwerze w zależności od jego konfiguracji. Jeżeli nie uda Ci się uruchomić Netinstaltora wykonaj <a href="%s">instalację ręczną</a>.';
+$lang['porg_guides_netinstall_paragraph1'] = 'Netinstalator wymaga przesłania jednego pliku na serwer. Skrypt Netinstalatora pobierze spakowane Piwigo z piwigo.org, rozpakuje pliki i uruchomi instalatora.';
+$lang['porg_guides_netinstall_image_caption2'] = 'Wybierz nazwę folderu dla twojej galerii';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo zostało zainstalowane';
+$lang['porg_guides_netinstall_step1'] = 'Krok 1 - Pobierz skrypt Netinstalatora';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">Pobierz skrypt Netinstalatora</a>';
+$lang['porg_guides_netinstall_step2'] = 'Krok 2 - Prześlij skrypt Netinstalatora';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Wybierz plik piwigo-netinstall.php na swoim dysku twardym i prześlij go na serwer';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'Gdy skrypt został przesłany, otwórz go w przeglądarce, na przykład';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Wybierz nazwę katalogu na serwerze dla Twojej galerii. Domyślnie Piwigo zostanie zainstalowany w katalogi piwigo. Dla pustego pola "Katalog docelowy" Piwigo zostanie zainstalowany w tym samym miejscu gdzie przesłano skrypt Netinstalatora.';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Wykonaj "Pobierz i rozpakuj Piwigo", a następnie "Zainstaluj Piwigo teraz"';
