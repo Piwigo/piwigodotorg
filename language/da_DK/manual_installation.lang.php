68645cb163eec9ca4f63df1188f8a0d006013662
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-22 23:35:15 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_image_caption1'] = 'Forbind og overfør';
+$lang['porg_guides_manual_installation_info3'] = 'Du kan også installere Piwigo i webstedets root, mappen "photos" er ikke et krav. Uanset hvilken mappe du vælger, anbefaler vi at undgå at vise Piwigos versionsnummer.';
+$lang['porg_guides_manual_installation_step1'] = 'Trin 1 - Hent det komplette arkiv';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'I den manuelle installeringsproces, skal du hente det komplette Piwigo-arkiv, udpakke dets indhold og overføre det til din webserver.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Hent det komplette arkiv</a> og udpak det.';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Overfør arkivindholdet til din webserver med en FTP-klient.';
+$lang['porg_guides_manual_installation_step2'] = 'Trin 2 - Overfør arkivets indhold';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1. Opret mappen "photos" på din webserver.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2. På din disk åbnes den udpakkede mappe "piwigo".';
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3. Vælg alle filerne og overfør dem til din server, til mappen "photos".';
