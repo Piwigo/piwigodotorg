e4550e64ed47516ea9066c7b6e3c96051f189117
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-22 23:35:29 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/netinstall.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_image_caption1'] = 'Uploadforbindelse';
+$lang['porg_guides_netinstall_image_caption2'] = 'Vælg et mappenavn til dit fotogalleri';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo er installeret';
+$lang['porg_guides_netinstall_info1'] = 'NetInstall-processen fungerer måske ikke på din webserver, afhængigt af funktionalitetsniveauet. Hvis du ikke kan få det til at fungere, så skift i stedet til den <a href="%s">manuelle installeringsvejledning</a>.';
+$lang['porg_guides_netinstall_paragraph1'] = 'NetInstall kræver at der overføres en enkelt fil til din webserver. Det indeholdte script henter Piwigo-arkivet fra piwigo.org, og udpakker dernæst indholdet og starter installeringsprogrammet.';
+$lang['porg_guides_netinstall_step1'] = 'Trin 1 - Hent NetInstall-scriptet';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">Hent NetInstall-scriptet.</a>';
+$lang['porg_guides_netinstall_step2'] = 'Trin 2 - Overfør NetInstall-scriptet';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Vælg filen piwigo-netinstall.php på din disk og overfør den til webserveren';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'Når NetInstall-scriptet er overført, så gå til dets webadresse i en webbrowser, for eksempel';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Vælg et mappenavn til dit fotogalleri på dit websted. Som standard bliver Piwigo installeret i mappen piwigo. Hvis du ikke udfyld Mål-feltet, vil Piwigo blive installeret det samme sted, som du overførte NetInstall-scriptet til.';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Kør handlingen “Retrieve and unzip Piwigo” ("Hent og udpak Piwigo"), and dernæst “Install Piwigo now” ("Installer Piwigo nu")';
