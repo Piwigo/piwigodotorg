04ed0e906ff1bc28821f27a47f7586daf2997a65
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-24 21:30:43 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/automatic_update.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_button2'] = 'Если Вы используете Piwigo 2.1.x или 2.2.x';
+$lang['porg_guides_automatic_update_button3'] = 'Если Вы используете Piwigo 2.0.х';
+$lang['porg_guides_automatic_update_button4'] = 'Если Вы используете Piwigo 1.7.х';
+$lang['porg_guides_automatic_update_button5'] = 'Если Вы используете Piwigo версии старше, чем 1.7.х';
+$lang['porg_guides_automatic_update_caption1'] = 'Да, только 2 клика';
+$lang['porg_guides_automatic_update_step1'] = 'Один простой шаг';
+$lang['porg_guides_automatic_update_time_1'] = '30 секунд';
