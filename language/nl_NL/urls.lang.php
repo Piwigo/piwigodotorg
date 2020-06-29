fff4a4594582354e4f96accf7827fcb2b9a8116f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-29 06:41:10 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/urls.lang.php
+++ language/nl_NL/urls.lang.php
@@ -1,18 +1,18 @@
 <?php
-$lang['porg_urls']['about-us'] = 'over-ons';
+$lang['porg_urls']['about-us'] = 'over ons';
 $lang['porg_urls']['changelogs'] = 'wijzigingen';
 $lang['porg_urls']['coding-activity'] = 'coderings-activiteit';
 $lang['porg_urls']['contact'] = 'contact';
 $lang['porg_urls']['features'] = 'kenmerken';
-$lang['porg_urls']['get-involved'] = 'raak-betrokken';
-$lang['porg_urls']['get-piwigo'] = 'verkrijg-piwigo';
+$lang['porg_urls']['get-involved'] = 'raak betrokken';
+$lang['porg_urls']['get-piwigo'] = 'verkrijg piwigo';
 $lang['porg_urls']['get-started'] = 'begin';
 $lang['porg_urls']['news'] = 'nieuws';
 $lang['porg_urls']['newsletters'] = 'nieuwsbrief';
 $lang['porg_urls']['press'] = 'pers';
 $lang['porg_urls']['release'] = 'release';
 $lang['porg_urls']['testimonials'] = 'getuigenissen';
-$lang['porg_urls']['what-is-piwigo'] = 'wat-is-piwigo';
+$lang['porg_urls']['what-is-piwigo'] = 'wat is piwigo';
 
 $lang['porg_doc_urls']['requirements'] = 'https://nl.piwigo.org/doc/doku.php?id=installatie:vereisten';
 $lang['porg_doc_urls']['install'] = 'https://nl.piwigo.org/doc/doku.php?id=installatie';
@@ -20,3 +20,9 @@ $lang['porg_doc_urls']['upgrade'] = 'https://nl.piwigo.org/doc/doku.php?id=upgra
 $lang['porg_doc_urls']['upgrade_auto'] = 'https://nl.piwigo.org/doc/doku.php?id=upgrade:automatische';
 $lang['porg_doc_urls']['upgrade_manual'] = 'https://nl.piwigo.org/doc/doku.php?id=upgrade:handmatige';
 $lang['porg_urls']['showcases'] = 'voorbeelden';
+$lang['porg_urls']['automatic_update'] = 'gids/update/automatisch';
+$lang['porg_urls']['guides'] = 'gidsen';
+$lang['porg_urls']['manual_installation'] = 'gidsen/insatalleer/handleiding';
+$lang['porg_urls']['manual_update'] = 'gidsen/update/handleiding';
+$lang['porg_urls']['netinstall'] = 'gidsen/installatie/netwerkinstallatie';
+$lang['porg_urls']['requirements'] = 'gidsen/installatie/vereisten';
