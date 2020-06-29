4ae523fa2f0525cbf00a7ca30b96e2f3b28d9c48
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-29 06:41:24 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = 'Welkom bij Piwigo-gidsen. Hier vind je de basis voor het installeren en updaten van jouw Piwigo. Sommige hulp wordt ook direct in jouw Piwigo geleverd met "Help me!" pagina\'s van het Beheer paneel of waar je ook maar een grafisch vraagteken ziet. Een wiki behandelt ook de documentatie voor ontwikkelaars';
