973fb2b72ce5950e96fc5825ede3bdfbf8a5f5a0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-06 15:01:15 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/automatic_update.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_time_1'] = '30 seconden';
+$lang['porg_guides_automatic_update_step1'] = 'Een enkele stap';
+$lang['porg_guides_automatic_update_paragraph9'] = 'Ga naar Beheer> Specials> Plug-ins> Piwigo AutoUpgrade en volg de gids. U kunt ervoor kiezen om te upgraden naar de laatste release van dezelfde "branch" of naar de allerlaatste Piwigo-release.';
+$lang['porg_guides_automatic_update_paragraph8'] = 'Ga naar Beheer> Specials> Plug-ins om deze nieuwe plug-in te installeren en te activeren';
+$lang['porg_guides_automatic_update_paragraph7'] = 'Beheer> Specials> Plug-ins> Andere plug-ins en voer een automatische installatie van Piwigo Auto Upgrade uit. Als de plug-in al is geïnstalleerd, controleer dan of je de nieuwste revisie gebruikt.';
+$lang['porg_guides_automatic_update_paragraph5'] = 'Ga naar Beheer> Plug-ins> Beheren> Lijst met plug-ins om deze nieuwe plug-in te installeren en te activeren.';
+$lang['porg_guides_automatic_update_paragraph6'] = 'Ga naar Beheer> Plug-ins> Piwigo AutoUpgrade en volg de aanwijzingen.';
+$lang['porg_guides_automatic_update_paragraph4'] = 'Ga naar Beheer> Plug-ins> Beheren> Andere beschikbare plug-ins en installeer Piwigo Auto Upgrade. Als de plug-in al is geïnstalleerd, controleer dan of je de nieuwste revisie uitvoert op het scherm Beheer> Plug-ins> Beheren> Controleren op updates.';
+$lang['porg_guides_automatic_update_paragraph12'] = 'Ga naar Beheer> Specials> Plug-ins> Piwigo AutoUpgrade en volg de aanwijzingen. Je kunt ervoor kiezen om te upgraden naar de laatste release van dezelfde "branch" of naar de allerlaatste Piwigo-release.';
+$lang['porg_guides_automatic_update_paragraph13'] = 'Als je een eerdere release hebt, volg dan de handmatige upgrade.';
+$lang['porg_guides_automatic_update_paragraph11'] = 'Ga naar Beheer> Specials> Plug-ins> Beheer en klik "installeren" voor de plug-in "Piwigo AutoUpgrade" en vervolgens op "Activeren".';
+$lang['porg_guides_automatic_update_info1'] = 'Zorg altijd voor een back-up van uw database en bestanden. Het beste is om ze regelmatig automatisch te laten maken. Als er tijdens de update iets ergs gebeurt, kunt u het met een back-up herstellen.';
+$lang['porg_guides_automatic_update_paragraph3'] = 'Ga naar Beheer> Extra> Updates en volg de aanwijzingen';
+$lang['porg_guides_automatic_update_paragraph10'] = 'Download de Piwigo Auto Upgrade-plug-in en pak deze uit in jouw "plug-ins" -directory';
+$lang['porg_guides_automatic_update_paragraph1'] = 'Dit is de aanbevolen methode om je Piwigo te upgraden naar de nieuwste release. Zo eenvoudig dat je geen excuus meer hebt om jouw upgrades te vergeten of uit te stellen. Het duurt echt minder dan 1 minuut om de nieuwste en veiligste Piwigo-release uit te voeren.';
+$lang['porg_guides_automatic_update_caption1'] = 'Ja, slechts 3 kliks';
+$lang['porg_guides_automatic_update_button5'] = 'Je werkt met een oudere versie dan 1.7.x';
+$lang['porg_guides_automatic_update_button4'] = 'Je werkt met Piwigo 1.7.x';
+$lang['porg_guides_automatic_update_button3'] = 'Je werkt met Piwigo 2.0.x';
+$lang['porg_guides_automatic_update_button2'] = 'Je werkt met Piwigo 2.1.x of 2.2.c';
