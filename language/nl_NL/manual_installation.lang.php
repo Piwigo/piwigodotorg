0b509722991115f994032edfcdc0feb3ef097358
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-09 12:24:07 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3. Selecteer alle bestanden en breng ze over naar de "foto\'s" map op jouw server ';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2.Open op jouw harddisk de uitgepakte "piwigo" map';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1.Maak een directory "foto\'s" op joue webserver.';
+$lang['porg_guides_manual_installation_step2'] = 'Stap 2 - Upload de archiefinhoud';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Breng de archiefinhoud over naar jouw webserver met een FTP-client.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s"> Download het volledige archief </a> en pak het uit';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'Tijdens het handmatige installatieproces download je het volledige archief van Piwigo, pak de inhoud ervan uit en zet deze over naar jouw webserver.';
+$lang['porg_guides_manual_installation_step1'] = 'Stap 1: dwonload het volledige archief';
+$lang['porg_guides_manual_installation_info3'] = 'U kunt Piwigo ook installeren in de root van de website, de map "foto\'s" is niet verplicht. Welke directory-naam u ook kiest, het wordt aanbevolen om te voorkomen dat het Piwigo-releasenummer wordt weergegeven.';
+$lang['porg_guides_manual_installation_image_caption1'] = 'Verbinding maken en verzenden';
