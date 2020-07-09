59f6d39284e14f072fc4194d2bf7dd1a0b1a4501
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-09 06:42:40 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_info3'] = 'También puede instalar Piwigo en el directorio raíz del sitio web; el directorio "fotos" no es obligatorio. Sea cual sea el nombre de directorio que escoja, le recomendamos que no aparezca el número de versión de Piwigo.';
+$lang['porg_guides_manual_installation_step1'] = 'Paso 1- Descargar el archivo completo';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'En el proceso de instalación manual descargará el archivo completo de Piwigo, extraerá su contenido y lo transferirá a su servidor web.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Descargue el archivo completo</a> y desempaquételo.';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Transfiera el contenido del archivo a su servidor web con cualquier cliente FTP.';
+$lang['porg_guides_manual_installation_step2'] = 'Paso 2 - Subir el contenido del archivo';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = 'Cree un directorio "fotos" en su servidor web.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = 'En su disco duro, abra el directorio extraído "piwigo".';
+$lang['porg_guides_manual_installation_step2_paragraph5'] = 'Seleccione todos los ficheros y transfiéralos al directorio "fotos" de su servidor.';
+$lang['porg_guides_manual_installation_image_caption1'] = 'Conectarse y transferir';
