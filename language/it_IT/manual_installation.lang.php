d016dc73695714a75a9173dd4d185a059f5d34d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-11 17:23:45 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_image_caption1'] = 'Connetti e trasferisci';
+$lang['porg_guides_manual_installation_info3'] = 'È inoltre possibile installare Piwigo nella directory principale del sito Web, la directory "foto" non è obbligatoria. Qualunque sia il nome della directory scelta, si consiglia di evitare di mostrare il numero di versione di Piwigo.';
+$lang['porg_guides_manual_installation_step1'] = 'Primo passo - scarica l\'archivio completo';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'Nel processo di installazione manuale, scarica l\'archivio completo di Piwigo, estraine il contenuto e lo trasferisci sul tuo server web.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Scarica l\'archivio completo</a> e decomprimilo.';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Trasferisci il contenuto dell\'archivio sul tuo server web con qualsiasi client FTP.';
+$lang['porg_guides_manual_installation_step2'] = 'Secondo passo - carica il contenuto dell\'archivio';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1. Creare una directory "foto" sul tuo server web.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2. Sull\'unità disco fisso, aprire la directory estratta “piwigo”.';
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3. Seleziona tutti i file e trasferiscili sul tuo server nella directory "foto".';
