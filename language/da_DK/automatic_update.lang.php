e52803fc1c99cfb4648c453bf9c83e5c2a27c4fe
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-14 07:47:44 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/automatic_update.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_button2'] = 'Hvis du kører Piwigo 2.1.x eller 2.2.x';
+$lang['porg_guides_automatic_update_button3'] = 'Hvis du kører Piwigo 2.0.x';
+$lang['porg_guides_automatic_update_button4'] = 'Hvis du kører Piwigo 1.7.x';
+$lang['porg_guides_automatic_update_button5'] = 'Hvis du kører en version ældre end 1.7.x';
+$lang['porg_guides_automatic_update_caption1'] = 'Ja, kun to klik';
+$lang['porg_guides_automatic_update_info1'] = 'Sørg for altid at have en sikkerhedskopi af din database og filer. Det bedste er jævnligt at få dem automatisk fremstillet. Hvis der går noget galt under installeringen, vil du være i stand til at gendanne en sikkerhedskopi.';
+$lang['porg_guides_automatic_update_paragraph1'] = 'Det er den anbefalede metode, at opgradere Piwigo til den seneste udgave på. Så enkelt at du ikke har nogen undskyldninger for at glemme eller udsætte dine opgraderinger. Det tager faktisk mindre end ét minut, at komme til at køre den seneste og mest sikre Piwigo-udgivelse.';
+$lang['porg_guides_automatic_update_paragraph10'] = 'Hent plugin\'en Piwigo Auto Upgrade og udpak den i din "pigiwo"-mappe';
+$lang['porg_guides_automatic_update_paragraph11'] = 'Gå til Administration > Specials > Plugins > Administration og klik på handlingen "install" foran plugin\'en "Piwigo AutoUpgrade", og dernæst på handlingen "Activate".';
+$lang['porg_guides_automatic_update_paragraph12'] = 'Gå til Administration > Specials > Plugins > Piwigo AutoUpgrade, og følg vejledningen, du kan enten vælge at opgradere til den seneste udgave i den samme "branch" eller til den allerseneste udgave af Piwigo.';
+$lang['porg_guides_automatic_update_paragraph13'] = 'Hvis du har en tidligere udgivelse, så følg i stedet den manuelle opgradering.';
+$lang['porg_guides_automatic_update_paragraph3'] = 'Gå til Administration > Værktøjer > Opdateringer, og følg vejledningen';
+$lang['porg_guides_automatic_update_paragraph4'] = 'Gå til Administration > Plugins > Administrer > Andre tilgængelige plugins, og installer Piwigo Auto Upgrade. Hvis plugin\'en allerede er installeret, så undersøg på skærmbilledet Administration > Plugins > Manage > Check for updates, om du kører den seneste version.';
+$lang['porg_guides_automatic_update_paragraph5'] = 'Gå til Administration > Plugins > Administrer > Pluginlist for at installere og aktivere den nye plugin';
+$lang['porg_guides_automatic_update_paragraph6'] = 'Gå til Administration > Plugins > Piwigo AutoUpgrade, og følg vejledningen.';
+$lang['porg_guides_automatic_update_paragraph7'] = 'Gå til Administration > Specials > Plugins > Other Plugins, og udfør en autmatisk installering af Piwigo Auto Upgrade. Hvis plugin\'en allerede er installeret, så undersøg om du kører den seneste version.';
+$lang['porg_guides_automatic_update_paragraph8'] = 'Gå til Administration > Specials > Plugins for at installere og aktivere den nye plugin';
+$lang['porg_guides_automatic_update_paragraph9'] = 'Gå til Administration > Specials > Plugins > Piwigo AutoUpgrade, og følg vejledningen, du kan vælge enten at opgradere til den seneste udgave i den samme "branch" eller til den allerseneste udgave af Piwigo.';
+$lang['porg_guides_automatic_update_step1'] = 'Et enkelt trin';
+$lang['porg_guides_automatic_update_time_1'] = '30 sekunder';
