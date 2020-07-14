f9313a8c2c8504d0cda0e88f01320ad9c552f7dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-14 07:47:59 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/requirements.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_requirements_minimal'] = 'Minimum Gereksinimler';
+$lang['porg_guides_requirements_optional'] = 'İsteğe Bağlı Gereksinimler';
