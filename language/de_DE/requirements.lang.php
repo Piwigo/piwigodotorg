2bb3791fc5decf5a7e77c1c4ade6e08e68f26271
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-14 22:14:19 +0200

    Update de_DE, thanks to Joergen

--- language/de_DE/requirements.lang.php
+++ language/de_DE/requirements.lang.php
@@ -4,4 +4,12 @@ $lang['porg_guides_requirements_paragraph'] = 'Für eine professionelle Foto-Gal
 $lang['porg_guides_requirements_minimal_first'] = 'Einen Webserver wie Nginx oder Apache';
 $lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 oder eine entsprechende MariaDB';
 
-?>
+$lang['porg_guides_requirements_minimal'] = 'Mindestanforderungen';
+$lang['porg_guides_requirements_minimal_fifth'] = 'Zum Hochladen der Dateien wird eine FTP-Client-Software benötigt (netinstall oder Vollpaket): Das Piwigo-Team empfiehlt FileZilla als FTP-Client-Software, da es wie Piwigo kostenlos und mit Windows, Mac und Linux kompatibel ist.';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Eine grafische Bibliothek: ImageMagick wird wegen seiner Leistung und Bildqualität empfohlen, aber auch GD, oft mit PHP gebündelt, kann diese Aufgabe erfüllen.';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Genügend Speicherplatz für Ihre Bilder: Zusätzlich zu den Fotos, die Sie hochladen, speichert Piwigo die "verschiedenen Größen" in einem Cache-Verzeichnis Ihres Servers.';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (empfohlen 7.0 oder 7.1)';
+$lang['porg_guides_requirements_optional'] = 'Optionale Anforderungen';
+$lang['porg_guides_requirements_optional_first'] = 'exiftool wird für das Plugin Write Metadata oder jedes andere Plugin, das sich mit EXIF/IPTC-Metadaten befasst, benötigt';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg ist für das Plugin VideoJS erforderlich, um Videoposter zu erstellen';
+$lang['porg_guides_requirements_optional_third'] = 'pdftoppm wird empfohlen, Poster aus PDF-Dateien zu generieren (anstelle von ImageMagick)';
