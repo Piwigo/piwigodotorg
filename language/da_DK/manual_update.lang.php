6b8c80ae77c519f58b2a202e76a2492af3c6fc7b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-14 07:47:30 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/manual_update.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_time_3'] = '1 min.';
+$lang['porg_guides_manual_update_time_4'] = '2 min.';
+$lang['porg_guides_manual_update_time_5'] = '15-120 min.';
+$lang['porg_guides_manual_update_step6_paragraph1'] = 'Brug din almindelige FTP-klient til at uploade din nye udgave, dvs. indholdet fra mappen "pigiwo" (udpakket under trin 1 og opdateret under trin 3) til eksisterende Piwigo-installeringsmappe.';
+$lang['porg_guides_manual_update_step6_paragraph2'] = 'Kontroller at din FTP-klient ikke stødte på fejl.';
+$lang['porg_guides_manual_update_step7'] = 'Trin 7 - Databaseopgradering';
+$lang['porg_guides_manual_update_step7_caption1'] = 'Start opgradering-side.';
+$lang['porg_guides_manual_update_step7_caption2'] = 'Opgrader memo-side.';
+$lang['porg_guides_manual_update_step7_paragraph1'] = 'Start opgradering';
+$lang['porg_guides_manual_update_step7_paragraph2'] = 'Åbn script\'et "upgrade.php" i din webbrowser og følg vejledningen, http://example.com/photos/upgrade.php';
+$lang['porg_guides_manual_update_step7_paragraph3'] = 'For at undgå en anden brugers opgradering, bedes du logge ind';
+$lang['porg_guides_manual_update_step7_paragraph4'] = 'Din tidligere udgave identificeres, og du vil få et resume af opgraderingshandlingerne;';
+$lang['porg_guides_manual_update_step7_paragraph5'] = 'Plugins der var aktive før opgraderingen, vil blive skiftet til inaktiv status, da de måske ikke fungerer med den nye udgave, og selv skal opgraderes.';
+$lang['porg_guides_manual_update_step8'] = 'Trin 8 - Tjek opgraderingsresultatet';
+$lang['porg_guides_manual_update_step8_paragraph1'] = 'Dine første kontroller vil tage nogen tid, da du ikke er bekendt med alle ændringerne';
+$lang['porg_guides_manual_update_step8_paragraph2'] = 'Først får du en liste over tidligere plugin\'er, nogle af dem er blevet fuldstændig integreret i kernen (så som Plugins Manager), mens andre nu distribueres (så som LocalFiles Editor), men enhver deaktiveret af plugins er gjort med god grund.';
+$lang['porg_guides_manual_update_step8_paragraph3'] = 'Prøv først at finde en opgradering; fanebladet plugins kan være en hjælp.';
+$lang['porg_guides_manual_update_step8_paragraph4'] = 'Hvad angår temaer, så hold en administrationsside åben i din browser, i tilfælde af at du er nødt til at rulle nogle af dine ændringer tilbage.';
+$lang['porg_guides_manual_update_step8_paragraph5'] = 'Glem ikke at dine medlemmer eller besøgende kan have valgt specifikke temaer, som ikke er kompatible med din nye udgave. Du bør nulstillet deres temavalg på administrationens brugerside.';
+$lang['porg_guides_manual_update_step9'] = 'Trin 9 - Lås dit galleri op';
+$lang['porg_guides_manual_update_step9_paragraph1'] = 'Du begynder at føle dig hjemme i den nye udgave, og glem ikke at låse dit galleri op for at give dine besøgende adgang igen.';
+$lang['porg_guides_manual_update_time_1'] = '3-5 min.';
+$lang['porg_guides_manual_update_time_2'] = '5 min.';
+$lang['porg_guides_manual_update_step3_paragraph1'] = 'Hvis du kører Piwigo 2.1 eller højere, skal dette trin springes over.';
+$lang['porg_guides_manual_update_step3_paragraph2'] = 'Lav en sikkerhedskopi af dine tilpassede filer eller af alle filer';
+$lang['porg_guides_manual_update_step4'] = 'Trin 4 - Galleriforberedelse';
+$lang['porg_guides_manual_update_step4_caption1'] = 'Lås galleri';
+$lang['porg_guides_manual_update_step4_paragraph1'] = 'Lås galleriet';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Med Piwigo 2.3 eller tidligere versioner: Opsætning > Generelt > Lås galleri. For Piwigo 2.4+: Værktøjer > Vedligeholdelse.';
+$lang['porg_guides_manual_update_step4_paragraph3'] = 'Nu skulle brugere, som ikke er administratorer af alle galleriversioner, få en besked noget i retning af: "Galleriet er låst pga. vedligeholdelse. Prøv igen senere."';
+$lang['porg_guides_manual_update_step5'] = 'Trin 5 - Oprydning';
+$lang['porg_guides_manual_update_step5_list1'] = 'Gallerier';
+$lang['porg_guides_manual_update_step5_list2'] = 'Upload';
+$lang['porg_guides_manual_update_step5_list3'] = 'Plugins';
+$lang['porg_guides_manual_update_step5_list4'] = 'Themes';
+$lang['porg_guides_manual_update_step5_list5'] = 'Template-extension';
+$lang['porg_guides_manual_update_step5_list6'] = 'Local';
+$lang['porg_guides_manual_update_step5_list7'] = '_data';
+$lang['porg_guides_manual_update_step5_paragraph1'] = 'Fjern alle filer fra din aktuelle Piwigo-installation, med følgende undtagelser, slet IKKE følgende mapper:';
+$lang['porg_guides_manual_update_step6'] = 'Trin 6 - FTP-upload';
+$lang['porg_guides_manual_update_info1'] = 'Tjek Piwigos aktulle <a href="%s">krav</a>. Særligt hvis din PHP- / MySQL-opsætning IKKE er tilstrækkelig, skal du ikke fortsætte, da opgraderingsprocessen vil mislykkes.';
+$lang['porg_guides_manual_update_info2'] = 'Vores råd er at fravælge "Extended inserts" eller at begrænse "Maximal length of created queries".';
+$lang['porg_guides_manual_update_paragraph1'] = 'Denne procedure er kompatibel med enhver version større end eller lig med 1.4. Bed om hjælp i forummet, ved ældre versioner.';
+$lang['porg_guides_manual_update_step1'] = 'Trin 1 - Forbered pakke';
+$lang['porg_guides_manual_update_step10'] = 'Trin 10 - Oprydning efter opgradering';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Intet';
+$lang['porg_guides_manual_update_step10_paragraph2'] = 'Intet skal fjernes efter nogen opgradering. "upgrade.php" selv skal ikke fjernes. Husk at meddelelsen "Ingen opgradering krævet" og loginprocessen, beskytter dit galleri.';
+$lang['porg_guides_manual_update_step1_list1'] = '<a href="%s">Hent et arkiv med den senete Piwigo-udgave</a>';
+$lang['porg_guides_manual_update_step1_list2'] = 'På din computer udpakkes mappen "piwigo".';
+$lang['porg_guides_manual_update_step1_list3'] = 'Fjern standardmappen "piwigo/local".';
+$lang['porg_guides_manual_update_step1_paragraph1'] = 'Lav en sikkerhedskopi af dine aktuelle tabeller';
+$lang['porg_guides_manual_update_step1_paragraph2'] = 'Hvis du benytter en version af PhpWebGallery 1.7-familien eller senere, kan du anvende plugin\'en DB Backup. Brugere af tidligere udgaver, bør lave en sikkerhedskopi af deres tabeller på en anden måde, for eksempel med phpMyAdmin.';
+$lang['porg_guides_manual_update_step1_paragraph3'] = 'Under alle omstændigheder, sørg for at kontrollere at sikkerhedskopien er komplet og blev gennemført uden fejl (resultatet forkortes nogle gange på grund af serverbegrænsninger).';
+$lang['porg_guides_manual_update_step2'] = 'Trin 2 - Databaseforberedelse (anbefalet)';
+$lang['porg_guides_manual_update_step2_caption1'] = 'Anbefalede PhpMyAdmin-valgmuligheder';
+$lang['porg_guides_manual_update_step3'] = 'Trin 3 - Filserverforberedelser';
+$lang['porg_guides_manual_update_step3_list1'] = 'Hent Prepare 2.1 Upgrade-udvidelsesværktøjet';
+$lang['porg_guides_manual_update_step3_list2'] = 'udpak script\'et prep21up.php, og overfør det til roden af din Piwigo-installation.';
+$lang['porg_guides_manual_update_step3_list3'] = 'åbn prep21up.php fra din webbrowser, http://example.com/photos/prep21up.php - og du vil modtage arkivet “upgrade21.zip”';
+$lang['porg_guides_manual_update_step3_list4'] = 'På din computer udpakkes mappen "local" fra “upgrade21.zip” til mappen "piwigo" (udpakket under trin 1).';
