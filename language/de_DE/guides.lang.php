ff680ca32ea733fa4f49379916d180ceececa450
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-23 14:02:08 +0200

    Update de_DE, thanks to Joergen

---
+++ language/de_DE/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = 'Willkommen bei Piwigo Guides. Hier finden Sie die Grundlagen zur Installation und Aktualisierung Ihres Piwigo. Einige Hilfestellungen finden Sie auch direkt in Ihrem Piwigo auf den "Helfen Sie mir!"-Seiten der Administrationsoberfläche oder überall dort, wo Sie ein grafisches Fragezeichen sehen. Ein Wiki deckt auch die Entwicklerdokumentation ab.';
