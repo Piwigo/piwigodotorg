e78231301e1dc504e0dcaf851931271f2cf925c0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-07-24 22:24:46 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/manual_update.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_time_5'] = '15-120 min';
+$lang['porg_guides_manual_update_time_4'] = '2 min';
+$lang['porg_guides_manual_update_time_3'] = '1 min';
+$lang['porg_guides_manual_update_time_2'] = '5 min';
+$lang['porg_guides_manual_update_time_1'] = '3-5min';
+$lang['porg_guides_manual_update_step9_paragraph1'] = 'Je begint gewend te raken aan onze laatste release, vergeet niet jouw galerij te ontgrendelen om toegang te geven aan bezoekers';
+$lang['porg_guides_manual_update_step8_paragraph5'] = 'Vergeet niet dat jouw leden of bezoekers een specifiek geselecteerd thema kunnen hebben dat niet compatibel is met jouw nieuwe release. Je kunt hun thema waarschijnlijk beter resetten op de pagina Admin-gebruikers';
+$lang['porg_guides_manual_update_step9'] = 'Stap 9 - Ontgrendel jouw galerij';
+$lang['porg_guides_manual_update_step8_paragraph4'] = 'Houd voor thema\'s een beheerderspagina geopend in jouw browser voor het geval je één van jouw tests moet terugdraaien.';
+$lang['porg_guides_manual_update_step8_paragraph3'] = 'Probeer eerst een upgrade te vinden; de plug-ins tabbladen kunnen jehelpen.';
+$lang['porg_guides_manual_update_step8_paragraph2'] = 'Je ziet een lijst met al je vorige plug-ins; sommige zijn volledig geïntegreerd in de kern (zoals Plugins Manager), andere zijn nu gedistribueerd (zoals LocalFiles Editor), maar elke gedeactiveerde plug-in heeft een ​​goede reden.';
+$lang['porg_guides_manual_update_step8_paragraph1'] = 'Jouw eerste controles kunnen even duren omdat je niet op de hoogte bent van alle wijzigingen';
+$lang['porg_guides_manual_update_step8'] = 'Stap 8 - Controleer het upgrade-resultaat';
+$lang['porg_guides_manual_update_step7_paragraph5'] = 'Plug-ins die actief waren vóór de upgrade, worden uitgeschakeld naar de inactieve status, omdat ze mogelijk niet werken met de nieuwe release en hun eigen specifieke upgrade nodig hebben.';
+$lang['porg_guides_manual_update_step7_paragraph4'] = 'Jouw vorige release wordt geïdentificeerd en je krijgt een overzicht van de upgradebewerkingen;';
+$lang['porg_guides_manual_update_step7_paragraph3'] = 'Om een ​​upgrade door een andere bezoeker te voorkomen, word je uitgenodigd om in te loggen';
+$lang['porg_guides_manual_update_step7_paragraph2'] = 'Open in jouw webbrowser het script "upgrade.php" en volg de gids, http://example.com/photos/upgrade.php';
+$lang['porg_guides_manual_update_step7_paragraph1'] = 'Upgrade starten';
+$lang['porg_guides_manual_update_step7_caption2'] = 'Memopagina upgraden.';
+$lang['porg_guides_manual_update_step7_caption1'] = 'Upgradepagina starten.';
+$lang['porg_guides_manual_update_step7'] = 'Stap 7 - Database-upgrade';
+$lang['porg_guides_manual_update_step6_paragraph2'] = 'Controleer of jouw FTP-client geen fout heeft gevonden.';
+$lang['porg_guides_manual_update_step6_paragraph1'] = 'Gebruik jouw standaard FTP-client om de nieuwe release, dat wil zeggen de inhoud van de "piwigo" -map (uitgepakt tijdens stap 1 en bijgewerkt tijdens stap 3), te uploaden naar de vorige Piwigo-installatiemap.';
+$lang['porg_guides_manual_update_step6'] = 'Stap 6 - FTP-upload';
+$lang['porg_guides_manual_update_step5_paragraph1'] = 'Verwijder alle bestanden van jouw huidige Piwigo-installatie, met de volgende uitzonderingen, verwijder deze mappen NIET:';
+$lang['porg_guides_manual_update_step5_list7'] = '_data';
+$lang['porg_guides_manual_update_step5_list6'] = 'Lokaal';
+$lang['porg_guides_manual_update_step5_list5'] = 'Sjabloon-extensie';
+$lang['porg_guides_manual_update_step5_list4'] = 'Thema\'s';
+$lang['porg_guides_manual_update_step5_list3'] = 'Plugins';
+$lang['porg_guides_manual_update_step5_list2'] = 'Uploaden';
+$lang['porg_guides_manual_update_step5_list1'] = 'Galerijen';
+$lang['porg_guides_manual_update_step5'] = 'Stap 5 - Reiniging';
+$lang['porg_guides_manual_update_step4_paragraph3'] = 'Vanaf nu zouden niet-beheerders van elke galerieversie iets moeten zien als: “De galerij is vergrendeld voor onderhoud. Kom alstublieft later terug.".';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Met Piwigo 2.3 of eerdere versie: Configuratie> Algemeen> Galerij vergrendelen. Voor Piwigo 2.4+: Tools> Onderhoud.';
+$lang['porg_guides_manual_update_step4_paragraph1'] = 'Vergrendel de galerij';
+$lang['porg_guides_manual_update_step4'] = 'Stap 4 - Galerijvoorbereiding';
+$lang['porg_guides_manual_update_step4_caption1'] = 'Galerij vergrendelen';
+$lang['porg_guides_manual_update_step3_paragraph2'] = 'Maak alleen een back-up van jouw aangepaste bestanden of van alle bestanden';
+$lang['porg_guides_manual_update_step3_paragraph1'] = 'Sla deze stap over als je Piwigo 2.1 of hoger gebruikt.';
+$lang['porg_guides_manual_update_step1'] = 'Stap 1 - Pakketvoorbereiding';
+$lang['porg_guides_manual_update_step3_list4'] = 'Pak op jouw computer de map "local" uit "upgrade21.zip" uit in de map "piwigo" (uitgepakt tijdens stap 1).';
+$lang['porg_guides_manual_update_step3_list3'] = 'open prep21up.php vanuit jouw webbrowser http://example.com/photos/prep21up.php en je ontvangt een "upgrade21.zip" -archief';
+$lang['porg_guides_manual_update_step3_list2'] = 'pak het script prep21up.php uit en breng het over naar de root van jouw Piwigo-installatie';
+$lang['porg_guides_manual_update_step3_list1'] = 'Download de Prepare 2.1 Upgrade extensie tool';
+$lang['porg_guides_manual_update_step3'] = 'Stap 3 - Voorbereiding van bestandsserver';
+$lang['porg_guides_manual_update_step2_caption1'] = 'PhpMyAdmin aanbevolen opties.';
+$lang['porg_guides_manual_update_step2'] = 'Stap 2 - Database-voorbereiding (aanbevolen)';
+$lang['porg_guides_manual_update_step1_paragraph3'] = 'Zorg er in ieder geval voor dat de back-up volledig is en met succes is voltooid (het resultaat wordt soms ingekort vanwege serverlimieten)';
+$lang['porg_guides_manual_update_step1_paragraph2'] = 'Als je een versie uit de PhpWebGallery 1.7-familie of hoger gebruikt, kun je de DB Backup-plug-in gebruiken. Gebruikers van eerdere releases zouden op een andere manier een back-up van hun tabellen moeten maken, bijvoorbeeld met phpMyAdmin.';
+$lang['porg_guides_manual_update_step1_paragraph1'] = 'Maak een back-up van jouw huidige tabellen';
+$lang['porg_guides_manual_update_step1_list3'] = 'Verwijder de standaard "piwigo / local" -map.';
+$lang['porg_guides_manual_update_step1_list2'] = 'Pak op jouw computer de map "piwigo" uit';
+$lang['porg_guides_manual_update_step10_paragraph2'] = 'Niets hoeft te worden verwijderd na een upgrade. De "upgrade.php" zelf mag niet worden verwijderd. Onthoud dat het bericht "Geen upgrade vereist" en het aanmeldingsproces jouw galerij beschermen.';
+$lang['porg_guides_manual_update_step1_list1'] = '<a href="%s"> Download het nieuwste Piwigo-releasearchief </a>';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Niets';
+$lang['porg_guides_manual_update_step10'] = 'Stap 10 - Reiniging na upgrade';
+$lang['porg_guides_manual_update_paragraph1'] = 'Deze procedure is compatibel met elke versie groter of gelijk aan 1.4. Vraag voor een oudere versie om hulp op het forum.';
+$lang['porg_guides_manual_update_info2'] = 'Ons advies: maak de selectie van "Uitgebreide invoegingen" ongedaan of verminder "Maximale lengte van gemaakte zoekopdrachten".';
+$lang['porg_guides_manual_update_info1'] = 'Controleer de huidige <a href="%s"> vereisten </a> van Piwigo. Vooral als jouw Php / MySQL-configuratie NIET voldoende is, ga dan niet verder, want het upgradeproces zal mislukken';
