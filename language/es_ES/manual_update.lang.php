5f35d14ce339c33221629940991aaf7983c02f68
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-22 08:08:52 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/manual_update.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Nada';
+$lang['porg_guides_manual_update_time_5'] = '15-120 min.';
+$lang['porg_guides_manual_update_time_4'] = '2 min.';
+$lang['porg_guides_manual_update_time_3'] = '1 min.';
+$lang['porg_guides_manual_update_time_2'] = '5 min.';
+$lang['porg_guides_manual_update_time_1'] = '3-5 min.';
