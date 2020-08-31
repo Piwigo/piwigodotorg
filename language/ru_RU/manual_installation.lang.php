89b5011d0f15103a9fc4263912d96c8b09a6a44e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-31 17:03:40 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/manual_installation.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_step1'] = 'Шаг 1 - Скачать полный архив';
+$lang['porg_guides_manual_installation_step2'] = 'Шаг 2 - Загрузить содержание архива';
