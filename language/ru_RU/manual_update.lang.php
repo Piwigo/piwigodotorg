552035c0cadb275d20a8fbbd811dd6a78ca82061
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-31 17:03:54 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/manual_update.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_step3_paragraph1'] = 'Если у Вас Piwigo 2.1 или старше, прорустите этот шаг.';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Для Piwigo 2.3 или более ранней версии: Конфигурация-Главная-Блокировка галереи. Для Piwigo 2.4 и старше: Инструменты-Обслуживание.';
+$lang['porg_guides_manual_update_step6_paragraph2'] = 'Убедитесь, что Ваш FTP-клиент отработал без ошибок.';
+$lang['porg_guides_manual_update_step7_paragraph1'] = 'Запуск обновления';
+$lang['porg_guides_manual_update_step7'] = 'Шаг 7 - Обновление базы данных';
+$lang['porg_guides_manual_update_step7_caption1'] = 'Страница обновления.';
+$lang['porg_guides_manual_update_step7_caption2'] = 'Обновление страницы заметок.';
+$lang['porg_guides_manual_update_step5_list5'] = 'Шаблон-расширение';
+$lang['porg_guides_manual_update_step5_list6'] = 'Локально';
+$lang['porg_guides_manual_update_step5_list7'] = '_data';
+$lang['porg_guides_manual_update_step5_paragraph1'] = 'Удалите все файлы текущей установки Piwigo, со следующими исключениями, НЕ удаляйте эти каталоги:';
+$lang['porg_guides_manual_update_step6'] = 'Шаг 6 - Загрузка по FTP';
+$lang['porg_guides_manual_update_step3_paragraph2'] = 'Резервное копирование только выбранных или всех файлов';
+$lang['porg_guides_manual_update_step5_list1'] = 'Галереи';
+$lang['porg_guides_manual_update_step5_list2'] = 'Загрузка';
+$lang['porg_guides_manual_update_step5_list3'] = 'Плагины';
+$lang['porg_guides_manual_update_step5_list4'] = 'Темы';
+$lang['porg_guides_manual_update_step4'] = 'Шаг 4 - Подготовка Галереи';
+$lang['porg_guides_manual_update_step4_caption1'] = 'Блокировка галереи';
+$lang['porg_guides_manual_update_step4_paragraph1'] = 'Блокировка галереи';
+$lang['porg_guides_manual_update_step5'] = 'Шаг 5 - Очистка';
+$lang['porg_guides_manual_update_info2'] = 'Наш совет: оставить пустым "Расширенные вставки" или уменьшить "Максимальную длину созданных запросов".';
+$lang['porg_guides_manual_update_step1'] = 'Шаг 1 - Подготовка пакета';
+$lang['porg_guides_manual_update_step10'] = 'Шаг 10 - очистка обновления Почты';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Ничего ';
+$lang['porg_guides_manual_update_step1_paragraph1'] = 'Резервное копирование текущих таблиц';
+$lang['porg_guides_manual_update_step2'] = 'Шаг 2 - Подготовка базы данных (рекомендуется)';
+$lang['porg_guides_manual_update_step2_caption1'] = 'Рекомендованые опции PhpMyAdmin.';
+$lang['porg_guides_manual_update_step3'] = 'Шаг 3 - Подготовка файлового сервера';
