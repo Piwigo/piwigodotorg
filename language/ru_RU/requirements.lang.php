ab0f0dde3dd869d899db539839f1e85e2b98f5ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-31 14:29:59 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/requirements.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_requirements_minimal_sixth'] = 'Достаточно места на диске для изображений: в дополнение к файлам, которые Вы загружаете, Piwigo будет хранить "кратный размер" в каталоге кэша Вашего сервера.';
+$lang['porg_guides_requirements_paragraph'] = 'Piwigo требует веб-хостинга для запуска. Для оценки галереи со всеми возможностями (установка, хостинг, резервные копии), или если Вы просто хотите попробовать Piwigo без установки, Вы можете попробовать <a href="%s">открыть бесплатный пробный аккаунт на Piwigo.com</a>.';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Графическая библиотека: рекомендуется ImageMagick, но, в принципе, GD в комплекте с PHP, также справятся.';
+$lang['porg_guides_requirements_optional_first'] = 'exiftool требуется для плагина Write Metadata или любых других плагинов, использующих метаданные EXIF/IPTC ';
+$lang['porg_guides_requirements_minimal_first'] = 'Веб-сервер типа Nginx или Apache';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (или эквивалент с MariaDB)';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg требуется для плагина VideoJS для создания видео-постеров';
+$lang['porg_guides_requirements_optional_third'] = 'pdftoppm рекомендуется для создания афиш из файлов PDF (вместо ImageMagick)';
+$lang['porg_guides_requirements_minimal'] = 'Минимальные требования';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (рекомендуем 7.0 или 7.1)';
+$lang['porg_guides_requirements_optional'] = 'Дополнительные (необязательные) требования';
