e9c875573ee96927434a629c0ff7532ef61e7c8d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-31 14:30:13 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/netinstall.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_image_caption1'] = 'Подключение загрузки';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Выберите имя каталога для галереи на Вашем сайте. По умолчанию Piwigo будет установлен в каталоге piwigo. Если Вы оставите поле назначения пустым, Piwigo будет установлен в том же месте, куда Вы перенесли скрипт NetInstall.';
+$lang['porg_guides_netinstall_info1'] = 'Процесс Netinstall, возможно, не работает на Вашем веб-сервере, в зависимости от уровня его особенностей. Если у Вас не получается заставить его работать, просто переключитесь на <a href="%s">ручную установку</a>.';
+$lang['porg_guides_netinstall_paragraph1'] = 'NetInstall требует передачи всего одного файла на Ваш веб-сервер. Содержащийся в нем скрипт загрузит архив Piwigo из piwigo.org, затем извлечет его содержимое и запустит установщик.';
+$lang['porg_guides_netinstall_image_caption2'] = 'Выберите имя папки для Вашей галереи';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">Скачать скрипт NetInstall.</a>';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Выберите файл piwigo-netinstall.php на жестком диске и перенесите его на веб-сервер';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'После того, как скрипт NetInstall был перенесен, перейдите на его веб-адрес, например, с веб-браузером';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Запустите действие "Извлечь и распаковать Piwigo", а затем "Установить Piwigo сейчас"';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo установлен ';
+$lang['porg_guides_netinstall_step1'] = 'Шаг 1 - Скачивание скрипта NetInstall ';
+$lang['porg_guides_netinstall_step2'] = 'Шаг 2 - Передача скрипта NetInstall';
