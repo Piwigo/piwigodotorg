a2fddb4dbef6c3a18d92cdc3f3fb9fc0ea5ba806
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-09-04 18:35:18 +0200

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/requirements.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_requirements_optional'] = 'Wymagania opcjonalne';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg jest wymagane dla wtyczki VideoJS lub do tworzenia plakatów wideo.';
+$lang['porg_guides_requirements_minimal'] = 'Wymagania minimalne';
+$lang['porg_guides_requirements_minimal_first'] = 'Serwer WWW jak Apache lub Nginx';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (lub inna, kompatybilna, jak MariaDB)';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (rekomendowana 7.0 lub 7.1)';
