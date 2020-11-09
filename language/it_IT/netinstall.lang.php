7fa7deae2fc799a08285cb5be888e88ec3cb40be
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-09 12:17:27 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/netinstall.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_image_caption1'] = 'Connessione e trasferimento dei file con FileZilla';
+$lang['porg_guides_netinstall_image_caption2'] = 'Selezione della posizione della galleria';
+$lang['porg_guides_netinstall_image_caption3'] = 'La galleria è installata';
+$lang['porg_guides_netinstall_info1'] = 'L\'installazione automatica potrebbe non essere fattibile a seconda del livello di servizi offerti dal tuo host. È quindi necessario eseguire un \'<a href="%s">installazione manuale</a> sul tuo spazio web.';
+$lang['porg_guides_netinstall_paragraph1'] = 'NetInstall richiede il caricamento di un singolo file sul tuo server web. Lo script che contiene scaricherà l\'archivio Piwigo su piwigo.org, quindi estrarrà il contenuto e avvierà il programma di installazione.';
+$lang['porg_guides_netinstall_step1'] = 'Fase 1 - Scaricare il programma di installazione';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">Scarica il programma di installazione</a>
+';
+$lang['porg_guides_netinstall_step2'] = 'Fase 2 - depositare il programma di installazione sul proprio spazio web';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Seleziona il file piwigo-netinstall.php sul tuo disco rigido e caricalo sul server web';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'Una volta depositato il programma di installazione, vai all\'indirizzo della sua posizione. Per esempio';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Seleziona la posizione della galleria sul tuo sito. Di default, la galleria verrà installata nella directory piwigo: puoi installare la galleria nella radice del tuo sito lasciando quest\'area vuota.';
+$lang['porg_guides_netinstall_step3_paragraph3'] = 'Esegui l\'azione "Recupera e decomprimi Piwigo", quindi "Installa Piwigo ora"';
