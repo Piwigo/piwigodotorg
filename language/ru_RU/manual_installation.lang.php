6fa94acf3b78a611b1b4806768bf7f61938b1fd5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-12-01 06:51:22 +0100

    Update ru_RU, thanks to Konve

--- language/ru_RU/manual_installation.lang.php
+++ language/ru_RU/manual_installation.lang.php
@@ -7,3 +7,11 @@
 // +-----------------------------------------------------------------------+
 $lang['porg_guides_manual_installation_step1'] = 'Шаг 1 - Скачать полный архив';
 $lang['porg_guides_manual_installation_step2'] = 'Шаг 2 - Загрузить содержание архива';
+$lang['porg_guides_manual_installation_image_caption1'] = 'Подключение и передача';
+$lang['porg_guides_manual_installation_info3'] = 'Вы можете установить Piwigo и в корне вебсайта, каталог "Images" не обязателен. Независимо от названия каталога, которое Dы выберете, рекомендуется избегать показа номера релиза Piwigo.';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'В процессе ручной установки Вы скачиваете полный архив Piwigo, извлекаете его содержимое и переносите его на свой веб-сервер.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Скачать полный архив</a> и распаковать его.';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Переместите содержимое архива на Ваш веб-сервер с помощью любого FTP-клиента.';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1.Создайте каталог "Images" на Вашем веб-сервере.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2. На жестком диске откройте извлеченную папаку "piwigo".';
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3. Выберите все файлы и перенесите их на сервер в каталог "Images".';
