6f4e193ed411847693d68cabfe0d311f1eda516f
plegall <plg@piwigo.org>
2020-12-01 15:45:41 +0100

    increase recommended version of PHP/MySQL

--- language/fr_FR/requirements.lang.php
+++ language/fr_FR/requirements.lang.php
@@ -3,8 +3,8 @@
 $lang['porg_guides_requirements_paragraph'] = 'Piwigo a besoin d\'un hébergement web pour fonctionner. Pour une solution de galerie photo “clef en main” (installation, hébergement, mises à jour, sauvegardes…), ou si vous souhaitez simplement essayer Piwigo sans l\'installer, <a href="%s">ouvrez un compte d\'essai gratuit sur Piwigo.com</a>.';
 $lang['porg_guides_requirements_minimal'] = 'Le minimum requis';
 $lang['porg_guides_requirements_minimal_first'] = 'un serveur web comme Apache ou Nginx';
-$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5 (ou l\'équivalent avec MariaDB)';
-$lang['porg_guides_requirements_minimal_third'] = 'PHP 5.3 (version 7.2 recommandée, pour ses performances)';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5.6+ ou MariaDB 10.1+. MySQL 5.0 fonctionne mais n\'est plus officiellement maintenue.';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 7.3+. Piwigo peut fonctionner avec PHP 5.3+ mais ces anciennes versions ne sont plus officiellement supportées et vous exposent à des failles de sécurité. Voir <a href="https://www.php.net/supported-versions.php" target="_blank">officially PHP supported versions</a> (en anglais).';
 $lang['porg_guides_requirements_minimal_fourth'] = 'Une bibliothèque graphique: ImageMagick est recommandé pour ses performances et sa qualité d\'image mais GD, souvent fourni avec PHP, peut également faire l\'affaire.';
 $lang['porg_guides_requirements_minimal_fifth'] = 'un logiciel client FTP sera nécessaire pour télécharger les fichiers (netinstall ou package complet): l\'équipe Piwigo recommande FileZilla comme logiciel client FTP, car il est gratuit comme Piwigo et compatible avec Windows, Mac et Linux.';
 $lang['porg_guides_requirements_minimal_sixth'] = 'Assez d\'espace disque pour vos photos: en plus des photos que vous téléchargez, Piwigo stockera les "tailles multiples" dans un répertoire cache de votre serveur.';
