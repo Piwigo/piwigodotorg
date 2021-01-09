1e7f668a1f08e1b9d64a2f7adbac48df12e381d6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-09 14:29:18 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-2.10.1.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1097'] = '[batch manager] calcolo checksum, la dimensione del blocco dovrebbe essere la stessa';
+$lang['porg_issue_1098'] = '[batch manager] calcolo checksum, ridurre la dimensione del blocco';
+$lang['porg_issue_1099'] = '[batch manager] TypeError: associated_categories est null';
+$lang['porg_issue_1089'] = 'Jshrink utilizza funzioni deprecate e interrompe javascript';
+$lang['porg_issue_1090'] = 'L\'avviso di checksum mancante dovrebbe spaventare gli utenti';
+$lang['porg_issue_1092'] = 'Errore: errore di sintassi, espressione non riconosciuta: input [nome=pwg_token]';
