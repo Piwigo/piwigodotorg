bd5d4c8ee3c3a8c669d3f60c79d0b9ae7b5e1952
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-09 14:29:04 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/requirements.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_requirements_paragraph'] = 'Piwigo ha bisogno del web hosting per funzionare. Per una soluzione di galleria fotografica "chiavi in ​​mano" (installazione, hosting, aggiornamenti, backup ...), o se vuoi semplicemente provare Piwigo senza installarlo, <a href="%s"> apri un account con prova gratuita su Piwigo.com </a>.';
+$lang['porg_guides_requirements_optional_third'] = 'pdftoppm sarà consigliato da Piwigo 2.10 per generare immagini che rappresentano PDF (invece di ImageMagick per questa specifica attività)';
+$lang['porg_guides_requirements_optional_second'] = 'ffmpeg è richiesto per il plug-in VideoJS durante la creazione del "poster" (immagine che rappresenta il video)';
+$lang['porg_guides_requirements_optional_first'] = 'exiftool è richiesto per il plug-in Write Metadata e altri plug-in che manipolano i metadati EXIF ​​/ IPTC';
+$lang['porg_guides_requirements_optional'] = 'Prerequisiti facoltativi';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Abbastanza spazio su disco per le tue foto: oltre alle foto che carichi, Piwigo memorizzerà "più dimensioni" in una directory della cache sul tuo server.';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 7.3+. Piwigo può funzionare con PHP 5.3+ ma queste versioni precedenti non sono più ufficialmente supportate e ti espongono a falle di sicurezza. Vedi <a href="https://www.php.net/supported-versions.php" target="_blank"> versioni ufficialmente supportate da PHP </a>.';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5.6+ o MariaDB 10.1+. MySQL 5.0 funziona ma non è più mantenuto ufficialmente.';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Una libreria grafica: ImageMagick è consigliato per le sue prestazioni e la qualità dell\'immagine, ma anche GD, spesso fornito con PHP, può fare il trucco.';
+$lang['porg_guides_requirements_minimal_first'] = 'un server web come Apache o Nginx';
+$lang['porg_guides_requirements_minimal_fifth'] = 'Per scaricare i file sarà necessario un software client FTP (netinstall o pacchetto completo): il team di Piwigo consiglia FileZilla come software client FTP, poiché è gratuito come Piwigo e compatibile con Windows, Mac e Linux.';
+$lang['porg_guides_requirements_minimal'] = 'Requisiti minimi';
