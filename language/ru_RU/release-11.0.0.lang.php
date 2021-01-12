f476d017ac19c9b0e2afc111f5e14bb945968a94
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-12 17:57:50 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-11.0.0.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v11_technical_features10'] = 'Плагин Has Settings';
+$lang['porg_v11_technical_features12'] = 'подробнее';
+$lang['porg_v11_technical_features13'] = 'Меньше активированных языков';
+$lang['porg_v11_info2_title'] = 'Дамп базы данных удален';
+$lang['porg_v11_mobile_image1_label'] = 'Пока эта функция работает только на устройствах iOS, то есть на iPhone или iPad.';
+$lang['porg_v11_requirements_title'] = 'Требования проверены перед обновлением';
+$lang['porg_v11_tag_manager_title'] = 'Менеджер тэгов';
+$lang['porg_v11_info3_title'] = 'Вход со своим адресом электронной почты';
+$lang['porg_v11_intro1'] = 'Готовы к множеству улучшений?';
+$lang['porg_v11_intro3'] = 'Готовьтесь к приятным сюрпризам!';
+$lang['porg_v11_mobile_title'] = 'Продвижение мобильного приложения';
+$lang['porg_v11_move_albums_title'] = 'Перемещение альбомов';
+$lang['porg_v11_photo_edition_title'] = 'Редактирование изображения';
+$lang['porg_v11_plugin_manager_title'] = 'Менеджер плагинов';
+$lang['porg_v11_related_albums_title'] = 'Похожие альбомы';
+$lang['porg_v11_technical_features4'] = 'Быстрый поиск и под-альбомы';
+$lang['porg_v11_technical_features6'] = 'Плагин "Has Settings" ';
+$lang['porg_v11_theme_manager_title'] = 'Менеджер тем';
+$lang['porg_v11_user_manager_title'] = 'Менеджер пользователей, умный выбор';
+$lang['porg_v11_album_search_title'] = 'Инструмент поиска альбома';
+$lang['porg_v11_badge_new'] = 'новинка';
+$lang['porg_v11_badge_redesign'] = 'редизайн';
+$lang['porg_v11_badge_refresh'] = 'освежение';
+$lang['porg_v11_batch_manager_title'] = 'Пакетный менеджер';
+$lang['porg_v11_custom_alerts_title'] = 'Пользовательские оповещения';
+$lang['porg_v11_group_manager_image2_label'] = 'Новая функция: теперь вы можете связать пользователей напрямую, без использования менеджера пользователя';
