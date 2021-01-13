7432ca5e2148a2e4de7cc1f00b8a56cf600925bd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-13 18:13:00 +0100

    Update de_DE, thanks to madmas

---
+++ language/de_DE/release-11.0.0.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v11_info1_title'] = 'Version "springt" von 2.10 auf 11';
+$lang['porg_v11_technical_features1'] = 'Bitte beachten Sie, das detailliertere Dokumentation verfügbar ist für';
+$lang['porg_v11_technical_features12'] = 'mehr erfahren';
+$lang['porg_v11_technical_features13'] = 'Weniger Sprachen aktiviert';
+$lang['porg_v11_technical_features19'] = 'API Änderungen';
+$lang['porg_v11_technical_features2'] = 'Technische Änderungen in Piwigo 11';
+$lang['porg_v11_technical_features20'] = 'Aufrufe von piwigo.org ausschliesslich über HTTPS';
+$lang['porg_v11_technical_features4'] = 'Schnellsuche und Unteralben';
+$lang['porg_v11_theme_manager_title'] = 'Theme-Verwaltung';
+$lang['porg_v11_group_manager_image2_label'] = 'Neues Feature: Benutzer können nun direkt verknüpft werden, ohne die Benutzerverwaltung verwenden zu müssen';
+$lang['porg_v11_batch_manager_image2_label'] = 'Die "Filter" Ansicht des Batch-Manager hat ein neues Design zum Hinzufügen von Filtern erhalten.';
+$lang['porg_v11_batch_manager_title'] = 'Batch-Manager';
+$lang['porg_v11_batch_manager_image1_label'] = 'Der Batch-Manager in Piwigo 11 hat ein überarbeitetes Design, das den Richlinien der anderen Ansichten der neuen Generation folgt.';
+$lang['porg_v11_badge_redesign'] = 'Neugestaltung';
+$lang['porg_v11_badge_refresh'] = 'aktualisieren';
+$lang['porg_v11_album_search_title'] = 'Albumsuche';
+$lang['porg_v11_album_search_image1_label'] = 'Dieser Version Piwigo 11 enthält eine neues Werkzeug für die Suche  in allen vorhandenen Alben. Ich denke, das dies für viele Administratoren <b>sehr</b> nützlich sein wird.';
+$lang['porg_v11_tag_manager_title'] = 'Schlagwort-Verwaltung';
+$lang['porg_v11_info3_title'] = 'Meden Sie sich mit Ihrer E-Mail Adresse an';
+$lang['porg_v11_requirements_title'] = 'Anforderungen vor dem Update geprüft';
+$lang['porg_v11_related_albums_title'] = 'verwandte Alben';
+$lang['porg_v11_plugin_manager_title'] = 'Pluginverwaltung';
+$lang['porg_v11_photo_edition_title'] = 'Fotos bearbeiten';
+$lang['porg_v11_move_albums_title'] = 'Alben verschieben';
+$lang['porg_v11_group_manager_title'] = 'Gruppen-Manager';
+$lang['porg_v11_badge_new'] = 'neu';
+$lang['porg_v11_album_manager_title'] = 'Album-Manager';
+$lang['porg_v11_album_edition_title'] = 'Album bearbeiten';
+$lang['porg_v11_album_manager_image1_label'] = 'Der Album-Manager folgt dem gleichen Design wie der in einer früheren Version hinzugefügte Gruppen-Manager.';
+$lang['porg_v11_album_edition_image1_label'] = 'Piwigo 11 bietet nun eine modernere und farbenfrohere Seite um Alben zu bearbeiten.';
+$lang['porg_v11_activity_chart_title'] = 'Aktivitätsdiagramm';
+$lang['porg_v11_activity_chart_image2_label'] = 'Je mehr Aktivität Sie haben, desto größer ist der Kreis. Piwigo zeigt Details zu jedem Tag mit Aktivität.';
+$lang['porg_v11_activity_chart_text'] = 'Mit Piwigo 2.10 wurde damit begonnen, viele Administrationstätigkeiten zu bieten, wie zum Beispiel das Bearbeiten von Alben und das Anlegen von Gruppen...Piwigo 11 kann nun ein Diagramm zu all diesen Aktivitäten anzeigen.';
+$lang['porg_v11_activity_chart_image1_label'] = 'Das Aktivitätsdiagramm wird auf dem Dashboard, neben der allgemeinen Statistik und den Quick-Links, angezeigt.';
