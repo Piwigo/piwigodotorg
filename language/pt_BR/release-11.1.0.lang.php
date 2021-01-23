c9d5fedb77ad0f9c0564d81571d2466ef38dd2b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-23 18:37:09 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-11.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1257'] = 'admin/batch_manager.php alguns erros/advertências em php7.4';
+$lang['porg_issue_1284'] = '\"Infinite loop?\" erro ao ir na página 2 dos álbuns';
+$lang['porg_issue_1287'] = 'Erro de sintaxe no arquivo do idioma norueguês após a atualização para o Piwigo 11';
+$lang['porg_issue_1290'] = '[11.x admin/intro] popen() foi desativado por razões de segurança';
+$lang['porg_issue_1291'] = '[11.x] Erro de análise: erro de sintaxe, inesperado \'[\'';
+$lang['porg_issue_1292'] = 'palavras de escape `groups` não podem ser desserializadas';
+$lang['porg_issue_1293'] = '11.x Painel, erro fatal: Tamanho de memória permitido';
+$lang['porg_issue_1294'] = '[11.x] código de download incorreto para atualização';
