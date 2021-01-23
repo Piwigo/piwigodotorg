ddd512f457a42685e9380e058858699339a022d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-23 18:37:24 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-11.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1294'] = '[11.x] onjuiste downloadcode voor upgrade';
+$lang['porg_issue_1293'] = '11.x Dashboard, Fatale fout: toegestane geheugengrootte';
+$lang['porg_issue_1292'] = 'Het ontsnapte woord `groepen` kan niet worden genegeerd';
+$lang['porg_issue_1291'] = '[11.x] Parseerfout: syntaxisfout, onverwachte \'[\'';
+$lang['porg_issue_1290'] = '[11.x admin / intro] popen () is om veiligheidsredenen uitgeschakeld';
+$lang['porg_issue_1287'] = 'Syntaxisfout in Noors taalbestand na upgrade naar Piwigo 11';
+$lang['porg_issue_1284'] = '\"Oneindige lus?\" Fout bij het openen van pagina 2 van albums';
+$lang['porg_issue_1257'] = 'admin / batch_manager.php enkele fouten / waarschuwingen op php7.4';
