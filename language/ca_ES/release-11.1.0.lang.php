520a02a17e678bdd3ddbae615b30ce3d3b7f5685
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-23 18:37:38 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-11.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1257'] = 'admin/batch_manager.php generava alguns errors/avisos en php 7.4';
+$lang['porg_issue_1284'] = 'Error \"Bucle infinit?\" en passar a la pàgina 2 dels àlbums';
+$lang['porg_issue_1287'] = 'Error de sintaxi en el fitxer de l\'idioma noruec després d\'actualitzar a Piwigo 11';
+$lang['porg_issue_1290'] = '[11.x admin/intro] S\'ha deshabilitat popen() per raons de seguretat';
+$lang['porg_issue_1291'] = '[11.x] Error de procés: error de sintaxi, \'[\' inesperat';
+$lang['porg_issue_1292'] = 'la paraula amb escape `groups` no es pot deserialitzar';
+$lang['porg_issue_1293'] = 'Panel de control 11.x, error fatal: mida de memòria permesa';
+$lang['porg_issue_1294'] = '[11.x] codi de descàrrega incorrecte per a l\'actualització';
