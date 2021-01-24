d4e36ef67b7f270d52a3786699f613a8a072c4aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-24 13:34:32 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/automatic_update.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_paragraph9'] = 'Przejdź do Administracja > Specjalne > Wtyczki > Piwigo Auto Upgrade i postępuj zgodnie ze wskazówkami; możesz wybrać uaktualnianie w ramach tej samej "gałęzi" lub do najnowszej wersji Piwigo.';
+$lang['porg_guides_automatic_update_paragraph7'] = 'Administracja > Specjalne > Wtyczki > Inne wtyczki i przeprowadź instalację Piwigo Auto Upgrade. Jeżeli wtyczka jest już zainstalowana, upewnij się że używasz jej najnowszej wersji.';
+$lang['porg_guides_automatic_update_paragraph4'] = 'Przejdź do Administracja > Wtyczki > Zarządzaj > Inne wtyczki i zainstaluj Piwigo Auto Upgrade. Jeżeli wtyczka jest już zainstalowana, upewnij się że używasz jej najnowszej wersji na ekranie Administracja > Wtyczki > Zarządzaj > Sprawdź dostępność aktualizacji.';
+$lang['porg_guides_automatic_update_paragraph6'] = 'Przejdź do Administracja > Wtyczki > Piwigo Auto Upgrade i postępuj zgodnie ze wskazówkami.';
+$lang['porg_guides_automatic_update_paragraph12'] = 'Przejdź do Administracja > Specjalne > Wtyczki > Piwigo Auto Upgrade i postępuj zgodnie ze wskazówkami; możesz wybrać uaktualnianie w ramach tej samej "gałęzi" lub do najnowszej wersji Piwigo.';
+$lang['porg_guides_automatic_update_paragraph11'] = 'Przejdź do Administracja > Specjalne > Wtyczki > Administracja i wybierz akcje "Instalacja", a następnie "Aktywacja" dla wtyczki “Piwigo Auto Upgrade”.';
+$lang['porg_guides_automatic_update_paragraph10'] = 'Pobierz wtyczkę Piwigo Auto Upgrade i wyodrębnij zawartość w folderze "plugins".';
+$lang['porg_guides_automatic_update_paragraph1'] = 'To jest zalecany sposób aktualizacji Twojego Piwigo do najnowszej wersji. Nie masz już więc żadnej wymówki, aby odłożyć uaktualnienie. Uruchomienie aktualizacji do najnowszej i najbezpieczniejsze wersji Piwigo naprawdę zabierze mniej niż minutę.';
+$lang['porg_guides_automatic_update_time_1'] = '30 sekund';
+$lang['porg_guides_automatic_update_step1'] = 'Jeden krok';
+$lang['porg_guides_automatic_update_paragraph8'] = 'Przejdź do Administracja > Specjalne > Wtyczki aby zainstalować i aktywować nową wtyczkę.';
+$lang['porg_guides_automatic_update_paragraph3'] = 'Przejdź do Administracja > Narzędzia > Aktualizacja i postępuj zgodnie ze wskazówkami.';
+$lang['porg_guides_automatic_update_paragraph5'] = 'Przejdź do Administracja > Wtyczki > Zarządzaj > Lista wtyczek aby zainstalować i aktywować nową wtyczkę.';
+$lang['porg_guides_automatic_update_paragraph13'] = 'Jeżeli używasz wcześniejszej wersji, zamiast tego przejdź do Ręcznej aktualizacji.';
+$lang['porg_guides_automatic_update_info1'] = 'Zawsze zrób kopię zapasową swojej bazy i plików. Najlepiej, żeby tworzyła się automatycznie, regularnie. Jeżeli coś złego stanie się w czasie uaktualnienia, będziesz w stanie powrócić do kopii.';
+$lang['porg_guides_automatic_update_caption1'] = 'Tak, tylko dwa kliknięcia';
+$lang['porg_guides_automatic_update_button5'] = 'Jeżeli używasz wersji starszej niż 1.7.x';
+$lang['porg_guides_automatic_update_button3'] = 'Jeżeli używasz Piwigo 2.0.x';
+$lang['porg_guides_automatic_update_button4'] = 'Jeżeli używasz Piwigo 1.7.x';
+$lang['porg_guides_automatic_update_button2'] = 'Jeżeli używasz Piwigo 2.1.x lub 2.2.x';
