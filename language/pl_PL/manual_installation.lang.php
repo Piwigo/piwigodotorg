786838aa5ea0f388d2f8ae613a33ea85973587b4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-24 13:35:14 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3. Zaznacz wszystkie pliki i prześlij je do folderu "photos" na swoim serwerze.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2. Na swoim dysku lokalnym otwórz wyodrębnioną zawartość archiwum "piwigo"';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1. Utwórz folder "photos" na swoim serwerze WWW.';
+$lang['porg_guides_manual_installation_step2'] = 'Krok 2. Prześlij zawartość archiwum';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Prześlij zawartość archiwum na swój serwer WWW korzystając z dowolnego klienta FTP.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Pobierz pełne archiwum</a> i wyodrębnij zawartość.';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'W czasie ręcznej instalacji pobierasz pełne archiwum Piwigo, wyodrębniasz zawartość i przesyłasz na swój serwer.';
+$lang['porg_guides_manual_installation_step1'] = 'Krok 1. Pobierz pełne archiwum';
+$lang['porg_guides_manual_installation_info3'] = 'Możesz również zainstalować Piwigo w folderze głównym witryny, folder "photos" nie jest wymagany. Jakąkolwiek nazwę folderu wybierzesz, zalecane jest nie używać numeru wersji Piwigo.';
+$lang['porg_guides_manual_installation_image_caption1'] = 'Łączenie i przesyłanie';
