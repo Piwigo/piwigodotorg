ead40d29914d70e49d41652e7de1bc38bf797c5d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-26 06:22:12 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-11.1.0.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1294'] = '[11.x] codice di download errato per l\'aggiornamento';
+$lang['porg_issue_1290'] = '[11.x admin/intro]popen() è stato disabilitato per motivi di sicurezza';
+$lang['porg_issue_1291'] = '[11.x] Parse error: errore di sintassi, "[" imprevisto';
+$lang['porg_issue_1287'] = 'Errore di sintassi nel file in lingua norvegese dopo l\'aggiornamento verso Piwigo 11';
+$lang['porg_issue_1284'] = '\"Infinite loop?\" Errore quando si va alla pagina 2 degli album';
+$lang['porg_issue_1257'] = 'admin/batch_manager.php alcuni errori/avvisi su php7.4';
