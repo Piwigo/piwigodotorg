8d69e1ca59ab69d8e0a796a50f16e59b0f7ad05a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 08:09:02 +0100

    Update de_DE, thanks to Yogie

---
+++ language/de_DE/release-11.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1257'] = 'admin/batch_manager.php einige Fehler/Warnungen auf php7.4';
+$lang['porg_issue_1284'] = '\"Endlosschleife?\" Fehler beim Wechsel auf Seite 2 der Alben';
+$lang['porg_issue_1287'] = 'Syntaxfehler in norwegischer Sprachdatei nach Upgrade auf Piwigo 11';
+$lang['porg_issue_1290'] = '[11.x admin/intro] popen() wurde aus Sicherheitsgründen deaktiviert';
+$lang['porg_issue_1291'] = '[11.x] Parse-Fehler: Syntaxfehler, unerwartetes \'[\'';
+$lang['porg_issue_1292'] = 'escaped word `groups` kann nicht desialisiert werden';
+$lang['porg_issue_1293'] = '11.x Dashboard, Schwerwiegender Fehler: Erlaubte Speichergröße';
+$lang['porg_issue_1294'] = '[11.x] Falscher Download-Code für Upgrade';
