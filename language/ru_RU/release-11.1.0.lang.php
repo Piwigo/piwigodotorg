9ac5cae00006dc6c61a63fe6e27dc74945ae2df7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 07:39:42 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-11.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1257'] = 'admin/batch_manager.php some errors/warnings on php7.4';
+$lang['porg_issue_1284'] = 'admin/batch_manager.php some errors/warnings on php7.4';
+$lang['porg_issue_1287'] = 'Syntax error in Norwegian language file after upgrading to Piwigo 11';
+$lang['porg_issue_1290'] = '[11.x admin/intro] popen() has been disabled for security reasons';
+$lang['porg_issue_1291'] = '[11.x] Parse error: syntax error, unexpected \'[\'';
+$lang['porg_issue_1292'] = 'escaped word `groups` can\'t be unserialized';
+$lang['porg_issue_1293'] = '11.x Dashboard, Fatal error: Allowed memory size';
+$lang['porg_issue_1294'] = '[11.x] incorrect download code for upgrade';
