acb6c2dab622ea329ba7bfc7cc4f627b89b9e782
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 20:38:59 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-11.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1257'] = 'Admin/batch_manager.php gav nogle fejl/advarsler under php7.4';
+$lang['porg_issue_1284'] = '\"Infinite loop?\"-fejl når man skiftede til side to albummer';
+$lang['porg_issue_1287'] = 'Syntaksfejl i den norske sprogfil efter opgradering til Piwigo 11';
+$lang['porg_issue_1290'] = '	￼	￼￼
+[11.x admin/intro] popen() er blevet deaktiveret af sikkerhedshensyn';
+$lang['porg_issue_1291'] = '[11.x] Fortolkningsfejl: syntaksfejl, uventet \'[\'';
+$lang['porg_issue_1292'] = 'Det escapede ord `groups` kan ikke afserialiseres';
+$lang['porg_issue_1293'] = '[11.x] Instrumentbræt, fatal fejl: Tilladt hukommelsesstørrelse';
+$lang['porg_issue_1294'] = '[11.x] Ukorrekt downloadkode til opgradering';
