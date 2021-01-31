be03d150571120672a8679983817c8296891a3a2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-31 18:46:35 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-11.1.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1290'] = '[11.x admin/intro] popen() zostało wyłączone ze względów bezpieczeństwa';
+$lang['porg_issue_1287'] = 'Błąd składni w języku norweskim po uaktualnieniu do Piwigo 11';
+$lang['porg_issue_1284'] = 'błąd \"Infinite loop?\" na drugiej stronie albumów';
+$lang['porg_issue_1257'] = 'admin/batch_manager.php kilka błędów/ostrzeżeń dla php7.4';
