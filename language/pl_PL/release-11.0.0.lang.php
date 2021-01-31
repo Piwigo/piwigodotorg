fe1060c781f71b1b8c200980b514ef9ee359ab20
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-31 05:49:33 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-11.0.0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v11_batch_manager_title'] = 'Menedżer wsadowy';
+$lang['porg_v11_album_manager_title'] = 'Menedżer albumów';
+$lang['porg_v11_album_edition_title'] = 'Edycja albumów';
+$lang['porg_v11_intro3'] = 'Przygotuj się na miłe niespodzianki!';
+$lang['porg_v11_intro1'] = 'Gotowi na wiele ulepszeń?';
+$lang['porg_v11_info3_text'] = 'Teraz możesz zalogować się również za pomocą adresu email, zamiast nazwy użytkownika. Piwigo sprawdza najpierw czy podane poświadczenia odpowiadają nazwie użytkownika, a jeżeli nie, to sprawdza wśród adresów email.';
+$lang['porg_v11_info3_title'] = 'Logowanie za pomocą adresu email';
+$lang['porg_v11_info2_title'] = 'Usunięty zrzut bazy danych';
+$lang['porg_v11_info2_text'] = 'Funkcja tworzenia zrzutu bazy danych na stronie uaktualniania została usunięta. Przestała działać z bieżącymi wersjami PHP. Została zastąpiona przez prosty komunikat, aby zawsze dbać o kopię plików i bazy danych.';
+$lang['porg_v11_info1_title'] = 'Wersja przeskakuje z 2.10 na 11';
+$lang['porg_v11_history_stats_image1_label'] = 'Statystyki historii w Piwigo 11 mają teraz lepiej wyglądające animowane krzywe.';
+$lang['porg_v11_group_manager_text'] = 'Napisany od nowa menedżer grup: bardziej przejrzysty, lepsze wrażenie użytkownika i możliwość przyporządkowania użytkowników.';
+$lang['porg_v11_history_stats_image2_label'] = 'Historia (strona statystyk) dostaje tryb porównania, pozwalający porównywać lata lub miesiące.';
+$lang['porg_v11_history_stats_title'] = 'Statystyki historii';
+$lang['porg_v11_group_manager_title'] = 'Menedżer grup';
+$lang['porg_v11_technical_features14'] = 'Tylko język instalacji jest domyślnie aktywowany, zamiast ponad 70 dostępnych.';
+$lang['porg_v11_technical_features13'] = 'Mniej aktywnych języków';
+$lang['porg_v11_requirements_title'] = 'Wymagania sprawdzone przed uaktualnieniem';
+$lang['porg_v11_requirements_image1_label'] = 'Przed uaktualnieniem funkcji, Piwigo 11+ sprawdzi wymaganą wersję PHP. Chcemy uniknąć sytuacji, kiedy zacząłbyś uaktualnianie Piwigo i utknął ze względu na za starą wersję PHP. Chcemy również poważnie zwiększyć wersję PHP w najbliższej przyszłości.';
+$lang['porg_v11_related_albums_title'] = 'Powiązane albumy';
+$lang['porg_v11_plugin_manager_title'] = 'Menedżer wtyczek';
+$lang['porg_v11_photo_edition_title'] = 'Edycja zdjęć';
+$lang['porg_v11_technical_features2'] = 'Zmiany techniczne w Piwigo 11';
+$lang['porg_v11_technical_features19'] = 'Zmiany API';
+$lang['porg_v11_technical_features17'] = 'Nowy przełącznik';
+$lang['porg_v11_technical_features4'] = 'Szybkie wyszukiwanie i pod-albumy';
+$lang['porg_v11_user_manager_title'] = 'Menedżer użytkowników';
+$lang['porg_v11_theme_manager_title'] = 'Menedżer szablonów wyglądu';
+$lang['porg_v11_technical_features6'] = 'Wtyczka "Has Settings"';
