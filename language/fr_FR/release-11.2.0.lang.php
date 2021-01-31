8607d6823b4e5fe41bb02b03eb17af8d4663c975
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-31 20:31:55 +0100

    Update fr_FR, thanks to Guillaume C

---
+++ language/fr_FR/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1295'] = '[UX] Ne pas cacher les plugins inactifs après l\'activation d\'un nouveau';
+$lang['porg_issue_1297'] = '[11.x] ob_clean() : échec de la suppression du tampon - téléchargements corrompus';
+$lang['porg_issue_1298'] = '[11.x] Boutons incohérents pour le gestionnaire de thèmes';
+$lang['porg_issue_1299'] = '[11.x] gestionnaire de plugin, ajout d\'une info-bulle sur le bouton des paramètres non disponibles';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] nouveau calculateur de la taille du cache';
+$lang['porg_issue_1302'] = '[Plugin Manager] Raccourci clavier pour le filtre';
+$lang['porg_issue_1303'] = '[11.1 tableau de bord] semaine 0';
+$lang['porg_issue_1304'] = '[11.x tableau de bord] Undefined offset: 0 dans admin/intro.php sur la ligne 324';
+$lang['porg_issue_1307'] = '[11.x] nom de l\'album non représenté sur les nouvelles fonctionnalités';
