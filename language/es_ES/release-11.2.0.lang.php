b38ca9da9d87265a0229f7c3b248c5b445809199
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-31 22:03:50 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1295'] = '[UX] No ocultar los plugins inactivos después de activar uno nuevo';
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): no podía suprimir la memoria intermedia - descargas corrompidas';
+$lang['porg_issue_1298'] = '[11.x] Botones incoherentes en el gestor de temas';
+$lang['porg_issue_1299'] = '[11.x] gestor de plugins, añadir ayudas flotantes en el botón de parámetros no disponible';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] el nuevo calculador de tamaño de la caché excede el tiempo';
+$lang['porg_issue_1302'] = '[Gestor de plugins] atajo de teclado para filtrar';
+$lang['porg_issue_1303'] = '[11.1 panel de control] semana 0';
+$lang['porg_issue_1304'] = '[11.1 panel de control] Desplazamiento no definido: 0 en admin/intro.php, línea 324';
+$lang['porg_issue_1307'] = '[11.x] no se muestra el nombre del álbum en las nuevas características';
