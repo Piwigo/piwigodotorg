98eaa84b634b0ae2591cacc64308d3f823f01c36
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-01 13:27:10 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): failed to delete buffer - corrupted downloads';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] новый калькулятор размера кэша';
+$lang['porg_issue_1302'] = '[Plugin Manager] сочетание клавиш для фильтра';
+$lang['porg_issue_1303'] = '[11.1 dashboard] неделя 0';
+$lang['porg_issue_1304'] = '[11.1 dashboard] Undefined offset: 0 in admin/intro.php on line 324';
+$lang['porg_issue_1298'] = '[11.x] несвязные кнопки для менеджера темы';
+$lang['porg_issue_1299'] = '[11.x] менеджер плагина, добавлены подсказки на кнопки недоступных настроек';
+$lang['porg_issue_1307'] = '[11.x] название альбома не отображается на новых функциях';
+$lang['porg_issue_1295'] = '[UX] Не скрываются неактивные плагины после активации нового';
