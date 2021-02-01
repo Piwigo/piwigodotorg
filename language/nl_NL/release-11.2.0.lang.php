31ec7d255cae4f0f9bdeec89f24485b0bcc411d7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-01 13:26:56 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1307'] = '[11.x] albumnaam wordt niet weergegeven bij nieuwe functies';
+$lang['porg_issue_1304'] = '[11.x dashboard] Ongedefinieerde offset: 0 in admin/intro.php op regel 324';
+$lang['porg_issue_1303'] = '[11.1 dashboard] week 0';
+$lang['porg_issue_1302'] = '[Plugin Manager] sneltoets voor filter';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] time-out van calculator voor cachegrootte';
+$lang['porg_issue_1299'] = '[11.x] plugin manager, voeg tooltip toe op niet-beschikbare instellingenknop';
+$lang['porg_issue_1298'] = '[11.x] onsamenhangende knoppen voor themamanager';
+$lang['porg_issue_1297'] = '[11.x] ob_verwijder(): kan door buffer beschadigde downloads niet verwijderen';
+$lang['porg_issue_1295'] = '[UX] Verberg inactieve plug-ins niet na het activeren van een nieuwe';
