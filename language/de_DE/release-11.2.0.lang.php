c470fa81cde119fb0f7a789c68a7b8180c898d6d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-01 06:19:06 +0100

    Update de_DE, thanks to Yogie

---
+++ language/de_DE/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1295'] = 'UX] Inaktive Plugins nicht ausblenden, wenn ein neues aktiviert wird';
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): Puffer konnte nicht gelöscht werden - korrupte Downloads';
+$lang['porg_issue_1298'] = '[11.x] inkohärente Schaltflächen für den Themenmanager';
+$lang['porg_issue_1299'] = '[11.x] Plugin-Manager, Tooltip auf nicht verfügbarem Einstellungsknopf hinzugefügt';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] neuer Cache-Größenrechner stürzt ab';
+$lang['porg_issue_1302'] = '[Plugin Manager] Tastaturkürzel für Filter';
+$lang['porg_issue_1303'] = '[11.1 Dashboard] Woche 0';
+$lang['porg_issue_1304'] = '[11.x Dashboard] Undefinierter Offset: 0 in admin/intro.php in Zeile 324';
+$lang['porg_issue_1307'] = '[11.x] Albumname wird bei neuen Funktionen nicht gerendert';
