9f3993bfc705ccc2ae0e631277746f67b3810f82
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-01 06:19:19 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-11.2.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1295'] = '[UX] Não esconda plugins Inativos após ativar um novo';
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): falha ao excluir o cache - arquivo baixado está corrompido';
