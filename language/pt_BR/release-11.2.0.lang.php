17227dbd5e0e34ecb916cbd8a981eb2ecde40153
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-02 16:11:37 +0100

    Update pt_BR, thanks to flaviove

--- language/pt_BR/release-11.2.0.lang.php
+++ language/pt_BR/release-11.2.0.lang.php
@@ -7,3 +7,10 @@
 // +-----------------------------------------------------------------------+
 $lang['porg_issue_1295'] = '[UX] Não esconda plugins Inativos após ativar um novo';
 $lang['porg_issue_1297'] = '[11.x] ob_clean(): falha ao excluir o cache - arquivo baixado está corrompido';
+$lang['porg_issue_1298'] = '[11.x] botões incoerentes para gerenciador de tema';
+$lang['porg_issue_1299'] = '[11.x] gerenciador de plug-ins, adicionar dica no botão de configurações indisponíveis';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] a calculadora do novo tamanho do cache atingiu o tempo limite';
+$lang['porg_issue_1302'] = 'Atalho de teclado [Plugin Manager] para o filtro';
+$lang['porg_issue_1303'] = '[11.1 dashboard] semana 0';
+$lang['porg_issue_1304'] = '[Painel 11.x] Deslocamento indefinido: 0 em admin/intro.php na linha 324';
+$lang['porg_issue_1307'] = '[11.x] nome do álbum não renderizado em novos recursos';
