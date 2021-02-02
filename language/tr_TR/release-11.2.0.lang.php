d7ebc12a1fadaf4fc30fe5925922f480f952dacd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-02 16:11:52 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): tamponun silinmesinde hata - bozuk indirmeler';
+$lang['porg_issue_1298'] = '[11.X] tema yöneticisi için tutarsız düğmeler ';
+$lang['porg_issue_1295'] = '[UX] Yenisini etkinleştirdikten sonra etkin olmayan eklentileri gizlemeyin';
+$lang['porg_issue_1299'] = '[11.x] eklenti yöneticisi, kullanılamayan ayarlar düğmesine araç ipucu ekle';
+$lang['porg_issue_1300'] = '[11.1 admin/intro yeni önbellek boyutu hesaplayıcı zaman aşımına uğradı';
+$lang['porg_issue_1302'] = '[Eklenti Yöneticisi] filtre için tuş kısayolu';
+$lang['porg_issue_1303'] = '[11.1 dashboard] hafta 0';
+$lang['porg_issue_1307'] = '[11.x] albüm adı yeni özelliklerde işlenmiyor ';
+$lang['porg_issue_1304'] = '[11.x dashboard] Tanımsız ofset: 324 satırında admin / intro.php\'de 0 ';
