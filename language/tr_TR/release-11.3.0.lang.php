9585a48ddc3039a4b3e6c3e839d3c250a906b0b1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-06 21:10:22 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1314'] = '[11.x, tag manager] artık silme işleminden sonra başarı mesajı eksik ';
+$lang['porg_issue_1317'] = '[11.x] önbellek dizini boyut hesaplamasını gösterge tablosundan kaldır';
+$lang['porg_issue_1319'] = '[11.x] Alt albümü listenin başına / sonuna taşıma ';
+$lang['porg_issue_1322'] = '[11.x tag manager] Söz dizilimi hatası: Beklenmedik belirteç =';
+$lang['porg_issue_1315'] = '[11.x admin/tags] filtre büyük harflerle çalışmıyor';
+$lang['porg_issue_1312'] = '11.2.0\'da işlevsiz anahtar kelimeler / etiketler yönetici sayfası';
+$lang['porg_issue_1308'] = '[11.2] yönetici kontrol paneli kullanılamıyor ';
+$lang['porg_issue_1313'] = 'profile.php \'de küme parantezleri kullanımdan kaldırıldı ';
+$lang['porg_issue_1311'] = 'Artık etiket silinemiyor';
