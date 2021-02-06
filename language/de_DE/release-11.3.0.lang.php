f8b98f8570f08b8215d072f52fe1e35e00748713
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-06 21:11:02 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = 'Das Admin-Dashboard ist nicht verfügbar';
+$lang['porg_issue_1311'] = 'Verwaistes Tag kann nicht gelöscht werden';
+$lang['porg_issue_1312'] = 'Dysfunktionale Schlüsselwörter/Tags-Admin-Seite in 11.3.0';
+$lang['porg_issue_1313'] = 'geschweifte Klammern sind in profile.php veraltet';
+$lang['porg_issue_1314'] = '[11.x, Tag-Manager] fehlende Erfolgsmeldung nach dem Löschen von Waisen';
+$lang['porg_issue_1315'] = '[11.x admin/tags] Filter funktioniert nicht mit Großbuchstaben';
+$lang['porg_issue_1317'] = '[11.x] Berechnung der Cache-Verzeichnisgröße aus dem Dashboard entfernen';
+$lang['porg_issue_1319'] = '[11.x] Verschieben von Unteralben an den Anfang/Ende der Liste';
+$lang['porg_issue_1322'] = '[11.x Tag-Manager] SyntaxFehler: Unerwartetes Token =';
