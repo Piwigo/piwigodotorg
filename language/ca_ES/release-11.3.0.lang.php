90a136e497c88f774e6789715d7c40c7e0f66226
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-06 21:10:35 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = '[11.2] el panell de control de l\'administrador no està disponible';
+$lang['porg_issue_1311'] = 'No es pot suprimir una etiqueta òrfena';
+$lang['porg_issue_1312'] = 'La pàgina d\'administració de paraules clau/etiquetes no funciona bé a 11.2.0';
+$lang['porg_issue_1313'] = 'Els caràcters de clau (corxets curvats) queden obsolets a profile.php';
+$lang['porg_issue_1314'] = '[11.x, gestor d\'etiquetes] manca el missatge de confirmació d\'èxit després de suprimir orfes';
+$lang['porg_issue_1315'] = '[11.x admin/etiquetes] el filtre no funciona amb majúscules';
+$lang['porg_issue_1317'] = '[11.x] eliminar el càlcul de la mida del directori de memòria cau del panell de control';
+$lang['porg_issue_1319'] = '[11.x] Moure sub-àlbums a l\'inici/final de la llista';
+$lang['porg_issue_1322'] = '[11.x gestor d\'etiquetes] Error de sintaxi: testimoni inesperat =';
