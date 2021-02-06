4f8d6d4c622f5da65d03fa81e7752f9e71d6c68b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-06 21:10:48 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = '[11.2] El panel de control del administrador no está disponible';
+$lang['porg_issue_1311'] = 'No se pueden suprimir etiquetas huérfanas';
+$lang['porg_issue_1312'] = 'La página de administración de palabras clave/etiquetas no funciona bien en 11.2.0';
+$lang['porg_issue_1313'] = 'los caracteres de llave ("corchetes ondulados") han quedado obsoletos en profile.php';
+$lang['porg_issue_1314'] = '[11.x, gestor de etiquetas] falta el mensaje de confirmación de éxito después de la supresión de huérfanos';
+$lang['porg_issue_1315'] = '[11.x, admin/etiquetas] el filtro no funciona con las mayúsculas';
+$lang['porg_issue_1317'] = '[11.x] eliminar el cálculo del tamaño del directorio de caché del panel de control';
+$lang['porg_issue_1319'] = '[11.x] Mover sub-álbum al principio/final de la lista';
+$lang['porg_issue_1322'] = '[11.x gestor de etiquetas] Error de sintaxis: señal (token) inesperada =';
