36be9ec3828b8a932aa69e18cae4d231f25691e1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-07 05:31:04 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = '[11.2] Admin-instrumentbræt er ikke tilgængeligt';
+$lang['porg_issue_1311'] = 'Kan ikke slette forældreløst tag';
+$lang['porg_issue_1312'] = 'Dysfunktionelle nøgleord/tags på adminside i 11.2.0';
+$lang['porg_issue_1313'] = 'Tuborgklammer er udfaset i profile.php';
+$lang['porg_issue_1314'] = '[11.x, taghåndtering] Manglende succesbesked efter sletning af forældreløs';
+$lang['porg_issue_1315'] = '[11.x, admin/tags] Filter fungerer ikke med store bogstaver';
+$lang['porg_issue_1317'] = '[11.x] Fjern beregning af størrelse på cachemappe fra instrumentbræt';
+$lang['porg_issue_1319'] = '[11.x] Flytning af underalbummer ved begyndelse/slutning af listen';
+$lang['porg_issue_1322'] = '[11.x, taghåndtering] SyntaxError: Unexpected token =';
