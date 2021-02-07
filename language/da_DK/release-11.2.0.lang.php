846ee4d103779955bc80b0de08a3c38d4acf9fc3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-07 05:31:18 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1295'] = '[UX] Skjul ikke inaktive plugins efter en ny er blevet aktiveret';
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): Kunne ikke slette buffer - korrupte downloads';
+$lang['porg_issue_1298'] = '[11.x] Usammenhængende knapper i temahåndtering';
+$lang['porg_issue_1299'] = '[11.x] Pluginhåndtering, tilføjer tooltip ved utilgængelig indstillingerknap';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] Ny beregning af cachestørrelse giver timeout';
+$lang['porg_issue_1302'] = '[Pluginhåndtering] Tastaturgenvej for filter';
+$lang['porg_issue_1303'] = '[11.1 dashboard] Uge 0';
+$lang['porg_issue_1304'] = '[11.x dashboard] Undefined offset: 0 in admin/intro.php on line 324';
+$lang['porg_issue_1307'] = '[11.x] Albumnavn bliver ikke renderet på nye funktioner';
