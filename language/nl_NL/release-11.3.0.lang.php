1970c00cdb563c4709579adb3e06fa2d65f1b1f9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-08 07:23:51 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1314'] = '[11.x, tag manager] succesbericht ontbreekt na verwijdering van wezen';
+$lang['porg_issue_1322'] = '[11.x tag manager] Fout zinsbouw: onverwacht token =';
+$lang['porg_issue_1319'] = '[11.x] Verplaatsev van subalbum aan het begin/einde van de lijst';
+$lang['porg_issue_1317'] = '[11.x] verwijder berekening van de grootte van de cachemap vanaf het dashboard';
+$lang['porg_issue_1315'] = '[11.x admin / tags] filter werkt niet met hoofdletters';
+$lang['porg_issue_1313'] = 'accolades zijn verouderd in profile.php';
+$lang['porg_issue_1312'] = 'Defecte trefwoorden / tags admin-pagina in 11.2.0';
+$lang['porg_issue_1311'] = 'Kan wees-tag niet verwijderen';
+$lang['porg_issue_1308'] = '[11.2] beheerdersdashboard is niet beschikbaar';
