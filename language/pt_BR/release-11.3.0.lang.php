44218866fb3144b10999391db467666991913bcf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-11 13:52:47 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = '[11.2] painel de administração não está disponível';
+$lang['porg_issue_1311'] = 'Não é possível excluir a etiqueta órfã';
+$lang['porg_issue_1312'] = 'Dysfunctional keywords/tags admin page in 11.2.0';
+$lang['porg_issue_1313'] = 'Página de administração de palavras-chave/etiquetas disfuncionais em';
+$lang['porg_issue_1314'] = '[11.x, gerenciador de etiquets] mensagem de sucesso ausente após exclusão órfã';
+$lang['porg_issue_1315'] = '[11.x admin/etiquetas] filtro não funciona com maiúsculas';
+$lang['porg_issue_1317'] = '[11.x] remova o cálculo do tamanho do diretório de cache do painel';
+$lang['porg_issue_1319'] = '[11.x] Movendo o subálbum no início/fim da lista';
+$lang['porg_issue_1322'] = '[11.x gerenciador de etiquetas] SintaxErro: Token inesperado =';
