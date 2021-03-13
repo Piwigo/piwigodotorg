06bf470828e63d861366879dd0c8f08fc8787165
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-13 06:20:57 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1322'] = '[11.x, menedżer tagów] Błąd składni: niespodziewany token =';
+$lang['porg_issue_1319'] = '[11.x] Przenoszenie podalbumów na początek/koniec listy';
+$lang['porg_issue_1317'] = '[11.x] z panelu usunięto obliczanie wielkości folderu buforu podręcznego';
+$lang['porg_issue_1315'] = '[11.x, menedżer tagów] filtrowanie nie działa dla wielkich liter';
+$lang['porg_issue_1314'] = '[11.x, menedżer tagów] brak komunikatu o pomyślnym usunięciu osieroconych tagów';
+$lang['porg_issue_1313'] = 'Koniec obsługi nawiasów klamrowych w profile.php';
+$lang['porg_issue_1312'] = 'Niedziałająca strona administracyjna tagów/słów kluczowych w 11.2.0';
+$lang['porg_issue_1311'] = 'Nie można usunąć osieroconego taga';
+$lang['porg_issue_1308'] = '[11.2] niedostępny panel administracyjny';
