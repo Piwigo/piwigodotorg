d2b5740c98f360e5663ba1c6aba14aa3e751e252
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-20 20:29:36 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-11.4.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1358'] = '[dashboard] mogelijkheid om het zoeken naar updates te de-activeren';
+$lang['porg_issue_1361'] = 'gerelateerde albums toont verboden albums';
+$lang['porg_issue_1355'] = '[11.x] Alternatieve weergave voor de plug-inbeheerder';
+$lang['porg_issue_1354'] = '[11.x] opslagberekeningsfout, JPG-bestanden beschouwd als \ "andere \"';
+$lang['porg_issue_1352'] = 'SQL-injectie in instellingen-taal in versie 11.3.0';
+$lang['porg_issue_1344'] = '[FOUT] Kan geen foto\'s uploaden met uploadAsync wanneer gasttoegang niet is toegestaan';
+$lang['porg_issue_1342'] = 'opslaggebruik verbroken met grote galerijen';
+$lang['porg_issue_1336'] = '[11.x] Alternatieve weergave voor de albumbeheerder';
+$lang['porg_issue_1334'] = 'Piwigo versie 11.3.0 genereert syntaxisfout bij het toevoegen van \'Groepen\' op PHP 5.3';
+$lang['porg_issue_1118'] = 'Zoek snel fout (MySQL 8 en REGEXP)';
