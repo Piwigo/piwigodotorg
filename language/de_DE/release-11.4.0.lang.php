a2c9e046ec7424704499c44ebaa89edc301270d4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-21 07:19:35 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-11.4.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1118'] = 'Fehler bei der Schnellsuche (MySQL 8 und REGEXP)';
+$lang['porg_issue_1334'] = 'Piwigo Version 11.3.0 wirft einen Syntaxfehler beim Hinzufügen von \'Groups\' auf PHP 5.3';
+$lang['porg_issue_1336'] = '[11.x] Alternative Ansicht für den Album-Manager';
+$lang['porg_issue_1342'] = 'Speichernutzung bei großen Galerien gebrochen';
+$lang['porg_issue_1344'] = '[BUG] Fotos können nicht mit uploadAsync hochgeladen werden, wenn Gastzugriff nicht erlaubt ist';
+$lang['porg_issue_1352'] = 'SQL-Injection in Einstellungen-Sprache in Version 11.3.0';
+$lang['porg_issue_1354'] = '[11.x] Speicherberechnungsfehler, JPG-Dateien werden als "anders" betrachtet';
+$lang['porg_issue_1355'] = '[11.x] Alternative Ansicht für den Plugin-Manager';
+$lang['porg_issue_1358'] = '[Dashboard] Möglichkeit, die Suche nach Updates zu deaktivieren';
+$lang['porg_issue_1361'] = 'verwandte Alben zeigt verbotene Alben';
