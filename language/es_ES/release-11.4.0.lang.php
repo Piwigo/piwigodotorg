6806a1b7649c8170fbe2b0eeccdf171f80de7ff1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-22 05:50:32 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-11.4.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1118'] = 'Error de búsqueda rápida (MySQL 8 y expresiones regulares)';
+$lang['porg_issue_1334'] = 'Piwigo versión 11.3.0 produce un error de sintaxis al añadir \'Grupos\' en PHP 5.3';
+$lang['porg_issue_1336'] = '[11.x] Vista alternativa para el gestor de álbumes';
+$lang['porg_issue_1342'] = 'El uso de almacenamiento no funciona con galerías grandes';
+$lang['porg_issue_1344'] = '[BUG] No se pueden subir fotos con uploadAsync cuando no se permite el acceso como invitado';
+$lang['porg_issue_1352'] = 'Inyección de SQL en settings-language en la versión 11.3.0';
+$lang['porg_issue_1354'] = '[11.x] Error en el cálculo del espacio de almacenamiento, los archivos JPG se consideran como \"otros\"';
+$lang['porg_issue_1355'] = '[11.x] Vista alternativa para el gestor de plugins';
+$lang['porg_issue_1358'] = '[panel de control] Posibilidad de desactivar la búsqueda de actualizaciones';
+$lang['porg_issue_1361'] = 'La función de álbumes relacionados muestra álbumes prohibidos';
