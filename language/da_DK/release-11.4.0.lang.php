0e2788c24e0e63b18c38b1d6a5c34e1a9cd34dc7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-22 19:38:43 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-11.4.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1118'] = 'Fejl i hurtigsøgning (MySQL 8 og regexp)';
+$lang['porg_issue_1334'] = 'Piwigo version 11.3.0 giver syntaksfejl når der tilføjes \'Groups\' under PHP 5.3';
+$lang['porg_issue_1336'] = '[11.x] Alternativ visning i albumhåndteringen';
+$lang['porg_issue_1342'] = 'Pladsforbrug fungerer ikke med store gallerier';
+$lang['porg_issue_1344'] = '[Fejl] Kan ikke uploade fotografier med uploadAsync når gæsteadgang ikke er tilladt';
+$lang['porg_issue_1352'] = 'SQL-indsprøjtning i settings-language i version 11.3.0';
+$lang['porg_issue_1354'] = '[11.x] Pladsberegningsfejl, JPG-filer betragtes som \"andet\"';
+$lang['porg_issue_1355'] = '[11.x] Alternativ visning i pluginhåndteringen';
+$lang['porg_issue_1358'] = '[Instrumentbræt] Mulighed for at deaktivere søgning efter opdateringer';
+$lang['porg_issue_1361'] = 'Relaterede albummer viser forbudte albummer';
