40b666dea900d6ed0a47fe2598cd8a95c2eca4e9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-04-03 06:11:09 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-11.4.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1361'] = 'среди связанных показывались и запрещенные альбомы';
+$lang['porg_issue_1352'] = 'SQL injection in settings-language in version 11.3.0';
+$lang['porg_issue_1354'] = 'SQL injection in settings-language in version 11.3.0';
+$lang['porg_issue_1358'] = '[dashboard] возможность отключения поиска обновлений';
+$lang['porg_issue_1344'] = '[BUG] Нельзя загружать фотографии с uploadAsync, когда доступ гостю запрещен';
+$lang['porg_issue_1355'] = '[11.x] Альтернативный просмотр для менеджера плагина';
+$lang['porg_issue_1334'] = 'Piwigo версии 11.3.0 исключает ошибки при добавлении "Группы" на PHP 5.3';
+$lang['porg_issue_1118'] = 'Ошибка быстрого поиска (MySQL 8 and REGEXP)';
+$lang['porg_issue_1342'] = 'использование хранилища, разбивающего большие галереи';
+$lang['porg_issue_1336'] = '[11.x] Альтернативный просмотр для менеджера альбома';
