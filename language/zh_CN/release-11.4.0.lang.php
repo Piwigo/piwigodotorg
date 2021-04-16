e6637b0f34d6900a28c32c98c7543aab2afe19e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-04-16 17:41:07 +0200

    Update zh_CN, thanks to deserteagle

---
+++ language/zh_CN/release-11.4.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1118'] = '快速查找错误（MySQL8及正则表达式）';
+$lang['porg_issue_1334'] = '在PHP5.3中添加\'Groups\'时Piwigo version 11.3.0 弹出语法错误';
+$lang['porg_issue_1336'] = '[11.x]相册管理者替代视图';
+$lang['porg_issue_1342'] = 'Gallery过大存储空间损坏';
+$lang['porg_issue_1344'] = '[BUG] 访客未被允许访问时不能使用上传同步上传图片';
+$lang['porg_issue_1352'] = '版本11.3.0中语言设置中的SQL注入';
+$lang['porg_issue_1354'] = '[11.x]存储空间计算错误，JPG文件视为\"other\"';
+$lang['porg_issue_1355'] = '[11.x]插件管理者替代视图';
+$lang['porg_issue_1358'] = '[仪表板]当更新时可以关闭搜索';
+$lang['porg_issue_1361'] = '相关联相册显示禁止的相册';
