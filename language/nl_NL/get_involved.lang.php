b6fbe1daf6c6fb31888f55b713db8598b294f077
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-04 21:16:44 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/get_involved.lang.php
+++ language/nl_NL/get_involved.lang.php
@@ -20,33 +20,30 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['porg_get_involved_desc2'] = '
-Er zijn talloze manieren om bij te dragen, afhankelijk van je vaardigheden en de tijd die je aan het project wilt besteden.';
-$lang['porg_get_involved_dev_plugins_desc'] = '
-U hebt een functie nodig die nog niet bestaat in Piwigo? Maak een plug-in, publiceer hem en maak hem beschikbaar voor de gemeenschap!';
-$lang['porg_get_involved_donate_desc1'] = '
-Wil je helpen, maar heb je geen tijd of technische vaardigheden?';
-$lang['porg_get_involved_donate_desc2'] = 'Wilt u ervoor zorgen dat de infrastructuur van Piwigo gezond en sterk blijft?';
-$lang['porg_get_involved_donate_desc3'] = 'Waarom zou u geen geldelijke donatie overwegen?';
+$lang['porg_get_involved_desc2'] = 'Er zijn talloze manieren om bij te dragen, afhankelijk van jouw vaardigheden en de tijd die je aan het project wilt besteden.';
+$lang['porg_get_involved_dev_plugins_desc'] = 'Heb je een functie nodig die nog niet bestaat in Piwigo? Maak een plug-in, publiceer hem en maak hem beschikbaar voor de gemeenschap!';
+$lang['porg_get_involved_donate_desc1'] = 'Wil je helpen, maar heb je geen tijd of technische vaardigheden?';
+$lang['porg_get_involved_donate_desc2'] = 'Wil je ervoor zorgen dat de infrastructuur van Piwigo gezond en sterk blijft?';
+$lang['porg_get_involved_donate_desc3'] = 'Waarom zou je geen geldelijke donatie overwegen?';
 $lang['porg_get_involved_donate_desc4'] = 'Dit geld, verzameld door de Piwigo Association, wordt gebruikt om directe vergoedingen te betalen, zoals piwigo.org hosting, domeinnamen, marktplaatsabonnement (Apple AppStore) ...';
 $lang['porg_get_involved_donate_desc5'] = 'Betaling met';
-$lang['porg_get_involved_grateful_desc1'] = '
+$lang['porg_get_involved_grateful_desc1'] = 'Bedankt voor jouw bijdragen en betrokkenheid.';
 Bedankt voor jouw bijdragen en betrokkenheid.';
-$lang['porg_get_involved_grateful_desc2'] = '
+$lang['porg_get_involved_grateful_desc2'] = 'We zullen altijd de inspanningen,hulp en bijdragen aan de gemeenschap en ontwikkeling van Piwigo waarderen!';
 We zullen altijd de inspanningen,hulp en bijdragen aan de gemeenschap en ontwikkeling van Piwigo waarderen!';
-$lang['porg_get_involved_how_to_code_desc'] = '
+$lang['porg_get_involved_how_to_code_desc'] = 'Selecteer problemen op Github, vork Piwigo ... <br> ... en dien trekverzoeken in.';
 Selecteer problemen op Github, vork Piwigo ... <br> ... en dien trekverzoeken in.';
-$lang['porg_get_involved_improvment_desc'] = '
+$lang['porg_get_involved_improvment_desc'] = 'Gebruik het forum om jouw ideeën te delen over hoe Piwigo te verbeteren.';
 Gebruik het forum om jouw ideeën te delen over hoe Piwigo te verbeteren.';
-$lang['porg_get_involved_recommend_desc'] = '
+$lang['porg_get_involved_recommend_desc'] = '... naar de communicatieafdeling in je bedrijf of op verschillende websites.';
 ... naar de communicatieafdeling in je bedrijf of op verschillende websites.';
-$lang['porg_get_involved_security_desc1'] = '
+$lang['porg_get_involved_security_desc1'] = 'Help ons om de veiligste fotomanager te bouwen.';
 Help ons om de veiligste fotomanager te bouwen.';
-$lang['porg_get_involved_security_desc2'] = '
+$lang['porg_get_involved_security_desc2'] = 'Als je een potentieel beveiligingsprobleem hebt vastgesteld in de Piwigo-software of een mobiele applicatie, meld dit dan hier.';
 Als je een potentieel beveiligingsprobleem hebt vastgesteld in de Piwigo-software of een mobiele applicatie, meld dit dan hier.';
-$lang['porg_get_involved_translate_desc1'] = '
+$lang['porg_get_involved_translate_desc1'] = 'Vertaal Piwigo in jouw taal, corrigeer wat er ontbreekt, verkeerd of beter is.';
 Vertaal Piwigo in jouw taal, corrigeer wat er ontbreekt, verkeerd of beter is.';
-$lang['porg_get_involved_translate_desc2'] = '
+$lang['porg_get_involved_translate_desc2'] = 'Dit is van toepassing op de kern en uitbreidingen van Piwigo.';
 Dit is van toepassing op de kern en uitbreidingen van Piwigo.';
 $lang['Bugtracker'] = 'Foutzoeker';
 $lang['Coding documentation'] = 'Programmeer documentatie';
@@ -62,11 +59,11 @@ $lang['Submit product improvment'] = 'Stuur productverbetering';
 $lang['Translate'] = 'Vertaal';
 $lang['Translation tool'] = 'Vertaal-gereedschap';
 $lang['We are grateful !'] = 'Wij zijn dankbaar!';
-$lang['You know how to code?'] = 'U kunt programmeren?';
+$lang['You know how to code?'] = 'Kun je programmeren?';
 $lang['Your Gift'] = 'Jouw talent';
-$lang['porg_get_involved_bugs_desc1'] = 'U hebt een fout in Piwigo geïdentificeerd? Rapporteer het op onze foutzoeker ... of op het forum als je het niet zeker weet.';
+$lang['porg_get_involved_bugs_desc1'] = 'Je hebt een fout in Piwigo geïdentificeerd? Rapporteer het op onze foutzoeker ... of op het forum als je het niet zeker weet.';
 $lang['porg_get_involved_bugs_desc2'] = 'Beschrijf hoe we het kunnen reproduceren.';
-$lang['porg_get_involved_community_desc'] = '
+$lang['porg_get_involved_community_desc'] = 'Ondersteun andere gebruikers op de forums <br> en ontmoet andere Piwigo-gebruikers.';
 Ondersteun andere gebruikers op de forums <br> en ontmoet andere Piwigo-gebruikers.';
-$lang['porg_get_involved_desc1'] = '
+$lang['porg_get_involved_desc1'] = 'Piwigo wordt gebouwd door een actieve gemeenschap van gebruikers en ontwikkelaars, die samenwerken om een ​​geweldige fotomanager te maken.';
 Piwigo wordt gebouwd door een actieve gemeenschap van gebruikers en ontwikkelaars, die samenwerken om een ​​geweldige fotomanager te maken.';
