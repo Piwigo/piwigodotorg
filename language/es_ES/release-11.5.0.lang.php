bbcc06f9983ec3db69c79e94265894a2f104cfb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-18 13:22:42 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-11.5.0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1392'] = 'álbumes relacionados, problemas de rendimiento';
+$lang['porg_issue_1393'] = '[Búsqueda de álbumes] El tamaño del título no es correcto cuando el camino de acceso/título es demasiado largo';
+$lang['porg_issue_1394'] = '[Gestor de grupos] Problema de foco al añadir un álbum';
+$lang['porg_issue_1395'] = '[Gestor de grupos] Se muestra un mensaje erróneo cuando el campo de nombre del grupo está vacío';
+$lang['porg_issue_1396'] = '[Mover álbums] Es necesario añadir líneas de traducción para las traducciones';
+$lang['porg_issue_1397'] = 'al desactivar un usuario deberían desconectarse sus sesiones';
+$lang['porg_issue_1398'] = '[Gestor de plugins] El tamaño del icono del menú desplegable es demasiado grande';
+$lang['porg_issue_1399'] = '[Búsqueda de álbumes] Limitar los resultados de la búsqueda a 100 álbumes';
+$lang['porg_issue_1400'] = '[Selector de vistas] Es necesario añadir el tema DarkMode';
+$lang['porg_issue_1410'] = '[11.x, gestor de usuarios] Inyección SQL';
+$lang['porg_issue_1412'] = '[Gestor de plugins, 11.x] El selector de vistas cambia después de la primera recarga de la página';
+$lang['porg_issue_1368'] = 'los usuarios con estado \"invitado\" aún pueden iniciar sesión de manera silenciosa';
+$lang['porg_issue_1369'] = '[Gestor de etiquetas] Número de imágenes incorrecto al filtrar';
+$lang['porg_issue_1372'] = 'nl_NL (Holandés/Países Bajos) el gestor de grupos falla';
+$lang['porg_issue_1377'] = 'Asociar / Mover a álbum en el gestor por lotes';
+$lang['porg_issue_1380'] = 'tiempo de espera excedido en la edición de álbumes';
+$lang['porg_issue_1382'] = 'último recurso cuando el almacenamiento local está lleno';
+$lang['porg_issue_1387'] = '[Movimiento de álbum] impedir la apertura automática al arrastrar y soltar';
+$lang['porg_issue_1388'] = 'La longitud máxima del nombre de álbum es demasiado corta en Álbum -> Mover';
+$lang['porg_issue_1080'] = '[2.10] AdminTools falla en el formulario de carga';
+$lang['porg_issue_1241'] = 'La identificación falla cuando la galería está vacía';
+$lang['porg_issue_1306'] = 'Error de sincronización en los metadatos del gestor por lotes';
+$lang['porg_issue_1323'] = '[11.x admin/movimiento de álbumes] Rango del álbum incorrecto al moverlo';
+$lang['porg_issue_1362'] = 'grupo duplicado, icono predeterminado no mostrado';
+$lang['porg_issue_1364'] = 'Problema con el plugin AdminTools en \"Gestor de etiquetas\" y \"Mover Álbums\"';
