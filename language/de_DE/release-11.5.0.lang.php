a2d56b2a4ed942c90fb9d7b40980b74bb9296e42
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-18 13:22:51 +0200

    Update de_DE, thanks to Yogie

---
+++ language/de_DE/release-11.5.0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1398'] = '[Plugin Manager] Größe des Dropdown-Menü-Symbols ist zu groß';
+$lang['porg_issue_1399'] = '[Album-Suche] Suchergebnisse auf 100 Alben begrenzen';
+$lang['porg_issue_1400'] = '[ViewSelector] DarkMode-Thema muss hinzugefügt werden';
+$lang['porg_issue_1410'] = '[11.x, Benutzerverwaltung] SQL-Injection';
+$lang['porg_issue_1412'] = '[Plugin Manager, 11.x] Ansichtsselektor ändert sich nach dem ersten Neuladen der Seite';
+$lang['porg_issue_1080'] = '[2.10] AdminTools im Upload-Formular defekt';
+$lang['porg_issue_1241'] = 'Identifikation schlägt fehl, wenn Galerie leer ist';
+$lang['porg_issue_1306'] = 'Batchmanager-Metadaten-Synchronisationsfehler';
+$lang['porg_issue_1323'] = '[11.x admin/album move] falscher Album-Rang beim Verschieben';
+$lang['porg_issue_1362'] = 'Gruppe duplizieren, Standardsymbol wird nicht angezeigt';
+$lang['porg_issue_1364'] = 'Problem mit Plugin AdminTools in \"Tag Manager\" und \"Move Albums\"';
+$lang['porg_issue_1368'] = 'Benutzer mit Status \"Gast\" kann sich trotzdem stillschweigend anmelden';
+$lang['porg_issue_1369'] = '[Tag Manager] falsche Anzahl von Bildern beim Filtern';
+$lang['porg_issue_1372'] = 'nl_NL (Dutch/Nederlands) Gruppenmanager defekt';
+$lang['porg_issue_1377'] = 'Zuordnen / In Album verschieben im Batchmanager';
+$lang['porg_issue_1380'] = 'Album-Edition, Zeitlimit';
+$lang['porg_issue_1382'] = 'Fallback, wenn der lokale Speicher voll ist';
+$lang['porg_issue_1387'] = '[Album verschieben] Auto-Öffnen beim Drag & Drop verhindern';
+$lang['porg_issue_1388'] = 'Albumname max-length ist zu kurz in Album -> Verschieben.';
+$lang['porg_issue_1392'] = 'verwandte Alben, Performanceprobleme';
+$lang['porg_issue_1393'] = '[Albumsuche] Titelgröße ist falsch, wenn Pfad/Titel zu lang ist';
+$lang['porg_issue_1394'] = '[Gruppenmanager] Fokusproblem beim Hinzufügen eines Albums';
+$lang['porg_issue_1395'] = '[Gruppenmanager] Anzeige einer falschen Meldung, wenn das Feld für den Gruppennamen leer ist';
+$lang['porg_issue_1396'] = '[Album Verschieben] Übersetzungszeilen müssen für die Übersetzung hinzugefügt werden';
+$lang['porg_issue_1397'] = 'Deaktivieren eines Benutzers sollte seine Sitzungen beenden';
