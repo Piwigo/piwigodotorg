a128af628cc9bf01602c545ea9b05b842a07c3b4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-18 21:30:04 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-11.5.0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1398'] = '[Pluginhåndtering] Dropdownmenus ikon er for stor';
+$lang['porg_issue_1399'] = '[Albumsøgning] Begrænsning af søgeresultat til 100 albummer';
+$lang['porg_issue_1400'] = '[ViewSelector] Mørkt tema skal tilføjes';
+$lang['porg_issue_1410'] = '[11.x, brugerhåndtering] SQL-indsprøjtning';
+$lang['porg_issue_1412'] = '[Pluginhåndtering, 11.x] Visningsvælger ændrer sig efter første genindlæsning af side';
+$lang['porg_issue_1080'] = '[2.10] AdminTools defekt i uploadformlar';
+$lang['porg_issue_1241'] = 'Identifikation fejler ved tomt album';
+$lang['porg_issue_1306'] = 'Fejl i Batch Managers metadata-synkronisering';
+$lang['porg_issue_1323'] = '[11.x admin/albumflytning] Forkert albumrangering ved flytning';
+$lang['porg_issue_1362'] = 'Duplikeret gruppe, standardikon vises ikke';
+$lang['porg_issue_1364'] = 'Problem med plugin\'en AdminTools i \"Tag Manager\" og \"Move Albums\"';
+$lang['porg_issue_1368'] = 'Bruger med statussen \"guest\" kan stadig logge på i stilhed';
+$lang['porg_issue_1369'] = '[Tag Manager] Ukorrekt antal billeder ved filtrering';
+$lang['porg_issue_1372'] = 'nl_NL (Dutch/Nederlands) Gruppehåndtering er defekt';
+$lang['porg_issue_1377'] = 'Tilknytning/flytning til album i Batch Manager';
+$lang['porg_issue_1380'] = 'Albumredigering, timeout';
+$lang['porg_issue_1382'] = 'Fallback når localstorage er fyldt op';
+$lang['porg_issue_1387'] = '[Albumflytning] Forhindrer automatisk åbning under træk og slip';
+$lang['porg_issue_1388'] = 'Albumnavns maksimale længde er for kort i Album -> Flyt';
+$lang['porg_issue_1392'] = 'Relaterede albummer, problemer med ydeevne';
+$lang['porg_issue_1393'] = '[Albumsøgning] Titelstørrelse er ikke ideel når sti/titel er for lang';
+$lang['porg_issue_1394'] = '[Gruppehåndtering] Fokusproblem ved tilføjelse af album';
+$lang['porg_issue_1395'] = '[Gruppehåndtering] Visning af forkert besked når gruppes navnefelt er tomt';
+$lang['porg_issue_1396'] = '[Albumflytning] Oversættelseslinjer skal tilføjes til oversættelse';
+$lang['porg_issue_1397'] = 'Deaktivering af en bruger bør afslutte dennes session';
