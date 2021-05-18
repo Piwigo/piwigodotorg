0be8db5eb78d88a19c9896ea314e906cac77e222
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-18 13:22:33 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-11.5.0.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1412'] = '[Plugin Manager, 11.x] Wijzigingen in de weergaveselector nadat de eerste pagina opnieuw is geladen
+[+] opslaan';
+$lang['porg_issue_1410'] = '[11.x, gebruikersbeheerder] SQL-injectie';
+$lang['porg_issue_1400'] = '[ViewSelector] DarkMode thema moet worden toegevoegd';
+$lang['porg_issue_1399'] = '[Album Search] Beperk zoekresultaten tot 100 albums';
+$lang['porg_issue_1398'] = '[Plugin Manager] Grootte van drop-down-menu-icoon is te groot';
+$lang['porg_issue_1397'] = 'het deactiveren van een gebruiker moet zijn sessies beëindigen';
+$lang['porg_issue_1396'] = '[Album Move] Vertaling lijnen moeten worden toegevoegd voor vertaling';
+$lang['porg_issue_1395'] = '[Group Manager] Toont verkeerde boodschap wanneer groepsnaamveld leeg is';
+$lang['porg_issue_1394'] = '[Group Manager] Focus probleem bij het toevoegen van een album';
+$lang['porg_issue_1393'] = '[Album zoeken] Titelgrootte is niet goed wanneer pad/titel te lang is';
+$lang['porg_issue_1392'] = 'gerelateerde albums, prestatieproblemen';
+$lang['porg_issue_1388'] = 'Albumnaam max-lengte is te kort in Album -> Verplaatsen';
+$lang['porg_issue_1387'] = '[Album verplaatsen] voorkom auto-openen bij slepen en neerzetten';
+$lang['porg_issue_1382'] = 'Probleem met plug-in AdminTools in "Tag Manager" en "Albums verplaatsen"';
+$lang['porg_issue_1380'] = 'dubbele groep, standaardpictogram niet weergegeven';
+$lang['porg_issue_1377'] = '[11.x admin / album verplaats] verkeerde albumrang bij verplaatsing';
+$lang['porg_issue_1372'] = 'Bug met synchronisatie van metagegevens in Batchmanager';
+$lang['porg_issue_1369'] = 'Identificatie mislukt als de galerie leeg is';
+$lang['porg_issue_1368'] = '[2.10] AdminTools verbroken op uploadformulier';
+$lang['porg_issue_1362'] = 'dubbele groep, standaardpictogram niet weergegeven';
+$lang['porg_issue_1364'] = 'Probleem met plug-in AdminTools in "Tag Manager" en "Albums verplaatsen"';
+$lang['porg_issue_1323'] = '[11.x admin / album verplaats] verkeerde albumrang bij verplaatsing';
+$lang['porg_issue_1306'] = 'Bug met synchronisatie van metagegevens in Batchmanager';
+$lang['porg_issue_1241'] = 'Identificatie mislukt als de galerie leeg is';
+$lang['porg_issue_1080'] = '[2.10] AdminTools verbroken op uploadformulier';
