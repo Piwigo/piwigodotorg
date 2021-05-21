2d47aef11c85bb2f80914c93eebc5e4e5514adeb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-21 15:50:29 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-11.5.0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1412'] = '[Gerenciador de plug-ins, 11.x] Ver mudanças no seletor após recarregar a primeira página ';
+$lang['porg_issue_1410'] = '[11.x, gerenciador de usuários] injeção SQL';
+$lang['porg_issue_1400'] = '[ViewSelector] O tema DarkMode precisa ser adicionado';
+$lang['porg_issue_1399'] = '[Pesquisa de álbum] Limitar os resultados da pesquisa a 100 álbuns';
+$lang['porg_issue_1398'] = '[Gerenciador de plug-ins] O tamanho do ícone do menu suspenso é muito grande';
+$lang['porg_issue_1397'] = 'desativar um usuário deve encerrar suas sessões ';
+$lang['porg_issue_1396'] = '[Mover Album] As linhas de tradução precisam ser adicionadas para a tradução';
+$lang['porg_issue_1395'] = '[Gerente de grupo] Exibindo mensagem errada quando o campo do nome do grupo está vazio';
+$lang['porg_issue_1394'] = '[Gerente de grupo] Problema de foco ao adicionar um álbum';
+$lang['porg_issue_1393'] = '[Pesquisa de álbum] O tamanho do título não é bom quando o caminho/título é muito longo';
+$lang['porg_issue_1392'] = 'álbuns relacionados, problemas de desempenho';
+$lang['porg_issue_1388'] = 'O comprimento máximo do nome do álbum é muito curto em Álbum->Mover';
+$lang['porg_issue_1387'] = '[Movimento do álbum] impede a abertura automática enquanto arrasta e soltar';
+$lang['porg_issue_1380'] = 'edição do álbum, tempo passou do limite';
+$lang['porg_issue_1382'] = 'reserva quando o armazenamento local está cheio';
+$lang['porg_issue_1377'] = 'Associando/Mover para o álbum no gerenciador de lote';
+$lang['porg_issue_1372'] = 'Gerente de grupo nl_NL (Holandês/Nederlands) quebrado';
+$lang['porg_issue_1369'] = '[Gerência de Etiquetas] número incorreto de imagens ao filtrar';
+$lang['porg_issue_1368'] = 'usuário com status \"convidado\" ainda pode logar silenciosamente ';
+$lang['porg_issue_1364'] = 'Problema com o plugin AdminTools em \"Gerenciador de etiquetas\" e \"Mover álbuns\" ';
+$lang['porg_issue_1362'] = 'grupo duplicado, ícone padrão não mostrado ';
+$lang['porg_issue_1323'] = '[11.x admin/album move] classificação errada do álbum ao mover ';
+$lang['porg_issue_1306'] = 'Bug de sincronização de metadados do Batchmanager ';
+$lang['porg_issue_1241'] = 'A identificação falha quando a galeria está vazia ';
+$lang['porg_issue_1080'] = '[2.10] Ferramentas de administração não funcionam no formulário de envio ';
