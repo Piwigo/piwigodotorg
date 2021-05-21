2992737bee2c36d651226202478d9f3d7d7608ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-21 15:50:20 +0200

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/release-11.5.0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1377'] = 'Связь/Переход к альбому в пакетном менеджере';
+$lang['porg_issue_1364'] = 'Проблема с плагином AdminTools в "Tag Manager" и в "Move Albums"';
+$lang['porg_issue_1388'] = 'Макс.длина названия альбома слишком короткая в форме  Album -> Move';
+$lang['porg_issue_1080'] = '[2.10] AdminTools на работает Upload form';
+$lang['porg_issue_1368'] = 'пользователь со статусом «гость» все еще может незаметно войти в систему';
+$lang['porg_issue_1412'] = '[Plugin Manager, 11.x] Изменение селектора представления после перезагрузки первой страницы';
+$lang['porg_issue_1400'] = '[ViewSelector]  Тему Darkmode нужно добавить';
+$lang['porg_issue_1410'] = '[11.x, user manager] SQL injection';
+$lang['porg_issue_1382'] = 'отступление при переполнении локального хранилища';
+$lang['porg_issue_1392'] = 'связанные альбомы, проблемы с производительностью';
+$lang['porg_issue_1393'] = '[Album search] Размер заглаавия не соответствует, когда заглавие/пути слишком длинное';
+$lang['porg_issue_1394'] = '[Group Manager] Проблема фокусировки при добавлении альбома';
+$lang['porg_issue_1395'] = '[Group Manager] Отображение неправильного сообщения при пустом поле имени группы';
+$lang['porg_issue_1396'] = '[Album Move] Для перевода необходимо добавить строки перевода';
+$lang['porg_issue_1397'] = 'деактивация пользователя должна закончить его сессии';
+$lang['porg_issue_1398'] = '[Plugin Manager] Размер значка меню слишком велик';
+$lang['porg_issue_1399'] = '[Album Search] Ограничение результатов поиска - 100 альбомов';
+$lang['porg_issue_1323'] = '[11.x admin/album move] неправильный ранг альбома при перемещении';
+$lang['porg_issue_1372'] = 'nl_NL (Dutch/Nederlands) сбой менеджера групп';
+$lang['porg_issue_1380'] = 'издание альбома, тайм-аут';
+$lang['porg_issue_1387'] = '[Album move] предотвращение авто-открытия во время drag & drop';
+$lang['porg_issue_1241'] = 'Идентификация не удается, когда галерея пуста';
+$lang['porg_issue_1306'] = 'Ошибка синхронизации метаданных Batchmanager';
+$lang['porg_issue_1362'] = 'дублирование группы, значок по умолчанию не отображается';
+$lang['porg_issue_1369'] = '[Tag Manager] неправильное количество изображений при фильтрации';
