f20ac32f52f4ab9f42f71ca364707c5e62ec4cbb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-24 22:06:12 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = '[11.2] Il dashboard dell\'amministratore non è disponibile';
+$lang['porg_issue_1311'] = 'Impossibile eliminare il tag orfano';
+$lang['porg_issue_1312'] = 'Disfunzione parole chiavi/Pagina d\'amministrazione tag nella 11.2.0';
+$lang['porg_issue_1313'] = 'parentesi graffe deprecate in profile.php';
+$lang['porg_issue_1314'] = '[11.x, tag manager] manca il messaggio di successo dopo eliminazione orfana';
+$lang['porg_issue_1315'] = '[11.x admin/tags] filtro non funzionante con le maiuscole';
+$lang['porg_issue_1317'] = '[11.x] rimuove il calcolo delle dimensioni della directory della cache dal dashboard';
+$lang['porg_issue_1319'] = '[11.x] Spostamento del sottoalbum all\'inizio/fine dell\'elenco';
+$lang['porg_issue_1322'] = '[11.x tag manager] SyntaxError: token = imprevisto';
