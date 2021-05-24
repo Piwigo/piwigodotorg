aa73cdafd999e90e3353cf6b65c2f77fcce7aee1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-24 22:06:03 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-11.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1295'] = '[UX] Non nascondere i plugin inattivi dopo averne attivato uno nuovo';
+$lang['porg_issue_1297'] = '[11.x] ob_clean(): eliminazione del buffer non riuscita - download danneggiati';
+$lang['porg_issue_1298'] = '[11.x] pulsanti incoerenti per il gestore del tema';
+$lang['porg_issue_1299'] = '[11.x] plugin manager, aggiungi un suggerimento sul pulsante delle impostazioni non disponibili';
+$lang['porg_issue_1300'] = '[11.1 admin/intro] il nuovo calcolatore delle dimensioni della cache è scaduto';
+$lang['porg_issue_1302'] = '[Plugin Manager] keyboard shortcut per il filtro';
+$lang['porg_issue_1303'] = '[11.1 dashboard] settimana 0';
+$lang['porg_issue_1304'] = '[Dashboard 11.x] Offset non definito: 0 in admin/intro.php sulla riga 324';
+$lang['porg_issue_1307'] = '[11.x] nome dell\'album non reso nelle nuove funzionalità';
