d38ce6a7e0db6c07768c7ae9cf2a3bb288d74423
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-08-22 17:28:20 +0200

    [hu_HU] add mobile_apps_privacy_policy.lang.php (manual commit)

---
+++ language/hu_HU/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_privacy_mobile_banner'] = 'Itt követi a Piwigo Mobile alkalmazások adatvédelmi szabályzatát, mind az iOS (azaz az iPhone, iPod vagy iPad) és az Android (alapvetően minden más okostelefon).';
+$lang['porg_privacy_mobile_storage_3'] = 'Mikor elindítja az Alkalmazásokat, személyes adatai nem valamely követési technológia által kerülnek gyűjtésre.';
+$lang['porg_privacy_mobile_storage_4'] = 'Kizárólag gyorsítótárazási mechanizmusokat használunk az Alkalmazások testreszabhatósága és az Ön felhasználói élményének javítása érdekében.';
+$lang['porg_privacy_mobile_storage_5'] = 'Kikapcsolhatja és törölheti a gyorsítótár tartalmát, de vegye figyelembe, hogy ez befolyásolhatja az alkalmazások elérhetőségét és funkcionalitását.';
+$lang['porg_privacy_mobile_storage_1'] = 'Az Alkalmazások egyetlen szándék által vezérelve tárolnak adatokat: hogy távoli szerveren üzemeltetett Piwigo galériához biztosítsanak hozzáférést.';
+$lang['porg_privacy_mobile_storage_2'] = 'Semmi olyan adat nem kerül gyűjtésre, továbbításra vagy tárolásra, mely ezen a funkcionalitáson kívül esik.';
+$lang['porg_privacy_mobile_security_3'] = 'Az interneten ismertetett információk sebezhetőek a jogosulatlan felek lehallgatásához és visszaéléséhez.';
+$lang['porg_privacy_mobile_security_4'] = 'Ennél fogva nem tudunk teljes biztonságot garantálni, amennyiben Ön személyes adatokat ad meg.';
+$lang['porg_privacy_mobile_security_2'] = 'Noha konkrét lépéseket tettünk személyes információjának védelme érdekében, legyen tisztában vele, hogy ennek ellenére nem létezik olyan biztonsági megoldás, amely tökéletes, feltörhetetlen, és olyan adattovábbítási módszer sem létezik, mely folyamán garantálható lenne a védelem lehallgatás, vagy egyéb visszaélésekkel szemben.';
+$lang['porg_privacy_mobile_security_1'] = 'Technológiai biztonsági megoldásokat alkalmazunk annak érdekében, hogy segítsük az Ön személyes adatainak védelmét.';
+$lang['porg_privacy_mobile_protection_6'] = 'Mikor képeket tölt fel a Piwigo galériába, a képek tartalmazhatnak személyes adatokat, mint GPS koordináták, vagy megjegyzések.';
+$lang['porg_privacy_mobile_protection_7'] = 'Az Alkalmazások lehetőséget adnak ezen adatok feltöltést megelőző eltávolítására a Beállítások részben.';
+$lang['porg_privacy_mobile_protection_8'] = 'A döntés, hogy ezen adatok feltöltésre kerülnek-e, a Felhasználó kezében van.';
+$lang['porg_privacy_mobile_protection_1'] = 'Az Alkalmazások csak lokálisan tárolnak adatokat az eszközön, amin futnak.';
+$lang['porg_privacy_mobile_protection_2'] = 'Az Ön felelőssége, hogy ezeket az adatokat illetéktelen kezektől megvédje.';
+$lang['porg_privacy_mobile_protection_3'] = 'Erősen ajánlott a HTTPS használata az adatok küldése közbeni védelem biztosítása okán, illetve az Alkalmazások megpróbálnak titkosított kapcsolatot létrehozni HTTPS alatt az első alkalommal, mikor a Felhasználó csatlakozik a Piwigo galériához.';
+$lang['porg_privacy_mobile_protection_5'] = 'Amennyiben az Alkalmazások nem tudnak HTTPS kapcsolatot létrehozni, HTTP-re váltanak, ahol adatátvitel során nincs titkosítás.';
+$lang['porg_privacy_mobile_protection_4'] = 'A tanúsítványok és programkönyvtárak naprakészen tartása a Piwigo szerver üzemeltetőjének felelőssége.';
+$lang['porg_privacy_mobile_intro_7'] = 'Kérem figyelmesen olvassa ezt az adatvédelmi szabályzatot!';
+$lang['porg_privacy_mobile_intro_8'] = 'Amennyiben nem ért egyet az Adatvédelmi Szabályzat feltételeivel, kérjük ne használja az Alkalmazásokat!';
+$lang['porg_privacy_mobile_intro_5'] = 'Ez az adatbiztonsági szabályzat kifejti hogyan gyűjtjük, használjuk, hozzuk nyilvánosságra, és védjük az Ön adatait, mikor a mobilalkalmazásokat (az "Alkalmazások") használja.';
+$lang['porg_privacy_mobile_intro_4'] = 'A Piwigo-Android és Piwigo-Mobile fejlesztői, mi (vagy "mi", vagy "miénk") tiszteletben tartjuk felhasználóink ("felhasználó" vagy "Ön") személyes adatait.';
+$lang['porg_privacy_mobile_intro_6'] = 'Neveink hozzáférhetőek a git adattár metaadataiban, és a Köszönetnyilvánítás szekcióban az Alkalmazásokban.';
+$lang['porg_privacy_mobile_intro_14'] = 'Nem áll módunkban felelősséget vállalni ilyen harmadik fél által gyűjtött adatokért.';
+$lang['porg_privacy_mobile_intro_13'] = 'Az Adatvédelmi Szabályzat nem vonatkozik harmadik fél által üzemeltetett online/mobil alkalmazásboltokra, ahonnan telepíti az Alkalmazásokat, fizet rajtuk, beleértve az alkalmazáson belüli virtuális eszközöket, melyek további adatokat gyűjthetnek.';
+$lang['porg_privacy_mobile_intro_12'] = 'Az Adatvédelmi Szabályzat változását követően, az alkalmazás továbbhasználatával úgy fogjuk tekinteni, hogy Ön minden változásról értesült, azokkal egyetért, és elfogadta a frissített Adatvédelmi Szabályzatot.';
+$lang['porg_privacy_mobile_intro_11'] = 'Arra ösztönözzük, hogy időről időre fussa át ezt az Adatvédelmi Szabályzatot, hogy naprakész legyen az esetleges változásokkal kapcsolatban.';
+$lang['porg_privacy_mobile_intro_9'] = 'Fenntartjuk a jogot, hogy bármikor és bármilyen okból kifolyólag változtassunk az Adatvédelmi Szabályzaton.';
+$lang['porg_privacy_mobile_intro_10'] = 'A változásokról úgy értesülhet, hogy az Adatvédelmi Szabályzatban található "Frissítve" szekció dátuma megváltozik.';
+$lang['porg_privacy_mobile_intro_3'] = 'Nézze át az adatvédelmi szabályzatot, mielőtt bejelentkezne fiókjába a Piwigo-Androidban, vagy Piwigo-Mobileban, galériához csatlakozás céljából!';
+$lang['porg_privacy_mobile_intro_2'] = 'Ez az adatvédelmi szabályzat csak ezekre az Android és iOS alkalmazásokra vonatkozik. A Piwigo helyek, melyekhez csatlakoznak, a készítők hatáskörén kívül esnek.';
+$lang['porg_privacy_mobile_intro_1'] = 'A <a href="%s" target="_blank">Piwigo-Android</a> és <a href="%s" target="_blank">Piwigo-Mobile</a> ingyenes, szabad és nyílt forráskódú kliens alkalmazások Piwigo galériákhoz.';
+$lang['porg_privacy_mobile_data_piwigo_2'] = 'Ezen túlmenően minden fotó, videó és egyéb állomány ami letöltésre/feltöltésre lett kiválasztva, illetve a beírt adatok is tárolásra-, feldolgozásra-, és továbbításra kerülnek a beállított galéria-oldalhoz.';
+$lang['porg_privacy_mobile_data_piwigo_1'] = 'A fiók hozzáférési adatait az Alkalmazások tárolják, elküldik a beállított weboldalnak, hogy egy vagy több Piwigo galériához biztosítsanak hozzáférést.';
+$lang['porg_privacy_mobile_data_geo_2'] = 'Amennyiben meg szeretné változtatni ezeket az jogosultságokat, a készülék beállításaiban szabadon megteheti.';
+$lang['porg_privacy_mobile_data_geo_1'] = 'Fenntartjuk a jogot, hogy engedélyt kérjünk a mobileszköz helyadatainak hozzáféréséhez, és kövessük azt folymatosan az Alkalmazások használata közben, hogy hely-alapú szolgáltatásokat tudjunk nyújtani.';
+$lang['porg_privacy_mobile_data_device_3'] = 'Mikor a támogatási részlegünkhöz fordul, az eszközzel kapcsolatos bizonyos információk, mint a mobil eszköz egyedi azonosítója, gyártója, az operációs rendszer verziója, az Ön e-mail címe, tartózkódási országa és helye, valamint egyéb, Ön által szabadon megválasztott adataok eljutnak az Alkalmazások fejlesztő csapatainak.';
+$lang['porg_privacy_mobile_data_device_4'] = 'Azon túl, hogy ezek az adatok eljutnak a fejlesztőkhöz, nem tároljuk, továbbítjuk, vagy kezeljük ezeket az adatokat.';
+$lang['porg_privacy_mobile_data_device_2'] = 'Amennyiben meg szeretné változtatni ezeket az jogosultságokat, a készülék beállításaiban szabadon megteheti, de vegye figyelembe, hogy néhány ezek közül elengedhetetlen a mediafájlok mobilról történő feltöltéséhez a Piwigo galériába, vagy éppen a Piwigo media fájlok mobil eszközön történő tárolásához.';
+$lang['porg_privacy_mobile_data_device_1'] = 'Az Alkalmazások hozzáférést kérhetnek bizonyos, a mobil eszközökön elérhető szolgáltatásokhoz, beleértve a mobil eszköz fotókönyvtárát, tárhelyét, az Android AccountManagert, az Apple Keychaint, külső és felhő alapú tárolók mappáit, lokalizációs adatokat, és egyéb funkciókat.';
+$lang['porg_privacy_mobile_contact_1'] = 'Amennyiben kérdése, vagy megjegyzése van az Adatvédelmi Szabályzattal kapcsolatban, írjon nekünk:';
+$lang['or'] = 'vagy';
+$lang['Why does the mobile app store data?'] = 'Miért tárol a mobilalkalmazás adatokat?';
+$lang['What data is processed and stored?'] = 'Milyen adatokat kezelünk és tárolunk?';
+$lang['Security of your information'] = 'Az Ön információinak biztonsága';
+$lang['Privacy Policy for Mobile Apps'] = 'Adatvédelmi Szabályzat Mobil Alkalmazásokhoz';
+$lang['Piwigo Gallery Data'] = 'Piwigo Galéria Adatok';
+$lang['Mobile Device Data'] = 'Mobil Eszköz Adatai';
+$lang['Mobile Device Access'] = 'Hozzáférés a Mobil Eszközhöz';
+$lang['Last updated on %s'] = 'Frissítve:%s';
+$lang['How is the data protected?'] = 'Hogyan védjük az adatokat?';
+$lang['Geo-Location Information'] = 'Földrajzi Helyadatok';
+$lang['Contact us'] = 'Kapcsolat';
