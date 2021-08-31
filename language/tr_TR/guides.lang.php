a47fbc7901e2595df7e3c9994f628a5f48f1e3eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-08-31 22:44:50 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = 'Piwigo Kılavuzlarına hoş geldiniz. Burada Piwigo\'nuzu nasıl kuracağınız ve güncelleyeceğinizle ilgili temel bilgileri bulacaksınız. "Yardım et!" ile yönetim panelinin sayfalarında veya grafiksel bir soru işareti gördüğünüz herhangi bir yerde doğrudan Piwigo\'nuzda bazı yardımlar da sağlanır. Bir wiki, geliştirici belgelerini de kapsar.';
