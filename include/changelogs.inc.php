a4e2724b94f04f0885ccbfe358976eba2cf24749
HWFord <lilypickle@hotmail.fr>
2021-09-06 16:20:00 +0200

    Changelogs edit include so minor version are in array in major version
    also add major version parent and image to array

--- include/changelogs.inc.php
+++ include/changelogs.inc.php
@@ -15,9 +15,21 @@ foreach ($porg_releases as $version => $version_content)
   $porg_releases[$version]['state'] = 'minor';
 
   $major_version_ends_with = (version_compare($version, '11') >= 0) ? '.0.0' : '.0';
+
   if (preg_match('/'.preg_quote($major_version_ends_with).'$/', $version))
   {
     $porg_releases[$version]['state'] = 'major';
+    if($porg_releases[$version]['state'] = 'major'){
+      $porg_releases[$version]['image'] = $version_content['imageDestination'];
+    }
+  }
+
+  if($version > 11){
+      $major_version = explode(".", $version);
+      $porg_releases[$version]['majorVersion'] = $major_version[0].".0";
+  }else{
+      $major_version = explode(".", $version);
+      $porg_releases[$version]['majorVersion'] = $major_version[0].".".$major_version[1];
   }
 
   $porg_releases[$version]['released_on'] = porg_date_format($porg_releases[$version]['released_on']);
@@ -29,8 +41,25 @@ foreach ($porg_releases as $version => $version_content)
   unset($porg_releases[$version]['features']);
 }
 
+$porg_releases_major_versions = [];
+
+foreach ($porg_releases as $major_v => $version)
+{
+  if($porg_releases[$major_v]['state'] == 'major'){
+    $porg_releases_major_versions[$major_v] = $porg_releases[$major_v];
+  }
+}
+
+foreach ($porg_releases as $major_v => $version)
+{
+  if($porg_releases[$major_v]['state'] == 'minor'){
+    $parent_version = $porg_releases[$major_v]['majorVersion'].'.0';
+    $porg_releases_major_versions[$parent_version]['minorVersions'][$major_v] = $porg_releases[$major_v];
+  }
+}
+
 $template->assign(array(
-    'releases' => $porg_releases,
+    'releases' => $porg_releases_major_versions,
   )
 );
 
