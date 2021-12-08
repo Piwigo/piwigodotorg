7855979c3e9ced348bf1b3005746c6a41e6734d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-08 20:05:21 +0100

    Update da_DK, thanks to Kaare

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -161,3 +161,4 @@ $lang['porg_guides_install_step3_paragraph0'] = 'Når alle filer er overført, s
 $lang['porg_guides_install_step3_paragraph1'] = 'Nu følger MySQL-databaseindstillingerne og webmasterkontoen til administration af dit galleri';
 $lang['porg_guides_install_info2'] = 'Piwigo-holdet anbefaler FileZilla som FTP-klient, da den som Piwigo er fri software og kompatibel med Windows og Linux.';
 $lang['porg_get_started_question_desc1'] = 'Du kan få hjælp i vores <a href="%s">forum</a> eller finde dine egne svar i <a href="%s">dokumentationen</a>';
+$lang['Coding Activity'] = 'Kodeaktivitet';
