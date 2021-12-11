f8efb41712a3ef7ec915a02841c716499ceac62c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 11:40:51 +0100

    Update nl_NL, thanks to plg

---
+++ language/nl_NL/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1546'] = 'Piwigo updates \"nieuwe spannende functies\" link is leeg';
+$lang['porg_issue_1547'] = '[upgrade] desc-groepen mislukt op MySQL 8';
+$lang['porg_issue_1549'] = 'Geen extensie-upgrades terwijl het dashboard anders zegt';
+$lang['porg_issue_1550'] = 'Plug-inspagina is kapot';
+$lang['porg_issue_1552'] = '[12.0] na het uploaden bevat Album X nu 0 foto\\'s';
+$lang['porg_issue_1553'] = 'pwg.images.uploadVo;Voltooid details over bijgewerkte categorie toevoegen';
+$lang['porg_issue_1554'] = '[12.x] uploadformulier, onverwacht geselecteerd album';
+$lang['porg_issue_1555'] = 'ontbrekende vertaling op fotobewerkingspagina';
