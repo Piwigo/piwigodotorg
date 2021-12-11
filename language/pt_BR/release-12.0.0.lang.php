09543ce3ec898ccd05dc5721be0caea1937d1d35
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 20:37:30 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-12.0.0.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v12_user_manager_image3_label'] = 'A caixa de edição do usuário também foi redesenhada.';
+$lang['porg_v12_user_manager_image2_label'] = 'Quando definido no modo de seleção, você pode aplicar a mesma ação a vários usuários ao mesmo tempo.';
+$lang['porg_v12_user_manager_image1_label'] = 'O gerenciador de usuários foi reescrito do zero. Os usuários não podem ser editados e selecionados ao mesmo tempo, você está no modo de edição ou no modo de seleção. O usuário "convidado" foi removido da lista e recebe um botão dedicado. Filtros avançados foram adicionados.';
+$lang['porg_v12_technical_features7'] = 'Piwigo 11 era a última versão compatível com PHP 5. Piwigo 12 agora requer PHP 7.0. Também o tornamos compatível com PHP 8.';
+$lang['porg_v12_technical_features6'] = 'PHP 7 necessário, compatível com PHP 8';
+$lang['porg_v12_technical_features5'] = 'Novo mecanismo de fundo, o “saguão“, para bufferizar fotos durante o upload. Isso evitará muitas reconstruções do cache do usuário. Para torná-lo mais compreensível: a carga do servidor será muito menor em grandes galerias durante o upload de fotos. Quando dizemos "galerias grandes", queremos dizer 100.000 fotos e mais.';
+$lang['porg_v12_technical_features4'] = 'Saguão';
+$lang['porg_v12_technical_features3'] = 'Por favor leia isto para tornar suas extensões compatíveis com Piwigo 12.';
+$lang['porg_v12_technical_features2'] = 'Mudanças técnicas no Piwigo 12';
+$lang['porg_v12_technical_features15'] = 'Mudanças de API';
+$lang['porg_v12_technical_features14'] = 'Principalmente para se tornar compatível com o PHP 8, atualizamos algumas bibliotecas usadas no núcleo do Piwigo: Smarty 3.1.39, PHPMailer 6.5.1, JShrink 1.4.0.';
+$lang['porg_v12_technical_features13'] = 'Bibliotecas atualizadas';
+$lang['porg_v12_technical_features1'] = 'Observe que documentação mais detalhada está disponível sobre';
+$lang['porg_v12_plugin_manager_title'] = 'Gerenciador de plugins';
+$lang['porg_v12_plugin_manager_text'] = 'O gerenciador de plugins já recebeu um facelift com Piwigo 11, mas decidimos fazê-lo evoluir novamente para esta versão 12. <br>
+<br>
+1) Até agora, os plug-ins ativados e desativados eram exibidos separadamente. Agora são todos exibidos juntos e podem ser filtrados. <br>
+<br>
+2) Nas versões anteriores, quando você ativava um plugin, a tela recarregava e era necessário pesquisar na lista o plugin recém-ativado para acessar suas configurações. Nem um pouco prático, especialmente com muitos plug-ins.';
+$lang['porg_v12_plugin_manager_image1_label'] = 'Com o Piwigo 12, um único clique em um botão ativa o plug-in. Não é mais necessário recarregar a página. Mudador de vida! Bem ... talvez não sua vida "inteira", mas pelo menos sua vida quando você gerencia seus plug-ins!';
+$lang['porg_v12_maintenance_title'] = 'Manutenção';
+$lang['porg_v12_maintenance_image2_label'] = 'Manutenção agora exibe a lista de plug-ins ativos.';
+$lang['porg_v12_maintenance_image1_label'] = 'O design da página de manutenção foi atualizado e agora fornece mais detalhes sobre o tamanho do cache e a capacidade de atualizá-lo (antes da eliminação).';
+$lang['porg_v12_louis_text2'] = 'Depois trabalhei nas mudanças para a versão 12: integração de páginas preparadas por designers. Trabalhei especialmente na nova geração do gerenciador de usuários. <i> Nota do editor: o novo gerenciador de usuários será preenchido por Matthieu, que terá seu próprio foco no Piwigo 13! </i>';
+$lang['porg_v12_user_manager_title'] = 'Gerente de usuário';
+$lang['porg_v12_user_manager_text'] = 'Reescrita completa do gerenciador de usuários: mais moderno e colorido, mais limpo, melhor experiência do usuário com filtragem fácil e modo de seleção dedicado.';
+$lang['porg_v12_louis_text1'] = 'Meu nome é Louis e comecei meu trabalho na Piwigo em outubro de 2020 para estudos em ciências da computação. Comecei corrigindo problemas na versão 11 durante o período de teste beta.';
+$lang['porg_v12_intro3'] = 'Pegue um café e boa leitura.';
+$lang['porg_v12_intro2'] = 'A versão 12 vem muito mais rápida do que a versão 11. Decidimos trazer menos mudanças, mas em um ciclo de lançamento mais curto. Menos mudanças, mas ainda mudanças fortes. No esforço contínuo dos últimos anos, continuamos focando no painel de administração. E ainda não terminamos! Ainda temos muitas melhorias em preparação para o futuro. Gerenciador de usuários reescrito, gerenciador de plugins redesenhado, visualizador de atividades adicionado são algumas mudanças óbvias que você verá imediatamente. Menos visível, implementamos um novo mecanismo, o lounge, para deixar suas grandes galerias muito mais leves para carregar.';
+$lang['porg_v12_intro1'] = 'Mais curto, mais forte, mais leve, mais rápido';
+$lang['porg_v12_enora_text2'] = 'Usei meus aprendizados e os atualizei graças aos conselhos da Equipe. Eu queria atualizar as interfaces do usuário tentando adaptá-las para cada tipo de usuário';
+$lang['porg_v12_enora_text1'] = 'Meu nome é Enora e tive a sorte de me juntar à equipe Piwigo durante meus estudos de design de UI / UX. Tenho trabalhado em novos designs para a administração do Piwigo na versão 12. Minha principal prioridade era torná-lo mais amigável.';
+$lang['porg_v12_badge_refresh'] = 'atualizar ';
+$lang['porg_v12_badge_redesign'] = 'redesenhar';
+$lang['porg_v12_badge_new'] = 'novo';
+$lang['porg_v12_alternate_views_title'] = 'Vistas alternativas';
+$lang['porg_v12_alternate_views_image1_label'] = 'Na verdade, adicionado na versão 11.4, vamos apresentar esse novo recurso corretamente! Recebemos muitos feedbacks sobre a visão "lado a lado" do gerente do álbum em Piwigo 11. Muitas mensagens infelizes. Decidimos oferecer visualizações alternativas no gerenciador de álbum, gerenciador de plug-ins e no novo gerenciador de usuário.';
+$lang['porg_v12_album_sort_title'] = 'Classificar álbuns';
+$lang['porg_v12_album_sort_image1_label'] = 'Um novo botão "mover" vai do gerenciador de álbum para o movedor de álbum.';
+$lang['porg_v12_activities_title'] = 'Registro de atividades dos usuários';
+$lang['porg_v12_activities_text'] = 'Uma nova tela aparece no Piwigo 12: o registro de atividades do usuário. Permite que você veja as ações realizadas na administração do Piwigo, por cada usuário: adicionar / deletar / modificar um álbum, uma foto, um grupo ... Você pode filtrar esta lista para cada administrador.';
+$lang['porg_v12_activities_image1_label'] = 'Você pode até exportar essas atividades para um arquivo CSV que pode ser aberto com qualquer software de planilha (Excel, Libre Office ...).';
+$lang['The menu'] = 'O menu';
+$lang['Technical features'] = 'Características técnicas';
+$lang['Interview %s'] = 'Entrevista %s';
+$lang['A major update, focused on design and user experience, once more.'] = 'Uma grande atualização, com foco no design e na experiência do usuário, mais uma vez.';
