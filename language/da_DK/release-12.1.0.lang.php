7620ee5c90f0da7eb87b425a7139ff1e59a8600d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 11:41:30 +0100

    Update da_DK, thanks to plg

---
+++ language/da_DK/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1546'] = 'Linket Piwigo-opdateringer \"nye spændende funktioner\" er tomt';
+$lang['porg_issue_1547'] = '[opgradering] desc-grupper fejler under MySQL 8';
+$lang['porg_issue_1549'] = 'Ingen udvidelsesopgraderinger mens instrumentbrættet siger det modsatte';
+$lang['porg_issue_1550'] = 'Pluginside er defekt';
+$lang['porg_issue_1552'] = '[12.0] efter upload indeholder ALbum X nu 0 fotografier';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted tilføjer detaljer om opdateret kategori';
+$lang['porg_issue_1554'] = '[12.x] uploadformular, uventet valgt album';
+$lang['porg_issue_1555'] = 'manglende oversættelse på fotoredigeringsside';
