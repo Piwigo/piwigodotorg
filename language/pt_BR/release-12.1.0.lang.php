5460da5c2ad2825297776ca3f20cad07a90e70eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 11:40:47 +0100

    Update pt_BR, thanks to plg

---
+++ language/pt_BR/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1546'] = 'O link de atualizações do Piwigo \"novos recursos interessantes\" está em branco';
+$lang['porg_issue_1547'] = '[update] grupos desc falham no MySQL 8';
+$lang['porg_issue_1549'] = 'Nenhuma atualização de extensão enquanto o painel disser o contrário';
+$lang['porg_issue_1550'] = 'A página de plugins não funciona';
+$lang['porg_issue_1552'] = '[12.0] após o envio, o ?lbum X agora contém 0 fotos';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted adicionar detalhes sobre a categoria atualizada';
+$lang['porg_issue_1554'] = '[12.x] formulário de envio, álbum selecionado inesperado';
+$lang['porg_issue_1555'] = 'falta de tradução na página de edição de fotos';
