50de20f762b5a6477c6d8e71d961d32095383cce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 11:33:50 +0100

    Update de_DE, thanks to plg

--- language/de_DE/release-12.1.0.lang.php
+++ language/de_DE/release-12.1.0.lang.php
@@ -6,3 +6,10 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['porg_issue_1554'] = '[12.x] Upload-Formular, unerwartetes ausgewähltes Album';
+$lang['porg_issue_1546'] = 'Piwigo aktualisiert \"neue spannende Funktionen\" Link ist leer';
+$lang['porg_issue_1547'] = '[upgrade] desc groups schlägt bei MySQL 8 fehl';
+$lang['porg_issue_1549'] = 'Keine Erweiterungs-Upgrades, obwohl das Dashboard etwas anderes sagt';
+$lang['porg_issue_1550'] = 'Die Plugin-Seite ist defekt';
+$lang['porg_issue_1552'] = '[12.0] Nach dem Hochladen enthält Album X jetzt 0 Fotos';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted Details zur aktualisierten Kategorie hinzufügen';
+$lang['porg_issue_1555'] = 'fehlende Ubersetzung auf der Fotobearbeitungsseite';
