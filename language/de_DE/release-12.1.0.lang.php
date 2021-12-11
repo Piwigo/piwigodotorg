afce7b1520674301bd5fe5a007c48db828ba5891
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 11:31:08 +0100

    Update de_DE, thanks to plg

---
+++ language/de_DE/release-12.1.0.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1554'] = '[12.x] Upload-Formular, unerwartetes ausgewähltes Album';
