635c12272cddbd3daa73a2f4da1ffecb2a6ecbbc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 20:37:34 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-12.0.0.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v12_technical_features6'] = 'PHP 7 erforderlich, PHP 8 kompatibel';
+$lang['porg_v12_technical_features7'] = 'Piwigo 11 war die letzte Version, die mit PHP 5 kompatibel war. Piwigo 12 benötigt nun PHP 7.0. Wir haben es auch mit PHP 8 kompatibel gemacht.';
+$lang['porg_v12_user_manager_image2_label'] = 'Im Auswahlmodus können Sie die gleiche Aktion auf mehrere Benutzer gleichzeitig anwenden.';
+$lang['porg_v12_user_manager_image3_label'] = 'Auch die User-Edition-Box wurde neu gestaltet.';
+$lang['porg_v12_user_manager_text'] = 'Komplette Überarbeitung des Benutzermanagers: moderner und farbenfroher, aufgeräumter, bessere Benutzererfahrung mit einfacher Filterung und speziellem Auswahlmodus.';
+$lang['porg_v12_user_manager_title'] = 'Benutzer-Manager';
+$lang['porg_v12_technical_features1'] = 'Bitte beachten Sie, dass eine ausführlichere Dokumentation zu folgenden Themen verfügbar ist';
+$lang['porg_v12_technical_features13'] = 'Aktualisierte Bibliotheken';
+$lang['porg_v12_technical_features15'] = 'API-Änderungen';
+$lang['porg_v12_technical_features2'] = 'Technische Änderungen in Piwigo 12';
+$lang['porg_v12_technical_features3'] = 'Bitte lesen Sie sie, um Ihre Erweiterungen mit Piwigo 12 kompatibel zu machen.
+';
+$lang['porg_v12_technical_features4'] = 'Aufenthaltsraum
+';
+$lang['porg_v12_plugin_manager_title'] = 'Plugin-Manager';
+$lang['porg_v12_technical_features14'] = 'Hauptsächlich um mit PHP 8 kompatibel zu werden, haben wir einige im Piwigo-Kern verwendete Bibliotheken aktualisiert: Smarty 3.1.39, PHPMailer 6.5.1, JShrink 1.4.0.';
+$lang['porg_v12_technical_features5'] = 'Neuer Hintergrundmechanismus, die "Lounge", um Fotos während des Uploads zu puffern. Dadurch werden zu viele Neuaufbauten des Benutzer-Caches vermieden. Um es verständlicher zu machen: die Serverlast wird bei großen Galerien während des Foto-Uploads viel geringer sein. Wenn wir von "großen Galerien" sprechen, meinen wir 100.000 Fotos und mehr.';
+$lang['porg_v12_user_manager_image1_label'] = 'Die Benutzerverwaltung wurde von Grund auf neu geschrieben. Benutzer können nicht gleichzeitig bearbeitet und ausgewählt werden, man befindet sich entweder im Bearbeitungsmodus oder im Auswahlmodus. Der "Gast"-Benutzer wurde aus der Liste entfernt und erhält eine eigene Schaltfläche. Erweiterte Filter wurden hinzugefügt.';
+$lang['porg_v12_louis_text1'] = 'Mein Name ist Louis und ich habe meine Arbeit bei Piwigo im Oktober 2020 begonnen, um Informatik zu studieren. Angefangen habe ich mit der Behebung von Problemen der Version 11 während der Beta-Testphase.';
+$lang['porg_v12_louis_text2'] = 'Dann habe ich an Änderungen für die Version 12 gearbeitet: Integration von Seiten, die von Designern vorbereitet wurden. Ich habe insbesondere an der neuen Generation des Benutzermanagers gearbeitet. <i>Anmerkung der Redaktion: der neue Benutzermanager wird dann von Matthieu vervollständigt, der in Piwigo 13 seinen eigenen Schwerpunkt bekommen wird!</i>';
+$lang['porg_v12_maintenance_image1_label'] = 'Das Design der Wartungsseite wurde aufgefrischt und bietet nun mehr Details über die Cache-Größe und die Möglichkeit, den Cache zu aktualisieren (bevor er gelöscht wird).';
+$lang['porg_v12_maintenance_image2_label'] = 'Die Wartung zeigt jetzt die Liste der aktiven Plugins an.
+';
+$lang['porg_v12_maintenance_title'] = 'Wartung';
+$lang['porg_v12_plugin_manager_image1_label'] = 'Mit Piwigo 12 genügt ein einziger Klick auf einen Schalter, um das Plugin zu aktivieren. Kein Neuladen der Seite mehr. Ändert das Leben! Nun... vielleicht nicht Ihr "ganzes" Leben, aber zumindest Ihr Leben, wenn Sie Ihre Plugins verwalten!
+
+';
+$lang['porg_v12_plugin_manager_text'] = 'Der Plugin-Manager hat bereits mit Piwigo 11 ein Facelifting erhalten, aber wir haben uns entschieden, ihn für die Version 12 weiterzuentwickeln.<br>
+<br>
+1) Bisher wurden aktivierte und deaktivierte Plugins separat angezeigt. Jetzt werden sie alle zusammen angezeigt und können gefiltert werden.<br>
+<br>
+2) In früheren Versionen wurde beim Aktivieren eines Plugins der Bildschirm neu geladen und man musste in der Liste nach dem neu aktivierten Plugin suchen, um auf dessen Einstellungen zugreifen zu können. Überhaupt nicht praktisch, vor allem bei vielen Plugins.
+
+';
+$lang['porg_v12_alternate_views_title'] = 'Alternative Ansichten';
+$lang['porg_v12_badge_new'] = 'Neu';
+$lang['porg_v12_badge_redesign'] = 'neu gestalten';
+$lang['porg_v12_badge_refresh'] = 'aktualisieren';
+$lang['porg_v12_enora_text1'] = 'Mein Name ist Enora und ich habe das Glück, während meines UI/UX-Design-Studiums dem Piwigo-Team beizutreten. Ich habe an neuen Designs für die Piwigo-Verwaltung in Version 12 gearbeitet. Meine Hauptpriorität war es, die Benutzerfreundlichkeit zu erhöhen.';
+$lang['porg_v12_enora_text2'] = 'Ich habe meine Erkenntnisse genutzt und sie dank der Ratschläge des Teams verbessert. Ich wollte die Benutzeroberflächen auffrischen, indem ich versuchte, sie für jeden Benutzertyp anzupassen.';
+$lang['porg_v12_intro1'] = 'Kürzere, stärkere, leichtere, schnellere';
+$lang['porg_v12_intro2'] = 'Version 12 kommt viel schneller als Version 11. Wir haben uns entschlossen, weniger Änderungen, dafür aber in einem kürzeren Veröffentlichungszyklus zu bringen. Weniger Änderungen, aber immer noch starke Änderungen. Im Rahmen der kontinuierlichen Bemühungen der letzten Jahre konzentrieren wir uns immer noch auf das Administrationspanel. Und wir sind noch nicht fertig! Wir haben noch viele Verbesserungen in Vorbereitung für die Zukunft. Ein überarbeiteter Benutzermanager, ein neu gestalteter Plugin-Manager und eine neue Aktivitätsanzeige sind einige offensichtliche Änderungen, die Sie sofort sehen werden. Weniger sichtbar haben wir einen neuen Mechanismus, die Lounge, implementiert, um Ihre großen Galerien viel leichter zu laden.
+
+
+';
+$lang['porg_v12_intro3'] = 'Nehmen Sie sich einen Kaffee und genießen Sie Ihre Lektüre.';
+$lang['A major update, focused on design and user experience, once more.'] = 'Ein großes Update, das sich einmal mehr auf Design und Benutzerfreundlichkeit konzentriert.';
+$lang['Interview %s'] = 'Interview %s';
+$lang['Technical features'] = 'Technische Merkmale';
+$lang['The menu'] = 'Die Speisekarte';
+$lang['porg_v12_activities_image1_label'] = 'Sie können diese Aktivitäten sogar in eine CSV-Datei exportieren, die mit jedem Tabellenkalkulationsprogramm (Excel, Libre Office...) geöffnet werden kann.';
+$lang['porg_v12_activities_text'] = 'Ein neuer Bildschirm erscheint in Piwigo 12: das Benutzeraktivitätsprotokoll. Hier können Sie die von jedem Benutzer in der Piwigo-Verwaltung durchgeführten Aktionen sehen: Hinzufügen / Löschen / Ändern eines Albums, eines Fotos, einer Gruppe... Sie können diese Liste für jeden Administrator filtern.';
+$lang['porg_v12_activities_title'] = 'Benutzeraktivitätsprotokoll';
+$lang['porg_v12_album_sort_image1_label'] = 'Eine neue "Verschieben"-Schaltfläche führt vom Album-Manager zum Album-Mover.';
+$lang['porg_v12_album_sort_title'] = 'Alben sortieren';
+$lang['porg_v12_alternate_views_image1_label'] = 'Diese neue Funktion wurde in Version 11.4 hinzugefügt und wird nun richtig vorgestellt! Wir haben viele Rückmeldungen bezüglich der "Kachel"-Ansicht des Album-Managers in Piwigo 11 erhalten. Viele unglückliche Nachrichten. Wir haben uns entschlossen, alternative Ansichten für den Album-Manager, den Plugin-Manager und den neuen Benutzer-Manager anzubieten.';
