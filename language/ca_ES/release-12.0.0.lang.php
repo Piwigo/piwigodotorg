48e4c030f49111ac1769a4e0aa24b6b0901015ff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 20:37:42 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-12.0.0.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v12_user_manager_text'] = 'S\'ha reescrit completament el gestor d\'usuaris: més modern i acolorit, més net, millor experiència d\'usuari amb filtratge fàcil i un mode de selecció dedicat.';
+$lang['porg_v12_user_manager_image1_label'] = 'El gestor d\'usuaris s\'ha reescrit des de zero. Els usuaris no es poden editar i seleccionar alhora: heu d\'estar o bé en mode d\'edicio o bé en mode de selecció. L\'usuari "convidat" s\'ha eliminat de la llista i té un botó dedicat. S\'han afegit filtres avançats.';
+$lang['porg_v12_technical_features5'] = 'Nou mecanisme en el rerefons, la "sala d\'espera", per mantenir una memòria temporal de fotos durant la pujada. Això evitarà moltes reconstruccions de la memòria cau de l\'usuari. Per fer-ho més comprensible: la càrrega del servidor serà molt més baixa en galeries grans durant la pujada de fotos. Quan diem "galeries grans" volem dir 100.000 fotos i més.';
+$lang['porg_v12_plugin_manager_text'] = 'El gestor de connectors ja va rebre un rentat de cara amb el Piwigo 11, però hem decidit fer-lo evolucionar més per a aquesta versió 12.<br/>
+&nbsp;<br/>
+1) Fins ara, els connectors activats i desactivats es mostraven per separat. Ara es mostren tots junts i es poden filtrar.<br/>&nbsp;<br/>
+2) A les versions anteriors, quan s\'activava un connector es recarregava la pantalla i havíeu de cercar a la llista el connector que acabàveu d\'activar per accedir-ne als paràmetres. No era gens pràctic, especialment quan teníeu molts connectors.';
+$lang['porg_v12_plugin_manager_image1_label'] = 'Amb el Piwigo 12, un simple clic a un commutador activa el connector. No cal recarregar la pàgina. Us canviarà la vida! Bé, potser no "tota" la vostra vida, però almenys sí la vostra vida quan administreu els connectors!';
+$lang['porg_v12_maintenance_image1_label'] = 'El disseny de la pàgina de manteniment s\'ha renovat i ara dóna més detalls sobre la mida de la memòria cau i la possibilitat d\'actualitzar-la (abans de purgar).';
+$lang['porg_v12_louis_text2'] = 'Després he treballat en els canvis per a la versió 12: integració de les pàgines preparades pels dissenyadors. He treballat especialment en la nova generació del gestor d\'usuaris. <i>Nota de l\'editor: el nou gestor d\'usuaris serà completat pel Matthieu, que posarà el seu propi focus sobre el Piwigo 13!</i>';
+$lang['porg_v12_louis_text1'] = 'El meu nom és Louis i vaig començar la meva feina a Piwigo l\'octubre del 2020 per estudis d\'informàtica. He començat a arreglar problemes en la versió 11 durant el període de proves beta.';
+$lang['porg_v12_intro2'] = 'La versió 12 ha arribat molt més ràpida que la versió 11. Hem decidit introduir menys canvis però en un cicle de producció més curt. I són menys canvis, però són canvis importants. En l\'esforç continuat dels darrers anys encara ens estem centrant en el panell d\'administració. I encara no hem acabat! Encara estem preparant moltes millores per al futur. Hem reescrit el gestor d\'usuaris, hem redissenyat el gestor de connectors, hem afegit el visualitzador d\'activitats i alguns canvis obvis que veureu de seguida. Entre els canvis menys visibles, hem implementat un nou mecanisme, la sala d\'espera, per fer que les galeries grans siguin molt més lleugeres de carregar.';
+$lang['porg_v12_technical_features4'] = 'Sala d\'espera';
+$lang['porg_v12_intro3'] = 'Agafeu-vos un cafetó i gaudiu de la lectura.';
+$lang['porg_v12_maintenance_image2_label'] = 'Ara el manteniment mostra la llista de connectors actius.';
+$lang['porg_v12_maintenance_title'] = 'Manteniment';
+$lang['porg_v12_plugin_manager_title'] = 'Gestor de connectors';
+$lang['porg_v12_technical_features1'] = 'Teniu en compte que hi ha documentació més detallada sobre';
+$lang['porg_v12_technical_features13'] = 'Biblioteques actualitzades';
+$lang['porg_v12_technical_features14'] = 'Principalment per raons de compatibilitat amb PHP 8, hem actualitzat algunes biblioteques del núcli del Piwigo: Smarty 3.1.39, PHPMailer 6.5.1, JShrink 1.4.0.';
+$lang['porg_v12_technical_features15'] = 'Canvis a l\'API';
+$lang['porg_v12_technical_features2'] = 'Canvis tècnics al Piwigo 12';
+$lang['porg_v12_technical_features3'] = 'Llegiu-ho per fer que les vostres extensions siguin compatibles amb Piwigo 12.';
+$lang['porg_v12_technical_features6'] = 'Requereix PHP 7, i és compatible amb PHP 8';
+$lang['porg_v12_technical_features7'] = 'Piwigo 11 va ser l\'última versió compatible amb PHP 5. Piwigo 12 requereix PHP 7.0, i també l\'hem fet compatible amb PHP 8.';
+$lang['porg_v12_user_manager_image2_label'] = 'Quan esteu al mode de selecció, podeu aplicar la mateixa acció a diversos usuaris a la vegada.';
+$lang['porg_v12_user_manager_image3_label'] = 'La caixa d\'edició d\'usuaris també s\'ha redissenyat';
+$lang['porg_v12_user_manager_title'] = 'Gestor d\'usuaris';
+$lang['Interview %s'] = 'Entrevisteu %s';
+$lang['Technical features'] = 'Característiques tècniques';
+$lang['The menu'] = 'El menú';
+$lang['porg_v12_activities_image1_label'] = 'Podeu exportar aquestes activitats a un fitxer CSV que es pot obrir amb qualsevol programa de full de càlcul (Excel, Libre Office...)';
+$lang['porg_v12_activities_text'] = 'Apareix una nova pantalla al Piwigo 12: el Registre d\'activitats dels usuaris. Us permet veure les accions realitzades a l\'administració de Piwigo per cada usuari: afegir / suprimir / modificar un àlbum, una foto, un grup... Podeu filtrar la llista per cada administrador.';
+$lang['porg_v12_activities_title'] = 'Registre d\'activitats dels usuaris';
+$lang['porg_v12_album_sort_image1_label'] = 'Un nou botó "mou" va del gestor d\'àlbums al gestor de moviment d\'àlbums.';
+$lang['porg_v12_album_sort_title'] = 'Ordenació d\'àlbums';
+$lang['porg_v12_alternate_views_image1_label'] = 'De fet aquesta característica es va afegir a la versió 11.4; anem a presentar-la correctament! Hem rebut molts comentaris sobre la vista "en rajoles" del gestor d\'àlbums del Piwigo 11. Molts comentaris insatisfets. Hem decidit oferir vistes alternatives al gestor d\'àlbums, el gestor de connectors i el nou gestor d\'usuaris.';
+$lang['porg_v12_alternate_views_title'] = 'Vistes alternatives';
+$lang['porg_v12_badge_new'] = 'nou';
+$lang['porg_v12_badge_redesign'] = 'redisseny';
+$lang['porg_v12_badge_refresh'] = 'actualització';
+$lang['porg_v12_enora_text1'] = 'Em dic Enora, i he tingut la sort d\'unir-me al\'equip de Piwigo durant els meus estudis d\'UI/UX (Interfície d\'Usuari/Experiència d\'usuari). He estat treballant en nous dissenys per a l\'administració de Piwigo a la versió 12. La meva prioritat principal ha estat fer-lo més amigable per a l\'usuari.';
+$lang['porg_v12_enora_text2'] = 'He utilitzat el que he après i l\'he actualitzat gràcies als consells de l\'Equip. Volia adaptar les interfícies d\'usuari intentant adaptar-les per a cada tipus d\'usuari.';
+$lang['porg_v12_intro1'] = 'Més curt, més fort, més lleuger, més ràpid';
+$lang['A major update, focused on design and user experience, once more.'] = 'Una actualització important, centrada un cop més en el disseny i l\'experiència d\'usuari.';
