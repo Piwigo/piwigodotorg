93c334d4667a9d92554629f46a710b055952b3ac
plegall <plg@piwigo.org>
2021-12-17 13:14:45 +0100

    add release 12.0.0 release notes from lexiglot (failed to commit)

---
+++ language/nl_NL/release-12.0.0.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v12_user_manager_image3_label'] = 'De gebruikerseditiebox is ook opnieuw ontworpen';
+$lang['porg_v12_user_manager_text'] = 'Volledige herschrijving van de gebruikersmanager: moderner en kleurrijker, schoner, betere gebruikerservaring met eenvoudige filtering en speciale selectiemodus.';
+$lang['porg_v12_user_manager_title'] = 'Gebruikersbeheer';
+$lang['porg_v12_album_sort_title'] = 'Albums sorteren';
+$lang['porg_v12_louis_text2'] = 'Daarna werkte ik aan wijzigingen voor versie 12: integratie van pagina\'s opgesteld door ontwerpers. Ik heb vooral gewerkt aan de nieuwe generatie van de user manager. <i>Noot van de redactie: de nieuwe gebruikersmanager wordt dan gecompleteerd door Matthieu, die zijn eigen focus krijgt in Piwigo 13!</i>';
+$lang['porg_v12_maintenance_image1_label'] = 'Het ontwerp van de onderhoudspagina is vernieuwd en biedt nu meer details over de cachegrootte en de mogelijkheid om deze te vernieuwen (vóór het opschonen).';
+$lang['porg_v12_maintenance_image2_label'] = 'Onderhoud geeft nu de lijst met actieve plug-ins weer';
+$lang['porg_v12_maintenance_title'] = 'Onderhoud';
+$lang['porg_v12_plugin_manager_image1_label'] = 'Met Piwigo 12 activeert een enkele klik op een schakelaar de plug-in. Geen pagina herladen meer. Levensveranderaar! Nou... misschien niet je "hele" leven, maar in ieder geval je leven als je je plug-ins beheert!';
+$lang['porg_v12_plugin_manager_text'] = 'De plug-inmanager heeft al een facelift gekregen met Piwigo 11, maar we hebben besloten om deze opnieuw te laten evolueren voor deze versie 12.<br>
+<br>
+1) Tot nu toe werden geactiveerde en gedeactiveerde plug-ins afzonderlijk weergegeven. Nu worden ze allemaal samen weergegeven en kunnen ze worden gefilterd.<br>
+<br>
+2) In eerdere versies, wanneer u een plug-in activeerde, werd het scherm opnieuw geladen en moest u in de lijst zoeken naar de nieuw geactiveerde plug-in om toegang te krijgen tot de instellingen. Helemaal niet praktisch, vooral met veel plug-ins.';
+$lang['porg_v12_plugin_manager_title'] = 'Plugin manager';
+$lang['porg_v12_technical_features1'] = 'Houd er rekening mee dat er meer gedetailleerde documentatie beschikbaar is over:';
+$lang['porg_v12_technical_features13'] = 'Bijgewerkte bibliotheken';
+$lang['porg_v12_technical_features14'] = 'Voornamelijk om compatibel te worden met PHP 8, hebben we enkele bibliotheken bijgewerkt die in Piwigo core worden gebruikt: Smarty 3.1.39, PHPMailer 6.5.1, JShrink 1.4.0.';
+$lang['porg_v12_technical_features15'] = 'API-wijzigingen';
+$lang['porg_v12_technical_features2'] = 'Technische wijzigingen in Piwigo 12';
+$lang['porg_v12_technical_features3'] = 'Lees het om uw extensies compatibel te maken met Piwigo 12.';
+$lang['porg_v12_technical_features4'] = 'Lounge';
+$lang['porg_v12_technical_features5'] = 'Nieuw achtergrondmechanisme, de "lounge", om foto\'s te bufferen tijdens het uploaden. Dit voorkomt te veel herbouw van de gebruikerscache. Om het begrijpelijker te maken: de serverbelasting zal veel lager zijn op grote galerijen tijdens het uploaden van foto\'s. Met "grote galerijen" bedoelen we 100.000 foto\'s en meer.';
+$lang['porg_v12_technical_features6'] = 'PHP 7 vereist, PHP 8 compatibel';
+$lang['porg_v12_technical_features7'] = 'Piwigo 11 was de laatste versie die compatibel was met PHP 5. Piwigo 12 vereist nu PHP 7.0. We hebben het ook compatibel gemaakt met PHP 8.';
+$lang['porg_v12_user_manager_image1_label'] = 'De gebruikersmanager is helemaal opnieuw geschreven. Gebruikers kunnen niet tegelijkertijd worden bewerkt en geselecteerd, je bevindt je ofwel in de bewerkingsmodus of in de selectiemodus. De "gast" gebruiker is uit de lijst verwijderd en krijgt een speciale knop. Er zijn geavanceerde filters toegevoegd.';
+$lang['porg_v12_user_manager_image2_label'] = 'In de selectiemodus kunt je dezelfde actie op meerdere gebruikers tegelijk toepassen.';
+$lang['A major update, focused on design and user experience, once more.'] = 'Een grote update, wederom gericht op design en gebruikerservaring.';
+$lang['Interview %s'] = 'Interview %s';
+$lang['Technical features'] = 'Technische kenmerken';
+$lang['The menu'] = 'Het menu';
+$lang['porg_v12_activities_image1_label'] = 'Je kunt deze activiteiten zelfs exporteren naar een CSV-bestand dat kan worden geopend met elke spreadsheetsoftware (Excel, Libre Office ...).';
+$lang['porg_v12_activities_text'] = 'Er verschijnt een nieuw scherm in Piwigo 12: het gebruikersactiviteitenlogboek. Hiermee kunt je de acties zien die door elke gebruiker in de Piwigo-administratie zijn uitgevoerd: een album, een foto, een groep toevoegen / verwijderen / wijzigen ... Je kunt deze lijst voor elke beheerder filteren';
+$lang['porg_v12_activities_title'] = 'Gebruikersactiviteitenlogboek';
+$lang['porg_v12_album_sort_image1_label'] = 'Een nieuwe "verplaats"-knop gaat van de albummanager naar de albumverplaatser.';
+$lang['porg_v12_alternate_views_image1_label'] = 'Eigenlijk toegevoegd in versie 11.4, laten we deze nieuwe functie goed introduceren! We hebben veel feedback gekregen over de "tegel"-weergave van de albummanager in Piwigo 11. Veel ongelukkige berichten. We hebben besloten om alternatieve weergaven aan te bieden voor de albummanager, plug-inmanager en de nieuwe gebruikersmanager.';
+$lang['porg_v12_alternate_views_title'] = 'Albums sorteren';
+$lang['porg_v12_badge_new'] = 'nieuw';
+$lang['porg_v12_badge_redesign'] = 'herontwerp';
+$lang['porg_v12_badge_refresh'] = 'verversen';
+$lang['porg_v12_enora_text1'] = 'Mijn naam is Enora en ik heb het geluk gehad om deel uit te maken van het Piwigo-team tijdens mijn UI/UX-ontwerpstudies. Ik heb gewerkt aan nieuwe ontwerpen voor Piwigo-beheer in versie 12. Mijn belangrijkste prioriteit was om het gebruiksvriendelijker te maken.';
+$lang['porg_v12_enora_text2'] = 'Ik heb mijn lessen gebruikt en verbeterd dankzij de adviezen van het team. Ik wilde gebruikersinterfaces vernieuwen door ze aan te passen voor elk type gebruiker';
+$lang['porg_v12_intro1'] = 'Korter, sterker, lichter, sneller';
+$lang['porg_v12_intro2'] = 'Versie 12 komt veel sneller dan versie 11. We hebben besloten om minder wijzigingen aan te brengen, maar met een kortere release-cyclus. Minder veranderingen maar nog steeds sterke veranderingen. In de voortdurende inspanningen van de afgelopen jaren concentreren we ons nog steeds op het administratiepaneel. En we zijn nog niet klaar! We hebben nog veel verbeteringen in voorbereiding voor de toekomst. Herschreven gebruikersbeheer, opnieuw ontworpen plug-inbeheer, toegevoegde activiteitenviewer zijn enkele voor de hand liggende veranderingen die je meteen zult zien. Minder zichtbaar, we hebben een nieuw mechanisme geïmplementeerd, de lounge, om jouw grote galerijen veel lichter te maken om te laden.';
+$lang['porg_v12_intro3'] = 'Pak een kopje koffie en geniet van jouw te lezen tekst';
+$lang['porg_v12_louis_text1'] = 'Mijn naam is Louis en ik ben in oktober 2020 begonnen met mijn werk bij Piwigo voor een studie informatica. Ik ben begonnen met het oplossen van problemen met versie 11 tijdens de bètatestperiode';
---
+++ language/tr_TR/release-12.0.0.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v12_plugin_manager_text'] = 'Eklenti yöneticisi, sürüm 11\'de zaten bir fırça darbesi almıştı ama biz onu tekrar sürüm 12\'ye yükseltmeye karar verdik. <br>
+<br>
+1) Şimdiye kadar etkin ve devre dışı eklentiler ayrı ayrı görüntüleniyordu. Artık birlikte görüntüleniyorlar ve filtrelenebilirler. <br>
+<br>
+2) Önceki sürümlerde, bir eklentiyi etkinleştirdiğinizde, sayfa tamamen yeniden yüklenirdi ve yapılandırma sayfasına erişmek için etkinleştirilen eklentiler listesinde eklentiyi aramanız gerekiyordu. Hiç pratik değildi, özellikle çok sayıda eklentiyle. a';
+$lang['porg_v12_technical_features5'] = 'Aktarılan fotoğrafları beklemeye almak için yeni arka plan mekanizması, "bekleme salonu". Bu, kullanıcı önbelleğini çok sık yeniden oluşturmaktan kaçınacak. Daha basit bir ifadeyle: Fotoğrafları büyük bir galeriye yüklerken sunucu yükü önemli ölçüde azalacaktır. "Büyük" galeri dediğimizde yüz bin fotoğraf ve ötesinden bahsediyoruz. a';
+$lang['porg_v12_technical_features7'] = 'Piwigo 11, PHP 5 ile uyumlu Piwigo\'nun son sürümüydü. Piwigo 12\'nin PHP 7.0\'a ihtiyacı var. Ayrıca PHP 8 ile uyumlu hale getirdik. a';
+$lang['porg_v12_technical_features14'] = 'Temelde PHP 8 ile uyumlu olması için Piwigo çekirdeği tarafından kullanılan teknik modülleri güncelledik: Smarty 3.1.39, PHPMailer 6.5.1, JShrink 1.4.0.  a';
+$lang['porg_v12_user_manager_image1_label'] = 'Kullanıcı Yöneticisi sıfırdan yeniden yazılmıştır. Kullanıcılar artık aynı anda düzenlenemez ve seçilemez: ya düzenleme modundasınız ya da seçim modundasınız. "Misafir" kullanıcı listeden kaldırılır ve kendi özel düğmesi vardır. Yeni gelişmiş filtreler. a';
+$lang['porg_v12_plugin_manager_image1_label'] = 'Piwigo 12 ile, bir "anahtar" üzerine basit bir tıklama bir eklentiyi etkinleştirir. Artık sayfayı yeniden yüklemek yok. Hayatı değiştirir! Şey ... belki hayatınızın tamamını değil ama en azından eklentilerinizi yönettiğiniz zamanki hayatınızı! a';
+$lang['porg_v12_louis_text2'] = 'Ardından tasarımcıların hazırladığı modelleri entegre ederek Piwigo\'nun 12. versiyonuna yeni özellikler ekledim. Özellikle bu yeni nesil kullanıcı yöneticisi. <i> Editörün notu: Kullanıcı yöneticisi, Piwigo 13 için size tanıtılacak olan Matthieu tarafından tamamlanacak!</i> a';
+$lang['porg_v12_intro2'] = 'Sürüm 12, sürüm 11\'den çok daha hızlı geliyor. Elbette daha az yeni özellikle, ancak daha kısa bir sürüm döngüsüyle. Daha az değişiklik ama yine de önemli değişiklikler! Yönetim kısmında çalışmalarımızı sürdürüyoruz ve bu sonraki sürümlerde de devam edecek. Yeni bir kullanıcı yöneticisi, yeni bir eklenti yöneticisi, yöneticilerin etkinliklerinin bir görünümü 12\'nin en büyük yenilikleri. Daha az görünür olan yeni bir mekanizma ortaya çıkıyor, "bekleme salonu", en büyük fotoğraf kitaplıklarını çok daha hızlı hale getirecek. a';
+$lang['porg_v12_louis_text1'] = 'Benim adım Louis ve bilgisayar bilimi eğitimim için Ekim 2020\'de Piwigo\'ya geldim. İlk önce sürüm 11 için beta test döneminde son hata düzeltmeleri üzerinde çalıştım. a';
+$lang['porg_v12_enora_text2'] = 'Ekibin tavsiyesi ile geliştirip yeniden işleyebildiğim ve yönetimin birçok modelini yapmamı sağlayan bilgi birikimimi kullandım. Arayüzleri yenilemek ve herkesin kullanımını kolaylaştırmak için her tür Piwigo kullanıcısına uyum sağlamaya çalışmak istedim. a';
+$lang['porg_v12_enora_text1'] = 'Benim adım Enora ve UI/UX tasarım çalışmalarım sırasında Piwigo Ekibine katıldığım için şanslıyım. 12.sürümde Piwigo yönetimi için yeni tasarımlar üzerinde çalışıyorum. Ana önceliğim onu daha kullanıcı dostu hale getirmekti. a';
+$lang['porg_v12_alternate_views_image1_label'] = '11.4 sürümünde pratik olarak eklenen bu yeni özellik hakkında olması gerektiği gibi konuşma zamanı! Piwigo 11 albüm yöneticisinin "karo" görünümü hakkında çoğu olumsuz olan birçok geri bildirim aldık. Bu nedenle albüm yöneticisi, eklenti yöneticisi ve yeni kullanıcı yöneticisi hakkında alternatif görünüşler sunmaya karar verdik. a';
+$lang['porg_v12_activities_text'] = 'Piwigo 12\'de yeni bir sayfa belirir: Kullanıcı etkinlikleri. Her kullanıcı için yönetimde gerçekleştirilen eylemleri görmenizi sağlar: albümleri, fotoğrafları, grupları ekleme / silme / değiştirme ... Bu liste kullanıcı tarafından filtrelenebilir. a';
+$lang['porg_v12_album_sort_title'] = 'Albümleri sıralama a';
+$lang['porg_v12_album_sort_image1_label'] = 'Yeni bir "taşı" düğmesi, albüm yöneticisinden albümleri taşıma aracına gitmenizi sağlar. a';
+$lang['porg_v12_activities_image1_label'] = 'Hatta bu etkinlikleri herhangi bir elektronik tablo yazılımıyla (Excel, Libre Office…) açılabilen bir CSV dosyasına aktarabilirsiniz. a';
+$lang['Interview %s'] = '%s ile yapılan görüşme a';
+$lang['porg_v12_technical_features4'] = 'Bekleme Salonu a';
+$lang['porg_v12_badge_refresh'] = 'yenileme a';
+$lang['porg_v12_badge_redesign'] = 'tasarım yenileme a';
+$lang['porg_v12_maintenance_image1_label'] = 'Bakım sayfası şimdi önbellek boyutu ve onu yenileme yeteneği (temizlemeden önce) hakkında daha fazla ayrıntı sağlayacak şekilde yenilendi. a';
+$lang['porg_v12_intro3'] = 'Bir kahve alın ve keyifle okuyun. a';
+$lang['porg_v12_technical_features3'] = 'Yüklü eklentileri Piwigo 12 ile uyumlu hale getirmek için lütfen bunlrı okuyunuz. a';
+$lang['porg_v12_intro1'] = 'Daha Kısa, Daha Güçlü, Daha Hafif, Daha Hızlı a';
+$lang['A major update, focused on design and user experience, once more.'] = 'Bir kez daha, dizayn ve kullanıcı deneyimlerine odaklanan bir büyük güncelleme. a';
+$lang['Technical features'] = 'Teknik özellikler a';
+$lang['porg_v12_alternate_views_title'] = 'Alternatif görünümler a';
+$lang['porg_v12_badge_new'] = 'yeni a';
+$lang['porg_v12_activities_title'] = 'Kullanıcı faaliyetleri kaydı a';
+$lang['The menu'] = ' ';
+$lang['porg_v12_user_manager_image2_label'] = 'Seçim modu ayarlandığında, aynı eylemi birkaç kullanıcıya aynı anda uygulayabilirsiniz. a';
+$lang['porg_v12_technical_features1'] = 'Bunun hakkında daha ayrıntılı belgelerin mevcut olduğunu lütfen unutmayın: a';
+$lang['porg_v12_technical_features2'] = 'Piwigo 12 \'deki teknik değişiklikler a';
+$lang['porg_v12_technical_features6'] = 'PHP 7 gereklidir, PHP 8 uyumludur a';
+$lang['porg_v12_technical_features15'] = 'API değişiklikleri a';
+$lang['porg_v12_technical_features13'] = 'Güncellenen kütüphaneler a';
+$lang['porg_v12_plugin_manager_title'] = 'Eklenti yöneticisi a';
+$lang['porg_v12_maintenance_title'] = 'Bakım a';
+$lang['porg_v12_maintenance_image2_label'] = 'Bakım şimdi aktif eklentilerin listesini de görüntülemekte. a';
+$lang['porg_v12_user_manager_image3_label'] = 'Kullanıcı sürümü kutusu da yeniden tasarlandı. a';
+$lang['porg_v12_user_manager_title'] = 'Kullanıcı yöneticisi a';
+$lang['porg_v12_user_manager_text'] = 'Kullanıcı yöneticisi tamamen yeniden yazıldı: kolay filtreleme ve özel seçim modu ile daha modern ve renkli, daha temiz, daha iyi kullanıcı deneyimi. a';
