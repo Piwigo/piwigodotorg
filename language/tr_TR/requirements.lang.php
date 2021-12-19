dfcb030abb64dcf4c478a7adb763b643d1fc3771
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-19 18:52:46 +0100

    Update tr_TR, thanks to LazBoy

--- language/tr_TR/requirements.lang.php
+++ language/tr_TR/requirements.lang.php
@@ -5,5 +5,15 @@
 // | For copyright and license information, please view the COPYING.txt    |
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
-$lang['porg_guides_requirements_minimal'] = 'Minimum Gereksinimler';
-$lang['porg_guides_requirements_optional'] = 'İsteğe Bağlı Gereksinimler';
+$lang['porg_guides_requirements_minimal'] = 'Minimum Gereklilikler';
+$lang['porg_guides_requirements_optional'] = 'İsteğe Bağlı Gereklilikler';
+$lang['porg_guides_requirements_minimal_first'] = 'Nginx veya Apache gibi bir ağ sunucusu';
+$lang['porg_guides_requirements_paragraph'] = 'Piwigo\'nun çalışması için bir web barındırma gerekir. Tüm içeriğin (kurulum, barındırma, yedeklemeler) dahil olduğu bir fotoğraf galerisi çözümü için veya yalnızca kurulum yapmadan Piwigo\'yu denemek istiyorsanız, <a href="%s">Piwigo.com\'da ücretsiz bir deneme hesabı açmayı düşünebilirsiniz< /a>.';
+$lang['porg_guides_requirements_minimal_third'] = 'PHP 7.3+. Piwigo, PHP 5.3+ ile çalışabilir, ancak eski sürümler sitenizi güvenlik açıklarına maruz bırakabilir. <a href="https://www.php.net/supported-versions.php" target="_blank">resmi olarak PHP tarafından desteklenen sürümlere</a> bakın.';
+$lang['porg_guides_requirements_minimal_fourth'] = 'Bir grafik kitaplığı: Performansları ve görüntü kalitesi için ImageMagick önerilir, ancak genellikle PHP ile birlikte verilen GD de yeterlidir.';
+$lang['porg_guides_requirements_optional_first'] = 'Meta Verileri Yazma veya EXIF/IPTC meta verileriyle ilgilenen diğer herhangi bir eklenti için exiftool gereklidir';
+$lang['porg_guides_requirements_optional_third'] = 'PDF dosyalarından poster oluşturmak için pdftoppm önerilir (ImageMagick yerine)';
+$lang['porg_guides_requirements_optional_second'] = 'VideoJS eklentisinin video posteri oluşturması için ffmpeg gereklidir';
+$lang['porg_guides_requirements_minimal_sixth'] = 'Resimleriniz için yeterli disk alanı: Yüklediğiniz fotoğraflara ek olarak, Piwigo "birden çok boyutu" sunucunuzun bir önbellek dizininde saklayacaktır. ';
+$lang['porg_guides_requirements_minimal_second'] = 'MySQL 5.6+ veya MariaDB 10.1+. MySQL 5.0 çalışıyor ancak artık bakım destği bulunmuyor.';
+$lang['porg_guides_requirements_minimal_fifth'] = 'Dosyaları yüklemek için bir FTP istemci yazılımı gerekecektir (netinstall veya tam paket): Piwigo ekibi, Piwigo gibi ücretsiz olduğu ve Windows, Mac ve Linux ile uyumlu olduğu için FTP istemci yazılımı olarak FileZilla\'yı önerir.';
