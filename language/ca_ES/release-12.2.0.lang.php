f1b6ec073220700951f01fad4cc2a8d06f00bde5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-06 20:22:10 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1573'] = '[12.x] El botó d\'edició de la cerca d\'àlbums no enllaça amb l\'àlbum correcte';
+$lang['porg_issue_1574'] = '[12.x] El missatge de desbloqueig de galeria és incorrecte';
+$lang['porg_issue_1587'] = 'actualitzar amb Piwigo NG genera massa activitats';
+$lang['porg_issue_1590'] = '[Pàgina de manteniment] No es maneguen les característiques avançades';
+$lang['porg_issue_1591'] = '[12.x] incompatibilitat amb els connectors de openstreetmap/videojs';
+$lang['porg_issue_1557'] = '[12.1] el gestor d\'usuaris es queda carrregant per sempre amb el MySQL 8';
+$lang['porg_issue_1567'] = '[12.x] Els distintius dels connectors no s\'actualitzen amb les entrades de cerques';
+$lang['porg_issue_1569'] = '[12.x] Treure la definició de grup en el grup por defecte causa un gir infinit';
+$lang['porg_issue_1570'] = 'àlbums relacionats i noms multi-idioma';
