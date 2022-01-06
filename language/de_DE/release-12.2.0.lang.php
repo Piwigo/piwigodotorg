2a4d3cfe27e3b46136e718269ca3a5afa855c890
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-06 20:22:24 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1587'] = 'Upload mit Piwigo NG erzeugt zu viele Aktivitäten';
+$lang['porg_issue_1590'] = '[Wartungsseite] Erweiterte Funktionen werden nicht behandelt';
+$lang['porg_issue_1591'] = '[12.x] Inkompatibilität mit openstreetmap/videojs-Plugins';
+$lang['porg_issue_1573'] = '[12.x] Die Schaltfläche zum Bearbeiten der Albumsuche verlinkt nicht auf das richtige Album';
+$lang['porg_issue_1574'] = '[12.x] Meldung zum Entsperren der Galerie ist falsch';
+$lang['porg_issue_1557'] = '[12.1] Benutzermanager wird mit MySQL 8 nicht mehr geladen';
+$lang['porg_issue_1567'] = '[12.x] Plugin-Abzeichen werden nicht mit Sucheinträgen aktualisiert';
+$lang['porg_issue_1569'] = '[12.x] Wenn eine Gruppe standardmäßig nicht gruppiert ist, führt dies zu unendlichem Spin';
+$lang['porg_issue_1570'] = 'verwandte Alben und mehrsprachige Namen';
