20dfe4930dc95d7174cf0ebefa3cd3dd66fc9d19
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-13 06:26:07 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1557'] = '[12.1] Brugerhåndteringen indlæses i al evighed med MySQL 8';
+$lang['porg_issue_1567'] = '[12.x] Plugin-badges opdateres ikke med søgningsinddata';
+$lang['porg_issue_1569'] = '[12.x] Fjernelse af en gruppe som standardgruppe forårsager uendelig rotering';
+$lang['porg_issue_1570'] = 'Relaterede albummer og navne på flere sprog';
+$lang['porg_issue_1573'] = '[12.x] Knap til redigering af albumsøgning linker ikke til the korrekte album';
+$lang['porg_issue_1574'] = '[12.x] Oplåsning af galleri-meddelelsen er forkert';
+$lang['porg_issue_1587'] = 'Upload med Piwigo NG medfører for mange aktiviteter';
+$lang['porg_issue_1590'] = '[Vedligeholdelsesside] Avancerede funktioner håndteres ikke';
+$lang['porg_issue_1591'] = '[12.x] Inkompabilitet med openstreetmap-/videojs-plugins';
