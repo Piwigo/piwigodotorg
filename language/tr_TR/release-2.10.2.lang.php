6d9f40b55521942fc9ba233daa0c520751df53a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:24:01 +0200

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/release-2.10.2.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1168'] = '[API], pwg.images.setInfo ile XSS\'yi depoladı';
+$lang['porg_issue_1162'] = '[kullanıcılar] gruba göre filtrele';
+$lang['porg_issue_1155'] = '[PHP 7.4] implode\'un kullanımdan kaldırılmış kullanımı';
+$lang['porg_issue_1151'] = '[PHP 7.4] uyumluluk için düzeltmeler';
+$lang['porg_issue_1150'] = '[grup yöneticisi] Depolanan XSS güvenlik açığı';
+$lang['porg_issue_1139'] = '[PHP 7.4] Fix count() uyarısı';
+$lang['porg_issue_1134'] = '[PHP 7.4] dizi/dize ofset erişimi kullanımdan kaldırma uyarısı';
+$lang['porg_issue_1133'] = '[admin menubar] yükseklik çok küçük olduğunda erişilemeyen öğeler';
+$lang['porg_issue_1130'] = '[PHP-7.4] \"Null türünde bir değerde dizi offsetine erişmeye çalışılıyor\"';
+$lang['porg_issue_1119'] = 'Henüz fotoğraf eklenmemişken şifre sıfırlama sayfasına ulaşılamıyor';
+$lang['porg_issue_1114'] = 'Yönetici, fotoğraf yükleme uygulamaları sekmesi';
+$lang['porg_issue_1112'] = 'Yönetici Başlığı, daha küçük ekranlarda iki satır';
+$lang['porg_issue_1102'] = 'menüdeki etiket sayısını sınırla';
+$lang['porg_issue_1095'] = 'mysql 8 grupları ayrılmış bir anahtar kelimedir';
