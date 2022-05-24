818502991ad4ea109e53cafbcefce689e164585c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:23:15 +0200

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/netinstall.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_step3_paragraph3'] = '"Piwigo\'yu al ve sıkıştırmasını aç" eylemini çalıştırın ve ardından "Piwigo\'yu şimdi yükleyin"';
+$lang['porg_guides_netinstall_step3_paragraph2'] = 'Web sitenizdeki fotoğraf galeriniz için bir dizin adı seçin. Varsayılan olarak, Piwigo piwigo dizinine kurulacaktır. Hedef alanını boş bırakırsanız, Piwigo, NetInstall betiğini aktardığınız yere kurulacaktır.';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'NetInstall betiği aktarıldıktan sonra, örneğin bir web tarayıcısıyla web adresine gidin.';
+$lang['porg_guides_netinstall_step2_paragraph3'] = 'Sabit disk sürücünüzdeki piwigo-netinstall.php dosyasını seçin ve web sunucusuna aktarın';
+$lang['porg_guides_netinstall_step2'] = 'Adım 2 - NetInstall komut dosyasını aktarın';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">NetInstall komut dosyasını indirin.</a>';
+$lang['porg_guides_netinstall_step1'] = 'Adım 1 - NetInstall komut dosyasını indirin';
+$lang['porg_guides_netinstall_paragraph1'] = 'NetInstall, web sunucunuzdaki tek bir dosyanın aktarılmasını gerektirir. İçerdiği komut dosyası, Piwigo arşivini piwigo.org\'dan indirecek, ardından içeriğini çıkaracak ve yükleyiciyi başlatacaktır.';
+$lang['porg_guides_netinstall_info1'] = 'Sağlanan özelliklerin düzeyine bağlı olarak NetInstall işlemi web sunucunuzda çalışmayabilir. Çalıştıramazsanız, bunun yerine <a href="%s">manuel kurulum kılavuzuna</a> geçmeniz yeterlidir.';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo yüklü';
+$lang['porg_guides_netinstall_image_caption2'] = 'Fotoğraf galeriniz için bir klasör adı seçin';
+$lang['porg_guides_netinstall_image_caption1'] = 'Bağlantı yükleme';
