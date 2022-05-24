d05ea4170a02e1ebefc460595b0b88e9f20e1aa1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:23:19 +0200

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1591'] = '[12.x] openstreetmap/videojs eklentileriyle uyumsuzluk';
+$lang['porg_issue_1590'] = '[Bakım sayfası] Gelişmiş özellikler işlenmez';
+$lang['porg_issue_1587'] = 'Piwigo NG ile yükleme çok fazla aktivite oluşturuyor';
+$lang['porg_issue_1574'] = '[12.x] Galeri kilidini aç mesajı yanlış';
+$lang['porg_issue_1573'] = '[12.x] Albüm arama düzenleme düğmesi doğru albüme bağlanmıyor';
+$lang['porg_issue_1570'] = 'ilgili albümler ve çok dilli isimler';
+$lang['porg_issue_1569'] = '[12.x] Grubun varsayılan olarak grup olması, sonsuz dönüşe neden oluyor';
+$lang['porg_issue_1567'] = '[12.x] Eklenti rozetleri, arama girişleriyle güncellenmiyor';
+$lang['porg_issue_1557'] = '[12.1] kullanıcı yöneticisi MySQL 8 ile sonsuza kadar yükleniyor';
