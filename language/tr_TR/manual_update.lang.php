bd551b8e95c746d3a4802a6a02c10ed7ccaf1e8a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:23:23 +0200

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/manual_update.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_time_5'] = '15-120 dk...';
+$lang['porg_guides_manual_update_time_4'] = '2 dakika';
+$lang['porg_guides_manual_update_time_3'] = '1 dakika';
+$lang['porg_guides_manual_update_time_2'] = '5 dakika';
+$lang['porg_guides_manual_update_time_1'] = '3-5dk';
+$lang['porg_guides_manual_update_step9_paragraph1'] = 'Son sürümünüzle rahat olmaya başladınız, ziyaretçilere erişim sağlamak için galerinizin kilidini açmayı unutmayın.';
+$lang['porg_guides_manual_update_step9'] = '9. Adım - Galerinizin kilidini açın';
+$lang['porg_guides_manual_update_step8_paragraph5'] = 'Üyelerinizin veya ziyaretçilerinizin yeni sürümünüzle uyumlu olmayan belirli bir seçilmiş temaya sahip olabileceğini unutmayın. Muhtemelen Yönetici kullanıcılar sayfasında temalarını sıfırlamanız daha iyi olur.';
+$lang['porg_guides_manual_update_step8_paragraph4'] = 'Temalar için, herhangi bir testinizi geri almanız gerekebileceğinden, tarayıcınızda bir yönetici sayfasını açık tutun.';
+$lang['porg_guides_manual_update_step8_paragraph3'] = 'Önce bir yükseltme bulmaya çalışın; eklentiler sekme sayfaları size yardımcı olabilir.';
+$lang['porg_guides_manual_update_step8_paragraph2'] = 'Önceki tüm eklentilerinizin bir listesini göreceksiniz; bazıları çekirdeğe tam olarak entegre edilmiştir (Eklentiler Yöneticisi gibi), diğerleri artık dağıtılmıştır (LocalFiles Editor gibi), ancak devre dışı bırakılan herhangi bir eklentinin iyi bir nedeni vardır.';
+$lang['porg_guides_manual_update_step8_paragraph1'] = 'Tüm değişikliklerden haberdar olmadığınız için ilk kontrolleriniz zaman alabilir.';
+$lang['porg_guides_manual_update_step8'] = '8. Adım - Yükseltme sonucunu kontrol edin';
+$lang['porg_guides_manual_update_step7_paragraph5'] = 'Yükseltmeden önce etkin olan eklentiler, yeni sürümle çalışamayacakları ve kendi özel yükseltmelerine ihtiyaç duyabilecekleri için etkin olmayan duruma kapatılacaktır.';
+$lang['porg_guides_manual_update_step7_paragraph4'] = 'Önceki sürümünüz belirlenir ve yükseltme işlemlerinin bir özetini alırsınız;';
+$lang['porg_guides_manual_update_step7_paragraph3'] = 'Başka bir ziyaretçi tarafından herhangi bir yükseltmeden kaçınmak için oturum açmaya davetlisiniz';
+$lang['porg_guides_manual_update_step7_paragraph2'] = 'Web tarayıcınızda “upgrade.php” komut dosyasını açın ve kılavuzu izleyin, http://example.com/photos/upgrade.php';
+$lang['porg_guides_manual_update_step7_paragraph1'] = 'Yükseltme başlatılıyor';
+$lang['porg_guides_manual_update_step7_caption2'] = 'Not sayfasını yükseltin.';
+$lang['porg_guides_manual_update_step7_caption1'] = 'Yükseltme sayfası başlatılıyor.';
+$lang['porg_guides_manual_update_step7'] = 'Adım 7 - Veritabanı yükseltmesi';
+$lang['porg_guides_manual_update_step6_paragraph2'] = 'FTP istemcinizin herhangi bir hatayla karşılaşmadığını kontrol edin.';
+$lang['porg_guides_manual_update_step6_paragraph1'] = 'Yeni sürümü, yani “piwigo” dizininin içeriğini (1. adımda çıkarılan ve 3. adımda güncellenen) önceki Piwigo kurulum dizinine yüklemek için standart FTP istemcinizi kullanın.';
+$lang['porg_guides_manual_update_step6'] = 'Adım 6 - FTP yüklemesi';
+$lang['porg_guides_manual_update_step5_list6'] = 'Yerel(Local)';
+$lang['porg_guides_manual_update_step5_paragraph1'] = 'Mevcut Piwigo kurulumunuzun tüm dosyalarını aşağıdaki istisnalar dışında kaldırın, bu dizinleri SİLMEYİN:';
+$lang['porg_guides_manual_update_step5_list7'] = '_data';
+$lang['porg_guides_manual_update_step5_list5'] = 'Şablon uzantısı';
+$lang['porg_guides_manual_update_step5_list4'] = 'Temalar';
+$lang['porg_guides_manual_update_step5_list3'] = 'Eklentiler';
+$lang['porg_guides_manual_update_step5_list2'] = 'karşıya yükle';
+$lang['porg_guides_manual_update_step5_list1'] = 'Galeriler';
+$lang['porg_guides_manual_update_step5'] = 'Adım 5 - Temizleme';
+$lang['porg_guides_manual_update_step4_paragraph3'] = 'Şu andan itibaren, herhangi bir galeri sürümünün yönetici olmayan kullanıcıları şöyle bir şey görmelidir: "Galeri bakım için kilitlendi. Lütfen daha sonra tekrar gelin."';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Piwigo 2.3 veya önceki sürümü ile: Yapılandırma > Genel > Galeriyi kilitle. Piwigo 2.4+ için: Araçlar > Bakım.';
+$lang['porg_guides_manual_update_step4_paragraph1'] = 'Galeriyi kilitle';
+$lang['porg_guides_manual_update_step4_caption1'] = 'Galeriyi kilitle';
+$lang['porg_guides_manual_update_step4'] = 'Adım 4 - Galeri hazırlığı';
+$lang['porg_guides_manual_update_step3_paragraph2'] = 'Yalnızca özelleştirilmiş dosyalarınızı veya tüm dosyalarınızı yedekleyin';
+$lang['porg_guides_manual_update_step3_paragraph1'] = 'Piwigo 2.1 veya üstünü çalıştırıyorsanız bu adımı atlayın.';
+$lang['porg_guides_manual_update_step3_list1'] = '2.1 Yükseltme uzantısı aracını hazırlayın';
+$lang['porg_guides_manual_update_step1_paragraph3'] = 'Her durumda, yedeklemenin kapsamlı olup olmadığını ve başarıyla tamamlandığını kontrol ettiğinizden emin olun (sonuç bazen sunucu sınırları nedeniyle kesilebilir).';
+$lang['porg_guides_manual_update_step1_paragraph2'] = 'PhpWebGallery 1.7 ailesinden veya sonraki bir sürümü kullanıyorsanız, DB Yedekleme eklentisini kullanabilirsiniz. Önceki sürümler, kullanıcıların tablolarını başka yollarla, örneğin phpMyAdmin ile yedeklemeleri gerekir.';
+$lang['porg_guides_manual_update_step1_paragraph1'] = 'Mevcut tablolarınızı yedekleyin';
+$lang['porg_guides_manual_update_step1_list3'] = 'Varsayılan "piwigo/local" dizinini kaldırın.';
+$lang['porg_guides_manual_update_step1_list2'] = 'Bilgisayarınızda “piwigo” dizinine çıkarın.';
+$lang['porg_guides_manual_update_step1_list1'] = '<a href="%s">En son Piwigo sürüm arşivini indirin</a>';
+$lang['porg_guides_manual_update_step3_list3'] = 'web tarayıcınızdan prep21up.php dosyasını açın. http://example.com/photos/prep21up.php ve bir "upgrade21.zip" arşivi alacaksınız';
+$lang['porg_guides_manual_update_step3_list2'] = 'prep21up.php betiğini çıkarın ve Piwigo kurulumunuzun kök dizinine aktarın';
+$lang['porg_guides_manual_update_step3_list4'] = 'Bilgisayarınızda, “/local” dizinini “upgrade21.zip” dosyasından “piwigo” dizinine çıkarın (1. adımda çıkartılır).';
+$lang['porg_guides_manual_update_step3'] = 'Adım 3 - Dosya sunucusu hazırlığı';
+$lang['porg_guides_manual_update_step2_caption1'] = 'PhpMyAdmin önerilen seçenekler.';
+$lang['porg_guides_manual_update_step2'] = 'Adım 2 - Veritabanı hazırlığı (Önerilen)';
+$lang['porg_guides_manual_update_step10_paragraph2'] = 'Herhangi bir yükseltmeden sonra hiçbir şeyin kaldırılması gerekmez. “upgrade.php”nin kendisi kaldırılamaz. "Yükseltme gerekli değil"(“No upgrade required”) mesajının ve oturum açma işleminin galerinizi koruduğunu unutmayın.';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Hiç';
+$lang['porg_guides_manual_update_step10'] = 'Adım 10 - Yükseltme sonrası temizlik';
+$lang['porg_guides_manual_update_step1'] = 'Adım 1 - Paket hazırlama';
+$lang['porg_guides_manual_update_paragraph1'] = 'Bu prosedür, 1.4\'e eşit veya daha büyük herhangi bir sürümle uyumludur. Daha eski bir sürüm için forumdan yardım isteyin.';
+$lang['porg_guides_manual_update_info2'] = 'Tavsiyemiz: "Genişletilmiş ekler"( “Extended inserts”) seçimini kaldırın veya "Oluşturulan sorguların maksimum uzunluğunu"( “Maximal length of created queries”) azaltın.';
+$lang['porg_guides_manual_update_info1'] = 'Lütfen Piwigo\'nun mevcut <a href="%s">Gereksinimlerini</a> kontrol edin. Özellikle Php/MySQL yapılandırmanız yeterli DEĞİLSE, yükseltme işlemi başarısız olacağından daha ileri gitmeyin.';
