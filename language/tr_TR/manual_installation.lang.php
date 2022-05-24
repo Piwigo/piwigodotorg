5344c1147e0dd7dd1c77b791fab0fb7d03f9c037
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:23:57 +0200

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3.Tüm dosyaları seçin ve sunucunuzdaki “photos” dizinine aktarın.';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2.Sabit disk sürücünüzde, çıkarılan “piwigo” dizinini açın.';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1.Web sunucunuzda bir “photos” dizini oluşturun.';
+$lang['porg_guides_manual_installation_step2'] = '2. Adım - Arşiv içeriğini yükleyin';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = 'Arşiv içeriğini herhangi bir FTP istemcisi ile web sunucunuza aktarın.';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">Arşivin tamamını indirin</a> ve sıkıştırmasını açın.';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = 'Manuel kurulum sürecinde Piwigo\'nun tam arşivini indirir, içeriğini çıkarır ve web sunucunuza aktarırsınız.';
+$lang['porg_guides_manual_installation_step1'] = 'Adım 1 - Tam arşivi indirin';
+$lang['porg_guides_manual_installation_image_caption1'] = 'Bağlan ve aktar';
+$lang['porg_guides_manual_installation_info3'] = 'Piwigo\'yu web sitesinin köküne de yükleyebilirsiniz, “fotoğraflar” dizini zorunlu değildir. Hangi dizin adını seçerseniz seçin, Piwigo sürüm numarasını göstermemeniz önerilir.';
