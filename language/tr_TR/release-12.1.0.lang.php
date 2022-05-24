dba0488b81a7cb9a92407cdb6c412cd28efd12c7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:23:32 +0200

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1555'] = 'fotoğraf düzenleme sayfasında eksik çeviri';
+$lang['porg_issue_1554'] = '[12.x] yükleme formu, beklenmeyen seçilmiş albüm';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted, güncellenen kategori hakkında ayrıntılar eklendi';
+$lang['porg_issue_1552'] = '[12.0] yüklendikten sonra, X Albümü artık 0 fotoğraf içeriyor';
+$lang['porg_issue_1550'] = 'Eklentiler sayfası bozuk';
+$lang['porg_issue_1549'] = 'Pano aksini söylerken uzantı yükseltmesi yok';
+$lang['porg_issue_1547'] = '[yükseltme] açıklama grupları MySQL 8\'de başarısız oluyor';
+$lang['porg_issue_1546'] = 'Piwigo güncellemeleri \"yeni heyecan verici özellikler\" bağlantısı boş';
