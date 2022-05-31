0b9c737d9b4aff613514c0f29ca7ad884855c911
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-31 13:00:32 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1591'] = '[12.x] incompatibiliteit met openstreetmap/videojs-plug-ins';
+$lang['porg_issue_1590'] = '[Onderhoudspagina] Geavanceerde functies worden niet behandeld';
+$lang['porg_issue_1587'] = 'uploaden met Piwigo NG genereert te veel activiteiten';
+$lang['porg_issue_1574'] = '[12.x] Ontgrendel galerijbericht is verkeerd';
+$lang['porg_issue_1573'] = '[12.x] Bewerkknop voor album zoeken linkt niet naar het juiste album';
+$lang['porg_issue_1570'] = 'gerelateerde albums en meertalige namen';
+$lang['porg_issue_1569'] = '[12.x] Het standaard uitschakelen van groep als groep veroorzaakt oneindige spin';
+$lang['porg_issue_1567'] = '[12.x] Plug-insbadges worden niet bijgewerkt met zoekinvoer';
+$lang['porg_issue_1557'] = '[12.1] gebruikersbeheerder laadt voor altijd met MySQL 8';
