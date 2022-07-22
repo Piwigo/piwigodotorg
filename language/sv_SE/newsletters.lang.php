31bdbf81c0feded5ac37a2d95f529b6bba641e4c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-07-22 06:43:03 +0200

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/newsletters.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Enter your email here'] = 'Skriv in din e-post här';
+$lang['Sign up'] = 'Anmäl dig';
+$lang['porg_newsletters_desc1'] = 'Håll dig uppdaterad med Piwigo-nyheter.';
+$lang['porg_newsletters_desc2'] = 'Du kommer få e-post när en ny release är tillgänglig och när stora händelser inträffar med projektet.';
+$lang['porg_newsletters_subscribe_title'] = 'Prenumerera på vårt nyhetsbrev och håll dig uppdaterad!';
