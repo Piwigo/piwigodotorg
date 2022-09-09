45a0bb9e47dafc7d799206ba391bdc1eeb15f08d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-09 15:41:59 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.0.0.lang.php
@@ -0,0 +1,71 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v13_technical_features15'] = 'API-Änderungen';
+$lang['porg_v13_technical_features2'] = 'Technische Änderungen in Piwigo 13';
+$lang['porg_v13_technical_features3'] = 'Bitte lesen Sie sie, um Ihre Erweiterungen mit Piwigo 13 kompatibel zu machen.';
+$lang['porg_v13_technical_features4'] = 'Präferenzen der Benutzer';
+$lang['porg_v13_technical_features12'] = 'mehr erfahren';
+$lang['porg_v13_technical_features13'] = 'Klassen und Daten für &lt;body&gt;';
+$lang['porg_v13_technical_features14'] = 'Um die Anpassung und die mögliche Verarbeitung im Frontend zu vereinfachen, haben wir CSS-Klassen für den &lt;body&gt; Tag hinzugefügt. Und auch ein <code>data</code>-Attribut.
+';
+$lang['porg_v13_tagrename_image1_label'] = 'Unterstützung von SVG-Dateien';
+$lang['porg_v13_tagrename_title'] = 'Tag-Umbenennung';
+$lang['porg_v13_technical_features1'] = 'Bitte beachten Sie, dass eine ausführlichere Dokumentation zu folgenden Themen verfügbar ist';
+$lang['porg_v13_technical_features5'] = 'Zwischen den Konfigurationsvariablen (die allen Benutzern gemeinsam sind) und den Cookies (die für einen Webbrowser spezifisch sind) angesiedelt, ermöglichen die Benutzereinstellungen, dass eine benutzerspezifische Präferenz unabhängig von der Sitzung bestehen bleibt. Neue Funktionen <code>userprefs_get_param</code>, <code>userprefs_update_param</code>, <code>userprefs_delete_param</code>.';
+$lang['porg_v13_technical_features6'] = 'Kompatibilität mit PHP 8.1';
+$lang['porg_v13_plugin_manager_title'] = 'Plugin-Manager';
+$lang['porg_v13_svg_title'] = 'Unterstützung für PSVG-Dateien';
+$lang['porg_v13_multiformat_title'] = 'Mehrere Formate';
+$lang['porg_v13_news_image1_label'] = 'Piwigo 13 zeigt die neuesten Nachrichten (wenn sie weniger als 30 Tage alt sind) von piwigo.org an.';
+$lang['porg_v13_news_title'] = 'Neueste Nachrichten';
+$lang['porg_v13_history_title'] = 'Historie besuchen';
+$lang['porg_v13_intro1'] = 'Fokussiert auf Benutzererfahrung';
+$lang['porg_v13_matthieu_text2'] = 'Ich habe noch ein Jahr bis zum Ende meines Studiums vor mir und ich hoffe, dass ich dank Piwigo noch mehr lernen kann!
+';
+$lang['porg_v13_associations_title'] = 'Verknüpfte Alben';
+$lang['porg_v13_badge_new'] = 'Neu';
+$lang['porg_v13_badge_redesign'] = 'Umgestaltung';
+$lang['porg_v13_badge_refresh'] = 'aktualisieren';
+$lang['porg_v13_history_image1_label'] = 'Auf vielfachen Wunsch können Sie nach "action=downloaded" filtern und nur die Liste der Downloads anzeigen.';
+$lang['porg_v13_apppromo_title'] = 'Mobile Anwendungen';
+$lang['porg_v13_api_s7'] = 'neuer Eingabeparameter <code>Position</code>';
+$lang['porg_v13_api_s8'] = 'neuer Ausgabeparameter <code>ist_favorite</code>';
+$lang['porg_v13_apibrowser_image1_label'] = 'Große Überarbeitung des API-Explorers. Ein Werkzeug für Entwickler. Integration eines Suchfilters in den Baum der verfügbaren Methoden.';
+$lang['porg_v13_apibrowser_title'] = 'API-Browser';
+$lang['porg_v13_api_s5'] = 'neuer Eingabeparameter <code>format_von</code>';
+$lang['porg_v13_api_s6'] = 'neue Eingabeparameter <code>Suche</code> und <code>zusätzliche_Ausgabe</code>';
+$lang['porg_v13_api_s3'] = 'eine Benutzerpräferenz festlegen';
+$lang['porg_v13_api_s4'] = 'Suche in der Historie der Besuche';
+$lang['porg_v13_plugin_manager_image1_label'] = 'Die Plugin-Installationsseite wurde modernisiert, mit Filtern angereichert und zeigt mehr Informationen über die verfügbaren Plugins an. Piwigo 13 führt ein Zertifizierungskonzept ein, das das Alter der einzelnen Plugins berücksichtigt.';
+$lang['porg_v13_plugin_manager_image2_label'] = 'Die Plugin-Update-Seite wurde neu gestaltet und bietet nun mehr Informationen. Beachten Sie, dass dies dieselbe Seite ist wie die Hauptseite zur Aktualisierung von Erweiterungen: Plugins, Themen, Sprachen.';
+$lang['porg_v13_plugin_manager_text'] = 'Nach der umfassenden Überarbeitung der ersten Registerkarte zur Verwaltung der installierten Plugins haben wir die Registerkarten zur Aktualisierung und Installation neuer Plugins übernommen.';
+$lang['porg_v13_remi_text1'] = 'Mein Name ist Rémi und ich arbeite seit mehr als einem Jahr [Anmerkung der Redaktion: seit Februar 2021] als Entwickler bei Piwigo. Ich bin dem Team beigetreten mit dem ersten Ziel, die Android-App mit der iOS-App gleichziehen zu lassen. Nicht einfach, aber ich bin motiviert!';
+$lang['porg_v13_remi_text2'] = 'Nachdem ich mich bemüht hatte, die alte Android-Anwendung zu übernehmen, bin ich dazu übergegangen, eine neue Anwendung zu erstellen. Piwigo NG verwendet neue Technologien [Anm. d. Red.: Flutter], um die Codierung zu erleichtern. Ich bin sehr stolz darauf, am Ursprung dieser Anwendung zu stehen. Ich bin auch stolz darauf, Teil eines so freundlichen und aufgeschlossenen Teams wie Piwigo zu sein.';
+$lang['porg_v13_svg_text'] = 'Sie wurden bereits als "beliebiger Dateityp" unterstützt, aber Piwigo 13 ist in der Lage, die SVG-Datei direkt auf der Webseite anzuzeigen. Wenn Sie Piwigo verwenden, um Ihre Logos oder Icons zu organisieren, werden Sie diese Verbesserung wirklich zu schätzen wissen!
+
+Wir haben bereits daran gearbeitet, Modus (Standard-Theme) und Bootstrap Darkroom (zweitbeliebtestes Theme) mit dieser neuen Funktion kompatibel zu machen.';
+$lang['porg_v13_technical_features7'] = 'PHP 8.1 kommt zu vielen Webhosts und viele von Ihnen haben Probleme mit Piwigo 12 gemeldet. Die Kompatibilität mit PHP 8.1 erforderte den Wechsel zu Smarty 4.1 (Template Engine).';
+$lang['porg_v13_multiformat_text'] = 'Mehrere Formate können genau wie Fotos hochgeladen werden. Die Funktion "mehrere Formate" wurde vor 6 Jahren in Piwigo 2.8 eingeführt, aber man konnte sie nur mit der Methode "Synchronisierung" hinzufügen. Diese Methode wird immer weniger genutzt, im Gegensatz zur "Web-Upload"-Methode. Sie können nun die Vorteile der Mehrfachformate mit dem Web-Upload-Formular nutzen.
+
+Aber Moment... was genau ist "mehrere Formate"? Es ist die Möglichkeit, ein Foto mit zusätzlichen Formaten zu erweitern. Sie haben zum Beispiel das JPG als Basisfoto und erweitern es mit einer RAW-Datei, einer TIFF-Datei, einem anderen JPG mit CMYK-Farbprofil... was immer Sie wollen. Die Formate sind neben den Fotos als "Download-Optionen" aufgeführt.';
+$lang['porg_v13_intro2'] = 'Piwigo arbeitet ständig daran, Ihre Benutzererfahrung so nahtlos wie möglich zu gestalten. So finden Sie in dieser Version 13 Designänderungen in der Verwaltung, einen neuen Album-Manager mit Baumstruktur, die Möglichkeit, mehrere Formate direkt von Ihrem Webbrowser aus hinzuzufügen, ein vollständigeres Plugin-Installationsprogramm... Auf der technischen Seite erhält Piwigo Kompatibilität mit PHP 8.1, das allmählich überall ankommt. Ein gutes Update für alle!';
+$lang['porg_v13_matthieu_text1'] = 'Grüße an alle! Ich bin Matthieu und arbeite schon seit eineinhalb Jahren bei Piwigo [Anm. d. Red.: seit Februar 2021], auch wenn Sie erst jetzt von mir hören. Ich bin während meines Studiums bei Epitech zum Team gestoßen. Meine Aufgabe bei Piwigo ist es, neue Funktionen in die Software zu implementieren und die Benutzeroberfläche zu modernisieren, vor allem im Administrationsbereich, aber auch für gute Laune bei unseren Treffen zu sorgen.';
+$lang['porg_v13_multiformat_image1_label'] = 'With Piwigo 13, the web form for adding photos is enriched with an add format mode, which can be activated in the top right corner. Use the local configuration setting <code>$conf[\'enable_formats\']</code> to make this switch visible.';
+$lang['porg_v13_apppromo_image1_label'] = 'Piwigo 13 wird mobile Anwendungen fördern. Zu viele Nutzer kennen sie noch nicht! Natürlich ist dieses Werbebanner mit 1 Klick "für immer" versteckt.';
+$lang['porg_v13_apppromo_text'] = 'Wie Sie vielleicht schon in den Nachrichten gesehen haben, hat Piwigo jetzt ein solides Angebot für Smartphones, sowohl für iOS (iPhone) als auch für Android (alle anderen Smartphones).';
+$lang['porg_v13_associations_image1_label'] = 'Die Benutzeroberfläche für die Auswahl der mit einem Foto verknüpften Alben wurde mit 2 Zielen völlig neu gestaltet: 1) das Gewicht der Seite (und damit die Anzeigegeschwindigkeit) unabhängig von der Anzahl der Alben in der Galerie zu machen, 2) die zugehörigen Alben mit einem einfachen Klick bearbeiten zu können.';
+$lang['porg_v13_history_text'] = 'Die Suchfunktion in der Besuchshistorie wurde überarbeitet. Sie ist sowohl ästhetisch als auch technisch modernisiert, aber es ist auch möglich, Filter zu kombinieren, indem man auf ein Foto, einen Nutzer oder eine IP-Adresse klickt.';
+$lang['porg_v13_albums_title'] = 'Album-Manager';
+$lang['porg_v13_api_s1'] = 'ein Foto suchen, das dem angegebenen Format entspricht';
+$lang['porg_v13_api_s2'] = 'ein erweitertes Fotoformat löschen';
+$lang['Interview %s'] = 'Interview von %s';
+$lang['New album manager, new visit log, multiple-format improved.'] = 'Neuer Album-Manager, neues Besuchsprotokoll, mehrere Formate verbessert.';
+$lang['Technical features'] = 'Technische Merkmale';
+$lang['The menu'] = 'Das Menü';
+$lang['porg_v13_albums_image1_label'] = 'Dort können Sie Unteralben erstellen, ein Album umbenennen oder ein Album verschieben, ohne die Seite zu wechseln. Zeitersparnis garantiert.';
+$lang['porg_v13_albums_text'] = 'Wir haben die Seite zum Verschieben von Alben und die Seite zur Verwaltung von Alben zusammengelegt. So zeigt der neue Album-Manager eine Baumansicht, in der Sie die gesamte Hierarchie Ihrer Alben sehen können, ohne jederzeit die Seite wechseln zu müssen.';
