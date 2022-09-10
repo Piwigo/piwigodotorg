026c69ecfcb19c34e561fdf7a8b95854bdd769ca
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-10 06:18:22 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.0.0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v13_history_text'] = 'La herramienta de búsqueda en el histórico de visitas se ha rediseñado. Se ha modernizado tanto estéticamente como técnicamente; también es posible combinar filtros haciendo clic en una foto, un usuario o una dirección IP.';
+$lang['porg_v13_intro1'] = 'Centrado en la experiencia de usuario';
+$lang['porg_v13_history_title'] = 'Histórico de visitas';
+$lang['porg_v13_badge_redesign'] = 'rediseñado';
+$lang['porg_v13_history_image1_label'] = 'Muy solicitado: se puede filtrar por "action=downloaded" y mostrar solamente la lista de descargas.';
+$lang['porg_v13_badge_refresh'] = 'renovado';
+$lang['porg_v13_badge_new'] = 'nuevo';
+$lang['porg_v13_associations_title'] = 'Álbumes enlazados';
+$lang['porg_v13_associations_image1_label'] = 'La interfaz de usuario para elegir los álbumes enlazados a una foto se ha rediseñado completamente con dos objetivos: 1) hacer que el peso de la página (y por tanto su velocidad de presentación) sea independiente del número de álbumes de la galería; 2) poder editar los álbumes asociados con un simple clic.';
+$lang['porg_v13_apppromo_title'] = 'Aplicaciones móviles';
+$lang['porg_v13_apppromo_text'] = 'Como es posible que ya sepas por las noticias, Piwigo tiene una sólida propuesta para smartphone, tanto para iOS (iPhone) como para Android (todos los demás smartphones).';
+$lang['porg_v13_apppromo_image1_label'] = 'Piwigo13 promocionará las aplicaciones móviles. ¡Hay demasiados usuarios que aún no las conocen! Por supuesto, el anuncio se podrá ocultar "para siempre" con un solo clic.';
+$lang['porg_v13_apibrowser_title'] = 'Navegador de API';
+$lang['porg_v13_apibrowser_image1_label'] = 'Gran reforma del explorador de API. Una herramienta para desarrolladores. Integración de un filtro de búsqueda en el árbol de métodos disponibles.';
+$lang['porg_v13_api_s8'] = 'nuevo parámetro de salida <code>is_favorite</code>';
+$lang['porg_v13_albums_title'] = 'Gestor de álbumes';
+$lang['porg_v13_api_s1'] = 'buscar una foto que coincida con el formato indicado';
+$lang['porg_v13_api_s2'] = 'suprimir un formato de foto extendido';
+$lang['porg_v13_api_s3'] = 'establecer una preferencia de usuario';
+$lang['porg_v13_api_s4'] = 'buscar en el historial de visitas';
+$lang['porg_v13_api_s5'] = 'nuevo parámetro de entrada <code>format_of</code>';
+$lang['porg_v13_api_s6'] = 'nuevos parámetros de entrada <code>search</code> y <code>additional_output</code>';
+$lang['porg_v13_api_s7'] = 'nuevo parámetro de entrada <code>position</code>';
+$lang['porg_v13_albums_text'] = 'Hemos fusionado la página de mover álbumes y la página de gestionar álbumes. El nuevo gestor de álbumes muestra una vista en árbol en la que se puede ver la jerarquía completa de álbumes, sin cambiar de página en ningún momento.';
+$lang['porg_v13_albums_image1_label'] = 'Aquí puedes crear sub-álbumes, renombrar un album o mover cualquier album sin cambiar de página. Ahorro de tiempo garantizado.';
+$lang['The menu'] = 'El menú';
+$lang['Technical features'] = 'Características técnicas';
+$lang['New album manager, new visit log, multiple-format improved.'] = 'Nuevo gestor de albums, nuevo registro de visitas, mejorado el formato múltiple.';
+$lang['Interview %s'] = 'Entrevista a %s';
