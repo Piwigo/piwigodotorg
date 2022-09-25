1cc04d9e04f5153f0db6267488de0eec363ad358
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-25 06:20:16 +0200

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/release-13.0.0.lang.php
@@ -0,0 +1,69 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v13_technical_features2'] = 'Technische wijzigingen in Piwigo 13';
+$lang['porg_v13_matthieu_text1'] = 'Gegroet allemaal! Ik ben Matthieu en ik werk al anderhalf jaar bij Piwigo [noot van de redactie: sinds februari 2021] al hoor je nu pas van mij. Ik ben bij het team gekomen tijdens mijn studie aan Epitech. Mijn taak bij Piwigo is het implementeren van nieuwe functies in de software en het moderniseren van de gebruikersinterface, vooral in het administratiepaneel, maar ook om goede vibes te brengen tijdens onze vergaderingen.';
+$lang['porg_v13_remi_text1'] = 'Mijn naam is Rémi en ik werk al meer dan een jaar als ontwikkelaar bij Piwigo [noot van de redactie: sinds februari 2021]. Ik ben bij het team gekomen met als eerste doel om de Android app de iOS app te laten inhalen. Niet gemakkelijk, maar ik ben gemotiveerd!';
+$lang['porg_v13_technical_features14'] = 'Om de aanpassing en eventuele verwerking aan de voorkant te vereenvoudigen, hebben we CSS-klassen toegevoegd aan de &lt;body&gt; tag. En ook een <code>data</code> attribuut.';
+$lang['porg_v13_technical_features7'] = 'PHP 8.1 komt naar veel webhosts en velen van u hebben problemen met Piwigo 12 gemeld. Compatibiliteit met PHP 8.1 vereiste de overstap naar Smarty 4.1 (template engine).';
+$lang['porg_v13_technical_features15'] = 'API wijzigingen';
+$lang['porg_v13_multiformat_title'] = 'Meervoudige formaten';
+$lang['porg_v13_plugin_manager_image1_label'] = 'De plugin installatiepagina is gemoderniseerd, verrijkt met filters en toont meer informatie over de beschikbare plugins. Piwigo 13 introduceert een certificeringsconcept, dat rekening houdt met de leeftijd van elke plugin.';
+$lang['porg_v13_news_image1_label'] = 'Piwigo 13 toont het laatste nieuws (als het minder dan 30 dagen oud is) van piwigo.org.';
+$lang['porg_v13_multiformat_image1_label'] = 'Met Piwigo 13 is het webformulier voor het toevoegen van foto\'s verrijkt met een modus voor het toevoegen van formaten, die in de rechterbovenhoek kan worden geactiveerd.  Gebruik de lokale configuratie-instelling <code>$conf[\'enable_formats\']</code> om deze schakelaar zichtbaar te maken.';
+$lang['porg_v13_technical_features5'] = 'Gelegen tussen configuratievariabelen (gemeenschappelijk voor alle gebruikers) en cookies (specifiek voor een webbrowser), gebruikersvoorkeuren zorgen ervoor dat een gebruikersspecifieke voorkeur blijft bestaan, ongeacht de sessie. Nieuwe functies <code>userprefs_get_param</code>, <code>userprefs_update_param</code>, <code>userprefs_delete_param</code>.';
+$lang['porg_v13_multiformat_text'] = 'Meervoudige formaten kunnen worden geüpload, net als fotos. De functie "meervoudige formaten" is 6 jaar geleden geïntroduceerd in Piwigo 2.8, maar u kon ze alleen toevoegen met de "synchronisatie" methode. Deze methode wordt steeds minder gebruikt, in tegenstelling tot de "web upload" methode. U kunt nu profiteren van het meervoudige formaat met het web upload formulier.<br>
+<br>
+Maar wacht... wat is "meervoudige formaten" precies? Het is de mogelijkheid om een foto uit te breiden met extra formaten. U hebt bijvoorbeeld een JPG als basisfoto en u breidt die uit met een RAW-bestand, een TIFF-bestand, een andere JPG met CMYK-kleurprofiel... wat u maar wilt. De formaten staan naast de foto\'s vermeld als "download opties".';
+$lang['porg_v13_technical_features13'] = 'Klassen en gegevens voor &lt;body&gt;';
+$lang['porg_v13_svg_text'] = 'Ze werden al ondersteund als "elk bestandstype", maar Piwigo 13 kan het SVG-bestand direct in de webpagina weergeven. Als u Piwigo gebruikt om uw logo\'s of iconen te organiseren, zult u deze verbetering zeker waarderen!<br>
+<br>
+We hebben er al aan gewerkt om Modus (standaardthema) en Bootstrap Darkroom (tweede populairste thema) compatibel te maken met deze nieuwe functie.';
+$lang['porg_v13_remi_text2'] = 'Na hard geprobeerd te hebben de oude Android applicatie over te nemen, ben ik overgestapt op het maken van een nieuwe applicatie. Piwigo NG maakt gebruik van nieuwe technologieën [Noot van de redactie: Flutter] om het coderen te vergemakkelijken. Ik ben erg trots om aan de wieg te staan van deze applicatie. Ik ben ook trots om deel uit te maken van zo\'n vriendelijk en gastvrij team als Piwigo.';
+$lang['porg_v13_technical_features1'] = 'Er is meer gedetailleerde documentatie beschikbaar';
+$lang['porg_v13_technical_features6'] = 'Compatibiliteit met PHP 8.1';
+$lang['porg_v13_technical_features3'] = 'Lees het om uw extensies compatibel te maken met Piwigo 13.';
+$lang['porg_v13_svg_title'] = 'Ondersteuning voor SVG-bestanden';
+$lang['porg_v13_tagrename_image1_label'] = 'We hebben de manier om een tag te hernoemen veranderd. Voorheen was het te klein dus hebben we het verplaatst in een pop-in.';
+$lang['porg_v13_tagrename_title'] = 'Tag hernoemen';
+$lang['porg_v13_technical_features12'] = 'meer leren';
+$lang['porg_v13_technical_features4'] = 'Gebruikersvoorkeuren';
+$lang['porg_v13_associations_title'] = 'Gekoppelde albums';
+$lang['porg_v13_badge_redesign'] = 'herontwerp';
+$lang['porg_v13_history_text'] = 'De zoekfunctie in de bezoekgeschiedenis is vernieuwd. Het is zowel esthetisch als technisch gemoderniseerd, maar het is ook mogelijk om filters te combineren door op een foto, een gebruiker of een IP-adres te klikken.';
+$lang['porg_v13_intro2'] = 'Piwigo blijft werken om uw gebruikerservaring zo naadloos mogelijk te maken. Zo vindt u in deze versie 13 ontwerpwijzigingen in de administratie, een nieuwe albummanager met boomstructuur, de mogelijkheid om meerdere formaten direct vanuit uw webbrowser toe te voegen, een completere plugin-installatie... Aan de technische kant krijgt Piwigo compatibiliteit met PHP 8.1 die overal begint te komen. Een goede update voor iedereen!';
+$lang['porg_v13_plugin_manager_title'] = 'Plugin manager';
+$lang['porg_v13_associations_image1_label'] = 'De gebruikersinterface voor het kiezen van de albums die aan een foto gekoppeld zijn, werd volledig herwerkt met 2 doelstellingen: 1) om het gewicht van de pagina (en dus de weergavesnelheid) onafhankelijk te maken van het aantal albums in de galerij, 2) om de gekoppelde albums met een eenvoudige klik te kunnen bewerken.';
+$lang['porg_v13_badge_new'] = 'nieuw';
+$lang['porg_v13_badge_refresh'] = 'verversen';
+$lang['porg_v13_history_image1_label'] = 'Zeer gevraagd, u kunt filteren op "action=downloaded" en alleen de lijst met downloads tonen.';
+$lang['porg_v13_history_title'] = 'Bezoek geschiedenis';
+$lang['porg_v13_intro1'] = 'Gericht op gebruikerservaring';
+$lang['porg_v13_matthieu_text2'] = 'Ik heb nog een jaar voordat ik mijn studie afrond en ik hoop nog meer te leren dankzij Piwigo!';
+$lang['porg_v13_news_title'] = 'Laatste nieuws';
+$lang['porg_v13_plugin_manager_image2_label'] = 'De plugin update pagina is opnieuw ontworpen en biedt meer informatie. Merk op dat dit dezelfde pagina is als de hoofdpagina voor het bijwerken van extensies: plugins, thema\'s, talen.';
+$lang['porg_v13_plugin_manager_text'] = 'Na de grote herbewerking van het eerste tabblad voor het beheer van geïnstalleerde plugins, hebben we de tabbladen voor het bijwerken en installeren van nieuwe plugins overgenomen.';
+$lang['Interview %s'] = '%s\'s interview';
+$lang['New album manager, new visit log, multiple-format improved.'] = 'Nieuwe albummanager, nieuw bezoeklogboek, meerdere formaten verbeterd.';
+$lang['Technical features'] = 'Technische kenmerken';
+$lang['The menu'] = 'Het menu';
+$lang['porg_v13_albums_image1_label'] = 'Daar kunt u subalbums aanmaken, een album hernoemen of een album verplaatsen zonder van pagina te veranderen. Tijdswinst gegarandeerd.';
+$lang['porg_v13_albums_text'] = 'We hebben de pagina om albums te verplaatsen en de pagina om albums te beheren samengevoegd. Zo toont de nieuwe albummanager een boomstructuur waarin u de hele hiërarchie van uw albums kunt zien, zonder dat u op enig moment van pagina hoeft te veranderen.';
+$lang['porg_v13_albums_title'] = 'Albummanager';
+$lang['porg_v13_api_s1'] = 'zoek een foto die voldoet aan het opgegeven formaat';
+$lang['porg_v13_api_s2'] = 'een uitgebreid fotoformaat verwijderen';
+$lang['porg_v13_api_s3'] = 'een gebruikersvoorkeur instellen';
+$lang['porg_v13_api_s4'] = 'zoeken onder geschiedenis van bezoeken';
+$lang['porg_v13_api_s5'] = 'nieuwe invoerparameter <code>format_of</code>';
+$lang['porg_v13_api_s6'] = 'nieuwe invoerparameters <code>search</code> en <code>additional_output</code>';
+$lang['porg_v13_api_s7'] = 'nieuwe invoerparameter <code>position</code>';
+$lang['porg_v13_api_s8'] = 'nieuwe uitvoerparameter <code>is_favorite</code>';
+$lang['porg_v13_apibrowser_image1_label'] = 'Grote opknapbeurt van de API-verkenner. Een hulpmiddel voor ontwikkelaars. Integratie van een zoekfilter op de boom van beschikbare methodes.';
+$lang['porg_v13_apibrowser_title'] = 'API-browser';
+$lang['porg_v13_apppromo_image1_label'] = 'Piwigo 13 gaat mobiele toepassingen promoten. Te veel gebruikers kennen ze nog steeds niet! Natuurlijk is deze banner advertentie "voor altijd" verborgen in 1 klik.';
+$lang['porg_v13_apppromo_text'] = 'Zoals je misschien al in het nieuws hebt gezien, heeft Piwigo nu een solide propositie op smartphone, zowel op iOS (iPhone) als Android (alle andere smartphones).';
+$lang['porg_v13_apppromo_title'] = 'Mobiele toepassingen';
