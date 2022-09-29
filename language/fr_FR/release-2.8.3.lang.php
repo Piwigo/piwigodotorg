07668849bec337af03c40123f071928bf2cf46ba
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-29 13:55:47 +0200

    Update fr_FR, thanks to plg

---
+++ language/fr_FR/release-2.8.3.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_546'] = 'Bogue sur la réinitialisation de mot de passe';
