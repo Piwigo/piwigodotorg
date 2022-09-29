488b134841e8b48b4378d1f4c3db901f2653a3be
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-29 13:55:42 +0200

    Update fr_FR, thanks to plg

--- language/fr_FR/release-2.9.3.lang.php
+++ language/fr_FR/release-2.9.3.lang.php
@@ -5,6 +5,5 @@ $lang['porg_issue_826'] = 'injection SQL sur la page de configuration';
 $lang['porg_issue_825'] = 'injection SQL sur la gestion par lot, en mode unitaire';
 $lang['porg_issue_824'] = 'le script de génération de release plante sur MacOS X';
 $lang['porg_issue_823'] = 'injection SQL sur la gestion des utilisateurs';
-$lang['porg_issue_822'] = 'faille xss+csrf sur les pages de configuration';
+$lang['porg_issue_822'] = 'Faille xss+csrf sur les pages de configuration';
 $lang['porg_issue_735'] = 'possibilité de récupérer une clef d\'auto-identification par l\'API web';
-?>
