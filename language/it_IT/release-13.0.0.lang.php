14e9d9e0819024930f1b9b009c14d53d2aef5ae3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-14 20:49:13 +0200

    Update it_IT, thanks to mpre

---
+++ language/it_IT/release-13.0.0.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v13_albums_title'] = 'Gestore degli album';
+$lang['Technical features'] = 'Caratteristiche tecniche';
+$lang['porg_v13_albums_text'] = 'Abbiamo unito la pagina di spostamento degli album e la pagina di gestione degli album. Così il gestore degli album offre una vista ad albero in cui puoi vedere tutti gli album senza cambiare pagina.';
+$lang['porg_v13_albums_image1_label'] = 'Si può creare sub-album, rinominare o spostare un album senza cambiare pagina. Risparmio di tempo garantito.';
+$lang['The menu'] = 'Nel menù';
+$lang['New album manager, new visit log, multiple-format improved.'] = 'Gestione degli album, storico delle visite, formati multipli...';
