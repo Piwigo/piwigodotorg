ea387af2461ff2607baa218fc87855f262454aac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-23 18:04:34 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = 'Traduction manquante sur la page de mise à jour';
+$lang['porg_issue_1746'] = 'Avertissement : Cannot modify header information - headers already sent in /var/www/piwigo/action.php on line 228';
+$lang['porg_issue_1747'] = '[Page d\'historique] Suppression des balises {icon} dans les détails.';
+$lang['porg_issue_1750'] = 'best_rated/created- conduit à : "Piwigo a rencontré une erreur irrécupérable".';
+$lang['porg_issue_1753'] = 'erreur de syntaxe, \')\' inattendu dans include/ws_functions/pwg.php causé par des virgules finales';
+$lang['porg_issue_1755'] = 'Avertissements de Php8 pour les albums liés';
+$lang['porg_issue_1756'] = 'Problèmes pour les commentaires d\'image avec PHP8';
+$lang['porg_issue_1758'] = '[Vérifier la mise à jour] Aucun lien vers la page de mise à jour après la vérification de la mise à jour';
+$lang['porg_issue_1759'] = '[PHP 8.1] erreur fatale sur la page de configuration';
