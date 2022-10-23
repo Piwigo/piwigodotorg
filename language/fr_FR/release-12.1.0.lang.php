58a0113ac4569f8bdf77a0106e1fca5712b6abb6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-23 19:02:29 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1546'] = 'Mises à jour de Piwigo : le lien "nouvelles fonctionnalités passionnantes" est vide.';
+$lang['porg_issue_1547'] = '[upgrade] desc groups échoue avec MySQL 8';
+$lang['porg_issue_1549'] = 'Aucune mise à niveau d\'extensions alors que le tableau de bord indique le contraire';
+$lang['porg_issue_1550'] = 'La page des plugins ne fonctionne plus';
+$lang['porg_issue_1552'] = '[12.0] Après le téléchargement, l\'album X ne contient désormais aucune photo.';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted ajoute des détails sur la catégorie mise à jour';
+$lang['porg_issue_1554'] = '[12.x] Formulaire de téléchargement, album sélectionné inattendu';
+$lang['porg_issue_1555'] = 'traduction manquante sur la page d\'édition des photos';
