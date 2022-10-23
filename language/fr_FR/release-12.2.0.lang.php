9da4b84d77a6a0ba5409eb4865f2f2f2d256d8ed
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-23 15:42:07 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1557'] = '[12.1] Le gestionnaire d\'utilisateurs se charge indéfiniment avec MySQL 8';
+$lang['porg_issue_1567'] = '[12.x] Les badges des plugins ne sont pas mis à jour avec les entrées de recherche';
+$lang['porg_issue_1569'] = '[12.x] Désactiver pour un groupe sa qualité de groupe par défaut entraîne une boucle infinie';
+$lang['porg_issue_1570'] = 'albums liés et noms multilingues';
+$lang['porg_issue_1573'] = '[12.x] Le bouton d\'édition de la recherche d\'album ne renvoie pas au bon album.';
+$lang['porg_issue_1574'] = '[12.x] Le message de déverrouillage de la galerie est erroné';
+$lang['porg_issue_1587'] = 'le téléchargement avec Piwigo NG génère trop d\'activités';
+$lang['porg_issue_1590'] = '[Page de maintenance]  Les fonctions avancées ne sont pas gérées';
+$lang['porg_issue_1591'] = '[12.x] incompatibilité avec les plugins openstreetmap/videojs';
