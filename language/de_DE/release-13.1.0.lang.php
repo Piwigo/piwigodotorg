3a753b0d6c02888efdf41b1de209f9fa6cb87951
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-23 18:04:30 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1746'] = 'Warnung: Kopfdaten können nicht geändert werden - Kopfdaten wurden bereits gesendet in /var/www/piwigo/action.php on line 228';
+$lang['porg_issue_1759'] = '[PHP 8.1] Fataler Fehler auf der Konfigurationsseite';
+$lang['porg_issue_1755'] = 'PHP8 Warnungen zu verwandten Alben';
+$lang['porg_issue_1756'] = 'Kommentare zu Bildproblemen mit PHP8';
+$lang['porg_issue_1758'] = '[Update prüfen] Kein Link zur Update-Seite nach Update-Prüfung';
+$lang['porg_issue_1745'] = 'Fehlende Übersetzung auf der Update-Seite';
+$lang['porg_issue_1747'] = '[Historische Seite] Entfernen Sie die {icon} Tags in den Details.';
+$lang['porg_issue_1750'] = 'best_rated/created- führt zu \"Piwigo ist auf einen nicht behebbaren Fehler gestoßen\"';
+$lang['porg_issue_1753'] = 'Syntaxfehler, unerwartetes \')\' in include/ws_functions/pwg.php verursacht durch nachgestellte Kommas';
