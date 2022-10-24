3b483b5fcba896a123c98377d65db68e9c0034a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-24 12:58:29 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1759'] = '[PHP 8.1] erro fatal na página de configuração';
+$lang['porg_issue_1758'] = '[Verificar Atualização] Nenhum link para a página de atualização após a verificação de atualização';
+$lang['porg_issue_1756'] = 'Comentários sobre problemas de fotos com php8';
+$lang['porg_issue_1755'] = 'Avisos do Php8 em álbuns relacionados';
+$lang['porg_issue_1753'] = 'erro de sintaxe, \')\' inesperado em include/ws_functions/pwg.php causado por vírgulas à direita';
+$lang['porg_issue_1750'] = 'best_rated/created- leva a \"Piwigo encontrou um erro irrecuperável\"';
+$lang['porg_issue_1747'] = '[Página de histórico] Remova as tags {icon} em detalhes.';
+$lang['porg_issue_1746'] = 'Aviso: Não é possível modificar as informações do cabeçalho - cabeçalhos já enviados em /var/www/piwigo/action.php na linha 228';
+$lang['porg_issue_1745'] = 'Tradução ausente na página de atualização';
