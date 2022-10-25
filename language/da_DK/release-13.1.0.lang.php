1f7410af4e50f56905d978711a7fe3a86b1d4226
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-25 06:39:01 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = 'Manglende oversættelse på opdateringsside';
+$lang['porg_issue_1746'] = ' Advarsel: Cannot modify header information - headers already sent in /var/www/piwigo/action.php on line 228';
+$lang['porg_issue_1747'] = '[Historikside] Fjern {icon}';
+$lang['porg_issue_1750'] = 'best_rated/created- fører til \"Piwigo encountered an unrecoverable error\"';
+$lang['porg_issue_1753'] = 'syntaksfejl, unexpected \')\' in include/ws_functions/pwg.php caused by trailing commas';
+$lang['porg_issue_1755'] = 'Php8-advarsler på relaterede albummer';
+$lang['porg_issue_1756'] = 'Billedkommentarer gav problemet med php8';
+$lang['porg_issue_1758'] = '[Tjek opdatering] Intet link til opdateringsssiden efter opdateringstjek';
+$lang['porg_issue_1759'] = '[PHP 8.1] fatal fejl på konfigurationssiden';
