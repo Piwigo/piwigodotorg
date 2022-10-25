04331b4f9f29240dcf775dbfe07f42e4d9eea71c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-25 12:50:12 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-11.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1257'] = 'admin/batch_manager.php quelques erreurs/ avertissements avec php7.4';
+$lang['porg_issue_1284'] = '\Erreur "Infinite loop ?" (boucle infinie) lors de l\'accès à la page 2 des albums.';
+$lang['porg_issue_1287'] = 'Erreur de syntaxe dans le fichier de langue norvégienne après la mise à niveau vers Piwigo 11';
+$lang['porg_issue_1290'] = '[11.x admin/intro] popen() a été désactivé pour des raisons de sécurité.';
+$lang['porg_issue_1291'] = '[11.x] Erreur d\'analyse : erreur de syntaxe, \'[\'] inattendu';
+$lang['porg_issue_1292'] = 'le mot échappé `groups` ne peut pas être désérialisé';
+$lang['porg_issue_1293'] = ' 11.x Tableau de bord, erreur fatale : Taille de mémoire autorisée';
+$lang['porg_issue_1294'] = '[11.x] code de téléchargement incorrect pour la mise à niveau';
