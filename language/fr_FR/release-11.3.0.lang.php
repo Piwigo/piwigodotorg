47da05f7e3b95faeb46f32e15c59e483dd7fa505
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-25 14:50:46 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-11.3.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1308'] = '[11.2] Le tableau de bord de l\'administrateur est indisponible.';
+$lang['porg_issue_1311'] = 'Impossible de supprimer un tag orphelin';
+$lang['porg_issue_1312'] = 'Page d\'administration des mots-clés/balises dysfonctionnelle dans la version 11.2.0';
+$lang['porg_issue_1313'] = 'les accolades ne sont plus utilisées dans profile.php';
+$lang['porg_issue_1314'] = '[11.x, gestionnaire de balises] message de réussite manquant après la suppression d\'un orphelin';
+$lang['porg_issue_1315'] = '[11.x admin/tags] Le filtre ne fonctionne pas avec les majuscules.';
+$lang['porg_issue_1317'] = '[11.x] suppression du calcul de la taille du répertoire de cache dans le tableau de bord';
+$lang['porg_issue_1319'] = '[11.x] Déplacement des sous-albums en début/fin de liste';
+$lang['porg_issue_1322'] = '[11.x gestionnaire de balises] SyntaxError : Marqueur inattendu =';
