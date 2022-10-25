b349f624e8627c414d8e4e932abb460c0f65fcd0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-25 14:50:04 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-11.5.0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1398'] = '[Gestionnaire de plugins] La taille de l\'icône du menu déroulant est trop grande.';
+$lang['porg_issue_1399'] = '[Recherche d\'albums] Limiter les résultats de la recherche à 100 albums.';
+$lang['porg_issue_1400'] = '[ViewSelector] Le thème sombre (DarkMode) doit être ajouté.';
+$lang['porg_issue_1410'] = '[11.x, gestionnaire d\'utilisateurs] Injection SQL';
+$lang['porg_issue_1412'] = '[Gestionnaire de plugins, 11.x] Le sélecteur d\'affichage change après le premier rechargement de la page.';
+$lang['porg_issue_1080'] = '[2.10] AdminTools ne fonctionne pas sur le formulaire de téléchargement';
+$lang['porg_issue_1241'] = 'L\'identification échoue lorsque la galerie est vide';
+$lang['porg_issue_1306'] = 'Bogue dans la synchronisation des métadonnées du gestionnaire de lots';
+$lang['porg_issue_1323'] = '[11.x admin/album move] rang de l\'album incorrect lors du déplacement';
+$lang['porg_issue_1362'] = 'groupe dupliqué, icône par défaut non affichée';
+$lang['porg_issue_1364'] = 'Problème avec le plugin AdminTools dans _\"Tag Manager\" et \"Move Albums\".';
+$lang['porg_issue_1368'] = 'L\'utilisateur avec le statut "invité" peut toujours se connecter silencieusement.';
+$lang['porg_issue_1369'] = '[Gestionnaire de balises] nombre incorrect d\'images lors du filtrage';
+$lang['porg_issue_1372'] = 'nl_NL (Néerlandais/Nederlands) gestionnaire de groupe brisé';
+$lang['porg_issue_1377'] = 'Associer / Déplacer vers un album dans le gestionnaire de lots';
+$lang['porg_issue_1380'] = 'édition d\'album, délai d\'attente';
+$lang['porg_issue_1382'] = 'solution de repli lorsque le stockage local est plein';
+$lang['porg_issue_1387'] = '[Déplacement d\'album] empêcher l\'ouverture automatique lors du glisser-déposer';
+$lang['porg_issue_1388'] = 'La longueur maximale du nom de l\'album est trop courte dans Album -> Déplacement';
+$lang['porg_issue_1392'] = 'albums liés, problèmes de performance';
+$lang['porg_issue_1393'] = '[Recherche d\'album] La taille du titre n\'est pas bonne lorsque le chemin/titre est trop long.';
+$lang['porg_issue_1394'] = '[Gestionnaire de groupe] Problème de focus lors de l\'ajout d\'un album';
+$lang['porg_issue_1395'] = '[Gestionnaire de groupes] Affichage d\'un message erroné lorsque le champ du nom du groupe est vide.';
+$lang['porg_issue_1396'] = '[Déplacement d\'album] Il faut ajouter des lignes de traduction pour la traduction';
+$lang['porg_issue_1397'] = 'la désactivation d\'un utilisateur doit mettre fin à ses sessions';
