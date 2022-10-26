ede6d8ac2bdd82be98c5128e631fb6b85ff45e13
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-26 06:37:50 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = 'Falta una traducción en la página de actualización';
+$lang['porg_issue_1746'] = 'Advertencia: No se puede modificar la información de cabecera; las cabeceras ya se han enviado, en la línea 228 de /var/www/piwigo/action.php';
+$lang['porg_issue_1747'] = '[Página de historia] Eliminar las etiquetas {icon} en los detalles.';
+$lang['porg_issue_1750'] = 'Mejores puntuaciones/creados- provoca un \"Piwigo ha encontrado un error no recuperable\"';
+$lang['porg_issue_1753'] = 'Error de sintaxis, \')\' inesperado en include/ws_functions/pwg.php causado por comas finales';
+$lang['porg_issue_1755'] = 'Advertencias de PHP8 en álbumes relacionados';
+$lang['porg_issue_1756'] = 'Problemas en los comentarios de las fotos con PHP8';
+$lang['porg_issue_1758'] = '[Comprobar actualizaciones] No aparece enlace a la página de actualización después de la comprobación de actualizaciones';
+$lang['porg_issue_1759'] = '[PHP 8.1] Error fatal en la página de configuración';
