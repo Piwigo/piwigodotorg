68670065ef2d0c5895e05b72a200f971c867cea6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-26 15:21:08 +0200

    Update it_IT, thanks to mpre

---
+++ language/it_IT/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1746'] = 'Avviso : Cannot modify header information - headers already sent in /var/www/piwigo/action.php on line 228';
+$lang['porg_issue_1753'] = 'errore di sintassi, \')\' imprevisto in include/ws_functions/pwg.php causato da virgole finali';
+$lang['porg_issue_1759'] = '[PHP 8.1] Errore fatale nella pagina di configurazione';
+$lang['porg_issue_1756'] = 'Problemi per i commenti alle immagini con PHP8';
+$lang['porg_issue_1755'] = 'Avvisi Php8 per gli album collegati';
+$lang['porg_issue_1750'] = 'best_rated/created- porta a: "Piwigo ha riscontrato un errore irreversibile".';
+$lang['porg_issue_1747'] = '[Pagina cronologia] Cancellazione dei tag {icon} nei dettagli.';
+$lang['porg_issue_1758'] = '[Verifica aggiornamento] Nessun collegamento alla pagina di aggiornamento dopo aver verificato l\'aggiornamento';
+$lang['porg_issue_1745'] = 'Traduzione mancante nella pagina di aggiornamento';
