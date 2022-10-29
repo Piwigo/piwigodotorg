20bd83fdc970afb5715b32d69055e588ff8139bc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-29 12:27:02 +0200

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/release-13.1.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = 'Ontbrekende vertaling op update pagina';
+$lang['porg_issue_1746'] = 'Waarschuwing: Kan headerinformatie niet wijzigen - headers reeds verzonden in /var/www/piwigo/action.php op lijn 228
+';
+$lang['porg_issue_1747'] = '[History page] Verwijder de {icon} Tags in details.';
+$lang['porg_issue_1750'] = 'best_rated/created- leads to\"Piwigo ondervond een onherstelbare fout\"';
+$lang['porg_issue_1753'] = 'syntaxisfout, unexpected\')\' in include/ws_functions/pwg.php veroorzaakt door achterliggende komma\'s';
+$lang['porg_issue_1755'] = 'Php8 Waarschuwingen over gerelateerde albums';
+$lang['porg_issue_1756'] = 'Opmerkingen over problemen met foto\'s in php8';
+$lang['porg_issue_1758'] = '[Check update] Geen link naar de update pagina na de update check';
+$lang['porg_issue_1759'] = '[PHP 8.1] fatale fout op de configuratiepagina';
