39c3398a818e2b22bbe631bc82795706fbadaeff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-31 20:41:22 +0100

    Update it_IT, thanks to kinmen

--- language/it_IT/release-13.0.0.lang.php
+++ language/it_IT/release-13.0.0.lang.php
@@ -44,3 +44,19 @@ $lang['porg_v13_matthieu_text2'] = 'Manca ancora un anno prima che io termini i 
 $lang['porg_v13_intro2'] = 'Piwigo continua a lavorare per rendere la tua esperienza utente più semplice possibile. Perciò in questa versione n. 13 troverai dei cambiamenti di design nella sezione amministrativa, una nuova struttura ad albero nella gestione degli album, la possibilità di aggiungere diversi formati immagine direttamente dal browser web, un installatore di plugin più completo. dal punto di vista tecnico, Piwigo è ora compatibile con PHP 8.1, il quale è ormai in arrivo ovunque. Buon aggiornamento a tutti!';
 $lang['porg_v13_multiformat_title'] = 'Formati multipli';
 $lang['porg_v13_news_title'] = 'Ultime notizie';
+$lang['porg_v13_technical_features4'] = 'Preferenze utente.';
+$lang['porg_v13_technical_features3'] = 'Per favore leggi per poter rendere le tue estensioni compatibili con Piwigo 13.';
+$lang['porg_v13_technical_features15'] = 'Cambiamenti delle API';
+$lang['porg_v13_technical_features13'] = 'Classi e dati per &lt;body&gt;';
+$lang['porg_v13_technical_features12'] = 'Apprendi di più';
+$lang['porg_v13_technical_features1'] = 'Prego notare che è disponibile della documentazione più dettagliata su';
+$lang['porg_v13_tagrename_title'] = 'Rinomina tag';
+$lang['porg_v13_tagrename_image1_label'] = 'Abbiamo cambiato la modalità per rinominare un tag. Prima era troppo piccolo, perciò lo abbiamo spostato all\'interno di ';
+$lang['porg_v13_svg_title'] = 'Supporto ai file SVG';
+$lang['porg_v13_svg_text'] = 'Essi già esistevano come "file di qualunque tipo", ma Piwigo 13 è in grado di mostrare file SVG direttamente all\'interno della pagina web. Se utilizzi Piwigo per organizzare i tuoi loghi ed icone, sicuramente apprezzerai tale miglioramento.
+Abbiamo già lavorato per rendere Modus (il tema predefinito) e Bootstrap (il secondo tema più popolare) compatibili con questa nuova caratteristica.';
+$lang['porg_v13_remi_text2'] = 'Dopo aver tentato invano di riprendere in mano la vecchia applicazione Android, ho deciso di crearne una nuova. Piwigo NG utilizza nuove tecnologie [Nota dell\'editore: Flutter] per facilitare la programmazione. Sono molto orgoglioso di essere il fautore di questa applicazione. Ed anche di essere parte di un team, quello di Piwigo, veramente amichevole ed accogliente.';
+$lang['porg_v13_multiformat_text'] = 'Molteplici formati possono essere caricati proprio come le stesse fotografie. la caratteristica dei "molteplici formati" venne introdotta 6 anni fa in Piwigo 2.8, ma essi si potevano aggiungere soltanto attraverso il sistema della sincronizzazione. quest\'ultimo viene utilizzato sempre meno, al contrario del caricamento via web. Ora puoi caricare i diversi formati di immagine attraverso il caricamento via web.
+Ma un momento... cosa sono esattamente questi "molteplici formati"? È possibile presentare una fotografia in diversi formati immagine. Per esempio puoi caricare la stessa immagine in formato JPEG, RAW, TIFF, ecc, nelle loro diverse varianti. I formati vengono elencati vicino alla stessa fotografia, come differenti opzioni di scaricamento.';
+$lang['porg_v13_remi_text1'] = 'Il mio nome è Rémi ed ho lavorato come sviluppatore per Piwigo per più di un anno [Nota dell\'editore: da febbraio 2021]. Sono entrato nel team con l\'obiettivo principale di rendere l\'applicazione Android in linea con quella di iOS. Non è facile, ma sono motivato.';
+$lang['porg_v13_plugin_manager_title'] = 'Gestione dei plugin';
