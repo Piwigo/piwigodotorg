2b390c82fb4a3158e7effbac5c3ef3b09d0e2a34
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-03 05:58:43 +0100

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/release-13.0.0.lang.php
@@ -0,0 +1,69 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v13_multiformat_text'] = 'Tıpkı fotoğraflar gibi birden fazla format yüklenebilir. "Çoklu format" özelliği 6 yıl önce Piwigo 2.8\'de tanıtıldı, ancak bunları yalnızca "senkronizasyon" yöntemiyle ekleyebilirsiniz. Bu yöntem, "web yükleme" yönteminin aksine giderek daha az kullanılmaktadır. Artık web yükleme formuyla çoklu formattan yararlanabilirsiniz.<br>
+<br>
+Ama bekleyin... "çoklu biçim" tam olarak nedir? Bir fotoğrafı ek formatlarla genişletme imkanı. Örneğin, temel fotoğraf olarak JPG\'niz var ve onu bir RAW dosyası, bir TIFF dosyası, CMYK renk profiline sahip başka bir JPG... istediğiniz herhangi bir şeyle genişletiyorsunuz. Biçimler, fotoğrafların yanında "indirme seçenekleri" olarak listelenir.';
+$lang['porg_v13_multiformat_title'] = 'Çoklu formatlar';
+$lang['porg_v13_news_image1_label'] = 'Piwigo 13, piwigo.org\'dan en son haberleri (30 günden daha eski değilse) görüntüler.';
+$lang['porg_v13_news_title'] = 'Son Haberler';
+$lang['porg_v13_plugin_manager_image1_label'] = 'Eklenti yükleme sayfası modernleştirildi, filtrelerle zenginleştirildi ve mevcut eklentiler hakkında daha fazla bilgi görüntülendi. Piwigo 13, her eklentinin yaşını dikkate alan bir sertifika konsepti sunar.';
+$lang['porg_v13_plugin_manager_image2_label'] = 'Eklenti güncelleme sayfası yeniden tasarlandı ve daha fazla bilgi sunuyor. Bunun ana uzantı güncelleme sayfasıyla aynı sayfa olduğunu unutmayın: eklentiler, temalar, diller.';
+$lang['porg_v13_plugin_manager_text'] = 'Yüklü eklentileri yönetmek için ilk sekmenin büyük bir kısmı yeniden yapılandırıldı sonra, yeni eklentileri güncelleme ve yükleme sekmelerini devraldık.';
+$lang['porg_v13_plugin_manager_title'] = 'Eklenti Yöneticisi';
+$lang['porg_v13_remi_text1'] = 'Benim adım Rémi ve bir yıldan fazla bir süredir Piwigo\'da geliştirici olarak çalışıyorum [Editörün notu: Şubat 2021\'den beri]. Android uygulamasının iOS uygulamasına yetişmesini sağlamak için ilk hedefle ekibe katıldım. Kolay değil, ama motive oldum!';
+$lang['porg_v13_remi_text2'] = 'Eski Android uygulamasını devralmak için çok uğraştıktan sonra yeni bir uygulama oluşturmaya geçtim. Piwigo NG, kodlamayı kolaylaştırmak için yeni teknolojiler [Editörün notu: Flutter] kullanır. Bu uygulamanın kökeninde olmaktan gurur duyuyorum. Piwigo gibi samimi ve konuksever bir ekibin parçası olmaktan da gurur duyuyorum.';
+$lang['porg_v13_svg_text'] = 'Zaten "herhangi bir dosya türü" olarak destekleniyorlardı, ancak Piwigo 13, SVG dosyasını doğrudan web sayfasında görüntüleyebilir. Logolarınızı veya simgelerinizi düzenlemek için Piwigo kullanıyorsanız, bu geliştirmeyi gerçekten takdir edeceksiniz!<br>
+<br>
+Modus (varsayılan tema) ve Bootstrap Darkroom\'u (en popüler ikinci tema) bu yeni özellikle uyumlu hale getirmek için zaten çalıştık.';
+$lang['porg_v13_svg_title'] = 'SVG dosyaları desteği';
+$lang['porg_v13_tagrename_image1_label'] = 'Bir etiketi yeniden adlandırmanın yolunu değiştirdik. Önceden çok küçüktü, bu yüzden bir popin içinde taşıdık.';
+$lang['porg_v13_tagrename_title'] = 'Etiketin yeniden adlandırılması';
+$lang['porg_v13_technical_features1'] = 'hakkında daha ayrıntılı belgelerin mevcut olduğunu lütfen unutmayın.';
+$lang['porg_v13_technical_features13'] = '&lt;body&gt; için sınıflar ve veriler;';
+$lang['porg_v13_technical_features14'] = 'Ön uçta özelleştirmeyi ve olası işlemleri basitleştirmek için &lt;body&gt; etiket. Ve ayrıca bir <code>data</code> özelliği.';
+$lang['porg_v13_technical_features15'] = 'API değişiklikleri';
+$lang['porg_v13_technical_features2'] = 'Piwigo 13\'teki teknik değişiklikler';
+$lang['porg_v13_technical_features3'] = 'Eklentilerinizi Piwigo 13 ile uyumlu hale getirmek için lütfen okuyunuz.';
+$lang['porg_v13_technical_features4'] = 'Kullanıcı tercihleri';
+$lang['porg_v13_technical_features5'] = 'Yapılandırma değişkenleri (tüm kullanıcılar için ortak) ve çerezler (bir web tarayıcısına özel) arasında yer alan kullanıcı tercihleri, oturumdan bağımsız olarak kullanıcıya özel bir tercihin devam etmesine izin verir. Yeni işlevler <code>userprefs_get_param</code>, <code>userprefs_update_param</code>, <code>userprefs_delete_param</code>.';
+$lang['porg_v13_technical_features6'] = 'PHP 8.1 ile uyumluluk';
+$lang['porg_v13_technical_features7'] = 'PHP 8.1 birçok web barındırıcısına geliyor ve birçoğunuz Piwigo 12 ile ilgili sorunlar bildirdiniz. PHP 8.1 ile uyumluluk, Smarty 4.1\'e (şablon motoru) geçişi gerektiriyordu.';
+$lang['porg_v13_technical_features12'] = 'daha fazla bilgi edin';
+$lang['porg_v13_matthieu_text1'] = 'Herkese selamlar! Ben Matthieu ve bir buçuk yıldır Piwigo ile birlikteyim [Editörün notu: Şubat 2021\'den beri] sadece beni şimdi duysanız bile. Takıma Epitech\'teki eğitimim sırasında katıldım. Piwigo\'daki işim, yazılımda yeni özellikler uygulamak ve özellikle yönetim panelinde kullanıcı arayüzünü modernize etmek ve aynı zamanda toplantılarımızda iyi hisler getirmek.';
+$lang['porg_v13_matthieu_text2'] = 'Eğitimimi bitirmeme daha bir yıl var ve Piwigo sayesinde daha fazlasını öğrenmeyi umuyorum!';
+$lang['porg_v13_multiformat_image1_label'] = 'Piwigo 13 ile, fotoğraf eklemek için web formu, sağ üst köşede etkinleştirilebilen bir format ekleme modu ile zenginleştirilmiştir. Bu anahtarı görünür kılmak için <code>$conf[\'enable_formats\']</code> yerel yapılandırma ayarını kullanın.';
+$lang['porg_v13_api_s4'] = 'ziyaret geçmişi arasında arama';
+$lang['porg_v13_api_s5'] = 'yeni giriş parametresi <code>format_of</code>';
+$lang['porg_v13_api_s6'] = 'yeni giriş parametreleri <code>search</code> ve <code>additional_output</code>';
+$lang['porg_v13_api_s7'] = 'yeni giriş parametresi <code>position</code>';
+$lang['porg_v13_api_s8'] = 'yeni çıktı parametresi <code>is_favorite</code>';
+$lang['porg_v13_apibrowser_image1_label'] = 'API gezgininde büyük yenileme. Geliştiriciler için bir araç. Mevcut yöntemler ağacına bir arama filtresi entegrasyonu.';
+$lang['porg_v13_apibrowser_title'] = 'API tarayıcısı';
+$lang['porg_v13_apppromo_image1_label'] = 'Piwigo 13, mobil uygulamaları tanıtacak. Çok fazla kullanıcı hala onları bilmiyor! Elbette bu banner reklam, 1 tıklamayla "sonsuza kadar" gizlenir.';
+$lang['porg_v13_apppromo_text'] = 'Haberlerde görmüş olabileceğiniz gibi, Piwigo\'nun artık hem iOS (iPhone) hem de Android (diğer tüm akıllı telefonlar) için akıllı telefonlarda sağlam bir teklifi var.';
+$lang['porg_v13_apppromo_title'] = 'Mobil uygulamalar';
+$lang['porg_v13_associations_image1_label'] = 'Bir fotoğrafa bağlı albümleri seçmeye yönelik kullanıcı arayüzü 2 hedefle tamamen yeniden tasarlandı: 1) sayfanın ağırlığını (ve dolayısıyla görüntülenme hızını) galerideki albüm sayısından bağımsız hale getirmek, 2) mümkün kılmak Basit bir tıklama ile ilişkili albümleri düzenlemek için.';
+$lang['porg_v13_associations_title'] = 'Bağlı albümler';
+$lang['porg_v13_badge_new'] = 'yeni';
+$lang['porg_v13_badge_redesign'] = 'yeniden tasarla';
+$lang['porg_v13_badge_refresh'] = 'yenile';
+$lang['porg_v13_history_image1_label'] = 'Çok talep edildi, "eylem=indirildi" olarak filtreleyebilir ve yalnızca indirilenlerin listesini gösterebilirsiniz.';
+$lang['porg_v13_history_text'] = 'Ziyaret geçmişindeki arama aracı yenilendi. Hem estetik hem de teknik olarak modernize edilmiştir, ancak bir fotoğrafa, bir kullanıcıya veya bir IP adresine tıklayarak filtreleri birleştirmek de mümkündür.';
+$lang['porg_v13_history_title'] = 'Ziyaret geçmişi';
+$lang['porg_v13_intro1'] = 'Kullanıcı Deneyimi Odaklı';
+$lang['porg_v13_intro2'] = 'Piwigo, kullanıcı deneyiminizi olabildiğince kusursuz hale getirmek için çalışmaya devam ediyor. Bu sürüm 13\'te yönetimde tasarım değişiklikleri, ağaç yapılı yeni bir albüm yöneticisi, doğrudan web tarayıcınızdan birden fazla format ekleme imkanı, daha eksiksiz bir eklenti yükleyici bulacaksınız... Teknik açıdan, Piwigo uyumluluk alır. Her yere gelmeye başlayan PHP 8.1 ile. Herkese iyi güncellemeler!';
+$lang['Interview %s'] = '%s\'in röportajı';
+$lang['New album manager, new visit log, multiple-format improved.'] = 'Yeni albüm yöneticisi, yeni ziyaret günlüğü, çoklu format geliştirildi.';
+$lang['Technical features'] = 'Teknik özellikler';
+$lang['The menu'] = 'Menü';
+$lang['porg_v13_albums_image1_label'] = 'Burada alt albümler oluşturabilir, bir albümü yeniden adlandırabilir veya herhangi bir albümü sayfayı değiştirmeden taşıyabilirsiniz. Zaman tasarrufu garantilidir.';
+$lang['porg_v13_albums_text'] = 'Albümleri taşımak için sayfayı ve albümleri yönetmek için sayfayı birleştirdik. Bu nedenle, yeni albüm yöneticisi, herhangi bir zamanda sayfa değiştirmeden albümlerinizin tüm hiyerarşisini görebileceğiniz bir ağaç görünümü gösterir.';
+$lang['porg_v13_albums_title'] = 'Albüm yöneticisi';
+$lang['porg_v13_api_s1'] = 'sağlanan formatla eşleşen bir fotoğraf arayın';
+$lang['porg_v13_api_s2'] = 'genişletilmiş bir fotoğraf biçimini sil';
+$lang['porg_v13_api_s3'] = 'bir kullanıcı tercihi ayarla';
