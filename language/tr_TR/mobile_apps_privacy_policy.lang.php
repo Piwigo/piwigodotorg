68deefee4ade5e12d4696250cb674a9ad27f0266
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-03 16:50:41 +0100

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Contact us'] = 'Bize Ulaşın';
+$lang['Geo-Location Information'] = 'Coğrafi Konum Bilgileri';
+$lang['How is the data protected?'] = 'Veriler nasıl korunur?';
+$lang['Last updated on %s'] = 'Son güncelleme %s';
+$lang['Mobile Device Access'] = 'Mobil Cihaz Erişimi';
+$lang['Mobile Device Data'] = 'Mobil Cihaz Verileri';
+$lang['Piwigo Gallery Data'] = 'Piwigo Galeri Verileri';
+$lang['Privacy Policy for Mobile Apps'] = 'Mobil Uygulamalar için Gizlilik Politikası';
+$lang['Security of your information'] = 'Bilgilerinizin güvenliği';
+$lang['What data is processed and stored?'] = 'Hangi veriler işlenir ve saklanır?';
+$lang['Why does the mobile app store data?'] = 'Mobil uygulama verileri neden depolar?';
+$lang['or'] = 'veya';
+$lang['porg_privacy_mobile_banner'] = 'Burada hem iOS (yani iPhone, iPod veya iPad) hem de Android (temelde diğer tüm akıllı telefonlar) üzerindeki Piwigo mobil uygulamaları için gizlilik politikası izlenmektedir.';
+$lang['porg_privacy_mobile_contact_1'] = 'Bu Gizlilik Politikası hakkında sorularınız veya yorumlarınız varsa, lütfen bizimle şu adresten iletişime geçin:';
+$lang['porg_privacy_mobile_data_device_1'] = 'Uygulamalar, mobil cihazınızın fotoğraf kitaplığı ve dosya deposu, Android Hesap Yöneticisi, Apple Keychain, yabancı ve bulut tabanlı depolama klasörleri, coğrafi konum belirleme ve diğer özellikler dahil olmak üzere mobil cihazınızdan belirli özelliklere erişim veya izin talep edebilir.';
+$lang['porg_privacy_mobile_data_device_2'] = 'Erişim veya izinlerimizi değiştirmek isterseniz, bunu cihazınızın ayarlarından yapabilirsiniz, ancak cep telefonunuzun medya dosyalarını Piwigo galerisine yüklemek veya Piwigo medya dosyalarını cep telefonunuzda saklamak için bazı erişimlerin gerekli olduğunu unutmayın.';
+$lang['porg_privacy_mobile_data_device_3'] = 'Destek ekibiyle iletişim kurarken, mobil cihaz kimliğiniz, modeliniz ve üreticiniz, işletim sisteminizin sürümü, e-posta, ülke, konum ve sağlamayı seçtiğiniz diğer veriler gibi cihaz bilgileri Uygulamaların geliştirme ekibine iletilebilir. .';
+$lang['porg_privacy_mobile_data_device_4'] = 'Kullanıcı tarafından geliştiricilerle açıkça paylaşılan verilerin yanı sıra hiçbir içerik tarafımızca saklanmayacak, iletilmeyecek veya işlenmeyecektir.';
+$lang['porg_privacy_mobile_data_geo_1'] = 'Konum tabanlı hizmetleri sağlamak için ya sürekli olarak ya da Uygulamaları kullanırken mobil cihazınızdan konum tabanlı bilgilere erişim veya izin talep edebilir ve bunları takip edebiliriz.';
+$lang['porg_privacy_mobile_data_geo_2'] = 'Erişimimizi veya izinlerimizi değiştirmek isterseniz, bunu cihazınızın ayarlarından yapabilirsiniz.';
