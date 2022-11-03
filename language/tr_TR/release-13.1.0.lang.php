ccc5ff6c393a873089785168144c31d0f9e816a6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-03 06:00:06 +0100

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = 'Güncelleme sayfasında eksik çeviri';
+$lang['porg_issue_1746'] = 'Uyarı: Başlık bilgileri değiştirilemiyor - zaten başlıklar 228. satırda /var/www/piwigo/action.php içinde gönderildi.';
+$lang['porg_issue_1747'] = '[Geçmiş sayfası] Ayrıntılardaki {icon} Etiketlerini kaldırın.';
+$lang['porg_issue_1750'] = 'best_rated/created- \"Piwigo kurtarılamaz bir hatayla karşılaştı\"ya yol açıyor';
+$lang['porg_issue_1753'] = 'sözdizimi hatası, beklenmeyen \')\' include/ws_functions/pwg.php dosyasında sondaki virgüllerin neden olduğu';
+$lang['porg_issue_1755'] = 'İlgili albümlerde Php8 Uyarıları';
+$lang['porg_issue_1756'] = 'Resim yorumları hakkında php8 sorunları';
+$lang['porg_issue_1758'] = '[Güncellemeyi kontrol et] Güncelleme kontrolünden sonra güncelleme sayfasına bağlantı yok';
+$lang['porg_issue_1759'] = '[PHP 8.1] yapılandırma sayfasında önemli hata';
