c7712dd439332278725ebc32ce50218c4e8627a8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-03 05:58:38 +0100

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/automatic_update.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_button2'] = 'Piwigo 2.1.x veya 2.2.x çalıştırıyorsanız';
+$lang['porg_guides_automatic_update_button3'] = 'Piwigo 2.0.x çalıştırıyorsanız';
+$lang['porg_guides_automatic_update_button4'] = 'Piwigo 1.7.x çalıştırıyorsanız';
+$lang['porg_guides_automatic_update_button5'] = '1.7.x\'ten daha eski bir sürüm kullanıyorsanız';
+$lang['porg_guides_automatic_update_caption1'] = 'Evet, sadece 2 tıklama';
+$lang['porg_guides_automatic_update_info1'] = 'Her zaman veritabanınızın ve dosyalarınızın yedeğini alın. En iyisi, bunları düzenli olarak otomatik olarak yapmaktır. Güncelleme sırasında kötü bir şey olursa, bir yedeği geri yükleyebilirsiniz.';
+$lang['porg_guides_automatic_update_paragraph1'] = 'Piwigo\'nuzu en son sürüme yükseltmek için önerilen yöntem budur. O kadar basit ki, yükseltmelerinizi unutmak veya ertelemek için artık bahaneniz yok. En son ve en güvenli Piwigo sürümünü çalıştırmak gerçekten 1 dakikadan az sürer.';
+$lang['porg_guides_automatic_update_paragraph10'] = 'Piwigo Auto Upgrade eklentisini indirin ve “eklentiler” dizininize açın';
+$lang['porg_guides_automatic_update_paragraph11'] = 'Yönetim > Özeller > Eklentiler > Yönetim\'e gidin ve “Piwigo AutoUpgrade” eklentisinin önündeki “yükle” eylemine ve ardından “Etkinleştir” eylemine tıklayın.';
+$lang['porg_guides_automatic_update_paragraph12'] = 'Yönetim > Özeller > Eklentiler > Piwigo Otomatik Yükseltme\'ye gidin ve kılavuzu izleyin, aynı "dalın" son sürümüne veya en son Piwigo sürümüne yükseltmeyi seçebilirsiniz.';
+$lang['porg_guides_automatic_update_paragraph13'] = 'Daha eski bir sürümünüz varsa, lütfen bunun yerine Manuel Yükseltmeyi uygulayın.';
+$lang['porg_guides_automatic_update_paragraph3'] = 'Yönetim > Araçlar > Güncellemeler\'e gidin ve kılavuzu izleyin';
+$lang['porg_guides_automatic_update_paragraph4'] = 'Yönetim > Eklentiler > Yönet > Mevcut diğer eklentiler\'e gidin ve Piwigo Otomatik Yükseltme\'yi yükleyin. Eklenti zaten kuruluysa, Yönetim > Eklentiler > Yönet > Güncellemeleri kontrol et ekranında en son revizyonu çalıştırıp çalıştırmadığınızı kontrol edin.';
+$lang['porg_guides_automatic_update_paragraph5'] = 'Bu yeni eklentiyi kurmak ve etkinleştirmek için Yönetim > Eklentiler > Yönet > Eklentiler listesine gidin.';
+$lang['porg_guides_automatic_update_paragraph6'] = 'Yönetim > Eklentiler > Piwigo Otomatik Yükseltme\'ye gidin ve kılavuzu izleyin.';
+$lang['porg_guides_automatic_update_paragraph7'] = 'Yönetim > Özel Ürünler > Eklentiler > Diğer Eklentiler ve Piwigo Otomatik Yükseltme\'nin otomatik kurulumunu gerçekleştirin. Eklenti zaten kuruluysa, en son revizyonu çalıştırıp çalıştırmadığınızı kontrol edin.';
+$lang['porg_guides_automatic_update_paragraph8'] = 'Bu yeni eklentiyi kurmak ve etkinleştirmek için Yönetim > Özeller > Eklentiler\'e gidin';
+$lang['porg_guides_automatic_update_paragraph9'] = 'Yönetim > Özeller > Eklentiler > Piwigo Otomatik Yükseltme\'ye gidin ve kılavuzu izleyin, aynı "dalın" son sürümüne veya en son Piwigo sürümüne yükseltmeyi seçebilirsiniz.';
+$lang['porg_guides_automatic_update_step1'] = 'Tek bir adım';
+$lang['porg_guides_automatic_update_time_1'] = '30 saniye';
