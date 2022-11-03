44a8363f84bc09815c110e68953691cb484f9a48
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-03 05:59:41 +0100

    Update tr_TR, thanks to kubikanber

---
+++ language/tr_TR/release-11.0.0.lang.php
@@ -0,0 +1,86 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v11_technical_features15'] = 'piwigo.org yalnızca HTTPS ile çağrılır';
+$lang['porg_v11_technical_features16'] = 'Piwigo\'nuz, uzantıların listesini almak veya mevcut yeni bir sürümü kontrol etmek için piwigo.org\'u arar (HTTP istekleri). Artık tüm bu çağrılar HTTPS üzerinden gerçekleştirilecek.';
+$lang['porg_v11_technical_features17'] = 'Yeni tetikleyici';
+$lang['porg_v11_technical_features18'] = 'Dosya yüklerken merkezi işlevin sonuna 1 tetikleyici ekledik';
+$lang['porg_v11_technical_features19'] = 'API değişiklikleri';
+$lang['porg_v11_technical_features2'] = 'Piwigo 11\'deki teknik değişiklikler';
+$lang['porg_v11_technical_features20'] = 'piwigo.org yalnızca HTTPS ile çağrılır';
+$lang['porg_v11_technical_features3'] = 'Eklentilerinizi Piwigo 11 ile uyumlu hale getirmek için lütfen okuyunuz.';
+$lang['porg_v11_technical_features4'] = 'Hızlı arama ve alt albümler';
+$lang['porg_v11_technical_features5'] = 'Hızlı arama (menüden arama) alt albüm fotoğraflarını arama sonuçlarına dahil edebilir. "Ayı" araması, "yabani ayılar / boz ayı" ve "vahşi ayılar / dev panda" albümlerindeki fotoğrafları gösterecektir. Bu özelliğin yerel yapılandırmayla açıkça etkinleştirilmesi gerekiyor';
+$lang['porg_v11_technical_features6'] = 'Eklenti "Ayarları Var"';
+$lang['porg_v11_technical_features7'] = 'Piwigo 11\'de eklentiler artık yönetim menüsünde listelenmiyor. Bunun yerine eklenti kutusunda bir "Ayarlar" düğmesi var. Düğme eski tarafından etkinleştirilir';
+$lang['porg_v11_technical_features9'] = 'tetikleyici, ancak tarafından değiştirilebilir';
+$lang['porg_v11_theme_manager_image1_label'] = 'Yeni eklenti yöneticisiyle aynı tasarım kuralları uygulanır.';
+$lang['porg_v11_theme_manager_title'] = 'Tema yöneticisi';
+$lang['porg_v11_user_manager_image1_label'] = 'Kullanıcı yöneticisi, Piwigo 11\'de küçük bir iyileştirme alır: yalnızca geçerli sayfayı veya tüm kullanıcı grubunu seçebilirsiniz. Şimdi "tüm seti seç" filtreyi hesaba katar. Bu şekilde, aynı eylemi belirli bir grubun tüm kullanıcılarına kolayca uygulayabilirsiniz.';
+$lang['porg_v11_user_manager_title'] = 'Kullanıcı yöneticisi, daha akıllı seçim';
+$lang['porg_v11_zacharie_text1'] = 'Benim adım Zacharie ve Nantes\'teki (Fransa) bilgisayar bilimi çalışmalarımın bir parçası olarak Nisan 2020\'de Piwigo macerasına katıldım. Yönetim alanının görsel arayüzlerini canlandırmak da dahil olmak üzere Piwigo\'nun 2.11 sürümünü yayınlamak için ekiple birlikte çalıştım.';
+$lang['porg_v11_zacharie_text2'] = 'Pierrick\'in tavsiyesini ve Hannah\'nın örneklerini kullanarak yeni Grup Yöneticisini, yeni Etiket yöneticisini uyguladım ve diğer birçok sayfayı yeniledim. Bunu yapmak için JavaScript ve CSS becerilerimi kullandım, ayrıca daha iyi bir kullanıcı deneyimi (UX) için birçok sayfaya AJAX ekledim. Yaratıcı bir tavırla, yeni sayfalarda bıraktığım birkaç animasyonu fark edebilirsiniz.';
+$lang['porg_v11_photo_edition_image1_label'] = 'Piwigo 11\'de fotoğraf düzenleme, her fotoğrafa ve verisine daha fazla önem vermek için yenilenmiş bir tasarıma sahiptir.';
+$lang['porg_v11_photo_edition_title'] = 'Fotoğraf düzenleme';
+$lang['porg_v11_plugin_manager_image1_label'] = 'Piwigo 11\'deki eklenti yöneticisi: yenilenmiş bir tasarım, menü bağlantısı eklenti kutusunun kendisine taşındı ve yeni bir filtreleme özelliği geldi.';
+$lang['porg_v11_plugin_manager_title'] = 'Eklenti Yöneticisi';
+$lang['porg_v11_related_albums_image1_label'] = 'Piwigo 11, "ilgili albümleri" gösterebilir ve albümleri birleştirebilir. Evet, "ilgili etiketlere" çok benziyor. En büyük fark, etiketler düzken albümlerin hiyerarşiye göre düzenlenebilmesidir.';
+$lang['porg_v11_related_albums_image2_label'] = 'Piwigo, aradığınız fotoğraf grubunu tam olarak bulmak için albümleri birleştirmenize izin verir.';
+$lang['porg_v11_related_albums_title'] = 'İlgili albümler';
+$lang['porg_v11_requirements_image1_label'] = 'Gelecekteki güncellemelerden önce Piwigo 11+ gerekli PHP sürümünü kontrol edecektir. Piwigo\'yu güncellemeye başladığınız durumdan kaçınmak ve PHP\'niz çok eski olduğu için takılıp kaldığınızı anlamak istiyoruz. Ayrıca yakın gelecekte PHP sürümünü ciddi anlamda artırabilmek istiyoruz.';
+$lang['porg_v11_requirements_title'] = 'Güncellemeden önce kontrol edilen gereksinimler';
+$lang['porg_v11_tag_manager_image1_label'] = 'Burada, yeni bir etiket ekleyebileceğiniz ve mevcut herhangi bir etikete bir işlem uygulayabileceğiniz varsayılan modumuz var...';
+$lang['porg_v11_tag_manager_image2_label'] = '... daha sonra aynı anda birkaç etiket seçmek ve birleştirme veya silme gerçekleştirmek için seçim modunu etkinleştirebilirsiniz.';
+$lang['porg_v11_tag_manager_image3_label'] = 'Etiketlerinizi bulmanıza yardımcı olmak için bir filtre sağlanmıştır. Birden fazla olduğunda bu yararlıdır.';
+$lang['porg_v11_tag_manager_text'] = 'Bu sürüm 11 ile etiket yöneticisi tamamen yeniden yazılıyor. Aynı özelliklere sahip ancak yeni bir kullanıcı deneyimi ve hızı var.';
+$lang['porg_v11_tag_manager_title'] = 'Etiket yöneticisi';
+$lang['porg_v11_technical_features1'] = 'hakkında daha ayrıntılı belgelerin mevcut olduğunu lütfen unutmayın.';
+$lang['porg_v11_technical_features10'] = 'Ayarlar Var';
+$lang['porg_v11_technical_features11'] = 'main.inc.php dosya başlığında';
+$lang['porg_v11_technical_features12'] = 'daha fazla bilgi edin';
+$lang['porg_v11_technical_features13'] = 'Daha az dil etkinleştirildi';
+$lang['porg_v11_technical_features14'] = 'Mevcut 70+ dil yerine yalnızca kurulum dili varsayılan olarak etkinleştirilir.';
+$lang['porg_v11_group_manager_text'] = 'Grup yöneticisinin tamamen yeniden yazılması: daha temiz, daha iyi kullanıcı deneyimi ve kullanıcıları ilişkilendirme yeteneği.';
+$lang['porg_v11_group_manager_title'] = 'Grup yöneticisi';
+$lang['porg_v11_history_stats_image1_label'] = 'Piwigo 11\'deki geçmiş istatistikleri artık daha güzel bir görünüm için animasyonlu eğrilere sahip.';
+$lang['porg_v11_history_stats_image2_label'] = 'Geçmiş istatistikleri sayfası, yılları veya ayları karşılaştırmak için bir karşılaştırma modu geldi.';
+$lang['porg_v11_history_stats_title'] = 'Geçmiş istatistikleri';
+$lang['porg_v11_info1_text'] = 'Güle güle sürüm 2.11, hoş geldiniz sürüm 11! 2008\'de PhpWebGallery\'nin adını Piwigo olarak değiştirmeye karar verdik ve sürümü 1.7\'den 2.0\'a çıkardık. Bu, 12 yıldır 2. versiyonda olduğumuz anlamına geliyor. Ömür boyu diğer birçok projeden çok daha uzun. 2.10 veya 2.11 sürümü, teknik arka uç çok fazla değişmemiş olsa bile, Piwigo kullanıcı arayüzünün yeniden tasarımına yaptığımız büyük çalışmayı yansıtmaz. Bu sürümlerle birçok iyileştirme getirdiğimizi açıkça belirtmek istiyoruz. Belki de bazı topluluk üyelerinin önerdiği gibi sürüm 2.10, sürüm 3 olmalıydı. Bu yüzden sürüm oluşturma sistemimizi gözden geçirmenin zamanı geldi. Sürüm 2.10\'dan 11.0\'a geçelim!';
+$lang['porg_v11_info1_title'] = 'Sürüm 2.10\'dan 11\'e atlıyor';
+$lang['porg_v11_info2_text'] = 'Güncelleme sayfasında önerilen veritabanı dökümü özelliği kaldırıldı. Artık PHP\'nin son sürümleriyle çalışmıyordu. Dosyaların ve veritabanının her zaman yedeğini almak için daha basit bir mesajla değiştirildi.';
+$lang['porg_v11_info2_title'] = 'Veritabanı dökümü kaldırıldı';
+$lang['porg_v11_info3_text'] = 'Artık kullanıcı adınız yerine e-posta adresinizle de giriş yapabilirsiniz. Önce Piwigo, sağlanan kimlik bilgilerinin bir kullanıcı adıyla eşleşip eşleşmediğini kontrol eder, değilse e-posta adreslerini kontrol ederiz.';
+$lang['porg_v11_info3_title'] = 'E-posta adresinizle giriş yapın';
+$lang['porg_v11_intro1'] = 'Bir ton iyileştirmeye hazır mısınız?';
+$lang['porg_v11_intro2'] = 'Geliştirmelerimiz ve yeniden tasarımlarımız çok fazla olduğundan, sürüm oluşturma sistemimizi değiştirmeye karar verdik! 2.10 sürümünün birkaç yönetim sayfası için yeni bir tasarım sunmasından sonra, bu sürüm 11 diğer birçok sayfada yeni bir tasarım uygular. Piwigo 11 ayrıca albüm arama aracı, etkinlik tablosu veya ilgili albümler gibi hem yönetici hem de galeri tarafı için yeni özellikler getiriyor.';
+$lang['porg_v11_intro3'] = 'Hoş sürprizlere hazır olun!';
+$lang['porg_v11_mobile_image1_label'] = 'Şimdilik bu özellik yalnızca iOS cihazlarda, yani iPhone veya iPad\'de çalışıyor.';
+$lang['porg_v11_mobile_text'] = 'Bir mobil cihazdan açıyorsanız, Piwigo, Piwigo uygulamasını açmaya/yüklemeye teşvik edecektir. Yönetimde veya galeride etkinleştirilebilir veya tamamen devre dışı bırakılabilir.';
+$lang['porg_v11_mobile_title'] = 'Mobil uygulama tanıtımı';
+$lang['porg_v11_move_albums_image1_label'] = 'Aynı albümdeki alt albümlerin sırasını değiştirebilir veya bir albümü başka bir albüme taşıyabilirsiniz.';
+$lang['porg_v11_move_albums_text'] = 'Piwigo 11 ile albümleri taşımak için tamamen yeni bir kullanıcı arayüzü elde ediyoruz. Bu, tüm albüm hiyerarşinizi görebileceğiniz tek sayfadır.';
+$lang['porg_v11_move_albums_title'] = 'Albümleri taşı';
+$lang['porg_v11_activity_chart_image1_label'] = 'Etkinlik çizelgesi, genel istatistiklerin ve hızlı bağlantıların yanında, gösterge tablosunda görüntülenir.';
+$lang['porg_v11_activity_chart_image2_label'] = 'Ne kadar çok aktiviteniz varsa, çember o kadar büyük olur. Piwigo, her etkinlik günü için ayrıntılarınızı verir';
+$lang['porg_v11_activity_chart_text'] = 'Piwigo 2.10, albüm baskısı veya grup oluşturma gibi birçok yönetim etkinliğini kaydetmeye başladı... Piwigo 11 artık tüm bu etkinliklerin bir grafiğini gösterebiliyor.';
+$lang['porg_v11_activity_chart_title'] = 'Etkinlik çizelgesi';
+$lang['porg_v11_album_edition_image1_label'] = 'Piwigo 11 artık albümleri düzenlemek için daha modern ve renkli bir sayfaya sahip.';
+$lang['porg_v11_album_edition_title'] = 'Albüm düzenleme';
+$lang['porg_v11_album_manager_image1_label'] = 'Albüm yöneticisi, daha önce tanıtılan grup yöneticisiyle aynı tasarımı izler.';
+$lang['porg_v11_album_manager_title'] = 'Albüm yöneticisi';
+$lang['porg_v11_album_search_image1_label'] = 'Bu sürümle Piwigo 11, tüm albümleriniz arasında arama yapmak için yeni bir araca sahiptir. Birçok yönetici için <b>çok</b> faydalı olacağını tahmin ediyorum.';
+$lang['porg_v11_album_search_title'] = 'Albüm arama aracı';
+$lang['porg_v11_badge_new'] = 'yeni';
+$lang['porg_v11_badge_redesign'] = 'yeniden tasarla';
+$lang['porg_v11_badge_refresh'] = 'yenile';
+$lang['porg_v11_batch_manager_image1_label'] = 'Piwigo 11\'in toplu iş yöneticisi, diğer yeni nesil sayfaların yönergelerini izleyerek yenilenmiş bir tasarıma kavuşuyor.';
+$lang['porg_v11_batch_manager_image2_label'] = 'Toplu İş Yöneticisinin "filtre" bölümü, filtre eklemek için yeni bir tasarım aldı.';
+$lang['porg_v11_batch_manager_title'] = 'Toplu iş yöneticisi';
+$lang['porg_v11_custom_alerts_image1_label'] = 'Tarayıcı uyarıları, özel uyarılarla değiştirilir. Ne yaptığınızı daha iyi anlamak için daha açık etiketler.';
+$lang['porg_v11_custom_alerts_title'] = 'Özel uyarılar';
+$lang['porg_v11_group_manager_image1_label'] = 'Yeni grup yöneticisinin genel görünümü. Önceki tüm özellikleri koruduk, ancak kullanıcı deneyimini çok daha güzel hale getirdik.';
+$lang['porg_v11_group_manager_image2_label'] = 'Yeni özellik: Artık kullanıcıları, kullanıcı yöneticisini kullanmadan doğrudan ilişkilendirebilirsiniz';
