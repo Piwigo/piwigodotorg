39104d022649f2744bfdc8b2f90aad8685bdf726
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-17 12:11:04 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1783'] = '[Historienseite] Möglichkeit zum Filtern eines Bildes auf der Seite "Bild bearbeiten" hinzufügen';
+$lang['porg_issue_1784'] = '[History page] Möglichkeit hinzufügen, nach einem Benutzer auf der Benutzerseite zu filtern';
+$lang['porg_issue_1787'] = '[Linked Albums popin] Die Bildlaufleiste ist in Chrome immer sichtbar';
+$lang['porg_issue_1792'] = 'Php8 \'get_magic_quotes_gpc\' wird entfernt und bricht die Benutzereingabe ab';
+$lang['porg_issue_1793'] = '[Album Manager] Die Beschriftung der Sortierüberprüfung ist nicht korrekt';
+$lang['porg_issue_1794'] = '[Album Manager] Drücken der Eingabetaste führt zur Erstellung mehrerer Alben';
+$lang['porg_issue_1795'] = '[Foto bearbeiten] Der Titel wird nicht übersetzt';
+$lang['porg_issue_1796'] = '[Album bearbeiten] Titel für Fotoreihenfolge und Benachrichtigungen brechen die Seite';
+$lang['porg_issue_1767'] = '[Benutzererstellung] Popin gibt nach der Erstellung keine Rückmeldung.';
+$lang['porg_issue_1768'] = 'Beschriftung der Schaltfläche zum Anwenden der Reihenfolge auf Alben im Stammverzeichnis ändern';
+$lang['porg_issue_1770'] = 'einige RAW-Dateien werden fälschlicherweise als TIFF-Dateien betrachtet';
+$lang['porg_issue_1772'] = '[13.x, Upload-Formular] Beförderung zur Seite für mobile Anwendungen immer auf Englisch';
+$lang['porg_issue_1774'] = 'Höhe des Popins von verlinkten Alben ändert sich immer';
+$lang['porg_issue_1775'] = '[Album Manager] Kein Tooltip auf der Schaltfläche für die Sortierreihenfolge';
+$lang['porg_issue_1780'] = '[13.x] Langsamer Album-Manager mit vielen Alben';
+$lang['porg_issue_1781'] = '[Album-Manager] Schlechte UX für das Album-Erstellungs-Popin';
+$lang['porg_issue_1782'] = '[Verlaufsseite] Kann nicht nach einer bestimmten IP filtern';
