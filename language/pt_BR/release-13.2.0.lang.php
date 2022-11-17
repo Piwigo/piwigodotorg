589ea0cde987abde6f13c3bec06ee9031af0e13e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-17 13:04:41 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1767'] = '[Criação de usuário] Popin não dá nenhuma resposta após a criação.';
+$lang['porg_issue_1768'] = 'Altere o rótulo do botão para aplicar a ordem aos álbuns na raiz';
+$lang['porg_issue_1770'] = 'alguns arquivos RAW são considerados incorretamente como arquivos TIFF';
+$lang['porg_issue_1772'] = '[13.x, formulário de upload] promoção para página de aplicativos móveis sempre em inglês';
+$lang['porg_issue_1774'] = 'A altura do pop-up dos álbuns vinculados sempre muda';
+$lang['porg_issue_1775'] = '[Gerenciador de Album] Nenhuma dica de ferramenta no botão de ordem de classificação';
+$lang['porg_issue_1780'] = '[13.x] gerenciador de álbuns lento com muitos álbuns';
+$lang['porg_issue_1781'] = '[Gerente do álbum] UX ruim para o pop-in de criação do álbum';
+$lang['porg_issue_1782'] = '[Página de histórico] Não é possível filtrar em um IP específico';
+$lang['porg_issue_1783'] = '[Página de histórico] Adiciona a possibilidade de filtrar uma imagem na página de edição de imagens';
+$lang['porg_issue_1784'] = '[Página de histórico] Adicionada a possibilidade de filtrar um usuário na página do usuário';
+$lang['porg_issue_1787'] = '[Álbuns vinculados aparecendo] A barra de rolagem está sempre visível no Chrome';
+$lang['porg_issue_1792'] = 'Php8 \'get_magic_quotes_gpc\' é removido e quebra o escape de entrada do usuário';
+$lang['porg_issue_1793'] = '[Gerenciador de Album] A validação de classificação btn label não está correta';
+$lang['porg_issue_1794'] = '[Gerenciador de Album] Pressionar enter causa a criação de vários álbuns';
+$lang['porg_issue_1795'] = '[Editar foto] O título não está traduzido';
+$lang['porg_issue_1796'] = '[Edição do álbum] Títulos para pedidos de fotos e notificações quebram a página';
