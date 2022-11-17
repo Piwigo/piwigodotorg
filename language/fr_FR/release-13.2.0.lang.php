40d8162b46c00e68b70d69689a64b2bfd55bca93
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-17 13:29:33 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1767'] = '[Création d\'utilisateur] Le Popin ne répond pas après la création.';
+$lang['porg_issue_1768'] = 'Changer le libellé du bouton pour appliquer l\'ordre aux albums à la racine';
+$lang['porg_issue_1770'] = 'Certains fichiers RAW sont considérés à tort comme des fichiers TIFF.';
+$lang['porg_issue_1772'] = '[13.x, formulaire de téléchargement] La promotion de la page des applications mobiles est toujours en anglais';
+$lang['porg_issue_1774'] = 'Le popin des albums liés changent toujours de hauteur.';
+$lang['porg_issue_1775'] = '[Gestionnaire d\'albums] Pas d\'infobulle sur le bouton d\'ordre de tri.';
+$lang['porg_issue_1780'] = '[13.x] Gestionnaire d\'albums ralenti avec de nombreux albums';
+$lang['porg_issue_1781'] = '[Gestionnaire d\'album] Mauvaise ergonomie de la fenêtre de création d\'album';
+$lang['porg_issue_1782'] = '[Page d\'historique] Impossible de filtrer sur une IP spécifique';
+$lang['porg_issue_1783'] = '[Page d\'historique] Ajout de la possibilité de filtrer sur une image à partir de la page d\'édition de l\'image.';
+$lang['porg_issue_1784'] = '[Page historique] Ajout de la possibilité de filtrer sur un utilisateur à partir de la page de l\'utilisateur.';
+$lang['porg_issue_1787'] = '[Popin des albums liés] La barre de défilement est toujours visible sur Chrome';
+$lang['porg_issue_1792'] = 'Php8 \'get_magic_quotes_gpc\' est supprimé et empêche la saisie de l\'échappement par l\'utilisateur.';
+$lang['porg_issue_1793'] = '[Gestionnaire d\'albums] Le libellé du bouton de validation du tri n\'est pas correct.';
+$lang['porg_issue_1794'] = '[Gestionnaire d\'albums] Appuyer sur la touche Entrée entraîne la création de plusieurs albums.';
+$lang['porg_issue_1795'] = '[Modification de la photo] Le titre n\'est pas traduit.';
+$lang['porg_issue_1796'] = '[Album Edit] Les titres pour l\'ordre des photos et les messages cassent la page.';
