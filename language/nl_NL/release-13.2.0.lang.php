01f795bd416c9aa596537d4dc0ac14e92fef7d19
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-17 13:04:45 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1796'] = '[Album Edit] Titels voor fotovolgorde en meldingen breekt de pagina';
+$lang['porg_issue_1795'] = '[Foto bewerken] De titel is niet vertaald';
+$lang['porg_issue_1794'] = '[Album Manager] Druk op enter om meerdere albums te maken';
+$lang['porg_issue_1793'] = '[Album Manager] Sorteervalidatie btn-label is niet correct';
+$lang['porg_issue_1792'] = 'Php8 \'get_magic_quotes_gpc\' is verwijderd en verbreekt de gebruikersinvoer escape';
+$lang['porg_issue_1787'] = '[Gekoppelde albums pop-in] De schuifbalk is altijd zichtbaar in Chrome';
+$lang['porg_issue_1784'] = '[Geschiedenispagina] Mogelijkheid toevoegen om op een gebruiker te filteren vanaf de gebruikerspagina';
+$lang['porg_issue_1783'] = '[Geschiedenispagina] Mogelijkheid toevoegen om op een afbeelding te filteren vanaf de afbeeldingspagina bewerken';
+$lang['porg_issue_1782'] = '[Geschiedenispagina] Kan niet filteren op een specifiek IP';
+$lang['porg_issue_1781'] = '[Albummanager] Slechte UX voor pop-up voor het maken van albums';
+$lang['porg_issue_1780'] = '[13.x] langzame albummanager met veel albums';
+$lang['porg_issue_1775'] = '[Album Manager] Geen tooltip op de sorteervolgorde-knop';
+$lang['porg_issue_1774'] = 'Gelinkte albums popin hoogte verandert altijd';
+$lang['porg_issue_1772'] = '[13.x, upload formulier] promotie naar pagina met mobiele applicaties altijd in het Engels';
+$lang['porg_issue_1770'] = 'sommige RAW-bestanden worden ten onrechte als TIFF-bestanden beschouwd';
+$lang['porg_issue_1768'] = 'Wijzig het label van de knop om de volgorde van albums in de root toe te passen';
+$lang['porg_issue_1767'] = '[Gebruikersaanmaak] Popin geeft geen antwoord na aanmaak';
