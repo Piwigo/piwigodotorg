6d7d5fa0e9376f713776477be91da82d29ddb6df
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-19 21:22:07 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1781'] = '[Gestor de álbumes] Interfaz de usuario incorrecta para el diálogo emergente de creación de álbumes';
+$lang['porg_issue_1784'] = '[Página de historial] Añadir la posibilidad de filtrar sobre un usuario desde la página de usuarios';
+$lang['porg_issue_1792'] = '\'get_magic_quotes_gpc\' se ha eliminado de PHP8 y eso rompe el escapado de las entradas de usuario';
+$lang['porg_issue_1767'] = '[Creación de usuario] El diálogo emergente no da ninguna respuesta después de la creación.';
+$lang['porg_issue_1768'] = 'Cambiada la etiqueta del botón para aplicar el orden de los álbumes en la raíz';
+$lang['porg_issue_1770'] = 'algunos archivos RAW considerados incorrectamente como TIFF';
+$lang['porg_issue_1772'] = '[13.x, formulario de carga] la promoción para la página de aplicaciones móviles siempre en inglés';
+$lang['porg_issue_1774'] = 'La altura de los diálogos emergentes de los álbumes enlazados siempre cambia';
+$lang['porg_issue_1775'] = '[Gestor de álbumes] No hay ayuda flotante en el botón del orden de clasificación';
+$lang['porg_issue_1780'] = '[13.x] el gestor de álbumes es lento cuando hay muchos álbumes';
+$lang['porg_issue_1782'] = '[Página de historial] No se puede filtrar por una dirección IP específica';
+$lang['porg_issue_1783'] = '[Página de historial] Añadir la posibilidad de filtrar sobre una imagen desde la página de edición de imágenes';
+$lang['porg_issue_1787'] = '[Diálogo emergente de álbumes enlazados] La barra de desplazamiento siempre es visible en Chrome';
+$lang['porg_issue_1793'] = '[Gestor de álbumes] La etiqueta del botón de validación de la clasificación no es correcta';
+$lang['porg_issue_1794'] = '[Gestor de álbumes] Pulsar Intro causa la creación de varios álbumes';
+$lang['porg_issue_1795'] = '[Editar foto] El título no está traducido';
+$lang['porg_issue_1796'] = '[Editar álbum] Los títulos para el orden de las fotos y las notifiaciones rompen la página';
