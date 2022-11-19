6c11ad2b66d64c962a6ee7efef1cde983f255218
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-19 18:04:29 +0100

    Update en_UK, thanks to Kaare

--- language/en_UK/release-13.2.0.lang.php
+++ language/en_UK/release-13.2.0.lang.php
@@ -1,19 +1,18 @@
 <?php
 $lang['porg_issue_1796'] = '[Album Edit] Titles for photo order and notifications breaks the page';
 $lang['porg_issue_1795'] = '[Edit photo] The title isn\'t translated';
-$lang['porg_issue_1794'] = '[Album Manager] Press enter causes multiple album creation';
-$lang['porg_issue_1793'] = '[Album Manager] Sort validation btn label isn\'t correct';
-$lang['porg_issue_1792'] = 'Php8 \'get_magic_quotes_gpc\' is removed and break user input escape';
-$lang['porg_issue_1787'] = '[Linked Albums popin] The scroll bar is always visible on Chrome';
+$lang['porg_issue_1794'] = '[Album Manager] Press enter causes multiple album creations';
+$lang['porg_issue_1793'] = '[Album Manager] Sort validation button label isn\'t correct';
+$lang['porg_issue_1792'] = 'PHP8 \'get_magic_quotes_gpc\' is removed and break user input escape';
+$lang['porg_issue_1787'] = '[Linked Albums popin] The scroll bar is always visible in Chrome';
 $lang['porg_issue_1784'] = '[History page] Add possibility to filter on a user from the user page';
-$lang['porg_issue_1783'] = '[History page] Add posibility to filter on an image from the edit image page';
+$lang['porg_issue_1783'] = '[History page] Add possibility to filter on an image from the edit image page';
 $lang['porg_issue_1782'] = '[History page] Can\'t filter on a specific IP';
 $lang['porg_issue_1781'] = '[Album manager] Bad UX for the album creation popin';
 $lang['porg_issue_1780'] = '[13.x] slow album manager with many albums';
 $lang['porg_issue_1775'] = '[Album Manager] No tooltip on the sort order button';
-$lang['porg_issue_1774'] = 'Linked albums popin height alway changes';
+$lang['porg_issue_1774'] = 'Linked albums popin height always changes';
 $lang['porg_issue_1772'] = '[13.x, upload form] promotion to mobile applications page always in English';
-$lang['porg_issue_1770'] = 'some RAW files incrrectly considered as TIFF files';
+$lang['porg_issue_1770'] = 'some RAW files incorrectly considered as TIFF files';
 $lang['porg_issue_1768'] = 'Change label of button to apply order to albums at root';
 $lang['porg_issue_1767'] = '[User Creation] Popin doesn\'t give any reponse after creation.';
-?>
