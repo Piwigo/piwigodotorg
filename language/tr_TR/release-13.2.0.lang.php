3c444d45b0e18b4311a185b2f8272bed89b3678b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-19 18:05:37 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1796'] = '[Albüm Düzenleme] Fotoğraf sıralamassı için başlıklar ve bildirimler sayfayı keser';
+$lang['porg_issue_1795'] = '[Fotoğrafı düzenle] Başlık çevrilmedi';
+$lang['porg_issue_1794'] = '[Albüm Yöneticisi] Enter tuşuna basmak birden fazla albüm oluşturulmasına neden oluyor';
+$lang['porg_issue_1793'] = '[Albüm Yöneticisi] Sıralama doğrulama düğme etiketi doğru değil';
+$lang['porg_issue_1792'] = 'Php8 \'get_magic_quotes_gpc\' kaldırıldı ve kullanıcı girişi kaçışını durdurdu';
+$lang['porg_issue_1787'] = '[Bağlantılı Albümler açılır penceresi] Kaydırma çubuğu Chrome\'da her zaman görünür durumda';
+$lang['porg_issue_1784'] = '[Geçmiş sayfası] Kullanıcı sayfasından bir kullanıcıyı filtreleme imkanı eklendi';
+$lang['porg_issue_1783'] = '[Geçmiş sayfası] Görüntü düzenleme sayfasından bir görüntüye filtre uygulama olanağı ekleyin';
+$lang['porg_issue_1782'] = '[Geçmiş sayfası] Belirli bir IP üzerinde filtreleme yapılamıyor';
+$lang['porg_issue_1781'] = '[Albüm yöneticisi] Albüm oluşturma açılır penceresi için hatalı kullanıcı deneyimi';
+$lang['porg_issue_1774'] = 'Bağlantılı albümlerin açılır pencere yüksekliği her zaman değişir';
+$lang['porg_issue_1767'] = '[Kullanıcı Oluşturma] Oluşturma sonrası açılır pencere herhangi bir yanıt vermiyor.';
+$lang['porg_issue_1780'] = '[13.x] birçok albüm olduğunda yavaş albüm yöneticisi';
+$lang['porg_issue_1775'] = '[Albüm Yöneticisi] Sıralama düzeni düğmesinde araç ipucu yok';
+$lang['porg_issue_1772'] = '[13.x, yükleme formu] mobil uygulamalar sayfasına tanıtım her zaman İngilizcedir';
+$lang['porg_issue_1768'] = 'Kök dizindeki albümlere sıralama uygulamak için düğmenin etiketini değiştirin';
+$lang['porg_issue_1770'] = 'bazı RAW dosyaları yanlışlıkla TIFF dosyası olarak kabul edildi';
