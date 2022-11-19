bc4217d86e6678172b821d012bb5963cb12f7fc5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-19 18:04:26 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1768'] = 'Ændrer label på knap til udførelse af albumrækkefølge i roden';
+$lang['porg_issue_1772'] = '[13.x, uploadformular] reklame for mobil-app er altid på engelsk';
+$lang['porg_issue_1780'] = '[13.x] langsom albumhåndtering ved mange albummer';
+$lang['porg_issue_1781'] = '[Albumhåndtering] Dårlig UX i albumoprettelsespopin';
+$lang['porg_issue_1787'] = '[Linket album-popin] Scrollbar vises altid i Chrome';
+$lang['porg_issue_1792'] = 'PHP8 \'get_magic_quotes_gpc\' er fjernet og medfører at escaping af brugerinddata ikke virker';
+$lang['porg_issue_1793'] = '[Albumhåndtering] Teksten på knappen til sorteringsvalidering er ikke korrekt';
+$lang['porg_issue_1794'] = '[Albumhåndtering] Tryk på enter-tasten medfører oprettelse af adskillige albummer';
+$lang['porg_issue_1795'] = '[Fotoredigering] Titlen er ikke oversat';
+$lang['porg_issue_1796'] = '[Albumredigering] Titler på fotorækkefølge og notifikationer gør siden defekt';
+$lang['porg_issue_1782'] = '[Historikside] Kan ikke filtrere på specifik IP';
+$lang['porg_issue_1783'] = '[Historikside] Tilføjer mulighed for at filtrere på et billede fra billedredigeringsside';
+$lang['porg_issue_1784'] = '[Historikside] Tilføjer mulighed for at filtrere fra brugerside';
+$lang['porg_issue_1767'] = '[Brugeroprettelse] Popin giver intet svar efter oprettelse.';
+$lang['porg_issue_1770'] = 'Nogle RAW-filer blev fejlagtigt opfattet som TIFF-filer';
+$lang['porg_issue_1774'] = 'Linkede albummer-popins højde ændrer sig altid';
+$lang['porg_issue_1775'] = '[Albumhåndtering] Intet tooltip på knap til sorteringsrækkefølge';
