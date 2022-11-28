d4b6de820303cd9b65b470d1659d9716574996f2
plegall <plg@piwigo.org>
2022-11-28 15:43:16 +0100

    missing language file for release 13.3.0

---
+++ language/en_UK/release-13.3.0.lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['porg_issue_1808'] = '[13.x, album manage] deleting album always delete orphans';
+$lang['porg_issue_1807'] = '[Plugin Manager] Latency problems when there is a lot of plugins';
+$lang['porg_issue_1806'] = '[Album Manager] UI Upgrade for the album manager';
+$lang['porg_issue_1803'] = '[Album Manager] Click on action icons is hard with tiptip';
+$lang['porg_issue_1802'] = 'Escapes in comments, users and albums are done twice';
+$lang['porg_issue_1801'] = 'calculating number of orphans is slow';
+$lang['porg_issue_1800'] = 'broken page (javascript issue) on user manager and photo edit in Català';
+$lang['porg_issue_1790'] = '[Album manager] Performance issues in front and backend for big album databases';
+?>
