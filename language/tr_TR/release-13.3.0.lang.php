addd8bf76565faf59f15a25be3e90fd735cdc346
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 06:41:28 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1790'] = '[Albüm Yöneticisi] Büyük albüm veritabanları için ön ve arka uçta performans sorunları';
+$lang['porg_issue_1808'] = '[13.x, Albüm Yönetimi] albümün silinmesi daima yetimleri de siliyor';
+$lang['porg_issue_1807'] = '[Eklenti Yöneticisi] Bir çok eklenti olduğunda gecikme problemi';
+$lang['porg_issue_1806'] = '[Albüm Yöneticisi] albüm yöneticisinde Kullanıcı arabirimi yükseltmesi';
+$lang['porg_issue_1803'] = '[Albüm Yöneticisi] Tiptip ile eylem simgelerine tıklamak zor';
+$lang['porg_issue_1802'] = 'Yorumlarda, kullanıcılarda ve albümlerde kaçışlar iki kez yapılır';
+$lang['porg_issue_1801'] = 'yetim sayısının hesaplanması yavaş';
+$lang['porg_issue_1800'] = 'Català kullanıcı yönetimi ve fotoğraf düzenleyicisinde kırık sayfa (javascript sorunu)';
