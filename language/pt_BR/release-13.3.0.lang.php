94d34eaebb455038f3e000e22cb1bc77ef58f416
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 12:34:12 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1790'] = '[Gerenciador de álbuns] Problemas de desempenho na frente e no back-end para grandes bancos de dados de álbuns';
+$lang['porg_issue_1800'] = 'página quebrada (problema de javascript) no gerenciador de usuários e edição de fotos em Català';
+$lang['porg_issue_1801'] = 'calcular número de órfãos é lento';
+$lang['porg_issue_1802'] = 'Escapes em comentários, usuários e álbuns são feitos duas vezes';
+$lang['porg_issue_1803'] = '[Gerenciador de álbuns] Clique em ícones de ação é difícil com tiptip';
+$lang['porg_issue_1806'] = '[Gerenciador de álbuns] Atualização da interface do usuário para o gerenciador de álbuns';
+$lang['porg_issue_1807'] = '[Gerenciador de Plugin] Problemas de latência quando há muitos plugins';
+$lang['porg_issue_1808'] = '[13.x, gerenciamento de álbum] excluir álbum sempre excluir órfãos';
