c7aeb2d4451026ef2d017173623c7b7e1c812fa5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 06:41:39 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1790'] = '[Gestionnaire d\'albums] Problèmes de performance en front et backend pour les grandes bases de données d\'albums';
+$lang['porg_issue_1800'] = 'page cassée (problème de javascript) dans le gestionnaire d\'utilisateurs et l\'édition de photos en catalan';
+$lang['porg_issue_1801'] = 'le calcul du nombre d\'orphelines est lent';
+$lang['porg_issue_1802'] = 'Les échappements dans les commentaires, les utilisateurs et les albums sont effectués deux fois.';
+$lang['porg_issue_1803'] = '[Gestionnaire d\'album] Il est difficile de cliquer sur les icônes d\'action avec l\'info-bulle.';
+$lang['porg_issue_1806'] = '[Gestionnaire d\'albums] Mise à jour de l\'interface utilisateur du gestionnaire d\'albums';
+$lang['porg_issue_1807'] = '[Plugin Manager] Problèmes de latence lorsqu\'il y a beaucoup de plugins';
+$lang['porg_issue_1808'] = '[13.x, gestion des albums] la suppression d\'un album supprime toujours les orphelines';
