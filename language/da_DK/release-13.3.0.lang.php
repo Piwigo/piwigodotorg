c9f5c6615618bdc7d220d88cef7c82a0af36fe36
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 20:57:32 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1790'] = '[Albumhåndtering] Problemer med ydeevnen i front- og backend ved store albumdatabaser';
+$lang['porg_issue_1800'] = 'Defekt side (JavaScript-problem) ved brugerhåndtering og fotoredigering på catalansk';
+$lang['porg_issue_1801'] = 'Beregning af antal forældreløse billeder er langsom';
+$lang['porg_issue_1802'] = 'Escapetegn i kommentarer, brugere og albummer gentages';
+$lang['porg_issue_1803'] = '[Albumhåndtering] Klik på en handling er svært med tooltip';
+$lang['porg_issue_1806'] = '[Albumhåndtering] UI-opgradering af albumhåndteringen';
+$lang['porg_issue_1807'] = '[Plugin-håndtering] Svartidsproblemer når der er mange plugins';
+$lang['porg_issue_1808'] = '[13.x, albumhåndtering] Sletning af album sletter altid forældreløse billeder';
