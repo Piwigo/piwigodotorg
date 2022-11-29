f78d3cfccd34eee151533177a6e50b8eb1dc91a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 06:41:20 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1800'] = 'Pàgina que no funcionava (problema de Javascript) en el gestor d\'usuaris y l\'editor de fotos en català';
+$lang['porg_issue_1801'] = 'El càlcul del nombre d\'orfes és lent';
+$lang['porg_issue_1802'] = 'Els caràcters d\'escapament en comentaris, usuaris y àlbums es posen dues vegades';
+$lang['porg_issue_1803'] = '[Gestor d\'àlbums] Pitjar les icones d\'acció és difícil amb tiptip';
+$lang['porg_issue_1806'] = '[Gestor d\'àlbums] Actualització de la interfície d\'usuari del gestor d\'àlbums';
+$lang['porg_issue_1807'] = '[Gestor de connectors] Problemes de latència quan hi ha molts connectors';
+$lang['porg_issue_1808'] = '[13.x, Gestor d\'àlbums] Suprimir un àlbum sempre suprimeix els orfes';
+$lang['porg_issue_1790'] = '[Gestor d\'àlbums] Problemes de rendimient en el <i>front-end</i> (la interfície d\'usuari) i en el <i>back-end</i> (accés a les dades) per a bases de dades d\'àlbums grans';
