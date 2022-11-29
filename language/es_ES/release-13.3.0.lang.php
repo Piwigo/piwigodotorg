85deaa3895854d64386a948b1972590cc8b4716d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 06:41:24 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1800'] = 'Página que no funcionaba (problema de Javascript) en el gestor de usuarios y el editor de fotos en catalán';
+$lang['porg_issue_1801'] = 'El cálculo del número de huérfanos es lento';
+$lang['porg_issue_1802'] = 'Los caracteres de escape en comentarios, usuarios y álbumes se ponen dos veces';
+$lang['porg_issue_1803'] = '[Gestor de álbumes] Pulsar en los iconos de acción es difícil con tiptip';
+$lang['porg_issue_1806'] = '[Gestor de álbumes] Actualización de la interfaz de usuario del gestor de álbumes';
+$lang['porg_issue_1807'] = '[Gestor de plugins] Problemas de latencia cuando hay muchos plugins';
+$lang['porg_issue_1808'] = '[13.x, Gestor de álbumes] Suprimir un álbum siempre suprime los huérfanos';
+$lang['porg_issue_1790'] = '[Gestor de álbumes] Problemas de rendimiento en el <i>front-end</i> (la interfaz de usuario) y en el <i>back-end</i> (acceso a los datos) para bases de datos de álbumes grandes';
