b771d59738a2abf24f36a7362d99e5a1cfd3d0e8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 20:57:36 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1808'] = '[13.x, album beheer] album verwijderen verwijdert altijd wezen';
+$lang['porg_issue_1807'] = '[Plugin Manager] Latentieproblemen wanneer er veel plug-ins zijn';
+$lang['porg_issue_1806'] = '[Albumbeheer] UI-upgrade voor de albumbeheerder';
+$lang['porg_issue_1803'] = '[Albumbeheer] Klikken op actiepictogrammen is moeilijk met tiptip';
+$lang['porg_issue_1802'] = 'Escapes in reacties, gebruikers en albums worden twee keer gedaan';
+$lang['porg_issue_1801'] = 'berekening aantal wezen verloopt traag';
+$lang['porg_issue_1800'] = 'gebroken pagina (javascript probleem) over user manager en foto bewerken in Català';
+$lang['porg_issue_1790'] = '[Albumbeheerder] Prestatieproblemen aan de voor- en achterkant voor grote albumdatabases';
