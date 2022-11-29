939c795d71faa307653f280d6f38fad63b1ed93f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-29 06:41:35 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1790'] = '[Album Manager] Leistungsprobleme im Front- und Backend bei großen Album-Datenbanken';
+$lang['porg_issue_1800'] = 'fehlerhafte Seite (Javascript-Problem) im Benutzermanager und bei der Fotobearbeitung in Català';
+$lang['porg_issue_1801'] = 'Berechnung der Anzahl der Waisenkinder ist langsam';
+$lang['porg_issue_1802'] = 'Eskapaden in Kommentaren, Benutzern und Alben werden doppelt ausgeführt';
+$lang['porg_issue_1803'] = '[Album Manager] Klick auf Aktionssymbole ist schwer mit Tiptip';
+$lang['porg_issue_1806'] = '[Album Manager] UI Upgrade für den Album Manager';
+$lang['porg_issue_1807'] = '[Plugin Manager] Latenzprobleme bei vielen Plugins';
+$lang['porg_issue_1808'] = '[13.x, Albumverwaltung] Löschen von Alben löscht immer Waisen';
