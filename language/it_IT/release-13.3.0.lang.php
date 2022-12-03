b773abb7bdf1dcf63d85b397cc34c499ce659e01
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-03 18:51:24 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1807'] = '[Gestione plugin]  Problemi di latenza in presenza di molti plugin.';
+$lang['porg_issue_1806'] = '[Gestione album] Aggiornamento interfaccia utente per la gestione album.';
+$lang['porg_issue_1803'] = '[Gestione album] Click sull\'icona delle azioni è difficoltoso con tiptip.';
+$lang['porg_issue_1800'] = 'pagina interrotta (problema javascript) della gestione utenti e della modifica fotografia in catalano.';
+$lang['porg_issue_1801'] = 'il calcolo degli elementi orfani è lento.';
+$lang['porg_issue_1802'] = 'Escape eseguite due volte in commenti, utenti ed album.';
+$lang['porg_issue_1790'] = '[Gestione album] Problemi di performance di front e backend per database di grandi album.';
+$lang['porg_issue_1808'] = '[13.x, gestione album] cancellazione album causa sempre la cancellazione degli elementi orfani.';
