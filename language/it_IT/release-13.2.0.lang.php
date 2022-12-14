860978d1ced7f6f94201e47155a0001b1a76c655
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-14 07:04:04 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1793'] = '[Gestione album] L\'etichetta del pulsante di validazione dell\'ordine è errata.';
+$lang['porg_issue_1794'] = '[Gestione album] La pressione del tasto Enter causa la creazione di molteplici album.';
+$lang['porg_issue_1795'] = '[Modifica fotografia] Il titolo non è tradotto.';
+$lang['porg_issue_1796'] = '[Modifica album] I titoli per l\'ordine delle foto e le notifiche interrompono la pagina.';
+$lang['porg_issue_1792'] = '\'get_magic_quotes_gpc\' è stato rimosso da PHP8 e ciò interrompe l\'escape dell\'input dell\'utente.';
+$lang['porg_issue_1787'] = '[Pop-in degli album collegati] La barra di scorrimento è sempre visibile in Chrome.';
+$lang['porg_issue_1784'] = '[Pagina dello storico] Aggiungere la possibilità di filtrare un utente dalla pagina utenti.';
+$lang['porg_issue_1783'] = '[Pagina dello storico] Aggiungere la possibilità di filtrare un\'immagine dalla pagina di modifica dell\'immagine.';
+$lang['porg_issue_1782'] = '[Pagina dello storico] Impossibile filtrare uno specifico indirizzo IP.';
+$lang['porg_issue_1767'] = '[Creazione utenti] In seguito alla creazione, il pop-in non risponde più.';
+$lang['porg_issue_1768'] = 'Cambiare l\'etichetta del pulsante per applicare l\'ordinamento degli album alla radice.';
+$lang['porg_issue_1770'] = 'Alcuni file RAW vengono erroneamente considerati come file TIFF.';
+$lang['porg_issue_1772'] = '[13.x, form di caricamento] Promozione della pagine delle applicazioni per mobile sempre in lingua inglese.';
+$lang['porg_issue_1774'] = 'L\'altezza del pop-in degli album collegati cambia sempre.';
+$lang['porg_issue_1775'] = '[Gestione Album] Nessun suggerimento riguardo al pulsante per l\'ordinamento.';
+$lang['porg_issue_1780'] = '[13.x] Con molti album, la loro gestione risulta lenta.';
+$lang['porg_issue_1781'] = '[Gestione Album] Cattiva esperienza utente per il pop-in utilizzato per la creazione di album.';
