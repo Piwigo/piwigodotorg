b010c65f7fd4f82ceb6e1666c6c27c2b9b707fb6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-17 15:49:07 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1824'] = '[user manager] Filter min_register/max_register werden auch bei geschlossenen Filtern angewendet';
+$lang['porg_issue_1825'] = '[User Manager] Selektionsmodus setzen Selektionsproblem';
+$lang['porg_issue_1826'] = '[User Manager] Fokus auf die Eingabe des Benutzernamens beim Anlegen eines Benutzers';
+$lang['porg_issue_1827'] = 'Bilder verschwunden/gelöscht nach Upgrade von 13.1 -> 13.3';
