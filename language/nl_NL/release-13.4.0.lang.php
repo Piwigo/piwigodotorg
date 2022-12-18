859ed1f395bae6684f243996ad2fa4febdbd8566
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-18 15:34:27 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.4.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1827'] = 'Foto\'s verdwenen /verwijderd na upgrade van 13.1 -> 13.3';
+$lang['porg_issue_1826'] = '[Gebruikersbeheer] Focus op de gebruikersnaaminvoer bij het maken van een gebruiker';
+$lang['porg_issue_1825'] = '[Gebruikersbeheer] Probleem met selectiemodussetselectie';
+$lang['porg_issue_1824'] = '
+
+Referentietaal wijzigen in 
+Nederlands [UK]
+[gebruikersbeheer] filters min_register/max_register toegepast, zelfs wanneer filters gesloten zijn';
