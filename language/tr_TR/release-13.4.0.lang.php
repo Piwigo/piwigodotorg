9752000bcb2c952af3ab32961a8314e318950caa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-18 15:34:22 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1824'] = '[Kullanıcı Yöneticisi] filtreler kapalıyken bile min_register/max_register filtreleri uygulanıyor';
+$lang['porg_issue_1825'] = '[Kullanıcı Yöneticisi] Seçim modu ayarında seçme sorunu';
+$lang['porg_issue_1826'] = '[Kullanıcı Yöneticisi] Kullanıcı oluştururken kullanıcı adı girişine odaklama';
+$lang['porg_issue_1827'] = '13.1 -> 13.3 Güncellemesi sonrasında fotoğraflar kayboldu/silindi';
