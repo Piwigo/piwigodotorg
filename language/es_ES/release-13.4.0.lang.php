126192b77770f3b5267a8ef9ecf7e940b92d4ea6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-19 09:32:32 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1824'] = '[Gestor de Usuarios] Los filtros min_register/max_register se aplican incluso cuando se han cerrado los filtros';
+$lang['porg_issue_1825'] = '[Gestor de Usuarios] Problema de selección con el modo de selección activado';
+$lang['porg_issue_1826'] = '[Gestor de Usuarios] Poner el foco en el campo de entrada de nombre de usuario al crear el usuario';
+$lang['porg_issue_1827'] = 'Fotos desaparecidas/suprimidas después de la actualización de 13.1 a 13.3';
