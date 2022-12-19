c034c4252921f6fc58a64042ea2d4f166cc08d6b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-19 09:32:27 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1825'] = '[Gestor d\'Usuaris] Problema de selecció amb el mode de selecció activat';
+$lang['porg_issue_1826'] = '[Gestor d\'Usuaris] Posar el focus al camp d\'entrada de nom d\'usuari en crear l\'usuari';
+$lang['porg_issue_1827'] = 'Fotos desaparegudes/suprimides després de l\'actualització de 13.1 a 13.3';
+$lang['porg_issue_1824'] = '[Gestor d\'Usuaris] Els filtres min_register/max_register s\'apliquen fins i tot quan s\'han tancat els filtres';
