b6ad4c9ae8294112ec5d45611d95d414539c64cc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-20 08:11:10 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1824'] = '[gerenciador de usuários] filtros min_register/max_register aplicados mesmo quando os filtros estão fechados';
+$lang['porg_issue_1825'] = '[Gerenciador de usuários] Problema de seleção do conjunto de modo de seleção';
+$lang['porg_issue_1826'] = '[Gerenciador de usuários] Concentre-se na entrada do nome de usuário ao criar o usuário';
+$lang['porg_issue_1827'] = 'Imagens desaparecidas/excluídas após a atualização de 13.1 -> 13.3';
