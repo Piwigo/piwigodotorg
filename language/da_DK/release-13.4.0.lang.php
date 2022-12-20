f85d22038e9fc2ca54833c6aeb813a7715c516c0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-20 12:25:38 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1824'] = '[Brugerhåndtering] Filtrene min_register og max_register benyttes selv når filtre er lukkede';
+$lang['porg_issue_1825'] = '[Brugerhåndtering] Problem selection set i udvælgelsestilstand';
+$lang['porg_issue_1826'] = '[Brugerhåndtering] Fokus på brugernavnsfelt ved oprettelse af bruger';
+$lang['porg_issue_1827'] = 'Billeder forsvundet/slettet efter opgradering fra 13.1 -> 13.3';
