08d9fa9abc31826e9c94f67bc6440bf93baac9a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-02 10:57:03 +0100

    Update it_IT, thanks to mpre

---
+++ language/it_IT/manual_update.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_step1_list2'] = 'Sul computer, estrarre la cartella "piwigo"';
+$lang['porg_guides_manual_update_step10_paragraph2'] = 'Non è necessario rimuovere nulla dopo un upgrade. "upgrade.php" non deve essere rimosso. Ricorda che il messaggio “No upgrade required” e il sign-on processo proteggono la galleria.';
+$lang['porg_guides_manual_update_step1_list1'] = '<a href="%s">Scaricare l\'ultimo archivio delle versioni di Piwigo</a>';
+$lang['porg_guides_manual_update_info2'] = 'Il nostro consiglio: deselezionate "Inserti estesi" o riducete "Lunghezza massima delle query create".';
+$lang['porg_guides_manual_update_paragraph1'] = 'Questa procedura è compatibile con qualsiasi versione maggiore o uguale alla 1.4. Per una versione precedente, chiedere aiuto al forum.';
+$lang['porg_guides_manual_update_step1'] = 'Fase 1 - Preparazione del package';
+$lang['porg_guides_manual_update_step10'] = 'Fase 10 - Pulizia post upgrade';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Nulla';
+$lang['porg_guides_manual_update_info1'] = 'Controllare gli attuali <a href="%s">Requisiti</a> di Piwigo. In particolare, se la configurazione di Php / MySQL NON è sufficiente, non andare oltre perché il processo di aggiornamento fallirà.';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Con Piwigo 2.3 o versione precedente: Configurazione > Generale > Blocca galleria. Per Piwigo 2.4: Tools > Manutenzione.';
