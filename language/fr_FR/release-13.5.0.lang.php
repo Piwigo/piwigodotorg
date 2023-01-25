f778aa75c37055507ef5c96447ab2dd526eb61c0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 12:27:15 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-13.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1832'] = '[Gestionnaire des utilisateurs] Enregistrement de la pagination dans les préférences de l\'utilisateur';
+$lang['porg_issue_1834'] = '[Gestionnaire des utilisateurs] Ajout d\'informations sur les filtres';
+$lang['porg_issue_1835'] = 'Vulnérabilité XSS stockée dans l\'agent utilisateur - Piwigo-13.4.0
+';
+$lang['porg_issue_1836'] = 'n\'exécute plus les instructions obsolete.list sur les mises à jour mineures';
+$lang['porg_issue_1837'] = '[Gestionnaire d\'albums] Ajout d\'une info-bulle personnalisée pour les badges.';
+$lang['porg_issue_1838'] = '[activités] masquage des connexions de la liste des activités de l\'utilisateur';
+$lang['porg_issue_1839'] = '[Activités] Noms d\'utilisateur manquants lors de l\'enregistrement d\'un utilisateur';
+$lang['porg_issue_1841'] = 'enregistrement des activités du système (mises à jour du noyau, activations de plugins...)';
+$lang['porg_issue_1849'] = 'juste après la mise à jour du noyau, Piwigo affiche toujours que la version actuelle est la précédente';
+$lang['porg_issue_1852'] = 'possibilité de désactiver les mises à jour du noyau et des extensions';
+$lang['porg_issue_1831'] = '[Édition d\'album] Redirection erronée lors de la suppression d\'un album';
