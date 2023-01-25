8197ec1d7f3f28c0904a07f6b089df1348a596d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 12:27:28 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1831'] = '[Edição do álbum] Redirecionamento incorreto ao excluir um álbum';
+$lang['porg_issue_1832'] = '[Gerenciador de Usuários] Salvar paginação em user_prefs';
+$lang['porg_issue_1834'] = '[Gerenciador de Usuários] Adicionar informações de filtro';
+$lang['porg_issue_1835'] = 'Vulnerabilidade Piwigo-13.4.0-Stored XSS no User-Agent';
+$lang['porg_issue_1836'] = 'não executa mais instruções obsolete.list em atualizações menores';
+$lang['porg_issue_1837'] = '[Album Manager] Adicionar dica de ferramenta personalizada para os emblemas';
+$lang['porg_issue_1838'] = '[atividades] ocultar conexões da lista de atividades do usuário';
+$lang['porg_issue_1839'] = '[atividades] nomes de usuários ausentes no registro do usuário';
+$lang['porg_issue_1841'] = 'registrar atividades do sistema (atualizações principais, ativações de plugins...)';
+$lang['porg_issue_1849'] = 'logo após a atualização do núcleo, o Piwigo ainda diz que a versão atual é a anterior';
+$lang['porg_issue_1852'] = 'capacidade de desabilitar atualizações de núcleo/extensões';
