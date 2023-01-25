278c09bc76bcfcc6d59d8821763c27f2121fab0b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 12:27:24 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1831'] = '[Modifica album] Errata redirezione quando si cancella un album.';
+$lang['porg_issue_1832'] = '[Gestione utente] Salva paginazione in user_prefs';
+$lang['porg_issue_1834'] = '[Gestione utente] Aggiunge filtro informazioni';
+$lang['porg_issue_1835'] = 'Piwigo-13.4.0-Stored XSS Vulnerability in User-Agent';
+$lang['porg_issue_1836'] = 'non esegue più le istruzioni obsolete.list in occasione di aggiornamenti minori';
+$lang['porg_issue_1837'] = '[Gestione album] Aggiunge descrizione personalizzata per i badge';
+$lang['porg_issue_1838'] = '[attività] nascondi le connessioni dalla lista delle attività dell\'utente';
+$lang['porg_issue_1839'] = '[attività] nomi utenti scomparsi dalla registrazione degli stessi';
+$lang['porg_issue_1841'] = 'registra attività di sistema (aggiornamenti del core, attivazione plug-in...)';
+$lang['porg_issue_1849'] = 'immediatamente dopo l\'aggiornamento del core, Piwigo continua a riferire che la versione in uso corrisponde a quella precedente l\'aggiornamento';
+$lang['porg_issue_1852'] = 'abilità di disabilitare gli aggiornamenti del core e delle estensioni';
