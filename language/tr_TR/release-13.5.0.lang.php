358594046b9f4345de32e00a4e688d56e409750c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 21:07:15 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1835'] = 'User-Agent\'ta Piwigo-13.4.0-Stored XSS Güvenlik Açığı';
+$lang['porg_issue_1838'] = '[aktiviteler] kullanıcı aktivite listesinden bağlantıları gizleme';
+$lang['porg_issue_1839'] = '[etkinlikler] kullanıcı kaydında eksik kullanıcı adları';
+$lang['porg_issue_1852'] = 'çekirdek/uzantı güncellemelerini devre dışı bırakma yeteneği';
+$lang['porg_issue_1849'] = 'çekirdek güncellemesinden hemen sonra, Piwigo hala mevcut versiyonun bir önceki versiyon olduğunu söylüyor';
+$lang['porg_issue_1841'] = 'sistem etkinliklerini kaydetme (temel güncellemeler, eklenti etkinleştirmeleri...)';
+$lang['porg_issue_1837'] = '[Albüm Yöneticisi] Rozetler için özel araç ipucu ekleyin';
+$lang['porg_issue_1836'] = 'küçük güncellemelerde artık obsolete.list talimatlarını çalıştırmayın';
+$lang['porg_issue_1834'] = '[Kullanıcı Yöneticisi] Filtre bilgilerini ekle';
+$lang['porg_issue_1832'] = '[Kullanıcı Yöneticisi] Sayfalandırmayı user_prefs içine kaydetme';
+$lang['porg_issue_1831'] = '[Albüm Düzenleme] Bir albüm silinirken yanlış yönlendirme';
