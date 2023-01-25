7f0d605d839ca6fbd8b8a9460715f5d677c5190b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 12:27:19 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1831'] = '[Album bearbeiten] Falsche Weiterleitung beim Löschen eines Albums';
+$lang['porg_issue_1832'] = '[User Manager] Paginierung in user_prefs speichern';
+$lang['porg_issue_1834'] = '[User Manager] Filterinformationen hinzufügen';
+$lang['porg_issue_1835'] = 'Piwigo-13.4.0-Stored XSS-Schwachstelle im User-Agent';
+$lang['porg_issue_1836'] = 'Anweisungen obsolete.list bei kleineren Updates nicht mehr ausführen';
+$lang['porg_issue_1837'] = '[Album Manager] Benutzerdefinierter Tooltip für die Badges hinzugefügt';
+$lang['porg_issue_1838'] = '[Aktivitäten] Verbindungen aus der Benutzeraktivitätsliste ausblenden';
+$lang['porg_issue_1839'] = '[Aktivitäten] fehlende Benutzernamen bei der Benutzerregistrierung';
+$lang['porg_issue_1841'] = 'Systemaktivitäten registrieren (Kern-Updates, Plugin-Aktivierungen...)';
+$lang['porg_issue_1849'] = 'direkt nach Core-Update sagt Piwigo immer noch, dass die aktuelle Version die vorherige ist';
+$lang['porg_issue_1852'] = 'Möglichkeit, Core/Extensions-Updates zu deaktivieren';
