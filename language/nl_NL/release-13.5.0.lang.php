0b6d11189c95d08feb60a096025e8bf4fb77468a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 21:07:25 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1832'] = '[Gebruikersbeheer] Paginering opslaan in gebruikers_voorkeuren';
+$lang['porg_issue_1852'] = 'mogelijkheid om kern-/extensie-updates uit te schakelen';
+$lang['porg_issue_1849'] = 'direct na de kernupdate zegt Piwigo nog steeds dat de huidige versie de vorige is';
+$lang['porg_issue_1841'] = 'systeemactiviteiten registreren (kernupdates, activeringen van plug-ins...)';
+$lang['porg_issue_1839'] = '[activiteiten] ontbrekende gebruikersnamen bij gebruikersregistratie';
+$lang['porg_issue_1838'] = '[activiteiten] verberg verbindingen in de lijst met gebruikersactiviteiten';
+$lang['porg_issue_1837'] = '[Album Manager] Voeg aangepaste tooltip toe voor de badges';
+$lang['porg_issue_1836'] = 'voer niet langer obsolete.list-instructies uit bij kleine updates';
+$lang['porg_issue_1835'] = 'Piwigo-13.4.0-opgeslagen XSS-kwetsbaarheid in gebruikers-agent';
+$lang['porg_issue_1834'] = '[Gebruikersbeheer] Filterinformatie toevoegen';
+$lang['porg_issue_1831'] = '[Album bewerken] Verkeerde omleiding bij het verwijderen van een album';
