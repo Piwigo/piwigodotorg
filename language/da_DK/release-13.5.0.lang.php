f6f73c5ca4152603a73b5b52034f6a1921fb167a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-25 21:07:20 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1831'] = '[Albumredigering] Forkert viderestilling ved sletning af et album';
+$lang['porg_issue_1832'] = '[Brugerhåndtering] Gem sidenummerering i user_prefs';
+$lang['porg_issue_1834'] = '[Brugerhåndtering] ';
+$lang['porg_issue_1835'] = 'Piwigo-13.4.0-Gemte XSS-sårbarhed i User-Agent';
+$lang['porg_issue_1836'] = 'Udfør ikke længere obsolete.list-instruktioner ved mindre opdateringer';
+$lang['porg_issue_1837'] = '[Albumhåndtering] Tilføj skræddersyet tooltip til mærkerne';
+$lang['porg_issue_1838'] = '[Aktiviteter] Skjul forbindelser fra brugeraktivitetslisten';
+$lang['porg_issue_1839'] = '[Aktiviteter] Manglende brugernavne ved brugerregistrering';
+$lang['porg_issue_1841'] = 'Registrér systemaktiviteter (core-opdateringer, plugin-aktiveringer ...)';
+$lang['porg_issue_1849'] = 'Lige efter en core-opdatering oplyser Piwigo stadig om den foregående';
+$lang['porg_issue_1852'] = 'Mulighed for at deaktivere opdateringer af core/udvidelser';
