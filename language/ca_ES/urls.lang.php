00ec3d30ec11136842974424138c8852347ca216
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-04 23:08:15 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/urls.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_doc_urls']['install'] = 'https://piwigo.org/doc/doku.php?id=user_documentation:learn:install';
+$lang['porg_doc_urls']['requirements'] = 'https://piwigo.org/doc/doku.php?id=user_documentation:learn:install:before';
+$lang['porg_doc_urls']['upgrade'] = 'https://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade';
+$lang['porg_doc_urls']['upgrade_auto'] = 'https://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade:upgrade_automatic';
+$lang['porg_doc_urls']['upgrade_manual'] = 'https://piwigo.org/doc/doku.php?id=user_documentation:learn:upgrade:upgrade_manual
+';
+$lang['porg_urls']['about-us'] = 'sobre-nosaltres';
+$lang['porg_urls']['automatic_update'] = 'guides/update/automatic';
+$lang['porg_urls']['changelogs'] = 'canvis';
+$lang['porg_urls']['coding-activity'] = 'activitat-codificacio';
+$lang['porg_urls']['contact'] = 'contacte';
+$lang['porg_urls']['features'] = 'caracteristiques';
+$lang['porg_urls']['get-involved'] = 'impliqueu-vos';
+$lang['porg_urls']['get-piwigo'] = 'agafa-piwigo';
+$lang['porg_urls']['get-started'] = 'comenceu';
+$lang['porg_urls']['guides'] = 'guies';
+$lang['porg_urls']['manual_installation'] = 'guides/install/manual';
+$lang['porg_urls']['manual_update'] = 'guides/update/manual';
+$lang['porg_urls']['mobile-applications'] = 'aplicacions-mobils';
+$lang['porg_urls']['netinstall'] = 'guides/install/netinstall';
+$lang['porg_urls']['news'] = 'novetats';
+$lang['porg_urls']['newsletters'] = 'newsletter';
+$lang['porg_urls']['press'] = 'premsa';
+$lang['porg_urls']['release'] = 'release';
+$lang['porg_urls']['requirements'] = 'guides/install/requirements';
+$lang['porg_urls']['showcases'] = 'exemples';
+$lang['porg_urls']['testimonials'] = 'testimonis';
+$lang['porg_urls']['what-is-piwigo'] = 'que-es-piwigo';
