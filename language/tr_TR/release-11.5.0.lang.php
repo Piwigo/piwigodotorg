b2ee750a97a796056e48a30feb62068c3b9eb6f5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-20 06:32:29 +0100

    Update tr_TR, thanks to Nakre

---
+++ language/tr_TR/release-11.5.0.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1398'] = '[Eklenti Yöneticisi] Açılır menü simgesinin boyutu çok büyük';
+$lang['porg_issue_1399'] = '[Albüm Arama] Arama sonuçlarını 100 albümle sınırlayın';
+$lang['porg_issue_1397'] = 'bir kullanıcının devre dışı bırakılması oturumlarını sonlandırmalıdır';
+$lang['porg_issue_1362'] = 'yinelenen grup, varsayılan simge gösterilmiyor';
+$lang['porg_issue_1241'] = 'Galeri boşken tanımlama başarısız oluyor';
+$lang['porg_issue_1380'] = 'albüm baskısı, zaman aşımı';
+$lang['porg_issue_1392'] = 'ilgili albümler, performans sorunları';
