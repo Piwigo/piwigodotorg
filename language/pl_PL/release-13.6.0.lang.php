d20a8c3024ee3df534a7dfc642c0376ad6fe26cb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-24 16:49:34 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages zwraca nieprawidłową liczbę wszystkich zdjęć';
+$lang['porg_issue_1872'] = '[13.x] Iniekcja SQL dla user_list_backend.php';
+$lang['porg_issue_1786'] = 'Niepoprawne przesyłanie z Digikam 7.8.0 do piwigo 13.1';
+$lang['porg_issue_1855'] = 'Potencjalny problem bezpieczeństwa w demo Smarty';
+$lang['porg_issue_1858'] = '[Zarządzanie użytkownikami] Kolejność na liście użytkowników';
+$lang['porg_issue_1859'] = 'Nowy projekt banera dla aplikacji mobilnej';
+$lang['porg_issue_1861'] = '[Tryb ciemny] Ślad dla formularza dodawania zdjęcia nie jest ciemny';
+$lang['porg_issue_1862'] = '[Zarządzanie grupami] Kolor lokatora w trybie ciemnym';
+$lang['porg_issue_1868'] = 'Przycisk pierwszego albumu nie jest wyśrodkowany';
