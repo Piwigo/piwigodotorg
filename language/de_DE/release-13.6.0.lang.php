5eff8ea90a00530b2ebc6050304c8a091115d894
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-24 13:47:32 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1786'] = 'Upload von Digikam 7.8.0 auf piwigo 13.1.0 defekt';
+$lang['porg_issue_1855'] = 'mögliches Sicherheitsproblem mit Smarty-Demo';
+$lang['porg_issue_1858'] = '[User Manager] Reihenfolge der Benutzerliste';
+$lang['porg_issue_1859'] = 'Neues Design für das Banner der mobilen App';
+$lang['porg_issue_1861'] = '[Dunkler Modus] Breadcrumb für das Formular zum Hinzufügen von Fotos ist nicht dunkel';
+$lang['porg_issue_1862'] = '[Gruppenmanager] Platzhalterfarbe im dunklen Modus';
+$lang['porg_issue_1868'] = 'Erste Album-Schaltfläche ist nicht zentriert';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages liefert eine falsche Gesamtzahl von Bildern';
+$lang['porg_issue_1872'] = '[13.x] SQL-Einschleusung in user_list_backend.php';
