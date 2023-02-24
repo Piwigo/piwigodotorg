012603f3e0613cafaad5cedcb03ec4ed05cc96e8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-24 16:49:29 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1872'] = 'SQL-injectie op user_list_backend.php';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages retourneert een verkeerd totaal aantal afbeeldingen';
+$lang['porg_issue_1868'] = 'De eerste albumknop is niet gecentreerd';
+$lang['porg_issue_1862'] = '[Groepsbeheerder] Plaats houderkleur in donkere modus';
+$lang['porg_issue_1861'] = '[Donkere modus] Breadcrumb voor het toevoegen van een fotoformulier is niet donker';
+$lang['porg_issue_1859'] = 'Nieuw ontwerp voor banner van mobiele app';
+$lang['porg_issue_1858'] = '[Gebruikersbeheer] Volgorde gebruikerslijst';
+$lang['porg_issue_1855'] = 'mogelijk beveiligingsprobleem met de Smarty-demo';
+$lang['porg_issue_1786'] = 'Uploaden van Digikam 7.8.0 naar piwigo 13.1.0 kapot';
