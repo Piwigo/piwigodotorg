6317de270aead2bb02e12fd9815c5f38686353aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-25 07:26:31 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1862'] = '[Grup Yöneticisi] Karanlık modda yer tutucu rengi';
+$lang['porg_issue_1861'] = '[Karanlık mod] Fotoğraf formu eklemek için içerik haritası karanlık değil';
+$lang['porg_issue_1858'] = '[Kullanıcı Yöneticisi] Kullanıcı listesi sırası';
+$lang['porg_issue_1786'] = 'Digikam 7.8.0\'dan piwigo 13.1.0\'a yükleme kırık';
+$lang['porg_issue_1872'] = '[13.x] user_list_backend.php üzerinde SQL enjeksiyonu';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages, yanlış bir toplam görüntü sayısı döndürür';
+$lang['porg_issue_1868'] = 'İlk albüm düğmesi ortalanmamış';
+$lang['porg_issue_1859'] = 'Mobil uygulama banner\'ı için yeni tasarım';
+$lang['porg_issue_1855'] = 'Smarty demo ile potansiyel güvenlik açığı';
