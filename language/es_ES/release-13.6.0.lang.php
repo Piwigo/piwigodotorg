085846251312485bb902a4774b6067845d0aa8fd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-25 07:26:38 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1786'] = 'La carga de Digikam 7.8.0 a Piwigo 13.1.0 está rota';
+$lang['porg_issue_1855'] = 'problema potencial de seguridad con la demostración de Smarty';
+$lang['porg_issue_1858'] = '[Gestor de usuarios] Orden de la lista de usuarios';
+$lang['porg_issue_1859'] = 'Nuevo diseño para el banner de la aplicación móvil';
+$lang['porg_issue_1861'] = '[Modo oscuro] El rastro de navegación para el formulario de añadir fotos no es oscuro';
+$lang['porg_issue_1862'] = '[Gestor de grupos] Color del marcador de posición en modo oscuro';
+$lang['porg_issue_1868'] = 'El botón del primer álbum no está centrado';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages devuelve un número total de imágenes erróneo';
+$lang['porg_issue_1872'] = '[13.x] Inyección SQL en user_list_backend.php';
