c4c9d5b58204ee264de1eb5429f195f7384a9b09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-01 13:11:47 +0100

    Update pt_PT, thanks to ANO

--- language/pt_PT/release-12.1.0.lang.php
+++ language/pt_PT/release-12.1.0.lang.php
@@ -6,3 +6,10 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['porg_issue_1555'] = 'tradução esquecida na página de edição de fotos';
+$lang['porg_issue_1546'] = 'Atualizações Piwigo \"novos recursos interessantes\" link está em branco';
+$lang['porg_issue_1547'] = '[upgrade] desc groups falha no MySQL 8';
+$lang['porg_issue_1549'] = 'Nenhuma atualização de extensão enquanto o painel diz o contrário';
+$lang['porg_issue_1550'] = 'A página de extensões está quebrada';
+$lang['porg_issue_1552'] = '[12.0] após o carregamento, o Álbum X agora contém 0 fotos';
+$lang['porg_issue_1553'] = 'pwg.images.upload Concluído adicionar detalhes sobre a categoria atualizada';
+$lang['porg_issue_1554'] = '[12.x] formulário de carregamento, álbum selecionado inesperado';
