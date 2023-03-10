7700ee5c15377521b0b175ee8273a78a96f675f8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-10 10:41:52 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1786'] = 'Upload fra Digikam 7.8.0 til Piwigo 13.1.0 virker ikke';
+$lang['porg_issue_1855'] = 'Potentielt sikkerhedsproblem med Smarty-demo';
+$lang['porg_issue_1858'] = '[Brugerhåndtering] Brugerlisterækkefølge';
+$lang['porg_issue_1859'] = 'Nyt design af mobilapps banner';
+$lang['porg_issue_1861'] = '[Mørk tilstand] Brødkrummer til tilføjelse af fotografi er ikke mørke';
+$lang['porg_issue_1862'] = '[Gruppehåndtering] Stedfortræderfarve i mørk tilstand';
+$lang['porg_issue_1868'] = 'Første albumknap er ikke centreret';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages returnerer et forkert antal billeder';
+$lang['porg_issue_1872'] = '[13.x] SQL-indsprøjtning mulig i user_list_backend.php';
