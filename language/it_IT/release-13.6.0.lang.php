b49a953c82dc0305aec4e6c8d148556c414137f3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-13 10:43:46 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1786'] = 'Caricamento da Digikam 7.8.0 a Piwigo 13.1.0 non funzionante.';
+$lang['porg_issue_1855'] = 'potenziale problema di sicurezza con la demo Smarty.';
+$lang['porg_issue_1858'] = '[Gestione utente] Ordine della lista utenti.';
+$lang['porg_issue_1859'] = 'Nuovo design per lo striscione dell\'applicazione mobile.';
+$lang['porg_issue_1861'] = '[Dark mode]Il menù "Briciole di pane" perl\'aggiunta delle fotografie non è scuro, come invece dovrebbe.';
+$lang['porg_issue_1862'] = '[Gestione gruppo] Colore segnaposto in Dark mode.';
+$lang['porg_issue_1868'] = 'Il bottone Primo album non è centrato.';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages ritorna un errato numero totale di immagini.';
+$lang['porg_issue_1872'] = '[13.x] SQL injection in user_list_backend.php';
