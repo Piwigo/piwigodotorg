8c5de3d934e60eac31f3a2618943da17642d6522
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-04-14 07:14:30 +0200

    Update tr_TR, thanks to LazBoy

--- language/tr_TR/get_piwigo.lang.php
+++ language/tr_TR/get_piwigo.lang.php
@@ -45,3 +45,5 @@ $lang['Clients'] = 'Müşteriler';
 $lang['Start free trial'] = 'Ücretsiz deneme sürümü başlat';
 $lang['porg_get_piwigo_desc2-2'] = 'veya Piwigo.com bulut sistemini kullanabilirsiniz.
 ';
+$lang['Get Professional Support'] = 'Profesyonel Destek Alın';
+$lang['Install & Upgrade Guides'] = 'Yükleme &amp; Yükseltme Kılavuzları';
