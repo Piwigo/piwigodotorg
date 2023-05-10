16b3c7799ee5c762a2effb2402f14120293078ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:14:38 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Coding activity'] = 'Kodierungstätigkeit';
+$lang['porg_codingactivity_intro'] = 'Neueste Commits in Piwigo Core, Plugins, Themes und anderen Tools. Diese Seite sammelt Daten aus verschiedenen Code-Repositories, um einen "One Place to Watch" zu bieten.';
