d1743ea79d84b59bf7b92ac3b187a9b995d2efda
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:14:33 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Coding activity'] = 'Kodeaktivitet';
+$lang['porg_codingactivity_intro'] = 'De seneste commits til Piwigo core, temaer og andre værktøjer. Denne side opsamler data fra forskellige kodearkiver, så man kun behøver at kigge ét sted.';
