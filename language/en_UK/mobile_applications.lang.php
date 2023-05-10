cbf912a21106c83880d4a5ca234d4ef24522ce73
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:13:42 +0200

    Update en_UK, thanks to Kaare

--- language/en_UK/mobile_applications.lang.php
+++ language/en_UK/mobile_applications.lang.php
@@ -9,9 +9,7 @@ $lang['mobile_applications_follows_you_texte'] = 'With the Piwigo mobile applica
 $lang['mobile_applications_albums_title'] = 'Albums always well classified' ;
 $lang['mobile_applications_albums_texte'] = 'From the Piwigo mobile application, you have full control over your albums. You can create them, modify them, rename them as you see fit. You can also define the parameters of each album: name, description, thumbnail image ...' ;
 $lang['mobile_applications_sort_photos_title'] = 'Finely manage your photos' ;
-$lang['mobile_applications_sort_photos_texte']= 'You have access to all the details of your photos from the Piwigo mobile application, and you can modify them: title, description, tags, author ... You can also classify your photos, download them to your mobile, or delete them .' ;
-$lang['mobile_applications_import_options_title'] = 'Import options for your photos' ;
-$lang['mobile_applications_size_quality_title'] = 'Size and quality';
+$lang['mobile_applications_sort_photos_texte'] = 'You have access to all the details of your photos from the Piwigo mobile application, and you can modify them: title, description, tags, author ... You can also classify your photos, download them to your mobile, or delete them.';
 $lang['mobile_applications_size_quality_texte'] = 'Change the size of your photos and the quality you want to keep on the server.';
 $lang['mobile_applications_metadata_title'] = 'Metadata';
 $lang['mobile_applications_metadata_texte'] = 'Access all the metadata of your files and modify them if necessary: ​​title, author, date, confidentiality, tags, description ...';
@@ -31,13 +29,11 @@ $lang['mobile_applications_confidentiality_texte'] = 'As always with Piwigo, you
 $lang['mobile_applications_control_title'] = 'Data control';
 $lang['mobile_applications_control_texte'] = 'Determine whether or not you want to pass the metadata of your photos when you share them from the app.';
 $lang['mobile_applications_download'] = 'Download the Piwigo app';
-$lang['mobile_applications_download_app_store1'] = 'Download for android';
+$lang['mobile_applications_download_app_store1'] = 'Download for Android';
 $lang['mobile_applications_download_play_store1'] = 'Download for IOS';
-$lang['mobile_applications_download_app_store2'] = 'or download the application via the play store';
-$lang['mobile_applications_download_play_store2'] = 'or download the application via the app store';
+$lang['mobile_applications_download_app_store2'] = 'or download the application via the Play Store';
+$lang['mobile_applications_download_play_store2'] = 'or download the application via the App Store';
 $lang['mobile_applications_scan_qr'] = 'Scan this QR code with your phone!';
 
 $lang['ios_only'] = 'iOS only';
 
-
-?>
