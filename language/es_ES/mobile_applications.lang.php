ab94d48517086a98ef21a2c660df972c9010bdcf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:13:31 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/mobile_applications.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['mobile_applications_user_rights_title'] = 'Derechos del usuario';
+$lang['ios_only'] = 'Sólo iOS';
