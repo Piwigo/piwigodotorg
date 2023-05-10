441a4ef9705c986f18b81c6b525b9ef3568eeada
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:13:36 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/mobile_applications.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['mobile_applications_import_options_title'] = 'Dine fotografiers importmuligheder';
+$lang['mobile_applications_sort_photos_texte'] = 'Du har adgang til alle oplysninger om dine fotografier fra Piwigos mobilapp, og du kan ændre dem: titel, beskrivelse, tags, forfatter ... Du kan også organisere dem, hente dem til din telefon, eller slette dem.';
+$lang['mobile_applications_import_title'] = 'Importer dine fotografier og videoer';
+$lang['mobile_applications_introduction'] = 'Piwigo er også på din mobiltelefon! En frit tilgængelig mobilapp til håndtering af dit fotobibliotek, med mange funktioner.';
+$lang['mobile_applications_invite_texte'] = 'Piwigos mobilapp er kompatibel med Community-plugin\'en, som gør det muligt for brugere, der ikke er administratorer, også at overføre fotografier til Piwigo.';
+$lang['mobile_applications_invite_title'] = 'Inviter din familie og kollegaer';
+$lang['mobile_applications_metadata_texte'] = 'Tilgå alle dine filers metadata og tilpas dem om nødvendigt: titel, forfatter, dato, fortrolighed, tags, beskrivelse ...';
+$lang['mobile_applications_metadata_title'] = 'Metadata';
+$lang['mobile_applications_scan_qr'] = 'Scan denne QR-kode med din telefon!';
+$lang['mobile_applications_security_title'] = 'Sikkerhed og fortrolighed';
+$lang['mobile_applications_share_texte'] = 'Del dine fotografier og videoer med dine kontakter, direkte fra Piwigos mobilapp, takket være den integrerede delingsmenu.';
+$lang['mobile_applications_share_title'] = 'Del dine fotografier og videoer';
+$lang['mobile_applications_size_quality_texte'] = 'Du kan ændre dine fotografiers størrelse, samt den kvalitet du ønsker at gemme på serveren.';
+$lang['mobile_applications_size_quality_title'] = 'Størrelse og kvalitet';
+$lang['mobile_applications_sort_photos_title'] = 'Detaljeret håndtering af dine fotografier';
+$lang['mobile_applications_title'] = 'Piwigos mobilapp findes til iOS og Android';
+$lang['mobile_applications_user_rights_texte'] = 'Autorisationerne opsat i Piwigo, er også gyldige i mobilappen: enhver bruger kan kun se de albummer, som de har lov til.';
+$lang['mobile_applications_user_rights_title'] = 'Brugerrettigheder';
+$lang['ios_only'] = 'Kun iOS';
+$lang['mobile_applications_albums_texte'] = 'Med Piwigos mobilapp har du fuld kontrol over dine albummer. Du kan oprette, ændre og omdøbe dem, som det passer dig. Du kan også tilpasse hvert albums indstillinger: navn, beskrivelse, miniaturebillede ...';
+$lang['mobile_applications_albums_title'] = 'Albummer er altid velorganiserede';
+$lang['mobile_applications_confidentiality_texte'] = 'Som altid med Piwigo, tilhører dine data dig. Der er ingen sporing, ingen deling af dine personlige oplysninger, ingen algoritme som kigger på dine filer.';
+$lang['mobile_applications_confidentiality_title'] = 'Fortrolighed';
+$lang['mobile_applications_control_texte'] = 'Bestem om du vil overføre dine fotografiers metadata eller ej, når du deler dem fra appen.';
+$lang['mobile_applications_control_title'] = 'Datakontrol';
+$lang['mobile_applications_copy_move_texte'] = 'Med Piwigos mobilapp til iOS, kan du let organisere og omorganisere dine fotografier, ændre hvilket album de er i, eller kopiere dem fra et album til et andet.';
+$lang['mobile_applications_copy_move_title'] = 'Kopier og flyt dine fotografier og videoer';
+$lang['mobile_applications_download'] = 'Hent Piwigo-app\'en';
+$lang['mobile_applications_download_app_store1'] = 'Hent til Android';
+$lang['mobile_applications_download_app_store2'] = 'eller hent app\'en via Play Store';
+$lang['mobile_applications_download_play_store1'] = 'Hent til iOS';
+$lang['mobile_applications_download_play_store2'] = 'eller hent app\'en via App Store';
+$lang['mobile_applications_follows_you_texte'] = 'Med Piwigos mobilapp, kan du tilgå dit fotobibliotek når som helst! Der er ikke behov for at gemme alle dine filer på din telefon: find alle dine albummer, fotografier og videoer ved hjælp af mobilappen.';
+$lang['mobile_applications_follows_you_title'] = 'Dit fotobibliotek følger dig overalt';
+$lang['mobile_applications_geolocalisation_texte'] = 'Hent automatisk dine fotografiers GPS-koordinater, og se dem på et kort i dit galleri.';
+$lang['mobile_applications_geolocalisation_title'] = 'Geotagging af dine fotografier';
+$lang['mobile_applications_import_text'] = 'Med Piwigos mobilapp, kan du tilføje fotografier til dit fotobibliotek når som helst fra din mobiltelefon. Opret nye albummer eller føj til eksisterende albummer. Du kan også importere vidoer, hvis denne mulighed er aktiveret i dit galleri (kompatibel med VideoJS-plugin\'en).';
