753d8d9f96a0a79b522a8d0ca3d975690a9aa853
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:14:42 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/showcases.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_showcases_indre_title'] = 'Bezirksrat von Indre';
+$lang['porg_showcases_macrogallery_desc'] = 'Ein leidenschaftlicher Amateur-Letoniker präsentiert in diesem Fotoarchiv eine Sammlung von Bildern über Insekten, aber auch Spinnen und Pflanzen. Die Fotos sind sorgfältig klassifiziert und nach Familie, Klasse und Ordnung geordnet ... eine beeindruckende Arbeit.';
+$lang['porg_showcases_macrogallery_title'] = 'Makro-Galerie';
+$lang['porg_showcases_milophotographie_desc'] = 'Maria, eine professionelle Fotografin in Paris, nutzt Piwigo, um ihre Arbeit zu präsentieren.';
+$lang['porg_showcases_milophotographie_title'] = 'Milo Fotografie';
+$lang['porg_showcases_nielfitzgerald_desc'] = 'Neil Fitzgerald, professioneller Fotograf in Neuseeland, der sich auf Natur- und Tierfotografie spezialisiert hat, wählte Piwigo, um seine Arbeit zu verbessern.';
+$lang['porg_showcases_nielfitzgerald_title'] = 'Neil Fitzgerald, Fotograf';
+$lang['porg_showcases_oceanopolis_desc'] = 'Oceanopolis, der berühmte bretonische Meeresentdeckungspark, nutzt Piwigo zur Verwaltung seiner Foto- und Videobibliothek.';
+$lang['porg_showcases_oceanopolis_title'] = 'Oceanopolis Brest';
+$lang['porg_showcases_parcdeslandesdegascogne_desc'] = 'Der regionale Naturpark Landes de Gascogne wählte Piwigo für die Erstellung und Verwaltung seiner Fotobibliothek.';
+$lang['porg_showcases_parcdeslandesdegascogne_title'] = 'Der regionale Naturpark Landes de Gascogne';
+$lang['porg_showcases_paris1sorbonne_desc'] = 'Die Universität Paris 1 Panthéon-Sorbonne hat sich für Piwigo entschieden, um ihre große Fotobibliothek zu verwalten und zu organisieren.';
+$lang['porg_showcases_paris1sorbonne_title'] = 'Universität Paris 1 Panthéon-Sorbonne';
+$lang['porg_showcases_photolib_desc'] = 'photolib.fr ist eine mit Piwigo erstellte Fotogalerie, die Fotos unter der Creative Commons Lizenz präsentiert.';
+$lang['porg_showcases_photolib_title'] = 'Fotobuch';
+$lang['porg_showcases_pinnacle_desc'] = 'In dieser Fotogalerie präsentiert Steve Raia seine fotografische Arbeit und vermarktet seine Fotos.';
+$lang['porg_showcases_pinnacle_title'] = 'Die Pinnaccle-Galerien';
+$lang['porg_showcases_societe3vallees_desc'] = 'Die Société des 3 Vallées, Meribel, Courchevel und Tania, hat Piwigo für die Organisation ihrer Medien ausgewählt.';
+$lang['porg_showcases_societe3vallees_title'] = 'Société des 3 vallées';
+$lang['porg_showcases_ect_desc'] = 'ECT verwaltet seine Bilddatenbank mit Piwigo für mehr Produktivität und Zusammenarbeit.';
+$lang['porg_showcases_ect_title'] = 'ECT-Gruppe';
+$lang['porg_showcases_ehcanada_desc'] = 'eh Canada, ein kanadischer Tourismusspezialist, verwaltet Tausende von Fotos in seiner mit Piwigo erstellten Fotobibliothek.';
+$lang['porg_showcases_ehcanada_title'] = 'Eh Kanada';
+$lang['porg_showcases_ffn_desc'] = 'Der französische Schwimmverband hat sich für Piwigo entschieden, um eine große Menge an Fotos und Videos zu speichern und zu teilen';
+$lang['porg_showcases_ffn_title'] = 'Französischer Schwimmverband';
+$lang['porg_showcases_gerarddubouloz_desc'] = 'Instant Photos ist die Galerie von Gérard Dubouloz, einem Fotografen, der die Natur und das Leben in der freien Natur liebt.';
+$lang['porg_showcases_gerarddubouloz_title'] = 'Sofortige Fotos';
+$lang['porg_showcases_guillaumecyriaque_desc'] = 'Guillaume Cyriaque, Amateurfotograf in Belgien, wählte Piwigo, um seine Arbeit zu verbessern.';
+$lang['porg_showcases_guillaumecyriaque_title'] = 'Guillaume Cyriaque, Fotograf';
+$lang['porg_showcases_hautegaronne_desc'] = 'Der Rat des Departements Haute Garonne hat seine Fotobibliothek mit Piwigo erstellt.';
+$lang['porg_showcases_hautegaronne_title'] = 'Rat des Departements Haute Garonne';
+$lang['porg_showcases_icam_desc'] = 'ICAM, eine französische Ingenieurschule, wählte Piwigo, um eine Fotobibliothek zu erstellen, die von ihren 11 Standorten auf der ganzen Welt genutzt wird.';
+$lang['porg_showcases_icam_title'] = 'Institut catholique d\'arts et métiers';
+$lang['porg_showcases_indre_desc'] = 'Der Bezirksrat von Indre entschied sich für Piwigo, um seine alte Fotobibliothekssoftware zu ersetzen, die teurer und weniger effizient war.';
+$lang['Visit Gallery'] = 'Galerie besuchen';
+$lang['page_meta_description'] = 'Unternehmen, Fotografen, öffentlicher Sektor: unsere Nutzer haben Talent! Entdecken Sie Beispiele von Online-Fotobibliotheken, die mit der Piwigo Software erstellt wurden.';
+$lang['page_meta_title'] = 'Piwigo Beispiele - Einige mit Piwigo erstellte Fotogalerien';
+$lang['porg_get_showcase_filter_all'] = 'Alle';
+$lang['porg_get_showcase_filter_companies'] = 'Unternehmen';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Fotografen und private Nutzer';
+$lang['porg_get_showcase_filter_public_sector'] = 'Öffentlicher Sektor';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Sport und Freizeit';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Reisen und Tourismus';
+$lang['porg_get_showcase_intro_paragraph'] = 'Entdecken Sie auf dieser Seite einige mit Piwigo erstellte Fotobibliotheken und öffentliche Galerien.';
+$lang['porg_get_showcase_intro_title'] = 'Piwigo-Nutzer haben Talent!';
+$lang['porg_showcases_arles_desc'] = 'Das Rathaus von Arles veröffentlicht auf seiner Piwigo-Galerie eine Auswahl von Fotos, die mit Bildern von den aktuellen Ereignissen der Stadt zeugen.';
+$lang['porg_showcases_arles_title'] = 'Stadt Arles';
+$lang['porg_showcases_dongshaw_desc'] = 'Dong Shaw Hwei, Maler aus Taiwan, präsentiert seine Werke in seiner Piwigo-Galerie.';
+$lang['porg_showcases_dongshaw_title'] = 'Die Kunst von Dong Shaw';
