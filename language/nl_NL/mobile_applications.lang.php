07dcc7d3329893529b5769f20f587f1ff38e1c34
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-10 06:13:48 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/mobile_applications.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['mobile_applications_user_rights_title'] = 'Gebruikersrechten';
+$lang['mobile_applications_user_rights_texte'] = 'De autorisaties die in Piwigo zijn geconfigureerd, zijn ook geldig op de mobiele applicatie: elke gebruiker kan alleen de albums zien waarop hij recht heeft.';
+$lang['mobile_applications_title'] = 'Piwigo mobiele app voor iOS en Android';
+$lang['mobile_applications_sort_photos_title'] = 'Beheer uw foto\'s nauwkeurig';
+$lang['mobile_applications_sort_photos_texte'] = 'U hebt toegang tot alle details van uw foto\'s vanuit de Piwigo mobiele applicatie en u kunt ze wijzigen: titel, beschrijving, tags, auteur ... U kunt uw foto\'s ook classificeren, downloaden naar uw mobiel of ze verwijderen.';
+$lang['mobile_applications_size_quality_texte'] = 'Wijzig de grootte van uw foto\'s en de kwaliteit die u op de server wilt behouden.';
+$lang['mobile_applications_share_title'] = 'Deel uw foto\'s en video\'s';
+$lang['mobile_applications_share_texte'] = 'Deel uw foto\'s en video\'s rechtstreeks met uw contacten vanuit de Piwigo mobiele applicatie, dankzij het geïntegreerde deelmenu.';
+$lang['mobile_applications_scan_qr'] = 'Scan deze QR-code met je telefoon!';
+$lang['mobile_applications_security_title'] = 'Beveiliging en vertrouwelijkheid';
+$lang['mobile_applications_metadata_title'] = 'Metadata';
+$lang['mobile_applications_metadata_texte'] = 'Krijg toegang tot alle metadata van uw bestanden en wijzig ze indien nodig: titel, auteur, datum, vertrouwelijkheid, tags, beschrijving ...';
+$lang['mobile_applications_invite_title'] = 'Nodig uw familieleden en collega\'s uit';
+$lang['mobile_applications_invite_texte'] = 'De Piwigo mobiele applicatie is compatibel met de Community-plug-in, waarmee niet-beheerders ook foto\'s naar Piwigo kunnen sturen.';
+$lang['mobile_applications_introduction'] = 'Piwigo staat ook op je smartphone! Een gratis mobiele applicatie om uw fotobibliotheek te beheren en te profiteren van vele functies.';
+$lang['mobile_applications_import_title'] = 'Importeer uw foto\'s en video\'s';
+$lang['mobile_applications_import_text'] = 'Met de mobiele applicatie van Piwigo kunt u op elk moment vanaf uw mobiel foto\'s aan uw fotobibliotheek toevoegen. Maak nieuwe albums of voeg toe aan uw bestaande albums. Importeer ook video\'s als je deze optie in je galerij hebt (compatibel met de VideoJS-plug-in).';
+$lang['mobile_applications_geolocalisation_title'] = 'Uw foto\'s geotaggen';
+$lang['mobile_applications_geolocalisation_texte'] = 'Je fotobibliotheek volgt je overal. Haal automatisch de GPS-coördinaten van je foto\'s op om ze op een kaart uit je galerij te kunnen bekijken.';
+$lang['mobile_applications_follows_you_title'] = 'Je fotobibliotheek volgt je overal';
+$lang['mobile_applications_follows_you_texte'] = 'Met de Piwigo mobiele applicatie heeft u op elk moment toegang tot uw fotobibliotheek! U hoeft niet al uw bestanden op uw telefoon te bewaren: vind al uw albums, foto\'s en video\'s via de mobiele applicatie.';
+$lang['mobile_applications_download_play_store2'] = 'of download de applicatie via de app store';
+$lang['mobile_applications_download_play_store1'] = 'Downloaden voor iOS';
+$lang['mobile_applications_download_app_store2'] = 'of download de applicatie via de play store';
+$lang['mobile_applications_download_app_store1'] = 'Downloaden voor android';
+$lang['mobile_applications_download'] = 'Download de Piwigo-app';
+$lang['mobile_applications_copy_move_title'] = 'Kopieer en verplaats uw foto\'s en video\'s';
+$lang['mobile_applications_copy_move_texte'] = 'Met de mobiele Piwigo-app voor iOS kunt u uw foto\'s eenvoudig ordenen en reorganiseren, wijzigen in welk album ze staan of ze van het ene album naar het andere kopiëren.';
+$lang['mobile_applications_control_title'] = 'Gegevens controle';
+$lang['mobile_applications_control_texte'] = 'Bepaal of je de metadata van je foto\'s wilt doorgeven wanneer je ze deelt vanuit de app.';
+$lang['mobile_applications_confidentiality_title'] = 'Vertrouwelijkheid';
+$lang['mobile_applications_confidentiality_texte'] = 'Zoals altijd bij Piwigo zijn uw gegevens alleen van u. Geen tracking, geen delen van uw persoonlijke gegevens, geen algoritme dat naar uw bestanden kijkt.';
+$lang['mobile_applications_albums_title'] = 'Albums altijd goed ingedeeld...';
+$lang['mobile_applications_albums_texte'] = 'Vanuit de Piwigo mobiele applicatie heeft u volledige controle over uw albums. U kunt ze naar eigen inzicht maken, wijzigen en hernoemen. U kunt ook de parameters van elk album definiëren: naam, beschrijving, miniatuurafbeelding ...';
+$lang['ios_only'] = 'Alleen iOS';
