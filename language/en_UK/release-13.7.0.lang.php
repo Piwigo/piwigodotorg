ea9dbe190fb70f731652b5cd6707e5631eb25354
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-11 13:18:35 +0200

    Update en_UK, thanks to Kaare

--- language/en_UK/release-13.7.0.lang.php
+++ language/en_UK/release-13.7.0.lang.php
@@ -6,9 +6,8 @@ $lang['porg_issue_1905'] = '[History page] Make the images clickable to go direc
 $lang['porg_issue_1903'] = '[User manager] Confirm popin for user delete has untranslated text in it';
 $lang['porg_issue_1901'] = '[Dark mode] Color updates';
 $lang['porg_issue_1887'] = '[Album Editor] Delete album doesnt work';
-$lang['porg_issue_1883'] = 'tag edition, cancel button does not work';
+$lang['porg_issue_1883'] = 'tag editing, cancel button does not work';
 $lang['porg_issue_1880'] = '[User Manager] Email edit block the user editor';
 $lang['porg_issue_1879'] = '[Dark mode] Change icon style in dark mode';
 $lang['porg_issue_1878'] = '[User Manager] User status isnt translated';
 $lang['porg_issue_1876'] = '[History page] SQL injection issue found in 13.5.0';
-?>
