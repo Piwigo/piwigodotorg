9e5f3ea69f0fe974b69cd59c35d86eeab469dc5b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-11 13:18:40 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.7.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1876'] = '[Historikside] SQL-indsprøjtningsfejl fundet i 13.5.0';
+$lang['porg_issue_1878'] = '[Brugerhåndtering] Brugerstatus er ikke oversat';
+$lang['porg_issue_1879'] = '[Mørk visning] Skifter ikonstil i mørk visning';
+$lang['porg_issue_1880'] = '[Brugerhåndtering] Mailredigering blokerer brugerredigeringen';
+$lang['porg_issue_1883'] = 'Tagredigering, knappen annuller virker ikke';
+$lang['porg_issue_1887'] = '[Albumredigering] Sletning af album virker ikkework';
+$lang['porg_issue_1901'] = '[Mørk visning] Farveopdateringer';
+$lang['porg_issue_1903'] = '[Brugerhåndtering] Bekræftelses-popin til brugersletning indeholder ikke-oversat tekst';
+$lang['porg_issue_1905'] = '[Historikside] Gør billederne klikbare så man kan springe direkte til deres redigeringsside';
+$lang['porg_issue_1907'] = '[da_DK] Manglende nyheder fra piwigo.org';
+$lang['porg_issue_1910'] = 'Der er en SQL-indsprøjtningsfejl i Piwigos funktion \"permalinks\"';
+$lang['porg_issue_1911'] = 'Der er en SQL-indsprøjtningsfejl i Piwigos funktion \"profile\"';
