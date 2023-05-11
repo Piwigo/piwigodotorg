0273d7f7aa33cf0b79eb038fd93fe53bc16aac7b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-11 20:55:16 +0200

    Update de_DE, thanks to slim

--- language/de_DE/release-13.7.0.lang.php
+++ language/de_DE/release-13.7.0.lang.php
@@ -5,8 +5,8 @@
 // | For copyright and license information, please view the COPYING.txt    |
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
-$lang['porg_issue_1911'] = 'Es gibt eine SQL Injection in der Funktion \"profile\" von piwigo';
-$lang['porg_issue_1910'] = 'Es gibt eine SQL Injection in der Funktion \"permalinks\" von piwigo';
+$lang['porg_issue_1911'] = 'Es gibt eine SQL Einspeisung in der Funktion \"profile\" von piwigo';
+$lang['porg_issue_1910'] = 'Es gibt eine SQL Einspeisung in der Funktion \"permalinks\" von piwigo';
 $lang['porg_issue_1907'] = '[da_DK] fehlende Nachrichten von piwigo.org';
 $lang['porg_issue_1905'] = '[Historienseite] Bilder anklickbar machen, um direkt zur Bearbeitungsseite zu gelangen';
 $lang['porg_issue_1903'] = '[Benutzermanager] Bestätigungs-Popin für Benutzer löschen hat unübersetzten Text darin';
@@ -15,5 +15,5 @@ $lang['porg_issue_1883'] = 'Tag-Edition, Abbrechen-Taste funktioniert nicht';
 $lang['porg_issue_1887'] = '[Album Editor] Album löschen funktioniert nicht';
 $lang['porg_issue_1879'] = '[Dunkler Modus] Icon-Stil im dunklen Modus ändern';
 $lang['porg_issue_1880'] = '[User Manager] E-Mail-Bearbeitung blockiert den User-Editor';
-$lang['porg_issue_1876'] = '[History page] SQL-Injection-Problem in 13.5.0 gefunden';
+$lang['porg_issue_1876'] = '[History page] SQL-Einspeisungs-Problem in 13.5.0 gefunden';
 $lang['porg_issue_1878'] = '[User Manager] Benutzerstatus wird nicht übersetzt';
