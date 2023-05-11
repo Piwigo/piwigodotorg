45728a115e0d6d81e8f6f45cef47e67f2536f90e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-11 13:18:30 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.7.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1911'] = 'Er is een SQL-injectie in de functie \"profiel\" van piwigo';
+$lang['porg_issue_1910'] = 'Er is een SQL-injectie in de functie \"permalinks\" van piwigo';
+$lang['porg_issue_1907'] = 'da_DK] ontbrekend nieuws van piwigo.org';
+$lang['porg_issue_1905'] = '[Geschiedenispagina] Maak de afbeeldingen klikbaar om direct naar hun bewerkingspagina te gaan';
+$lang['porg_issue_1903'] = '[Gebruikersbeheer] Bevestig dat pop-in voor gebruikersverwijdering onvertaalde tekst bevat';
+$lang['porg_issue_1901'] = '[Donkere modus] Kleurupdates';
+$lang['porg_issue_1887'] = '[Album Editor] Album verwijderen werkt niet';
+$lang['porg_issue_1883'] = 'ag bewerken, annuleerknop werkt niet';
+$lang['porg_issue_1880'] = '[Gebruikersbeheer] E-mailbewerking blokkeert de gebruikerseditor';
+$lang['porg_issue_1879'] = '[Donkere modus] Pictogramstijl wijzigen in donkere modus';
+$lang['porg_issue_1878'] = '[Gebruikersbeheer] Gebruikersstatus is niet vertaald';
+$lang['porg_issue_1876'] = '[Geschiedenispagina] Probleem met SQL-injectie gevonden in 13.5.0';
