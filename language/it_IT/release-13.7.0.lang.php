81fa039d6d9446132b906d2997fe7ab757d088d8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-12 10:36:37 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-13.7.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1878'] = '[Gestione utente] Lo stato dell\'utente non è stato tradotto.';
+$lang['porg_issue_1879'] = '[Dark mode] Cambio dello stile icone in Dark mode.';
+$lang['porg_issue_1880'] = '[Gestione utente] La modifica dell\'email blocca l\'editor utente.';
+$lang['porg_issue_1883'] = 'modifica tag, il pulsante per la cancellazione non funziona.';
+$lang['porg_issue_1887'] = '[Modifica album] Cancellazione album non funzionante';
+$lang['porg_issue_1901'] = '[Dark mode] Aggiornamento colori.';
+$lang['porg_issue_1903'] = '[Gestione utente] Il popin di conferma per la cancellazione dell\'utente presenta del testo non tradotto.';
+$lang['porg_issue_1905'] = '[Cronologia] Fare in modo che il click sulle immagini porti direttamente alla pagina per la loro modifica.';
+$lang['porg_issue_1907'] = '[da_DK] News da piwigo.org mancanti.';
+$lang['porg_issue_1910'] = 'C\'è una SQL injection nella funzione \"permalinks\" di Piwigo.';
+$lang['porg_issue_1911'] = 'C\'è una SQL injection nella funzione \"profile\" di Piwigo.';
+$lang['porg_issue_1876'] = '[Cronologia]. Problema relativo ad SQL injection riscontrato nella versione 13.5.0';
