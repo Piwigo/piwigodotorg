0fa11f4f90cc212fbabfc24c938db5de671c6c86
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-12 13:52:17 +0200

    Update it_IT, thanks to kinmen

--- language/it_IT/showcases.lang.php
+++ language/it_IT/showcases.lang.php
@@ -6,3 +6,21 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['Visit Gallery'] = 'Visita la galleria.';
+$lang['porg_showcases_ect_desc'] = 'ECT gestisce la propria collezione d\'immagini con Piwigo, per una migliore produttività e collaborazione.';
+$lang['porg_showcases_ect_title'] = 'Gruppo ETC.';
+$lang['porg_showcases_ehcanada_desc'] = 'eh Canada, specialista di turismo in Canada, ospita migliaia di fotografie all\'interno della propria collezione creata con Piwigo.';
+$lang['porg_showcases_ehcanada_title'] = 'Eh Canada.';
+$lang['page_meta_title'] = 'Esempi Piwigo - Alcune gallerie fotografiche create con Piwigo.';
+$lang['porg_get_showcase_filter_all'] = 'Tutto.';
+$lang['porg_get_showcase_filter_companies'] = 'Aziende.';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Fotografi ed utenti privati.';
+$lang['porg_get_showcase_filter_public_sector'] = 'Settore pubblico.';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Sport e divertimento.';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Viaggi e turismo.';
+$lang['porg_get_showcase_intro_paragraph'] = 'Scopri in questa pagina alcune librerie fotografiche e gallerie create con Piwigo.';
+$lang['porg_get_showcase_intro_title'] = 'Gli utenti di Piwigo hanno del talento!';
+$lang['porg_showcases_arles_desc'] = 'Il municipio di Arles (Provenza) pubblica sulla propria istanza di Piwigo una selezione di fotografie che attraverso le immagini presenta gli eventi in corso in città.';
+$lang['porg_showcases_arles_title'] = 'Città di Arles.';
+$lang['porg_showcases_dongshaw_desc'] = 'Dong Shaw Hwei, pittore di Taiwan, presenta i propri lavori sulla sua galleria di Piwigo.';
+$lang['porg_showcases_dongshaw_title'] = 'L\'arte di Dong Shaw.';
+$lang['page_meta_description'] = 'Aziende, fotografi, settore pubblico: i nostri utenti hanno talento! Scopri esempi di librerie fotografiche online create con Piwigo.';
