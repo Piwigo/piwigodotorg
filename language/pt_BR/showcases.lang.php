98700992b9bfe8b0b9fef830b9ea1c9c7be05f3d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-15 16:30:59 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/showcases.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_showcases_parcdeslandesdegascogne_title'] = 'O Parque Natural Regional Gascogne Lands';
+$lang['porg_showcases_paris1sorbonne_desc'] = 'A universidade Paris 1 Panthéon-Sorbonne escolheu Piwigo para gerenciar e organizar sua grande biblioteca de fotos.';
+$lang['porg_showcases_paris1sorbonne_title'] = 'Universidade Paris 1 Pantheon Sorbonne';
+$lang['porg_showcases_photolib_desc'] = 'photolib.fr é uma galeria de fotos criada com Piwigo que apresenta fotos sob a licença Creative Commons.';
+$lang['porg_showcases_photolib_title'] = 'Photolib';
+$lang['porg_showcases_pinnacle_desc'] = 'Nesta galeria de fotos, Steve Raia apresenta seu trabalho fotográfico e comercializa suas fotos.';
+$lang['porg_showcases_pinnacle_title'] = 'As Galerias Pinnaccle';
+$lang['porg_showcases_societe3vallees_desc'] = 'A empresa dos 3 vales, Meribel, Courchevel e Tania, escolheu a Piwigo para organizar sua mídia.';
+$lang['porg_showcases_societe3vallees_title'] = 'Sociedade dos 3 vales';
+$lang['porg_showcases_ffn_title'] = 'Federação Francesa de Natação';
+$lang['porg_showcases_gerarddubouloz_desc'] = 'Instant Photos é a galeria de Gérard Dubouloz, fotógrafo apaixonado pela natureza e pelo ar livre.';
+$lang['porg_showcases_gerarddubouloz_title'] = 'Fotos instantâneas';
+$lang['porg_showcases_guillaumecyriaque_desc'] = 'Guillaume Cyriaque, fotógrafo amador na Bélgica, escolheu Piwigo para aprimorar seu trabalho.';
+$lang['porg_showcases_guillaumecyriaque_title'] = 'Guillaume Cyriaque, fotógrafo';
+$lang['porg_showcases_hautegaronne_desc'] = 'O conselho do condado de Haute Garonne criou sua biblioteca de fotos com Piwigo.';
+$lang['porg_showcases_hautegaronne_title'] = 'Câmara Municipal de Haute Garonne';
+$lang['porg_showcases_icam_desc'] = 'A ICAM, uma escola de engenharia francesa, escolheu a Piwigo para criar uma fototeca compartilhada por seus 11 campi ao redor do mundo.';
+$lang['porg_showcases_icam_title'] = 'Instituto Católico de Artes e Ofícios';
+$lang['porg_showcases_indre_desc'] = 'O Conselho do Condado de Indre escolheu o Piwigo para substituir seu antigo software de biblioteca de fotos, que era mais caro e menos eficiente.';
+$lang['porg_showcases_indre_title'] = 'Câmara Municipal de Indre';
+$lang['porg_showcases_macrogallery_desc'] = 'Um apaixonado letão amador apresenta nesta fototeca um banco de imagens sobre insetos, mas também aranhas e flora. As fotos são cuidadosamente classificadas e indexadas por família, turma, ordem... um trabalho impressionante.';
+$lang['porg_showcases_macrogallery_title'] = 'Galeria de Macros';
+$lang['porg_showcases_milophotographie_desc'] = 'Maria, uma fotógrafa profissional em Paris, usa o Piwigo para compartilhar seu trabalho.';
+$lang['porg_showcases_milophotographie_title'] = 'Milo Fotografia';
+$lang['porg_showcases_nielfitzgerald_desc'] = 'Neil Fitzgerald, fotógrafo profissional neozelandês especializado em fotografia de natureza e vida selvagem, escolheu Piwigo para aprimorar seu trabalho.';
+$lang['porg_showcases_nielfitzgerald_title'] = 'Neil Fitzgerald, fotógrafo';
+$lang['porg_showcases_oceanopolis_desc'] = 'Océanpolis, o famoso parque bretão de descoberta do oceano, usa o Piwigo para gerenciar sua fototeca e sua videoteca.';
+$lang['porg_showcases_oceanopolis_title'] = 'Oceanópolis Brest';
+$lang['porg_showcases_parcdeslandesdegascogne_desc'] = 'O Parque Natural Regional Landes de Gascogne escolheu Piwigo para criar e gerenciar sua fototeca.';
+$lang['Visit Gallery'] = 'Visite a Galeria';
+$lang['page_meta_description'] = 'Empresas, fotógrafos, setor público: nossos usuários têm talento! Descubra exemplos de bibliotecas de fotos online criadas com o software Piwigo.';
+$lang['page_meta_title'] = 'Exemplos do Piwigo - Algumas galerias de fotos criadas com o Piwigo';
+$lang['porg_get_showcase_filter_all'] = 'Todos';
+$lang['porg_get_showcase_filter_companies'] = 'Empresas';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Fotógrafos e usuários privados';
+$lang['porg_get_showcase_filter_public_sector'] = 'Setor público';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Esportes e Lazer';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Viagem e Turismo';
+$lang['porg_get_showcase_intro_paragraph'] = 'Descubra nesta página algumas fototecas e galerias públicas criadas com o Piwigo.';
+$lang['porg_get_showcase_intro_title'] = 'Os usuários do Piwigo têm talento!';
+$lang['porg_showcases_arles_desc'] = 'A Câmara Municipal de Arles publica na sua galeria Piwigo uma seleção de fotografias que testemunham com imagens os acontecimentos atuais da cidade.';
+$lang['porg_showcases_arles_title'] = 'Cidade de Arles';
+$lang['porg_showcases_dongshaw_desc'] = 'Dong Shaw Hwei, pintor de Taiwan, apresenta seu trabalho em sua galeria Piwigo.';
+$lang['porg_showcases_dongshaw_title'] = 'A arte de Dong Shaw';
+$lang['porg_showcases_ect_desc'] = 'A ECT gerencia seu banco de imagens com o Piwigo para maior produtividade e colaboração.';
+$lang['porg_showcases_ect_title'] = 'Grupo ECT';
+$lang['porg_showcases_ehcanada_desc'] = 'A eh Canada, especialista em turismo no Canadá, hospeda milhares de fotos em sua fototeca criada com o Piwigo.';
+$lang['porg_showcases_ehcanada_title'] = 'Eh canadá';
+$lang['porg_showcases_ffn_desc'] = 'A federação francesa de natação escolheu Piwigo para armazenar e compartilhar um grande volume de fotos e vídeos';
