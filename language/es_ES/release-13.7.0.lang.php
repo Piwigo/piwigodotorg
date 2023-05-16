7d1883ed2d9fd36ad14003714cbff76b6771a389
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-16 06:19:04 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.7.0.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1876'] = '[Página de historial] Se ha encontrado un problema de inyección SQL en 13.5.0';
+$lang['porg_issue_1878'] = '[Gestor de usuarios] El estado del usuario no está traducido';
+$lang['porg_issue_1879'] = '[Modo oscuro] Cambio del estilo del icono en el modo oscuro';
+$lang['porg_issue_1880'] = '[Gestor de usuarios] La edición del e-mail bloquea el editor de usuarios';
+$lang['porg_issue_1883'] = '[Edición de etiquetas] El botón de cancelar no funciona';
+$lang['porg_issue_1887'] = '[Editor de álbumes] La supresión de un álbum no funciona';
+$lang['porg_issue_1901'] = '[Modo oscuro] Actualizaciones de color';
+$lang['porg_issue_1903'] = '[Gestor de usuarios] El diálogo emergente de confirmación de supresión de usuarios tiene texto sin traducir';
+$lang['porg_issue_1905'] = '[Página de historial] Hacer las imágenes clicables para ir directamente a su página de edición';
+$lang['porg_issue_1907'] = '[da_DK] Faltan noticias de piwigo.org';
+$lang['porg_issue_1910'] = 'Hay una inyección de SQL en la función \"permalinks\" de Piwigo';
+$lang['porg_issue_1911'] = 'Hay una inyección de SQL en la función \"profile\" de Piwigo';
+$lang['porg_issue_1914'] = '[13.7.0] Solicitud incorrecta (bad request) en la ordenación de álbumes';
