f05497964d1e905f3b8f5411af87d269b6c08cf8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-16 06:19:18 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/showcases.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_showcases_societe3vallees_title'] = 'Vereniging van de 3 valleien';
+$lang['porg_showcases_societe3vallees_desc'] = 'Het bedrijf van de 3 valleien, Meribel, Courchevel en Tania, koos Piwigo om zijn media te organiseren.';
+$lang['porg_showcases_pinnacle_title'] = 'De Pinnacle-galerijen';
+$lang['porg_showcases_pinnacle_desc'] = 'In deze fotogalerij presenteert Steve Raia zijn fotografisch werk en brengt hij zijn foto\'s op de markt.';
+$lang['porg_showcases_photolib_title'] = 'Fotolib';
+$lang['porg_showcases_photolib_desc'] = 'photolib.fr is een fotogalerij gemaakt met Piwigo die foto\'s presenteert onder de Creative Commons-licentie.';
+$lang['porg_showcases_paris1sorbonne_title'] = 'Universiteit Parijs 1 Panthéon Sorbonne';
+$lang['porg_showcases_paris1sorbonne_desc'] = 'De universiteit Paris 1 Panthéon-Sorbonne heeft Piwigo gekozen om haar grote fotobibliotheek te beheren en te organiseren.';
+$lang['porg_showcases_parcdeslandesdegascogne_title'] = 'Het Regionaal Natuurpark Landes de Gascogne.';
+$lang['porg_showcases_parcdeslandesdegascogne_desc'] = 'Het Regionaal Natuurpark Landes de Gascogne heeft Piwigo gekozen om zijn fotobibliotheek aan te maken en te beheren.';
+$lang['porg_showcases_oceanopolis_title'] = 'Oceanopolis Brest';
+$lang['porg_showcases_oceanopolis_desc'] = 'Océanpolis, het beroemde Bretonse oceaanontdekkingspark, gebruikt Piwigo om zijn fotobibliotheek en videotheek te beheren.';
+$lang['porg_showcases_nielfitzgerald_title'] = 'Neil Fitzgerald, fotograaf';
+$lang['porg_showcases_nielfitzgerald_desc'] = 'Neil Fitzgerald, professionele Nieuw-Zeelandse fotograaf gespecialiseerd in natuur- en natuurfotografie, koos Piwigo om zijn werk te verbeteren.';
+$lang['porg_showcases_milophotographie_title'] = 'Milo Fotografie';
+$lang['porg_showcases_milophotographie_desc'] = 'Maria, een professionele fotograaf in Parijs, gebruikt Piwigo om haar werk te delen.';
+$lang['porg_showcases_macrogallery_title'] = 'Macro-galerij';
+$lang['porg_showcases_macrogallery_desc'] = 'Een gepassioneerde amateur lethon presenteert in deze fototheek een bank met beelden over insecten, maar ook over spinnen en flora. De foto\'s zijn zorgvuldig geordend en geïndexeerd per familie, klasse, orde... een indrukwekkende klus.';
+$lang['porg_showcases_indre_title'] = 'Provincieraad van Indre.';
+$lang['porg_showcases_indre_desc'] = 'De provincieraad van Indre koos Piwigo om zijn oude fotobibliotheeksoftware te vervangen, die duurder en minder efficiënt was.';
+$lang['porg_showcases_icam_title'] = 'Institut catholique d\'arts et métiers';
+$lang['porg_showcases_icam_desc'] = 'ICAM, een Franse technische school, koos Piwigo om een fotobibliotheek te creëren die wordt gedeeld door zijn 11 campussen over de hele wereld';
+$lang['porg_showcases_hautegaronne_title'] = 'Provincieraad van de Haute-Garonne';
+$lang['porg_showcases_hautegaronne_desc'] = 'Het gemeentebestuur van Haute Garonne heeft zijn fotobibliotheek met Piwigo gemaakt.';
+$lang['porg_showcases_guillaumecyriaque_title'] = 'Guillaume Cyriaque, fotograaf';
+$lang['porg_showcases_guillaumecyriaque_desc'] = 'Guillaume Cyriaque, amateurfotograaf in België, koos Piwigo om zijn werk te verbeteren.';
+$lang['porg_showcases_gerarddubouloz_title'] = 'Directe foto\'s';
+$lang['porg_showcases_gerarddubouloz_desc'] = 'Instant Photos is de galerij van Gérard Dubouloz, fotograaf die verliefd is op de natuur en het buitenleven.';
+$lang['porg_showcases_ffn_title'] = 'Franse zwemfederatie';
+$lang['porg_showcases_ffn_desc'] = 'De Franse zwemfederatie heeft Piwigo uitgekozen om een grote hoeveelheid foto\'s en video\'s op te slaan en te delen';
+$lang['porg_showcases_ehcanada_title'] = 'Eh Canada';
+$lang['porg_showcases_ehcanada_desc'] = 'eh Canada, een toerismespecialist in Canada, herbergt duizenden foto\'s in zijn fotobibliotheek gemaakt met Piwigo.';
+$lang['porg_showcases_ect_title'] = 'ECT Groep';
+$lang['porg_showcases_ect_desc'] = 'ECT beheert haar beeldbank met Piwigo voor meer productiviteit en samenwerking.';
+$lang['porg_showcases_dongshaw_title'] = 'De kunst van Dong Shaw';
+$lang['porg_showcases_dongshaw_desc'] = 'Dong Shaw Hwei, schilder uit Taiwan, presenteert zijn werk in zijn Piwigo-galerij.';
+$lang['porg_showcases_arles_title'] = 'Stad Arles';
+$lang['porg_showcases_arles_desc'] = 'Het stadhuis van Arles publiceert op zijn Piwigo-galerij een selectie foto\'s die met afbeeldingen getuigen van de huidige gebeurtenissen in de stad.';
+$lang['porg_get_showcase_intro_title'] = 'Piwigo-gebruikers hebben talent!';
+$lang['porg_get_showcase_intro_paragraph'] = 'Ontdek op deze pagina enkele fotobibliotheken en openbare galerijen gemaakt met Piwigo.';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Reizen en toerisme';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Sport en vrije tijd';
+$lang['porg_get_showcase_filter_public_sector'] = 'Publieke sector';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Fotografen en particuliere gebruikers';
+$lang['porg_get_showcase_filter_companies'] = 'Bedrijven';
+$lang['porg_get_showcase_filter_all'] = 'Alle';
+$lang['page_meta_title'] = 'Piwigo-voorbeelden - Enkele fotogalerijen gemaakt met Piwigo';
+$lang['page_meta_description'] = 'Bedrijven, fotografen, publieke sector: onze gebruikers hebben talent! Ontdek voorbeelden van online fotobibliotheken gemaakt met Piwigo-software.';
+$lang['Visit Gallery'] = 'Bezoek Galerij';
