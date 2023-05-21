7b2878bce2dc026036c143a14f6c9d58c80a8104
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-21 21:32:14 +0200

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Coding activity'] = 'Codeer-activiteit';
+$lang['porg_codingactivity_intro'] = 'Meest recente commits in Piwigo core, plugins, thema\'s en andere tools. Deze pagina verzamelt data van verschillende code repositories en voorziet een "enkele plaats om te bekijken".';
