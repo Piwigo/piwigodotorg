77fcb66dd8ea945b03acfde557370a5468ff68aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-30 06:53:47 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/manual_update.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_update_info1'] = 'Sprawdź proszę bieżące <a href="%s">wymagania</a> Piwigo. Szczególnie jeżeli Twoja konfiguracja Php / MySql jest niewystarczająca, nie kontynuuj, bo aktualizacja się nie powiedzie.';
+$lang['porg_guides_manual_update_step1_list1'] = '<a href="%s">Pobierz archiwum z ostatnim wydaniem Piwigo</a>';
+$lang['porg_guides_manual_update_step4_paragraph3'] = 'Od teraz użytkownicy którejkolwiek wersji galerii, niebędący administratorami, powinni widzieć coś podobnego do: „Galeria jest zablokowana na czas obsługi. Proszę zajrzeć ponownie później”';
+$lang['porg_guides_manual_update_step5_list5'] = 'Szablon-wtyczka';
+$lang['porg_guides_manual_update_step5_list7'] = '_data';
+$lang['porg_guides_manual_update_step7_caption1'] = 'Uruchamianie strony aktualizacji. ';
+$lang['porg_guides_manual_update_step3_paragraph1'] = 'Jeżeli używasz Piwigo w wersji 2.1 lub wyższej, pomiń ten krok.';
+$lang['porg_guides_manual_update_step3_paragraph2'] = 'Zachowaj kopię zapasową tylko zmodyfikowanych przez ciebie lub wszystkich pików';
+$lang['porg_guides_manual_update_step5_list2'] = 'Wysyłanie';
+$lang['porg_guides_manual_update_step6'] = 'Krok 6 - przesyłanie FTP';
+$lang['porg_guides_manual_update_step6_paragraph2'] = 'Sprawdź czy twój klient FTP nie napotkał żadnych problemów.';
+$lang['porg_guides_manual_update_step7_paragraph1'] = 'Uruchamianie aktualizacji';
+$lang['porg_guides_manual_update_step8'] = 'Krok 8 - sprawdzenie wyników uaktualnienia';
+$lang['porg_guides_manual_update_step9'] = 'Krok 9 - odblokowanie galerii';
+$lang['porg_guides_manual_update_time_1'] = '3-5 min';
+$lang['porg_guides_manual_update_time_2'] = '5 min';
+$lang['porg_guides_manual_update_time_3'] = '1 min';
+$lang['porg_guides_manual_update_time_4'] = '2 min';
+$lang['porg_guides_manual_update_time_5'] = '15-120 min';
+$lang['porg_guides_manual_update_step5_list3'] = 'Wtyczki';
+$lang['porg_guides_manual_update_step7'] = 'Krok 7 - uaktualnienie bazy danych';
+$lang['porg_guides_manual_update_step4'] = 'Krok r - przygotowanie galerii';
+$lang['porg_guides_manual_update_step4_caption1'] = 'Zablokuj galerię';
+$lang['porg_guides_manual_update_step4_paragraph1'] = 'Zablokuj galerię';
+$lang['porg_guides_manual_update_step5'] = 'Krok 5 - porządkowanie';
+$lang['porg_guides_manual_update_step5_list1'] = 'Galerie';
+$lang['porg_guides_manual_update_step5_list4'] = 'Szablony wyglądu';
+$lang['porg_guides_manual_update_step5_list6'] = 'Lokalne';
+$lang['porg_guides_manual_update_paragraph1'] = 'Ta procedura jest odpowiednia dla wersji 1.4 lub wyższych. Dla starszych wersji poproś o pomoc na forum.';
+$lang['porg_guides_manual_update_step1'] = 'Krok 1 - przygotowanie pakietu';
+$lang['porg_guides_manual_update_step10'] = 'Krok 10 - porządki po uaktualnieniu';
+$lang['porg_guides_manual_update_step10_paragraph1'] = 'Nic';
+$lang['porg_guides_manual_update_step1_list2'] = 'Rozpakuj folder „piwigo” na swoim komputerze.';
+$lang['porg_guides_manual_update_step1_list3'] = 'Usuń domyślny folder „piwigo/lokal”.';
+$lang['porg_guides_manual_update_step1_paragraph1'] = 'Utwórz kopię zapasową obecnie używanych tabel';
+$lang['porg_guides_manual_update_step2'] = 'Krok 2 - przygotowanie bazy danych (zalecany)';
+$lang['porg_guides_manual_update_step2_caption1'] = 'Rekomendowane opcje PhpMyAdmin.';
+$lang['porg_guides_manual_update_step3'] = 'Krok 3 - przygotowanie serwera plików';
+$lang['porg_guides_manual_update_step3_list1'] = 'Pobierz rozszerzenie narzędzia Prepare 2.1 Upgrade';
+$lang['porg_guides_manual_update_step3_list2'] = 'wyodrębnij skrypt prep21up.php i przenieś go do folderu głównego swojej instalacji Piwigo';
+$lang['porg_guides_manual_update_step3_list3'] = 'otwórz prep21up.php w swojej przeglądarce internetowej http://example.com/photos/prep21up.php aby otrzymać archiwum „upgrade21.zip”';
+$lang['porg_guides_manual_update_step3_list4'] = 'Na swoim komputerze, rozpakuj folder „local” z „upgrade21.zip” do folderu „piwigo” (rozpakowane w kroku 1)';
