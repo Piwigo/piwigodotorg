1d4b05fb2a74ce63f2323aa16946275cb7e0d820
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-05 13:19:49 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_codingactivity_intro'] = 'Ultimio commiti in Piwigo core, plugins, themes ed altri tools. Questa pagina raccoglie dati da vari repository di codice per fornire un "unico luogo dove osservare".';
+$lang['Coding activity'] = 'Attività di programmazione.';
