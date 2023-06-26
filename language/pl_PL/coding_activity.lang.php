dc377d8675b41d05399ac625b7492ae112311744
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-26 06:55:41 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Coding activity'] = 'Aktywność programistyczna';
+$lang['porg_codingactivity_intro'] = 'Ostatnie zmiany w Piwigo, dodatkach, szablonach wyglądu i innych narzędziach. Ta strona zbiera dane z różnych repozytoriów, stając się „jedynym miejscem do sprawdzania”.';
