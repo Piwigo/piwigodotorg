bcd079ac346979920f40a1c9b5b2bfd1004f7e78
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-26 06:55:31 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/showcases.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_showcases_nielfitzgerald_title'] = 'Neil Fitzgerald, fotograf';
+$lang['Visit Gallery'] = 'Odwiedź galerię';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Sport i wypoczynek';
+$lang['porg_get_showcase_intro_title'] = 'Talenty użytkowników Piwigo';
+$lang['porg_showcases_arles_title'] = 'Miasto Arles';
+$lang['porg_showcases_dongshaw_title'] = 'Prace Dong Shawa';
+$lang['porg_showcases_ect_desc'] = 'ECT zarządza zdjęciami swoich banków w Piwigo zwiększając wydajność i współpracę.';
+$lang['porg_showcases_ect_title'] = 'ECT Group';
+$lang['porg_showcases_ehcanada_title'] = 'Eh, Kanada';
+$lang['porg_showcases_ffn_title'] = 'Francuska federacja pływacka';
+$lang['porg_showcases_gerarddubouloz_title'] = 'Instant Photos';
+$lang['porg_showcases_guillaumecyriaque_title'] = 'Guillaume Cyriaque, fotograf';
+$lang['porg_get_showcase_filter_all'] = 'Wszystkie';
+$lang['porg_get_showcase_filter_companies'] = 'Firmy';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Fotograficy i użytkownicy prywatni';
+$lang['porg_get_showcase_filter_public_sector'] = 'Sektor publiczny';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Podróże i turystyka';
