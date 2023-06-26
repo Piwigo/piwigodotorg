618ad3d25d2b717e152a1efe6ad1389c2dcb89ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-26 06:55:36 +0200

    Update pl_PL, thanks to yareq

--- language/pl_PL/manual_update.lang.php
+++ language/pl_PL/manual_update.lang.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 $lang['porg_guides_manual_update_info1'] = 'Sprawdź proszę bieżące <a href="%s">wymagania</a> Piwigo. Szczególnie jeżeli Twoja konfiguracja Php / MySql jest niewystarczająca, nie kontynuuj, bo aktualizacja się nie powiedzie.';
 $lang['porg_guides_manual_update_step1_list1'] = '<a href="%s">Pobierz archiwum z ostatnim wydaniem Piwigo</a>';
-$lang['porg_guides_manual_update_step4_paragraph3'] = 'Od teraz użytkownicy którejkolwiek wersji galerii, niebędący administratorami, powinni widzieć coś podobnego do: „Galeria jest zablokowana na czas obsługi. Proszę zajrzeć ponownie później”';
+$lang['porg_guides_manual_update_step4_paragraph3'] = 'Od teraz użytkownicy którejkolwiek wersji galerii, niebędący administratorami, powinni widzieć coś podobnego do: „Galeria jest zablokowana na czas konserwacji. Proszę zajrzeć ponownie później”';
 $lang['porg_guides_manual_update_step5_list5'] = 'Szablon-wtyczka';
 $lang['porg_guides_manual_update_step5_list7'] = '_data';
 $lang['porg_guides_manual_update_step7_caption1'] = 'Uruchamianie strony aktualizacji. ';
@@ -47,3 +47,21 @@ $lang['porg_guides_manual_update_step3_list1'] = 'Pobierz rozszerzenie narzędzi
 $lang['porg_guides_manual_update_step3_list2'] = 'wyodrębnij skrypt prep21up.php i przenieś go do folderu głównego swojej instalacji Piwigo';
 $lang['porg_guides_manual_update_step3_list3'] = 'otwórz prep21up.php w swojej przeglądarce internetowej http://example.com/photos/prep21up.php aby otrzymać archiwum „upgrade21.zip”';
 $lang['porg_guides_manual_update_step3_list4'] = 'Na swoim komputerze, rozpakuj folder „local” z „upgrade21.zip” do folderu „piwigo” (rozpakowane w kroku 1)';
+$lang['porg_guides_manual_update_info2'] = 'Nasza porada: wyłącz opcję „Extended inserts” lub zmniejsz wartość „Maximal length of created queries”.';
+$lang['porg_guides_manual_update_step10_paragraph2'] = 'Nic nie musi być usuwane po uaktualnieniu. Sam skrypt „upgrade.php” nie powinien być usunięty. Pamiętaj, że komunikat „Uaktualnienie nie jest potrzebne” i proces logowania zabezpieczają Twoją galerię.';
+$lang['porg_guides_manual_update_step1_paragraph3'] = 'W każdym przypadku upewnij się, że kopia zapasowa jest kompletna i jej tworzenie się zakończyło (wynik czasem jest niekompletny ze względu na ograniczenia serwera).';
+$lang['porg_guides_manual_update_step4_paragraph2'] = 'Dla Piwigo 2.3 lub wersji wcześniejszych: Konfiguracja -> Ogólne -> Zablokuj galerię. Dla Piwigo 2.4+: Narzędzia -> Konserwacja.';
+$lang['porg_guides_manual_update_step7_caption2'] = 'Uaktualnij stronę notatek.';
+$lang['porg_guides_manual_update_step8_paragraph4'] = 'Dla szablonów wyglądu, pozostaw stronę administracyjną otwartą w swojej przeglądarce, na wypadek konieczności przywrócenia po testach.';
+$lang['porg_guides_manual_update_step9_paragraph1'] = 'Zapoznaj się z nową wersją i nie zapomnij ponownie otworzyć galerii, aby przywrócić dostęp odwiedzającym.';
+$lang['porg_guides_manual_update_step8_paragraph2'] = 'Wyświetlona będzie lista wszystkich Twoich poprzednich wtyczek; funkcje niektórych z nich zostały dodane do podstawowych funkcji Piwigo (jak Plugins Manager), niektóre są teraz dostarczane (jak LocalFiles Editor), ale każda dezaktywacja wtyczki odbyła się z dobrego powodu.';
+$lang['porg_guides_manual_update_step1_paragraph2'] = 'Jeżeli używasz wersji z rodziny PhpWebGallery 1.7 lub późniejszej, możesz użyć wtyczki DB Backup. Użytkownicy poprzednich wersji powinni wykonać kopię zapasową innym sposobem, na przykład używając phpMyAdmin.';
+$lang['porg_guides_manual_update_step8_paragraph3'] = 'Spróbuj najpierw znaleźć uaktualnienie; zestawienie wtyczek może Ci w tym pomóc';
+$lang['porg_guides_manual_update_step5_paragraph1'] = 'Usuń wszystkie pliki z bieżącej instalacji Piwigo, z następującymi wyjątkami; NIE usuwaj tych folderów:';
+$lang['porg_guides_manual_update_step6_paragraph1'] = 'Użyj swojego standardowego klienta FTP do wysłania nowej wersji, to znaczy zawartości folderu „piwigo” (wyodrębnionego w pierwszym kroku i uaktualnionego w trzecim kroku), do folderu instalacyjnego poprzedniej wersji Piwigo.';
+$lang['porg_guides_manual_update_step7_paragraph2'] = 'W swojej przeglądarce internetowej otwórz skrypt „upgrade.php” i podążaj za wskazówkami, http://example.com/photos/upgrade.php';
+$lang['porg_guides_manual_update_step7_paragraph3'] = 'Aby zapobiec uaktualnieniu przez innego odwiedzającego, jesteś poproszony o zalogowanie się';
+$lang['porg_guides_manual_update_step7_paragraph4'] = 'Twoja poprzednia wersja wersja zostanie rozpoznana i otrzymasz podsumowanie operacji uaktualnienia;';
+$lang['porg_guides_manual_update_step7_paragraph5'] = 'Wtyczki aktywne w momencie aktualizacji zostaną zdezaktywowane, jako że mogą przestać działać w nowej wersji i mogą wymagać osobnego uaktualnienia.';
+$lang['porg_guides_manual_update_step8_paragraph1'] = 'Twoje pierwsze działania mogą wymagać czasu, jako że możesz nie być świadom/a wszystkich zmian';
+$lang['porg_guides_manual_update_step8_paragraph5'] = 'Nie zapominaj, że członkowie lub odwiedzający mogą mieć wybrane szablony wyglądu niezgodne z nową wersją. Może być lepiej ustawić im szablony na stronie administracyjnej. ';
