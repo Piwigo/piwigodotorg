2c8c21c288cd9b45457c1fcab94611541de4f921
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-27 13:38:00 +0200

    Update fr_CA, thanks to executive

---
+++ language/fr_CA/release-13.7.0.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1876'] = '[Page d\'historique] Une vulnérabilité d\'injection SQL a été trouvée dans la version 13.5.0';
+$lang['porg_issue_1878'] = '[Gestionnaire des utilisateurs] Le statut de l\'utilisateur n\'est pas traduit';
+$lang['porg_issue_1879'] = '[Mode sombre] Modification du style de l\'icône en mode sombre';
+$lang['porg_issue_1880'] = '[Gestionnaire des utilisateurs] L\'édition de l\'email bloque l\'édition de l\'utilisateur';
+$lang['porg_issue_1883'] = 'Édition des mots-clef : le bouton annuler ne fonctionne pas';
+$lang['porg_issue_1887'] = '[Edition d\'albums] La suppression d\'un album ne fonctionne pas';
+$lang['porg_issue_1901'] = '[Mode sombre] Mise à jour des couleurs';
+$lang['porg_issue_1903'] = '[Gestionnaire des utilisateurs] la fenêtre de confirmation pour la suppression d\'un utilisateur contient un texte non traduit';
+$lang['porg_issue_1905'] = '[Page d\'historique]  Les images sont rendues cliquables pour aller directement à leur page d\'édition';
+$lang['porg_issue_1907'] = '[da_DK] Manquaient les nouvelles de piwigo.org';
+$lang['porg_issue_1910'] = 'Il y a une injection SQL dans la fonction \"permalinks\" de Piwigo.';
+$lang['porg_issue_1911'] = 'Il y a une injection SQL dans la fonction \"profile\" de piwigo.';
+$lang['porg_issue_1914'] = 'Muvaise demande de tri d\'album';
