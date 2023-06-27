73a5d6c820864067d2be0b523e4f3f5fe3b45920
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-27 13:37:49 +0200

    Update cs_CZ, thanks to executive

---
+++ language/cs_CZ/release-13.7.0.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1901'] = '[Tmavé Zobrazení] Vylepšení barev';
+$lang['porg_issue_1907'] = '[da_DK] Na piwigo.org chybí zprávy';
+$lang['porg_issue_1914'] = 'Neplatný dotaz na řazení alba';
