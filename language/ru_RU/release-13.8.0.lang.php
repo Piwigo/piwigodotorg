a40ecf5505393dc1d9da396ff2a17a1df267c2d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:12:34 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1916'] = '[Менеджер альбомов] Действие сортировки вызывало ошибку';
+$lang['porg_issue_1925'] = '[Добавление фото] горизонтальный скролл на странице добавленных фото';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL Инъекция в поле "User-Agent"';
+$lang['porg_issue_1924'] = 'SQL Инъекция в функциональности \"Users\" в piwigo';
+$lang['porg_issue_1914'] = '[13.7.0] bad request в сортировке альбома';
