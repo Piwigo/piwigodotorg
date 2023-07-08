1aef418ca1b6a4002ed065de021349de8cba107c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:17:46 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = 'Отсутствует перевод на странице обновления';
+$lang['porg_issue_1746'] = 'Warning: Cannot modify header information - headers already sent in /var/www/piwigo/action.php on line 228';
+$lang['porg_issue_1747'] = '[Страница истории] Удалены теги {icon} в деталях.';
+$lang['porg_issue_1750'] = 'best_rated/created- ведет к \"Piwigo обнаружил неисправимую ошибку\"';
+$lang['porg_issue_1753'] = 'синтаксическая ошибка, неожиданное \')\' в include/ws_functions/pwg.php, вызванное запятыми в конце';
+$lang['porg_issue_1755'] = 'Предупреждения Php8 о связанных альбомах';
+$lang['porg_issue_1756'] = 'Комментарии по поводу проблем с картинками в php8';
+$lang['porg_issue_1758'] = '[Проверить обновление] Нет ссылки на страницу обновления после проверки обновления';
+$lang['porg_issue_1759'] = '[PHP 8.1] фатальная ошибка на странице конфигурации';
