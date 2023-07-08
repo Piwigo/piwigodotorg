bf1cc52d7e12f7e6a48b6acd99cca84a4e459a96
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:30:02 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Coding activity'] = 'Кодинг';
+$lang['porg_codingactivity_intro'] = 'Последние коммиты в ядре Piwigo, плагинах, темах и других инструментах. Эта страница собирает данные из различных репозиториев кода, чтобы предоставить «одно место для просмотра».';
