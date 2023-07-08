760e224dde765d1aef8e64ba839adaca7de679f2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:12:39 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL-injectie in "User-Agent"';
+$lang['porg_issue_1925'] = '[Foto\'s toevoegen] Horizontaal scrollen op de pagina met toegevoegde foto\'s';
+$lang['porg_issue_1924'] = '[SQL-injectie in de functie \"Gebruikers\" van piwigo';
+$lang['porg_issue_1916'] = '[Albumbeheer] Sorteeractie activeert een fout';
+$lang['porg_issue_1914'] = '[13.7.0] slecht verzoek bij het sorteren van albums';
