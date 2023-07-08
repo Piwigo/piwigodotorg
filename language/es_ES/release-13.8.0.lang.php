085a2e7a0e6e5224e01b2d4e5cfc8e992ac66cfe
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 18:19:50 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0] Petición in correcta en la ordenación de álbum';
+$lang['porg_issue_1916'] = '[Gestor de álbumes] La acción de ordenar provoca un error';
+$lang['porg_issue_1924'] = 'Inyección SQL en la función \"Usuarios\" de Piwigo';
+$lang['porg_issue_1925'] = '[Añadir fotos] Añadido desplazamiento horizontal en la página de fotos añadidas';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'Inyección SQL en "User-Agent"';
