f629dc24eb99f0cff3da5cd479e2b3eabf9317c3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:28:23 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/showcases.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Visit Gallery'] = 'Посетите галерею';
+$lang['page_meta_description'] = 'Компании, фотографы, публичный сектор: у наших пользователей есть талант! Откройте для себя примеры онлайн-библиотек фотографий, созданных с помощью программного обеспечения Piwigo.';
+$lang['page_meta_title'] = 'Примеры Piwigo - некоторые фотогалереи, созданные с помощью Piwigo';
+$lang['porg_get_showcase_filter_all'] = 'Все';
+$lang['porg_get_showcase_filter_companies'] = 'Компании';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Фотографы и частные пользователи';
+$lang['porg_get_showcase_filter_public_sector'] = 'Публичный сектор';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Спорт и хобби';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Путешествия и туризм';
+$lang['porg_get_showcase_intro_paragraph'] = 'Откройте для себя на этой странице несколько фотобиблиотек и общедоступных галерей, созданных с помощью Piwigo.';
+$lang['porg_get_showcase_intro_title'] = 'У пользователей Piwigo есть талант!';
+$lang['porg_showcases_arles_desc'] = 'Мэрия Арля публикует в своей галерее Piwigo подборку фотографий, которые вместе с изображениями свидетельствуют о текущих событиях в городе.';
+$lang['porg_showcases_arles_title'] = 'Город Арль';
+$lang['porg_showcases_dongshaw_desc'] = 'Dong Shaw Hwei, художник из Тайваня, представляет свои работы в своей галерее Piwigo.';
+$lang['porg_showcases_dongshaw_title'] = 'Искусство Dong Shaw';
+$lang['porg_showcases_ect_desc'] = 'ECT управляет своим банком изображений с помощью Piwigo для повышения производительности и совместной работы.';
+$lang['porg_showcases_ect_title'] = 'ECT Group';
+$lang['porg_showcases_ehcanada_desc'] = 'Eh Canada, специалист по туризму в Канаде, хранит тысячи фотографий в своей фотобиблиотеке, созданной с помощью Piwigo.';
+$lang['porg_showcases_ehcanada_title'] = 'Eh Canada';
+$lang['porg_showcases_ffn_desc'] = 'Французская федерация плавания выбрала Piwigo для хранения и обмена большим объемом фотографий и видео';
+$lang['porg_showcases_ffn_title'] = 'Французская федерация плавания';
+$lang['porg_showcases_gerarddubouloz_desc'] = 'Instant Photos — это галерея Жерара Дюбуло, фотографа, влюбленного в природу и отдых на свежем воздухе.';
+$lang['porg_showcases_gerarddubouloz_title'] = 'Мгновенные фотографии';
+$lang['porg_showcases_guillaumecyriaque_desc'] = 'Guillaume Cyriaque, фотограф-любитель из Бельгии выбрал Piwigo для улучшения своей работы';
+$lang['porg_showcases_guillaumecyriaque_title'] = 'Guillaume Cyriaque, фотограф';
+$lang['porg_showcases_hautegaronne_desc'] = 'Совет графства Верхняя Гаронна создал свою фотобиблиотеку с помощью Piwigo.';
+$lang['porg_showcases_hautegaronne_title'] = 'Совет графства Верхняя Гаронна';
+$lang['porg_showcases_icam_desc'] = 'ICAM, французская инженерная школа, выбрала Piwigo для создания библиотеки фотографий, которой будут пользоваться ее 11 кампусов по всему миру.';
+$lang['porg_showcases_icam_title'] = 'Institut catholique d’arts et métiers';
+$lang['porg_showcases_indre_desc'] = 'Совет округа Индре выбрал Piwigo для замены своего старого программного обеспечения для библиотеки фотографий, которое было более дорогим и менее эффективным.';
+$lang['porg_showcases_indre_title'] = 'Совет графства Индре';
+$lang['porg_showcases_macrogallery_desc'] = 'Страстный любитель представляет в этой фотобиблиотеке банк изображений насекомых, а также пауков и растений. Фотографии тщательно классифицированы и проиндексированы по семьям, классам, порядкам... впечатляющая работа.';
+$lang['porg_showcases_macrogallery_title'] = 'Macro Gallery';
+$lang['porg_showcases_milophotographie_desc'] = 'Мария, профессиональный фотограф из Парижа, использует Piwigo для публикации своих работ.';
+$lang['porg_showcases_milophotographie_title'] = 'Milo фотографии';
+$lang['porg_showcases_nielfitzgerald_desc'] = 'Neil Fitzgerald, профессиональный новозеландский фотограф, специализирующийся на съемке природы и дикой природы, выбрал Piwigo для улучшения своей работы.';
+$lang['porg_showcases_nielfitzgerald_title'] = 'Neil Fitzgerald, фотограф';
+$lang['porg_showcases_oceanopolis_desc'] = 'Océanpolis, знаменитый бретонский океанический парк, использует Piwigo для управления своей фото- и видеотекой.';
+$lang['porg_showcases_oceanopolis_title'] = 'Océanopolis Brest';
+$lang['porg_showcases_parcdeslandesdegascogne_desc'] = 'The Regional Natural Park Landes de Gascogne выбрал Piwigo для создания своей фотобиблиотеки и управления ею.';
+$lang['porg_showcases_parcdeslandesdegascogne_title'] = 'The Regional Natural Park Landes de Gascogne';
+$lang['porg_showcases_paris1sorbonne_desc'] = 'University Paris 1 Panthéon Sorbonne выбрал Piwigo для управления и организации своей большой фототеки.';
+$lang['porg_showcases_paris1sorbonne_title'] = 'University Paris 1 Panthéon Sorbonne';
+$lang['porg_showcases_photolib_desc'] = 'photolib.fr - это фотогалерея, созданная с помощью Piwigo, в которой представлены фотографии под лицензией Creative Commons.';
+$lang['porg_showcases_photolib_title'] = 'Photolib';
+$lang['porg_showcases_pinnacle_desc'] = 'В этой фотогалерее Steve Raia представляет свои фотоработы и продает свои фотографии.';
+$lang['porg_showcases_pinnacle_title'] = 'The Pinnaccle Galleries';
+$lang['porg_showcases_societe3vallees_desc'] = 'he société des 3 vallées, Meribel, Courchevel and Tania, выбрала Piwigo для организации своих СМИ. ';
+$lang['porg_showcases_societe3vallees_title'] = 'Société des 3 vallées';
