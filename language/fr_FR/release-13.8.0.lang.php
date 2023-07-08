e96ec53a6a14edbc6fa453b2e83a4bf36b8bb968
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:12:44 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0] Mauvaise requête sur le tri des albums';
+$lang['porg_issue_1916'] = '[Gestionnaire d\'albums] L\'action de tri déclenche une erreur';
+$lang['porg_issue_1924'] = 'Injection SQL dans la fonction \"Users\" de piwigo';
+$lang['porg_issue_1925'] = '[Ajouter des photos] Défilement horizontal sur la page des photos ajoutées';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'Injection SQL dans "User-Agent"';
