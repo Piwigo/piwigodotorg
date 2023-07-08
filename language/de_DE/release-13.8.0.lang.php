51be0dff9089d7d20c9efc04939f910ff20b827c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:12:48 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0] Fehlerhafte Anfrage beim Sortieren von Alben';
+$lang['porg_issue_1916'] = '[Album Manager] Sortieraktion löst einen Fehler aus';
+$lang['porg_issue_1924'] = 'SQL Injection in der Funktion \"Users\" von piwigo';
+$lang['porg_issue_1925'] = '[Fotos hinzufügen] Hozizontales Scrollen auf der Seite für hinzugefügte Fotos';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL-Einschleusung in "User-Agent"';
