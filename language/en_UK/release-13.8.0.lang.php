901d03341da983595e5ede27b729f8f39e94d919
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-09 07:28:54 +0200

    Update en_UK, thanks to Kaare

--- language/en_UK/release-13.8.0.lang.php
+++ language/en_UK/release-13.8.0.lang.php
@@ -1,7 +1,6 @@
 <?php
 $lang['porg_issue_1925'] = '[Add photos] Hozizontal scroll on added photos page';
-$lang['porg_issue_1924'] = 'SQL Injection in the \"Users\" function of piwigo';
+$lang['porg_issue_1924'] = 'SQL Injection in the "Users" function of Piwigo';
 $lang['porg_issue_1916'] = '[Album Manager] Sort action triggers an error';
-$lang['porg_issue_1914'] = '[13.7.0] bad request on album sort';
+$lang['porg_issue_1914'] = '[13.7.0] Bad request on album sort';
 $lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL Injection in "User-Agent"';
-?>
