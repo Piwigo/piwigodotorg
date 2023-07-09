9906d4e7f859030eeac20fdfb8b5c7dd1844c6a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-09 07:29:00 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0] <q>bad request</q> ved sortering af album';
+$lang['porg_issue_1916'] = '[Albumhåndtering] Sorteringshandling udløser en fejl';
+$lang['porg_issue_1924'] = 'SQL-indsprøjtning i Piwigos <q>Users</q>-funktion.';
+$lang['porg_issue_1925'] = '[Tilføj fotografier] Horisontal scrolling på siden med tilføjede fotografier';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL-indsprøjtning i <q>User-Agent</q>';
