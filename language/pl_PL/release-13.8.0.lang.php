74990bb55a9c83ed53f515d382e0fee90829a8f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-10 08:29:11 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1916'] = '[Menedżer albumu] Wywołanie sortowania wyzwala błąd';
+$lang['porg_issue_1924'] = 'Iniekcja SQL w funkcji „Users” Piwigo';
+$lang['porg_issue_1925'] = '[Dodawanie zdjęć] Przewijanie poziome na stronie dodanych zdjęć';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'Iniekcja SQL w „User-Agent”';
+$lang['porg_issue_1914'] = '[13.7.0] Nieprawidłowe żądanie dla sortowania albumu';
