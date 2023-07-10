31a8c622f3a937e3f335e66bed480f10d5bbf93f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-10 17:30:03 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0]Solicitação incorreta na classificação do álbum';
+$lang['porg_issue_1916'] = '[Gerenciador de Álbuns] A ação de classificação aciona um erro';
+$lang['porg_issue_1924'] = 'Injeção SQL na função "Usuáerios" do Piwigo';
+$lang['porg_issue_1925'] = '[Adicionar fotos] Rolagem horizontal na página de fotos adicionadas';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL Injection em "Agente-Usuário"';
