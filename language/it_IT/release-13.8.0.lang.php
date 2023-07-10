3a6e4838eda834def42b2e826131e56c508ccc39
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-10 14:09:57 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0] Richiesta errata sull\'ordinamento dell\'album.';
+$lang['porg_issue_1916'] = '[Gestione album] L\'ordinamento causa un errore.';
+$lang['porg_issue_1924'] = 'SQL Injection nella funzione "Utenti" di Piwigo.';
+$lang['porg_issue_1925'] = '[Aggiungi foto] Scorrimento orizzontale nella pagina delle foto aggiunte.';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = 'SQL Injection in "User-Agent".';
