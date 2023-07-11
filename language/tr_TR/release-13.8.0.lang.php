51dae1e29fa7ae494f87f10ec3899bbf04472cad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-11 06:34:36 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1925'] = '[Fotoğraf ekle] Eklenen fotoğraflar sayfasında yatay kaydırma';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = '"User-Agent" içinde SQL enjeksiyonu';
+$lang['porg_issue_1924'] = 'Piwigo\'nun "Kullanıcılar" işlevinde SQL Enjeksiyonu';
+$lang['porg_issue_1916'] = '[Albüm Yöneticisi] Sıralama eylemi bir hata tetikler';
+$lang['porg_issue_1914'] = '[13.7.0] Albüm sıralamada hatalı istek';
