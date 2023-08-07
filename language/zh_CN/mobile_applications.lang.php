f8426bf87662aeab05da431ffecd6bd827294342
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-08-07 08:27:31 +0200

    Update zh_CN, thanks to yidong

---
+++ language/zh_CN/mobile_applications.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['mobile_applications_albums_texte'] = 'Piwigo移动端同样具备相册的全部使用权限，你可以新增，修改，重命名，你也可以重新定义相册的名字，描述，缩略图。';
+$lang['mobile_applications_albums_title'] = '相册可以自动分类';
+$lang['mobile_applications_confidentiality_texte'] = '你的数据是安全的，不会被追踪，分享或是用于算法。';
+$lang['mobile_applications_confidentiality_title'] = '隐私';
+$lang['mobile_applications_control_texte'] = '在分享时可以指定是否分享元数据';
+$lang['mobile_applications_control_title'] = '数据控制';
+$lang['mobile_applications_copy_move_texte'] = '使用Piwigo的苹果移动端，可以轻松整理图片，改变图片的相册保存位置，或是从一个相册复制到另一个相册。';
+$lang['mobile_applications_copy_move_title'] = '复制和移动图片和视频';
+$lang['mobile_applications_download'] = '下载Piwigo客户端';
+$lang['mobile_applications_download_app_store1'] = '下载安卓版';
+$lang['mobile_applications_download_app_store2'] = '通过PlayStore下载应用';
+$lang['mobile_applications_download_play_store1'] = '下载苹果端';
+$lang['mobile_applications_download_play_store2'] = '通过苹果appStore下载应用';
+$lang['mobile_applications_follows_you_texte'] = '适用Piwigo移动端可以在任何时候进入图片相册，却不需要把所有文件都保存在您的手机里面，包括所有的相册，图片，视频。';
+$lang['mobile_applications_follows_you_title'] = '你的图片库将跟随你到任何地方';
+$lang['mobile_applications_geolocalisation_texte'] = '自动检索图片GPS定位功能可以让你方便的在地图上查看您的相册';
+$lang['mobile_applications_geolocalisation_title'] = '使用地理标记来标注图片';
+$lang['mobile_applications_import_options_title'] = '导入图片的配置';
+$lang['mobile_applications_import_text'] = '使用Piwigo的移动端应用，可以在任何时候通过手机添加图片，新增相册或是放入现有相册，也可通过VideoJS导入视频。';
+$lang['ios_only'] = '仅适用于苹果端';
+$lang['mobile_applications_introduction'] = 'Piwigo也是一个免费的移动端图片管理系统，可以使用利用很多的功能。';
+$lang['mobile_applications_scan_qr'] = '使用手机扫描二维码';
+$lang['mobile_applications_size_quality_title'] = '尺寸大小和分辨率';
+$lang['mobile_applications_metadata_texte'] = '可以查看所有的元数据并且修改，包括标题，作者，日期，隐私，标签，描述等。';
+$lang['mobile_applications_invite_texte'] = 'Piwigo移动端也是对外兼容管理的，可以允许非管理员用户发送图片资源到系统里面。';
+$lang['mobile_applications_user_rights_texte'] = '在Piwigo系统的授权同样适用于移动端，每个用户可以进入自己被授权的相册。';
+$lang['mobile_applications_sort_photos_title'] = '精细化管理图片';
+$lang['mobile_applications_size_quality_texte'] = '可以在服务器上修改您所期望的图片尺寸大小和分辨率。';
+$lang['mobile_applications_sort_photos_texte'] = '你可以通过Piwigo移动端操作所有的图片详情，包括修改标题，描述，标签，作者。。。你也可以把图片进行归类，下载到你的手机，或者是以后删除。';
+$lang['mobile_applications_title'] = 'Piwigo移动端包括苹果版和安卓版';
+$lang['mobile_applications_user_rights_title'] = '用户权利';
+$lang['mobile_applications_share_title'] = '分享图片和视频';
+$lang['mobile_applications_share_texte'] = '使用分享功能，从Piwigo移动端直接分享发送图片给您的好友';
+$lang['mobile_applications_security_title'] = '安全和隐私';
+$lang['mobile_applications_metadata_title'] = '元数据';
+$lang['mobile_applications_invite_title'] = '邀请您的同事';
+$lang['mobile_applications_import_title'] = '导入你的相片和视频';
