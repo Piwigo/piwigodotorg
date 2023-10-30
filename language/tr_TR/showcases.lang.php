d76c097aa6839b78e266621cb513f897858c3cbf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-30 07:28:28 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/showcases.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Visit Gallery'] = 'Galeriyi ziyaret et';
+$lang['porg_get_showcase_filter_all'] = 'Hepsi';
+$lang['porg_get_showcase_filter_companies'] = 'Şirketler';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = 'Fotoğrafçılar ve özel kullanıcılar';
+$lang['porg_get_showcase_filter_public_sector'] = 'Kamu sektörü';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = 'Sporlar ve hobiler';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = 'Gezi ve Turizm';
