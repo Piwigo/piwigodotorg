a057bb56cd7b86cfee87d81ff3a49fe6dfac553b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-31 06:39:11 +0100

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/release-13.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1876'] = '[Página história] Ocorrência Injeção SQL encontrada em 13-5-o';
+$lang['porg_issue_1878'] = '[Administrar utilizador] O status do utilizador não foi traduzido';
+$lang['porg_issue_1879'] = '[Modo escuro] Alterar o estilo do icon no modo escuro';
+$lang['porg_issue_1901'] = '[Modo escuro] Atualizar cores';
