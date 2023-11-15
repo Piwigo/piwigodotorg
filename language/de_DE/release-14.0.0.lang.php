2bd49709a116f68475f581b5d93df98f8be8a4a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-15 16:20:42 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-14.0.0.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v14_newfiles_title_menu'] = 'HEIC-, WebP-, PSD- und EPS-Unterstützung';
+$lang['porg_v14_notificationgroup_title'] = 'Benachrichtigungsgruppe';
+$lang['porg_v14_intro1'] = 'Suche und darüber hinaus';
+$lang['porg_v14_mimetypes_title'] = 'Mimetypen-Symbole';
+$lang['porg_v14_newfiles_title'] = 'Neue Dateitypen werden unterstützt';
+$lang['porg_v14_badge_refresh'] = 'Aktualisierung';
+$lang['porg_v14_duplicates_title'] = 'Vermeiden Sie Duplikate';
+$lang['porg_v14_albumeditor_title'] = 'Album-Editor';
+$lang['porg_v14_badge_new'] = 'Neu';
+$lang['porg_v14_badge_redesign'] = 'Neugestaltung';
+$lang['porg_v13_technical_features13'] = 'Berechnen Sie Waisen';
+$lang['porg_v13_technical_features15'] = 'API-Änderungen';
+$lang['porg_v13_technical_features4'] = 'Konfigurationseinstellung für url_port';
+$lang['porg_v13_technical_features6'] = 'Kompatibilität mit PHP 8.2';
+$lang['porg_v14_mimetypes_image1_label'] = 'Die Mimetype-Icons, die von Piwigo angezeigt werden, wenn kein Poster verfügbar ist, erhalten ein Update.';
+$lang['porg_v14_search_image1_label'] = 'Diese neue Filterleiste für die Suche ersetzt sowohl die alte „Schnellsuche“ im Menü als auch die Seite „Erweiterte Suche“.';
+$lang['porg_v14_search_title'] = 'Suchmaschine';
+$lang['porg_v13_api_s3'] = 'Neuer Parameter <code>is_download</code>';
+$lang['porg_v13_api_s4'] = 'Neuer Parameter <code>search</code>';
+$lang['porg_v13_api_s5'] = 'neue Parameter';
+$lang['porg_v14_storagetooltips_image1_label'] = 'Schöne Details an der Ablageleiste im Armaturenbrett. Für jede „Kategorie“ von Dateien teilt Ihnen Piwigo mit, wie viele Dateien für jede Dateierweiterung vorhanden sind und welche Dateigröße sie auf Ihrer Speicherkapazität beanspruchen.';
+$lang['porg_v14_storagetooltips_title'] = 'Speicherdetails';
+$lang['porg_v14_technical_features1'] = 'Bitte beachten Sie, dass eine ausführlichere Dokumentation zu verfügbar ist';
+$lang['porg_v14_technical_features2'] = 'Technische Änderungen in Piwigo 14';
+$lang['porg_v14_technical_features3'] = 'Bitte lesen Sie es, um Ihre Erweiterungen mit Piwigo 14 kompatibel zu machen.';
+$lang['porg_v14_search_text'] = 'Die neue Suchmaschine in der Galerie ist sicherlich die größte Weiterentwicklung von Piwigo 14. Anstelle einer Formularseite und dann einer Ergebnisseite zeigt die neue „Multikriterien“-Suche gleichzeitig Suchkriterien und Ergebnisse an. Im Moment können Sie nach Alben, Tags filtern, nach Wort suchen, hinzugefügt von, Autor, Veröffentlichungsdatum und Dateitypen. Neue Filter werden in zukünftigen Versionen verfügbar sein.';
+$lang['porg_v14_search_image3_label'] = '<b>In diesem Satz suchen</b>: Beim Durchsuchen eines Albums, eines Tags oder einer Kombination von Tags stellt Piwigo die Schaltfläche und das Symbol „In diesem Satz suchen“ zur Verfügung. Wenn Sie darauf klicken, erstellen Sie eine neue Suche, die vorab auf das Album (oder die Liste der Tags) gefiltert ist, und Sie können Filter hinzufügen. Wenn Sie beispielsweise im Album „Tier/Füchse“ nach Fotos suchen möchten, die im Jahr 2018 hinzugefügt wurden, ist das ganz einfach.';
+$lang['porg_v14_search_image2_label'] = 'Wenn Sie nach dem Wort „Panda“ suchen, findet Piwigo möglicherweise Übereinstimmungen zwischen Tags oder Alben. Piwigo zeigt die Liste an und ermöglicht Ihnen das direkte Öffnen des Tags oder Albums außerhalb der Suche.';
+$lang['porg_v14_notificationgroup_image1_label'] = 'Wenn Sie viele Administratoren haben, aber nicht alle benachrichtigt werden sollen, wenn sich ein neuer Benutzer anmeldet, können Sie nach einer Gruppe von Benutzern filtern.';
+$lang['porg_v14_albumeditor_image1_label'] = 'Neuer Album-Editor: bessere Raumnutzung, konsistentere Anzeige auf kleinen und großen Bildschirmen, feste Position der Schaltfläche bestätigen (unten rechts).';
+$lang['porg_v14_alice_text2'] = 'Manchmal trage ich auch meinen Grafikdesigner-Hut, um Visuals aller Art zu erstellen, um die verschiedenen Websites und Kommunikationsinitiativen von Piwigo zu veranschaulichen. Wie zum Beispiel das Banner für Version 14!';
+$lang['porg_v14_intro3'] = 'Piwigo 14 bietet außerdem die mit Spannung erwartete Unterstützung für neue Bilddateitypen wie HEIC, Kompatibilität mit PHP 8.2, einen neuen Album-Editor und Geschwindigkeitsverbesserungen.';
+$lang['porg_v14_newfiles_text'] = '<b>HEIC</b>: Dies ist derzeit der Standarddateityp, der für Fotos auf iOS- und Android-Telefonen verwendet wird. Piwigo generiert eine JPEG-Datei zur Anzeige und behält die HEIC als Originaldatei bei.<br>
+<br>
+<b>WebP</b>: Der am häufigsten von Webbrowsern unterstützte Dateityp als JPEG-Ersatz. Piwigo generiert WebP-Dateien für die Anzeige in mehreren Größen. Dies ist ein großer Unterschied zu anderen Dateitypen, die nur „als Eingabe“ unterstützt werden. WebP wird sowohl für den Import als auch für die Anzeige unterstützt. Das Interessante an WebP ist, dass es JPEG mit höherer Qualität bei gleichem Dateigewicht ersetzen kann, animiertes GIF bei viel geringerem Gewicht ersetzen kann und auch PNG mit verlustfreier Qualität und Transparenz ersetzen kann.<br>
+<br>
+<b>PSD</b> (Photoshop) und <b>EPS</b>: Piwigo generiert ein JPEG oder PNG zur Anzeige und behält das PSD/EPS als Originaldatei.';
+$lang['porg_v14_claire_text1'] = 'Mein Name ist Claire und bei Piwigo bin ich für Marketing, Kommunikation, Dokumentation und vieles mehr zuständig! Ich liebe es, mit Benutzern zu sprechen, um ihre Erwartungen und die Art und Weise, wie sie Piwigo verwenden, besser zu verstehen.';
+$lang['porg_v14_claire_text2'] = 'Für diese Piwigo 14-Version habe ich an einem Benchmark verschiedener Such- und Filterlösungen gearbeitet und mich dabei von Fotobibliothekstools, aber auch E-Commerce-Sites, CRM-Software usw. inspirieren lassen. Mein Ziel war es, den Entwicklungs- und Designteams Quellen bereitzustellen Inspiration zur Verbesserung der Such- und Filterfunktionen in Piwigo-Galerien.';
+$lang['porg_v14_intro2'] = 'Die Suchmaschine in der Galerie erhält ein tiefgreifendes Redesign. Unsere Umfragen haben gezeigt, dass Piwigo gut darin ist, Fotos unter Tausenden zu finden. Lassen Sie uns Piwigo bei dieser Aufgabe perfekt machen! Aus diesem Grund haben wir das Benutzererlebnis von Grund auf überarbeitet.';
+$lang['porg_v14_duplicates_text'] = 'Beim Hochladen erkennt Piwigo, ob dieselbe Datei bereits zuvor hochgeladen wurde. Wenn eine Datei gefunden wird, verwendet Piwigo einfach das vorhandene Foto und verknüpft es mit dem Upload-Album, ohne die Datei zu duplizieren.<br>
+<br>
+Diese Option ist standardmäßig für alle Neuinstallationen aktiviert, wird jedoch für aktualisierte Installationen deaktiviert, um eine Änderung des Upload-Verhaltens zu vermeiden. Natürlich können Sie diese Einstellung auf der Seite [Administration &gt;] ändern. Konfiguration &gt; Optionen].';
+$lang['porg_v13_technical_features14'] = 'In großen Galerien mit mehr als 100.000 Fotos kann es mehrere Sekunden dauern, die Waisenkinder zu finden. Piwigo 14 verkürzt die Zeit zum Auffinden von Waisenkindern und verwendet einen Cache, um diese Suche viel seltener durchzuführen.';
+$lang['porg_v13_technical_features5'] = 'Neue erweiterte Konfigurationseinstellung <code>$conf[\'url_port\']</code>, sehr nützlich bei exotischen Reverse-Proxy-Umgebungen.';
+$lang['porg_v13_technical_features7'] = 'PHP 8.2 ist auf vielen Servern schnell angekommen und Piwigo musste kompatibel sein! Aktualisierte Bibliotheken: Smarty 4.3, PHPMailer 6.8.0, benutzerdefinierte CSSmin-Änderung.';
+$lang['porg_v14_alice_text1'] = 'Mein Name ist Alice und ich bin seit Juni 2022 UX/UI-Designerin bei Piwigo. Hinter diesem kuriosen Akronym verbirgt sich ein faszinierender Job: die Erstellung ergonomischer, intuitiver und benutzerfreundlicher Oberflächen für die gesamte Anwendung. Mein Hauptziel ist es, dass Sie jeden Tag Freude an der Nutzung von Piwigo haben!';
+$lang['porg_v13_api_s1'] = 'Fotos verknüpfen/trennen/verschieben';
+$lang['porg_v13_api_s2'] = 'Erstellen Sie eine neue Suche';
+$lang['Technical features'] = 'Technische Funktionen';
+$lang['The menu'] = 'Das Menü';
+$lang['Interview %s'] = 'Interview mit %s';
+$lang['New search engine, new album editor, new file types.'] = 'Neue Suchmaschine, neuer Album-Editor, neue Dateitypen.';
