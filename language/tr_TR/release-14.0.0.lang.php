6223a5dd2c842d6a672f7748cd634ddebcf3607c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-16 10:10:18 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/release-14.0.0.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v14_search_text'] = 'Galerideki yeni arama motoru kesinlikle Piwigo 14\'ün getirdiği en büyük evrimdir. Bir form sayfası ve ardından bir sonuç sayfası yerine, yeni "çok kriterli" arama, arama kriterlerini ve sonuçları aynı anda görüntüler. Şimdilik, albümleri, etiketleri, kelimeye göre arama yapabilir, ekleyen, yazar, gönderi tarihi ve dosya türlerini filtreleyebilirsiniz. Gelecek sürümlerde yeni filtreler gelecek.';
+$lang['porg_v14_search_title'] = 'Arama motoru';
+$lang['porg_v14_storagetooltips_image1_label'] = 'Pano depolama çubuğunda güzel görünümlü ayrıntılar. Piwigo, her bir dosya "kategorisi" için, her bir dosya uzantısı için kaç dosya olduğunu ve bunların depolama kapasitenizde ne kadar dosya kapladığını söyler.';
+$lang['porg_v14_storagetooltips_title'] = 'Depolama ayrıntıları';
+$lang['porg_v14_technical_features1'] = 'Lütfen aşağıdakiler hakkında daha ayrıntılı belgelerin mevcut olduğunu unutmayın:';
+$lang['porg_v14_technical_features2'] = 'Piwigo 14\'teki teknik değişiklikler';
+$lang['porg_v14_technical_features3'] = 'Uzantılarınızı Piwigo 14 ile uyumlu hale getirmek için lütfen okuyun.';
+$lang['porg_v14_badge_new'] = 'yeni';
+$lang['porg_v14_badge_redesign'] = 'yeniden tasarlama';
+$lang['porg_v14_badge_refresh'] = 'yenileme';
+$lang['porg_v14_claire_text1'] = 'Benim adım Claire ve Piwigo\'da pazarlama, iletişim, dokümantasyon ve çok daha fazlasından sorumluyum! Beklentilerini ve Piwigo\'yu nasıl kullandıklarını daha iyi anlamak için kullanıcılarla konuşmayı seviyorum.';
+$lang['porg_v14_claire_text2'] = 'Bu Piwigo 14 sürümü için, fotoğraf kitaplığı araçlarından ve aynı zamanda e-ticaret sitelerinden, CRM yazılımlarından ilham alarak farklı arama ve filtreleme çözümlerinin bir kıyaslaması üzerinde çalıştım... Amacım, geliştirme ve tasarım ekiplerine Piwigo galerilerindeki arama ve filtreleme özelliklerini geliştirmeleri için ilham kaynağı sağlamaktı.';
+$lang['porg_v14_duplicates_text'] = 'Yükleme sırasında Piwigo, aynı dosyanın daha önce yüklenip yüklenmediğini tespit edecektir. Eğer bir dosya bulunursa, Piwigo sadece mevcut fotoğrafı kullanır ve dosyayı çoğaltmadan yükleme albümüyle ilişkilendirir. <br>
+<br>
+Bu seçenek tüm yeni yüklemeler için varsayılan olarak etkinleştirilecek, ancak yükleme davranışının değişmesini önlemek için güncellenmiş yüklemeler için devre dışı bırakılacaktır. Elbette, bu ayarı [Yönetim &gt; Yapılandırma &gt; Seçenekleri] sayfasında değiştirebilirsiniz.';
+$lang['porg_v14_duplicates_title'] = 'Çiftlerden kaçının';
+$lang['porg_v14_intro1'] = 'Arama ve ötesi';
+$lang['porg_v14_intro2'] = 'Galerideki arama motoru derinlemesine bir yeniden tasarıma kavuşuyor. Yaptığımız anketler Piwigo\'nun binlerce fotoğraf arasında iyi olduğunu gösterdi. Piwigo\'nun bu görevde mükemmel olmasını sağlayın! Bu nedenle kullanıcı deneyimini sıfırdan elden geçirdik.';
+$lang['porg_v14_intro3'] = 'Piwigo 14 ayrıca HEIC gibi yeni görüntü dosyası türleri için beklenen destek, PHP 8.2 ile uyumluluk, yeni albüm editörü ve hız iyileştirmeleri ile birlikte geliyor.';
+$lang['porg_v14_mimetypes_image1_label'] = 'Poster olmadığında Piwigo tarafından görüntülenen mimetype simgeleri yenilenir.';
+$lang['porg_v14_mimetypes_title'] = 'Mime türleri simgeleri';
+$lang['porg_v14_newfiles_text'] = '<b>HEIC</b> : bu şu anda iOS ve Android telefonlarda fotoğraflar için kullanılan varsayılan dosya türüdür. Piwigo, görüntülenmek üzere bir JPEG dosyası oluşturacak ve HEIC\'i orijinal dosya olarak saklayacaktır. <br>
+<br>
+<b>WebP</b> : JPEG yerine en çok "web tarayıcıları tarafından desteklenen" dosya türü. Piwigo, çok boyutlu gösterim için WebP dosyaları oluşturacaktır. Bu, yalnızca "giriş olarak" desteklenen diğer dosya türlerine kıyasla büyük bir farktır. WebP, içe aktarmanın yanı sıra görüntüleme için de desteklenir. WebP ile ilginç olan şey, aynı dosya ağırlığı için JPEG\'i daha yüksek kaliteyle değiştirebilmesi, çok daha küçük bir ağırlık için animasyonlu GIF\'in yerini alabilmesi ve ayrıca PNG\'yi kayıpsız kalite ve şeffaflıkla değiştirebilmesidir. <br>
+<br>
+<b>PSD</b> (Photoshop) ve EPS : Piwigo, PSD/<b>EPS\'yi</b> orijinal dosya olarak saklar ve görüntülemek için bir JPEG veya PNG oluşturur.';
+$lang['porg_v14_newfiles_title'] = 'Desteklenen yeni dosya türleri';
+$lang['porg_v14_newfiles_title_menu'] = 'HEIC, WebP, PSD, EPS desteği';
+$lang['porg_v14_notificationgroup_image1_label'] = 'Çok sayıda yöneticiniz varsa ancak yeni bir kullanıcı abone olduğunda hepsinin bilgilendirilmesini istemiyorsanız, bir kullanıcı grubuyla filtreleme yapabilirsiniz.';
+$lang['porg_v14_notificationgroup_title'] = 'Bildirim grubu';
+$lang['porg_v14_search_image1_label'] = 'Bu yeni arama filtre çubuğu, hem menüdeki eski "hızlı arama" hem de "gelişmiş arama" sayfasının yerini alıyor.';
+$lang['porg_v14_search_image2_label'] = '"panda" kelimesini arattığınızda, Piwigo etiketler veya albümler arasında eşleşmeler bulabilir. Piwigo listeyi görüntüler ve arama dışında doğrudan etiketi veya albümü açmanızı sağlar.';
+$lang['porg_v14_search_image3_label'] = '<b>Bu sette arama yapın</b>: Bir albüme, bir etikete veya bir etiket kombinasyonuna göz atarken, Piwigo bir "bu <b>sette</b> ara" düğmesi ve simgesi sağlayacaktır. Üzerine tıkladığınızda, albümde (veya etiket listesinde) önceden filtrelenmiş yeni bir arama oluşturacaksınız ve filtre eklemekte özgür olacaksınız. Örneğin, 2018 yılında "hayvan / tilkiler" albümünde eklenen fotoğrafları bulmak istiyorsanız, bu çok kolay olacaktır.';
+$lang['Interview %s'] = '%s ile görüşme';
+$lang['New search engine, new album editor, new file types.'] = 'Yeni arama motoru, yeni albüm editörü, yeni dosya türleri.';
+$lang['Technical features'] = 'Teknik özellikler';
+$lang['The menu'] = 'Menü';
+$lang['porg_v13_api_s1'] = 'Fotoğrafları ilişkilendirme/ayrıştırma/taşıma';
+$lang['porg_v13_api_s2'] = 'Yeni arama oluşturma';
+$lang['porg_v13_api_s3'] = 'yeni parametre <code>is_download</code>';
+$lang['porg_v13_api_s4'] = 'yeni parametre <code>search</code>';
+$lang['porg_v13_api_s5'] = 'yeni parametreler';
+$lang['porg_v13_technical_features13'] = 'Öksüzleri hesapla';
+$lang['porg_v13_technical_features14'] = 'Büyük galerilerde, 100k+ fotoğrafla, yetimleri bulmak birkaç saniye sürebilir. Piwigo 14, öksüzleri bulma süresini kısaltır ve bu aramayı çok daha az sıklıkta çalıştırmak için önbellek kullanır.';
+$lang['porg_v13_technical_features15'] = 'API değişiklikleri';
+$lang['porg_v13_technical_features4'] = 'url_port için yapılandırma ayarı';
+$lang['porg_v13_technical_features5'] = 'Yeni gelişmiş yapılandırma ayarı <code>$conf[\'url_port\']</code>, egzotik ters proxy ortamlarında çok kullanışlıdır.';
+$lang['porg_v13_technical_features6'] = 'PHP 8.2 için uyumlu';
+$lang['porg_v13_technical_features7'] = 'PHP 8.2 hızla birçok sunucuya ulaştı ve Piwigo\'nun uyumlu olması gerekiyordu! Kütüphaneler güncellendi: Smarty 4.3, PHPMailer 6.8.0, cssmin özel değişikliği.';
+$lang['porg_v14_albumeditor_image1_label'] = 'Yeni albüm düzenleyici: daha iyi alan kullanımı, küçük ve büyük ekranlarda daha tutarlı görüntüleme, düğmenin sabit konumunu doğrulama (sağ alt).';
+$lang['porg_v14_albumeditor_title'] = 'Albüm editörü';
+$lang['porg_v14_alice_text1'] = 'Benim adım Alice ve Haziran 2022\'den beri Piwigo\'da UX/UI tasarımcısıyım. Bu ilginç kısaltmanın arkasında büyüleyici bir iş yatıyor: tüm uygulama için ergonomik, sezgisel ve kullanıcı dostu arayüzler oluşturmak. Asıl amacım, Piwigo\'yu her gün keyifle kullanmanız!';
+$lang['porg_v14_alice_text2'] = 'Ayrıca bazen Piwigo\'nun çeşitli web sitelerini ve iletişim girişimlerini göstermek için her türlü görseli oluşturmak için grafik tasarımcı şapkamı takıyorum. Örneğin, sürüm 14\'ün afişi gibi!';
