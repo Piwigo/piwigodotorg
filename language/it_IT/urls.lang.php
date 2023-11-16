3effe91234d2d1bafbea6ca7cc01f7831463a75d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-16 07:00:34 +0100

    Update it_IT, thanks to rio

--- language/it_IT/urls.lang.php
+++ language/it_IT/urls.lang.php
@@ -49,3 +49,4 @@ $lang['porg_urls']['manual_update'] = 'guide/aggiornamento/manuale';
 $lang['porg_urls']['netinstall'] = 'guide/installazione/netinstall';
 $lang['porg_urls']['requirements'] = 'guide/installazione/prerequisiti';
 $lang['porg_urls']['mobile-applications'] = 'Applicazioni per dispositivi mobili.';
+$lang['porg_urls']['users'] = 'chi-usa-piwigo';
