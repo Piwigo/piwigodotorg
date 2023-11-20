c8c3e3fd93c0e2b8f8fa9cc10bd7c064f78453e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-20 07:00:28 +0100

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/release-14.0.0.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v14_newfiles_title'] = 'Nieuwe bestandsformaten ondersteund';
+$lang['porg_v14_storagetooltips_title'] = 'Opslag details';
+$lang['porg_v14_technical_features1'] = 'Bemerk dat meer gedetailleerde documentatie beschikbaar is over';
+$lang['porg_v14_claire_text2'] = 'Voor Piwigo 14 heb ik gewerkt aan een benchmark van verschillende zoek en filter oplossingen, geinspireerd door foto-bibliotheken, maar ook e-commerce sites, CRM toepassingen... Mijn doel was om de onwikkel- en designteams inspiratiebronnen te geven voor het verbetern van zoeken en filteren in Piwigo galerijen.';
+$lang['porg_v14_notificationgroup_title'] = 'Notificatie groep';
+$lang['porg_v14_claire_text1'] = 'Mijn naam is Claire en bij Piwigo zorg ik voor marketing, communicatie, documentatie en veel meer! Ik hou ervan met andere gebruikers te praten om beter te begrijpen wat hun verwachtingen zijn en hoe ze Piwigo gebruiken.';
+$lang['porg_v14_alice_text2'] = 'Soms draag ik ook de hoed van grafisch designer om verscheidene afbeeldingen te maken voor de verschillende websites en communicatie intitiatieven van Piwigo. Bijvoorbeeld de banner voor versie 14!';
+$lang['porg_v14_alice_text1'] = 'Mijn naam is Alice en ik ben UX/UI designer voor Piwigo sinds juni 2022. Achter dit acroniem ligt een boeiende taak: het maken van ergonomische, intuitieve an gebruiksvriendelijke interfaces voor de hele applicatie. Mijn hoofddoel is dat jij Piwigo elke dag met plezier gebruikt!';
+$lang['porg_v14_albumeditor_image1_label'] = 'Nieuwe album bewerker: beter gebruik van ruimte, meer consistente afbeelding op kleine en grote schermen, vaste positie voor validatie-knop (onderaan rechts).';
+$lang['porg_v13_technical_features7'] = 'PHP 8.2 wordt snel beschikbaar op veel servers en Piwigo moest compatibel zijn! Bijgewerkte libraries: Smarty 4.3, PHPMailer 6.8.0, cssmin aangepaste wijziging.';
+$lang['porg_v13_technical_features4'] = 'Configuratie instelling voor url_port';
+$lang['porg_v13_technical_features5'] = 'Nieuwe gevorderde  configuratie instelling <code>$conf[\'url_port\']</code>, zeer nuttig in combinatie met een exotische reverse-proxy.';
+$lang['porg_v14_badge_redesign'] = 'herontwerp';
+$lang['porg_v14_badge_refresh'] = 'verfrissing';
+$lang['porg_v14_albumeditor_title'] = 'Album bewerker';
+$lang['porg_v13_technical_features6'] = 'Compatibel met PHP 8.2';
+$lang['porg_v13_technical_features14'] = 'In grote galerijen, met meer dan 100k foto\'s, kan het vinden van wezen verschillende seconden duren. Piwigo 14 vermindert de tijd om wezen te vinden en gebruikt een cache om deze zoekopdracht minder vaak uit te voeren.';
+$lang['porg_v13_technical_features13'] = 'Bereken wezen';
+$lang['porg_v14_technical_features3'] = 'Gelieve dit te lezen om uw uitbreidingen compatibel te maken met Piwigo 14.';
+$lang['porg_v14_technical_features2'] = 'Technische wijzigingen in Piwigo 14';
+$lang['porg_v14_search_title'] = 'Zoekmachine';
+$lang['porg_v14_newfiles_title_menu'] = 'HEIC, WebP, PSD, EPS ondersteuning';
+$lang['porg_v14_duplicates_title'] = 'Vermijd dubbels';
+$lang['porg_v14_badge_new'] = 'nieuw';
+$lang['porg_v13_technical_features15'] = 'API wijzigingen';
+$lang['porg_v13_api_s5'] = 'nieuwe parameters';
+$lang['porg_v13_api_s4'] = 'nieuwe parameter <code>zoek</code>';
+$lang['porg_v13_api_s3'] = 'nieuwe parameter <code>is_download</code>';
+$lang['Interview %s'] = 'interview met %s';
+$lang['porg_v13_api_s2'] = 'maak een nieuwe zoekopdracht';
+$lang['The menu'] = 'Het menu';
+$lang['porg_v13_api_s1'] = 'foto\'s linken/losmaken/verplaatsen';
+$lang['Technical features'] = 'Technische eigenschappen';
+$lang['New search engine, new album editor, new file types.'] = 'Nieuwe zoekmachine, nieuwe album bewerker, nieuwe bestandstypes.';
