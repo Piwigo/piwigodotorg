53ed7695cb4dd450aec79f06ced75018dc07cd20
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-26 05:55:37 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-14.0.0.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v14_badge_new'] = 'nowe';
+$lang['porg_v14_badge_redesign'] = 'przeprojektowane';
+$lang['porg_v14_badge_refresh'] = 'odświeżone';
+$lang['porg_v14_claire_text1'] = 'Nazywam się Claire i w Piwigo odpowiadam za marketing, komunikację, dokumentację i wiele innych! Uwielbiam rozmawiać z użytkownikami, aby lepiej zrozumieć ich oczekiwania i w jaki sposób korzystają z Piwigo.';
+$lang['porg_v14_claire_text2'] = 'Dla tej, 14 wersji Piwigo, pracowałam nad porównaniem różnych rozwiązań do wyszukiwania i filtrowania, czerpiąc inspirację z narzędzi bibliotek zdjęć, ale również witryn sklepów internetowych, systemów CRM... Dążyłam do przedstawienia programistom i zespołu projektantów źródeł inspiracji dla poprawienia wyszukiwania i filtrowania dla galerii Piwigo.';
+$lang['porg_v14_duplicates_title'] = 'Ustrzeż się powtórzeń';
+$lang['porg_v14_intro1'] = 'Wyszukiwanie i nie tylko';
+$lang['porg_v14_intro2'] = 'Silnik wyszukiwania w galerii został głęboko przebudowany. Nasze ankiety pokazały, że Piwigo było dobre w wyszukiwaniu spośród tysięcy zdjęć. Pozwólmy Piwigo być w tym perfekcyjnym! Dlatego od podstaw przeprojektowaliśmy doświadczenie użytkownika.';
+$lang['Interview %s'] = 'wywiad z %s';
+$lang['New search engine, new album editor, new file types.'] = 'Nowy silnik wyszukiwania, nowy edytor albumu, nowe formaty plików';
+$lang['Technical features'] = 'Cechy techniczne';
+$lang['The menu'] = 'Menu';
+$lang['porg_v13_api_s1'] = 'skojarz/odłącz/przenieś zdjęcia';
+$lang['porg_v13_api_s2'] = 'utwórz nowe wyszukiwanie';
+$lang['porg_v13_api_s3'] = 'nowy parametr <code>is_download</code>';
+$lang['porg_v13_api_s4'] = 'nowy parametr <code>search</code>';
+$lang['porg_v13_api_s5'] = 'nowe parametry';
+$lang['porg_v13_technical_features13'] = 'Przetwarzanie sierot';
+$lang['porg_v13_technical_features14'] = 'W dużych galeriach, powyżej 100000 zdjęć, odnajdywanie osieroconych zdjęć może zająć kilkanaście sekund. Piwigo 14 skraca czas wyszukiwania sierot i używa pamięci podręcznej, aby przeprowadzać takie wyszukiwanie rzadziej.';
+$lang['porg_v13_technical_features15'] = 'Zmiany w API';
+$lang['porg_v13_technical_features4'] = 'Ustawienia konfiguracyjne dla url_port';
+$lang['porg_v13_technical_features5'] = 'Nowe zaawansowane ustawienia konfiguracyjne <code>$conf[\'url_port\']</code>, bardzo przydatne dla egzotycznych środowisk odwrotnego proxy.';
+$lang['porg_v13_technical_features6'] = 'Kompatybilność z PHP 8.2';
+$lang['porg_v13_technical_features7'] = 'PHP 8.2 bardzo szybko pojawiło się na wielu serwerach i Piwigo musi być kompatybilne! Uaktualnione biblioteki: Smarty 4.3, PHPMailer 6.8.0, oraz zmiany w cssmin.';
+$lang['porg_v14_albumeditor_image1_label'] = 'Nowy edytor albumu: lepsze wykorzystanie przestrzeni, bardziej spójne wyświetlanie na małych i dużych ekranach, poprawione położenie przycisku walidacji (prawy dół).';
+$lang['porg_v14_albumeditor_title'] = 'Edytor albumu';
+$lang['porg_v14_alice_text1'] = 'Nazywam się Alice i jestem projektantką UX/UI w Piwigo od czerwca 2022. Za tym ciekawym skrótem kryje się fascynująca praca: tworzenie ergonomicznego, intuicyjnego i przyjaznego użytkownikom interfejsu dla całej aplikacji. Moim głównym celem jest Twoja radość z codziennego korzystania z Piwigo!';
+$lang['porg_v14_alice_text2'] = 'Czasami też wcielam się w rolę grafika, tworząc różnego rodzaju wizualizacje, aby zilustrować różne witryny Piwigo i inicjatywy komunikacyjne. Jak na przykład baner dla wersji 14!';
