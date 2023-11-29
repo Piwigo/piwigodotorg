4a9e31b7b8acbcbc3908bb09deba8128f1243a1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-29 06:37:42 +0100

    Update pl_PL, thanks to yareq

--- language/pl_PL/release-14.0.0.lang.php
+++ language/pl_PL/release-14.0.0.lang.php
@@ -33,3 +33,28 @@ $lang['porg_v14_albumeditor_image1_label'] = 'Nowy edytor albumu: lepsze wykorzy
 $lang['porg_v14_albumeditor_title'] = 'Edytor albumu';
 $lang['porg_v14_alice_text1'] = 'Nazywam się Alice i jestem projektantką UX/UI w Piwigo od czerwca 2022. Za tym ciekawym skrótem kryje się fascynująca praca: tworzenie ergonomicznego, intuicyjnego i przyjaznego użytkownikom interfejsu dla całej aplikacji. Moim głównym celem jest Twoja radość z codziennego korzystania z Piwigo!';
 $lang['porg_v14_alice_text2'] = 'Czasami też wcielam się w rolę grafika, tworząc różnego rodzaju wizualizacje, aby zilustrować różne witryny Piwigo i inicjatywy komunikacyjne. Jak na przykład baner dla wersji 14!';
+$lang['porg_v14_technical_features1'] = 'Pamiętaj o dostępności bardziej szczegółowej dokumentacji';
+$lang['porg_v14_technical_features2'] = 'Zmiany techniczne w Piwigo 14';
+$lang['porg_v14_technical_features3'] = 'Prosimy o przeczytanie tego, aby zapewnić kompatybilność Twoich wtyczek z Piwigo 14';
+$lang['porg_v14_storagetooltips_image1_label'] = 'Ładnie wyglądające szczegóły na pasku miejsca przechowywania plików w panelu informacyjnym. Dla każdej "kategorii" plików Piwigo powie Ci ile jest plików każdego rozszerzenia i jaki rozmiar zajmują one w Twoim obszarze przechowywania.';
+$lang['porg_v14_storagetooltips_title'] = 'Szczegóły obszaru przechowywania';
+$lang['porg_v14_search_title'] = 'Silnik wyszukiwania';
+$lang['porg_v14_search_text'] = 'Nowa wyszukiwarka w galerii to z pewnością największa ewolucja wniesiona przez Piwigo 14. Zamiast strony formularza, a następnie strony wyników, nowa "wielokryterialna" wyszukiwarka wyświetla kryteria wyszukiwania i wyniki jednocześnie. Na razie możesz filtrować albumy, tagi, wyszukiwać według słów, przez kogo dodane, autorów, daty publikacji i typów plików. Nowe filtry pojawią się w przyszłych wersjach.';
+$lang['porg_v14_search_image3_label'] = '<b>Szukaj w tym zestawie</b>: podczas przeglądania albumu, tagu lub kombinacji tagów, Piwigo udostępni przycisk i ikonę "szukaj w tym zestawie". Po kliknięciu utworzysz nowe wyszukiwanie na podstawie wstępnie przefiltrowanego albumu (lub listy tagów) i będziesz mógł dowolnie dodawać filtry. Na przykład, jeśli chcesz znaleźć zdjęcia dodane w roku 2018 w albumie „zwierzęta/lisy”, będzie to teraz bardzo proste.';
+$lang['porg_v14_search_image1_label'] = 'Ten nowy pasek filtrów dla wyszukiwania zastępuje zarówno stare "szybkie wyszukiwanie” z menu, jak i stronę "wyszukiwanie zaawansowane”.';
+$lang['porg_v14_search_image2_label'] = 'Gdy szukasz słowa „panda”, Piwigo może znaleźć dopasowania wśród tagów lub albumów. Piwigo wyświetli listę i pozwoli Ci bezpośrednio otworzyć tag lub album, poza wyszukiwaniem.';
+$lang['porg_v14_newfiles_title'] = 'Obsługa nowych typów plików';
+$lang['porg_v14_newfiles_title_menu'] = 'Wsparcie dla HEIC, WebP, PSD i EPS.';
+$lang['porg_v14_notificationgroup_image1_label'] = 'Jeżeli masz wielu administratorów, ale nie chcesz aby wszyscy byli powiadamiani o subskrypcji nowego użytkownika, możesz filtrować według grupy użytkowników.';
+$lang['porg_v14_notificationgroup_title'] = 'Powiadamianie grup';
+$lang['porg_v14_newfiles_text'] = '<b>HEIC</b>: to obecnie domyślny typ plików dla zdjęć na telefonach z iOS i Androidem. Piwigo wygeneruje plik JPEG do wyświetlania i zachowa HEIC jako oryginał pliku.<br>
+<br>
+<b>WebP</b>: najlepiej "wspierany przez przeglądarki internetowe" typ plików, zastępujący JPEG. Piwigo wygeneruje pliki WebP do wyświetlania na ekranach różnej wielkości. To duża różnica w stosunku do innych typów plików wspieranych jedynie jako "źródło". WebP jest wspierane zarówno dla importu, jak i dla wyświetlania. WebP jest o tyle interesujący, że może zastąpić JPEG z lepszą jakością przy tej samej wielkości pliku, może zastąpić animowane GIFy przy znacznie mniejszej wielkości i jednocześnie może zastąpić PNG z bezstratną jakością i przezroczystością.<br>
+<br>
+<b>PSD</b> (Photoshop) i <b>EPS</b>: Piwigo wygeneruje JPEG lub PNG dla wyświetlania i zachowa PSD/EPS jako oryginał pliku.';
+$lang['porg_v14_mimetypes_image1_label'] = 'Ikony typów MIME, wyświetlane kiedy podgląd w Piwigo nie jest dostępny, zostały odświeżone.';
+$lang['porg_v14_intro3'] = 'Piwigo 14 nadchodzi z równie mocno oczekiwanym wsparciem dla nowych typów plików, jak HEIC, kompatybilnością z PHP 8.2, nowym edytorem albumu i poprawioną szybkością/';
+$lang['porg_v14_duplicates_text'] = 'Podczas przesyłania Piwiko sprawdzi czy identyczny plik był już wcześniej przesłany. Jeżeli plik zostanie znaleziony, Piwigo po prostu użyje istniejące zdjęcie i dołączy je do przesyłanego albumu, bez tworzenia duplikatu.<br>
+<br>
+Ta opcja będzie domyślnie aktywowana dla wszystkich nowych instalacji, ale będzie nieaktywna w czasie uaktualniania instalacji, aby nie zmienić zachowania przesyłania. Oczywiście możesz zmienić to ustawienie na stronie [Administracja &gt; Konfiguracja &gt; Opcje].';
+$lang['porg_v14_mimetypes_title'] = 'Ikony typów MIME';
