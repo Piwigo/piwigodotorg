06eaa030544e1704806324230a79619edd1375ca
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-12-03 18:27:50 +0100

    Update tr_TR, thanks to Nakre

---
+++ language/tr_TR/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_codingactivity_intro'] = 'Piwigo çekirdeği, eklentileri, temaları ve diğer araçlarındaki en son güncellemeler. Bu sayfa, "izlenecek tek bir yer" sağlamak için çeşitli kod depolarından veri toplar.';
+$lang['Coding activity'] = 'Kodlama geçmişi';
