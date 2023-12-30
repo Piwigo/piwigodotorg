558f4aad668c61b2b021dc39c07aebc4e68ed6ab
plegall <plg@piwigo.org>
2023-12-30 12:53:00 +0100

    add missing language file for release 14.1.0

---
+++ language/en_UK/release-14.1.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['porg_issue_2080'] = '[14.x] Parse error: syntax error, unexpected \')\' in cat_modify.php';
+$lang['porg_issue_2077'] = 'Javascript error in Piwigo v14 in Italian translation';
+$lang['porg_issue_2076'] = '[search 14.x] impossible to clear album filter ';
+$lang['porg_issue_2075'] = 'Fix missing EXIF metadata for images uploaded in HEIC format';
+$lang['porg_issue_2074'] = '[install] error on plugins auto activation on PHP 7.4+';
+$lang['porg_issue_2073'] = 'Tag manager and Extended Description: you cannot modify the raw version';
+$lang['porg_issue_2072'] = '[Album Editor 14.x] Description field is too small';
+$lang['porg_issue_2070'] = '[batch manager] Column \'object_id\' cannot be null';
+$lang['porg_issue_2066'] = '[14.x] keep compatibility with PHP 7.0';
+?>
