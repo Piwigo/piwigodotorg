8fac70200db04a7aefb49601f34ac4a4e660df8e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-12-30 18:53:48 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-14.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2066'] = '[14.x] Bevar kompabilitet med PHP 7.0';
+$lang['porg_issue_2070'] = '[batchhåndtering] "Column \'object_id\' cannot be null"';
+$lang['porg_issue_2072'] = '[Albumredigering 14.x] Beskrivelsesfeltet er for lille';
+$lang['porg_issue_2073'] = '[Taghåndtering og udvidede beskrivelser] "you cannot modify the raw version"';
+$lang['porg_issue_2074'] = '[installering] Fejl ved automatisk aktivering af plugins med PHP 7.4+';
+$lang['porg_issue_2075'] = 'Retter manglende EXIF-metadata til billeder uploadet i HEIC-format';
+$lang['porg_issue_2076'] = '[søgning 14.x] Umuligt at nulstille albumfilter';
+$lang['porg_issue_2077'] = 'JavaScript-fejl i Piwigo 14 i den italienske oversættelse';
+$lang['porg_issue_2080'] = '[14.x] Fortolkningsfejl: Syntaksfejl, uventet \')\' i cat_modify.php';
