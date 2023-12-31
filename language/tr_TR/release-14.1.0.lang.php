41628987c62acaf975ca65fa01600c520758db45
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-12-31 06:40:20 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-14.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2080'] = '[14.x] Ayrıştırma hatası: at_modify.php\'de beklenmeyen \')\' sözdizimi hatası';
+$lang['porg_issue_2077'] = 'İtalyanca çeviride Piwigo v14\'teki Javascript hatası';
+$lang['porg_issue_2076'] = '[search 14.x] albüm filtresini temizlemek imkansız';
+$lang['porg_issue_2075'] = 'HEIC formatında yüklenen görseller için eksik EXIF meta verilerinde düzeltme';
+$lang['porg_issue_2074'] = '[install] PHP 7.4+ üzerinde eklentilerin otomatik etkinleştirilmesinde hata';
+$lang['porg_issue_2073'] = 'Tag manager and Extended Description: ham sürümü değiştiremezsiniz';
+$lang['porg_issue_2072'] = '[Album Editor 14.x] Açıklama alanı çok küçük';
+$lang['porg_issue_2066'] = '[14.x] PHP 7.0 ile uyumluluğu korur';
+$lang['porg_issue_2070'] = '[batch manager] \'object_id\' sütunu boş olamaz';
