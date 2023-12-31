15a549d587dc93b84180112bc9ac7b67751b6669
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-12-31 06:40:17 +0100

    Update nl_NL, thanks to watertoren5044, Pat2309

---
+++ language/nl_NL/release-14.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2072'] = '[Album Editor 14.x] Beschrijvingveld is te klein';
+$lang['porg_issue_2074'] = '[installatie)fout bij automatische activering van plugins op PHP 7.4+';
+$lang['porg_issue_2075'] = 'Repareer ontbrekende EXIF metadata voor afbeeldingen geüpload in HEIC formaat';
+$lang['porg_issue_2076'] = '[zoeken 14.x] onmogelijk om albumfilter te wissen';
+$lang['porg_issue_2080'] = '[zoeken 14.x] onmogelijk om albumfilter te wissen [14.x] Parseerfout: syntaxisfout, onverwacht \')\' in cat_modify.php';
+$lang['porg_issue_2066'] = '[14.x] behoud compatibiliteit met PHP 7.0';
+$lang['porg_issue_2070'] = '[batch manager] Kolom \'object_id\' kan niet nul zijn';
+$lang['porg_issue_2073'] = 'Tag manager en uitgebreide beschrijving: u kunt de onbewerkte versie niet wijzigen';
+$lang['porg_issue_2077'] = 'Javascript-fout in Piwigo v14 in Italiaanse vertaling';
