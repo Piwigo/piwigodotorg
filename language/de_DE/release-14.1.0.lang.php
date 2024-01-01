a2097ebd58fc1ba21d15e1b386c4eb7cbaf12ccc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-01 07:31:52 +0100

    Update de_DE, thanks to bigant

---
+++ language/de_DE/release-14.1.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2066'] = '[14.x] Kompatibilität mit PHP 7.0 beibehalten';
+$lang['porg_issue_2070'] = '[batch manager] Spalte \'object_id\' darf nicht leer sein';
+$lang['porg_issue_2072'] = '[Album Editor 14.x] Beschriftungsfeld ist zu klein';
+$lang['porg_issue_2073'] = 'Tag manager und Extended Description: die RAW-Version kann nicht verändert werden';
+$lang['porg_issue_2074'] = '[install] Fehler bei der Auto-Aktivierung der Plugins mit PHP 7.4+';
+$lang['porg_issue_2075'] = 'Fehlende EXIF-Metadaten bei hochgeladenen Bildern im HEIC-Format beheben';
+$lang['porg_issue_2076'] = '[search 14.x] Albumfilter kann nicht geleert werden';
+$lang['porg_issue_2077'] = 'Javascript-Fehler in Piwigo v14 mit der italienischen Übersetzung';
+$lang['porg_issue_2080'] = '[14.x] Parsing-Fehler: Syntax-Fehler, unerwartete Zeichenfolge \')\' in cat_modify.php
+
+';
