32823895e53480f05cf16b37f81dcfb1028ca026
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-02 13:33:49 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-14.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2066'] = '[14.x] mantiene la compatibilità con PHP 7.0';
+$lang['porg_issue_2070'] = '[batch manager] Colonna \'object_id\' non può essere null';
+$lang['porg_issue_2072'] = '[Album Editor 14.x] Il campo Descrizione è troppo piccolo';
+$lang['porg_issue_2073'] = 'Gestione tag e Descrizione estesa: non puoi modificare la versione raw ';
+$lang['porg_issue_2074'] = '[installazione] errore sul plugin auto attivazione con PHP 7.4+';
+$lang['porg_issue_2075'] = 'Fissa metadati EXIF mancanti per immagini caricate in formato HEIC';
+$lang['porg_issue_2076'] = '[ricerca 14.x] impossibile ripulire il filtro album';
+$lang['porg_issue_2077'] = 'Errore javascript nella versione italiana di Piwigo 14';
+$lang['porg_issue_2080'] = '[14.x] Errore di analisi: errore di sintassi, inatteso \')\' in cat_modify.php';
