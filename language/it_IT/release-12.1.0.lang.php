a309f2decb5aa3883d949b928054ffabd161b114
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-14 11:40:11 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1546'] = 'Aggiornamenti Piwigo: il collegamento "nuove entusiasmanti funzionalità" è vuoto.';
+$lang['porg_issue_1547'] = '[upgrade] desc groups fallisce con MySQL 8';
+$lang['porg_issue_1549'] = 'Nessun aggiornamento dell\'estensione anche se il dashboard dice il contrario';
+$lang['porg_issue_1550'] = 'La pagina dei plugin non funziona più';
+$lang['porg_issue_1552'] = '[12.0] Dopo il download, l\'album X non contiene ora mai più nessuna foto.';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted aggiunge dettagli sulla categoria aggiornata';
+$lang['porg_issue_1554'] = '[12.x] Modulo di caricamento, album selezionato inaspettato';
+$lang['porg_issue_1555'] = 'traduzione mancante nella pagina di modifica della foto';
