87fa8593e89204d789c85b809f08e3dfc443ea4f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-14 11:40:10 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-12.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1557'] = '[12.1] Il gestore utenti si carica senza interruzione con MySQL 8';
+$lang['porg_issue_1567'] = '[12.x] I badge dei plugin non vengono aggiornati con le voci di ricerca';
+$lang['porg_issue_1569'] = '[12.x] La disabilitazione di un gruppo dalla qualità di gruppo predefinita provoca un ciclo infinito';
+$lang['porg_issue_1570'] = 'album collegati e nomi multilingue';
+$lang['porg_issue_1573'] = '[12.x] Il pulsante di modifica della ricerca dell\'album non riporta all\'album corretto.';
+$lang['porg_issue_1574'] = '[12.x] Il messaggio di sblocco della Galleria è errato';
+$lang['porg_issue_1587'] = 'il download con Piwigo NG genera troppa attività';
+$lang['porg_issue_1590'] = '[Pagina di manutenzione] Le funzioni avanzate non sono supportate';
+$lang['porg_issue_1591'] = '[12.x] incompatibilità con i plugin openstreetmap/videojs';
