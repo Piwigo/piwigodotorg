184a0b977be15ba2cf2c7f3dd803f2433e3b2e20
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-22 09:51:14 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/what_is_piwigo.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_whatispiwigo_self_hosting'] = 'Kendi Kendine Barındırma';
+$lang['porg_whatispiwigo_support'] = 'Destek, güncellemeler ve bakım dahil';
+$lang['porg_whatispiwigo_sustainable_desc1'] = '2002 doğumlu Piwigo, olgunluk <em>seviyesine</em> ulaştığı aşikardır.';
+$lang['porg_whatispiwigo_sustainable_desc2'] = 'Tabii ki, mevcut teknolojilere uyacak şekilde gelişmeye devam ediyor!';
+$lang['porg_whatispiwigo_test_free'] = 'Ücretsiz test edin';
+$lang['porg_whatispiwigo_use_desc1'] = 'Piwigo kullanıcı topluluğu arasında çok çeşitli kullanımlar vardır.';
+$lang['porg_whatispiwigo_use_desc2'] = 'Orta büyüklükteki galerilerden büyük resim bankalarına kadar, Piwigo hayatınızı kolaylaştırabilir ve zamandan ve emekten tasarruf etmenizi sağlayabilir.';
+$lang['porg_whatispiwigo_gnu_desc5'] = ' Özgür\'ün iki anlamı arasındaki fark genellikle "Konuşmada olduğu gibi özgür ve birada olduğu gibi özgür" olarak nitelendirilir.';
+$lang['porg_whatispiwigo_gnu_desc6'] = ' GPL, konuşmada olduğu gibi özgürdür.';
+$lang['porg_whatispiwigo_high_volume_desc1'] = 'Koleksiyonunuzda 500.000 fotoğraf mı var? Piwigo için sorun değil.';
+$lang['porg_whatispiwigo_high_volume_desc2'] = 'Yıldırım hızında performanslar varsayılan olarak dahildir.';
+$lang['porg_whatispiwigo_how_to_desc1'] = 'Piwigo\'yu <span style="color:#f70;">Piwigo.org adresinden indirebilir</span> ve uygun teknik gereksinimlerle web hosting\'inize kurabilirsiniz.';
+$lang['porg_whatispiwigo_how_to_desc2'] = 'Piwigo\'yu kullanmak ücretsizdir, sadece barındırma sağlayıcınıza ödeme yaparsınız.';
+$lang['porg_whatispiwigo_how_to_desc3'] = 'Piwigo ayrıca <span style="color:#ff3363" üzerinde <em>bir hizmet olarak</em> gelir>Piwigo.com</span>: Deneme hesabı açmak için birkaç saniyenizi ayırın, Piwigo\'yu kullanın ve teknik detayları unutun.';
+$lang['porg_whatispiwigo_how_to_desc4'] = 'Bir Piwigo.com hesabı, barındırma ücretlerini içerir.';
+$lang['porg_whatispiwigo_individual_desc1'] = 'Son 20 yılda bir aile albümünü yönetmek, Avrupa örümceklerinden oluşan bir koleksiyon düzenlemek, en iyi manzara fotoğraflarınızı fotoğraf amatör olarak göstermek...';
+$lang['porg_whatispiwigo_individual_desc2'] = 'Bunlar, Piwigo\'nun kendi topluluğu tarafından nasıl kullanıldığına dair birkaç örnektir.';
+$lang['porg_whatispiwigo_infinitely_customizable'] = 'Sonsuz özelleştirilebilir';
+$lang['porg_whatispiwigo_noSkills'] = 'Teknik beceri gerekmez';
+$lang['porg_whatispiwigo_oneClick'] = 'Tek tıkla kurulum';
+$lang['porg_whatispiwigo_open_source_desc1'] = 'Piwigo her zaman özgür bir yazılım olmuştur ve her zaman da öyle kalacaktır.';
+$lang['porg_whatispiwigo_open_source_desc2'] = 'Okuyabileceğiniz ve anlayabileceğiniz bir kod, güvenebileceğiniz bir koddur.';
+$lang['porg_whatispiwigo_organisation_desc1'] = 'Kamu sektörü, şirketler, vakıflar... Hepsi kendi koleksiyonunu yönetmek zorunda.';
+$lang['porg_whatispiwigo_organisation_desc2'] = '<em>Dijital Varlık Yönetimi</em> alanında Piwigo, kullanım kolaylığı, uygun fiyat, performans ve sürdürülebilirlik konularına odaklanmaktadır.';
+$lang['porg_whatispiwigo_professional_desc1'] = 'Profesyonel fotoğrafçıların çalışmalarını müşterilerine herkese açık veya özel olarak göstermeleri gerekir.';
+$lang['porg_whatispiwigo_professional_desc2'] = 'Piwigo gelişmiş izin kontrolü sağlar.';
+$lang['porg_whatispiwigo_professional_desc3'] = 'Müşteriler fotoğrafları tek tek veya tüm albümleri indirebilir.';
+$lang['Amateurs'] = 'Acemiler';
+$lang['Batch Downloader'] = 'Toplu İndirici';
+$lang['Collections'] = 'Koleksiyonlar';
+$lang['Families'] = 'Aileler';
+$lang['Fansites'] = 'Hayran Siteleri';
+$lang['GNU License'] = 'GNU Lisans';
+$lang['How to use Piwigo ?'] = 'Piwigo nasıl kullanılır?';
+$lang['Lightning Fast'] = 'Yıldırım gıbı hızlı';
+$lang['Multiple Administrators'] = 'Birden Çok Yönetici';
+$lang['Share with Clients'] = 'Müşterilerle Paylaş';
+$lang['What can I use Piwigo for ?'] = 'Piwigo\'yu ne için kullanabilirim?';
+$lang['Why choosing Piwigo ?'] = 'Neden Piwigo\'yu seçmeli?';
+$lang['porg_whatispiwigo_desc1'] = 'Piwigo, fotoğraf koleksiyonunuzu ve diğer medyaları yönetmenizi sağlayan bir web uygulamasıdır.';
+$lang['porg_whatispiwigo_desc2'] = 'Güçlü özellikler içeren, dünyanın her yerindeki fotoğraf galerilerine güç verir.';
+$lang['porg_whatispiwigo_download'] = 'Piwigo İndir';
+$lang['porg_whatispiwigo_full_autonomy'] = 'Tam özerklik';
+$lang['porg_whatispiwigo_gnu_desc1'] = 'GNU Genel Kamu Lisansı veya GPL, açık kaynaklı bir lisanstır.';
+$lang['porg_whatispiwigo_gnu_desc2'] = 'Açık kaynak, yalnızca kaynak kodunu görüntüleyebileceğiniz anlamına gelmez, aynı zamanda politik ve felsefi etkileri de vardır.';
+$lang['porg_whatispiwigo_gnu_desc3'] = 'Açık kaynak veya "Özgür Yazılım", belirli koşullar altında kaynak kodunu değiştirmekte ve yeniden dağıtmakta özgür olduğunuz anlamına gelir.';
+$lang['porg_whatispiwigo_gnu_desc4'] = 'Ücretsiz, fiyatı değil, özgürlüğü ifade eder.';
