3e0ebb6fc2ff0178a128cc51dfd365043c5303f4
plegall <plg@piwigo.org>
2024-01-25 11:38:46 +0100

    copy demo.lang.php from piwigo.com

---
+++ language/de_DE/demo.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_demo_1_text'] = 'Diese Piwigo Demo-Galerie basiert auf unserem Standard-Theme und -Skin: Modus Newspaper. Für diese Demo haben wir ein Banner mit einem von uns entworfenen Bild und unserem Logo hinzugefügt. Unser Einleitungstext und die Schaltflächen wurden mit dem Plugin PWG stuffs und zusätzlichem CSS-Code hinzugefügt.';
+$lang['porg_demo_1_title'] = 'Vorführung Nr. 1';
+$lang['porg_demo_2_text'] = 'Unsere zweite Piwigo-Demo-Galerie verwendet den Standard-Skin des Bootstrap Darkroom-Themas. Bei dieser Demo haben wir die Plugins "Erweiterte Beschreibung" und "Zusätzliche Seiten" aktiviert. Diese Plugins ermöglichen es uns, ein Karussell auf der Homepage der Galerie zu haben.';
+$lang['porg_demo_2_title'] = 'Vorführung Nr. 2';
+$lang['porg_demo_3_text'] = 'Für die dritte Piwigo-Demo-Galerie verwenden wir das Theme Modus mit dem Splash-Skin. Wir haben dieses Skin angepasst, um bestimmte Farben zu verwenden, und unser Logo entsprechend ausgewählt. Mithilfe der Banner-Optionen wird unser Logo oben links eingefügt und auf der rechten Seite steht unser Galeriename mit einem einleitenden Text. In dieser Galerie ist das Plugin PWG stuffs aktiviert, um einen Tag-Block vor unseren Alben anzuzeigen.';
+$lang['porg_demo_3_title'] = 'Vorführung Nr. 3';
+$lang['porg_demo_4_text'] = 'Die vierte Demo-Galerie verwendet Bootstrap Darkroom mit einem Hero-Image. Wir haben den Cyborg-Skin dieses Themas personalisiert, damit er zu dem Bild passt, das wir als Hero-Image gewählt haben. Eine andere Sache, die wir einzigartig gemacht haben, ist die Verwendung einer anderen Schriftart für den Titel dieser Galerie und das Hinzufügen unseres Logos in der oberen linken Ecke. Schließlich haben wir auch das Banner angepasst, um ein Anmeldeformular für unsere Galerie zu enthalten';
+$lang['porg_demo_4_title'] = 'Vorführung Nr. 4';
+$lang['porg_demo_into_text'] = 'Entdecken Sie unsere piwigo Demo-Galerien. Schauen Sie sich diese an und sehen Sie, wie personalisierbar unsere Galerien sind und was unsere Technologie zu leisten imstande ist.';
+$lang['porg_demo_page_title'] = 'Piwigo Vorführungen';
+$lang['porg_demo_video_support'] = 'Ihr Browser unterstützt das Video-Tag nicht.';
