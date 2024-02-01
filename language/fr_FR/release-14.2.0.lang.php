f786a6e8bad8e79f289089e3c76e23a16d4b89e3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-01 12:45:56 +0100

    Update fr_FR, thanks to laurent.duretz

---
+++ language/fr_FR/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2081'] = '[14.1.0] menu perdu et icônes du calendrier';
+$lang['porg_issue_2086'] = 'Mauvaises icônes lorsque AdminTools est activé';
+$lang['porg_issue_2094'] = '[recherche 14.x] la listbox selectize est trop courte ';
+$lang['porg_issue_2088'] = 'l\'autocomplétion d\'history.section ne devrait pas être sensible à la casse';
+$lang['porg_issue_2089'] = 'cacher le bouton \"Rechercher dans ce \" par défaut';
+$lang['porg_issue_2093'] = 'Supprimer le saut de ligne lors de l\'import/synchronisation des métadonnées IPTC';
+$lang['porg_issue_2095'] = '[UX] rendre évident que \"Qui peut voir cette photo ?\" est relatif au niveau de sécurité';
+$lang['porg_issue_2096'] = '[recherche] enregistrer la liste des filtres';
