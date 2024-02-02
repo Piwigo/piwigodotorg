761250a48fc734d56dd5b34e3812112f2c52edc0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-02 11:39:45 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2096'] = 'zoeken] lijst met filters opslaan';
+$lang['porg_issue_2095'] = '[UX] maakt duidelijk dat \"Wie kan deze foto zien?\" gerelateerd is aan het privacyniveau';
+$lang['porg_issue_2094'] = '[zoek 14.x] selecteer keuzelijst is te kort';
+$lang['porg_issue_2093'] = 'verwijder nieuwe regel tijdens import/synchronisatie van IPTC-metagegevens';
+$lang['porg_issue_2089'] = 'verberg knop \"zoek in deze set\" standaard';
+$lang['porg_issue_2088'] = 'geschiedenis-sectie automatisch aanvullen mag niet hoofdlettergevoelig zijn';
+$lang['porg_issue_2086'] = 'Verkeerde pictogrammen wanneer Beheer Gereedschap is geactiveerd';
+$lang['porg_issue_2081'] = '[14.1.0] verloren menu- en kalenderpictogram';
