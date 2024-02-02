61e29a40587ef4f013a4ff160d4ab1434fe6c814
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-02 11:39:42 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2081'] = '[14.1.0] kayıp menü ve takvim simgeleri';
+$lang['porg_issue_2086'] = 'AdminTools etkinleştirildiğinde yanlış simgeler';
+$lang['porg_issue_2088'] = 'history.section otomatik doldurma büyük/küçük harfe duyarlı olmamalıdır';
+$lang['porg_issue_2089'] = 'varsayılan olarak \"Bu sette ara\" düğmesini gizle';
+$lang['porg_issue_2093'] = 'IPTC meta verilerini içe aktarma/eşitleme sırasında yeni satırı kaldır';
+$lang['porg_issue_2094'] = '[search 14.x] seç liste kutusu çok kısa';
+$lang['porg_issue_2095'] = '[UX] açıkça belirtmek \"Bu fotoğrafı kim görebilir?\" gizlilik düzeyiyle ilgilidir';
+$lang['porg_issue_2096'] = '[search] filtre listesini kaydet';
