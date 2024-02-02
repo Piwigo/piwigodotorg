35da1dfce40026dbaa6684a643af1ee2309a059e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-02 11:39:39 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2081'] = '[14.1.10] s\'han perdut el menú i les icones del calendari';
+$lang['porg_issue_2086'] = 'Icones incorrectes en activar AdminTools';
+$lang['porg_issue_2088'] = 'l\'emplenament automàtic de history.section no hauria de ser sensible a majúscules i minúscules';
+$lang['porg_issue_2089'] = 'amagar per defecte el botó \"cercar en aquest conjunt\"';
+$lang['porg_issue_2093'] = 'eliminar el salt de línia durant la importació/sincronització de metadades IPTC';
+$lang['porg_issue_2094'] = '[cerca 14.x] la caixa de llista selectsize és massa curta';
+$lang['porg_issue_2095'] = '[IU] fer obvi que \"Qui pot veure aquesta foto?\" està relacionat amb el nivell de privadesa';
+$lang['porg_issue_2096'] = '[cerca] desar la llista de filtres';
