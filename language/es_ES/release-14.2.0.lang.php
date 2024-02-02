91816efb2996fa0570adb87e443eb3a58e09d244
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-02 11:39:41 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2081'] = '[14.1.10] perdidos el menú y los iconos del calendario';
+$lang['porg_issue_2086'] = 'Iconos incorrectos al activar AdminTools';
+$lang['porg_issue_2088'] = 'el rellenado automático de history.section no debería ser sensible a mayúsculas y minúsculas';
+$lang['porg_issue_2089'] = 'ocultar el botón \"buscar en este conjunto\" por defecto';
+$lang['porg_issue_2093'] = 'eliminar el salto de línea durante la importación/sincronización de metadatos IPTC';
+$lang['porg_issue_2094'] = '[búsqueda 14.x] la caja de lista selectsize es demasiado corta';
+$lang['porg_issue_2095'] = '[IU] hacer obvio que \"¿Quién puede ver esta foto?\" está relacionado con el nivel de privacidad';
+$lang['porg_issue_2096'] = '[búsqueda] guardar la lista de filtros';
