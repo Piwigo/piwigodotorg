5d549ddc51d03db59a5c58ec11b7e3d0dab611b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-02 11:39:44 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2081'] = '[14.1.0] Mistede menu- og kalenderikoner';
+$lang['porg_issue_2086'] = 'Forkerte ikoner når AdminTools aktiveres';
+$lang['porg_issue_2088'] = 'Autoudfylding i history.section bør ikke være følsom over for store og små bogstaver';
+$lang['porg_issue_2089'] = 'Skjul som standard knappen \"søg i dette sæt\"';
+$lang['porg_issue_2093'] = 'Fjern ny linje under import/synk. af IPTC-metadata';
+$lang['porg_issue_2094'] = '[Søgning 14.x] Selectize-listeboks er for lille';
+$lang['porg_issue_2095'] = '[UX] Gør det klart at \"Hvem kan se dette fotografi?\" er relateret til privatlivsniveau';
+$lang['porg_issue_2096'] = '[Søgning] Gem liste over filtre';
