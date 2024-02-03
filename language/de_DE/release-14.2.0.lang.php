dc54497b8f287b26fec25304f46dc1224fe79bbe
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-03 12:53:23 +0100

    Update de_DE, thanks to nobby

---
+++ language/de_DE/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2095'] = '[UX] "Wer kann dieses Foto sehen?" bezieht sich auf die Privatsphäre';
+$lang['porg_issue_2096'] = '[Suche] Liste der Filter speichern';
+$lang['porg_issue_2093'] = 'neue Zeile beim Import/Synchronisierung von IPTC-Metadaten entfernen';
+$lang['porg_issue_2094'] = '[search 14.x] Auswahl-Listenfeld ist zu kurz';
+$lang['porg_issue_2088'] = 'history.section autofill sollte nicht zwischen Groß- und Kleinschreibung unterscheiden';
+$lang['porg_issue_2089'] = 'Schaltfläche \"In diesem Set suchen\" standardmäßig ausblenden';
+$lang['porg_issue_2081'] = '[14.1.0] Menü- und Kalendersymbole verloren';
+$lang['porg_issue_2086'] = 'Falsche Symbole, wenn AdminTools aktiviert ist';
