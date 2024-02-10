8cb2792c6e53452d34579121460edbf15a433c1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-10 15:31:14 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/changelogs.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Activities'] = 'Faaliyetler';
+$lang['Admin redesign'] = 'Yöneticilerin yeniden tasarımı';
+$lang['Album editor'] = 'Albüm editörü';
+$lang['Album manager'] = 'Albüm yöneticisi';
+$lang['Dark mode'] = 'Gece modu';
+$lang['Default theme Modus'] = 'Varsayılan tema tarzı';
+$lang['Multiple-format'] = 'Çoklu format';
+$lang['New file formats'] = 'Yeni dosya biçimleri';
+$lang['Plugin manager'] = 'Eklenti yöneticisi';
+$lang['Read more'] = 'Devamını oku';
+$lang['Search engine'] = 'Arama motoru';
+$lang['See all the details'] = 'Ayrıntıları göster';
+$lang['User manager'] = 'Kullanıcı yöneticisi';
+$lang['Visit log'] = 'Günlüğü görüntüle';
+$lang['porg_changelogs_desc1'] = 'İşte tüm kararlı Piwigo sürümlerinin listesi.';
+$lang['porg_changelogs_desc2'] = 'En son sürümümüzü kullanın: daha iyi destek alacaksınız ve bilinen tüm güvenlik hataları düzeltilecektir.';
+$lang['porg_changelogs_technicals_desc1'] = 'Sürümlerin teknik görünümlerine ihtiyacınız varsa <a href="%s">Dallar ve Sürümler</a> sayfasına gidin.';
