4bf1e31388b7b371b8465a000c8b194c2e35494b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-19 09:23:31 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/get_started.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Discover Requirements'] = 'Descobriu els requisits';
+$lang['Get-now'] = 'Aconseguiu Piwigo ara';
+$lang['Installation'] = 'Instal·lació';
+$lang['Installation guide'] = 'Guia d\'instal·lació';
+$lang['Requirements'] = 'Requisits';
+$lang['Still got some unanswered questions ?'] = 'Encara teniu preguntes sense respondre?';
+$lang['get-started-download'] = 'Descarrega';
+$lang['porg_get_started_desc1'] = 'Com posar en marxa el vostre Piwigo auto-allotjat, per exemple, en el vostre propi servei d\'allotjament';
+$lang['porg_get_started_desc2'] = 'Què necessiteu preparar, què descarregar, com instal·lar-lo.';
+$lang['porg_get_started_download_desc'] = 'Descarregueu el paquet de Piwigo incloent-hi tots els fitxers, o només el script de NetInstall, que descarregarà el paquet directament al vostre allotjament.';
+$lang['porg_get_started_guide_desc'] = 'Apreneu com instal·lar Piwigo en el vostre allotjament, des de la descàrrega i les credencials de la base de dades fins a la primera connexió a la vostra biblioteca fotogràfica.';
+$lang['porg_get_started_requirement_desc1'] = 'Quins són els requisits previs per executar Piwigo al vostre allotjament?';
+$lang['porg_get_started_requirement_desc2'] = 'PHP, base de dades, servidor web, procés d\'imatges... Apreneu-ne tots els detalls.';
