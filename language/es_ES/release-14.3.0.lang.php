82598e00e833291a9adf7337bafd469b25b67db6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-02 13:20:45 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1612'] = '[Tema de Admin] La ventana de etiquetas es demasiado pequeña cuando hay varias';
+$lang['porg_issue_2005'] = 'Falla la carga desde Darktable';
+$lang['porg_issue_2079'] = 'Gestor de lotes: Intentar ejecutar una acción en todos los elementos produce un error de \"Intento de hackeo\"';
+$lang['porg_issue_2100'] = 'No se puede cargar por lotes';
+$lang['porg_issue_2102'] = '[búsqueda] No se puede pulsar en x cuando se seleccionan varios álbumes';
+$lang['porg_issue_2103'] = '[editor de álbumes] ESC abre la caja de descripción aumentada';
+$lang['porg_issue_2104'] = '[editor de álbumes 14.x] Miniatura del álbum borrosa';
+$lang['porg_issue_2105'] = '[editor de álbumes 14.x] Confusión entre el rastro de navegación y los iconos de acción';
+$lang['porg_issue_2106'] = '[Búsqueda en historial] Las líneas relacionadas con las búsquedas de galería tienen un \'detalle 1\'';
+$lang['porg_issue_2112'] = '[búsqueda] Más caracteres permitidos en la búsqueda por palabras';
+$lang['porg_issue_2117'] = 'Doble solicitud debido al selector de fechas en la página de historial';
+$lang['porg_issue_2118'] = 'Posibilidad de configurar compresión de webP animadas para cambiar el tamaño';
+$lang['porg_issue_2119'] = '[Gestor de Álbumes] Error al mover un álbum después de cambiarle el nombre';
+$lang['porg_issue_2120'] = '[Gestor de Álbumes] Error en las insignias \"nb_sub_photos\" al mover un álbum';
+$lang['porg_issue_2122'] = '[búsqueda] desplazamiento indefinido cuando falta el usuario en added_by';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Vulnerabilidad de script cruzado (Cross-Site-Scripting) en Etiqueta';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS a RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'Vulnerabilidad de CSRF + XSS almacenado para llevar a cabo una ejecución remota de código';
