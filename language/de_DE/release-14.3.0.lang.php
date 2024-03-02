3aab112b69709e7733af4e4f10204056d2939338
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-02 09:14:07 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2122'] = '[search] undefinierter Offset bei fehlendem added_by Benutzer';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Cross-Site-Scripting im Tag';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS zu RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + Stored XSS zur Ausführung von Remote Code Execution';
+$lang['porg_issue_2112'] = '[search] mehr erlaubte Zeichen bei der Suche nach Wörtern';
+$lang['porg_issue_2117'] = 'Doppelte Anfrage durch Datumsauswahl auf der Historienseite';
+$lang['porg_issue_2118'] = 'Möglichkeit zur Konfiguration der animierten webP-Kompression für die Größenänderung';
+$lang['porg_issue_2119'] = '[Album Manager] Fehler beim Verschieben eines Albums nach der Änderung seines Namens';
+$lang['porg_issue_2120'] = '[Album Manager] Fehler bei "nb_sub_photos"-Plaketten beim Verschieben eines Albums';
+$lang['porg_issue_2103'] = '[Album-Editor] ESC öffnet das vergrößerte Beschreibungsfeld';
+$lang['porg_issue_2104'] = '[14.x album editor] Unscharfe Album-Miniaturansicht';
+$lang['porg_issue_2105'] = '[14.x Album Editor] Verwechslung zwischen Breadcrumbs und Aktionssymbolen';
+$lang['porg_issue_2106'] = '[Verlaufssuche] Zeilen, die Galeriesuchen betreffen, haben ein \'Detail 1\'';
+$lang['porg_issue_1612'] = '[Admin Theme] Tags-Fenster zu klein bei mehreren Tags';
+$lang['porg_issue_2005'] = 'Hochladen aus Darktable schlägt fehl';
+$lang['porg_issue_2079'] = 'Batch Manager: Der Versuch, eine Aktion für alle Objekte auszuführen, führt zu dem Fehler "Hacking attempt".';
+$lang['porg_issue_2100'] = 'Batch-Upload nicht möglich';
+$lang['porg_issue_2102'] = '[Suche] Kann nicht auf x klicken, wenn mehrere Alben ausgewählt sind';
