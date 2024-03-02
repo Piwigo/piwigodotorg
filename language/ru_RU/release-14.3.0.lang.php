3ebce98a3b262fb3788a417239e19eb7c15f1e9b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-02 20:53:36 +0100

    Update ru_RU, thanks to nadusha

---
+++ language/ru_RU/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2106'] = '[History search] строки, касающиеся поиска в галерее, имеют \'деталь 1\'';
+$lang['porg_issue_2112'] = '[search] больше допустимых символов при поиске по словам';
+$lang['porg_issue_2117'] = 'Двойной запрос из-за наличия датапикера на странице истории';
+$lang['porg_issue_2118'] = 'возможность настроить анимированное сжатие webP для изменения размера';
+$lang['porg_issue_2119'] = '[Album Manager] Ошибка при перемещении альбома после изменения его названия';
+$lang['porg_issue_2120'] = '[Album Manager] Ошибка в значках \"nb_sub_photos\" при перемещении альбома';
+$lang['porg_issue_2122'] = '[search] неопределенное смещение при отсутствии пользователя added_by';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Межсайтовый скриптинг в теге';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS to RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + Stored XSS To Perform Remote Code Execution';
+$lang['porg_issue_1612'] = '[Admin Theme] Окно тегов становится маленьким при использовании нескольких тегов';
+$lang['porg_issue_2005'] = 'Загрузка из Darktable не удается';
+$lang['porg_issue_2079'] = 'Пакетный менеджер: Попытка запустить действие для всех элементов приводит к ошибке \"Попытка взлома\".';
+$lang['porg_issue_2100'] = 'Невозможно выполнить пакетную загрузку';
+$lang['porg_issue_2102'] = '[search] Невозможно нажать на x, если выбрано несколько альбомов';
+$lang['porg_issue_2103'] = '[редактор альбомов] ESC открывает увеличенное поле описания';
+$lang['porg_issue_2104'] = '[14.x album editor] размытые миниатюры альбомов';
+$lang['porg_issue_2105'] = '[14.x album editor] путаница между иконками и значками действий';
