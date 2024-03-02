8ecf6991e41c74af1c0bc37963f50753076c82b9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-02 13:38:30 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + opgeslagen XSS om uitvoering van externe code uit te voeren';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS naar RCE';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Cross-site scripting in tag';
+$lang['porg_issue_2122'] = '[zoeken] ongedefinieerde offset bij ontbrekend toegevoegde_door gebruiker';
+$lang['porg_issue_2120'] = '[Albumbeheer] Bug op \"nb_sub_photos\"-badges bij het verplaatsen van een album';
+$lang['porg_issue_2119'] = '[Albumbeheer] Bug bij het verplaatsen van een album nadat de naam is gewijzigd';
+$lang['porg_issue_2118'] = 'mogelijkheid om geanimeerde webP-compressie te configureren voor formaatwijziging';
+$lang['porg_issue_2117'] = 'Dubbel verzoek vanwege datumkiezer op geschiedenispagina';
+$lang['porg_issue_2112'] = '[zoeken] meer toegestane tekens bij zoeken op woorden';
+$lang['porg_issue_2106'] = '[Geschiedenis zoeken] regels met betrekking tot galerijzoekopdrachten hebben een \'detail 1\'';
+$lang['porg_issue_2105'] = '[14.x albumeditor] verwarring tussen broodkruimels en actiepictogrammen';
+$lang['porg_issue_2104'] = '[14.x albumeditor] wazige albumminiatuur';
+$lang['porg_issue_2103'] = '[albumeditor] ESC opent het vergrote beschrijvingsvak';
+$lang['porg_issue_2102'] = '[zoeken] Kan niet op x klikken als er meerdere albums zijn geselecteerd';
+$lang['porg_issue_2100'] = 'Kan niet batchgewijs uploaden';
+$lang['porg_issue_2079'] = 'Batchbeheer: pogingen om actie uit te voeren op alle items leiden tot de fout \'hackpoging\'';
+$lang['porg_issue_2005'] = '[Uploaden vanuit Darktable mislukt';
+$lang['porg_issue_1612'] = '[Admin Theme] Tagsvenster te klein met meerdere tags';
