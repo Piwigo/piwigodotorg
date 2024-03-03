c7a4b1209f39ec2d2ea41a6e6775ba5611048504
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-03 14:18:40 +0100

    Update fr_FR, thanks to laurent.duretz

---
+++ language/fr_FR/release-14.3.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS vers RCE';
+$lang['porg_issue_2005'] = 'L\'envoi depuis Darktable a échoué';
