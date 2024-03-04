fdb82f7276474409a42014bdc007874dc69754bf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-04 09:18:41 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2005'] = 'Fallimento del caricamento da Darktable.';
+$lang['porg_issue_2079'] = 'Batch Manager: Tentando di eseguire azioni su tutti gli oggetti causa l\'errore \"Hacking attempt\".';
+$lang['porg_issue_2100'] = 'Impossibile caricare in serie.';
+$lang['porg_issue_2102'] = '[ricerca] Impossibile cliccare sulla x quando sono selezionati diversi album.';
+$lang['porg_issue_2103'] = '[modifica album] ESC apre la finestra della descrizione ingrandita.';
+$lang['porg_issue_2104'] = '[14.x modifica album] anteprima album offuscata.';
+$lang['porg_issue_2105'] = '[14.x modifica album]confusione fra briciole di pane e icone azioni.';
+$lang['porg_issue_2106'] = '[Storico ricerca] le righe relative alle ricerche in galleria hanno un \'dettaglio 1\'.';
+$lang['porg_issue_2112'] = '[ricerca] permessi più caratteri nella ricerca per parole.';
+$lang['porg_issue_2117'] = 'Doppia richiesta a causa del datepicker nella pagina della cronologia.';
+$lang['porg_issue_2118'] = 'possibilità di configurare la compressione webP animata per il ridimensionamento.';
+$lang['porg_issue_2119'] = '[Gestione album] Bug quando un album viene spostato dopo averne cambiato il nome.';
+$lang['porg_issue_2120'] = '[Gestione album] Bug sul badge \"nb_sub_photos\" quando un album viene spostato.';
+$lang['porg_issue_2122'] = '[ricerca] offset indefinito in caso di mancanza di "added_by user".';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Cross-Site Scripting in Etichette.';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS to RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + Stored XSS Per eseguire codice remoto.';
+$lang['porg_issue_1612'] = '[Tema amministrativo] Finestra delle etichette troppo piccola per molteplici etichette.';
