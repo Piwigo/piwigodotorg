5f29be343c6888a2e8f70de592ddd2ac791d7fbd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-04 08:09:50 +0100

    Update fr_FR, thanks to laurent.duretz

--- language/fr_FR/release-14.3.0.lang.php
+++ language/fr_FR/release-14.3.0.lang.php
@@ -7,3 +7,19 @@
 // +-----------------------------------------------------------------------+
 $lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS vers RCE';
 $lang['porg_issue_2005'] = 'L\'envoi depuis Darktable a échoué';
+$lang['porg_issue_2100'] = 'Impossible de téléverser par lot';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + Stored XSS pour exécuter du code à distance';
+$lang['porg_issue_1612'] = '[Thème Admin] fenêtre des tags trop petite avec de multiples tags';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Cross-Site Scripting dans les tags';
+$lang['porg_issue_2118'] = 'Possibilité de configurer la compression pour les webP animés';
+$lang['porg_issue_2117'] = 'Double requête due au sélectionneur de date sur la page d\'historique';
+$lang['porg_issue_2112'] = '[Recherche] plus de caractères autorisés dans la recherche par mots';
+$lang['porg_issue_2106'] = '[Historique de recherche] les lignes concernant les recherches de galeries ont un \'detail1\'';
+$lang['porg_issue_2105'] = '[Éditeur d\'album 14.x] confusion entre le fil d\'Ariane et les icônes d\'action';
+$lang['porg_issue_2104'] = '[Éditeur d\'album 14.x] Icône d\'album floue';
+$lang['porg_issue_2103'] = '[Éditeur d\'album] ESC ouvre la boite de description';
+$lang['porg_issue_2079'] = '[gestion par lot] Lancer une action sur tous les éléments entraîne l\'erreur \"Tentative de hack\" ';
+$lang['porg_issue_2102'] = '[Recherche] impossible de cliquer sur x lorsque plusieurs albums sont sélectionnés';
+$lang['porg_issue_2122'] = '[Recherche] offset indéfini quand \"Ajouté par l\'utilisateur\" est absent';
+$lang['porg_issue_2120'] = '[Gestion d\'album] Bug sur les badges \"nb_sub_photos\" au déplacement d\'un album';
+$lang['porg_issue_2119'] = '[Gestion d\'album] Bug lors du déplacement d\'un album après avoir changé son nom';
