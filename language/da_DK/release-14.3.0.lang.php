14fbb35a03f0b0561b6b5696f13b51e5bb59cd45
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-04 11:42:54 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1612'] = '[Admintema] Tags-vinduet er for lille ved flere tags';
+$lang['porg_issue_2005'] = 'Upload fra Darktable fejler';
+$lang['porg_issue_2079'] = '[Batch Manager] Forsøg på at udføre handling på alle enheder medfører \"Hacking attempt\"-fejl';
+$lang['porg_issue_2100'] = 'Ikke i stand til at foretage batchupload';
+$lang['porg_issue_2102'] = '[Søgning] Ikke muligt at klikke på X når flere albummer er valgt';
+$lang['porg_issue_2103'] = '[Albumeditor] Escape åbner den forstørrede beskrivelsesboks';
+$lang['porg_issue_2104'] = '[14.x-albumeditor] Utydeligt albums miniaturebillede';
+$lang['porg_issue_2105'] = '[14.x-albumeditor] Forvirring mellem brødkrummer og handlingsikoner';
+$lang['porg_issue_2106'] = '[Historiksøgning] Linjer vedrørende gallerysøgninger har en \'detail 1\'';
+$lang['porg_issue_2112'] = '[Søgning] Flere tilladte tegn i søgning efter ord';
+$lang['porg_issue_2117'] = 'Dobbelt forespørgsel på grund af datovælger på historikside';
+$lang['porg_issue_2118'] = 'Mulighed for at opsætte animeret webP-komprimering til ændring af størrelse';
+$lang['porg_issue_2119'] = '[Albumhåndtering] Fejl ved flytning af album efter at have ændret dets navn';
+$lang['porg_issue_2120'] = '[Albumhåndtering] Fejl i \"nb_sub_photos\"-badges ved flytning af et album';
+$lang['porg_issue_2122'] = '[Søgning] Udefineret offset når added_by-bruger mangler';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Udførelse af scripts på tværs af websteder i tag';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS til RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + Stored XSS til fjernudførelse af kode';
