680d8d6a217f3b528e013611bb3beed8ffb159b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-05 18:27:02 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'Uzaktan Kod Yürütmeyi Gerçekleştirmek için CSRF + Depolanan XSS';
+$lang['porg_issue_2120'] = '[Album Manager] Bir albümü taşırken \"nb_sub_photos\" rozetleriyle ilgili hata';
+$lang['porg_issue_2119'] = '[Album Manager] Adını değiştirdikten sonra bir albümü taşırken hata oluştu';
+$lang['porg_issue_2112'] = '[search] kelimelere göre aramada daha fazla izin verilen karakter';
+$lang['porg_issue_2106'] = '[History search] galeri aramalarıyla ilgili satırlarda \'ayrıntı 1\' var';
+$lang['porg_issue_2105'] = '[14.x album editor] içerik kırıntıları ve eylem simgeleri arasındaki karışıklık';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS \'den RCE';
+$lang['porg_issue_2122'] = '[search] kullanıcı tarafından add_by eksik olduğunda tanımsız ofset';
+$lang['porg_issue_2117'] = 'Geçmiş sayfasındaki tarih seçici nedeniyle çift istek';
+$lang['porg_issue_2104'] = '[14.x album editor] bulanık albüm küçük resmi';
+$lang['porg_issue_2103'] = '[album editor] ESC büyütülmüş açıklama kutusunu açar';
+$lang['porg_issue_2102'] = '[search] Birkaç albüm seçildiğinde x\'e tıklanamıyor';
+$lang['porg_issue_2118'] = 'yeniden boyutlandırma için animasyonlu webP sıkıştırmasını yapılandırma yeteneği';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Etikette Siteler Arası Komut Dosyası Çalıştırma';
+$lang['porg_issue_2079'] = 'Toplu İş Yöneticisi: Tüm öğeler üzerinde işlem çalıştırmaya çalışmak \"Hackleme girişimi\" hatasına yol açıyor';
+$lang['porg_issue_2005'] = 'Darktable\'dan yükleme başarısız oluyor';
+$lang['porg_issue_1612'] = '[Admin Theme] Birden fazla etiket içeren etiketler penceresi çok küçük';
+$lang['porg_issue_2100'] = 'Toplu yükleme yapılamıyor';
