070442605d6b0a1837fe4683d30c7181f7985d86
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-07 20:12:39 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/release-13.7.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1876'] = '[Geçmiş sayfası] 13.5.0\'da SQL enjeksiyonu sorunu bulundu';
+$lang['porg_issue_1878'] = '[Kullanıcı Yöneticisi] Kullanıcın durumu çevrilmedi';
+$lang['porg_issue_1879'] = '[Karanlık mod] Karanlık modda simge stilini değiştirme';
+$lang['porg_issue_1880'] = '[Kullanıcı Yöneticisi] E-posta düzenleme, kullanıcı düzenleyicisini engeller';
+$lang['porg_issue_1883'] = 'etiket düzenleme, iptal düğmesi çalışmıyor';
+$lang['porg_issue_1887'] = '[Albüm Editörü] Albüm silme çalışmıyor';
+$lang['porg_issue_1901'] = '[Karanlık mod] Renk güncellemeleri';
+$lang['porg_issue_1905'] = '[Geçmiş sayfası] Doğrudan düzenleme sayfalarına gitmek için resimleri tıklanabilir hale getirin';
+$lang['porg_issue_1907'] = '[da_DK] piwigo.org\'den haber gelmez';
+$lang['porg_issue_1910'] = 'piwigo\'nun \"permalinks\" fonksiyonunda SQL enjeksiyonu imkanı bulunmaktadır';
+$lang['porg_issue_1911'] = 'piwigo\'nun \"profile\" fonksiyonunda SQL enjeksiyonu imkanı bulunmaktadır';
+$lang['porg_issue_1914'] = '[13.7.0] Albüm sıralamasında hata veriyor';
