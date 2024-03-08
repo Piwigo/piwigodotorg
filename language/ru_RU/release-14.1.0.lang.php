4231f0660f8f3b3dbeac3c123f002a69e819604c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-08 14:42:19 +0100

    Update ru_RU, thanks to evadim

---
+++ language/ru_RU/release-14.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2077'] = 'Ошибка Javascript в Piwigo v14 в итальянском переводе';
+$lang['porg_issue_2076'] = '[поиск 14.x] невозможно очистить фильтр поиска альбомов';
+$lang['porg_issue_2074'] = '[установка] ошибка автоматической активации плагинов в PHP 7.4+';
+$lang['porg_issue_2072'] = '[Редактор Альбомов 14.x] Поле Описание слишком маленькое';
+$lang['porg_issue_2066'] = '[14.x] сохранена совместимость с PHP 7.0';
