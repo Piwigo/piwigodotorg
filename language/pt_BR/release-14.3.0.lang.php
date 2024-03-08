d88edc2b52b37f7daea4978038d9b48037a9bba9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-08 16:15:32 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1612'] = '[Tema do administrador] Janela de tags muito pequena com várias tags';
+$lang['porg_issue_2005'] = 'O upload do Darktable falhou';
+$lang['porg_issue_2079'] = 'Gerenciador de lote: tentar executar uma ação em todos os itens leva ao erro "Tentativa de hacking"';
+$lang['porg_issue_2100'] = 'Não foi possível fazer upload em lote';
+$lang['porg_issue_2102'] = '[pesquisa] Não é possível clicar em x quando vários álbuns são selecionados';
+$lang['porg_issue_2103'] = '[editor do álbum] ESC abre a caixa de descrição ampliada';
+$lang['porg_issue_2104'] = '[Editor de álbum 14.x] miniatura borrada do álbum';
+$lang['porg_issue_2105'] = '[Editor de álbum 14.x] confusão entre localização atual e ícones de ação';
+$lang['porg_issue_2106'] = 'As linhas de [pesquisa no histórico] relativas às pesquisas na galeria têm um \'detalhe1\'';
+$lang['porg_issue_2112'] = '[pesquisar] mais caracteres permitidos na busca por palavras';
+$lang['porg_issue_2117'] = 'Solicitação dupla devido ao selecionador de data na página de histórico';
+$lang['porg_issue_2118'] = 'capacidade de configurar compactação webP animada para redimensionamento';
+$lang['porg_issue_2119'] = '[Album Manager] Bug ao mover um álbum após alterar seu nome';
+$lang['porg_issue_2120'] = '[Gerenciador de Álbum] Bug nos emblemas \"nb_sub_photos\" ao mover um álbum';
+$lang['porg_issue_2122'] = '[pesquisa] deslocamento indefinido quando falta o usuário add_by';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Scripting entre sites em tag';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS para RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + XSS armazenado para executar execução remota de código';
