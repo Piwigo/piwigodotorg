d98a70ad8d4a5d07ed0aa1da33c23af723c9f4ed
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-16 08:15:49 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/mobile_applications.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['mobile_applications_user_rights_title'] = 'Kullanıcı hakları';
+$lang['mobile_applications_user_rights_texte'] = 'Piwigo\'da yapılandırılan yetkiler mobil uygulamada da geçerlidir: her kullanıcı yalnızca hak sahibi olduğu albümleri görebilir.';
+$lang['mobile_applications_title'] = 'iOS ve Android için Piwigo mobil uygulaması';
+$lang['mobile_applications_sort_photos_title'] = 'Fotoğraflarınızı hassas bir şekilde yönetin';
+$lang['mobile_applications_sort_photos_texte'] = 'Piwigo mobil uygulamasından fotoğraflarınızın tüm detaylarına erişebilir ve bunları değiştirebilirsiniz: başlık, açıklama, etiketler, yazar... Ayrıca fotoğraflarınızı sınıflandırabilir, cep telefonunuza indirebilir veya silebilirsiniz.';
+$lang['mobile_applications_size_quality_title'] = 'Boyut ve kalite';
+$lang['mobile_applications_size_quality_texte'] = 'Fotoğraflarınızın boyutunu ve sunucuda tutmak istediğiniz kaliteyi değiştirin.';
+$lang['mobile_applications_share_title'] = 'Fotoğraflarınızı ve videolarınızı paylaşın';
+$lang['mobile_applications_share_texte'] = 'Entegre paylaşım menüsü sayesinde fotoğraflarınızı ve videolarınızı doğrudan Piwigo mobil uygulamasından kişilerinizle paylaşın.';
+$lang['mobile_applications_security_title'] = 'Güvenlik ve gizlilik';
+$lang['mobile_applications_scan_qr'] = 'Bu QR kodunu telefonunuzla tarayın!';
+$lang['mobile_applications_metadata_title'] = 'Meta veri';
+$lang['mobile_applications_metadata_texte'] = 'Dosyalarınızın tüm meta verilerine erişin ve gerekirse bunları değiştirin: başlık, yazar, tarih, gizlilik, etiketler, açıklama...';
+$lang['mobile_applications_invite_title'] = 'Akrabalarınızı ve meslektaşlarınızı davet edin';
+$lang['mobile_applications_invite_texte'] = 'Piwigo mobil uygulaması, yönetici olmayan kullanıcıların da Piwigo\'ya fotoğraf göndermesine olanak tanıyan Topluluk eklentisiyle uyumludur.';
+$lang['mobile_applications_introduction'] = 'Piwigo aynı zamanda akıllı telefonunuzda! Fotoğraf kitaplığınızı yönetmek ve birçok özellikten yararlanmak için ücretsiz bir mobil uygulama.';
+$lang['mobile_applications_import_title'] = 'Fotoğraflarınızı ve videolarınızı içe aktarın';
+$lang['mobile_applications_import_text'] = 'Piwigo mobil uygulaması ile istediğiniz zaman cep telefonunuzdan fotoğraf kütüphanenize fotoğraf ekleyin. Yeni albümler oluşturun veya mevcut albümlerinize ekleyin. Ayrıca galerinizde bu seçenek varsa (VideoJS eklentisiyle uyumlu) videoları da içe aktarın.';
+$lang['mobile_applications_geolocalisation_texte'] = 'Fotoğraflarınızı galerinizdeki bir haritada görüntüleyebilmek için otomatik olarak GPS koordinatlarını alın.';
+$lang['mobile_applications_follows_you_title'] = 'Fotoğraf kitaplığınız sizi her yerde takip ediyor';
+$lang['mobile_applications_follows_you_texte'] = 'Piwigo mobil uygulamasıyla fotoğraf kütüphanenize istediğiniz zaman erişin! Tüm dosyalarınızı telefonunuzda saklamanıza gerek yok: tüm albümlerinizi, fotoğraflarınızı ve videolarınızı mobil uygulamadan bulun.';
+$lang['mobile_applications_download_play_store2'] = 'veya uygulamayı App Store\'dan indirin';
+$lang['mobile_applications_download_app_store2'] = 'veya uygulamayı Play Store\'dan indirin';
+$lang['mobile_applications_copy_move_title'] = 'Fotoğraflarınızı ve videolarınızı kopyalayın ve taşıyın';
+$lang['mobile_applications_copy_move_texte'] = 'iOS için Piwigo mobil uygulamasıyla fotoğraflarınızı kolayca düzenleyebilir ve yeniden düzenleyebilir, hangi albümde olduklarını değiştirebilir veya bir albümden diğerine kopyalayabilirsiniz.';
+$lang['mobile_applications_control_texte'] = 'Fotoğraflarınızı uygulamadan paylaşırken meta verilerini iletmek isteyip istemediğinizi belirleyin.';
+$lang['mobile_applications_confidentiality_title'] = 'Gizlilik';
+$lang['mobile_applications_confidentiality_texte'] = 'Piwigo\'da her zaman olduğu gibi verileriniz yalnızca size aittir. Takip yok, kişisel verilerinizin paylaşımı yok, dosyalarınıza bakacak algoritma yok.';
+$lang['mobile_applications_albums_texte'] = 'Piwigo mobil uygulamasından albümleriniz üzerinde tam kontrole sahip olursunuz. Albümleri oluşturabilir, değiştirebilir, uygun gördüğünüz şekilde yeniden adlandırabilirsiniz. Ayrıca her albümün parametrelerini de tanımlayabilirsiniz: ad, açıklama, küçük resim ...';
+$lang['mobile_applications_albums_title'] = 'Albümler her zaman iyi sınıflandırılmıştır';
+$lang['mobile_applications_geolocalisation_title'] = 'Fotoğraflarınıza coğrafi etiketleme';
+$lang['mobile_applications_import_options_title'] = 'Fotoğraflarınız için içe aktarma seçenekleri';
+$lang['mobile_applications_download_play_store1'] = 'iOS için İndir';
+$lang['mobile_applications_download_app_store1'] = 'Android için İndir';
+$lang['mobile_applications_download'] = 'Piwigo app İndir';
+$lang['mobile_applications_control_title'] = 'Veri kontrol';
+$lang['ios_only'] = 'Sadece iOS';
