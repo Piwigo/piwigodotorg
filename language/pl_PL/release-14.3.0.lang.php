828a980af79c69b6952538c008e1c265188acfa8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-17 18:14:08 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + przechowywany XSS w celu zdalnego wykonania kodu';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS do RCE';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = 'Skrypty między witrynami dla tagów';
+$lang['porg_issue_2122'] = '[Wyszukiwanie] Niezdefiniowane przesunięcie w przypadku braku added_by user';
+$lang['porg_issue_2120'] = '[Menedżer albumów] Błąd w plakietkach „nb_sub_photos” podczas przenoszenia albumu';
+$lang['porg_issue_2119'] = '[Menedżer albumów] Błąd podczas przenoszenia albumu po zmianie jego nazwy';
+$lang['porg_issue_2118'] = 'Możliwość skonfigurowania animowanej kompresji webP dla zmiany rozmiaru';
+$lang['porg_issue_2117'] = 'Podwójne żądanie z powodu wyboru daty na stronie historii';
+$lang['porg_issue_2112'] = '[Wyszukiwanie] Więcej dozwolonych znaków w wyszukiwaniu słów';
+$lang['porg_issue_2106'] = '[Wyszukiwanie w historii] Linie dotyczące przeszukiwania galerii mają „szczegół 1”';
+$lang['porg_issue_2105'] = '[Edytor albumów 14.x] nieścisłość pomiędzy historią operacji (okruszkami)a ikonami akcji';
+$lang['porg_issue_2104'] = '[Edytor albumów 14.x] niewyraźna miniatura albumu';
+$lang['porg_issue_2103'] = '[Edytor albumu] ESC otwiera powiększone okno opisu';
+$lang['porg_issue_2102'] = '[Wyszukiwanie] Nie można kliknąć x, gdy wybranych jest kilka albumów';
+$lang['porg_issue_2100'] = 'Nie można przesłać wsadowo';
+$lang['porg_issue_2005'] = 'Niepowodzenie przesyłania z Darktable';
+$lang['porg_issue_2079'] = 'Menedżer wsadowy: próba wykonania akcji na wszystkich elementach prowadzi do błędu „Próba włamania”.';
+$lang['porg_issue_1612'] = '[Motyw Admin] Za małe okno dla wielu tagów';
