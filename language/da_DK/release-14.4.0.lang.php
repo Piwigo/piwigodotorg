ae2ec879e3523fa50a267dcce4991246009d68a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-18 06:17:16 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = 'Nedbrud ved upload af et fotografi, som allerede findes';
+$lang['porg_issue_2129'] = '[brugeraktivitet] Brugerfilterboksen var større end containeren';
+$lang['porg_issue_2131'] = '[søgning 14.2+] Manglende ikon i albumvælger';
+$lang['porg_issue_2134'] = 'Flere end en mailbesked når en ny version af Piwigo er tilgængelig';
+$lang['porg_issue_2136'] = '[søgning 14.2+] Sægning efter ord hvor der er match på flere end to ord giver intet resultat';
+$lang['porg_issue_2138'] = '[rediger album] Det er ikke indlysende hvordan håndtering af albummers fotoikoner fungerer';
+$lang['porg_issue_2140'] = '[PHP 8.x] Fejl i menuen';
+$lang['porg_issue_2142'] = 'Data for langt til kolonnen \'tag_ids\'';
+$lang['porg_issue_2145'] = '[PHP 8.x] exifs ModeArray-felt medfører at billedsiden ikke virker';
+$lang['porg_issue_2146'] = '[søgning] Foran- eller bagvedstillede mellemrum efter søgeresultater';
+$lang['porg_issue_2149'] = '[albumhåndtering] Ikke i stand til at oprette underalbum efter omdøbning';
