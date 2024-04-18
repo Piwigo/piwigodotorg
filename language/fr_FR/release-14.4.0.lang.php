b00cdc7dd6a4a2c49807443ab5d520eb0fdcc013
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-18 06:17:13 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2149'] = '[Gestion des albums] Impossible de créer un sous-album après un renommage';
+$lang['porg_issue_2146'] = '[Recherche] Les espaces en tête ou en fin de saisie modifient les résultats de la recherche';
+$lang['porg_issue_2145'] = '[PHP 8.x] Le champ ModeArray de l\'exif casse la page de l\'image';
+$lang['porg_issue_2142'] = 'Données trop longues pour la colonne \'tag_ids\'';
+$lang['porg_issue_2140'] = '[PHP 8.x] Erreurs dans le menu';
+$lang['porg_issue_2136'] = '[search 14.x] La recherche par mot sur 2 mots correspondant à des mots-clés ne donne aucun résultat';
+$lang['porg_issue_2129'] = '[activité de l\'utilisateur] La boîte de filtre de l\'utilisateur dépasse la taille du conteneur';
+$lang['porg_issue_2134'] = 'Plusieurs notifications par email au sujet d\'une nouvelle version de Piwigo disponible';
+$lang['porg_issue_2138'] = '[Édition d\'album] L\'icône pour l\'action "Gérer les photos de l\'album" ne renseigne pas bien sur son usage.';
+$lang['porg_issue_2131'] = '[Recherche 14.2+] Icône manquante dans le sélecteur d\'album';
+$lang['porg_issue_2110'] = 'Plantage lors du téléversement d\'une photo déjà présente';
