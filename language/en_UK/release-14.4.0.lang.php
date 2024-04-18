f898046a356a88caaf77efba47c520d1ee763a8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-18 09:01:24 +0200

    Update en_UK, thanks to ddtddt

--- language/en_UK/release-14.4.0.lang.php
+++ language/en_UK/release-14.4.0.lang.php
@@ -4,10 +4,9 @@ $lang['porg_issue_2146'] = '[search] leading or trailing spaces alter search res
 $lang['porg_issue_2145'] = '[PHP 8.x] exif ModeArray field breaks picture page';
 $lang['porg_issue_2142'] = 'Data too long for column \'tag_ids\'';
 $lang['porg_issue_2140'] = '[PHP 8.x] errors on menu';
-$lang['porg_issue_2138'] = '[edit album] manage album photos icon not ovious to its use';
+$lang['porg_issue_2138'] = '[edit album] manage album photos icon not obvious to its use';
 $lang['porg_issue_2136'] = '[search 14.x] search by word on 2 words matching tags gives no result';
 $lang['porg_issue_2134'] = 'more than one email notification on new Piwigo version available';
 $lang['porg_issue_2131'] = '[Search 14.2+] Missing icon in album selector';
 $lang['porg_issue_2129'] = '[user activity] user filter box exceeds container size';
 $lang['porg_issue_2110'] = 'Crashes when uploading a photo that is already present';
-?>
