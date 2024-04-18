56bf6b5e92ac51e764f4e51560d5bae2b12ba7d6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-18 06:17:19 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2145'] = '[PHP 8.x] exif ModeArray Feld bricht Bildseite';
+$lang['porg_issue_2146'] = '[Suche] Führende oder abschließende Leerzeichen verändern die Suchergebnisse';
+$lang['porg_issue_2149'] = '[album manager] Unteralbum kann nach Umbenennung nicht erstellt werden';
+$lang['porg_issue_2138'] = '[Album bearbeiten] Album verwalten Fotos Symbol nicht offensichtlich zu seiner Verwendung';
+$lang['porg_issue_2140'] = '[PHP 8.x] Fehler im Menü';
+$lang['porg_issue_2142'] = 'Daten zu lang für Spalte \'tag_ids\'';
+$lang['porg_issue_2131'] = '[Suche 14.2+] Fehlendes Symbol in der Albumauswahl';
+$lang['porg_issue_2134'] = 'mehr als eine E-Mail-Benachrichtigung über eine neue Piwigo-Version verfügbar';
+$lang['porg_issue_2136'] = '[search 14.x] Wortsuche nach 2 Wörtern mit übereinstimmenden Tags ergibt kein Ergebnis';
+$lang['porg_issue_2110'] = 'Absturz beim Hochladen eines Fotos, das bereits vorhanden ist';
+$lang['porg_issue_2129'] = '[Benutzeraktivität] Benutzerfilterbox überschreitet Containergröße';
