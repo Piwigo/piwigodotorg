9be4719ae7c3e215200034f795b2dcf03225f7b5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-18 11:19:01 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2149'] = '[album manager] impossibile creare un sottoalbum dopo aver cambiano il nome.';
+$lang['porg_issue_2146'] = '[search] gli spazi iniziali o finali alterano i risultati della ricerca.';
+$lang['porg_issue_2145'] = '[PHP 8.x] campo exif ModeArray interrompe la pagina dell\'immagine.';
+$lang['porg_issue_2142'] = 'Dati troppo lunghi per la colonna \'tag_ids\'.';
+$lang['porg_issue_2131'] = '[Search 14.2+] Icona mancante nella selezione dell\'album.';
+$lang['porg_issue_2140'] = 'PHP 8.x] errori nel menù.';
+$lang['porg_issue_2138'] = '[edit album] l\'icona di gestione delle fotografie degli album non è di uso così immediato.';
+$lang['porg_issue_2136'] = '[search 14.x] la ricerca per parola, su 2 parole corrispondenti ai tag, non fornisce risultati.';
+$lang['porg_issue_2134'] = 'disponibile più di una notifica via email sulla nuova versione disponibile di Piwigo.';
+$lang['porg_issue_2110'] = 'Crasha quando viene caricata una fotografia già esistente.';
+$lang['porg_issue_2129'] = 'user activity]la casella del filtro utente supera le dimensioni del contenitore.';
