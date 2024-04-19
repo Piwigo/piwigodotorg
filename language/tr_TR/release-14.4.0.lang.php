8bfd436cde16ebff9051f59eec290d5e928280c0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-19 07:04:46 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2149'] = '[albüm yöneticisi] yeniden adlandırdıktan sonra alt albüm oluşturulamıyor';
+$lang['porg_issue_2146'] = '[arama] baştaki veya sondaki boşluklar arama sonuçlarını değiştirir';
+$lang['porg_issue_2145'] = '[PHP 8.x] exif ModeArray alanı resim sayfasını kesiyor';
+$lang['porg_issue_2142'] = '\'tag_ids\' sütunu için veriler çok uzun';
+$lang['porg_issue_2140'] = '[PHP 8.x] menüde hatalar';
+$lang['porg_issue_2138'] = '[albümü düzenle] albüm fotoğraflarını yönet simgesinin kullanımı açık değil';
+$lang['porg_issue_2136'] = '[Arama 14.x] etiketlerle eşleşen 2 kelime üzerinde kelimeye göre arama sonuç vermiyor';
+$lang['porg_issue_2134'] = 'Yeni Piwigo sürümüne ilişkin birden fazla e-posta bildirimi mevcut';
+$lang['porg_issue_2131'] = '[Arama 14.2+] Albüm seçicisinde ikon eksik';
+$lang['porg_issue_2129'] = '[kullanıcı etkinliği] kullanıcı filtre kutusu kapsayıcı boyutunu aşıyor';
+$lang['porg_issue_2110'] = 'Zaten mevcut bir fotoğraf yüklenirken kilitleniyor';
