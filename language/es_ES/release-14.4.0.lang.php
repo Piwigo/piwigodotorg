e8df7c9d623ed09d06c86b613b62d2b734e8ccbd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-19 07:04:43 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = 'Se detiene cuando se carga una foto que ya está presente';
+$lang['porg_issue_2129'] = '[actividad de usuario] la caja del filtro de usuario excede el tamaño del contenedor';
+$lang['porg_issue_2131'] = '[Búsqueda 14.2+] Falta el icono en el selector de álbumes';
+$lang['porg_issue_2134'] = 'más de una notificación por correo electrónico cuando hay disponible una nueva versión de Piwigo';
+$lang['porg_issue_2136'] = '[búsqueda 14.x] buscar por palabra con dos palabras que coinciden con etiquetas no da resultados';
+$lang['porg_issue_2138'] = '[editar álbum] el icono de gestionar el álbum de fotos no es obvio para el usuario';
+$lang['porg_issue_2140'] = '[PHP 8.x] errores en el menú';
+$lang['porg_issue_2142'] = 'Datos demasiado largos para la columna \'tag_ids\'';
+$lang['porg_issue_2145'] = '[PHP 8.x] el campo exif ModeArray estropea la página de la foto';
+$lang['porg_issue_2146'] = '[búsqueda] los espacios iniciales o finales alteran los resultados de las búsquedas';
+$lang['porg_issue_2149'] = '[gestor de álbumes] no se puede crear un sub-álbum después de renombrar';
