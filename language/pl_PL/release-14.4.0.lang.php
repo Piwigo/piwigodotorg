d88e8a3662ccbf630cc297be68ca38c32c16c88d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-05-27 09:29:12 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = 'Awarie w czasie przesyłania istniejącego już zdjęcia';
+$lang['porg_issue_2129'] = '[aktywność użytkownika] Pole filtra użytkownika przekracza rozmiar kontenera';
+$lang['porg_issue_2131'] = '[Wyszukiwanie 14.2+] Brakująca ikona dla wyboru albumów';
+$lang['porg_issue_2134'] = 'więcej niż jedno powiadomienie e-mail o dostępności nowej wersji Piwigo';
+$lang['porg_issue_2136'] = '[Wyszukiwanie 14.x] Wyszukiwanie według słów: dla 2 słów pasujących do tagów nie daje rezultatu';
+$lang['porg_issue_2138'] = '[Edycja albumu] Ikona zarządzania zdjęciami albumu nie jest oczywista w użyciu';
+$lang['porg_issue_2140'] = '[PHP 8.x] Błędy w menu';
+$lang['porg_issue_2142'] = 'Zbyt długie dane dla kolumny „tag_ids”';
+$lang['porg_issue_2145'] = '[PHP 8.x] Pole exif ModeArray dzieli stronę zdjęcia';
+$lang['porg_issue_2146'] = '[Wyszukiwanie] Spacje początkowe lub końcowe wpływają na wyniki wyszukiwania';
+$lang['porg_issue_2149'] = '[Menedżer albumów] Nie może utworzyć podalbumu po zmianie nazwy';
