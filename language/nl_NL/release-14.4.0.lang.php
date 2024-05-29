2a28b7d17dceeefbf0c5bfc5f22171f985372428
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-05-29 06:29:57 +0200

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = 'Crasht bij het uploaden van een foto die al aanwezig is';
+$lang['porg_issue_2129'] = '[user activity] gebruiker filter box overschrijdt container grootte';
+$lang['porg_issue_2131'] = '[Search 14.2+] Ontbrekend pictogram in albumkiezer';
+$lang['porg_issue_2134'] = 'meer dan één e-mailmelding over nieuwe Piwigo-versie beschikbaar';
+$lang['porg_issue_2136'] = '[search 14.x] zoeken op woord op 2 woorden die overeenkomen met tags geeft geen resultaat';
+$lang['porg_issue_2138'] = '[edit album] beheer album foto\'s pictogram niet duidelijk voor het gebruik ervan';
+$lang['porg_issue_2140'] = '[PHP 8.x] fouten in menu';
+$lang['porg_issue_2142'] = 'Gegevens te lang voor kolom \'tag_ids';
+$lang['porg_issue_2145'] = '[PHP 8.x] exif ModeArray-veld breekt fotopagina';
+$lang['porg_issue_2146'] = '[search] spaties voor of achter veranderen zoekresultaten';
+$lang['porg_issue_2149'] = '[album manager] kan geen subalbum maken na hernoemen';
