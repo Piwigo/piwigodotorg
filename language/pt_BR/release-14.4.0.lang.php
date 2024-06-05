8ae728ae6adb3d7264ba3f007324dd02ae44e4cb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-06-05 18:55:40 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = 'Falha ao enviar uma foto que já está presente';
+$lang['porg_issue_2129'] = 'A caixa de filtro do usuário [atividade do usuário] excede o tamanho do contêiner';
+$lang['porg_issue_2131'] = '[Pesquisa 14.2+] Ícone ausente no seletor de álbum';
+$lang['porg_issue_2134'] = 'mais de uma notificação por e-mail sobre a nova versão do Piwigo disponível';
+$lang['porg_issue_2136'] = '[pesquisa 14.x] pesquisa por palavra em tags correspondentes de 2 palavras não dá resultado';
+$lang['porg_issue_2138'] = '[editar álbum] ícone de gerenciamento de fotos do álbum não é óbvio para seu uso';
+$lang['porg_issue_2140'] = 'Erros [PHP 8.x] no menu';
+$lang['porg_issue_2142'] = 'Dados muito longos para a coluna \'tag_ids\'';
+$lang['porg_issue_2145'] = '[PHP 8.x] campo exif ModeArray quebra a página da imagem';
+$lang['porg_issue_2146'] = '[pesquisa] espaços iniciais ou finais alteram os resultados da pesquisa';
+$lang['porg_issue_2149'] = '[gerente de álbum] não é possível criar sub-álbum após renomear';
