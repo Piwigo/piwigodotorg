723d342215e962a274e7f330eabc6135cd1cb125
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-06-20 07:03:33 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/coding_activity.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Coding activity'] = '编码活动';
+$lang['porg_codingactivity_intro'] = 'Piwigo核心、插件、主题和其他工具的最新提交。这个页面汇集了来自各种代码仓库的数据，为您提供一个“一站式观察”平台。';
