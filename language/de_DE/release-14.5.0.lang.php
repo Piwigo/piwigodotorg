268a318adf7a6741085c54e45ed32991194d7904
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-16 14:27:24 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1296'] = 'batch_manager metadata synchronisation meta_all';
+$lang['porg_issue_2155'] = 'PHP 8.2 und veraltet: Erstellung einer dynamischen Eigenschaft';
+$lang['porg_issue_2156'] = '[PHP 8.2] Undefinierter Array-Schlüssel \"PAGE_BANNER\“';
+$lang['porg_issue_2161'] = '[Suche] trennt Wort nicht immer nach Punkt';
+$lang['porg_issue_2163'] = 'Download-Link erscheint für Gastbenutzer, wenn Download nicht erlaubt ist';
+$lang['porg_issue_2167'] = '[Dashboard] Details der Ablageleiste zu groß';
+$lang['porg_issue_2169'] = '[Batch-Manager, Sync-Metadaten] Eingabevariablen überschritten 1000';
+$lang['porg_issue_1760'] = '[sync] Bilder mit falscher Länge/Breite stürzen die Synchronisation ab';
+$lang['porg_issue_1762'] = 'Behebt #1760 Überprüfung von Längen- und Breitengraden';
+$lang['porg_issue_2064'] = '[Alben] undefiniert node.parent';
+$lang['porg_issue_2150'] = '[14.4.0, Synchronisation] Kann strip_html_in_metadata() nicht neu deklarieren';
