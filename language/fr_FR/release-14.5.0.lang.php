aa1029b7bc5f04e4763661b3ec4fb6dd162799e1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-16 14:27:21 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2167'] = '[tableau de bord] détails de la barre de stockage trop larges';
+$lang['porg_issue_1296'] = 'Synchronisation des métadonnées meta-all dans la gestion par lot';
+$lang['porg_issue_1760'] = '[sync] Les images avec mauvaise longitude/latitude bloquent la synchronisation';
+$lang['porg_issue_1762'] = 'Corrige #1760 vérification de longitude/latitude';
+$lang['porg_issue_2064'] = '[albums] node.parent non défini';
+$lang['porg_issue_2150'] = '[14.4.0, synchronisation] Impossible de redéclarer strip_html_in_metadata()';
+$lang['porg_issue_2155'] = 'PHP 8.2 et Obsolète : Création d\'une propriété dynamique';
+$lang['porg_issue_2156'] = '[PHP 8.2] Clé de tableau non définie "PAGE_BANNER"';
+$lang['porg_issue_2169'] = '[Gestion par lot, synchronisation des métadonnées] Les variables d\'entrée dépassent 1000';
+$lang['porg_issue_2163'] = 'Le lien de téléchargement apparaît pour l\'utilisateur invité alors que le téléchargement n\'est pas autorisé';
+$lang['porg_issue_2161'] = '[recherche] ne divise pas toujours le mot au point';
