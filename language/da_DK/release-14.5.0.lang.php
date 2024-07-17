eb7d8ed7e72492d7416cc0fb2a4b0256f04270da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:12 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1296'] = '[batch_manager] metadata sync meta_all';
+$lang['porg_issue_1760'] = '[synkronisering] billeder med dårlig længdegrad/breddegrad får synkronisering til at gå ned';
+$lang['porg_issue_1762'] = 'Retter #1760 tjek længdegrad/breddegrad';
+$lang['porg_issue_2064'] = '[albummer] undefined node.parent';
+$lang['porg_issue_2150'] = '[14.4.0, synkronisering] Cannot redeclare strip_html_in_metadata()';
+$lang['porg_issue_2155'] = 'PHP 8.2 og udfaset: Creation of dynamic property';
+$lang['porg_issue_2156'] = '[PHP 8.2] Undefined array key \"PAGE_BANNER\"';
+$lang['porg_issue_2161'] = '[søgning] deler ikke altid ord ved et punktum';
+$lang['porg_issue_2163'] = 'Downloadlink vises for gæstebrugere selv om download ikke er tilladt';
+$lang['porg_issue_2167'] = '[instrumentbræt] storagebjælkens detaljerede oplysninger er for stor';
+$lang['porg_issue_2169'] = '[batch manager, synkronisering af metadata] Input variables exceeded 1000';
