59a7adae2301f91163fde333acb58a208c6b90cc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:27 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/manual_installation.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_manual_installation_image_caption1'] = '连接和传输';
+$lang['porg_guides_manual_installation_info3'] = '您也可以将Piwigo安装在网站根目录下，“photos”目录不是必需的。无论您选择什么目录名称，建议避免显示Piwigo版本号。';
+$lang['porg_guides_manual_installation_step1'] = '第一步：下载完整文件';
+$lang['porg_guides_manual_installation_step1_paragraph1'] = '在手动安装过程中，您可以下载Piwigo的完整文件，提取其内容并将其传输到您的Web服务器上。';
+$lang['porg_guides_manual_installation_step1_paragraph2'] = '<a href="%s">下载完整文件</a> 并解压缩。';
+$lang['porg_guides_manual_installation_step1_paragraph3'] = '使用任何FTP客户端将文件传输到您的Web服务器。';
+$lang['porg_guides_manual_installation_step2'] = '第二步：上传文件';
+$lang['porg_guides_manual_installation_step2_paragraph3'] = '1.在您的网络服务器上创建一个目录“photos”。';
+$lang['porg_guides_manual_installation_step2_paragraph4'] = '2.在您的硬盘驱动器上，打开“piwigo”解压的目录。';
+$lang['porg_guides_manual_installation_step2_paragraph5'] = '3.选择所有文件并将它们传输到您服务器上的“photos”目录中。';
