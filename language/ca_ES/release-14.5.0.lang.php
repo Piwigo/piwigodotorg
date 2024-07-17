f578d9c78fef9c551e9994a318fb75345f3baeba
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:18 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1296'] = 'Sincronització de metadades (meta_all) al gestor per lots';
+$lang['porg_issue_1760'] = '[sincronització] les imatges amb longitud/latitud incorrectes bloquegen la sincronització';
+$lang['porg_issue_1762'] = 'Arranjament #1760 comprovació de longitud/latitud';
+$lang['porg_issue_2064'] = '[àlbums] node.parent no definit';
+$lang['porg_issue_2150'] = '[14.4.0, sincronització] No es pot tornar a declarar strip_html_in_metadata()';
+$lang['porg_issue_2155'] = 'PHP 8.2 i Obsolet: Creació de propietat dinàmica';
+$lang['porg_issue_2156'] = '[PHP 8.2] Clau de matriu \"PAGE_BANNER\" no definida';
+$lang['porg_issue_2161'] = '[recerca] no sempre es parteix la paraula on hi ha un punt';
+$lang['porg_issue_2163'] = 'L\'enllaç de descàrrega apareix per a l\'usuari convidat quan les descàrregues no estan permeses';
+$lang['porg_issue_2167'] = '[panell] detalls de la barra d\'emmagatzematge massa llargs';
+$lang['porg_issue_2169'] = '[gestor per lots, sincronització de metadades] Les variables d\'entrada passaven del 1000';
