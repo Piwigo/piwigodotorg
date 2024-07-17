02d29cec00f108425ba7f9502a0fd22a0280ff0b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:15 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1760'] = '[sincronización] las imágenes con longitud/latitud incorrectas bloquean la sincronización';
+$lang['porg_issue_2169'] = '[gestor por lotes, sincronización de metadatos] Las variables de entrada pasaban de 1000';
+$lang['porg_issue_1296'] = 'Sincronización de metadatos (meta_all) en el gestor por lotes';
+$lang['porg_issue_1762'] = 'Arregla #1760 comprobación de longitud/latitud';
+$lang['porg_issue_2064'] = '[albums] node.parent no definido';
+$lang['porg_issue_2150'] = '[14.4.0, sincronización] No se puede volver a declarar strip_html_in_metadata()';
+$lang['porg_issue_2155'] = 'PHP 8.2 y Obsoleto: Creación de propiedad dinámica';
+$lang['porg_issue_2156'] = '[PHP 8.2] Clave de matriz \"PAGE_BANNER\" no definida';
+$lang['porg_issue_2161'] = '[búsqueda] no siempre se parte la palabra donde hay un punto';
+$lang['porg_issue_2163'] = 'El enlace de descarga aparece para el usuario invitado cuando no están permitidas las descargas';
+$lang['porg_issue_2167'] = '[panel] detalles de la barra de almacenamiento demasiado largos';
