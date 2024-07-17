41597a28b38e5d176ce3c5263e05be45bd85dacc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:24 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1296'] = 'batch_manager 元数据同步';
+$lang['porg_issue_1760'] = '[同步​​] 经度/纬度不正确的图像同步会崩溃';
+$lang['porg_issue_1762'] = '修复 #1760 检查经度/纬度';
+$lang['porg_issue_2064'] = '[相册] 未定义 node.parent';
+$lang['porg_issue_2150'] = '[14.4.0，同步] 无法重新声明 strip_html_in_metadata()';
+$lang['porg_issue_2155'] = 'PHP 8.2 及已弃用：创建动态属性';
+$lang['porg_issue_2156'] = '[PHP 8.2] 未定义数组键“PAGE_BANNER”';
+$lang['porg_issue_2161'] = '[搜索] 不要总是用点来分割单词';
+$lang['porg_issue_2163'] = '不允许下载时，访客用户会显示下载链接';
+$lang['porg_issue_2167'] = '[仪表盘] 存储栏详细信息太大';
+$lang['porg_issue_2169'] = '[批次管理器，同步元数据] 输入变量超过 1000';
