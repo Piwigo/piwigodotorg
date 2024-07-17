b548f542da2499d5e6afe20dc1a5d3f1acd4871a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:38 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/guides.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_intro'] = '欢迎来到 Piwigo 指南。在这里，您将找到有关如何安装和更新 Piwigo 的基础知识。您的 Piwigo 中还直接提供了一些帮助，包括管理面板的“帮助我！”页面或任何您看到图形问号的地方。Wiki还涵盖了开发人员文档。';
