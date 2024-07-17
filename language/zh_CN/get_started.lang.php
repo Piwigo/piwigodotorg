0db48356ef62029032aa3f67b03b4980f2e0a004
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:59:20 +0200

    Update zh_CN, thanks to walle

--- language/zh_CN/get_started.lang.php
+++ language/zh_CN/get_started.lang.php
@@ -15,6 +15,6 @@ $lang['porg_get_started_desc1'] = '本页面将会教您如何在自己的服务
 $lang['porg_get_started_desc2'] = '下面的内容将会提及准备事项、下载方式与安装步骤。';
 $lang['porg_get_started_download_desc'] = '您可以下载Piwigo完整安装包，也可以下载在线安装脚本，在线安装脚本将会自动从服务器下载所需文件。';
 $lang['porg_get_started_guide_desc'] = '了解如何在您的服务器上安装Piwigo，您将会学习从下载到数据库配置到访问相片库的全过程。';
-$lang['porg_get_started_question_desc1'] = '您可以在我们的<a href="%s">论坛中</a>寻求帮助，或在<a href="%s">文档</a>中自行寻找相关内容。';
 $lang['porg_get_started_requirement_desc1'] = '什么样的服务器才能安装Piwigo呢？';
 $lang['porg_get_started_requirement_desc2'] = 'PHP、数据库、Web服务器、图像处理库...你可以在下面的链接中查看具体要求。';
+$lang['Installation guide'] = '安装指南';
