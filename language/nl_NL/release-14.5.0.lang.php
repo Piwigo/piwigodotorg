81df6c21498e9e181c081611a239b46abf64a5d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-17 07:58:21 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1296'] = 'batch_manager metagegevens synchronisatie meta_all';
+$lang['porg_issue_1760'] = '[sync] afbeeldingen met een onjuiste lengtegraad/breedtegraad zorgen ervoor dat de synchronisatie vastloopt';
+$lang['porg_issue_1762'] = 'Herstelt #1760 controle lengtegraad/breedtegraad';
+$lang['porg_issue_2064'] = '[albums] ongedefinieerd node.parent';
+$lang['porg_issue_2150'] = '[14.4.0, synchronisatie] Kan strip_html_in_metadata() niet opnieuw declareren';
+$lang['porg_issue_2155'] = 'PHP 8.2 en verouderd: Dynamische eigenschap aanmaken';
+$lang['porg_issue_2156'] = '[PHP 8.2] Ongedefinieerde array-sleutel \"PAGE_BANNER\"';
+$lang['porg_issue_2161'] = '[zoeken] splits niet altijd woorden op punt';
+$lang['porg_issue_2163'] = 'Downloadlink verschijnt voor gastgebruiker wanneer downloaden niet is toegestaan';
+$lang['porg_issue_2167'] = '[dashboard] opslagbalkdetails te groot';
+$lang['porg_issue_2169'] = '[batch manager, synchronisatie metadata] Invoervariabelen overschreden 1000';
