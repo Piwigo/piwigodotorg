3e0bf5645487b0384eb12b914dd8c01e5a5f0ea7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-18 06:05:55 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-14.5.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2155'] = 'PHP 8.2 e Deprecati: Creazione di proprietà dinamica';
+$lang['porg_issue_1296'] = 'batch_manager sincronizzazione metadati meta_all';
+$lang['porg_issue_1760'] = '[sync] immagini con longitudine/latitudine malformati fanno crashare la sincronizzazione';
+$lang['porg_issue_1762'] = 'Fixes #1760 controllo longitudine/latitudine';
