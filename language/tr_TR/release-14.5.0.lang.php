7a10818bc3b76235fd30a05ea17b67594cacf9a3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-18 16:18:02 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2169'] = '[toplu iş yöneticisi, meta verileri senkronize et] Giriş değişkenleri 1000\'i aştı';
+$lang['porg_issue_2167'] = '[kontrol paneli] depolama çubuğu ayrıntıları çok büyük';
+$lang['porg_issue_2163'] = 'İndirmeye izin verilmediğinde konuk kullanıcı için indirme bağlantısı görünür';
+$lang['porg_issue_2161'] = '[arama] sözcüğü her zaman noktaya bölmeyin';
+$lang['porg_issue_2156'] = '[PHP 8.2] Tanımsız dizi anahtarı \"PAGE_BANNER\"';
+$lang['porg_issue_2155'] = 'PHP 8.2 ve Kullanımdan Kaldırılmış: Dinamik özelliğin oluşturulması';
+$lang['porg_issue_2150'] = '14.4.0, senkronizasyon] strip_html_in_metadata() yeniden bildirilemiyor';
+$lang['porg_issue_2064'] = '[albümler] tanımsız node.parent';
+$lang['porg_issue_1762'] = 'Düzeltme #1760 enlem/boylam kontrolü';
+$lang['porg_issue_1760'] = '[senkronizasyon] enlem/boylamın kötü olduğu görsellerin senkronizasyonu bozulur';
+$lang['porg_issue_1296'] = 'batch_manager meta veri senkronizasyonu meta_all';
