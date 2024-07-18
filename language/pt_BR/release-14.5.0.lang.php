e3691db5cd948d35209634578b0b41a7de7567b3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-18 06:05:58 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-14.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1296'] = 'sincronização de metadados do batch_manager meta_all';
+$lang['porg_issue_1760'] = '[sync] imagens com sincronização de travamentos de longitude/latitude incorreta';
+$lang['porg_issue_1762'] = 'Correções #1760 verificam longitude/latitude';
+$lang['porg_issue_2064'] = '[albums] node.parent indefinido';
+$lang['porg_issue_2150'] = '[14.4.0, syncronazation] Não é possível redeclarar strip_html_in_metadata()';
+$lang['porg_issue_2155'] = 'PHP 8.2 e Obsoleto: Criação de propriedade dinâmica';
+$lang['porg_issue_2156'] = '[PHP 8.2] Chave de array indefinida \"PAGE_BANNER\"';
+$lang['porg_issue_2161'] = '[search] nem sempre divide a palavra em ponto';
+$lang['porg_issue_2163'] = 'O link de download aparece para o usuário convidado quando o download não é permitido';
+$lang['porg_issue_2167'] = '[dashoboard] detalhes da barra de armazenamento muito grandes';
+$lang['porg_issue_2169'] = '[batch manager, sync metadata] Variáveis ​​de entrada excederam 1.000';
