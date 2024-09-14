9c0ca8f9b9bcc06155924a24dfd5ca69016ea2a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-14 07:48:13 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/netinstall.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_netinstall_image_caption1'] = '连接上传';
+$lang['porg_guides_netinstall_image_caption3'] = 'Piwigo已安装';
+$lang['porg_guides_netinstall_step3_paragraph3'] = '运行“检索并解压缩Piwigo”操作，然后“立即安装Piwigo”';
+$lang['porg_guides_netinstall_image_caption2'] = '为您的照片库选择一个文件夹名称';
+$lang['porg_guides_netinstall_step3_paragraph2'] = '在您的网站上为您的照片库选择一个目录名称。默认情况下，Piwigo将安装在piwigo目录中。如果您将目的地字段留空，Piwigo将安装在您传输NetInstall脚本的同一位置。';
+$lang['porg_guides_netinstall_step3_paragraph1'] = 'NetInstall脚本传输完成后，使用Web浏览器访问其网址，例如';
+$lang['porg_guides_netinstall_step2_paragraph3'] = '选择硬盘上的piwigo-netinstall.php文件并将其传输到Web服务器';
+$lang['porg_guides_netinstall_paragraph1'] = 'NetInstall需要将单个文件传输到您的Web服务器上。它包含的脚本将从 piwigo.org 下载Piwigo档案，然后提取其内容并启动安装程序。';
+$lang['porg_guides_netinstall_info1'] = 'NetInstall可能无法在您的Web服务器上运行，具体取决于所提供的功能级别。如果您无法使其运行，只需切换到<a href="%s">手动安装指南</a>即可。';
+$lang['porg_guides_netinstall_step1_paragraph1'] = '<a href="%s">下载 NetInstall 脚本。</a>';
+$lang['porg_guides_netinstall_step1'] = '第一步：下载NetInstall脚本';
+$lang['porg_guides_netinstall_step2'] = '第二步：传输NetInstall脚本';
