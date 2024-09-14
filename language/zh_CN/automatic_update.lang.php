1020ed1d4c1d4636f50e31a2a7f9ca47c2375871
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-14 07:48:07 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/automatic_update.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_automatic_update_caption1'] = '是的，只需点击2次';
+$lang['porg_guides_automatic_update_paragraph10'] = '下载Piwigo自动升级插件，并将其解压缩到您的“插件”目录中';
+$lang['porg_guides_automatic_update_paragraph11'] = '转到管理>特殊>插件>管理，然后单击“Piwigo自动升级”插件前面的“安装”操作，然后单击“激活”操作。';
+$lang['porg_guides_automatic_update_paragraph12'] = '转到管理>特殊>插件>Piwigo自动升级并按照指南操作，您可以选择升级到同一“分支”的最新版本或最新的Piwigo版本。';
+$lang['porg_guides_automatic_update_paragraph3'] = '转到“管理”>“工具”>“更新”，然后按照指南操作';
+$lang['porg_guides_automatic_update_paragraph4'] = '转到管理>插件>管理>其他可用的插件，并安装Piwigo自动升级。如果插件已安装，请在“管理”>“插件”>“管理”>“检查更新”屏幕上检查您正在运行最新版本。';
+$lang['porg_guides_automatic_update_paragraph5'] = '转到“管理”>“插件”>“管理”>“插件列表”来安装并激活此新插件。';
+$lang['porg_guides_automatic_update_paragraph6'] = '转到管理>插件>Piwigo自动升级，然后按照指南操作。';
+$lang['porg_guides_automatic_update_paragraph7'] = '管理>特殊>插件>其他插件，并执行Piwigo自动升级的自动安装。如果插件已安装，请检查您正在运行最新版本。';
+$lang['porg_guides_automatic_update_paragraph8'] = '转到管理>特殊>插件来安装和激活此新插件';
+$lang['porg_guides_automatic_update_paragraph9'] = '转到管理>特殊>插件>Piwigo自动升级并按照指南操作，您可以选择升级到同一“分支”的最新版本或最新的Piwigo版本。';
+$lang['porg_guides_automatic_update_paragraph13'] = '如果你使用的是较早版本，请按照手动升级进行操作';
+$lang['porg_guides_automatic_update_button2'] = '如果您正在运行Piwigo 2.1.x或2.2.x';
+$lang['porg_guides_automatic_update_button3'] = '如果您正在运行Piwigo 2.0.x';
+$lang['porg_guides_automatic_update_button4'] = '如果您正在运行Piwigo 1.7.x';
+$lang['porg_guides_automatic_update_button5'] = '如果您使用的是比1.7.x更早的版本';
+$lang['porg_guides_automatic_update_step1'] = '只需一步';
+$lang['porg_guides_automatic_update_time_1'] = '30秒';
+$lang['porg_guides_automatic_update_info1'] = '始终备份您的数据库和文件。最好定期自动备份。如果在更新过程中发生意外，您可以恢复备份。';
+$lang['porg_guides_automatic_update_paragraph1'] = '这是将Piwigo升级到最新版本的推荐方法。太简单了，您再也没有理由忘记或推迟升级了。运行最新和最安全的Piwigo版本只需不到1分钟。';
