5d34717aacbf4c02820dcb723d51a16da2ab4db4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:34:52 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/showcases.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_showcases_indre_desc'] = '安德尔县议会选择Piwigo来取代其旧的照片库软件，该软件更昂贵且效率较低。';
+$lang['porg_showcases_indre_title'] = '安德尔县议会';
+$lang['porg_showcases_macrogallery_desc'] = '一位热情的业余爱好者在这个照片库中展示了一系列有关昆虫、蜘蛛和植物的图像。这些照片按照家庭、阶级、秩序进行了仔细分类和索引……这是一项令人印象深刻的工作。';
+$lang['porg_showcases_macrogallery_title'] = '宏观画廊宏观画廊';
+$lang['porg_showcases_milophotographie_desc'] = 'Maria是巴黎的一名专业摄影师，她使用Piwigo来分享她的作品。';
+$lang['porg_showcases_milophotographie_title'] = 'Milo摄影';
+$lang['porg_showcases_nielfitzgerald_desc'] = 'Neil Fitzgerald是新西兰专业摄影师，专门从事自然和野生动物摄影，他选择Piwigo来增强他的作品。';
+$lang['porg_showcases_nielfitzgerald_title'] = '尼尔·菲茨杰拉德，摄影师';
+$lang['porg_showcases_oceanopolis_desc'] = 'Océanpolis是著名的布雷顿海洋探索公园，它使用Piwigo来管理其照片库和视频库。';
+$lang['porg_showcases_oceanopolis_title'] = '布雷斯特海洋城';
+$lang['porg_showcases_parcdeslandesdegascogne_desc'] = 'Landes de Gascogne地区自然公园选择Piwigo来创建和管理其照片库。';
+$lang['porg_showcases_parcdeslandesdegascogne_title'] = '加斯科涅朗德地区自然公园';
+$lang['porg_showcases_paris1sorbonne_desc'] = '巴黎第一大学先贤祠-索邦大学选择Piwigo来管理和组织其大型照片库。';
+$lang['porg_showcases_paris1sorbonne_title'] = '巴黎第一大学先贤祠索邦大学';
+$lang['porg_showcases_photolib_desc'] = 'photolib.fr是一个使用Piwigo创建的照片库，它在知识共享许可下展示照片。';
+$lang['porg_showcases_photolib_title'] = '照片库';
+$lang['porg_showcases_pinnacle_desc'] = '在这个照片库中，史蒂夫·拉亚 (Steve Raia) 展示了他的摄影作品并推销他的照片。';
+$lang['porg_showcases_pinnacle_title'] = '顶峰画廊';
+$lang['porg_showcases_societe3vallees_desc'] = '梅里贝尔 (Meribel)、高雪维尔 (Courchevel) 和塔尼亚 (Tania) 三个山谷的协会选择Piwigo来组织其媒体。';
+$lang['porg_showcases_societe3vallees_title'] = '三谷公司';
+$lang['porg_get_showcase_intro_paragraph'] = '在此页面上发现一些使用Piwigo创建的照片库和公共画廊。';
+$lang['porg_get_showcase_intro_title'] = 'Piwigo用户很有才华！';
+$lang['porg_showcases_arles_desc'] = '阿尔勒市政厅在其Piwigo画廊上发布了一系列照片，以图像证明了该市当前发生的事件。';
+$lang['porg_showcases_arles_title'] = '阿尔勒市';
+$lang['porg_showcases_dongshaw_desc'] = '来自台湾的画家董小蕙在她的Piwigo画廊展示了他的作品。';
+$lang['porg_showcases_dongshaw_title'] = '董小蕙的艺术';
+$lang['porg_showcases_ect_desc'] = 'ECT通过Piwigo管理其图像库，以提高生产力和协作。';
+$lang['porg_showcases_ect_title'] = 'ECT集团';
+$lang['porg_showcases_ehcanada_desc'] = 'eh Canada是加拿大的一家旅游专家，在其用Piwigo创建的照片库中保存了数千张照片。';
+$lang['porg_showcases_ehcanada_title'] = 'Eh Canada';
+$lang['porg_showcases_ffn_desc'] = '法国游泳联合会选择Piwigo来存储和分享大量照片和视频';
+$lang['porg_showcases_ffn_title'] = '法国游泳联合会';
+$lang['porg_showcases_gerarddubouloz_desc'] = '即时照片是热爱自然和户外活动的摄影师Gérard Dubouloz的画廊。';
+$lang['porg_showcases_gerarddubouloz_title'] = '即时照片';
+$lang['porg_showcases_guillaumecyriaque_desc'] = '比利时业余摄影师Guillaume Cyriaque选择Piwigo来增强他的作品。';
+$lang['porg_showcases_guillaumecyriaque_title'] = '纪尧姆·西里亚克，摄影师';
+$lang['porg_showcases_hautegaronne_desc'] = '上加龙省议会使用Piwigo创建了照片库。';
+$lang['porg_showcases_hautegaronne_title'] = '上加龙省议会';
+$lang['porg_showcases_icam_desc'] = '法国工程学院ICAM选择Piwigo创建一个由其全球 11 个校区共享的照片库。';
+$lang['porg_showcases_icam_title'] = '天主教艺术与工艺学院';
+$lang['Visit Gallery'] = '参观画廊';
+$lang['page_meta_description'] = '公司、摄影师、公共部门：我们的用户有才华！发现使用 Piwigo 软件创建的在线照片库的示例';
+$lang['page_meta_title'] = 'Piwigo示例 - 使用Piwigo创建的一些照片库';
+$lang['porg_get_showcase_filter_all'] = '全部';
+$lang['porg_get_showcase_filter_companies'] = '公司';
+$lang['porg_get_showcase_filter_photographers_and_private_users'] = '摄影师和个人';
+$lang['porg_get_showcase_filter_public_sector'] = '公共部门';
+$lang['porg_get_showcase_filter_sports_and_leisure'] = '运动和爱好';
+$lang['porg_get_showcase_filter_travel_and_tourism'] = '旅游和文化';
