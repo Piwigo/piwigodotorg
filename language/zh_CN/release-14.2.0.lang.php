6884482c0c55ee848d5c6b387406a2291a83e4be
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:33:33 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-14.2.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2081'] = '[14.1.0] 丢失菜单和日历图标';
+$lang['porg_issue_2086'] = 'AdminTools 激活时出现错误图标';
+$lang['porg_issue_2088'] = 'history.section 自动填充不应区分大小写';
+$lang['porg_issue_2089'] = '默认隐藏“在此集合中搜索”按钮';
+$lang['porg_issue_2093'] = '在 IPTC 元数据导入/同步期间删除新行';
+$lang['porg_issue_2094'] = '[搜索 14.x] 选择列表框太短';
+$lang['porg_issue_2095'] = '[UX] 明确“谁可以看到这张照片？”与隐私级别相关';
+$lang['porg_issue_2096'] = '[搜索] 保存过滤器列表';
