b495061fac410a955c309645eb622a424cfc7be6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:34:43 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-14.3.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1612'] = '[管理主题] 标签窗口太小，包含多个标签';
+$lang['porg_issue_2005'] = '从 Darktable 上传失败';
+$lang['porg_issue_2079'] = '批次管理器：尝试对所有项目运行操作会导致“黑客尝试”错误';
+$lang['porg_issue_2100'] = '无法批量上传';
+$lang['porg_issue_2102'] = '[搜索] 选择多个相册时无法单击 x';
+$lang['porg_issue_2103'] = '[相册编辑器] ESC 打开放大的描述框';
+$lang['porg_issue_2104'] = '[14.x 相册编辑器] 模糊的相册缩略图';
+$lang['porg_issue_2105'] = '[14.x 相册编辑器] 面包屑和操作图标之间的混淆';
+$lang['porg_issue_2106'] = '[历史搜索] 与图库搜索相关的行有“详细信息 1”';
+$lang['porg_issue_2112'] = '[搜索] 按词搜索允许更多字符';
+$lang['porg_issue_2117'] = '历史页面上的日期选择器导致重复请求';
+$lang['porg_issue_2118'] = '能够配置动画 webP 压缩以调整大小';
+$lang['porg_issue_2119'] = '[相册管理器] 修改相册名称后移动相册时出现错误';
+$lang['porg_issue_2120'] = '[相册管理器] 移动相册时“nb_sub_photos”徽章出现错误';
+$lang['porg_issue_2122'] = '[搜索] 缺少用户添加时偏移量未定义';
+$lang['porg_issue_GHSA-7379-w44f-mfw4'] = '标签中的跨站点脚本';
+$lang['porg_issue_GHSA-8g2g-6f2c-6h7j'] = 'CSRF + XSS to RCE';
+$lang['porg_issue_GHSA-p362-cfpj-q55f'] = 'CSRF + 存储型 XSS 执行远程代码执行';
