74c8148c4134af695786e2449d068e856265ae99
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:34:46 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-14.4.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = '上传已存在的照片时崩溃';
+$lang['porg_issue_2129'] = '[用户活动] 用户过滤框超出容器大小';
+$lang['porg_issue_2131'] = '[搜索 14.2+] 相册选择器中缺少图标';
+$lang['porg_issue_2134'] = 'Piwigo新版本提供多封电子邮件通知';
+$lang['porg_issue_2136'] = '[搜索 14.x] 按单词搜索匹配标签的2个单词，没有结果';
+$lang['porg_issue_2138'] = '[编辑相册] 管理相册照片图标用途不明显';
+$lang['porg_issue_2140'] = '[PHP 8.x] 菜单错误';
+$lang['porg_issue_2142'] = '‘tag_ids’列的数据太长';
+$lang['porg_issue_2145'] = '[PHP 8.x] exif ModeArray 字段破坏图片页面';
+$lang['porg_issue_2146'] = '[搜索] 前导或尾随空格会改变搜索结果';
+$lang['porg_issue_2149'] = '[相册管理] 重命名后无法创建子相册';
