7e4197185903cd6350a07d91b568a8cf2f98f360
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:33:36 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-14.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2066'] = '[14.x] 保持与 PHP 7.0 的兼容性';
+$lang['porg_issue_2070'] = '[批次管理器] 列“object_id”不能为空';
+$lang['porg_issue_2072'] = '[相册编辑器 14.x] 描述字段太小';
+$lang['porg_issue_2073'] = '标签管理器和扩展描述：您无法修改原始版本';
+$lang['porg_issue_2074'] = '[安装] PHP 7.4+上插件自动激活错误';
+$lang['porg_issue_2075'] = '修复以 HEIC 格式上传的图像缺少 EXIF 元数据的问题';
+$lang['porg_issue_2076'] = '[搜索 14.x] 无法清除相册过滤器';
+$lang['porg_issue_2077'] = 'Piwigo v14 意大利语翻译中出现 Javascript 错误';
+$lang['porg_issue_2080'] = '[14.x] 解析错误：语法错误，cat_modify.php 中出现意外的 \')\'';
