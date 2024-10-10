046bbbfb33876b548ab371e6ea537fa623381921
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-10-10 18:32:04 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.0.0.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v15_willy_text2'] = 'Na Piwigo, tive a chance de melhorar certos recursos, como o seletor de álbuns ou o gerenciador de usuários e criar novas ferramentas, como as atividades do sistema. Espero que todos os recursos que eu desenvolvo tragam um pouco dessa energia e entusiasmo que tenho por esse trabalho, e que isso é sentido por aqueles que usam o software. É inspirador fazer parte de uma equipe que compartilha essa visão, e estou ansioso para continuar inovando com Piwigo!';
+$lang['porg_v15_willy_text1'] = 'Olá a todos, meu nome é Willy (ou Linty no fórum), e já faz quase um ano desde que entrei na equipe do Piwigo como desenvolvedor de pilhas de estudante. Eu sempre fui apaixonado pelo desenvolvimento, e todos os projetos em que trabalho são uma oportunidade para compartilhar essa paixão.';
+$lang['porg_v15_user_manager_title'] = 'Gerenciador de usuários';
+$lang['porg_v15_user_manager_text'] = 'Sabemos que alguns de vocês gerenciam centenas de usuários em seu Piwigo. Às vezes, você precisa criar novos usuários regularmente ou gerenciar usuários que criam sua conta na sua galeria da web. Com o Piwigo 15, pensamos em você e fizemos algumas melhorias importantes no gerenciamento de usuários. Esperamos que você goste.';
+$lang['porg_v15_user_manager_image6_label'] = 'Os filtros na lista de usuários ficam mais inteligentes: exibem o número de usuários para cada opção e desabilitam opções vazias.';
+$lang['porg_v15_user_manager_image5_label'] = 'Agora você pode alterar o administrador principal do Piwigo na interface do usuário. Isso era gerenciado antes pela configuração local "webmaster_id". Apenas um webmaster pode ser eleito como usuário principal. Ele é representado na lista de usuários por uma coroa amarela.';
+$lang['porg_v15_user_manager_image4_label'] = 'O formulário de edição do usuário foi redesenhado. Agora está mais limpo, mais simples e integra plugins mais facilmente.';
+$lang['porg_v15_user_manager_image3_label'] = 'Não há mais senhas por e-mail: o registro do usuário fica mais seguro. Depois de criar um novo usuário, eles receberão um link para definir sua senha antes do primeiro login. Caso o Piwigo não possa enviar o email (o email está longe de ser 100% confiável), qualquer administrador pode regenerar um link de senha (re) definir, copiá -lo e enviá -lo ao usuário em qualquer canal confiável.';
+$lang['porg_v15_user_manager_image2_label'] = 'Ao criar um novo usuário, você pode definir imediatamente o status do usuário, o nível de privacidade e os grupos. E você não precisa mais criar uma senha.';
+$lang['porg_v15_user_manager_image1_label'] = 'Você estava esperando por isso! Agora você pode classificar a lista de usuários por nome de usuário ou data de registro, em ordem crescente ou decrescente, com apenas um clique.';
+$lang['porg_v15_technical_features7'] = 'Nós projetamos o código do novo usuário popin e o novo gerenciador de lotes no modo unitário para ser facilmente extensível. Nós atualizamos o plugin Skeleton para mostrar exemplos.
+';
+$lang['porg_v15_technical_features6'] = 'Extensibilidade';
+$lang['porg_v15_technical_features3'] = 'Leia-o para tornar suas extensões compatíveis com o Piwigo 15.';
+$lang['porg_v15_technical_features2'] = 'Mudanças técnicas no Piwigo 15';
+$lang['porg_v15_technical_features15'] = 'Mudanças na API';
+$lang['porg_v15_technical_features14'] = 'O <code>_data/cache</code> "persistente" não é frequentemente purgado. Considerando que ele é frequentemente relacionado ao cache do usuário, o Piwigo 15 irá purgá-lo cada vez que <code>invalidate_user_cache</code>.';
+$lang['porg_v15_technical_features13'] = 'Otimize o tamanho do cache';
+$lang['porg_v15_technical_features1'] = 'Observe que uma documentação mais detalhada está disponível sobre';
+$lang['porg_v15_system_activities_title'] = 'Atividades do sistema';
+$lang['porg_v15_system_activities_image1_label'] = 'As atividades do usuário foram introduzidas na versão 12. As atividades do sistema foram adicionadas na versão 15. Você pode ver quando seu Piwigo foi atualizado, para qual versão, quando um plugin foi ativado, quando uma ação de manutenção foi realizada...';
+$lang['porg_v15_searchdynamicfilter_title'] = 'Filtros dinâmicos';
+$lang['porg_v15_searchdynamicfilter_text'] = 'O novo mecanismo de pesquisa, introduzido na versão 14, passou por uma grande melhoria.
+Agora você pode alterar o valor de um filtro a qualquer momento, sem ter que reiniciar sua pesquisa. <br>
+<br>
+Por exemplo, se você aplicou o filtro "Adicionado por: John", pode alterar a seleção para "adicionada por: Paul". E se você tiver outro filtro ativo, como "FileType: MP4", verá apenas usuários que adicionaram um arquivo MP4 no filtro "adicionado por". É uma melhoria real da experiência do usuário, e esperamos que você o aprecie.';
+$lang['porg_v15_search_engine_title'] = 'Mecanismo de pesquisa';
+$lang['porg_v15_search_engine_image2_label'] = 'Se você precisa filtrar fotos por data de criação, agora você pode selecionar um intervalo de datas personalizado, que pode ser um ano, um mês, um dia ou vários deles de uma vez. Você também obtém verificações inteligentes para mostrar datas dos pais e dos filhos.';
+$lang['porg_v15_search_engine_image1_label'] = 'A busca na galeria ganha novos filtros. Agora você pode filtrar fotos: por classificações, por tamanho de arquivo, por proporção de dimensões (retrato, quadrado, paisagem), por altura, por largura e por data de criação.';
+$lang['porg_v15_marceau_text2'] = 'Entre algumas outras missões, sou o responsável pela maior parte da integração do novíssimo Batch Manager Unit Mode. Essa foi uma tarefa tediosa, mas espero que os usuários e colaboradores do Piwigo gostem e estou ansioso para ouvir suas opiniões sobre isso.';
+$lang['porg_v15_marceau_text1'] = 'Meu nome é Marceau, sou estudante de desenvolvimento web e tive a oportunidade de trabalhar com a equipe como estagiário de abril a setembro de 2024. Meu objetivo como desenvolvedor é codificar com eficiência e garantir que os usuários se sintam confortáveis ​​usando tudo o que eu integro.';
+$lang['porg_v15_intro2'] = 'Às vezes são as pequenas coisas, aparentemente menores que mudam tudo. O Piwigo 15 traz uma série de melhorias para tornar o trabalho dos usuários mais confortável. Com um forte foco no gerenciamento de usuários, o redesenho do gerente de lote em modo unitário, novos filtros dinâmicos na pesquisa e outras melhorias de UX, esta versão se concentra nos recursos que algumas pessoas usam todos os dias. Sem mencionar novos recursos, como histórico de atividades do sistema ou um leitor de PDF incorporado.';
+$lang['porg_v15_intro1'] = 'Torne sua vida mais fácil';
+$lang['porg_v15_embedpdf_title'] = 'Leitor de PDF incorporado';
+$lang['porg_v15_batch_manager_image1_label'] = 'O Modo de Unidade do Gerente de Lotes foi redesenhado: capacidade de modificar filtros como no modo global, imagem maior para ajudar a preencher propriedades, espaço otimizado, mais ações disponíveis para cada foto.';
+$lang['porg_v15_embedpdf_image1_label'] = 'Ao visualizar um arquivo PDF na sua galeria, o Piwigo 15 abrirá automaticamente o leitor de PDF do seu navegador da web, incorporado na página. Funciona nos temas Modus e Bootstrap Darkroom por enquanto.';
+$lang['porg_v15_batch_manager_title'] = 'Gerente em lote';
+$lang['porg_v15_batch_manager_image2_label'] = 'No modo global, os administradores agora podem selecionar vários álbuns para associar, tudo na mesma ação.';
+$lang['porg_v15_badge_refresh'] = 'atualizar';
+$lang['porg_v15_badge_redesign'] = 'redesenhar';
+$lang['porg_v15_badge_new'] = 'novo';
+$lang['porg_v15_api_s1'] = 'Aceite Image_Ids como lista separada por vírgula';
+$lang['porg_v15_anonymous_stats_title'] = 'Estatísticas anônimas';
+$lang['porg_v15_anonymous_stats_text'] = 'Na Piwigo, não sabemos muito sobre nossos usuários. Não temos idéia exata de quantos piwigo estão instalados. Temos algumas evidências que você tem mais de 100 mil, mas isso pode ser muito mais. Não sabemos se você tem muitos usuários ou apenas o usa. Não sabemos se é para 100 fotos ou 100 mil fotos. Ter esse tipo de dados nos ajudaria a tomar decisões de design/desenvolvimento. É por isso que agora coletaremos algumas estatísticas. <br>
+<br>
+Não queremos saber nada privado: nem sabemos o endereço da sua Piwigo. Nós apenas queremos estatísticas gerais. Detalhes do que é compartilhado são fornecidos no problema do GitHub. Você pode desativar esse recurso, mas esperamos que não, porque você pode ver nosso código e confiar em nós.';
+$lang['porg_v15_album_selector_title'] = 'Seletor de álbuns';
+$lang['porg_v15_album_selector_image2_label'] = 'Segunda adição: agora você pode criar um novo álbum onde precisar. Neste exemplo, você pode ver que o seletor de álbum foi adicionado ao formulário de upload de fotos.';
+$lang['porg_v15_album_selector_image1_label'] = 'Associando uma foto a um minério, vários álbuns ficam mais fáceis, mesmo quando você tem muitos álbuns em sua biblioteca. De fato, o seletor de álbuns recebe aprimoramentos importantes. Primeiro: sempre exibe a hierarquia dos álbuns. Não há necessidade de executar uma pesquisa.';
+$lang['porg_v15_album_manager_title'] = 'Gerenciador de álbuns';
+$lang['porg_v15_album_manager_text'] = 'O gerente do álbum recebe performances muito melhores em alto volume. Até agora, se você tivesse muitos álbuns - e queremos dizer mais de 10 milhares - o gerente do álbum poderia levar alguns segundos para carregar, dependendo da velocidade do seu computador. Com Piwigo 15 e otimizações implementadas por Linty, o gerente do álbum carregará em milissegundos, independentemente do número de álbuns.';
+$lang['The menu'] = 'O menu';
+$lang['porg_v15_album_manager_image1_label'] = 'Pesquisar um álbum ficou mais fácil! Anteriormente fornecido como uma aba dedicada, o campo de pesquisa de álbum agora está disponível diretamente no gerenciador de álbuns.';
+$lang['Technical features'] = 'Características técnicas';
+$lang['Enhanced user management, system activities, batch manager redesign.'] = 'Gerenciamento de usuários aprimorado, atividades do sistema, redesenho do gerenciador de lotes.';
+$lang['Interview %s'] = 'entrevista de %s';
