bbf317518ef3b92c4e4af1966bd151dfbcde408a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-10-10 18:30:24 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-15.0.0.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v15_willy_text2'] = 'Bij Piwigo heb ik de kans gehad om bepaalde functies te verbeteren, zoals de albumselector of de gebruikersmanager, en om nieuwe tools te creëren, zoals de systeemactiviteiten. Ik hoop dat elke functie die ik ontwikkel een beetje van die energie en dat enthousiasme met zich meebrengt die ik voor deze baan heb, en dat het gevoeld wordt door degenen die de software gebruiken. Het is inspirerend om deel uit te maken van een team dat deze visie deelt, en ik kijk ernaar uit om te blijven innoveren met Piwigo!';
+$lang['porg_v15_willy_text1'] = 'Hallo allemaal, mijn naam is Willy (of Linty op het forum) en het is bijna een jaar geleden dat ik bij het Piwigo-team kwam als student fullstack developer. Ik ben altijd al gepassioneerd geweest door ontwikkeling en elk project waar ik aan werk is een kans voor mij om die passie te delen.';
+$lang['porg_v15_user_manager_title'] = 'Gebruikersbeheerder';
+$lang['porg_v15_user_manager_text'] = 'We weten dat sommigen van jullie honderden gebruikers beheren in jullie Piwigo. Soms moet je regelmatig nieuwe gebruikers aanmaken, of gebruikers beheren die hun account aanmaken vanuit je webgalerij. Met Piwigo 15 hebben we aan jullie gedacht en een aantal belangrijke verbeteringen doorgevoerd in het gebruikersbeheer. We hopen dat jullie ervan genieten.';
+$lang['porg_v15_user_manager_image6_label'] = 'Filters in de gebruikerslijst worden slimmer: het aantal gebruikers voor elke optie wordt weergegeven en lege opties worden uitgeschakeld.';
+$lang['porg_v15_user_manager_image5_label'] = 'U kunt nu de hoofdbeheerder van Piwigo wijzigen in de gebruikersinterface. Dit werd voorheen beheerd door de lokale configuratie-instelling "webmaster_id". Alleen een webmaster kan worden gekozen als hoofdgebruiker. Deze wordt in de gebruikerslijst weergegeven door een gele kroon.';
+$lang['porg_v15_user_manager_image4_label'] = 'Het gebruikersbewerkingsformulier is opnieuw ontworpen. Het is nu schoner, eenvoudiger en integreert plug-ins gemakkelijker.';
+$lang['porg_v15_user_manager_image3_label'] = 'Geen e-mailwachtwoorden meer: ​​gebruikersregistratie wordt veiliger. Na het aanmaken van een nieuwe gebruiker, ontvangen ze een link om hun wachtwoord in te stellen voor de eerste keer inloggen. Mocht Piwigo de e-mail niet kunnen versturen (e-mail is verre van 100% betrouwbaar), dan kan elke beheerder een wachtwoord (re)set link regenereren, kopiëren en naar de gebruiker sturen in elk betrouwbaar kanaal.';
+$lang['porg_v15_user_manager_image2_label'] = 'Wanneer u een nieuwe gebruiker aanmaakt, kunt u nu direct de gebruikersstatus, het privacyniveau en de groepen instellen. En u hoeft geen wachtwoord meer aan te maken.';
+$lang['porg_v15_user_manager_image1_label'] = 'Hier heb je op gewacht! Je kunt nu de lijst met gebruikers sorteren op gebruikersnaam of registratiedatum, in oplopende of aflopende volgorde, met slechts één klik.';
+$lang['porg_v15_technical_features7'] = 'We hebben de code van de nieuwe gebruikerspopin en de nieuwe batchmanager in unit-modus zo ontworpen dat deze eenvoudig uitbreidbaar is. We hebben plugin Skeleton bijgewerkt om voorbeelden te tonen.';
+$lang['porg_v15_technical_features6'] = 'Uitbreidbaarheid';
+$lang['porg_v15_technical_features3'] = 'Lees dit aandachtig door om uw extensies compatibel te maken met Piwigo 15.';
+$lang['porg_v15_technical_features2'] = 'Technische wijzigingen in Piwigo 15';
+$lang['porg_v15_technical_features15'] = 'API-wijzigingen.';
+$lang['porg_v15_technical_features14'] = 'De "persistente" <code>_data/cache</code> wordt niet vaak gewist. Aangezien het vaak gerelateerd is aan de gebruikerscache, zal Piwigo 15 het wissen elke keer dat we <code>invalidate_user_cache</code>.';
+$lang['porg_v15_technical_features13'] = 'Optimaliseer cachegrootte';
+$lang['porg_v15_technical_features1'] = 'Houd er rekening mee dat er meer gedetailleerde documentatie beschikbaar is over';
+$lang['porg_v15_system_activities_title'] = 'Systeemactiviteiten';
+$lang['porg_v15_system_activities_image1_label'] = 'Gebruikersactiviteiten zijn geïntroduceerd in versie 12. Systeemactiviteiten zijn toegevoegd in versie 15. U kunt zien wanneer uw Piwigo is geüpgraded, naar welke versie, wanneer een plugin is geactiveerd, wanneer een onderhoudsactie is uitgevoerd.';
+$lang['porg_v15_searchdynamicfilter_title'] = 'Dynamische filters';
+$lang['porg_v15_searchdynamicfilter_text'] = 'De nieuwe zoekmachine, geïntroduceerd in versie 14, heeft een grote verbetering ondergaan.
+U kunt nu op elk gewenst moment de waarde van een filter wijzigen, zonder dat u uw zoekopdracht opnieuw hoeft te starten.<br>
+<br>
+Als u bijvoorbeeld het filter "Toegevoegd door: John" hebt toegepast, kunt u de selectie wijzigen in "Toegevoegd door: Paul". En als u een ander filter actief hebt, zoals "bestandstype: MP4", ziet u alleen gebruikers die een MP4-bestand hebben toegevoegd in het filter "Toegevoegd door". Het is een echte verbetering van de gebruikerservaring en we hopen dat u het zult waarderen.';
+$lang['porg_v15_search_engine_title'] = 'Zoekmachine';
+$lang['porg_v15_search_engine_image2_label'] = 'Als u foto\'s wilt filteren op aanmaakdatum, kunt u nu een aangepast datumbereik selecteren, dat een jaar, een maand, een dag of meerdere tegelijk kan zijn. U krijgt ook slimme controles om ouder- en kinddatums weer te geven.';
+$lang['porg_v15_search_engine_image1_label'] = 'De zoekfunctie in de galerij krijgt nieuwe filters. U kunt nu foto\'s filteren: op beoordelingen, op bestandsgrootte, op dimensieverhouding (portret, vierkant, landschap), op hoogte, op breedte en op aanmaakdatum.';
+$lang['porg_v15_marceau_text2'] = 'Naast een paar andere missies ben ik degene die de meeste integratie van de gloednieuwe Batch Manager Unit Mode voor zijn rekening neemt. Dit was een vervelende taak, maar ik hoop dat Piwigo-gebruikers en -medewerkers het leuk zullen vinden en ik ben benieuwd naar jullie mening hierover.';
+$lang['porg_v15_marceau_text1'] = 'Mijn naam is Marceau, ik ben een student webontwikkeling en ik heb van april tot september 2024 als stagiair met het team mogen werken. Mijn doel als ontwikkelaar is om efficiënt te coderen en ervoor te zorgen dat gebruikers zich op hun gemak voelen bij het gebruik van alles wat ik integreer.';
+$lang['porg_v15_intro2'] = 'Soms zijn het de kleine, ogenschijnlijk onbeduidende dingen die alles veranderen. Piwigo 15 brengt een reeks verbeteringen om het werk van gebruikers comfortabeler te maken. Met een sterke focus op gebruikersbeheer, het herontwerp van de batchmanager in unitaire modus, nieuwe dynamische filters in zoeken en andere UX-verbeteringen, richt deze versie zich op functies die sommige mensen elke dag gebruiken. En dan hebben we het nog niet eens gehad over nieuwe functies zoals de systeemactiviteitsgeschiedenis of een ingebedde PDF-lezer.';
+$lang['porg_v15_intro1'] = 'Maak je leven makkelijker';
+$lang['porg_v15_embedpdf_title'] = 'Ingebouwde PDF-lezer';
+$lang['porg_v15_embedpdf_image1_label'] = 'Bij het bekijken van een PDF-bestand in uw galerij opent Piwigo 15 automatisch uw webbrowser PDF-lezer, ingebed in de pagina. Werkt op dit moment op Modus en Bootstrap Darkroom thema\'s.';
+$lang['porg_v15_batch_manager_title'] = 'Bij het bekijken van een PDF-bestand in uw galerij opent Piwigo 15 automatisch uw webbrowser PDF-lezer, ingebed in de pagina. Werkt op dit moment op Modus en Bootstrap Darkroom thema\'s.';
+$lang['porg_v15_batch_manager_image2_label'] = 'In de globale modus kunnen beheerders nu meerdere albums selecteren om te koppelen, allemaal in dezelfde actie.';
+$lang['porg_v15_batch_manager_image1_label'] = 'De eenheidsmodus van de Batch Manager is opnieuw ontworpen: u kunt nu filters aanpassen zoals in de globale modus, een groter beeld om eigenschappen beter te kunnen vullen, geoptimaliseerde ruimte en meer acties beschikbaar voor elke foto.';
+$lang['porg_v15_badge_refresh'] = 'vernieuwen';
+$lang['porg_v15_badge_redesign'] = 'her-ontwerp';
+$lang['porg_v15_badge_new'] = 'nieuw';
+$lang['porg_v15_api_s1'] = 'accepteer image_ids als komma-gescheiden lijst';
+$lang['porg_v15_anonymous_stats_title'] = 'Anonieme statistieken';
+$lang['porg_v15_anonymous_stats_text'] = 'Bij Piwigo weten we niet veel over onze gebruikers. We hebben geen exact idee van hoeveel Piwigo\'s er zijn geïnstalleerd. We hebben wat bewijs dat je meer dan 100k hebt, maar dat kunnen er veel meer zijn. We weten niet of je veel gebruikers hebt of het alleen voor jezelf gebruikt. We weten niet of het voor 100 of 100k foto\'s is. Met dit soort gegevens kunnen we ontwerp-/ontwikkelingsbeslissingen nemen. Daarom gaan we nu wat statistieken verzamelen.<br>
+<br>
+We willen niets privé weten: we kennen niet eens het webadres van je Piwigo. We willen alleen algemene statistieken. Details over wat er wordt gedeeld, staan ​​in het Github-probleem. Je kunt deze functie de-activeren, maar we hopen dat je dat niet doet, omdat je onze code kunt zien en ons kunt vertrouwen.';
+$lang['porg_v15_album_selector_title'] = 'Albumkiezer';
+$lang['porg_v15_album_selector_image2_label'] = 'Tweede toevoeging: u kunt nu een nieuw album maken waar u maar wilt. In dit voorbeeld ziet u dat de albumselector is toegevoegd aan het foto-uploadformulier.';
+$lang['porg_v15_album_selector_image1_label'] = 'Het associëren van een foto met een of meerdere albums wordt eenvoudiger, zelfs als u veel albums in uw bibliotheek hebt. De albumselector krijgt inderdaad belangrijke verbeteringen. De eerste: het toont altijd de hiërarchie van albums. U hoeft geen zoekopdracht uit te voeren.';
+$lang['porg_v15_album_manager_title'] = 'Albumbeheerder';
+$lang['porg_v15_album_manager_text'] = 'De albummanager presteert veel beter op hoog volume. Tot nu toe kon het, als je veel albums had - en we bedoelen meer dan 10 duizend - enkele seconden duren voordat de albummanager geladen was, afhankelijk van de snelheid van je computer. Met Piwigo 15 en optimalisaties geïmplementeerd door Linty, laadt de albummanager in milliseconden, ongeacht het aantal albums.';
+$lang['porg_v15_album_manager_image1_label'] = 'Zoeken naar een album wordt eenvoudiger! Het albumzoekveld, dat voorheen als een speciaal tabblad werd aangeboden, is nu direct beschikbaar in de albummanager.';
+$lang['The menu'] = 'Het menu';
+$lang['Technical features'] = 'Technische kenmerken';
+$lang['Interview %s'] = 'Interview van %s.';
+$lang['Enhanced user management, system activities, batch manager redesign.'] = 'Verbeterd gebruikersbeheer, systeemactiviteiten en herontwerp van batchbeheer.';
