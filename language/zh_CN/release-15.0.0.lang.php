780159a86c186aefb640d2d2e7c13e94260060c7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-10-10 18:30:27 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.0.0.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v15_batch_manager_image1_label'] = '批量管理器单一模式已重新设计：可以像在全局模式中一样修改过滤器，提供更大的图片以帮助填写属性，优化空间，为每张照片提供更多可用操作。';
+$lang['porg_v15_batch_manager_image2_label'] = '在全局模式下，管理员现在可以选择多个相册进行关联，所有操作都在同一动作中完成。';
+$lang['porg_v15_intro1'] = '让你的生活更轻松';
+$lang['porg_v15_technical_features1'] = '请注意，有关 Piwigo 15 的更详细文档可供参考。';
+$lang['porg_v15_technical_features2'] = 'Piwigo 15的技术变化';
+$lang['porg_v15_technical_features3'] = '请阅读以使您的扩展与 Piwigo 15 兼容。';
+$lang['porg_v15_batch_manager_title'] = '批量管理器';
+$lang['porg_v15_technical_features13'] = '优化缓存大小';
+$lang['porg_v15_technical_features14'] = '“持久性”_data/cache不经常被清除。考虑到它通常与用户缓存有关，Piwigo 15 每次都会清除它invalidate_user_cache。';
+$lang['porg_v15_technical_features6'] = '可扩展性';
+$lang['porg_v15_technical_features7'] = '我们设计了新的用户弹出窗口和单元模式下的新批量管理器的代码，使其易于扩展。我们更新了插件 Skeleton 以展示示例。';
+$lang['porg_v15_search_engine_image1_label'] = '图库中的搜索功能新增了过滤器。现在，您可以按以下条件过滤照片：评分、文件大小、尺寸比例（纵向、方形、横向）、高度、宽度和创建日期。';
+$lang['porg_v15_search_engine_image2_label'] = '如果你需要按创建日期过滤照片，现在可以选择自定义日期范围，可以是某一年、某个月、某一天，或同时选择多个日期。你还可以使用智能检查来显示父日期和子日期。';
+$lang['porg_v15_search_engine_title'] = '搜索引擎';
+$lang['porg_v15_embedpdf_image1_label'] = '当您在图库中查看 PDF 文件时，Piwigo 15 将自动打开嵌入在页面中的 Web 浏览器 PDF 阅读器。目前适用于 Modus 和 Bootstrap Darkroom 主题。';
+$lang['porg_v15_embedpdf_title'] = '嵌入式 PDF 阅读器';
+$lang['porg_v15_marceau_text1'] = '我的名字是 Marceau，我是一名Web开发专业的学生，​​我有机会在2024年4月至9月期间作为实习生与团队合作。作为开发人员，我的目标是高效地编写代码并确保用户能够舒适地使用我集成的所有内容。';
+$lang['porg_v15_marceau_text2'] = '除了其他几项任务外，我还负责了全新批次管理器单元模式的大部分集成工作。这是一项繁琐的任务，但我希望 Piwigo 用户和合作者会喜欢它，我也很想听听你们的想法。';
+$lang['porg_v15_searchdynamicfilter_text'] = '在版本 14 中引入的新搜索引擎进行了重大改进。你现在可以随时更改过滤器的值，而无需重新开始搜索。<br>
+<br>
+例如，如果你应用了“添加者：John”过滤器，你可以将选择更改为“添加者：Paul”。如果你还有另一个过滤器处于活动状态，例如“文件类型：MP4”，你将只会看到在“添加者”过滤器中添加了 MP4 文件的用户。这是一个真正的用户体验改进，我们希望你会喜欢。';
+$lang['porg_v15_searchdynamicfilter_title'] = '动态过滤器';
+$lang['porg_v15_system_activities_image1_label'] = '用户活动在版本 12 中引入。系统活动在版本 15 中添加。你可以看到你的 Piwigo 何时升级，升级到哪个版本，何时激活插件，何时执行维护操作……';
+$lang['porg_v15_system_activities_title'] = '系统活动';
+$lang['porg_v15_user_manager_image1_label'] = '你一直在等待这个功能！现在，你可以通过点击一下按用户名或注册日期对用户列表进行升序或降序排序';
+$lang['porg_v15_user_manager_image2_label'] = '当你创建新用户时，现在可以立即设置用户状态、隐私级别和群组。而且你不再需要创建密码。';
+$lang['porg_v15_user_manager_image3_label'] = '不再通过电子邮件发送密码：用户注册变得更安全。创建新用户后，他们将收到一个链接，以便在首次登录前设置密码。如果 Piwigo 无法发送电子邮件（电子邮件远非 100% 可靠），任何管理员都可以重新生成密码重置链接，复制并通过任何可靠的渠道发送给用户。';
+$lang['porg_v15_user_manager_image4_label'] = '用户编辑表单已重新设计。现在它更简洁、更简单，并且更容易集成插件。';
+$lang['porg_v15_user_manager_image5_label'] = '你现在可以在用户界面中更改 Piwigo 的主要管理员。之前这是通过本地配置设置 “webmaster_id” 来管理的。只有网站管理员可以被选为主要用户。在用户列表中，它由一个黄色皇冠表示。';
+$lang['porg_v15_user_manager_image6_label'] = '用户列表中的过滤器变得更智能：显示每个选项的用户数量，并禁用空选项。';
+$lang['porg_v15_user_manager_text'] = '我们知道你们中的一些人在 Piwigo 中管理着数百个用户。有时你需要定期创建新用户，或者管理那些从你的网络图库创建账户的用户。Piwigo 15 考虑到了你的需求，并对用户管理进行了重大改进。希望你会喜欢这些改进。';
+$lang['porg_v15_user_manager_title'] = '用户管理器';
+$lang['porg_v15_willy_text1'] = '大家好，我叫 Willy（在论坛上叫 Linty），我以学生全栈开发者的身份加入 Piwigo 团队已经快一年了。我一直对开发充满热情，我参与的每个项目都是我分享这份热情的机会。';
+$lang['porg_v15_willy_text2'] = '在 Piwigo，我有机会改进了一些功能，比如相册选择器和用户管理器，并创建了新的工具，比如系统活动。我希望我开发的每个功能都能带来我对这份工作的热情，并让使用软件的人感受到这种热情。能够成为一个分享这种愿景的团队的一员，我感到非常鼓舞，并期待继续与 Piwigo 一起创新！';
+$lang['porg_v15_intro2'] = '有时候，正是那些看似微小的变化改变了一切。Piwigo 15 带来了许多改进，使用户的工作更加舒适。新版重点关注用户管理，重新设计了单一模式下的批处理管理器，新增了搜索中的动态过滤器和其他用户体验改进，这些功能是一些人每天都会使用的。此外，还增加了系统活动历史记录和嵌入式 PDF 阅读器等新功能。';
+$lang['porg_v15_technical_features15'] = 'API 更改';
+$lang['Enhanced user management, system activities, batch manager redesign.'] = '增强的用户管理、系统活动、批处理管理器重新设计。';
+$lang['Interview %s'] = '
+%s的采访';
+$lang['Technical features'] = '技术特点';
+$lang['The menu'] = '菜单';
+$lang['porg_v15_album_manager_image1_label'] = '搜索相册变得更容易了！以前作为一个独立标签提供的相册搜索字段，现在可以直接在相册管理器中使用。';
+$lang['porg_v15_album_manager_text'] = '相册管理器在高容量时性能大大提升。到目前为止，如果你有很多相册 - 我们指的是超过1万个 - 相册管理器可能需要几秒钟才能加载，具体取决于你电脑的速度。随着 Piwigo 15和Linty实施的优化，无论相册数量多少，相册管理器都将在毫秒内加载。';
+$lang['porg_v15_album_manager_title'] = '相册管理器';
+$lang['porg_v15_album_selector_image1_label'] = '将照片关联到一个或多个相册变得更容易，即使你的库中有很多相册。实际上，相册选择器得到了重要的增强。首先:它始终显示相册的层次结构。无需进行搜索。';
+$lang['porg_v15_album_selector_image2_label'] = '第二个新增功能：你现在可以在任何需要的地方创建新相册。在这个例子中，你可以看到相册选择器已被添加到照片上传表单中。';
+$lang['porg_v15_album_selector_title'] = '相册选择器';
+$lang['porg_v15_anonymous_stats_text'] = '在 Piwigo，我们对用户了解不多。我们并不确切知道有多少 Piwigo 被安装。我们有一些证据表明你们超过 10 万，但可能远不止这些。我们不知道你们是否有很多用户，还是仅供自己使用。我们不知道是用于 100 张照片还是 10 万张照片。拥有这些数据将帮助我们做出设计/开发决策。这就是为什么我们现在将收集一些统计数据。<br>
+<br>
+我们不想知道任何私人信息：我们甚至不知道你们 Piwigo 的网址。我们只想要一般的统计数据。共享的详细信息在 Github 问题中提供。你可以禁用此功能，但我们希望你不会，因为你可以查看我们的代码并信任我们。';
+$lang['porg_v15_anonymous_stats_title'] = '匿名统计';
+$lang['porg_v15_api_s1'] = '接受以逗号分隔的列表形式显示 image_id';
+$lang['porg_v15_badge_new'] = '新的';
+$lang['porg_v15_badge_redesign'] = '重新设计';
+$lang['porg_v15_badge_refresh'] = '刷新';
