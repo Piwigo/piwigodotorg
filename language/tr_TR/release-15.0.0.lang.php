322b38ae8e30f76a7a42e6a1d1a28e0f5e7a11d6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-10-11 08:29:10 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/release-15.0.0.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v15_user_manager_image1_label'] = 'Bunu bekliyordunuz! Artık kullanıcı listesini kullanıcı adına veya kayıt tarihine göre artan veya azalan düzende tek bir tıklamayla sıralayabilirsiniz.';
+$lang['porg_v15_user_manager_image2_label'] = 'Yeni bir kullanıcı oluşturduğunuzda artık kullanıcı durumunu, gizlilik seviyesini ve grupları hemen ayarlayabilirsiniz. Ve artık bir parola oluşturmanız gerekmiyor.';
+$lang['porg_v15_user_manager_image3_label'] = 'E-postayla gönderilen şifreler yok: kullanıcı kaydı daha güvenli hale geliyor. Yeni bir kullanıcı oluşturduktan sonra, ilk oturum açmadan önce şifrelerini ayarlamaları için bir bağlantı alacaklar. Piwigo\'nun e-postayı gönderememesi durumunda (e-posta %100 güvenilir olmaktan uzaktır), herhangi bir yönetici bir şifre (yeniden) sıfırlama bağlantısı oluşturabilir, kopyalayabilir ve kullanıcıya herhangi bir güvenilir kanaldan gönderebilir.';
+$lang['porg_v15_user_manager_image4_label'] = 'Kullanıcı düzenleme formu yeniden tasarlandı. Artık daha temiz, daha basit ve eklentileri daha kolay entegre ediyor.';
+$lang['porg_v15_user_manager_image5_label'] = 'Artık ana Piwigo yöneticisini kullanıcı arayüzünde değiştirebilirsiniz. Bu, daha önce "webmaster_id" yerel yapılandırma ayarı tarafından yönetiliyordu. Ana kullanıcı olarak yalnızca bir web yöneticisi seçilebilir. Kullanıcı listesinde sarı bir taç ile temsil edilir.';
+$lang['porg_v15_user_manager_image6_label'] = 'Kullanıcı listesindeki filtreler daha akıllı hale gelir: her seçenek için kullanıcı sayısını görüntüleyin ve boş seçenekleri devre dışı bırakın.';
+$lang['porg_v15_user_manager_text'] = 'Bazılarınızın Piwigo\'nuzda yüzlerce kullanıcıyı yönettiğini biliyoruz. Bazen düzenli olarak yeni kullanıcılar oluşturmanız veya hesaplarını oluşturan kullanıcıları web galerinizden yönetmeniz gerekir. Piwigo 15 ile sizi düşündük ve kullanıcı yönetiminde bazı önemli iyileştirmeler yaptık. Umarız beğenirsiniz.';
+$lang['porg_v15_user_manager_title'] = 'Kullanıcı yöneticisi';
+$lang['porg_v15_willy_text1'] = 'Herkese merhaba, benim adım Willy (veya forumdaki Linty) ve Piwigo ekibine öğrenci fullstack developer olarak katılalı neredeyse bir yıl oldu. Gelişim konusunda her zaman tutkulu oldum ve üzerinde çalıştığım her proje benim için bu tutkuyu paylaşmam için bir fırsat.';
+$lang['porg_v15_willy_text2'] = 'Piwigo\'da, albüm seçici veya kullanıcı yöneticisi gibi belirli özellikleri geliştirme ve sistem aktiviteleri gibi yeni araçlar oluşturma şansım oldu. Umarım geliştirdiğim her özellik, bu iş için sahip olduğum o enerji ve coşkuyu biraz getirir ve bu, yazılımı kullananlar tarafından hissedilir. Bu vizyonu paylaşan bir ekibin parçası olmak ilham verici ve Piwigo ile yenilik yapmaya devam etmeyi dört gözle bekliyorum!';
+$lang['porg_v15_embedpdf_title'] = 'Gömülü PDF okuyucu';
+$lang['porg_v15_intro1'] = 'Hayatınızı kolaylaştırın';
+$lang['porg_v15_intro2'] = 'Bazen her şeyi değiştiren küçük, görünüşte önemsiz şeylerdir. Piwigo 15, kullanıcıların işini daha rahat hale getirmek için bir dizi iyileştirme getiriyor. Kullanıcı yönetimine güçlü bir odaklanma, üniter modda toplu yöneticinin yeniden tasarımı, aramada yeni dinamik filtreler ve diğer UX iyileştirmeleriyle bu sürüm, bazı kişilerin her gün kullandığı özelliklere odaklanıyor. Sistem etkinlik geçmişi veya gömülü PDF okuyucu gibi yeni özelliklerden bahsetmiyorum bile.';
+$lang['porg_v15_marceau_text1'] = 'Benim adım Marceau, web geliştirme öğrencisiyim ve Nisan-Eylül 2024 tarihleri arasında ekiple stajyer olarak çalışma şansım oldu. Bir geliştirici olarak amacım, verimli bir şekilde kodlamak ve kullanıcıların entegre ettiğim her şeyi kullanırken kendilerini rahat hissetmelerini sağlamaktır.';
+$lang['porg_v15_marceau_text2'] = 'Diğer birkaç görevin yanı sıra, yepyeni Batch Manager Unit Mode\'un entegrasyonunun çoğunun arkasındaki el benim. Bu sıkıcı bir işti ama umarım Piwigo kullanıcıları ve işbirlikçileri bundan hoşlanır ve bu konudaki düşüncelerinizi duymak için sabırsızlanıyorum.';
+$lang['porg_v15_search_engine_image1_label'] = 'Galeride arama yeni filtreler alır. Artık fotoğrafları filtreleyebilirsiniz: derecelendirmelere göre, dosya boyutuna göre, boyut oranına göre (portre, kare, yatay), yüksekliğe, genişliğe ve oluşturma tarihine göre.';
+$lang['porg_v15_search_engine_image2_label'] = 'Fotoğrafları oluşturulma tarihine göre filtrelemeniz gerekiyorsa, artık bir yıl, bir ay, bir gün veya aynı anda birkaç tane olabilen özel bir tarih aralığı seçebilirsiniz. Ayrıca ebeveyn tarihlerini ve alt tarihleri göstermek için akıllı kontroller alırsınız.';
+$lang['porg_v15_search_engine_title'] = 'Arama motoru';
+$lang['porg_v15_searchdynamicfilter_text'] = 'Sürüm 14\'te tanıtılan yeni arama motoru, büyük bir iyileştirme geçirdi.
+Artık, aramanızı yeniden başlatmak zorunda kalmadan bir filtrenin değerini istediğiniz zaman değiştirebilirsiniz. <br>
+<br>
+Örneğin, "Ekleyen: John" filtresini uyguladıysanız, seçimi "Ekleyen: Paul" olarak değiştirebilirsiniz. Ve "dosya türü: MP4" gibi etkin başka bir filtreniz varsa, yalnızca "Ekleyen" filtresine MP4 dosyası ekleyen kullanıcıları görürsünüz. Bu gerçek bir kullanıcı deneyimi iyileştirmesidir ve umarız takdir edersiniz.';
+$lang['porg_v15_searchdynamicfilter_title'] = 'Dinamik filtreler';
+$lang['porg_v15_system_activities_image1_label'] = 'Kullanıcı aktiviteleri sürüm 12\'de tanıtıldı. Sistem etkinlikleri sürüm 15\'te eklenmiştir. Piwigo\'nuzun ne zaman yükseltildiğini, hangi sürüme yükseltildiğini, bir eklentinin ne zaman etkinleştirildiğini, ne zaman bir bakım işlemi gerçekleştirildiğini görebilirsiniz...';
+$lang['porg_v15_system_activities_title'] = 'Sistem aktiviteleri';
+$lang['porg_v15_technical_features1'] = 'Lütfen aşağıdakiler hakkında daha ayrıntılı belgelerin mevcut olduğunu unutmayın:';
+$lang['porg_v15_technical_features13'] = 'Önbellek boyutunu optimize edin';
+$lang['porg_v15_technical_features14'] = '"Kalıcı" <code>_data/önbellek</code> genellikle temizlenmez. Genellikle kullanıcı önbelleği ile ilgili olduğu göz önüne alındığında, Piwigo 15 her <code>invalidate_user_cache</code> yaptığımızda bunu temizleyecektir.';
+$lang['porg_v15_technical_features15'] = 'API değişiklikleri';
+$lang['porg_v15_technical_features2'] = 'Piwigo 15\'teki teknik değişiklikler';
+$lang['porg_v15_technical_features3'] = 'Eklentilerinizi Piwigo 15 ile uyumlu hale getirmek için lütfen okuyunuz.';
+$lang['porg_v15_technical_features6'] = 'Genişletilebilirlik';
+$lang['porg_v15_technical_features7'] = 'Yeni kullanıcı popin\'inin ve birim modundaki yeni toplu yöneticinin kodunu kolayca genişletilebilir olacak şekilde tasarladık. Örnekleri göstermek için eklenti Skeleton\'ı güncelledik.';
+$lang['Enhanced user management, system activities, batch manager redesign.'] = 'Gelişmiş kullanıcı yönetimi, sistem etkinlikleri, toplu iş yöneticisinin yeniden tasarımı.';
+$lang['Interview %s'] = '%s\'s röportaj';
+$lang['Technical features'] = 'Teknik özellikler';
+$lang['The menu'] = 'Menü';
+$lang['porg_v15_album_manager_image1_label'] = 'Bir albüm aramak artık daha kolay! Daha önce özel bir sekme olarak sağlanan albüm arama alanı artık doğrudan albüm yöneticisinde kullanılabilir.';
+$lang['porg_v15_album_manager_text'] = 'Albüm yöneticisi yüksek ses seviyesinde çok daha iyi performanslar elde ediyor. Şimdiye kadar, çok sayıda albümünüz varsa - ve 10 binden fazla demek istiyoruz - albüm yöneticisinin yüklenmesi, bilgisayarınızın hızına bağlı olarak birkaç saniye sürebilir. Piwigo 15 ve Linty tarafından uygulanan optimizasyonlar ile albüm yöneticisi, albüm sayısı ne olursa olsun milisaniyeler içinde yüklenir.';
+$lang['porg_v15_album_manager_title'] = 'Albüm yöneticisi';
+$lang['porg_v15_album_selector_image1_label'] = 'Kitaplığınızda çok sayıda albüm olsa bile, bir fotoğrafı bir veya birkaç albümle ilişkilendirmek daha kolay hale gelir. Gerçekten de, albüm seçici önemli geliştirmeler alıyor. Birincisi: her zaman albümlerin hiyerarşisini görüntüler. Arama yapmanıza gerek yok.';
+$lang['porg_v15_album_selector_image2_label'] = 'İkinci ekleme: Artık ihtiyacınız olan her yerde yeni bir albüm oluşturabilirsiniz. Bu örnekte, albüm seçicinin fotoğraf yükleme formuna eklendiğini görebilirsiniz.';
+$lang['porg_v15_album_selector_title'] = 'Albüm seçici';
+$lang['porg_v15_anonymous_stats_text'] = 'Piwigo\'da kullanıcılarımız hakkında çok fazla şey bilmiyoruz. Kaç tane Piwigo\'nun kurulu olduğu hakkında kesin bir fikrimiz yok. 100 binin üzerinde olduğunuza dair bazı kanıtlarımız var, ancak bu çok daha fazla olabilir. Çok sayıda kullanıcınız olup olmadığını veya sadece kendiniz için mi kullandığınızı bilmiyoruz. 100 fotoğraf için mi yoksa 100 bin fotoğraf için mi bilmiyoruz. Bu tür verilere sahip olmak, tasarım/geliştirme kararları almamıza yardımcı olacaktır. Bu yüzden şimdi bazı istatistikler toplayacağız. <br>
+<br>
+Özel bir şey bilmek istemiyoruz: Piwigo\'nuzun web adresini bile bilmiyoruz. Biz sadece genel istatistikler istiyoruz. Nelerin paylaşıldığına dair ayrıntılar Github sorununda verilmiştir. Bu özelliği devre dışı bırakabilirsiniz, ancak umarız devre dışı bırakmazsınız, çünkü kodumuzu görebilir ve bize güvenebilirsiniz.';
+$lang['porg_v15_anonymous_stats_title'] = 'Anonim istatistikler';
+$lang['porg_v15_api_s1'] = 'image_ids virgülle ayrılmış liste olarak kabul et';
+$lang['porg_v15_badge_new'] = 'yeni';
+$lang['porg_v15_badge_redesign'] = 'yeniden tasarım';
+$lang['porg_v15_badge_refresh'] = 'yenile';
+$lang['porg_v15_batch_manager_image1_label'] = 'Batch Manager Birim Modu yeniden tasarlandı: genel modda olduğu gibi filtreleri değiştirme yeteneği, özellikleri doldurmaya yardımcı olmak için daha büyük resim, optimize edilmiş alan, her fotoğraf için daha fazla eylem mevcut.';
+$lang['porg_v15_batch_manager_image2_label'] = 'Genel modda, yöneticiler artık ilişkilendirmek için tümü aynı eylemde olan birkaç albüm seçebilir.';
+$lang['porg_v15_batch_manager_title'] = 'Toplu iş yöneticisi';
+$lang['porg_v15_embedpdf_image1_label'] = 'Galerinizde bir PDF dosyasını görüntülerken, Piwigo 15 otomatik olarak web tarayıcınızın PDF okuyucusunu sayfa içine gömülü olarak açacaktır. Şimdilik Modus ve Bootstrap Darkroom temaları üzerinde çalışıyor.';
