5c49dc9092a8f797d3b56b68304afb9f91ff7fae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-10-24 06:51:45 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-15.0.0.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v15_user_manager_image2_label'] = 'Kiedy tworzysz nowego użytkownika, możesz teraz natychmiast ustawić status użytkownika, poziom prywatności i grupy. I nie musisz już tworzyć hasła.';
+$lang['porg_v15_user_manager_image3_label'] = 'Koniec z hasłami wysyłanymi e-mailem: rejestracja użytkownika staje się bezpieczniejsza. Po utworzeniu nowego użytkownika, otrzyma on link do ustawienia hasła przed pierwszym logowaniem. W przypadku, gdy Piwigo nie będzie w stanie wysłać wiadomości e-mail (poczta e-mail jest daleka od 100% niezawodności), każdy administrator może ponownie wygenerować link do ponownego ustawienia hasła, skopiować go i wysłać użytkownikowi dowolnym niezawodnym kanałem.';
+$lang['porg_v15_user_manager_image4_label'] = 'Formularz edycji użytkownika został przeprojektowany. Jest teraz bardziej przejrzysty, prostszy i łatwiej integruje wtyczki.';
+$lang['porg_v15_user_manager_image5_label'] = 'Możesz teraz zmienić głównego administratora Piwigo w interfejsie użytkownika. Wcześniej było to zarządzane przez lokalne ustawienie konfiguracji „webmaster_id”. Tylko webmaster może zostać wybrany jako główny użytkownik. Jest on reprezentowany na liście użytkowników przez żółtą koronę.';
+$lang['porg_v15_user_manager_image6_label'] = 'Filtry na liście użytkowników stają się bardziej inteligentne: wyświetlają liczbę użytkowników dla każdej opcji i wyłączają puste opcje.';
+$lang['porg_v15_user_manager_text'] = 'Wiemy, że niektórzy z Was zarządzają setkami użytkowników w Piwigo. Czasami musicie regularnie tworzyć nowych użytkowników lub zarządzać użytkownikami, którzy tworzą swoje konta z galerii internetowej. W Piwigo 15 pomyśleliśmy o Was i wprowadziliśmy kilka istotnych ulepszeń w zarządzaniu użytkownikami. Mamy nadzieję, że Wam się spodoba.';
+$lang['porg_v15_user_manager_title'] = 'Menedżer użytkowników';
+$lang['porg_v15_willy_text1'] = 'Cześć wszystkim, nazywam się Willy (lub Linty na forum) i minął prawie rok odkąd dołączyłem do zespołu Piwigo jako student programowania full stack. Zawsze pasjonowałem się programowaniem, a każdy projekt, nad którym pracuję, jest dla mnie okazją do dzielenia się tą pasją.';
+$lang['porg_v15_willy_text2'] = 'W Piwigo miałem okazję udoskonalić pewne funkcje, takie jak selektor albumów lub menedżer użytkowników, a także stworzyć nowe narzędzia, takie jak aktywności systemowe. Mam nadzieję, że każda funkcja, którą opracowuję, wniesie odrobinę tej energii i entuzjazmu, które mam do tej pracy, i że poczują to osoby korzystające z oprogramowania. To inspirujące być częścią zespołu, który podziela tę wizję, i nie mogę się doczekać, aby kontynuować innowacje z Piwigo!';
+$lang['porg_v15_batch_manager_image2_label'] = 'W trybie globalnym administratorzy mogą teraz wybrać kilka albumów do powiązania, a wszystko to w ramach tej samej akcji.';
+$lang['porg_v15_embedpdf_image1_label'] = 'Podczas przeglądania pliku PDF w galerii, Piwigo 15 automatycznie otworzy osadzony czytnik PDF w przeglądarce internetowej. Na razie działa w motywach Modus i Bootstrap Darkroom.';
+$lang['porg_v15_embedpdf_title'] = 'Osadzony czytnik PDF';
+$lang['porg_v15_intro1'] = 'Ułatw sobie życie';
+$lang['porg_v15_marceau_text1'] = 'Nazywam się Marceau, jestem studentem programowania stron internetowych i miałem okazję pracować z zespołem jako stażysta od kwietnia do września 2024 r. Moim celem jako programisty jest wydajne programowanie i upewnienie się, że użytkownicy czują się komfortowo korzystając ze wszystkiego, co integruję.';
+$lang['porg_v15_search_engine_image1_label'] = 'Wyszukiwanie w galerii otrzymuje nowe filtry. Teraz możesz filtrować zdjęcia: według ocen, rozmiaru pliku, współczynnika wymiarów (pionowy, kwadratowy, poziomy), wysokości, szerokości i daty utworzenia.';
+$lang['porg_v15_search_engine_image2_label'] = 'Jeśli chcesz filtrować zdjęcia według daty utworzenia, możesz teraz wybrać niestandardowy zakres dat, który może obejmować rok, miesiąc, dzień lub kilka z nich naraz. Możesz również korzystać z inteligentnych kontroli, aby wyświetlać daty elementów nadrzędnych i podrzędnych.';
+$lang['porg_v15_search_engine_title'] = 'Silnik wyszukiwania';
+$lang['porg_v15_searchdynamicfilter_text'] = 'Nowa wyszukiwarka, wprowadzona w wersji 14, otrzymała znaczną usprawnienia.
+Możesz teraz zmienić wartość filtra w dowolnym momencie, bez konieczności ponownego uruchamiania wyszukiwania.<br>
+<br>
+Na przykład, jeśli zastosowałeś filtr „Dodane przez: Jan”, możesz zmienić wybór na „Dodane przez: Paweł”. A jeśli masz aktywny inny filtr, taki jak „typ pliku: MP4”, zobaczysz tylko użytkowników, którzy dodali plik MP4 w filtrze „Dodane przez”. To prawdziwa poprawa komfortu użytkowania i mamy nadzieję, że ją docenisz.';
+$lang['porg_v15_searchdynamicfilter_title'] = 'Filtry dynamiczne';
+$lang['porg_v15_system_activities_image1_label'] = 'Aktywności użytkownika wprowadzono w wersji 12. Aktywności systemowe dodano w wersji 15. Możesz zobaczyć, kiedy Twoje Piwigo zostało uaktualnione, do której wersji, kiedy wtyczka została aktywowana, kiedy wykonano akcję konserwacyjną...';
+$lang['porg_v15_system_activities_title'] = 'Aktywności systemowe';
+$lang['porg_v15_technical_features1'] = 'Należy pamiętać, że dostępna jest bardziej szczegółowa dokumentacja';
+$lang['porg_v15_technical_features13'] = 'Zoptymalizuj rozmiar pamięci podręcznej';
+$lang['porg_v15_technical_features14'] = '„Trwały” <code>_data/cache</code> nie jest często czyszczony. Biorąc pod uwagę, że często jest powiązany z pamięcią podręczną użytkownika, Piwigo 15 będzie go czyścić za każdym razem, gdy <code>invalidate_user_cache</code>.';
+$lang['porg_v15_technical_features15'] = 'Zmiany API';
+$lang['porg_v15_technical_features2'] = 'Zmiany techniczne w Piwigo 15';
+$lang['porg_v15_technical_features3'] = 'Przeczytaj tę informację, aby zapewnić kompatybilność Twoich rozszerzeń z Piwigo 15.';
+$lang['porg_v15_technical_features6'] = 'Elastyczność';
+$lang['porg_v15_user_manager_image1_label'] = 'Czekałeś na to! Teraz możesz sortować listę użytkowników według nazwy użytkownika lub daty rejestracji, w kolejności rosnącej lub malejącej, za pomocą jednego kliknięcia.';
+$lang['porg_v15_batch_manager_image1_label'] = 'Jednolity moduł Menadżera wsadowego został przeprojektowany: możliwość modyfikowania filtrów jak w trybie globalnym, większy obraz ułatwiający wypełnianie właściwości, zoptymalizowana przestrzeń, więcej akcji dostępnych dla każdego zdjęcia.';
+$lang['porg_v15_batch_manager_title'] = 'Menadżer wsadowy';
+$lang['porg_v15_intro2'] = 'Czasami to drobne, pozornie nieistotne rzeczy zmieniają wszystko. Piwigo 15 wprowadza mnóstwo ulepszeń, aby praca użytkowników była bardziej komfortowa. Z silnym naciskiem na zarządzanie użytkownikami, przeprojektowaniem jednolitego modułu menedżera wsadowego, nowymi dynamicznymi filtrami wyszukiwania i innymi ulepszeniami UX, ta wersja koncentruje się na funkcjach, z których niektórzy korzystają codziennie. Nie wspominając o nowych funkcjach, takich jak historia aktywności systemu lub osadzony czytnik PDF.';
+$lang['porg_v15_marceau_text2'] = 'Wśród kilku innych misji, jestem odpowiedzialny za większość integracji zupełnie nowego trybu jednolitego Menedżera wsadowego. To było żmudne zadanie, ale mam nadzieję, że spodoba się użytkownikom Piwigo i współpracownikom, i chętnie usłyszę, co o tym myślicie.';
+$lang['porg_v15_technical_features7'] = 'Zaprojektowaliśmy nowy dialog tworzenia użytkownika i jednolity moduł menedżera wsadowego, aby można go było łatwo rozszerzać. Zaktualizowaliśmy wtyczkę Skeleton, aby pokazać przykłady.';
+$lang['Technical features'] = 'Cechy techniczne';
+$lang['porg_v15_anonymous_stats_title'] = 'Anonimowe statystyki';
+$lang['porg_v15_api_s1'] = 'akceptacja image_ids jako lista rozdzielana przecinkami';
+$lang['porg_v15_badge_new'] = 'nowość';
+$lang['porg_v15_badge_redesign'] = 'przeprojektowane';
+$lang['porg_v15_badge_refresh'] = 'odświeżone';
+$lang['Enhanced user management, system activities, batch manager redesign.'] = 'Ulepszone zarządzanie użytkownikami, działania systemowe, przeprojektowany menedżer wsadowy.';
+$lang['Interview %s'] = 'Wywiad z %s';
+$lang['The menu'] = 'Menu';
+$lang['porg_v15_album_manager_image1_label'] = 'Wyszukiwanie albumu staje się łatwiejsze! Pole wyszukiwania albumu, wcześniej dostępne jako osobna zakładka, jest teraz dostępne bezpośrednio w menedżerze albumów.';
+$lang['porg_v15_album_manager_text'] = 'Menedżer albumów działa znacznie lepiej dla dużych zbiorów. Do tej pory, jeśli miałeś dużo albumów - a mamy na myśli ponad 10 tysięcy - załadowanie menedżera albumów mogło zająć kilka sekund, w zależności od szybkości komputera. Dzięki Piwigo 15 i optymalizacjom wdrożonym przez Linty, menedżer albumów załaduje się w milisekundy, niezależnie od liczby albumów.';
+$lang['porg_v15_album_manager_title'] = 'Menedżer albumów';
+$lang['porg_v15_album_selector_image1_label'] = 'Przypisanie zdjęcia do jednego lub kilku albumów staje się łatwiejsze, nawet jeśli masz wiele albumów w swojej bibliotece. Rzeczywiście, selektor albumów otrzymuje ważne ulepszenia. Pierwsze: zawsze wyświetla hierarchię albumów. Nie ma potrzeby przeprowadzania wyszukiwania.';
+$lang['porg_v15_album_selector_image2_label'] = 'Drugi dodatek: teraz możesz utworzyć nowy album gdziekolwiek potrzebujesz. Na tym przykładzie możesz zobaczyć, że selektor albumu został dodany do formularza przesyłania zdjęć.';
+$lang['porg_v15_album_selector_title'] = 'Selektor albumów';
+$lang['porg_v15_anonymous_stats_text'] = 'W Piwigo niewiele wiemy o naszych użytkownikach. Nie wiemy dokładnie 
+ile jest zainstalowanych Piwigo. Mamy pewne dowody na to, że masz ich ponad 100 tys., ale może to być znacznie więcej. Nie wiemy, czy masz wielu użytkowników, czy używasz ich tylko dla siebie. Nie wiemy, czy chodzi o 100 zdjęć, czy 100 tys. zdjęć. Posiadanie takich danych pomogłoby nam podejmować decyzje dotyczące projektowania/rozwoju. Dlatego teraz zbierzemy pewne statystyki.<br>
+<br>
+Nie chcemy wiedzieć niczego prywatnego: nie znamy nawet adresu internetowego Twojego Piwigo. Chcielibyśmy tylko ogólnych statystyk. Szczegóły dotyczące udostępnianych treści podano w Github. Możesz dezaktywować tę funkcję, ale mamy nadzieję, że tego nie zrobisz, ponieważ możesz przejrzeć nasz kod i nam zaufać.';
