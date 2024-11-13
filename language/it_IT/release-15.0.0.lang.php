fe1c99ee405a27953d19a2a1dc711122412490ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-13 05:41:25 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-15.0.0.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v15_user_manager_title'] = 'Gestione utenti';
+$lang['Enhanced user management, system activities, batch manager redesign.'] = 'Gestione utenti migliorata, attività di sistema, revisione gestione batch.';
+$lang['Interview %s'] = 'Intervista con %s';
+$lang['Technical features'] = 'Caratteristiche tecniche';
+$lang['porg_v15_album_manager_image1_label'] = 'La ricerca degli album è resa più semplice: inizialmente aggiunto in una scheda separata, il campo di ricerca è ora disponibile direttamente dalla gestione degli album.';
+$lang['porg_v15_album_manager_text'] = 'La gestione degli album migliora le prestazioni, anche su grandi volumi. Fino ad ora, se avevi molti album, più di 10.000, il caricamento dell\'elenco degli album poteva richiedere diversi secondi, a seconda della velocità del tuo computer. Con Piwigo 15 e le ottimizzazioni apportate da Linty, il gestore degli album si carica in millisecondi, indipendentemente dal numero di album nel tuo Piwigo.';
+$lang['porg_v15_album_manager_title'] = 'Gestore degli album';
+$lang['porg_v15_album_selector_image1_label'] = 'Associare una foto ad uno o più album diventa più semplice, anche se hai molti album nel tuo Piwigo. In effetti, lo strumento di selezione degli album è stato oggetto di numerosi miglioramenti. <br>
+Innanzitutto, ora viene sempre visualizzata la struttura degli album: non è più necessario effettuare la ricerca.';
+$lang['porg_v15_album_selector_image2_label'] = 'Seconda novità: puoi aggiungere un nuovo album in qualsiasi momento. In questo esempio, puoi vedere che lo strumento di selezione dell\'album è stato aggiunto alla pagina di importazione delle foto.';
+$lang['porg_v15_album_selector_title'] = 'Selettore d\'album';
+$lang['porg_v15_anonymous_stats_text'] = 'Noi di Piwigo non sappiamo molto dei nostri utenti. Non abbiamo un\'idea esatta di quanti Piwigo siano installati. Abbiamo prove che siete più di 100.000, ma potrebbero essere molti di più. Non sappiamo se hai molti utenti o lo usi solo per te stesso. Non sappiamo se si tratta di 100 foto o 100.000 foto. Questo tipo di dati ci aiuterebbe a prendere decisioni di progettazione e sviluppo. Per questo motivo stiamo predisponendo una raccolta di statistiche.<br>
+<br>
+  Non vogliamo sapere nulla di confidenziale: non sappiamo nemmeno l\'indirizzo del tuo Piwigo. Dobbiamo solo raccogliere statistiche generali e anonime. Per scoprire i dettagli dei dati condivisi con noi, puoi guardare il collegamento Github. Puoi disabilitare questa funzione, ma speriamo di no perché il nostro codice è pubblico e puoi fidarti di noi.';
+$lang['porg_v15_anonymous_stats_title'] = 'Statistiche anonime';
+$lang['porg_v15_api_s1'] = 'accetta image_ids come elenco separato da virgole.';
+$lang['porg_v15_badge_new'] = 'nuovo';
+$lang['porg_v15_badge_redesign'] = 'riprogettazione';
+$lang['porg_v15_badge_refresh'] = 'aggiornamento';
+$lang['porg_v15_batch_manager_image1_label'] = 'Un nuovo design per la gestione batch in modalità singola. Possibilità di modificare i filtri come in modalità globale, immagine più grande, spazio ottimizzato, più azioni disponibili per ogni foto.';
+$lang['porg_v15_batch_manager_image2_label'] = 'In modalità globale, gli amministratori possono ora assegnare una selezione di foto a più album in un\'unica azione.';
+$lang['porg_v15_batch_manager_title'] = 'Gestione lotti';
+$lang['porg_v15_embedpdf_image1_label'] = 'Con Piwigo 15, i file PDF si apriranno direttamente nella tua galleria, utilizzando il lettore PDF del tuo browser. Al momento funziona solo sui temi Modus e Bootstrap Darkoom.';
+$lang['porg_v15_embedpdf_title'] = 'Nuovo lettore PDF';
+$lang['porg_v15_intro1'] = 'Rendi la tua vita più semplice';
+$lang['porg_v15_intro2'] = 'A volte sono piccole cose, apparentemente insignificanti, che cambiano tutto. Piwigo 15 apporta numerosi miglioramenti per rendere il lavoro degli utenti più confortevole. Con una grande attenzione alla gestione degli utenti, alla revisione della gestione batch in modalità unità, nuovi filtri dinamici nella ricerca e altri miglioramenti nell\'amministrazione, questa versione enfatizza le funzionalità che alcuni utilizzano ogni giorno. Senza dimenticare alcune nuove funzionalità come la cronologia delle attività del sistema o un nuovo lettore PDF.';
+$lang['porg_v15_marceau_text1'] = 'Mi chiamo Marceau, sono uno studente di sviluppo web e ho avuto la possibilità di unirmi al team Piwigo per uno stage da aprile a settembre 2024. Il mio obiettivo come sviluppatore è produrre codice efficiente e garantire che gli utenti si sentano a proprio agio nell\'usare il funzionalità che integro.';
+$lang['porg_v15_marceau_text2'] = 'Tra le altre cose, mi sono occupato della revisione della gestione dei lotti in modalità unitaria. È stata una missione complessa ma spero che il risultato possa piacere agli utenti e ai collaboratori di Piwigo: non vedo l\'ora di sapere cosa ne pensate!';
+$lang['porg_v15_search_engine_title'] = 'Motore di ricerca';
+$lang['porg_v15_searchdynamicfilter_title'] = 'Filtri dinamici';
+$lang['porg_v15_system_activities_title'] = 'Attività di sistema';
+$lang['porg_v15_technical_features1'] = 'Tieni presente che la documentazione dettagliata è disponibile all\'indirizzo';
+$lang['porg_v15_technical_features13'] = 'Ottimizza la dimensione della cache';
