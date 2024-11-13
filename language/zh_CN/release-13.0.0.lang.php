3339d471ee81cb536db2d0805573b7904e0e6913
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-13 16:37:35 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.0.0.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v13_remi_text1'] = '我的名字是Rémi，在 Piwigo 担任开发人员已有一年多 [编者注：自 2021 年 2 月起]。我加入团队的首要目标是让Android应用赶上iOS应用。这并不容易，但我很有动力！';
+$lang['porg_v13_remi_text2'] = '在努力接管旧的Android应用程序后，我转而创建一个新应用程序。Piwigo NG使用新技术（编者注： Flutter）来简化编码。我非常自豪能成为这个应用程序的创始人之一。我也很自豪能成为像Piwigo这样友好和热情的团队的一员。';
+$lang['porg_v13_svg_text'] = '它们已经支持“任何文件类型”，但Piwigo 13能够直接在网页中显示SVG文件。如果您使用Piwigo来组织您的徽标或图标，您会非常欣赏这一改进！<br>
+<br>
+我们已经在致力于使Modus（默认主题）和Bootstrap Darkroom（第二受欢迎的主题）与此新功能兼容。';
+$lang['porg_v13_svg_title'] = 'SVG文件支持';
+$lang['porg_v13_tagrename_image1_label'] = '我们改变了重命名标签的方式。以前它太小了，所以我们把它移到了一个弹出窗口中。';
+$lang['porg_v13_tagrename_title'] = '标签重命名';
+$lang['porg_v13_technical_features1'] = '请注意，有更详细的文档可供参考';
+$lang['porg_v13_technical_features12'] = '了解更多';
+$lang['porg_v13_technical_features13'] = '类和数据用于&lt;body&gt;';
+$lang['porg_v13_technical_features14'] = '为了简化前端的自定义和可能的处理，我们在&lt;body&gt;标签上添加了CSS类和一个<code>data</code>属性。';
+$lang['porg_v13_technical_features15'] = 'API更改';
+$lang['porg_v13_technical_features2'] = 'Piwigo 13中的技术更改';
+$lang['porg_v13_technical_features3'] = '请阅读它以使您的扩展与Piwigo 13兼容。';
+$lang['porg_v13_technical_features4'] = '用户偏好';
+$lang['porg_v13_technical_features5'] = '位于配置变量（所有用户通用）和cookie（特定于网络浏览器）之间，用户偏好允许用户特定的偏好持久保留，无论会话如何。新函数<code>userprefs_get_param</code>、<code>userprefs_update_param</code>、<code>userprefs_delete_param</code>。';
+$lang['porg_v13_technical_features6'] = '与PHP 8.1兼容';
+$lang['porg_v13_technical_features7'] = 'PHP 8.1即将进入许多网络主机，许多人报告了Piwigo 12的问题。与PHP 8.1的兼容性需要切换到Smarty 4.1（模板引擎）。';
+$lang['porg_v13_plugin_manager_title'] = '插件管理器';
+$lang['Interview %s'] = '%s的采访';
+$lang['New album manager, new visit log, multiple-format improved.'] = '新的相册管理器、新的访问日志、多种格式改进。';
+$lang['Technical features'] = '技术特点';
+$lang['The menu'] = '菜单';
+$lang['porg_v13_albums_image1_label'] = '在这里，您可以创建子相册、重命名相册或在不更改页面的情况下移动任何相册。保证节省时间。';
+$lang['porg_v13_albums_text'] = '我们已将移动相册页面和管理相册页面合并。因此，新的相册管理器显示了一个树状视图，您可以在其中看到所有相册的整个层次结构，而无需随时更改页面。';
+$lang['porg_v13_albums_title'] = '相册管理器';
+$lang['porg_v13_api_s1'] = '搜索匹配提供格式的照片';
+$lang['porg_v13_api_s2'] = '删除扩展照片格式';
+$lang['porg_v13_api_s3'] = '设置用户偏好';
+$lang['porg_v13_api_s4'] = '在访问历史中搜索';
+$lang['porg_v13_api_s5'] = '新的输入参数<code>format_of</code>';
+$lang['porg_v13_api_s6'] = '新的输入参数<code>search</code>和<code>additional_output</code>';
+$lang['porg_v13_api_s7'] = '新的输入参数<code>position</code>';
+$lang['porg_v13_api_s8'] = '新的输出参数<code>is_favorite</code>';
+$lang['porg_v13_apibrowser_image1_label'] = 'API浏览器的大改版。开发人员的工具。在可用方法树上集成了搜索过滤器。';
+$lang['porg_v13_apibrowser_title'] = 'API浏览器';
+$lang['porg_v13_apppromo_image1_label'] = 'Piwigo 13将推广移动应用程序。太多用户仍然不知道它们！当然，只需单击一下，这个横幅广告就会“永久”隐藏。';
+$lang['porg_v13_apppromo_text'] = '正如您可能已经在新闻中看到的那样，Piwigo现在在智能手机上有了强大的竞争力，既适用于iOS（iPhone），也适用于Android（所有其他智能手机）。';
+$lang['porg_v13_apppromo_title'] = '移动应用程序';
