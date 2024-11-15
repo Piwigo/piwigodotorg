6a1702451b6ee03a295e4decff1e59a8211f4908
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:48:15 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.5.0.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1831'] = '[相册编辑] 删除相册时错误的重定向';
+$lang['porg_issue_1832'] = '[用户管理器] 在user_prefs中保存分页 ';
+$lang['porg_issue_1834'] = '[用户管理器] 添加过滤器信息';
+$lang['porg_issue_1835'] = 'Piwigo-13.4.0-在User-Agent中存在存储型XSS漏洞';
+$lang['porg_issue_1836'] = '在小更新时不再执行obsolete.list指令';
+$lang['porg_issue_1837'] = '[相册管理器] 为徽章添加自定义工具提示';
+$lang['porg_issue_1838'] = '[活动] 从用户活动列表中隐藏链接';
+$lang['porg_issue_1839'] = '[活动] 用户注册时缺少用户名';
+$lang['porg_issue_1841'] = '注册系统活动（核心更新、插件激活等）';
+$lang['porg_issue_1849'] = '核心更新后，Piwigo仍显示当前版本为之前的版本';
+$lang['porg_issue_1852'] = '能够禁用核心/扩展更新';
