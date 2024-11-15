365a270c8e584db579fe4a0d46847bbae66967b5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:47:45 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.8.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1914'] = '[13.7.0] 相册排序时出现错误请求';
+$lang['porg_issue_1916'] = '[相册管理器] 排序操作触发错误';
+$lang['porg_issue_1924'] = 'Piwigo 的“用户”功能中存在 SQL 注入';
+$lang['porg_issue_1925'] = '[添加照片] 添加照片页面的水平滚动';
+$lang['porg_issue_GHSA-934w-qj9p-3qcx'] = '“用户代理”中存在 SQL 注入';
