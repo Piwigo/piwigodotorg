ddff7209dee48b5d3cb8ac4bd4b04d1b008c9fcd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:48:28 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.4.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1824'] = '[用户管理器] 即使过滤器关闭，min_register/max_register过滤器仍然应用';
+$lang['porg_issue_1825'] = '[用户管理器] 选择模式设置选择问题';
+$lang['porg_issue_1826'] = '[用户管理器] 创建用户时聚焦在用户名输入框';
+$lang['porg_issue_1827'] = '从13.1升级到13.3后图片消失/被删除';
