4e9841e59c8ef8f717deda27a28bceabeacdeb0b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:48:42 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.3.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1790'] = '[相册管理器] 大型相册数据库在前端和后端的性能问题';
+$lang['porg_issue_1800'] = '用户管理器和照片编辑页面在加泰罗尼亚语中出现页面损坏（JavaScript问题）';
+$lang['porg_issue_1801'] = '计算孤立文件的数量很慢';
+$lang['porg_issue_1802'] = '评论、用户和相册中的转义操作被执行了两次';
+$lang['porg_issue_1803'] = '[相册管理器] 使用tiptip点击操作图标很困难';
+$lang['porg_issue_1806'] = '[相册管理器] 相册管理器的UI升级';
+$lang['porg_issue_1807'] = '[插件管理器] 插件数量很多时出现延迟问题';
+$lang['porg_issue_1808'] = '[13.x，相册管理] 删除相册时总是删除孤立文件';
