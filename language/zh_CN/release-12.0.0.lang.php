25a4b2a9ffa6902a47c36f4c4be926be8088371f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:47:38 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-12.0.0.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_v12_user_manager_text'] = '完全重写的用户管理器：更加现代和多彩，更加简洁，提供更好的用户体验，具有便捷的过滤和专用的选择模式。';
+$lang['porg_v12_user_manager_title'] = '用户管理器';
+$lang['porg_v12_louis_text2'] = '然后我开始着手对版本 12 进行改进：集成设计师准备的页面。我特别致力于新一代用户管理器的开发。<i>编者注：新的用户管理器将在 Piwigo 13 中由 Matthieu 完成，他将获得自己的重点！</i>';
+$lang['porg_v12_maintenance_image1_label'] = '维护页面设计已刷新，现在提供有关缓存大小的更多详细信息以及刷新缓存（在清除之前）的功能。';
+$lang['porg_v12_maintenance_image2_label'] = '维护现在显示活动插件列表。';
+$lang['porg_v12_maintenance_title'] = '维护';
+$lang['porg_v12_plugin_manager_image1_label'] = '在 Piwigo 12 中，只需单击开关即可激活插件。无需重新加载页面。生活改变者！好吧……也许不是你的“整个”生活，但至少是你管理插件时的生活！';
+$lang['porg_v12_plugin_manager_text'] = '插件管理器在 Piwigo 11 中已经进行了改版，但我们决定在版本 12 中再次进行改进。<br> <br>
+1) 到目前为止，激活和停用的插件是分别显示的。现在它们都显示在一起，并且可以过滤。<br> <br>
+2) 在以前的版本中，当你激活一个插件时，屏幕会重新加载，你必须在列表中搜索新激活的插件，以访问其设置。这一点都不实用，尤其是当有很多插件时。';
+$lang['porg_v12_plugin_manager_title'] = '插件管理器';
+$lang['porg_v12_technical_features1'] = '请注意，有关更多详细文档可用';
+$lang['porg_v12_technical_features13'] = '更新的库';
+$lang['porg_v12_technical_features14'] = '主要是为了兼容 PHP 8，我们更新了 Piwigo 核心中使用的一些库：Smarty 3.1.39、PHPMailer 6.5.1、JShrink 1.4.0。';
+$lang['porg_v12_technical_features15'] = 'API 更改';
+$lang['porg_v12_technical_features2'] = 'Piwigo 12 的技术更改';
+$lang['porg_v12_technical_features3'] = '请阅读它以使您的扩展与 Piwigo 12 兼容。';
+$lang['porg_v12_technical_features4'] = '休息室';
+$lang['porg_v12_technical_features5'] = '新的后台机制，“休息室”，在上传照片时进行缓冲。这将避免过多的用户缓存重建。为了更容易理解：在上传照片期间，大型画廊的服务器负载将大大降低。当我们说“大型画廊”时，我们指的是 100,000 张照片及以上。';
+$lang['porg_v12_technical_features6'] = '需要 PHP 7，兼容 PHP 8';
+$lang['porg_v12_technical_features7'] = 'Piwigo 11 是最后一个兼容 PHP 5 的版本。Piwigo 12 现在需要 PHP 7.0。我们还使其兼容 PHP 8。';
+$lang['porg_v12_user_manager_image1_label'] = '用户管理器从头开始重写。用户不能同时被编辑和选择，你要么处于编辑模式，要么处于选择模式。“访客”用户已从列表中删除，并获得了一个专用按钮。添加了高级过滤器。';
+$lang['porg_v12_user_manager_image2_label'] = '在选择模式下，你可以对多个用户同时应用相同的操作。';
+$lang['porg_v12_user_manager_image3_label'] = '用户编辑框也进行了重新设计。';
+$lang['A major update, focused on design and user experience, once more.'] = '重大更新，再次专注于设计和用户体验。';
+$lang['Interview %s'] = '采访 %s';
+$lang['Technical features'] = '技术特性';
+$lang['The menu'] = '菜单';
+$lang['porg_v12_activities_image1_label'] = '您甚至可以将这些活动导出为可以用任何电子表格软件（Excel、Libre Office 等）打开的 CSV 文件。';
+$lang['porg_v12_activities_text'] = 'Piwigo 12 中出现了一个新屏幕：用户活动日志。它允许您查看每个用户在 Piwigo 管理中执行的操作：添加/删除/修改相册、照片、组等。您可以为每个管理员过滤此列表。';
+$lang['porg_v12_activities_title'] = '用户活动日志';
+$lang['porg_v12_album_sort_image1_label'] = '一个新的“移动”按钮从相册管理器转到相册移动器。';
+$lang['porg_v12_album_sort_title'] = '相册排序';
+$lang['porg_v12_alternate_views_image1_label'] = '实际上在 11.4 版本中添加了此功能，让我们正式介绍这个新功能！我们收到了很多关于 Piwigo 11 中相册管理器“平铺”视图的反馈。许多不满的信息。我们决定在相册管理器、插件管理器和新的用户管理器上提供替代视图。';
+$lang['porg_v12_alternate_views_title'] = '替代视图';
+$lang['porg_v12_badge_new'] = '新的';
+$lang['porg_v12_badge_redesign'] = '重新设计';
+$lang['porg_v12_badge_refresh'] = '刷新';
+$lang['porg_v12_enora_text1'] = '我的名字是 Enora，我很幸运在 UI/UX 设计学习期间加入了 Piwigo 团队。我一直在为 Piwigo 12 版本的管理界面设计新界面。我的主要优先事项是使其更加友好。';
+$lang['porg_v12_enora_text2'] = '我利用我的学习成果，并在团队的建议下进行了升级。我想通过尝试为每种类型的用户适应界面来刷新用户界面。';
+$lang['porg_v12_intro1'] = '更短、更强、更轻、更快';
+$lang['porg_v12_intro2'] = '版本 12 比版本 11 来得快得多。我们决定在更短的发布周期内进行更少的更改。更改较少，但变化仍然很大。在过去几年的持续努力中，我们仍然专注于管理面板。而且我们还没有完成！我们仍在为未来准备许多改进。重写的用户管理器、重新设计的插件管理器、添加的活动查看器是您立即会看到的一些明显变化。不太明显的是，我们实施了一种新机制，休息室，使您的大画廊加载更轻松。';
+$lang['porg_v12_intro3'] = '喝杯咖啡，享受阅读。';
+$lang['porg_v12_louis_text1'] = '我的名字是 Louis，我在 2020 年 10 月开始在 Piwigo 工作，学习计算机科学。我在测试期间开始修复版本 11 的问题。';
