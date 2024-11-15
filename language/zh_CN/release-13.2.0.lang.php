e23b472ca5a6b5216e6c095e0f7089d551db6d38
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:48:58 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.2.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1767'] = '[用户创建] 创建后弹出窗口没有响应。';
+$lang['porg_issue_1768'] = '更改按钮标签以应用根目录中的相册顺序';
+$lang['porg_issue_1770'] = '一些RAW文件被错误地认为是TIFF文件';
+$lang['porg_issue_1772'] = '[13.x，上传表单] 移动应用程序页面的推广总是显示英文';
+$lang['porg_issue_1774'] = '链接相册弹出窗口的高度总是变化';
+$lang['porg_issue_1775'] = '[相册管理器] 排序按钮没有工具提示';
+$lang['porg_issue_1780'] = '[13.x] 拥有许多相册的相册管理器运行缓慢';
+$lang['porg_issue_1781'] = '[相册管理器] 相册创建弹出窗口的用户体验不好';
+$lang['porg_issue_1782'] = '[历史页面] 无法过滤特定IP';
+$lang['porg_issue_1783'] = '[历史页面] 添加从编辑图片页面过滤图片的功能';
+$lang['porg_issue_1784'] = '[历史页面] 添加从用户页面过滤用户的功能';
+$lang['porg_issue_1787'] = '[链接相册弹出窗口] 在Chrome中滚动条始终可见';
+$lang['porg_issue_1792'] = 'PHP8 ‘get_magic_quotes_gpc’ 已被移除，导致用户输入转义失败';
+$lang['porg_issue_1793'] = '[相册管理器] 排序验证按钮标签不正确';
+$lang['porg_issue_1794'] = '[相册管理器] 按回车键会导致多次创建相册';
+$lang['porg_issue_1795'] = '[编辑照片] 标题未翻译';
+$lang['porg_issue_1796'] = '[编辑相册] 照片顺序和通知的标题会破坏页面';
