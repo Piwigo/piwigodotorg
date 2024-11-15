d904ca90d486c248792d55c05b15b591593b288c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:48:04 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.6.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1786'] = '从 Digikam 7.8.0 上传到 Piwigo 13.1.0 失败';
+$lang['porg_issue_1855'] = 'Smarty 演示中存在潜在安全问题';
+$lang['porg_issue_1858'] = '[用户管理器] 用户列表顺序';
+$lang['porg_issue_1859'] = '移动应用横幅的新设计';
+$lang['porg_issue_1861'] = '[暗模式] 添加照片表单的面包屑不是暗色';
+$lang['porg_issue_1862'] = '[组管理器] 暗模式下占位符颜色';
+$lang['porg_issue_1868'] = '第一个相册按钮未居中';
+$lang['porg_issue_1871'] = '[13.x] pwg.categories.getImages 返回错误的图片总数';
+$lang['porg_issue_1872'] = '[13.x] user_list_backend.php 中的 SQL 注入';
