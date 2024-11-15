3db05437786a83116ad78d2cf94ae6752e11f879
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:47:54 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.7.0.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1876'] = '[历史页面] 在 13.5.0 版本中发现 SQL 注入问题';
+$lang['porg_issue_1878'] = '[用户管理器] 用户状态未翻译';
+$lang['porg_issue_1879'] = '[暗模式] 更改暗模式中的图标样式';
+$lang['porg_issue_1880'] = '[用户管理器] 编辑电子邮件时阻止用户编辑器';
+$lang['porg_issue_1883'] = '标签编辑，取消按钮不起作用';
+$lang['porg_issue_1887'] = '[相册编辑器] 删除相册不起作用';
+$lang['porg_issue_1901'] = '[暗模式] 颜色更新';
+$lang['porg_issue_1903'] = '[用户管理器] 删除用户的确认弹窗中有未翻译的文本';
+$lang['porg_issue_1905'] = '[历史页面] 使图片可点击以直接进入其编辑页面';
+$lang['porg_issue_1907'] = '[da_DK] piwigo.org 上缺少新闻';
+$lang['porg_issue_1910'] = 'Piwigo 的“永久链接”功能中存在 SQL 注入';
+$lang['porg_issue_1911'] = 'Piwigo 的“个人资料”功能中存在 SQL 注入';
+$lang['porg_issue_1914'] = '[13.7.0] 相册排序时出现错误请求';
