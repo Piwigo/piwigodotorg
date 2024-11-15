5403d245620497c131400304e91bc29b3e210392
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:49:16 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-13.1.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1745'] = '更新页面缺少翻译';
+$lang['porg_issue_1746'] = '警告：无法修改标头信息 - 标头信息已在 /var/www/piwigo/action.php 第 228 行发送';
+$lang['porg_issue_1747'] = '[历史页面]：在详情中移除 {icon} 标签。';
+$lang['porg_issue_1750'] = 'best_rated/created：导致“Piwigo 遇到了不可恢复的错误”';
+$lang['porg_issue_1753'] = '语法错误，由include/ws_functions/pwg.php 中的尾随逗号引起的意外“）”';
+$lang['porg_issue_1755'] = '相关相册上的 Php8 警告';
+$lang['porg_issue_1756'] = '图片评论与 php8 有关的问题';
+$lang['porg_issue_1758'] = '[检查更新] 更新检查后没有指向更新页面的链接';
+$lang['porg_issue_1759'] = '[PHP 8.1] 配置页面上的致命错误';
