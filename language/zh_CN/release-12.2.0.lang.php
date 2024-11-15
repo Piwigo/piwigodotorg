c2e714eb036009edc54ddc99fe4c1b066b202a0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:47:30 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-12.2.0.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1557'] = '
+[12.1] 用户管理器在 MySQL 8 上持续加载';
+$lang['porg_issue_1567'] = '[12.x] 插件徽章不会随搜索输入更新';
+$lang['porg_issue_1569'] = '[12.x] 取消设置默认组导致无限旋转';
+$lang['porg_issue_1570'] = '相关相册和多语言名称';
+$lang['porg_issue_1573'] = '[12.x] 相册搜索编辑按钮未链接到正确的相册';
+$lang['porg_issue_1574'] = '[12.x] 解锁图库消息错误';
+$lang['porg_issue_1587'] = '使用 Piwigo NG 上传会生成太多活动';
+$lang['porg_issue_1590'] = '[维护页面] 高级功能未处理';
+$lang['porg_issue_1591'] = '[12.x] 与 openstreetmap/videojs 插件不兼容';
