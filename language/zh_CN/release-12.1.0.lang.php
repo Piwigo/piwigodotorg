62695b74daf9fe774360332f985e6741b1deda09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-15 05:47:33 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-12.1.0.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1546'] = 'Piwigo 更新 \"激动人心的新功能\" 链接为空白';
+$lang['porg_issue_1547'] = '[升级] 在 MySQL 8 上 desc groups 失败';
+$lang['porg_issue_1549'] = '仪表板显示有扩展更新，但实际上没有';
+$lang['porg_issue_1550'] = '插件页面损坏';
+$lang['porg_issue_1552'] = '[12.0] 上传后，相册 X 现在包含 0 张照片';
+$lang['porg_issue_1553'] = 'pwg.images.uploadCompleted 添加有关更新类别的详细信息';
+$lang['porg_issue_1554'] = '上传表单，意外选择的相册';
+$lang['porg_issue_1555'] = '照片编辑页面缺少翻译';
