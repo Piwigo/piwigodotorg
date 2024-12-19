d8faa5f4c347ff21b80885bd926e7b3e3c730eb7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-19 21:00:32 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-15.2.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2288'] = '[utilisateurs] lorsque l\'on demande le renouvellement d\'un mot de passe après l\'avoir changé, l\'interface utilisateur affiche l\'ancien email';
+$lang['porg_issue_2289'] = '[utilisateurs] bug de l\'affichage de la modale de l\'utilisateur invité';
+$lang['porg_issue_1152'] = 'Information "dernière visite" incohérente pour un nouvel utilisateur';
+$lang['porg_issue_1960'] = 'Obsolète en PHP8.1 :  nl2br() : Passer null au paramètre #1';
+$lang['porg_issue_2159'] = '\"Obsolète : addslashes()....\" lors de la synchronisation de la galerie';
+$lang['porg_issue_2234'] = '[mode sombre] uniformisation des couleurs';
+$lang['porg_issue_2269'] = 'coquille';
+$lang['porg_issue_2274'] = 'Déplacement d\'un album vers un album non ouvert, liste uniquement le ou les album(s) déplacé(s)';
+$lang['porg_issue_2275'] = '[Sélecteur d\'album] Les albums dont le nom ne comporte que des chiffres sont affichés de manière incorrecte.';
+$lang['porg_issue_2277'] = 'Lien manquant dans le popin "Quoi de neuf"';
+$lang['porg_issue_2279'] = 'Les dates de création ne sont pas dans le bon ordre pour les filtres de la galerie';
+$lang['porg_issue_2281'] = '[utilisateurs] lien d\'activation valable seulement 1 heure';
+$lang['porg_issue_2283'] = '[utilisateurs] affichage incorrect d\'un nom d\'utilisateur long et de ses initiales';
+$lang['porg_issue_2286'] = 'La modification des paramètres à partir des images ne peut être confirmée qu\'en cliquant sur Appliquer l\'action, et non sur Entrer.';
+$lang['porg_issue_2287'] = '[utilisateurs] mauvaise langue lors de la génération de l\'email du mot de passe';
+$lang['porg_issue_2290'] = '[plugins] les liens vers les pages des extensions mènent toujours à la langue anglaise';
+$lang['porg_issue_2292'] = '[activités] distinguer les méthodes d\'authentification';
+$lang['porg_issue_2294'] = 'Avertissement de session obsolète en PHP 8.4';
+$lang['porg_issue_2295'] = '[statistiques anonymes à piwigo.org] envoi des mises à jour du noyau';
