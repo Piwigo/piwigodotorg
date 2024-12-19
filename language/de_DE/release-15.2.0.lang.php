f0398cf6d3fdf9bf6451e33b19919ff23262ca85
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-19 21:00:35 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-15.2.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2294'] = 'Veraltete Session-Warnung in PHP 8.4';
+$lang['porg_issue_2295'] = '[anonyme Statistik an piwigo.org] Kern-Updates senden';
+$lang['porg_issue_2289'] = '[users] Fehler in der modalen Anzeige für Gastbenutzer';
+$lang['porg_issue_2290'] = '[plugins] die Links zu PEM sind immer in der Sprache EN';
+$lang['porg_issue_2292'] = '[Aktivitäten] Unterscheidung der Authentifizierungsmethoden';
+$lang['porg_issue_2286'] = 'Bearbeitungseinstellungen von Bildern können nur durch Klicken auf Aktion übernehmen bestätigt werden, nicht durch Eingabe.';
+$lang['porg_issue_2287'] = '[users] falsche Sprache beim Generieren der Passwort-E-Mail';
+$lang['porg_issue_2288'] = '[users] wenn man ein neues Passwort anfordert, nachdem man es geändert hat, zeigt die Benutzeroberfläche die alte E-Mail an';
+$lang['porg_issue_2277'] = 'Das fehlende Glied in der Kette der Neuerscheinungen';
+$lang['porg_issue_2279'] = 'Erstellungsdaten nicht in der richtigen Reihenfolge für Galeriefilter';
+$lang['porg_issue_2281'] = '[Benutzer] einen Aktivierungslink, der nur 1 Stunde lang gültig ist';
+$lang['porg_issue_2283'] = '[users] Falsche Anzeige eines großen Benutzernamens und der Initialen';
+$lang['porg_issue_2234'] = '[dunkler Modus] Farben vereinheitlichen';
+$lang['porg_issue_2269'] = 'Tippfehler';
+$lang['porg_issue_2274'] = 'Verschieben eines Albums in ein nicht geöffnetes Album, Auflistung nur des verschobenen Albums/der verschobenen Alben';
+$lang['porg_issue_2275'] = '[Albumauswahl] Alben, die nur Zahlen im Namen enthalten, werden nicht korrekt angezeigt';
+$lang['porg_issue_1152'] = 'unzusammenhängend \„letzter Besuch\“ für einen neuen Benutzer';
+$lang['porg_issue_1960'] = 'PHP8.1:  Veraltet: nl2br(): Übergabe von Null an Parameter #1 ';
+$lang['porg_issue_2159'] = '\„Veraltet: addslashes()....\“ beim Synchronisieren der Galerie';
