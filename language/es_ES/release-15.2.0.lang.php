3f40508c7ac7436187ecf598d1c51f994022d668
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-19 21:00:29 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-15.2.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1152'] = '\"Última visita\" incoherente para un nuevo usuario';
+$lang['porg_issue_1960'] = 'PHP8.1: Obsoleto: nl2br(): Pasar null al primer parámetro';
+$lang['porg_issue_2159'] = '\"Obsoleto: addslashes()....\" al sincronizar la galería';
+$lang['porg_issue_2234'] = '[modo oscuro] estandarizar colores';
+$lang['porg_issue_2269'] = 'error tipográfico';
+$lang['porg_issue_2274'] = 'Al mover un álbum a un álbum no abierto, enumerar solo los álbumes movidos';
+$lang['porg_issue_2275'] = '[selector de álbumes] los álbumes con solo números en el nombre se muestran incorrectamente';
+$lang['porg_issue_2277'] = 'Falta un enlace en la ventana emergente de novedades';
+$lang['porg_issue_2279'] = 'Las fechas de creación no están en el orden correcto para los filtros de la galería';
+$lang['porg_issue_2281'] = '[usuarios] El enlace de activación es válido sólo por una hora';
+$lang['porg_issue_2283'] = '[usuarios] visualización incorrecta de un nombre de usuario e iniciales grandes';
+$lang['porg_issue_2286'] = 'La edición de la configuración de las imágenes sólo se puede confirmar haciendo clic en la acción de Aplicar, no pulsando Intro';
+$lang['porg_issue_2287'] = '[usuarios] idioma incorrecto al generar el correo electrónico de contraseña';
+$lang['porg_issue_2288'] = '[usuarios] al solicitar una renovación de contraseña después de haberla cambiado, la interfaz de usuario muestra el correo electrónico anterior';
+$lang['porg_issue_2289'] = '[usuarios] error en la visualización modal del usuario invitado';
+$lang['porg_issue_2290'] = '[plugins] los enlaces a PEM siempre están en inglés';
+$lang['porg_issue_2292'] = '[actividades] distinguir métodos de autenticación';
+$lang['porg_issue_2294'] = 'Advertencia de sesión obsoleta en PHP 8.4';
+$lang['porg_issue_2295'] = '[estadísticas anónimas a piwigo.org] enviar actualizaciones del núcleo';
