246f22e36b33edf0af74727222de178adca3d574
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-19 21:00:38 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2268'] = 'Hinzufügen eines Popup-Fensters für Neuigkeiten in Piwigo';
+$lang['porg_issue_2270'] = '[15.0.0 search] Aufruf einer undefinierten Funktion array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] ungenaues Installationsdatum';
+$lang['porg_issue_2259'] = '[Die Reihenfolge der Systemaktivitäten ist manchmal zufällig.';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 in der Geschichte immer noch auf 15 Zeichen abgeschnitten';
+$lang['porg_issue_2267'] = '[15.0.0 batch manager unit] kaputtes Anzeigezeichen neben den Warnmeldungen';
+$lang['porg_issue_2254'] = 'svg upload setzt Abmessungen auf 0x0';
+$lang['porg_issue_2256'] = '[Suche] alte Links zur Suche in einem Album sind kaputt';
+$lang['porg_issue_2258'] = '[Fotos] Verwaiste Fotos werden nach dem Löschen nicht zurückgesetzt';
+$lang['porg_issue_2250'] = '[users] fehlende Tooltips auf dem Hauptbenutzersymbol';
+$lang['porg_issue_2251'] = '[users] fehlende Übersetzungsschlüssel für Benutzerpasswörter und Hauptbenutzer';
+$lang['porg_issue_2252'] = '[add_picture] Albumselektor kann während des Bild-Uploads geöffnet werden';
+$lang['porg_issue_2253'] = '[15.0.0] Das Löschen eines übergeordneten Albums erzeugt Fehler in der Stapelverwaltung';
+$lang['porg_issue_2197'] = 'SQL Injection in der Visualisierung der Benutzerliste über Admin > Verwalten';
+$lang['porg_issue_2237'] = '[15.0.0 batch manager unit] kann keine neuen Tags erstellen';
+$lang['porg_issue_2239'] = 'MP4 TS-Videobetrachtung geht der Speicher aus, wenn sie versucht, pdf-Seiten zu zählen';
+$lang['porg_issue_2247'] = 'Das Hinzufügen eines Unteralbums unter ein ungeöffnetes Album führt dazu, dass das Unteralbum ohne Aktualisierung nicht angezeigt wird';
