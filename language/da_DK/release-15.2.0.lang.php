d83f94fbd83cd81c3083af3cbe36cc0177411183
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:53:44 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1152'] = 'Uforståelig \"seneste besøg\" for en ny bruger';
+$lang['porg_issue_1960'] = 'PHP8.1-fejl: \"Deprecated: nl2br(): Passing null to parameter #1\"';
+$lang['porg_issue_2159'] = 'Fejl \"Deprecated: addslashes()....\" når galleri synkroniseres';
+$lang['porg_issue_2234'] = '[Mørk tilstand] farver standardiseret';
+$lang['porg_issue_2269'] = 'Slåfejl';
+$lang['porg_issue_2274'] = 'Flytning af album til ikke-åbnet album, medfører visning af kun flyttede albummer';
+$lang['porg_issue_2275'] = '[Albumudvælger] Albummer kun med numre i navnet, vises forkert';
+$lang['porg_issue_2277'] = 'Manglende link i hvad er nyt-popin';
+$lang['porg_issue_2279'] = 'Oprettelsesdatoer ikke i korrekt rækkefølge i gallerifiltre';
+$lang['porg_issue_2281'] = '[Brugere] Aktiveringslink kun gyldigt i 1 time';
+$lang['porg_issue_2283'] = '[Brugere] Ukorrekt visning af et langt brugernavn og initialer';
+$lang['porg_issue_2286'] = 'Redigering af indstillinger fra billeder kan kun bekræftes ved at klikke på udfør, men ikke med enter-tasten';
+$lang['porg_issue_2287'] = '[Brugere] Forkert sprog når der genereres adgangskodemail';
+$lang['porg_issue_2288'] = '[Brugere] Ved anmodning om fornyelse af adgangskode, efter at have ændret den, viser brugerfladen den gamle e-mail';
+$lang['porg_issue_2289'] = '[Brugere] Modal visningsfejl ved gæstebrugere';
+$lang['porg_issue_2290'] = '[Plugins] Links til PEM er altid på engelsk';
+$lang['porg_issue_2292'] = '[Aktiviteter] Gør forskel på autorisationsmetoder';
+$lang['porg_issue_2294'] = 'Udgået session-advarsel med PHP 8.4';
+$lang['porg_issue_2295'] = '[Anonym statisik til piwigo.org] Send core-opdateringer';
+$lang['porg_issue_2296'] = 'Fatal fejl: \"Declaration of PwgSession::open must be compatible with SessionHandlerInterface::open\"';
