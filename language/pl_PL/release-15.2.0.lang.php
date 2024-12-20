7a0fb01b23e3528c89f1c539ae61738c4195d19e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:53:51 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2275'] = '[wybór albumów] niepoprawnie wyświetlane albumy z tylko numerami w nazwie';
+$lang['porg_issue_2296'] = 'Błąd krytyczny: Deklaracja PwgSession::open musi być kompatybilna z SessionHandlerInterface::open';
+$lang['porg_issue_2295'] = '[anonimowe statystyki do piwigo.org] wysyłanie aktualizacji rdzenia';
+$lang['porg_issue_2294'] = 'Ostrzeżenie o przestarzałej sesji w PHP 8.4';
+$lang['porg_issue_2292'] = '[aktywności] rozróżniają metody auth';
+$lang['porg_issue_2290'] = '[wtyczki] linki do PEM są zawsze w języku EN';
+$lang['porg_issue_2289'] = '[użytkownicy] błąd modalnego wyświetlania użytkownika gość';
+$lang['porg_issue_2288'] = '[użytkownicy] podczas żądania odnowienia hasła, po jego zmianie interfejs użytkownika wyświetla stary e-mail';
+$lang['porg_issue_2287'] = '[użytkownicy] niewłaściwy język podczas generowania wiadomości z hasłem';
+$lang['porg_issue_2283'] = '[użytkownicy] nieprawidłowe wyświetlanie długiej nazwy użytkownika i inicjałów';
+$lang['porg_issue_2286'] = 'Edycja ustawień zdjęć może być potwierdzona tylko przez kliknięcie zastosuj, a nie klawiszem enter';
+$lang['porg_issue_2281'] = '[użytkownicy] link aktywacyjny ważny tylko przez 1 godzinę';
+$lang['porg_issue_2279'] = 'Daty tworzenia w niewłaściwej kolejności dla filtrów galerii';
+$lang['porg_issue_2277'] = 'Brakujące linki w wyskakującym okienku „co nowego”';
+$lang['porg_issue_1152'] = 'Niespójna \"ostatnia wizyta\" dla nowego użytkownika';
+$lang['porg_issue_1960'] = 'PHP8.1: Wycofane: nl2br(): Przekazywanie null do parametru #1';
+$lang['porg_issue_2159'] = '\"Wycofane: addslashes()....\" podczas synchronizacji galerii';
+$lang['porg_issue_2234'] = '[Tryb ciemny] ustandaryzowane kolory';
+$lang['porg_issue_2269'] = 'literówki';
+$lang['porg_issue_2274'] = 'Przy przenoszeniu albumu do nieotwieranego albumu, wymienione tylko przeniesione albumy';
