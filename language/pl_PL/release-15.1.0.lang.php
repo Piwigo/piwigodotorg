279047efa7fd99b553eb874e68437866f793ecda
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:53:41 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2268'] = 'Dodane wyskakujące okienko co nowego w Piwigo';
+$lang['porg_issue_2270'] = '[15.0.0 wyszukiwanie] Wywołanie do niezdefiniowanej funkcji array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] niedokładna data instalacji';
+$lang['porg_issue_2267'] = '[15.0.0 jednostka menedżera wsadowego] uszkodzony znak wyświetlany obok komunikatów ostrzegawczych';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 w historii nadal przycięty do 15 znaków';
+$lang['porg_issue_2259'] = '[aktywności] kolejność działań systemowych jest czasami losowa';
+$lang['porg_issue_2256'] = '[wyszukiwanie] stare odnośniki do wyszukiwania w albumie są uszkodzone';
+$lang['porg_issue_2258'] = '[zdjęcia] osierocone zdjęcia nie są resetowane po usunięciu';
+$lang['porg_issue_2254'] = 'przesyłanie svg ustawia wymiary na 0x0';
+$lang['porg_issue_2253'] = '[15.0.0] usunięcie albumu nadrzędnego generuje błędy zarządzania przetwarzania wsadowego';
+$lang['porg_issue_2197'] = 'SQL Injection w wizualizacji listy użytkowników poprzez Admin > Zarządzaj';
+$lang['porg_issue_2237'] = '[15.0.0 jednostka menedżera wsadowego] nie można tworzyć nowych tagów';
+$lang['porg_issue_2239'] = 'Widokowi wideo MP4 TS brakuje pamięci próbując policzyć strony pdf';
+$lang['porg_issue_2247'] = 'Dodanie podalbumu do nieotwartego albumu powoduje, że podalbum nie jest wyświetlany bez odświeżenia';
+$lang['porg_issue_2250'] = '[użytkownicy] brakująca podpowiedź na ikonie użytkownika głównego';
+$lang['porg_issue_2251'] = '[użytkownicy] brakujące klucze do tłumaczenia dla haseł użytkowników i głównego użytkownika';
+$lang['porg_issue_2252'] = '[dodawanie zdjęcia] selektor albumu można otworzyć podczas przesyłania obrazu';
