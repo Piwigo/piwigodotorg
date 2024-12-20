f4d84b5322a3799480ed68ccb82dfa791187e10b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 22:49:37 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/release-15.3.0.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2296'] = 'Error fatal: La declaració de PwgSession::open ha d\'ésser compatible amb SessionHandlerInterface::open';
