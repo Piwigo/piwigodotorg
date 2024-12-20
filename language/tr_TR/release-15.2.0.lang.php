8d6c41eba2f5b1a68f02ce67c23c091e968e13ce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:04:47 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2283'] = '[kullanıcılar] büyük kullanıcı adı ve baş harflerin yanlış görüntülenmesi';
+$lang['porg_issue_2289'] = '[kullanıcılar] konuk kullanıcı modal görüntüleme hatası';
+$lang['porg_issue_2290'] = '[eklentiler] PEM\'e bağlantılar her zaman EN dilindedir';
+$lang['porg_issue_2292'] = '[etkinlikler] kimlik doğrulama yöntemlerini ayırt eder';
+$lang['porg_issue_2294'] = 'PHP 8.4\'te kullanım dışı bırakılmış oturum uyarısı';
+$lang['porg_issue_2295'] = '[piwigo.org\'a anonim istatistikler] çekirdek güncellemeleri gönder';
+$lang['porg_issue_2296'] = 'Ciddi hata: PwgSession::open bildirimi SessionHandlerInterface::open ile uyumlu olmalıdır';
+$lang['porg_issue_2288'] = '[Kullanıcılar] şifreyi değiştirdikten sonra yenileme isteğinde bulunulduğunda kullanıcı arayüzü eski e-postayı gösteriyor';
+$lang['porg_issue_2287'] = '[kullanıcılar] şifre e-postası oluştururken yanlış dil';
+$lang['porg_issue_2286'] = 'Görüntülerden düzenleme ayarları yalnızca eylem uygulaya tıklanarak onaylanabilir, girilemez';
+$lang['porg_issue_1960'] = 'PHP8.1: Kullanım dışı bırakıldı: nl2br(): Parametre #1\'e null geçiriliyor';
+$lang['porg_issue_2159'] = 'Galeriyi senkronize ederken \"Kullanım dışı: addslashes()....\"';
+$lang['porg_issue_2279'] = 'Galeri filtreleri için oluşturma tarihleri ​​doğru sırada değil';
+$lang['porg_issue_2281'] = '[Kullanıcılar] yalnızca 1 saat geçerli bir aktivasyon bağlantısı';
+$lang['porg_issue_1152'] = 'yeni bir kullanıcı için tutarsız \"son ziyaret\"';
+$lang['porg_issue_2234'] = '[koyu mod] renkleri standartlaştır';
+$lang['porg_issue_2269'] = 'yazım hatası';
+$lang['porg_issue_2274'] = 'Bir albümü açılmamış bir albüme taşıyın, yalnızca taşınan albümü/albümleri listeleyin';
+$lang['porg_issue_2275'] = '[albüm seçici] adında yalnızca numara bulunan albümler yanlış görüntüleniyor';
+$lang['porg_issue_2277'] = '"Neler yeni" popup ekranında eksik bağlantı';
