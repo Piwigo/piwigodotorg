4192958560aed8413fbb8723600c1eff3d9bcbc4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 15:51:41 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2197'] = '通过管理 > 管理中的用户列表可视化中的 SQL 注入';
+$lang['porg_issue_2237'] = '[15.0.0 批量管理单元] 无法创建新标签';
+$lang['porg_issue_2239'] = 'MP4 TS 视频查看时尝试计算 PDF 页数导致内存不足';
+$lang['porg_issue_2247'] = '在未打开的相册下添加子相册导致不刷新时不显示子相册';
+$lang['porg_issue_2250'] = '[用户] 主用户图标缺少工具提示';
+$lang['porg_issue_2251'] = '[用户] 用户密码和主用户缺少翻译键';
+$lang['porg_issue_2252'] = '[添加图片] 在上传图片期间可以打开相册选择器';
+$lang['porg_issue_2253'] = '[15.0.0] 删除父相册会生成批量管理错误';
+$lang['porg_issue_2254'] = 'SVG 上传设置的尺寸为 0x0';
+$lang['porg_issue_2256'] = '[搜索] 旧链接在相册中搜索已损坏';
+$lang['porg_issue_2258'] = '[照片] 删除后孤立照片不会重置';
+$lang['porg_issue_2259'] = '[活动] 系统活动的顺序有时是随机的';
+$lang['porg_issue_2265'] = '[15.0.0] 历史记录中的 IPv6 仍然截断为 15 个字符';
+$lang['porg_issue_2267'] = '[15.0.0 批量管理单元] 警告消息旁边的显示字符损坏';
+$lang['porg_issue_2268'] = '添加 Piwigo 新功能弹出窗口';
+$lang['porg_issue_2270'] = '[15.0.0 搜索] 调用未定义函数 array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] 安装日期不准确';
