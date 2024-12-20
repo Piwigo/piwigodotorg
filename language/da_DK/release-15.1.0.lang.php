b91fd3e0e7da59bd1d83c2f2ff0df15ef3955fa3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:53:38 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2197'] = 'SQL-indsprøjtning i visualisering af brugerliste i Admin -> Håndter';
+$lang['porg_issue_2237'] = '[15.0.0 batchhåndtering] Kan ikke oprette nye tags';
+$lang['porg_issue_2239'] = 'Visning af MP4 TS-video løber tør for hukommelse ved forsøg på at optælle antal PDF-sider';
+$lang['porg_issue_2247'] = 'Tilføjelse af underalbum under uåbnet album medfører at underalbummet først kan vises efter en genopfriskning';
+$lang['porg_issue_2250'] = '[Brugere] Manglende tooltips ved primær bruger-ikon';
+$lang['porg_issue_2251'] = '[Brugere] Manglende oversættelsesnøgler for brugers adgangskode og primær bruger';
+$lang['porg_issue_2252'] = '[add_picture] Albumudvælger kan åbnes under billedupload';
+$lang['porg_issue_2253'] = '[15.0.0] Sletning af forælderalbum medføre fejl i batchhåndtering';
+$lang['porg_issue_2254'] = 'Svg-upload opsætter dimenionser til 0x0';
+$lang['porg_issue_2256'] = '[Søgning] Gamle links til søgning i et album, fungerer ikke';
+$lang['porg_issue_2258'] = '[Fotografier] Forældreløse fotografier nulstilles ikke efter sletning';
+$lang['porg_issue_2259'] = '[Aktivitet] Nogle gange er rækkefølgen af akvititeter tilfældig';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 i historisk bliver stadig afkortet til 15 tegn';
+$lang['porg_issue_2267'] = '[15.0.0 batchhåndtering] Defekt visning af tegn ved siden af advarselsmedddelelser';
+$lang['porg_issue_2268'] = 'Tilføj en popup med hvad der er nyt i Piwigo';
+$lang['porg_issue_2270'] = '[15.0.0 søgning] Kald af udefineret funktion array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] Upræcis installeringsdato';
