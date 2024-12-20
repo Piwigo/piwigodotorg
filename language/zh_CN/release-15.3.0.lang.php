ccf90ccdf6bf586231a368936ed81691d640c007
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:09:36 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.3.0.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2296'] = '[15.2.0] 致命错误：PwgSession::open 的声明必须与 SessionHandlerInterface::open 兼容';
