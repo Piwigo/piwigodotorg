2e2728eb37f2735e156ba867fe03e09e4cc6ec63
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 15:51:38 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2288'] = '[utenti] quando si richiede il rinnovo della password dopo averla modificata, l\'interfaccia utente visualizza la vecchia email';
+$lang['porg_issue_2289'] = '[utenti] bug di visualizzazione modale dell\'utente ospite';
+$lang['porg_issue_2290'] = '[plugin] i collegamenti a PEM sono sempre in lingua EN';
+$lang['porg_issue_2292'] = '[attività] distinguono i metodi di autenticazione';
+$lang['porg_issue_2295'] = '[statistiche anonime su piwigo.org] invia aggiornamenti principali';
+$lang['porg_issue_2296'] = 'Errore irreversibile: la dichiarazione di PwgSession::open deve essere compatibile con SessionHandlerInterface::open';
+$lang['porg_issue_2287'] = '[utenti] lingua errata durante la generazione dell\'e-mail con password';
+$lang['porg_issue_2286'] = 'La modifica delle impostazioni dalle immagini può essere confermata solo facendo clic su Applica azione, non su Invio';
+$lang['porg_issue_2279'] = 'Date di creazione per i filtri della galleria non sono nell\'ordine corretto ';
+$lang['porg_issue_2281'] = '[utenti] un link di attivazione valido solo per 1 ora';
+$lang['porg_issue_2283'] = '[utenti] visualizzazione errata di un nome utente di grandi dimensioni e delle iniziali ';
+$lang['porg_issue_2294'] = 'Avviso di sessione deprecato su PHP 8.4';
+$lang['porg_issue_2277'] = 'Collegamento mancante nel popup delle notizie';
+$lang['porg_issue_1960'] = 'PHP8.1:  Deprecato: nl2br(): Passing null to parameter #1 ';
+$lang['porg_issue_2159'] = '\"Deprecato: addslashes()....\" durante la sincronizzazione della galleria';
+$lang['porg_issue_2234'] = '[modalità scura] normalizza i colori';
+$lang['porg_issue_2269'] = 'errore di battitura';
+$lang['porg_issue_2274'] = 'Quando si sposta un album in un album non aperto, elenca solo gli album spostati';
+$lang['porg_issue_2275'] = '[selettore album] gli album con solo numeri nel nome vengono visualizzati in modo errato';
+$lang['porg_issue_1152'] = '\"ultima visita\" incoerente per un nuovo utente';
