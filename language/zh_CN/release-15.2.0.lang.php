8e8b062c2a60169f470d863fa92f611c76d21701
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 15:51:32 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1152'] = '新用户的“最后访问”时间不一致';
+$lang['porg_issue_1960'] = 'PHP8.1：已弃用：nl2br()：传递 null 给参数 #1';
+$lang['porg_issue_2159'] = '同步图库时“已弃用：addslashes()…”';
+$lang['porg_issue_2234'] = '[暗模式] 标准化颜色';
+$lang['porg_issue_2269'] = '拼写错误';
+$lang['porg_issue_2274'] = '将相册移动到未打开的相册，仅列出已移动的相册';
+$lang['porg_issue_2275'] = '[相册选择器] 名称中仅包含数字的相册显示不正确';
+$lang['porg_issue_2277'] = '“新功能”弹出窗口缺少链接';
+$lang['porg_issue_2279'] = '图库过滤器的创建日期顺序不正确';
+$lang['porg_issue_2281'] = '[用户] 激活链接仅在1小时内有效';
+$lang['porg_issue_2283'] = '[用户] 用户名和首字母显示不正确';
+$lang['porg_issue_2286'] = '编辑图片设置只能通过点击应用操作确认，不能按回车键';
+$lang['porg_issue_2287'] = '[用户] 生成密码电子邮件时语言错误';
+$lang['porg_issue_2288'] = '[用户] 更改密码后请求密码重置时，界面显示旧电子邮件';
+$lang['porg_issue_2289'] = '[用户] 访客用户模式显示错误';
+$lang['porg_issue_2290'] = '[插件] 链接到 PEM 的始终是英文语言';
+$lang['porg_issue_2292'] = '[活动] 区分身份验证方法';
+$lang['porg_issue_2294'] = 'PHP 8.4 上的已弃用会话警告';
+$lang['porg_issue_2295'] = '[向 piwigo.org 发送匿名统计数据] 发送核心更新';
+$lang['porg_issue_2296'] = '致命错误：PwgSession::open 的声明必须与 SessionHandlerInterface::open 兼容';
