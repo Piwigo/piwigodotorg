5ecd05d8164e24f900dfacba26610099575c3d68
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-23 07:19:42 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.3.0.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2296'] = '[15.2.0] Errore fatale: Dichiarazione di PwgSession::open deve essere compatibile con SessionHandlerInterface::open';
