fff4d2c6e24922e45d8ba3b9fea6cdfe0d87f06c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-28 12:21:15 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2197'] = 'Inyección SQL en la visualización de la lista de usuarios a través de Admin > Gestionar';
+$lang['porg_issue_2237'] = '[Unidad de administrador de lotes 15.0.0] no se pueden crear nuevas etiquetas';
+$lang['porg_issue_2239'] = 'La vista de vídeo MP4 TS se queda sin memoria al intentar contar páginas PDF';
+$lang['porg_issue_2247'] = 'Añadir un subálbum en un álbum sin abrir hace que no se muestre el subálbum si no se actualiza';
+$lang['porg_issue_2250'] = '[usuarios] Faltan ayudas emergentes en el icono del usuario principal';
+$lang['porg_issue_2251'] = '[usuarios] Faltan claves de traducción para las contraseñas de usuario y para el usuario principal';
+$lang['porg_issue_2252'] = '[add_picture] El selector de álbum se puede abrir durante la carga de imágenes';
+$lang['porg_issue_2253'] = '[15.0.0] La supresión del álbum padre genera errores de gestión de lotes';
+$lang['porg_issue_2254'] = 'La carga de imágenes svg establece las dimensiones en 0x0';
+$lang['porg_issue_2256'] = '[búsqueda] Los enlaces antiguos para buscar en un álbum están rotos';
+$lang['porg_issue_2258'] = '[fotos] Las fotos huérfanas no se restablecen después de suprimirse';
+$lang['porg_issue_2259'] = '[actividad] A veces el orden de actividades del sistema es aleatorio';
+$lang['porg_issue_2265'] = '[15.0.0] Las direcciones IPv6 en el historial aún se truncan a 15 caracteres';
+$lang['porg_issue_2267'] = '[15.0.0 unidad de administración de lotes] carácter de visualización incorrecto junto a los mensajes de advertencia';
+$lang['porg_issue_2268'] = 'Añadir una ventana emergente de Novedades en Piwigo';
+$lang['porg_issue_2270'] = '[15.0.0 búsqueda] Llamada a la función indefinida array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] fecha de instalación inexacta';
