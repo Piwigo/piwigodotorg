4cf05e5f9c529d277a135b2a528e5ffadef2c919
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-29 18:26:15 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2271'] = '[15.0.0] yanlış kurulum tarihi';
+$lang['porg_issue_2270'] = '[15.0.0 arama] Tanımsız array_key_first() fonksiyonuna çağrı';
+$lang['porg_issue_2268'] = 'Piwigo\'da yenilikleri içeren bir açılır pencere ekleyin';
+$lang['porg_issue_2267'] = '[15.0.0 toplu yönetici birimi] uyarı mesajlarının yanında bozuk görüntü karakteri';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 geçmişte hala 15 karakterde kesiliyor';
+$lang['porg_issue_2259'] = '[aktivite] sistem aktivitelerinin sırası bazen rastgeledir';
+$lang['porg_issue_2258'] = '[fotoğraflar] yetim fotoğraflar silindikten sonra sıfırlanmıyor';
+$lang['porg_issue_2256'] = '[arama] bir albümde arama yapmak için kullanılan eski bağlantılar bozuk';
+$lang['porg_issue_2254'] = 'svg yüklemesi boyutları 0x0 olarak ayarlar';
+$lang['porg_issue_2253'] = '[15.0.0] ana albümün silinmesi toplu yönetim hatalarına neden oluyor';
+$lang['porg_issue_2252'] = '[add_picture] albüm seçicisi resim yükleme sırasında açılabilir';
+$lang['porg_issue_2251'] = '[Kullanıcılar] kullanıcı parolaları ve ana kullanıcı için çeviri anahtarları eksik';
+$lang['porg_issue_2250'] = '[Kullanıcılar] ana kullanıcı simgesinde araç ipuçları eksik';
+$lang['porg_issue_2247'] = 'Açılmamış albüme alt albüm eklemek, yenileme yapılmadan alt albümün görüntülenmemesine neden oluyor';
+$lang['porg_issue_2239'] = 'MP4 TS video görünümü pdf sayfalarını saymaya çalışırken hafızayı tüketiyor';
+$lang['porg_issue_2237'] = '[15.0.0 toplu iş yöneticisi birimi] yeni etiketler oluşturulamıyor';
+$lang['porg_issue_2197'] = 'Admin > Manage aracılığıyla Kullanıcı Listesi görselleştirmesinde SQL Enjeksiyonu';
