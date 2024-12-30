a995b620dc818e7c4f8416b49f28c0ba48ee3b6e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-30 12:01:02 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2267'] = '[15.0.0 unità gestione lotti] carattere danneggiato mostrato accanto ai messaggi di avviso';
+$lang['porg_issue_2268'] = 'Aggiungi un popup "Cosa c\'è di nuovo in Piwigo"';
+$lang['porg_issue_2270'] = '[ricerca 15.0.0] Call to undefined function array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] data di installazione non corretta';
+$lang['porg_issue_2259'] = '[attività] l\'ordine delle attività di sistema è talvolta casuale';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 nella cronologia è ancora troncato a 15 caratteri';
+$lang['porg_issue_2250'] = '[utenti] suggerimenti mancanti sull\'icona dell\'utente principale';
+$lang['porg_issue_2251'] = '[utenti] chiavi di traduzione mancanti per le password utente e l\'utente principale';
+$lang['porg_issue_2252'] = '[aggiunta_immagine] il selettore dell\'album può essere aperto durante il caricamento dell\'immagine';
+$lang['porg_issue_2253'] = '[15.0.0] L\'eliminazione dell\'album padre genera errori di gestione dei lotti';
+$lang['porg_issue_2254'] = 'Il caricamento in formato svg imposta le dimensioni su 0x0';
+$lang['porg_issue_2256'] = '[cerca] i vecchi collegamenti per la ricerca in un album sono interrotti';
+$lang['porg_issue_2258'] = '[foto] le foto orfane non si reimpostano dopo l\'eliminazione';
+$lang['porg_issue_2197'] = 'SQL injection nella vista User List via Admin -> Gestione';
+$lang['porg_issue_2237'] = '[15.0.0 batch manager unit] impossibile creare nuove etichette';
+$lang['porg_issue_2239'] = 'La visualizzazione video MP4 TS esaurisce la memoria nel tentativo di contare le pagine PDF';
+$lang['porg_issue_2247'] = 'L\'aggiunta di un sottoalbum in un album non aperto comporta la mancata visualizzazione del sottoalbum, senza l\'aggiornamento';
