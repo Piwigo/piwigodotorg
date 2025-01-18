8c848b626f7dcb46f05131f2b78ae73c9ea22645
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-01-18 06:34:30 +0100

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2270'] = '[15.0.0 search] aanroepen van een ongedefinieerde function array_key_first()';
+$lang['porg_issue_2267'] = '[15.0.0 batch manager unit] defect weergave karakter bij waarschuwingsbericht';
+$lang['porg_issue_2259'] = '[activity] volgorde van systeem activiteiten is soms willekeurig';
+$lang['porg_issue_2258'] = '[photos] wees foto\'s zijn niet verdwenen na verwijderen';
+$lang['porg_issue_2256'] = '[search] oude links om op te zoeken in een album zijn kapot';
+$lang['porg_issue_2252'] = '[add_picture] album selector kan worden geopend gedurende upload van een plaatje';
+$lang['porg_issue_2253'] = '[15.0.0] verwijderen van bovengelegen album geeft batch management fouten';
+$lang['porg_issue_2250'] = '[users] missende tooltips op het hoofduser icoon';
+$lang['porg_issue_2251'] = '[users] missende vertaal sleutels voor gebruiker wachtwoorden en hoofd gebruiker';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 in historie wordt nog steeds afgekort op 15 karakters. ';
+$lang['porg_issue_2268'] = 'Voeg een wat is er nieuw in een Piwigo popup toe. ';
+$lang['porg_issue_2271'] = '[15.0.0] ongeldige installatie datum';
+$lang['porg_issue_2254'] = 'svg upload zet afmetingen naar 0x0';
+$lang['porg_issue_2247'] = 'Toevoegen van een subalbum onder een niet geopend album, toont het subalbum niet zonder refresh.';
+$lang['porg_issue_2239'] = 'MP4 TS video loopt uit geheugen bij het tellen van PDF pagina\'s';
+$lang['porg_issue_2237'] = '[15.0.0 batch manager unit] kan geen nieuwe tags maken';
+$lang['porg_issue_2197'] = 'SQL injectie in gebruikerslijst zichtbaar via administratie > beheer';
