4f11c1e0f8b013ae5a730e6b9e6eef6791046f53
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-01-21 16:25:41 +0100

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2286'] = 'Instellingen bewerken van afbeeldingen kan alleen worden bevestigd door op actie toepassen te klikken, niet op enter';
+$lang['porg_issue_2287'] = '[Users] verkeerde taal bij het genereren van wachtwoord e-mail';
+$lang['porg_issue_2288'] = '[users] bij het aanvragen van een wachtwoordvernieuwing na het wijzigen ervan toont de ui de oude e-mail';
+$lang['porg_issue_2289'] = '[users] fout in de weergave van de gastgebruiker';
+$lang['porg_issue_2290'] = '[plugins]de links naar PEM zijn altijd in de EN-taal';
+$lang['porg_issue_2292'] = '[activities]onderscheid auth-methoden';
+$lang['porg_issue_2294'] = 'Waarschuwing voor verouderde sessie op PHP 8.4';
+$lang['porg_issue_2295'] = '[anonymous statistics to piwigo.org] stuur kernupdates';
+$lang['porg_issue_2296'] = 'Fatale fout: Declaratie van PwgSession::open moet compatibel zijn met SessionHandlerInterface::open';
+$lang['porg_issue_1152'] = 'onsamenhangend \"laatste bezoek\" voor een nieuwe gebruiker';
+$lang['porg_issue_1960'] = 'PHP8.1: Verouderd: nl2br(): Null doorgeven aan parameter #1';
+$lang['porg_issue_2159'] = '\"Verouderd: addslashes()....\" bij synchronisatie van galerij';
+$lang['porg_issue_2234'] = '[donkere modus] kleuren standaardiseren';
+$lang['porg_issue_2269'] = 'typfout';
+$lang['porg_issue_2274'] = 'Een album verplaatsen naar een niet-geopend album, alleen de verplaatste albums weergeven';
+$lang['porg_issue_2275'] = '[albumselector] albums met alleen cijfers in de naam worden onjuist weergegeven';
+$lang['porg_issue_2277'] = 'Ontbrekende link op whats new popin';
+$lang['porg_issue_2279'] = 'Aanmaakdatums niet in de juiste volgorde voor galerijfilters';
+$lang['porg_issue_2281'] = '[users] een activeringslink die slechts 1 uur geldig is';
+$lang['porg_issue_2283'] = '[users] onjuiste weergave van een grote gebruikersnaam en initialen';
