20bdf17e71feda38b0f84a9e600adfbeca228caf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-18 15:48:35 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2271'] = '[15.0.0] Date d\'installation inexacte';
+$lang['porg_issue_2270'] = '[15.0.0 search] Appel à une fonction non définie array_key_first()';
+$lang['porg_issue_2268'] = 'Ajout d\'un popup "Quoi de neuf dans Piwigo ?"';
+$lang['porg_issue_2267'] = '[15.0.0 unité de gestion par lot] caractère d\'affichage brisé à côté des messages d\'avertissement';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 dans l\'historique toujours tronqué à 15 caractères';
+$lang['porg_issue_2259'] = '[activité] l\'ordre des activités du système est parfois aléatoire';
+$lang['porg_issue_2258'] = '[photos] les photos orphelines ne sont pas réinitialisées après leur suppression';
+$lang['porg_issue_2256'] = '[recherche] les anciens liens de recherche dans un album sont cassés';
+$lang['porg_issue_2254'] = 'Le téléchargement svg fixe les dimensions à 0x0';
+$lang['porg_issue_2253'] = '[15.0.0] La suppression de l\'album parent génère des erreurs de gestion par lot';
+$lang['porg_issue_2252'] = '[add_picture] le sélecteur d\'album peut être ouvert lors du téléversement d\'une image';
+$lang['porg_issue_2251'] = '[utilisateurs] chaînes de langue manquantes pour les mots de passe des utilisateurs et l\'utilisateur principal';
+$lang['porg_issue_2250'] = '[Utilisateurs] Infobulles manquantes sur l\'icône de l\'utilisateur principal';
+$lang['porg_issue_2247'] = 'L\'ajout d\'un sous-album dans un album non ouvert a pour conséquence de ne pas afficher le sous-album sans rafraîchissement.';
+$lang['porg_issue_2239'] = 'La visualisation de vidéos MP4 TS manque de mémoire lorsqu\'elle tente de compter les pages d\'un document PDF.';
+$lang['porg_issue_2237'] = '[15.0.0 unité de gestion par lots] ne peut pas créer de nouveaux mots-clefs';
+$lang['porg_issue_2197'] = 'Injection SQL dans la visualisation de la liste des utilisateurs via Admin > Manage';
