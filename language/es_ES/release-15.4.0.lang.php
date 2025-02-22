d6526554c4302597a06d82922921cf56a863e1cb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:16:22 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2234'] = '[modo oscuro] estandarizar colores';
+$lang['porg_issue_2235'] = 'El texto en chino no se muestra en una línea';
+$lang['porg_issue_2280'] = 'Falta una variable para una notificación de usuario';
+$lang['porg_issue_2300'] = 'Pide a los robots que eviten seguir enlaces de varios álbumes';
+$lang['porg_issue_2303'] = '[usuarios] La contraseña se sigue enviando en texto sin cifrar por correo electrónico después del registro';
+$lang['porg_issue_2308'] = '[acceso denegado] después de iniciar sesión, va a parar a la página de inicio en lugar de a la dirección solicitada';
+$lang['porg_issue_2309'] = '[búsqueda] El filtro de calificación sigue apareciendo';
+$lang['porg_issue_2310'] = '[administrador de lotes global] Presionar Intro para confirmar una acción impide que las áreas de texto regresen a la línea';
+$lang['porg_issue_2311'] = 'Carga extrema al cargar fotos y muchos visitantes';
+$lang['porg_issue_2314'] = '[álbumes] Siempre se muestra un mensaje de error al añadir un álbum';
+$lang['porg_issue_2315'] = '[usuarios] La inicialización de la contraseña activa la visualización del reinicio de la interfaz de usuario';
+$lang['porg_issue_2316'] = '[selector de álbumes] Aparecen errores cuando se pulsa Intro y el selector de álbumes está abierto';
+$lang['porg_issue_2317'] = '[usuarios] Visualización incorrecta de un nombre de usuario largo';
+$lang['porg_issue_2319'] = 'Orden incorrecto de las sumas MD5 generadas';
+$lang['porg_issue_2322'] = 'Advertencia num_rows cuando show_queries = true';
+$lang['porg_issue_2323'] = 'En el correo electrónico de configuración o reinicio, la hora de validación del enlace no está en el idioma correcto';
+$lang['porg_issue_2324'] = 'Página rota (problema de JavaScript) en el cargador de fotos y el administrador de álbumes en catalán';
+$lang['porg_issue_2325'] = 'Color de las letras incorrecto al pasar el ratón sobre el botón para crear el primer álbum';
+$lang['porg_issue_698'] = 'show_metadata=true contra la lógica noindex,nofollow en picture.php';
