896fcdcc5b56ace0dc4fac5b78a078e1d2d0d4cc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:16:25 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_698'] = 'Contradiction dans picture.php entre show_metadata=true et noindex,nofollow ';
+$lang['porg_issue_2325'] = 'mauvaise couleur de police pour le survol du bouton de création du premier album';
+$lang['porg_issue_2324'] = 'page cassée (problème de javascript) sur le téléverseur de photos et le gestionnaire d\'albums en Catalan';
+$lang['porg_issue_2323'] = 'Dans l\'e-mail de configuration ou de réinitialisation, l\'heure de validation du lien n\'est pas dans la bonne langue.';
+$lang['porg_issue_2322'] = 'avertissement num_rows quand show_queries = true';
+$lang['porg_issue_2319'] = 'Mauvais type de somme md5 générée';
+$lang['porg_issue_2317'] = '[utilisateurs] affichage incorrect d\'un grand nom d\'utilisateur';
+$lang['porg_issue_2315'] = '[utilisateurs] L\'initialisation du mot de passe déclenche l\'affichage de l\'interface utilisateur de réinitialisation.';
+$lang['porg_issue_2314'] = '[albums] un message d\'erreur lors de l\'ajout d\'un album est toujours affiché';
+$lang['porg_issue_2311'] = 'Charge extrême lors du téléversement de photos et en présence de nombreux visiteurs';
+$lang['porg_issue_2310'] = '[batch manager global] le fait d\'appuyer sur entrée pour confirmer une action empêche les zones de texte de revenir à la ligne';
+$lang['porg_issue_2308'] = '[accès refusé] après la connexion, la page d\'accueil est affichée au lieu de l\'URL demandée à l\'origine.';
+$lang['porg_issue_2303'] = '[utilisateurs] le mot de passe est toujours envoyé en clair par email après l\'enregistrement';
+$lang['porg_issue_2300'] = 'demander aux robots d\'éviter de suivre les liens vers plusieurs albums';
+$lang['porg_issue_2280'] = 'variable manquante pour la notification à l\'utilisateur';
+$lang['porg_issue_2235'] = 'Le texte en chinois ne s\'affiche pas sur une seule ligne';
+$lang['porg_issue_2234'] = '[mode sombre] uniformiser les couleurs';
+$lang['porg_issue_2309'] = '[recherche] Le filtre de notation réapparaît sans cesse';
+$lang['porg_issue_2316'] = '[sélecteur d\'album] Des bugs apparaissent quand on appuie sur la touche Entrée et que le sélecteur d\'album est ouvert.';
