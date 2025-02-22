bb76fadcc7bd32975685b2d498dbcfc7f8da9e3f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:16:19 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2315'] = '[users] l\'inizializzazione della password attiva la visualizzazione dell\'interfaccia utente di ripristino';
+$lang['porg_issue_2316'] = '[album selector] compaiono dei bug quando premi invio e il selettore album è aperto';
+$lang['porg_issue_2317'] = '[users] errata visualizzazione di un nome utente con molti caratteri';
+$lang['porg_issue_2319'] = 'ordinamento errato del generato md5sum';
+$lang['porg_issue_2322'] = 'avviso di num_rows quando show_queries = true';
+$lang['porg_issue_2323'] = 'Nell\'e-mail di impostazione o ripristino, il tempo di convalida del collegamento non è mostrato nella lingua corretta';
+$lang['porg_issue_2324'] = 'pagina non funzionante (problema javascript) su photo uploader e album manager in lingua Català';
+$lang['porg_issue_2325'] = 'colore del carattere errato al passaggio del mouse sul pulsante Crea il primo album';
+$lang['porg_issue_698'] = 'show_metadata=true vs. logica noindex,nofollow in picture.php';
+$lang['porg_issue_2234'] = '[modalità scura] colori standardizzati';
+$lang['porg_issue_2235'] = 'Il testo in cinese non viene visualizzato su una riga';
+$lang['porg_issue_2280'] = 'variabile mancante per la notifica utente';
+$lang['porg_issue_2300'] = 'chiedi ai robot di evitare di seguire più link degli album';
+$lang['porg_issue_2303'] = '[utenti] la password viene ancora inviata in chiaro tramite e-mail dopo la registrazione';
+$lang['porg_issue_2308'] = '[accesso negato] dopo l\'accesso, si viene indirizzati alla home page anziché all\'URL richiesto in origine';
+$lang['porg_issue_2309'] = '[ricerca] Il filtro di valutazione continua a riapparire';
+$lang['porg_issue_2310'] = '[batch manager global] la pressione di invio per confermare un\'azione impedisce alle aree di testo di tornare alla riga';
+$lang['porg_issue_2311'] = 'Carico estremo mentre vengono caricate fotografie e ci sono molti visitatori';
+$lang['porg_issue_2314'] = '[album] viene sempre visualizzato un messaggio di errore quando si aggiunge un album';
