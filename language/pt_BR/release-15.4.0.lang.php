9838fa5602b03c214173a7224c7defe98a3a6276
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:16:17 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2309'] = '[pesquisar] O filtro de classificação continua reaparecendo';
+$lang['porg_issue_2310'] = '[batch manager global] pressionar enter para confirmar uma ação impede que textareas retornem à linha';
+$lang['porg_issue_2311'] = 'Carga extrema ao carregar fotos e muitos visitantes';
+$lang['porg_issue_2314'] = '[álbuns] mensagem de erro ao adicionar um álbum é sempre exibida';
+$lang['porg_issue_2315'] = '[usuários] a inicialização da senha aciona a exibição da IU de redefinição';
+$lang['porg_issue_2316'] = '[seletor de álbum] bugs aparecem quando você pressiona enter e o seletor de álbum está aberto';
+$lang['porg_issue_2317'] = '[usuários] exibição incorreta de um nome de usuário grande';
+$lang['porg_issue_2319'] = 'tipo incorreto de md5sum gerado';
+$lang['porg_issue_2322'] = 'aviso num_rows quando show_queries = true';
+$lang['porg_issue_2323'] = 'Nenhum e-mail definido ou redefinido, o tempo de validação do link não está no idioma correto';
+$lang['porg_issue_2324'] = 'página quebrada (problema de javascript) no carregador de fotos e gerenciador de álbuns em Catalão';
+$lang['porg_issue_2325'] = 'cor de fonte errada para passar o mouse no botão criar primeiro álbum';
+$lang['porg_issue_698'] = 'show_metadata=true vs. lógica noindex,nofollow em picture.php';
+$lang['porg_issue_2234'] = '[modo escuro] padronizar cores';
+$lang['porg_issue_2235'] = 'O texto chinês não é exibido em uma linha';
+$lang['porg_issue_2280'] = 'variável ausente para notificação do usuário';
+$lang['porg_issue_2300'] = 'peça aos robôs para evitar seguir vários links de álbuns';
+$lang['porg_issue_2303'] = '[usuários] a senha ainda é enviada em texto simples por e-mail após o registro';
+$lang['porg_issue_2308'] = '[acesso negado] após efetuar login, levado para a página inicial em vez do URL originalmente solicitado';
