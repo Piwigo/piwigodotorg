3d873a475ce621e1f10bc0ab2eceb4cdd9f890df
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:16:28 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2325'] = 'falsche Schriftfarbe für Hover auf der Schaltfläche „Erstes Album erstellen';
+$lang['porg_issue_698'] = 'show_metadata=true vs. noindex,nofollow Logik in picture.php';
+$lang['porg_issue_2323'] = 'In der E-Mail zum Einstellen oder Zurücksetzen des Links ist der Zeitpunkt der Validierung nicht in der richtigen Sprache angegeben';
+$lang['porg_issue_2324'] = 'fehlerhafte Seite (Javascript-Problem) bei Foto-Uploader und Album-Manager in Català';
+$lang['porg_issue_2317'] = '[Benutzer] Falsche Anzeige eines großen Benutzernamens';
+$lang['porg_issue_2319'] = 'falsche Art der generierten md5sum';
+$lang['porg_issue_2322'] = 'Warnung num_rows wenn show_queries = true';
+$lang['porg_issue_2314'] = '[albums] Fehlermeldung beim Hinzufügen eines Albums wird immer angezeigt';
+$lang['porg_issue_2315'] = '[Die Initialisierung des Kennworts löst die Anzeige der zurückgesetzten Benutzeroberfläche aus.';
+$lang['porg_issue_2316'] = 'Wenn Sie die Eingabetaste drücken und der Albumselektor geöffnet ist, erscheinen [Albumselektor] Fehler';
+$lang['porg_issue_2310'] = '[batch manager global] Drücken der Eingabetaste zur Bestätigung einer Aktion verhindert, dass Textareas in die Zeile zurückkehren';
+$lang['porg_issue_2311'] = 'Extreme Belastung beim Hochladen von Fotos und viele Besucher';
+$lang['porg_issue_2309'] = '[Suche] Bewertungsfilter taucht immer wieder auf';
+$lang['porg_issue_2303'] = '[Benutzer] das Passwort wird nach der Registrierung immer noch im Klartext per E-Mail verschickt';
+$lang['porg_issue_2308'] = '[Zugriff verweigert] nach dem Einloggen zur Startseite statt zur ursprünglich angeforderten URL';
+$lang['porg_issue_2280'] = 'fehlende Variable für die Benutzerbenachrichtigung';
+$lang['porg_issue_2300'] = 'bitten Sie die Roboter, nicht mehreren Alben-Links zu folgen';
+$lang['porg_issue_2234'] = '[dunkler Modus] Farben vereinheitlichen';
+$lang['porg_issue_2235'] = 'Chinesischer Text wird nicht in einer Zeile angezeigt';
