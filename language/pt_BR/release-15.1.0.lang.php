64c3c8dab38f086764f33af6cffb857fd5f80ed2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:13:45 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.1.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2197'] = 'Injeção de SQL na visualização da lista de usuários via Admin > Gerenciar';
+$lang['porg_issue_2237'] = '[Unidade de gerenciamento de lote 15.0.0] não é possível criar novas tags';
+$lang['porg_issue_2239'] = 'A visualização do vídeo MP4 TS fica sem memória ao tentar contar páginas do PDF';
+$lang['porg_issue_2247'] = 'Adicionar subálbum em álbum fechado resulta em não mostrar o subálbum sem atualização';
+$lang['porg_issue_2250'] = '[usuários] dicas de ferramentas ausentes no ícone do usuário principal';
+$lang['porg_issue_2251'] = '[usuários] chaves de tradução ausentes para senhas de usuários e usuário principal';
+$lang['porg_issue_2252'] = 'O seletor de álbum [add_picture] pode ser aberto durante o upload da imagem';
+$lang['porg_issue_2253'] = '[15.0.0] A exclusão do álbum pai gera erros de gerenciamento de lote';
+$lang['porg_issue_2254'] = 'upload svg define dimensões para 0x0';
+$lang['porg_issue_2256'] = '[pesquisar] links antigos para pesquisar em um álbum estão quebrados';
+$lang['porg_issue_2258'] = '[fotos] fotos órfãs não são redefinidas após a exclusão';
+$lang['porg_issue_2259'] = '[atividade] a ordem das atividades do sistema às vezes é aleatória';
+$lang['porg_issue_2265'] = '[15.0.0] IPv6 na história ainda truncado em 15 caracteres';
+$lang['porg_issue_2267'] = '[Unidade de gerenciamento de lote 15.0.0] caractere de exibição quebrado próximo a mensagens de aviso';
+$lang['porg_issue_2268'] = 'Adicione um pop-up de novidades no Piwigo';
+$lang['porg_issue_2270'] = '[15.0.0 search] Chamada para função indefinida array_key_first()';
+$lang['porg_issue_2271'] = '[15.0.0] data de instalação imprecisa';
