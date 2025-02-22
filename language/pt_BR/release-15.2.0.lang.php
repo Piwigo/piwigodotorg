cc316d30f06b047fad2d025a9fe2fb1b99a914f0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:13:39 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.2.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_1152'] = '\"última visita\" incoerente para um novo usuário';
+$lang['porg_issue_1960'] = 'PHP8.1: Obsoleto: nl2br(): Passando nulo para o parâmetro #1';
+$lang['porg_issue_2159'] = '\"Obsoleto: addslashes()....\" ao sincronizar a galeria';
+$lang['porg_issue_2234'] = '[modo escuro] padronizar cores';
+$lang['porg_issue_2269'] = 'erro de digitação';
+$lang['porg_issue_2274'] = 'Mover um álbum para um álbum não aberto, listar apenas o(s) álbum(s) movido(s)';
+$lang['porg_issue_2275'] = '[seletor de álbum] álbuns com apenas números no nome são exibidos incorretamente';
+$lang['porg_issue_2277'] = 'Link perdido no que há de novo popin';
+$lang['porg_issue_2279'] = 'Datas de criação não estão na ordem correta para filtros de galeria';
+$lang['porg_issue_2281'] = '[usuários] um link de ativação válido apenas por 1 hora';
+$lang['porg_issue_2283'] = '[usuários] exibição incorreta de um nome de usuário grande e iniciais';
+$lang['porg_issue_2286'] = 'As configurações de edição das imagens só podem ser confirmadas clicando em aplicar ação, não em entrar';
+$lang['porg_issue_2287'] = '[usuários] idioma errado ao gerar e-mail de senha';
+$lang['porg_issue_2288'] = '[usuários] ao solicitar a renovação da senha após alterá-la a interface exibe o e-mail antigo';
+$lang['porg_issue_2289'] = '[usuários] bug de exibição modal do usuário convidado';
+$lang['porg_issue_2290'] = '[plugins] os links para PEM estão sempre no idioma EN';
+$lang['porg_issue_2292'] = '[atividades] distinguir métodos de autenticação';
+$lang['porg_issue_2294'] = 'Aviso de sessão obsoleta no PHP 8.4';
+$lang['porg_issue_2295'] = '[estatísticas anônimas para piwigo.org] enviar atualizações principais';
+$lang['porg_issue_2296'] = 'Erro fatal: A declaração de PwgSession::open deve ser compatível com SessionHandlerInterface::open';
