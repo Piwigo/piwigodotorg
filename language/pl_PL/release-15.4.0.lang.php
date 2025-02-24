7ee8b09247966f96a0710537533aa47a7932cea9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-24 08:40:13 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_698'] = 'logika show_metadata=true vs. noindex,nofollow w picture.php';
+$lang['porg_issue_2317'] = '[użytkownikcy] nieprawidłowe wyświetlanie długiej nazwy użytkownika';
+$lang['porg_issue_2319'] = 'nieprawidłowy rodzaj wygenerowanego md5sum';
+$lang['porg_issue_2322'] = 'ostrzeżenie num_rows kiedy show_queries = true';
+$lang['porg_issue_2323'] = 'W e-mailu ustawiania lub resetowania czas sprawdzania poprawności linku jest w nieprawidłowym języku';
+$lang['porg_issue_2324'] = 'zepsuta strona (problem z javascriptem) przesyłania zdjęć i menedżera albumów w katalońskim';
+$lang['porg_issue_2325'] = 'niewłaściwy kolor czcionki po najechaniu kursorem na przycisk tworzenia pierwszego albumu';
+$lang['porg_issue_2314'] = '[albumy] komunikat o błędzie podczas dodawania albumu jest zawsze wyświetlany';
+$lang['porg_issue_2315'] = '[użytkownicy] inicjalizacja hasła wywołuje reset interfejsu użytkownika';
+$lang['porg_issue_2316'] = '[selektor albumu] pojawiają się błędy po naciśnięciu klawisza enter, kiedy album jest otwarty';
+$lang['porg_issue_2234'] = '[tryb ciemny] standaryzacja kolorów';
+$lang['porg_issue_2235'] = 'Chiński tekst niewyświetlany w jednym wierszu';
+$lang['porg_issue_2280'] = 'Brakująca zmienna dla powiadomień użytkownika';
+$lang['porg_issue_2300'] = 'Prośba do robotów, aby unikały linków do wielu albumów';
+$lang['porg_issue_2303'] = '[użytkownicy] hasło nadal wysyłane w postaci zwykłego tekstu w e-mailu po rejestracji';
+$lang['porg_issue_2308'] = '[odmowa dostępu] po zalogowaniu, przeniesiony na stronę główną zamiast do pierwotnie żądanego adresu URL';
+$lang['porg_issue_2309'] = '[wyszukiwanie] Filtr oceny ciągle się pojawia';
+$lang['porg_issue_2310'] = '[globalny menedżer wsadowy] naciśnięcie entera w celu potwierdzenia działania, uniemożliwia powrót pól tekstowych do linii';
+$lang['porg_issue_2311'] = 'Ekstremalne obciążenie podczas przesyłania zdjęć i wielu odwiedzających';
