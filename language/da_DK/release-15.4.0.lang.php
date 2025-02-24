637631aa318807ebcdfd3255729832ef301e8473
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-24 18:51:05 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2234'] = '[mørk tilstand] Standardisering af farver';
+$lang['porg_issue_2235'] = 'Kinesisk tekst vises ikke på én linje';
+$lang['porg_issue_2280'] = 'Manglende variabel til brugernotifikation';
+$lang['porg_issue_2300'] = 'Bed robotter om undgå at følge flere albumlinks';
+$lang['porg_issue_2303'] = '[brugere] Efter registrering sendes adgangskoden i klar tekst pr. mail';
+$lang['porg_issue_2308'] = '[adgang nægtet] Efter login bliver man ført til forsiden i stedet for den oprindeligt ønskede side';
+$lang['porg_issue_2309'] = '[søgning] Rating-filter bliver ved med at vise sig';
+$lang['porg_issue_2310'] = '[batch manager global] Tryk på enter for at bekræfte en handling forhindrer tekstarealer i at vende tilbage til linjen';
+$lang['porg_issue_2311'] = 'Ekstrem belasning når der uploades fotografier og der er mange besøgende';
+$lang['porg_issue_2314'] = '[albummer] Dervises altid en fejlmeddelelse når der tilføjes et album';
+$lang['porg_issue_2315'] = '[brugere] Initialisering af adgangskode udløser nulstilling af UI-visning';
+$lang['porg_issue_2316'] = '[albumudvælger] Fejl viser sig når man trykker på enter og albumudvælgeren er åben';
+$lang['porg_issue_2317'] = '[brugere] Ukorrekt visning af lange brugernavne';
+$lang['porg_issue_2319'] = 'Ukorrekt sortering af genereret md5sum';
+$lang['porg_issue_2322'] = 'Advarsel num_rows når show_queries = true';
+$lang['porg_issue_2323'] = 'I opsæt- eller nulstilmailen, er linkvalideringstiden ikke på det korrekte sprog';
+$lang['porg_issue_2324'] = 'Defekt side (JavaScript-problem) i fotouploaderen og albumhåndteringen på catalansk';
+$lang['porg_issue_2325'] = 'Forkert skrifttypefarve når der svæves over opret første album-knappen';
+$lang['porg_issue_698'] = 'show_metadata=true vs. noindex,nofollow-logik i picture.php';
