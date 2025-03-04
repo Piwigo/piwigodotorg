636e826212f9916816a63e51d85af5c837d66099
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-04 20:13:40 +0100

    Update nl_NL, thanks to iamjwk

---
+++ language/nl_NL/release-15.4.0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_698'] = 'show_metadata=true vs. noindex,nofollow logica in picture.php';
+$lang['porg_issue_2325'] = 'verkeerde font kleur voor \'hover\' op de knop \'eerste album maken\'';
+$lang['porg_issue_2324'] = 'kapotte pagina (javascript-probleem) m.b.t. foto-uploader en albumbeheerder in Català';
+$lang['porg_issue_2323'] = 'In de instellen of reset van e-mail staat de link validatietijd niet in de juiste taal';
+$lang['porg_issue_2322'] = 'waarschuwing num_rows wanneer show_queries = true';
+$lang['porg_issue_2319'] = 'onjuiste sortering van gegenereerde md5sum';
+$lang['porg_issue_2317'] = '[gebruikers] onjuiste weergave van een lange gebruikersnaam';
+$lang['porg_issue_2316'] = '
+[album keuze] fouten (bugs) worden weergegeven indien u op enter drukt en de album keuze open staat';
+$lang['porg_issue_2315'] = '[gebruikers] wachtwoord initialisatie activeert de reset van de gebruikersinterface weergave';
+$lang['porg_issue_2314'] = '[albums] foutmelding bij het toevoegen van een album wordt altijd weergegeven';
+$lang['porg_issue_2311'] = 'Extreme belasting bij het uploaden van foto\'s en veel bezoekers';
+$lang['porg_issue_2310'] = '[batch manager globaal] door op enter te drukken om een ​​actie te bevestigen, wordt voorkomen dat tekstgebieden terugkeren naar de regel';
+$lang['porg_issue_2309'] = '[zoeken] Beoordelingsfilter blijft terugkomen';
+$lang['porg_issue_2308'] = '[toegang geweigerd] na inloggen, doorgestuurd naar de homepagina in plaats van de oorspronkelijk gevraagde URL';
+$lang['porg_issue_2303'] = '[gebruikers] het wachtwoord wordt na registratie nog steeds als tekst per e-mail verzonden';
+$lang['porg_issue_2300'] = 'vraag robots om het volgen van links naar meerdere albums te vermijden';
+$lang['porg_issue_2280'] = 'variabele ontbreekt voor gebruikersmelding';
+$lang['porg_issue_2235'] = 'Chinese tekst wordt niet op één regel weergegeven';
+$lang['porg_issue_2234'] = '[donkere modus] kleuren standaardiseren';
