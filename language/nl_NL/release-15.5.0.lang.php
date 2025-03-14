89a2347e4df65b9c69f4019c77a58fec565881a9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 07:47:07 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2342'] = 'Tag bewerken donkere modus, verkeerde kleuren';
+$lang['porg_issue_2341'] = 'anonieme gegevens tweemaal verzameld';
+$lang['porg_issue_2340'] = '[gebruikers] standaardgroepen worden niet toegewezen bij het toevoegen van een nieuwe gebruiker';
+$lang['porg_issue_2339'] = '[batch manager filters] validatie van albums en tags veroorzaakt backend-fouten vanwege ontbrekende frontend-controles';
+$lang['porg_issue_2337'] = '[gebruikers] de methode ‘pwg.users.preferences.set’ wordt systematisch aangeroepen telkens wanneer de pagina user_list wordt geopend';
+$lang['porg_issue_2336'] = 'Meer details in logger';
+$lang['porg_issue_2335'] = '[Batch Manager Unit Mode] vermijd het herbouwen van de user_cache';
+$lang['porg_issue_2334'] = 'Verhoog de wachttijd van de user_cache';
+$lang['porg_issue_2333'] = '[Batch Manager Unit Mode] deadlocks vanwege user_cache rebuild';
+$lang['porg_issue_2329'] = '[batch manager unit] parallelle aanroepen naar pwg.images.setInfo veroorzaken bugs';
+$lang['porg_issue_2327'] = '[gebruikers] wachtwoordmaskeringlengte varieert in bevestigingsmail';
+$lang['porg_issue_2326'] = '[batch manager] voor bepaalde acties dwingt het tweemaal klikken op \"verwijderen\" u om de muis te verplaatsen';
