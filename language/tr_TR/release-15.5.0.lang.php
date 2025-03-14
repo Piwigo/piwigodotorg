7c12b62bf092ec6f3480f26a434b42cce585ea76
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 07:47:01 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2326'] = '[toplu iş yöneticisi] belirli eylemler için, \"sil\" üzerine iki kez tıklamak fareyi hareket ettirmenizi zorunlu kılar';
+$lang['porg_issue_2327'] = '[kullanıcılar] şifre maskeleme uzunluğu onay e-postasında değişir';
+$lang['porg_issue_2329'] = '[toplu yönetici birimi] pwg.images.setInfo\'ya paralel çağrılar hatalara neden oluyor';
+$lang['porg_issue_2333'] = '[Toplu İş Yöneticisi Birim Modu] user_cache yeniden oluşturma nedeniyle kilitlenmeler';
+$lang['porg_issue_2335'] = '[Toplu İş Yöneticisi Birim Modu] user_cache\'in yeniden oluşturulmasını önle';
+$lang['porg_issue_2337'] = '[kullanıcılar] ‘pwg.users.preferences.set’ yöntemi, user_list sayfası her açıldığında sistematik olarak çağrılır';
+$lang['porg_issue_2339'] = '[toplu yönetici filtreleri] albüm ve etiket doğrulaması, eksik ön uç kontrolleri nedeniyle arka uç hatalarını tetikliyor';
+$lang['porg_issue_2340'] = '[kullanıcılar] varsayılan grupları yeni bir kullanıcı eklerken atanmaz';
+$lang['porg_issue_2342'] = 'Etiket düzenleme koyu modu, yanlış renkler';
+$lang['porg_issue_2341'] = 'anonim veriler iki kez toplandı';
+$lang['porg_issue_2336'] = 'Günlükleyicide daha fazla ayrıntı';
+$lang['porg_issue_2334'] = 'kullanıcı belleği (user_cache) bekleme süresi artırılması';
