00ebeb0f686631089fdbd0f02ef8b07108d7c309
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 12:42:43 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2326'] = '[batchhåndtering] Ved visse handlinger tvinger to klik på \"slet\" brugeren til at flytte musen';
+$lang['porg_issue_2327'] = '[brugere] Adgangskodemaskeringslængden er forskellig i bekræftelsesmail';
+$lang['porg_issue_2329'] = '[batchhåndtering-unit mode] Parallelle kald til pwg.images.setInfo medfører fejl';
+$lang['porg_issue_2333'] = '[batchhåndtering-unit mode] Deadlocks på grund af genopbygning af user_cache';
+$lang['porg_issue_2334'] = 'Forøg user_cache-ventetid ';
+$lang['porg_issue_2335'] = '[batchhåndtering-unit mode] Undgå genopbygning af user_cache';
+$lang['porg_issue_2336'] = 'Mere detaljerede oplysninger i logger';
+$lang['porg_issue_2337'] = '[brugere] Metoden ‘pwg.users.preferences.set’ kaldes systematisk hver gang siden user_list åbnes';
+$lang['porg_issue_2339'] = '[batchhåndtering-filtre] Validering af albummer og tags udløser backend-fejl på grund af manglende frontend-tjek';
+$lang['porg_issue_2340'] = '[brugere] Standardgrupper tildeles ikke når der tilføjes en ny bruger';
+$lang['porg_issue_2341'] = 'Anonyme data indsamles to gange';
+$lang['porg_issue_2342'] = 'Redigering af tag i mørk tilstand har forkerte farver';
