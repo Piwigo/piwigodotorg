cc2e16e1e993bd019d693116d8b59eb11a4eedae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 07:46:58 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2326'] = '[Gestión de lotes] Para ciertas acciones, al hacer doble clic en "Eliminar", se obliga a mover el ratón';
+$lang['porg_issue_2327'] = '[Usuarios] La longitud del enmascaramiento de contraseña varía en el correo electrónico de confirmación';
+$lang['porg_issue_2329'] = '[Modo unitario de la gestión de lotes] Las llamadas paralelas a pwg.images.setInfo causan errores';
+$lang['porg_issue_2333'] = '[Modo unitario de la gestión de lotes] Interbloqueos debido a la reconstrucción de user_cache';
+$lang['porg_issue_2334'] = 'Aumento del tiempo de espera de user_cache';
+$lang['porg_issue_2335'] = '[Modo unitario de la gestión de lotes] Evitar la reconstrucción de user_cache';
+$lang['porg_issue_2336'] = 'Más detalles en el registro';
+$lang['porg_issue_2337'] = '[Usuarios] El método "pwg.users.preferences.set" se invoca sistemáticamente cada vez que se abre la página user_list';
+$lang['porg_issue_2339'] = '[Filtros del administrador de lotes] La validación de álbumes y etiquetas genera errores de backend debido a la omisión de comprobaciones en el frontend.';
+$lang['porg_issue_2340'] = '[Usuarios] Los grupos predeterminados no se asignan al añadir un nuevo usuario.';
+$lang['porg_issue_2341'] = 'Los datos anónimos se recopilan dos veces. ';
+$lang['porg_issue_2342'] = 'Edición de etiquetas en modo oscuro: colores incorrectos.';
