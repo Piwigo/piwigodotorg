cd09334719920e762457fbb5515f676835ebdff1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 07:47:10 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2342'] = 'Modification de la balise mode sombre, couleurs erronées';
+$lang['porg_issue_2341'] = 'Les données anonymes sont collectées à deux reprises';
+$lang['porg_issue_2340'] = '[utilisateurs] les groupes par défaut ne sont pas attribués lors de l\'ajout d\'un nouvel utilisateur';
+$lang['porg_issue_2339'] = '[Filtres de la gestion par lot] : la validation des albums et des mots-clefs déclenche des erreurs en administration, à cause de vérifications manquantes dans les pages publiques.';
+$lang['porg_issue_2337'] = '[utilisateurs] la méthode \'pwg.users.preferences.set\' était systématiquement appelée à chaque ouverture de la page de la liste des utilisateurs';
+$lang['porg_issue_2336'] = 'Vous trouverez plus de détails dans le journal d\'accès.';
+$lang['porg_issue_2335'] = '[gestion par lot mode unitaire] éviter de reconstruire le cache utilisateur';
+$lang['porg_issue_2333'] = '[gestion par lot mode unitaire] : blocages dus à la reconstruction du cache utilisateur';
+$lang['porg_issue_2334'] = 'augmentation du temps d\'attente du cache utilisateur';
+$lang['porg_issue_2329'] = '[gestion par lot unitaire] Des appels parallèles à pwg.images.setInfo provoquent des bogues.';
+$lang['porg_issue_2327'] = '[utilisateurs] La longueur du masquage du mot de passe varie dans l\'email de confirmation';
+$lang['porg_issue_2326'] = '[gestion par lot] pour certaines actions, cliquer deux fois sur « supprimer » vous oblige à déplacer la souris';
