d8402b0f0b7b1be445d090c77eb9494eb8d1c796
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 07:47:12 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2340'] = '[users] Standardgruppen werden beim Hinzufügen eines neuen Benutzers nicht zugewiesen';
+$lang['porg_issue_2341'] = 'anonyme Daten, die zweimal erhoben werden';
+$lang['porg_issue_2342'] = 'Tag im dunklen Modus bearbeiten, falsche Farben';
+$lang['porg_issue_2336'] = 'Mehr Details im Logger';
+$lang['porg_issue_2337'] = '[users] die Methode \'pwg.users.preferences.set\' wird systematisch jedes Mal aufgerufen, wenn die Seite user_list geöffnet wird';
+$lang['porg_issue_2339'] = '[Batchmanager-Filter] Alben- und Tag-Validierung löst Backend-Fehler aufgrund fehlender Frontend-Prüfungen aus';
+$lang['porg_issue_2333'] = '[Batch Manager Unit Mode] Deadlocks aufgrund von user_cache rebuild';
+$lang['porg_issue_2334'] = 'Wartezeit für user_cache erhöhen';
+$lang['porg_issue_2335'] = '[Batch Manager Unit Mode] Neuaufbau des user_cache vermeiden';
+$lang['porg_issue_2326'] = '[Batch-Manager] bei bestimmten Aktionen zwingt ein Doppelklick auf \„löschen\“ Sie dazu, die Maus zu bewegen';
+$lang['porg_issue_2327'] = '[users] Passwort-Maskierungslänge variiert in Bestätigungs-E-Mail';
+$lang['porg_issue_2329'] = '[batch manager unit] Parallele Aufrufe von pwg.images.setInfo führen zu Fehlern';
