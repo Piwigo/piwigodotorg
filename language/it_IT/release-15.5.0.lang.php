704552ea01bdc45bba9a2cd131845616d5b0086f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-14 07:47:04 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2326'] = '[batch manager] per alcune azioni, cliccando due volte su \"elimina\" si è costretti a spostare il mouse';
+$lang['porg_issue_2327'] = '[utenti] la lunghezza del mascheramento della password varia nell\'e-mail di conferma';
+$lang['porg_issue_2329'] = '[batch manager unit] chiamate parallele a pwg.images.setInfo causano dei bug';
+$lang['porg_issue_2333'] = '[Modalità unità Batch Manager] si blocca a causa della ricostruzione della user_cache';
+$lang['porg_issue_2334'] = 'incrementa il tempo di attesa di user_cache';
+$lang['porg_issue_2335'] = '[Batch Manager Unit Mode] evita di ricostruire user_cache';
+$lang['porg_issue_2336'] = 'Maggiori dettagli nel logger';
+$lang['porg_issue_2337'] = '[utenti] il metodo ‘pwg.users.preferences.set’ viene chiamato sistematicamente ogni volta che viene aperta la pagina user_list';
+$lang['porg_issue_2339'] = '[filtri del gestore batch] la convalida degli album e dei tag innesca errori di backend a causa di controlli mancanti del frontend';
+$lang['porg_issue_2340'] = '[utenti] i gruppi predefiniti non vengono assegnati quando si aggiunge un nuovo utente';
+$lang['porg_issue_2341'] = 'dati anonimi raccolti due volte';
+$lang['porg_issue_2342'] = 'Modifica tag modalità oscura, colori errati';
