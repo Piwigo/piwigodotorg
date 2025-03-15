c569eb08dc06324ce5584670412732a54160714c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-15 06:01:58 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2342'] = 'Editar etiquetas modo escuro, cores erradas';
+$lang['porg_issue_2340'] = '[users] grupos padrão não são atribuídos ao adicionar um novo usuário';
+$lang['porg_issue_2335'] = '[BatchManagerUnitMode] evita reconstruir o user_cache';
+$lang['porg_issue_2336'] = 'Mais detalhes no logger';
+$lang['porg_issue_2337'] = '[users] o método ‘pwg.users.preferences.set’ é chamado sistematicamente cada vez que a página user_list é aberta';
+$lang['porg_issue_2339'] = '[batchmanagerfilters] validação de álbuns e tags aciona erros de backend devido a verificações de frontend ausentes';
+$lang['porg_issue_2341'] = 'dados anônimos coletados duas vezes';
+$lang['porg_issue_2334'] = 'increase user_cache tempo de espera';
+$lang['porg_issue_2333'] = '[BatchManagerUnitMode] deadlocks devido à reconstrução do user_cache';
+$lang['porg_issue_2329'] = '[batchmanagerunit] chamadas paralelas para pwg.images.setInfo causando bugs';
+$lang['porg_issue_2327'] = 'O comprimento da máscara de senha [users] varia no e-mail de confirmação';
+$lang['porg_issue_2326'] = '[batchmanager] para certas ações, clicar duas vezes em \"delete\" força você a mover o mouse';
