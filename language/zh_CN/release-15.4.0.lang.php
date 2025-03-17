e947bf9d4c1bf228c55809fbe96502a023b1afe1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-17 06:52:23 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2234'] = '[暗黑模式] 颜色标准化';
+$lang['porg_issue_2235'] = '中文文本换行显示异常';
+$lang['porg_issue_2280'] = '用户通知变量缺失';
+$lang['porg_issue_2300'] = '机器人需避免批量抓取相册链接';
+$lang['porg_issue_2303'] = '[用户] 注册后仍通过明文邮件发送密码';
+$lang['porg_issue_2308'] = '[权限拒绝] 登录后跳转至首页而非目标页面';
+$lang['porg_issue_2309'] = '[搜索] 评级筛选器反复弹出';
+$lang['porg_issue_2310'] = '[批量管理] 回车确认操作后文本域无法回到原行';
+$lang['porg_issue_2311'] = '图片上传与高并发访问时服务器负载过高';
+$lang['porg_issue_2314'] = '[相册] 添加相册时错误提示始终存在';
+$lang['porg_issue_2315'] = '[用户] 密码重置触发重置界面显示';
+$lang['porg_issue_2316'] = '[相册选择器] 打开时按回车出现 Bug';
+$lang['porg_issue_2317'] = '[用户] 显示过长的用户名';
+$lang['porg_issue_2319'] = '生成的 MD5 校验值排序错误';
+$lang['porg_issue_2322'] = '开启 show_queries = true 时显示 num_rows 警告';
+$lang['porg_issue_2323'] = '[设置/重置邮箱] 链接验证时间语言未适配';
+$lang['porg_issue_2324'] = '[图片上传器/相册管理]（加泰罗尼亚语）页面 JS 错误';
+$lang['porg_issue_2325'] = '创建首个相册按钮悬停字体颜色错误';
+$lang['porg_issue_698'] = 'show_metadata=true 与 noindex,nofollow 逻辑冲突';
