5179aa3421d9e2db85357dad1c277a909ff3a55a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-17 06:52:20 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.5.0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2326'] = '[批量管理] 某些操作中，双击「删除」需手动调整鼠标位置';
+$lang['porg_issue_2327'] = '[用户] 确认邮件中的密码掩码长度不一致';
+$lang['porg_issue_2329'] = '[批量管理单元] 并行调用 pwg.images.setInfo 引发错误';
+$lang['porg_issue_2333'] = '[批量管理单元模式] 用户缓存重建导致死锁';
+$lang['porg_issue_2334'] = '延长用户缓存等待时间';
+$lang['porg_issue_2335'] = '[批量管理单元模式] 避免重建用户缓存';
+$lang['porg_issue_2336'] = '日志中已记录详细信息';
+$lang['porg_issue_2337'] = '[用户] 每次打开用户列表页面时都会系统性调用 pwg.users.preferences.set 方法';
+$lang['porg_issue_2339'] = '[批量管理过滤器] 相册和标签验证因缺少前端检查触发后端错误';
+$lang['porg_issue_2340'] = '[用户] 添加新用户时未分配默认用户组';
+$lang['porg_issue_2341'] = '匿名数据被重复收集';
+$lang['porg_issue_2342'] = '编辑标签暗黑模式颜色显示错误';
