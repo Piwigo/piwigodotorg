830f838c47fe3fcfba03579604fdba55fb16257b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-03-22 05:36:54 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-15.4.0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2234'] = '[koyu mod] renkleri standartlaştır';
+$lang['porg_issue_2235'] = 'Çince metin tek satırda görüntülenmiyor';
+$lang['porg_issue_2280'] = 'kullanıcı bildirimi için eksik değişken';
+$lang['porg_issue_2300'] = 'robotlardan birden fazla albüm bağlantısını takip etmemelerini isteyin';
+$lang['porg_issue_2303'] = '[Kullanıcılar] Şifre, kayıttan sonra e-postayla hala açık metin olarak gönderiliyor';
+$lang['porg_issue_2308'] = '[erişim engellendi] oturum açtıktan sonra, başlangıçta istenen URL yerine ana sayfaya yönlendiriliyorum';
+$lang['porg_issue_2310'] = '[toplu yönetici genel] bir eylemi onaylamak için enter\'a basmak, textareas\'ın satıra dönmesini engeller';
+$lang['porg_issue_2309'] = '[ara] Derecelendirme filtresi tekrar tekrar görünüyor';
+$lang['porg_issue_2311'] = 'Fotoğraf yüklerken aşırı yük ve çok sayıda ziyaretçi';
+$lang['porg_issue_2314'] = '[albümler] albüm eklerken her zaman hata mesajı görüntüleniyor';
+$lang['porg_issue_2315'] = '[kullanıcılar] parola başlatma, sıfırlama kullanıcı arayüzü görüntüsünü tetikler';
+$lang['porg_issue_2316'] = '[albüm seçici] albüm seçici açıkken enter\'a bastığınızda hatalar görünüyor';
+$lang['porg_issue_2317'] = '[kullanıcılar] büyük bir kullanıcı adının yanlış görüntülenmesi';
+$lang['porg_issue_2319'] = 'yanlış türde üretilen md5sum';
+$lang['porg_issue_2322'] = 'show_queries = true olduğunda num_rows uyarısı';
+$lang['porg_issue_2323'] = 'Ayarlanan veya sıfırlanan e-postada bağlantı doğrulama süresi doğru dilde değil';
+$lang['porg_issue_2324'] = 'Katalanca\'daki fotoğraf yükleyici ve albüm yöneticisinde bozuk sayfa (javascript sorunu)';
+$lang['porg_issue_2325'] = 'ilk albümü oluştur düğmesinin üzerine gelindiğinde yanlış yazı tipi rengi';
+$lang['porg_issue_698'] = 'picture.php\'deki show_metadata=true vs. noindex,nofollow mantığı';
