209cd9f9133a2fdab7e07dd9b32a27c0874a3f69
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-11 16:37:13 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'osierocone zdjęcia po opróżnieniu galerii';
+$lang['porg_issue_2390'] = 'kilka zdjęć może dzielić tę samą ścieżkę';
