780cd9139a6deb15334447201435595cb524bde7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-11 16:37:10 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2390'] = 'più fotografie possono condividere lo stesso percorso';
+$lang['porg_issue_2376'] = 'fotografie orfane, quando la lounge viene svuotata';
