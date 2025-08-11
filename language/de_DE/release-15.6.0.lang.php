3ee3888c0df30d47aac2feb286d8b7cbd1dacf66
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-11 16:37:19 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'verwaiste Fotos, wenn die Lounge geleert wird';
+$lang['porg_issue_2390'] = 'Mehrere Fotos können denselben Pfad haben.';
