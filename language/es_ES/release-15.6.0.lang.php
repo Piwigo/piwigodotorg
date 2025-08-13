ff5486d4d9769fb7e8626ff193d52e340a42b304
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-13 18:09:05 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'marcar las fotos como huérfanas cuando se vacía el vestíbulo';
+$lang['porg_issue_2390'] = 'algunas fotos pueden compartir la misma ruta';
