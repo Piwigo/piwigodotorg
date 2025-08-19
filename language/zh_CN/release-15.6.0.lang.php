a7dfc96c2c0d7cd2845f3c065b95891c1234799b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-19 09:00:20 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2390'] = '多张照片可能共享同一路径';
+$lang['porg_issue_2376'] = '当休息区被清空时可能会出现孤立照片';
