1c4f35a11491aff75ac5e1cd4382ffaa724c77d2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-19 05:49:55 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'forældreløse fotografier når lounge tømmes';
+$lang['porg_issue_2390'] = 'flere fotografier kan dele den samme sti';
