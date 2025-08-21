a044806e254a5a2068b86400b960dcf00cfe8fdd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-21 06:23:46 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'fotos órfãs quando o salão está vazio';
+$lang['porg_issue_2390'] = 'várias fotos podem compartilhar o mesmo caminho';
