adbe36b33e972f22ce2d40d9cbc21fe8a2ec2cf0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-24 06:16:22 +0200

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'Weesfoto’s wanneer de lounge wordt geleegd';
+$lang['porg_issue_2390'] = 'Meerdere foto’s kunnen hetzelfde pad delen';
