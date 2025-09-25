7e82d3aa30c7b3a67d5a4cb6154f3b5cb568577b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-25 13:44:10 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-15.6.0.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2376'] = 'yetim fotoğraflar';
+$lang['porg_issue_2390'] = 'birkaç fotoğraf aynı yolu paylaşabilir';
