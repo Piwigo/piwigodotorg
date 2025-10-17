eb8d51d2fef01d30c91b7ab182aeb66315f3ab05
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-17 12:40:57 +0200

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/release-14.4.0.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2110'] = 'Cai ao carregar uma foto que já está presente
+';
+$lang['porg_issue_2129'] = '[atividade do utilizador] a caixa de filtro do utilizador excede o tamanho do contentor';
+$lang['porg_issue_2131'] = '[Pesquisa 14.2+] Ícone ausente no seletor do album
+';
+$lang['porg_issue_2134'] = 'Disponivel, mais de uma notificação por e-mail, na nova versão do Piwigo ';
