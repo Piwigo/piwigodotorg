aa83b6d27e02c8c69cb714f0e4b1aa45da6c499e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-21 09:26:51 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Enumeração de usuários por meio de endpoint de redefinição de senha';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Assuma o controle da conta com um clique, modificando o link de redefinição de senha';
+$lang['porg_issue_2408'] = 'Palavras-chave IPTC separadas apenas por retorno de carro';
+$lang['porg_issue_2396'] = '[upload] Erro fatal: mysqli_sql_exception não detectado: Entrada duplicada para a chave PRIMARY';
