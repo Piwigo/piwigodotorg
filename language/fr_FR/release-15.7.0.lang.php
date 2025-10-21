e6406c16b8c56c0fc31c16424d6dd812cf79a55a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-21 13:29:00 +0200

    Update fr_FR, thanks to laurent.duretz

---
+++ language/fr_FR/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Identification des utilisateurs par l\'interface de réinitialisation du mot de passe';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Récupération du compte en un clic en modifiant le lien de réinitialisation du mot de passe';
+$lang['porg_issue_2396'] = '[téléversement] Erreur fatale : mysqli_sql_exception non gérée : Doublon pour la clé primaire';
+$lang['porg_issue_2408'] = 'Mots clés IPTC séparés par un seul saut de ligne';
