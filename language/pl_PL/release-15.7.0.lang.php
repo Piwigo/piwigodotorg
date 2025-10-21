3cbace0e17584ef976d053c7c0153254de602e09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-21 09:26:54 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2396'] = '[przesyłanie] Błąd krytyczny: niewyłapany mysqli_sql_exception: zduplikowany wpis dla klucza PRIMARY';
+$lang['porg_issue_2408'] = 'Słowa kluczowe IPTC oddzielone tylko znakiem powrotu karetki';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Przejęcie konta jednym kliknięciem poprzez modyfikację linku do resetowania hasła';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Identyfikacja kont użytkowników przez interfejs resetowania hasła';
