e2b668bc1c26da7218b605dbf755c0e5240cf20e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-21 09:26:57 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Übernahme des Kontos mit einem Klick durch Ändern des Links zum Zurücksetzen des Passworts';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Benutzeraufzählung über Endpunkt zum Zurücksetzen des Passworts';
+$lang['porg_issue_2396'] = '[Hochladen] Schwerwiegender Fehler: Uncaught mysqli_sql_exception: Doppelte Eingabe für Schlüssel PRIMARY';
+$lang['porg_issue_2408'] = 'IPTC-Schlüsselwörter nur durch Zeilenumbruch getrennt';
