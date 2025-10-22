46d3a36e48085f0d6241cc115ae46eb33195f062
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-22 06:35:04 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2396'] = '[caricamento] Errore fatale: Uncaught mysqli_sql_exception: voce duplicata per la chiave PRIMARY';
+$lang['porg_issue_2408'] = 'Parole chiave IPTC separate solamente da "ritorno a capo"';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Acquisizione dell\'account con un clic modificando il collegamento per la reimpostazione della password';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Enumerazione degli utenti tramite endpoint di reimpostazione della password';
