52dfc3044261fd5d4ce23ba175416e3e45a78d97
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-22 06:35:01 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2396'] = '[carga] Error crítico: Excepción mysqli_sql_exception no capturada: entrada duplicada para clave PRIMARY';
+$lang['porg_issue_2408'] = 'Palabras clave de IPTC separadas solamente por retorno de carro';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Robo de cuenta de un solo clic modificando el enlace de restauración de contraseña';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Enumeración de usuarios a través del punto final de restauración de contraseña';
