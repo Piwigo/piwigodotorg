a3d496431a6bf5c9b439af6e473b760c3d7844f7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-22 06:34:58 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2396'] = '[yükleme] Ölümcül hata: Yakalanmamış mysqli_sql_exception: PRIMARY anahtarı için yinelenen giriş';
+$lang['porg_issue_2408'] = 'IPTC anahtar sözcükleri yalnızca satır sonuyla ayrılır';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Şifre sıfırlama bağlantısını değiştirerek tek tıklamayla hesap devralma';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Şifre Sıfırlama Uç Noktası aracılığıyla Kullanıcı Numaralandırması';
