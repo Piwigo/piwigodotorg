b98fe271a10ee8f97f32065086d29db5d6ff16e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-25 20:25:29 +0200

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2408'] = 'IPTC-trefwoorden alleen gescheiden door carriage return';
+$lang['porg_issue_2396'] = '[upload] Fatale fout: Niet afgevangen mysqli_sql_exception: Dubbele invoer voor PRIMARY key';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Overname van uw account met één klik door de link voor het opnieuw instellen van uw wachtwoord te wijzigen';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Gebruikers-opsomming via wachtwoordherstel-eindpunt';
