92cced9de6013a412220efd7ef03df02860f1674
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-30 19:01:46 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2396'] = '[上传] 致命错误：未捕获的mysqli_sql_exception：主键重复';
+$lang['porg_issue_2408'] = '仅使用回车符分隔的IPTC关键词';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = '通过修改密码重置链接实现一键接管账户';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = '通过密码重置接口实现用户枚举';
