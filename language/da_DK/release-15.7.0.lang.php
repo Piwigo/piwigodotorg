130f65f6d7e5becd05756cbd6ec7db8f61bd16e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-03 11:39:33 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-15.7.0.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2396'] = '[upload] Fatal fejl: Ikke opfanget mysqli_sql_exception: Duplikeret forekomst af nøglen PRIMARY';
+$lang['porg_issue_2408'] = 'IPTC-nøgleord kun adskilt af return';
+$lang['porg_issue_GHSA-9986-w7jf-33f6'] = 'Overtagelse af konto ved et enkelt klik ved at ændre link til nulstilling af adgangskode';
+$lang['porg_issue_GHSA-h4wx-7m83-xfxc'] = 'Brugeroptælling vha. endpoint til nulstilling af adgangskode';
