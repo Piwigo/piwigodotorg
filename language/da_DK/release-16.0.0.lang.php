28e70ba0654d2694cc599a6030c582d756930ad0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-20 06:28:17 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-16.0.0.lang.php
@@ -0,0 +1,83 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['pwg_org_release16_expert_mode_des3'] = 'For brugere der aldrig har prøvet den avancerede syntaks før (primært fordi den kun var tilgængelig i administrationens batchhåndtering), har vi redesignet hjælpepopin\'en. Her finder man vejledning i hvordan eksperttilstanden benyttes mere effektivt.';
+$lang['pwg_org_release16_comment_manager_desc'] = 'Kommentarhåndteringen er blevet forandet, i håb om at det vil være lettere end tidligere, at håndtere kommentarer til dine fotografier.';
+$lang['pwg_org_release16_tech_features_change_img_priority_desc'] = 'Anvender prioriteret "External ImageMagick" (kommandolinje) frem for "ImageMagick" (PHP-wrapper). Kald til ImageMagick på kommandolinjen er mere optimeret og anvender sin egen hukommelse. PHP-wrapperen kender ikke altid de seneste tilgængelige ændringer, som er tilgængelige fra kommandolinjen.';
+$lang['pwg_org_release16_interview_lana2'] = 'Det aktive fællesskab, som Piwigo er omgivet af, der udvikler plugins og skræddersyede teamer, motiverer mig altid til at være endnu bedre.
+Min drøm har altid være, at arbejde i et open source-projekt; jeg håber at det afspejles i mine bidrag til Piwigo!';
+$lang['pwg_org_release16_intro_desc'] = 'Piwigo 16 indeholder et nyt lag af konsistens, kontrol og sikkerhed. Med standardsidernes nye design, eksempelvis login og profil m.fl., har nu et fælles, ensartet og tilpasningsvenligt udseende. API-nøgler og en ny pluging til tofaktor-autetifikation forbedrer beskyttelsen af galleriet, mens opdateringer til den avancerede søgning og administationsbillederne gør almindelige opgaver lettere.';
+$lang['pwg_org_release16_interview_romain'] = 'Hej, mit navn er Romain (perrom på GitHub). Jeg arbejde som fullstack-udviklerpraktikant i Piwigo-holdet fra april til august 2025. Under mit ophold var jeg altid ivrig efter at forbedre min kode og blive en bedre udvikler, så jeg gennem mit arbejde kunne gøre det bedste for alle Piwigo-brugere.';
+$lang['pwg_org_release16_interview_lana'] = 'Hej! Jeg hedder Lana (RushLana eller Renarde på GitHub og i forummet). I april begyndte jeg som praktikant i Piwigo-holdet og siden september har jeg været fuldtidsansat.
+De fleste af mine bidrag har været i forbindelse med det nyligt udgivne officielle Docker-image, til sikring af at det både er stabilt og stemmer overens med Piwigos standardinstallering.';
+$lang['pwg_org_release16_interview_romain_part2'] = 'Jeg implementerede blandt andre en ny valgmulighedssside til galleriets søgefiltre. Desuden foretog jeg en mindre omorganisering af brugeraktivitetssiden, som tidligere havde nogle behandlingsproblemer og begrænsede filtre. Det var ikke lette opgaver, men jeg er tilfreds med, hvad jeg fik gennemført i min praktikperiode. Jeg håber at du fortsat til støtte Piwigo og holdet bag; de hjalp mig meget, mens jeg var hos dem.';
+$lang['pwg_org_release16_update_img_batches_desc'] = 'For at opdatere et billedesæt, er der en valgmulighed på formularen til upload af fotografier, som kan aktiveres til enhver tid, når du overfører billeder.';
+$lang['pwg_org_release16_update_img_batches_desc2'] = 'Piwigo genkender filer i det samme album med det samme filnavn. Visuelt vil fotografiet blive opdateret, men dets egenskaber (titel, tags, beskrivelse osv.) vil ikke blive ændret.';
+$lang['pwg_org_release16_gallery_search_desc2'] = 'Det er nu muligt at beslutte hvem der skal have adgang til hvilket filter. Vælg hvad kun administratorer, kun registrerede brugere eller hvad alle kan anvende i den filtrerede søgning.';
+$lang['pwg_org_release16_gallery_search_desc3'] = 'Administratorer kan vælge hvilke filtre, der bliver vist som standard. Det er også muligt at opsætte standardfiltre til hvad brugeren senest anvendte.';
+$lang['pwg_org_release16_img_format_desc'] = 'Med HD-billeder og skærmstørrelser, som bliver større og større, havde vi fået brug for større visningsstørrelser. Selv på en lille 14-tommers skærm udfyldte XXL-størrelsen ikke altid hele skærmen, da skærmen ofte har en høj opløsning. 3XL og 4XL gør det nu muligt at "fylde hele skærmarealet". Hvis det oprindelige billede tillader det, vil de nye 3XL- og 4XL-størrelser blive genereret af Piwigo. For at spare plads, er denne mulighed ikke aktiveret som standard.';
+$lang['pwg_org_release16_tech_features_docker_image'] = 'Docker-image';
+$lang['pwg_org_release16_tech_features_docker_image_get'] = 'Hent vores officielle Docker-image';
+$lang['pwg_org_release16_title_date_format'] = 'Datoformater afhænger af sproget';
+$lang['pwg_org_release16_title_expert_mode'] = 'Nye søgefiltre, eksperttilstand';
+$lang['pwg_org_release16_title_gallery_search'] = 'Gallerisøgning, filterhåndtering';
+$lang['pwg_org_release16_title_img_format'] = 'Nye billedstørrelser, 3XL og 4XL';
+$lang['pwg_org_release16_title_mv_save_btn'] = 'Flyttede gem-knapper';
+$lang['pwg_org_release16_title_related_tags'] = 'En ny måde at udforske relaterede tags på';
+$lang['pwg_org_release16_title_update_img_batches'] = 'Opdatering af billeder i bundter';
+$lang['pwg_org_release16_intro_short_text'] = 'Igen viser Piwigo 16 at gennemtænkte detaljer gør hele forskellen, når man håndterer sit fotobibliotek.';
+$lang['pwg_org_release16_menu_std_pages_desc2'] = 'Måler et at tilbyde et minimalistisk design, der er let at tilpasse, som er er det samme for alle brugere, uanset hvilket tema de vælger.';
+$lang['pwg_org_release16_menu_std_pages_desc4'] = 'Alle standardsider har ganske nyttig funktionalitet: En kontakt til at skifte mellem lys og mørk tilstand, et link til at finde hjælp i vores dokumentation og et sprogskifter.';
+$lang['pwg_org_release16_mv_save_btn_desc'] = 'I forlængelse af en tidligere opdatering af redigering af en albumside, hvor gem-knappen blev flyttet, har vi konstateret at det er en bedre brugeroplevelse, at knappen altid er synlig nederst på siden.';
+$lang['pwg_org_release16_related_tags_desc'] = 'Vi har besluttet af forbedre hvor og hvordan relaterede tags vises. Vi synes at det er, og altid har været, en god og ganske usædvanlig funktionalitet, som tilbydes af Piwigo. Den nye måde at vise relaterede tags på, opmuntrer til at bruge dem, og vi håber at du med glæde genopdager dem.';
+$lang['pwg_org_release16_related_tags_desc2'] = 'På enhver side med miniaturebilleder, finder man relaterede tags-dropdownlisten i menuen med de sekundære handlinger (på højre side ved benyttelse af temaet Modus).';
+$lang['pwg_org_release16_related_tags_desc3'] = 'Når en tag-side åbnes, bliver de relaterede tags vist som badges til kombinering med aktuelle tags med et enkelt klik.';
+$lang['pwg_org_release16_tech_features_docker_image_desc'] = 'Piwigo 16 indfører et officielt Docker-image, som tilbyder en simpel og pålidelig måde at afvikle Piwigo i containerorienterede miljøer. Dette image vedligeholdes af vores team og gør installering og opdatering lettere for brugere, der foretrækker Docker-baseret udrulning.';
+$lang['pwg_org_release16_title'] = 'Optimeret til udfordringsfri anvendelse.';
+$lang['pwg_org_release16_title_activity_filter'] = 'Nye filtre til brugeraktivitet';
+$lang['pwg_org_release16_title_comment_manager'] = 'Kommentarhåndtering';
+$lang['pwg_org_release16_tech_features_php_min_version'] = 'Kompatibilitet med PHP 8.4 er krævet for at kunne skifte til Smarty 5.5.2 (skabelonmotor). Det betyder dog at den påkrævede minimale version af PHP, for at kunne køre Piwigo, nu er PHP 7.4.';
+$lang['pwg_org_release16_menu_std_pages_desc3'] = 'Administratorer kan vælge hvorvidt de ønsker at benytte standardsidernes design eller bevare det nuværende temas design';
+$lang['pwg_org_release16_menu_std_pages_desc5'] = 'På profilsiden kan brugere håndtere alt som før (opdatere deres mailadresse, adgangskode og galleripræferencer)';
+$lang['pwg_org_release16_menu_std_pages_desc'] = 'Vi har indført et valgtfrit standarddesign til almindelige sider i alle teamer. Dvs. loginsiden, opret, glemt adgangskode og profil.';
+$lang['pwg_org_release16_tech_features_api_keys'] = 'API-nøgler';
+$lang['pwg_org_release16_tech_features_change_img_priority'] = 'Skift prioritet på billedbiblioteker';
+$lang['pwg_org_release16_menu_update_img_batches'] = 'Opdater billede i bundter';
+$lang['pwg_org_release16_new_badge'] = 'Ny';
+$lang['pwg_org_release16_menu_mv_save_btn'] = 'Flyttet gem-knap';
+$lang['pwg_org_release16_menu_newsletter_banner'] = 'Nyhedsbrevsbanner';
+$lang['pwg_org_release16_menu_related_tags'] = 'Relaterede tags';
+$lang['pwg_org_release16_menu_std_pages'] = 'Standardsider';
+$lang['pwg_org_release16_menu_technical_features'] = 'Tekniske funktioner';
+$lang['pwg_org_release16_menu_title_std_pages'] = 'Standardsider';
+$lang['pwg_org_release16_gallery_search_desc'] = 'Med Piwigo 14 indførte vi en ny måde at søge på i galleriet. Fire filtre var som standard aktive. Piwigo 16 gør det muligt at ændre tilgængelige filtre og hvilke der skal vises som standard.';
+$lang['pwg_org_release16_in_american_english'] = 'på amerikansk-engelsk';
+$lang['pwg_org_release16_in_french'] = 'på fransk';
+$lang['pwg_org_release16_in_german'] = 'på tysk';
+$lang['pwg_org_release16_in_spanish'] = 'på spansk';
+$lang['pwg_org_release16_menu_2FA'] = 'Tofaktor-autentifikation';
+$lang['pwg_org_release16_menu_2FA_title'] = 'Tofaktor-autentifikation, fornyet';
+$lang['pwg_org_release16_menu_activity_log_filter'] = 'Filtrering af aktivitetslog';
+$lang['pwg_org_release16_menu_comment_manager'] = 'Kommentarhåndtering';
+$lang['pwg_org_release16_menu_expert_mode'] = 'Eksperttilstand';
+$lang['pwg_org_release16_menu_gallery_search'] = 'Gallerisøgning, filterhåndtering';
+$lang['pwg_org_release16_date_format_desc'] = 'Vi har indført understøttelse af forskellige måder at skrive datoer på ifølge sproget. Den engelske måde at skrive datoer på, er standarden. For eksempel:';
+$lang['pwg_org_release16_end_notes'] = 'Bemærk at mere detaljeret information er tilgængelig vedrørende';
+$lang['pwg_org_release16_end_notes2'] = 'Tekniske ændringer i Piwigo 16';
+$lang['pwg_org_release16_end_notes3'] = 'Læs hvordan dine udvidelser gøres kompatible med Piwigo 16.';
+$lang['pwg_org_release16_expert_mode_desc'] = 'Gallerisøgningen har fået et nyt filter til søgning efter ord, sammen med det eksisterende. Filteret anvender udvidet syntaks og operatorer for at udføre søgningen.';
+$lang['pwg_org_release16_expert_mode_desc2'] = 'Vi har tilføjet et eksperttilstand-søgefilter, som gør det muligt at forfine forespørgsler med meget større præcision. Du kan nu fravælge ord, søge efter nøjagtige forekomster og gøre brug af et udvalg af operatorer.';
+$lang['pwg_org_release16_2FA_desc'] = 'Tofaktor-autentifikation (eller 2FA) er den velkendte bekræftelseskode, som loginformularer beder om. Bekræftelseskoden sendes af en tredjepart for at forhindre identitetstyveri.';
+$lang['pwg_org_release16_2FA_desc2'] = 'Piwigo har siden 2022 haft plugin\'en "Two-factor Authentication" (kun via mail), men aktivering af den umuliggjorde autentifikation gennem mobile apps eller alle scripts som benytter Piwigos API. Vi har besluttet om lave det om. Den nye plugin er nu kompatibel med mobile apps, ved at benytte de nye API-nøgler. I stedet for at benytte mail til modtagelse af bekræftelseskoden, har vi tilføjet kompatibilitet med TOTP (time-based one-time password, eller tidsbaseret engangsadgangskode), som er meget mere pålideligt.';
+$lang['pwg_org_release16_activity_filter_desc'] = 'Det kunne nogle gange være besværligt at finde oplysninger i aktivitetsloggen. Med nye filtre, har det aldrig været lettere at finde oplysninger om brugeraktivitet.';
+$lang['pwg_org_release16_activity_filter_desc2'] = 'Det er muligt at vælge en bestemt bruger eller handling, for at finde ud af hvorfor oplysninger om fotografier/albummer/grupper ... kan være blevet ændret. Det kan være en fordel til at finde ud af hvilken kollega, som har udført en uventet handling, som for eksempel sletning af et album (naturligvis ved en fejl).';
+$lang['pwg_org_release16_api_key_desc'] = 'Scripts og apps, der anvender Piwigos API, skal ikke længere overvære brugernavn/adgangskode ved autentifikation. I stedet er det nu muligt at anvende en API-nøgle. Hver API-nøgle er forbundet med en bestemt bruger og er tidsbegrænset.';
+$lang['pwg_org_release16_api_key_desc2'] = 'For at håndtere disse API-nøgler, finder man dem (indtil videre) kun i galleriprofilens standardsider. API-nøgler kan også anvendes til at logge på, ved at benytte API-nøglens id som brugernavn og API-nøglens hemmelighed som adgangskode, for at gøre 2FA kompatibel med mobile apps.';
+$lang['pwg_org_release16_badge_redesign'] = 'Redesign';
+$lang['pwg_org_release16_badge_refresh'] = 'Opfriskning';
+$lang['pwg_org_release16_comment_manager_desc2'] = 'Designet gør det mere tydeligt, hvilke kommentarer der stadig mangler validering og hvilke der allerede er blevet valideret. En udvælgelsestilstand gør det lettere at håndtere kommentarer i bundter.';
+$lang['pwg_org_release16_compatibility'] = 'Kompabilitet med PHP 8.4';
