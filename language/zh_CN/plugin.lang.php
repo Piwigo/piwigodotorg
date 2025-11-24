f20539c235ae16129ffa2826d8585bb1051a60c6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-24 06:24:31 +0100

    Update zh_CN, thanks to walle

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -175,3 +175,4 @@ $lang['porg_users_tourism'] = '旅游和文化';
 $lang['language_ga_IE'] = '盖尔语';
 $lang['language_hy_AM'] = '亚美尼亚语';
 $lang['language_te_IN'] = '泰卢固语';
+$lang['porg_guides_img_docker'] = 'Docker镜像';
