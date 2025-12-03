2befd6be70ac3e625e02c8ee09f9fa98191d55c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-03 08:20:34 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2467'] = 'Erreur de type entier, compatibilité PHP 8.5';
+$lang['porg_issue_2462'] = 'Problème avec ImageMagick - Piwigo 16';
+$lang['porg_issue_2461'] = 'indique aux thèmes si les pages standard sont actives ou non';
+$lang['porg_issue_2460'] = 'Utilisation de l\'authentification HTTP Basic sur Nginx renvoyant {\"stat\":\"fail\",\"err\":401,\"message\":\"Invalid api_key\"}';
+$lang['porg_issue_2458'] = '[16.0.0, PHP 7.4] Téléversement d\'image : appel à une fonction non définie str_starts_with()';
