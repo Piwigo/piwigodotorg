a222fcc77d831e2ef64e2c6e1cfb1c1b097b853d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-03 08:20:37 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2458'] = '[16.0.0, PHP 7.4] billedupload: Call to undefined function str_starts_with()';
+$lang['porg_issue_2460'] = 'Brug af HTTP Basic Auth med Nginx giver {\"stat\":\"fail\",\"err\":401,\"message\":\"Invalid api_key\"}';
+$lang['porg_issue_2461'] = 'Fortæl skabeloner hvorvidt standardsider er aktive eller ej';
+$lang['porg_issue_2462'] = 'Problem med ImageMagick-plugin - Piwigo 16';
+$lang['porg_issue_2467'] = 'Fejl ved konvertering til heltal, php 8.5-kompatibilitet';
