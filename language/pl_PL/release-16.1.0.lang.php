da6ee70402e4abe44fe62847269c89ea9929c18e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-05 06:41:37 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2458'] = '[16.0.0, PHP 7.4] przesyłanie zdjęcia: Wywołanie niezdefiniowanej funkcji str_starts_with()';
+$lang['porg_issue_2460'] = 'Użycie HTTP Basic Auth na Nginx zwracające {\"stat\":\"fail\",\"err\":401,\"message\":\"Invalid api_key\"}';
+$lang['porg_issue_2461'] = 'Informacja dla szablonów, czy standardowe strony są aktywne, czy nie';
+$lang['porg_issue_2462'] = 'Problem z Ext ImageMagick - Piwigo 16';
+$lang['porg_issue_2467'] = 'Błąd liczby całkowitej, kompatybilność z php 8.5';
