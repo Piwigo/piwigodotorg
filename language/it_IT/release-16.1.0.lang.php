7fbe23a7b885d706664f6cbb95bc10c643dd9c8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-05 06:41:35 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2458'] = '[16.0.0, PHP 7.4] image upload: Call to undefined function str_starts_with()';
+$lang['porg_issue_2460'] = 'Using HTTP Basic Auth on Nginx returning {\"stat\":\"fail\",\"err\":401,\"message\":\"Invalid api_key\"}';
+$lang['porg_issue_2461'] = 'indica ai modelli se le pagine standard sono attive o meno';
+$lang['porg_issue_2462'] = 'Problema ext ImageMagick - Piwigo 16';
+$lang['porg_issue_2467'] = 'errore di cast integer, compatibilità con php 8.5';
