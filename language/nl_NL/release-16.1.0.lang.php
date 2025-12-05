2bd855e8c62c252965d9e1d75438439137f772d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-05 06:41:43 +0100

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2458'] = '[16.0.0, PHP 7.4] afbeelding uploaden: oproep naar ongedefinieerde functie str_starts_with()';
+$lang['porg_issue_2460'] = 'HTTP Basic Auth gebruiken op Nginx retourneert {\"stat\":\"fail\",\"err\":401,\"message\":\"Invalid api_key\"}';
+$lang['porg_issue_2461'] = 'vertel sjablonen of standaardpagina\'s actief zijn of niet';
+$lang['porg_issue_2462'] = 'Ext ImageMagick-probleem - Piwigo 16';
+$lang['porg_issue_2467'] = 'cast integer fout, php 8.5 compatibiliteit';
