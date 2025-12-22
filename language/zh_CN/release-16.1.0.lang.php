4cdc686f2102ef74f4a0fcc813b47aece43b5e23
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-22 14:48:53 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2458'] = '[16.0.0 版本，PHP 7.4 环境] 图片上传：调用未定义的函数 str_starts_with ()';
+$lang['porg_issue_2460'] = '在 Nginx 上启用 HTTP 基本认证后，返回 {"stat":"fail","err":401,"message":"Invalid api_key"}';
+$lang['porg_issue_2461'] = '告知模板引擎标准页面是否处于激活状态';
+$lang['porg_issue_2462'] = '扩展组件 ImageMagick 异常 - Piwigo 16 版本';
+$lang['porg_issue_2467'] = '整数类型转换错误，PHP 8.5 兼容性问题';
