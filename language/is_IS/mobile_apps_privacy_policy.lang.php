cb4bfe4b470db447ca41704f14abaf6642444531
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-29 06:58:32 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/mobile_apps_privacy_policy.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_privacy_mobile_protection_6'] = 'Þegar þú hleður upp myndum í Piwigo myndasafn geta myndir innihaldið persónuupplýsingar eins og GPS hnit eða athugasemdir.';
+$lang['porg_privacy_mobile_protection_7'] = 'Forritin bjóða upp á möguleika á að fjarlægja þessi gögn úr myndaskrám áður en þeim er hlaðið upp í stillingahlutanum.';
+$lang['porg_privacy_mobile_protection_8'] = 'Það er á ábyrgð notandans að ákveða hvort þessum persónuupplýsingum sé hlaðið upp.';
+$lang['porg_privacy_mobile_security_1'] = 'Við notum tæknilegar öryggisráðstafanir til að vernda persónuupplýsingar þínar.';
+$lang['porg_privacy_mobile_security_2'] = 'Þó að við höfum gripið til hæfilegra ráðstafana til að tryggja öryggi persónuupplýsinganna sem þú lætur okkur í té, vinsamlegast hafðu í huga að þrátt fyrir viðleitni okkar eru engar öryggisráðstafanir fullkomnar eða órjúfanlegar og engin aðferð til gagnaflutnings er tryggð gegn hlerun eða annarri tegund misnotkunar.';
+$lang['porg_privacy_mobile_security_3'] = 'Allar upplýsingar sem birtar eru á netinu eru viðkvæmar fyrir misnotkun og misnotkun óviðkomandi aðila.';
+$lang['porg_privacy_mobile_security_4'] = 'Þess vegna getum við ekki ábyrgst fullkomið öryggi ef þú gefur upp persónuupplýsingar.';
+$lang['porg_privacy_mobile_storage_1'] = 'Forritin geyma aðeins gögn til að uppfylla eitt markmið: að fá aðgang að Piwigo myndasafni sem er hýst á fjarlægum vefþjóni.';
+$lang['porg_privacy_mobile_storage_2'] = 'Engum gögnum er safnað, sent eða geymt sem ekki eru nauðsynleg fyrir tilætlaða virkni þeirra.';
+$lang['porg_privacy_mobile_storage_3'] = 'Þegar þú opnar forritin eru persónuupplýsingar þínar ekki safnaðar með notkun rakningartækni.';
+$lang['porg_privacy_mobile_storage_4'] = 'Við notum eingöngu skyndiminni til að sérsníða forritin og bæta upplifun þína.';
+$lang['porg_privacy_mobile_storage_5'] = 'Þú getur slökkt á eða hreinsað skyndiminnið, en hafðu í huga að slík aðgerð gæti haft áhrif á aðgengi og virkni forritanna.';
+$lang['porg_privacy_mobile_data_piwigo_2'] = 'Auk þess eru allar myndir, myndbönd og aðrar skrár sem valdar eru til niðurhals/upphleðslu og innslegið efni geymdar, unnið úr þeim og sendar á stillta gallerísíðuna.';
+$lang['porg_privacy_mobile_intro_1'] = '<a href="%s" target="_blank">Piwigo-Android</a> og <a href="%s" target="_blank">Piwigo-Mobile</a> eru ókeypis, frjáls og opinn hugbúnaður fyrir Piwigo.';
+$lang['porg_privacy_mobile_intro_10'] = 'Við munum láta þig vita af öllum breytingum með því að uppfæra dagsetninguna „Síðast uppfært“ í þessari persónuverndarstefnu.';
+$lang['porg_privacy_mobile_intro_11'] = 'Þér er bent á að skoða þessa persónuverndarstefnu reglulega til að vera upplýstur um uppfærslur.';
+$lang['porg_privacy_mobile_intro_12'] = 'Þú telst hafa verið upplýst(ur) um, verður háður(ur) og telst(ur) hafa samþykkt breytingarnar á endurskoðaðri persónuverndarstefnu með því að halda áfram notkun þinni á forritunum eftir að slík endurskoðuð persónuverndarstefna var birt.';
+$lang['porg_privacy_mobile_intro_13'] = 'Þessi persónuverndarstefna á ekki við um net-/snjallsímaverslun þriðja aðila þar sem þú setur upp forritin eða greiðir, þar með talið sýndarvörur í forritum, sem kunna einnig að safna og nota gögn um þig.';
+$lang['porg_privacy_mobile_intro_14'] = 'Við berum ekki ábyrgð á neinum gögnum sem slíkur þriðji aðili safnar.';
+$lang['porg_privacy_mobile_intro_2'] = 'Þessi persónuverndarstefna á við um þessi innbyggðu Android og iOS forrit sem tengjast Piwigo síðum sem höfundarnir hafa ekki stjórn á.';
+$lang['porg_privacy_mobile_intro_3'] = 'Kynntu þér persónuverndarstefnu viðkomandi vefsíðna áður en þú stillir reikning í Piwigo-Android eða Piwigo-Mobile til að tengjast myndasafni.';
+$lang['porg_privacy_mobile_intro_4'] = 'Sem forritarar Piwigo-Android og Piwigo-Mobile virðum við (eða „okkur“ eða „okkar“) friðhelgi notenda okkar („notandi“ eða „þú“).';
+$lang['porg_privacy_mobile_intro_5'] = 'Þessi persónuverndarstefna útskýrir hvernig við söfnum, notum, birtum og verndum upplýsingar þínar þegar þú notar smáforritin okkar („forritin“).';
+$lang['porg_privacy_mobile_intro_6'] = 'Hægt er að draga nöfn okkar úr lýsigögnum í git-geymslunum og í þakkarhlutanum í forritunum.';
+$lang['porg_privacy_mobile_intro_7'] = 'Vinsamlegast lesið þessa persónuverndarstefnu vandlega.';
+$lang['porg_privacy_mobile_intro_8'] = 'Ef þú samþykkir ekki skilmála þessarar persónuverndarstefnu skaltu ekki nota forritin.';
+$lang['porg_privacy_mobile_intro_9'] = 'Við áskiljum okkur rétt til að gera breytingar á þessari persónuverndarstefnu hvenær sem er og af hvaða ástæðu sem er.';
+$lang['porg_privacy_mobile_protection_1'] = 'Forritin geyma gögn staðbundið á tækinu sem þau eru keyrð á.';
+$lang['porg_privacy_mobile_protection_2'] = 'Þú berð ábyrgð á að vernda geymd gögn gegn óheimilum aðgangi.';
+$lang['porg_privacy_mobile_protection_3'] = 'Við mælum eindregið með því að nota HTTPS til að vernda gögn við sendingu og forritin reyna að nota dulkóðaðar tengingar yfir HTTPS í fyrsta skipti sem notandinn tengist Piwigo myndasafni.';
+$lang['porg_privacy_mobile_protection_4'] = 'Rekstraraðili Piwigo-þjónsins ber ábyrgð á að halda vottorðum og bókasöfnum fyrir HTTPS-aðgang uppfærðum.';
+$lang['porg_privacy_mobile_protection_5'] = 'Þegar forritin geta ekki notað HTTPS, skipta þau yfir í HTTP og gögn eru ekki dulkóðuð við sendingu.';
+$lang['Geo-Location Information'] = 'Upplýsingar um landfræðilega staðsetningu';
+$lang['How is the data protected?'] = 'Hvernig eru gögnin vernduð?';
+$lang['Last updated on %s'] = 'Síðast uppfært þann %s';
+$lang['Mobile Device Access'] = 'Snjalltækjaaðgengi';
+$lang['Mobile Device Data'] = 'Snjalltækjagögn';
+$lang['Piwigo Gallery Data'] = 'Gögn um Piwigo galleríið';
+$lang['Privacy Policy for Mobile Apps'] = 'Persónuverndarstefna fyrir snjalltækjaforrit';
+$lang['Security of your information'] = 'Öryggi upplýsinga þinna';
+$lang['What data is processed and stored?'] = 'Hvaða gögn eru unnin og geymd?';
+$lang['Why does the mobile app store data?'] = 'Af hverju geymir snjalltækið gögn?';
+$lang['or'] = 'eða';
+$lang['porg_privacy_mobile_banner'] = 'Eftirfarandi er persónuverndarstefna fyrir Piwigo snjalltækjaforrit, bæði á iOS (þ.e. iPhone, iPod eða iPad) og Android (í grundvallaratriðum öllum öðrum snjalltækjum).';
+$lang['porg_privacy_mobile_contact_1'] = 'Ef þú hefur spurningar eða athugasemdir varðandi þessa persónuverndarstefnu, vinsamlegast hafðu samband við okkur hér:';
+$lang['porg_privacy_mobile_data_device_1'] = 'Forritin kunna að óska ​​eftir aðgangi eða leyfi fyrir ákveðnum eiginleikum úr snjalltæknu þínu, þar á meðal myndasafni og skráageymslu snjalltækisins, Android AccountManager, Apple Keychain, erlendum og skýjabundnum geymslumöppum, staðsetningu og öðrum eiginleikum.';
+$lang['porg_privacy_mobile_data_device_2'] = 'Ef þú vilt breyta aðgangi okkar eða heimildum geturðu gert það í stillingum tækisins þíns en hafðu í huga að sum aðgangsskilyrði eru nauðsynleg til að hlaða upp margmiðlunarskrám úr snjalltækinu þínum í Piwigo myndasafnið eða til að geyma Piwigo margmiðlunarskrár í snjalltækinu þínu.';
+$lang['porg_privacy_mobile_data_device_3'] = 'Þegar þú hefur samband við þjónustuteymið geta upplýsingar um tækið þitt, svo sem auðkenni snjalltækisins, gerð og framleiðandi, útgáfa stýrikerfisins, netfang, land, staðsetning og allar aðrar upplýsingar sem þú velur að láta í té, verið sendar til þróunarteymisins fyrir forritin.';
+$lang['porg_privacy_mobile_data_device_4'] = 'Fyrir utan þau gögn sem notandinn hefur sérstaklega deilt með forriturunum mun ekkert efni verða geymt, sent eða unnið úr af okkur.';
+$lang['porg_privacy_mobile_data_geo_1'] = 'Við gætum óskað eftir aðgangi eða leyfi til að rekja staðsetningarupplýsingar úr snjalltækinu þínu, annað hvort stöðugt eða á meðan þú notar forritin, til að veita staðsetningartengda þjónustu.';
+$lang['porg_privacy_mobile_data_geo_2'] = 'Ef þú vilt breyta aðgangi okkar eða heimildum geturðu gert það í stillingum tækisins.';
+$lang['porg_privacy_mobile_data_piwigo_1'] = 'Forritin geyma aðgangsupplýsingar og sendir þær á stilltar síður til að tengjast einu eða fleiri Piwigo myndasöfnum.';
