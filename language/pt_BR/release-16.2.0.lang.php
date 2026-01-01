6432c8dcd0f42bc1dd3aa927aa876bc659638bcf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-01 17:44:55 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2473'] = 'menu suspenso de tags relacionadas vazio';
+$lang['porg_issue_2474'] = 'O representante não foi gerado ao carregar fotos \'.heic\' do Piwigo iOS.';
+$lang['porg_issue_2480'] = 'A detecção de contêineres gera um aviso se `open_basedir` estiver configurado.';
+$lang['porg_issue_2481'] = 'O que há de novo no Piwigo 16 Popin? Ajuste o tamanho da escala ao passar o mouse.';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo retorna os tamanhos das novas imagens derivadas como strings em vez de números inteiros';
+$lang['porg_issue_2486'] = 'Adicionar configuração para páginas padrão';
+$lang['porg_issue_2488'] = '[derivadas] erro ao ativar derivadas desativadas';
+$lang['porg_issue_2490'] = '[tags] notificação de gatilho ausente para tag mesclada';
+$lang['porg_issue_2494'] = 'Erro "[Imagem] extensão de arquivo não suportada" em arquivos TIFF';
