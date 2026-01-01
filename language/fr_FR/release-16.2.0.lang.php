c42e647d5f040742ed6e5a8c0b23c0cbd8a63935
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-01 17:44:58 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2490'] = '[tags] notification de déclencheur manquante pour le mot-clef fusionné';
+$lang['porg_issue_2488'] = 'Erreur [tailles de photos] lors de l\'activation des tailles désactivées';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo renvoie les nouvelles tailles des photos sous forme de chaînes de caractères au lieu de leur valeur.';
+$lang['porg_issue_2494'] = 'Erreur « [Image] extension de fichier non prise en charge » pour les fichiers TIFF';
+$lang['porg_issue_2486'] = 'Ajout d\'une configuration pour les pages standard';
+$lang['porg_issue_2481'] = 'La fenêtre modale "Quoi de neuf" de Piwigo 16 ajuste sa taille au survol';
+$lang['porg_issue_2480'] = 'La détection de conteneur génère un avertissement si open_basedir est défini.';
+$lang['porg_issue_2474'] = 'La miniature n\'est pas générée lorsque des photos « .heic » sont téléversées depuis Piwigo iOS.';
+$lang['porg_issue_2473'] = 'Le menu déroulant des mots-clefs liés est vide';
