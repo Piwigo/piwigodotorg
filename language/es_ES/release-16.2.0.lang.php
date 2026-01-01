3c9f9a90fa09434a67bf8672684ed945ab0e06f4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-01 17:45:03 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2481'] = 'Pop-in de novedades en Piwigo 16, ajustar el tamaño de escala de los textos flotantes ';
+$lang['porg_issue_2484'] = '[API] pwg.images.getinfo devuelve los nuevos tamaños de derivadas como cadenas en lugar de enteros';
+$lang['porg_issue_2486'] = 'Añadida configuración para páginas estándar';
+$lang['porg_issue_2488'] = '[derivadas] error al habilitar devivadas inhabilitadas';
+$lang['porg_issue_2490'] = '[etiquetas] falta un disparador de notificación para etiqueta combinada';
+$lang['porg_issue_2494'] = 'error \"[imagen] extensión de archivo no soportada\" en los archivos TIFF';
+$lang['porg_issue_2473'] = 'lista desplegable de etiquetas relacionadas vacía';
+$lang['porg_issue_2474'] = 'representación no generada cuando se cargan fotos \'.heic\' desde Piwigo iOS';
+$lang['porg_issue_2480'] = 'La detección de contenedores genera un aviso si open_basedir está establecido';
