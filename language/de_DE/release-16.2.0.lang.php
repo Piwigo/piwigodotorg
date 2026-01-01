e6539103d454607116ca9a27e0732e8f7b8c05cc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-01 09:37:49 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2488'] = '[Derivate] Fehler beim Aktivieren deaktivierter Derivate';
+$lang['porg_issue_2490'] = '[Tags] Fehlende Benachrichtigung bei Trigger für zusammengeführte Tags';
+$lang['porg_issue_2494'] = 'Fehler „[Bild] Nicht unterstützte Dateiendung“ bei TIFF-Dateien';
+$lang['porg_issue_2473'] = 'Leere Dropdown-Liste mit verwandten Tags';
+$lang['porg_issue_2474'] = 'Vertreter wird nicht generiert, wenn ‚.heic‘-Fotos von Piwigo iOS hochgeladen werden.';
+$lang['porg_issue_2480'] = 'Containererkennung generiert eine Warnung, wenn open_basedir gesetzt ist';
+$lang['porg_issue_2481'] = 'Was ist neu in Piwigo 16 Popin, Größe beim Hover anpassen';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo gibt neue Derivatgrößen als Zeichenfolgen statt als Ganzzahlen zurück.';
+$lang['porg_issue_2486'] = 'Konfiguration für Standardseiten hinzufügen';
