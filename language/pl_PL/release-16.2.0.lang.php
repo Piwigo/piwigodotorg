0a98dd619350c4eb96abd4f3bc715efc81431f02
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-01 17:45:06 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2474'] = 'Reprezentacja nie wygenerowana po przesłaniu zdjęć \'.heic\' z Piwigo iOS';
+$lang['porg_issue_2494'] = 'Błąd \"[Image] unsupported file extension\" dla plików TIFF';
+$lang['porg_issue_2490'] = '[Tagi] brak powiadomienia o wyzwalaczu dla łączenia tagów';
+$lang['porg_issue_2488'] = '[Pochodne] błąd podczas włączania wyłączonych wcześniej pochodnych';
+$lang['porg_issue_2486'] = 'Dodana konfiguracja dla stron standardowych';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo zwraca nowe rozmiary pochodnych jako ciągi zamiast liczb całkowitych';
+$lang['porg_issue_2481'] = 'Dostosowanie skali wyświetlania powiadomienia o nowościach w Piwigo 16';
+$lang['porg_issue_2480'] = 'Wykrywanie kontenerów generuje ostrzeżenie, jeśli open_basedir jest ustawione';
+$lang['porg_issue_2473'] = 'Pusta lista wyboru powiązanych tagów';
