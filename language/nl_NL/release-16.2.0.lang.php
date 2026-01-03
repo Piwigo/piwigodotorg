4496ef53eda2e0eddb89008d11a2f53d39b7af8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-03 07:31:46 +0100

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2473'] = 'lege dropdownlijst met gerelateerde tags';
+$lang['porg_issue_2474'] = 'Er wordt geen representatief voorbeeld gegenereerd wanneer \'.heic\'-foto\'s worden geüpload vanaf Piwigo iOS.';
+$lang['porg_issue_2480'] = 'Containerdetectie genereert een waarschuwing als open_basedir is ingesteld.';
+$lang['porg_issue_2481'] = 'Wat is er nieuw in Piwigo 16 Popin? Pas de hover-schaalgrootte aan.';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo geeft de afmetingen van nieuwe afgeleiden als tekenreeksen in plaats van gehele getallen.';
+$lang['porg_issue_2486'] = 'Voeg configuratie toe voor standaardpagina\'s';
+$lang['porg_issue_2488'] = '[afgeleiden] fout bij het inschakelen van uitgeschakelde afgeleiden';
+$lang['porg_issue_2490'] = '[tags] ontbrekende trigger melding voor samengevoegde tag';
+$lang['porg_issue_2494'] = '"[Image] niet-ondersteunde bestandsextensie" foutmelding bij TIFF-bestanden';
