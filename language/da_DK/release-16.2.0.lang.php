5004d40c2cd4ef23047b9c6a74308a5a82828e27
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-03 07:31:43 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2473'] = 'tom dropdown med relaterede tags';
+$lang['porg_issue_2474'] = 'miniturebillede ikke genereret når \'.heic\'-fotografier overføres fra Piwigo iOS';
+$lang['porg_issue_2480'] = 'Container-genkendelse medfører en advarsel hvis open_basedir er opsat';
+$lang['porg_issue_2481'] = 'Piwigo 16\'s "what\'s new"-popin, justering af hover-skalering';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo returnerer nye afledte billeders størrelser som strenge i stedet for heltal';
+$lang['porg_issue_2486'] = 'Tilføj opsætning af standardsider';
+$lang['porg_issue_2488'] = '[afledte billeder] fejl ved aktivering af deaktiverede afledte billeder';
+$lang['porg_issue_2490'] = '[tags] manglende trigger-besked ved sammenlagt tag';
+$lang['porg_issue_2494'] = '\"[Image] unsupported file extension\"-fejl ved TIFF-filer';
