4875c428647aae36dd98c72c6cee0c737f3bda20
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-05 06:24:02 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2473'] = '清空相关标签下拉菜单';
+$lang['porg_issue_2474'] = '从 Piwigo iOS 端上传 .heic 格式照片时，未生成封面图';
+$lang['porg_issue_2480'] = '若开启 open_basedir 限制，容器检测功能会触发警告';
+$lang['porg_issue_2481'] = 'Piwigo 16 新功能弹窗，调整悬浮缩放尺寸';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo 接口返回的新衍生图尺寸为字符串类型（而非整数）';
+$lang['porg_issue_2486'] = '新增标准页面相关配置项';
+$lang['porg_issue_2488'] = '[衍生图] 启用已禁用的衍生图尺寸时触发错误';
+$lang['porg_issue_2490'] = '[标签] 合并标签时缺失通知触发机制';
+$lang['porg_issue_2494'] = 'TIFF文件上的“[图像] 不支持的文件扩展名”错误';
