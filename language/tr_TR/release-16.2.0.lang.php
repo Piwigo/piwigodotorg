c1266b385c2338039685c9d12c07579c09d967f2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-07 06:55:17 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2494'] = 'TIFF dosyalarında \"[Resim] desteklenmeyen dosya uzantısı\" hatası';
+$lang['porg_issue_2490'] = '[etiketler] birleştirilmiş etiket için eksik tetikleyici bildirimi';
+$lang['porg_issue_2488'] = '[Türevler] devre dışı bırakılmış türevleri etkinleştirirken hata oluştu';
+$lang['porg_issue_2486'] = 'Standart sayfalar için yapılandırma ekleyin';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo yeni türev boyutlarını tamsayılar yerine dizeler olarak döndürüyor';
+$lang['porg_issue_2481'] = 'Piwigo 16 Popin\'de neler yeni, fareyle üzerine gelindiğinde ölçek boyutunu ayarlama özelliği var.';
+$lang['porg_issue_2480'] = 'Konteyner algılama, open_basedir ayarı etkinleştirilmişse uyarı oluşturur.';
+$lang['porg_issue_2474'] = 'Piwigo iOS uygulamasından \'.heic\' fotoğraflar yüklendiğinde temsilci oluşturulmuyor.';
+$lang['porg_issue_2473'] = 'boş ilgili etiketler açılır menüsü';
