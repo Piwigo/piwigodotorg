c3d6e421dd1b435059ce9040b411b035c3b947a9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-20 14:02:22 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/release-16.2.0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2473'] = 'Il menu a discesa per le parole chiave correlate è vuoto.';
+$lang['porg_issue_2474'] = 'La miniatura non viene generata quando le foto ".heic" vengono caricate da Piwigo iOS.';
+$lang['porg_issue_2480'] = 'Il rilevamento del contenitore genera un avviso se è definito open_basedir.';
+$lang['porg_issue_2481'] = 'La finestra modale "Novità" di Piwigo 16 regola le sue dimensioni al passaggio del mouse.';
+$lang['porg_issue_2484'] = '[API] pwg.images.getInfo restituisce le nuove dimensioni delle foto come stringhe anziché come valore.';
+$lang['porg_issue_2486'] = 'Aggiunta una configurazione per le pagine standard';
+$lang['porg_issue_2488'] = 'Errore [dimensioni foto] durante l\'abilitazione delle dimensioni disabilitate';
+$lang['porg_issue_2490'] = '[tag] Notifica di attivazione mancante per la parola chiave fusionata';
+$lang['porg_issue_2494'] = 'Erreur « [Image] extension de fichier non prise en charge » pour les fichiers TIFF';
