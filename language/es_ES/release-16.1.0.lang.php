b3fa283445da0244052f9258d341ac7b4f073572
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-31 17:39:31 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-16.1.0.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2458'] = '16.0.0, PHP 7.4] carga de imagen: Llamada a la función no definida str_starts_with()';
+$lang['porg_issue_2460'] = 'El uso de la autenticación básica HTTP en Nginx devuelve {\"stat\":\"fail\",\"err\":401,\"message\":\"Clave de API no válida\"}';
+$lang['porg_issue_2461'] = 'indicar a las plantillas si las páginas estándar están activas o no';
+$lang['porg_issue_2462'] = 'Problema externo de ImageMagick - Piwigo 16';
+$lang['porg_issue_2467'] = 'Error de <i>cast</i> a entero, compatibilidad con PHP 8.5';
