be3cd6d9b44eb4f093d277377202fe6eefdd4659
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-11 22:00:56 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/docker_update.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_guides_img_docker_update_pwg_higher_16_3'] = 'Dla kontenera w wersji 16.3 i wyższej';
+$lang['porg_guides_img_docker_update_official_disclaimer_2'] = 'Jeśli chcesz przełączyć się na oficjalny obraz, możesz <a href="%s">postępować zgodnie z tym przewodnikiem</a>';
+$lang['porg_guides_img_docker_update_official_disclaimer_1'] = 'Ten przewodnik dotyczy tylko oficjalnego obrazu Piwigo, jeśli używasz kontenera serwera Linux, skorzystaj raczej z ich dokumentacji.';
