9dc4d459bb189daedbdfae180a718569627a323a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-22 10:07:53 +0100

    Update pl_PL, thanks to yareq

--- language/pl_PL/docker_update.lang.php
+++ language/pl_PL/docker_update.lang.php
@@ -8,3 +8,11 @@
 $lang['porg_guides_img_docker_update_pwg_higher_16_3'] = 'Dla kontenera w wersji 16.3 i wyższej';
 $lang['porg_guides_img_docker_update_official_disclaimer_2'] = 'Jeśli chcesz przełączyć się na oficjalny obraz, możesz <a href="%s">postępować zgodnie z tym przewodnikiem</a>';
 $lang['porg_guides_img_docker_update_official_disclaimer_1'] = 'Ten przewodnik dotyczy tylko oficjalnego obrazu Piwigo, jeśli używasz kontenera serwera Linux, skorzystaj raczej z ich dokumentacji.';
+$lang['porg_guides_img_docker_update_step2_db'] = 'Kopia zapasowa bazy danych';
+$lang['porg_guides_img_docker_update_step2_files'] = 'Kopia zapasowa plików';
+$lang['porg_guides_img_docker_update_pwg_lower_16_3'] = 'Dla kontenera w wersji 16.2 i niższej';
+$lang['porg_guides_img_docker_update_step4'] = 'Krok 4 - aktualizacja i ponowne uruchomienie kontenera';
+$lang['porg_guides_img_docker_update_step3'] = 'Krok 3 - pobranie nowego pliku składu i aktualizacja <code>.env</code>';
+$lang['porg_guides_img_docker_update_step2'] = 'Krok 2 - utworzenie kopii zapasowej';
+$lang['porg_guides_img_docker_update_step1'] = 'Krok 1 - sprawdzanie aktualizacji';
+$lang['porg_guides_img_docker_update_pwg_step4_paragraph_2'] = 'Po potwierdzeniu, że wszystko działa, możesz odłączyć terminal, naciskając <code>d</code>';
