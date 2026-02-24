cabb6aa9bd8117ee6c321a1e1cbb8510ab3f2b96
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-24 21:26:53 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'SQL-Injection in Activity.getList';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Unauthentifizierte Offenlegung von Informationen über die pwg.history.search-API';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'SQL-Injection in der API-Methode pwg.users.getList über den Filterparameter';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'Vorab-Autorisierung SQL-Injection über Datumsfilterparameter in ws_std_image_sql_filter';
+$lang['porg_issue_2522'] = '[Standardseiten] Die Meldung zum erfolgreichen Zurücksetzen des Passworts ist irreführend.';
+$lang['porg_issue_2525'] = 'Fehlende Übersetzung im Gruppenauswahlmodus';
+$lang['porg_issue_2528'] = 'Fehlendes Symbol zum Entfernen bei ausgewählten verwandten Tags';
+$lang['porg_issue_2517'] = 'Standardseiten Sprachumschaltung läuft über';
+$lang['porg_issue_2518'] = 'Standardseiten Responsive Design nicht gut';
+$lang['porg_issue_2519'] = '[SICHERHEIT] CSRF-Sicherheitslücke beim Versenden von E-Mails aus dem Album';
+$lang['porg_issue_2508'] = 'Standardseiten verwenden das Standard-Theme header.tpl.';
+$lang['porg_issue_2509'] = 'Falsche Schaltflächenbeschriftung in site_update.tpl';
+$lang['porg_issue_2510'] = '[16.x] stripslashes(): Die Übergabe von null an Parameter #1 ($string) vom Typ string ist in pwg.comments.php veraltet.';
+$lang['porg_issue_2499'] = 'Die Überprüfung auf neue Piwigo-Versionen generiert zu viele Anfragen auf piwigo.org.';
+$lang['porg_issue_2501'] = 'Fügen Sie eine detailliertere Containererkennung hinzu.';
+$lang['porg_issue_2503'] = 'Schaltfläche für automatische Aktualisierung nicht sichtbar';
+$lang['porg_issue_2507'] = 'Benachrichtigung zum Album, Fehler in der E-Mail-Vorlage';
