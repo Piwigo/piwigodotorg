4b44129880c323b27ef9b033eb666cb67c1ef76f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-24 21:26:50 +0100

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2517'] = 'Le changement de langue des pages standard déborde';
+$lang['porg_issue_2503'] = 'Bouton de mise à jour automatique non visible';
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'Injection SQL dans Activity.getList';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'Injection SQL pré-authentification via les paramètres du filtre de date dans ws_std_image_sql_filter';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'Injection SQL dans la méthode API pwg.users.getList via le paramètre filter';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Divulgation d\'informations non authentifiées via l\'API pwg.history.search';
+$lang['porg_issue_2528'] = 'Icône de suppression manquante sur les mots-clefs associés sélectionnés';
+$lang['porg_issue_2525'] = 'Traduction manquante en mode sélection de groupes';
+$lang['porg_issue_2522'] = '[pages standard] Le message indiquant que la réinitialisation du mot de passe a été effectuée avec succès est trompeur.';
+$lang['porg_issue_2519'] = '[SÉCURITÉ] Vulnérabilité CSRF (attaque de falsification de requête inter-sites) dans l\'envoi d\'e-mails d\'album';
+$lang['porg_issue_2518'] = 'Le design responsive des pages standard n\'est pas satisfaisant.';
+$lang['porg_issue_2510'] = '[16.x] stripslashes() : passer la valeur null au paramètre #1($string) de type chaîne de caractères est obsolète dans pwg.comments.php.';
+$lang['porg_issue_2509'] = 'Étiquette de bouton erronée dans site_update.tpl';
+$lang['porg_issue_2508'] = 'Les pages standard utilisent header.tpl du thème par défaut.';
+$lang['porg_issue_2507'] = 'Notification d\'album, erreur de template dans l\'e-mail envoyé';
+$lang['porg_issue_2501'] = 'Ajoute une détection plus granulaire des conteneurs';
+$lang['porg_issue_2499'] = 'la vérification des nouvelles versions de Piwigo génère trop de requêtes sur piwigo.org.';
