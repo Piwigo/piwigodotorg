aa5510df1ecde6ccf1730b0517a84ceb1408b8b1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 06:27:58 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2499'] = 'La comprobación de nuevas versiones de Piwigo generaba demasiadas solicitudes en piwigo.org';
+$lang['porg_issue_2501'] = 'Añadida una detección de contenedores más granular';
+$lang['porg_issue_2503'] = 'Botón de actualización automática no visible';
+$lang['porg_issue_2507'] = 'Notificación de álbum: error de plantilla en el correo electrónico enviado';
+$lang['porg_issue_2508'] = 'Las páginas estándar usan el tema predeterminado header.tpl';
+$lang['porg_issue_2509'] = 'Etiqueta de botón incorrecta en site_update.tpl';
+$lang['porg_issue_2510'] = '[16.x] stripslashes(): Pasar un valor nulo al primer parámetro ($string) de tipo cadena es obsoleto en pwg.comments.php';
+$lang['porg_issue_2517'] = 'Desbordamientos en el cambio de idioma de las páginas estándar';
+$lang['porg_issue_2518'] = 'El diseño responsivo de las páginas estándar no era correcto';
+$lang['porg_issue_2519'] = '[SEGURIDAD] Vulnerabilidad CSRF en el envío de álbumes por correo electrónico.';
+$lang['porg_issue_2522'] = '[Páginas estándar] El mensaje de restablecimiento de contraseña era confuso';
+$lang['porg_issue_2525'] = 'Faltaba traducción en el modo de selección de grupos';
+$lang['porg_issue_2528'] = 'Falta el icono de eliminar en las etiquetas relacionadas seleccionadas';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Divulgación de información sin autenticación mediante la API pwg.history.search';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'Inyección SQL en el método de la API pwg.users.getList mediante el parámetro de filtro';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'Inyección SQL previa a la autenticación mediante los parámetros de filtro de fecha en ws_std_image_sql_filter';
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'Inyección SQL en Activity.getList';
