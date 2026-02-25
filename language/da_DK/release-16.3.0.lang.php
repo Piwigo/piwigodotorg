d79e10dec7882ffbbbb71a4813256893aad8a75c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 08:18:39 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2499'] = 'Kig efter nye versioner af Piwigo genererer for mange forespørgsler til piwigo.org';
+$lang['porg_issue_2501'] = 'Tilføjer en mere detaljeret container-genkendelse';
+$lang['porg_issue_2503'] = 'Knap til automatisk opdatering ikke synlig';
+$lang['porg_issue_2507'] = 'Albumnotifikation, skabelonfejl i sendt mail';
+$lang['porg_issue_2508'] = '[Standardsider] anvender standardtemaets header.tpl';
+$lang['porg_issue_2509'] = 'Forkert knaptekst i site_update.tpl';
+$lang['porg_issue_2510'] = '[16.x] stripslashes(): Overførsel af null til parameter #1 ($string) af typen string er forældret i pwg.comments.php';
+$lang['porg_issue_2517'] = '[Standardsider] sprogskifter flyder over';
+$lang['porg_issue_2518'] = '[Standardsider] responsive design er ikke godt';
+$lang['porg_issue_2519'] = '[SIKKERHED] CSRF-sårbarhed i afsendelse af albummail';
+$lang['porg_issue_2522'] = '[Standardsider] Besked om vellykket nulstilling af adgangskode er misvisende';
+$lang['porg_issue_2525'] = 'Manglende oversættelse i gruppevalg-tilstand';
+$lang['porg_issue_2528'] = 'Manglende fjernikon på valgte relaterede tags';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Uautentificeret blotlæggelse af information via pwg.history.search-API';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'SQL-indsprøjtning i pwg.users.getList-API-metode via filterparameter';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'SQL-indsprøjtning før autorisation via datafilters parametre i ws_std_image_sql_filter';
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'SQL-indsprøjtning i Activity.getList';
