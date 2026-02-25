ed8127978230b8bae3e663c96f6a88f6bf800408
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 16:07:11 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2525'] = 'Tradução ausente no modo de seleção de grupos';
+$lang['porg_issue_2528'] = 'Ícone de remoção ausente nas tags relacionadas selecionadas';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Divulgação de informações não autenticadas por meio da API pwg.history.search';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'Injeção de SQL no método da API pwg.users.getList por meio do parâmetro filter';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'Injeção SQL pré-autenticada por meio de parâmetros de filtro de data em ws_std_image_sql_filter';
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'Injeção de SQL em Activity.getList';
+$lang['porg_issue_2522'] = '[páginas padrão] A mensagem de sucesso na redefinição de senha é enganosa.';
+$lang['porg_issue_2519'] = '[SEGURANÇA] Vulnerabilidade CSRF no envio de e-mails de álbuns';
+$lang['porg_issue_2518'] = 'O design responsivo das páginas padrão não é bom.';
+$lang['porg_issue_2517'] = 'Transbordamento de troca de idioma de páginas padrão';
+$lang['porg_issue_2510'] = '[16.x] stripslashes(): Passar null para o parâmetro #1 ($string) do tipo string está obsoleto em pwg.comments.php';
+$lang['porg_issue_2509'] = 'Rótulo de botão incorreto em site_update.tpl';
+$lang['porg_issue_2508'] = 'As páginas padrão usam o cabeçalho do tema padrão (header.tpl).';
+$lang['porg_issue_2507'] = 'Notificação de álbum, erro de modelo no e-mail enviado';
+$lang['porg_issue_2503'] = 'O botão de atualização automática não está visível.';
+$lang['porg_issue_2501'] = 'Adicionar detecção de contêineres mais granular';
+$lang['porg_issue_2499'] = 'A verificação de novas versões do Piwigo gera muitas solicitações em piwigo.org.';
