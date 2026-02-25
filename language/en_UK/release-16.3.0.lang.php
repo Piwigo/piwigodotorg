8ca2b286a8a686c05e5e552fea9065db02475753
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 14:03:05 +0100

    Update en_UK, thanks to ddtddt

--- language/en_UK/release-16.3.0.lang.php
+++ language/en_UK/release-16.3.0.lang.php
@@ -3,7 +3,7 @@ $lang['porg_issue_2528'] = 'Missing remove icon on selected related tags';
 $lang['porg_issue_2525'] = 'Missing translation in groups selection mode';
 $lang['porg_issue_2522'] = '[standard pages] password reset success message is misleading';
 $lang['porg_issue_2519'] = '[SECURITY] CSRF vulnerability in album email sending';
-$lang['porg_issue_2518'] = 'Standard pages reponsive design not good';
+$lang['porg_issue_2518'] = 'Standard pages responsive design not good';
 $lang['porg_issue_2517'] = 'Standard pages language switch overflows';
 $lang['porg_issue_2510'] = '[16.x] stripslashes(): Passing null to parameter #1 ($string) of type string is deprecated in pwg.comments.php';
 $lang['porg_issue_2509'] = 'wrong button label in site_update.tpl';
@@ -17,4 +17,3 @@ $lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'Pre-auth SQL injection via date filte
 $lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Unauthenticated Information Disclosure via pwg.history.search API';
 $lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'SQL Injection in pwg.users.getList API Method via filter Parameter';
 $lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'SQL Injection in Activity.getList';
-?>
