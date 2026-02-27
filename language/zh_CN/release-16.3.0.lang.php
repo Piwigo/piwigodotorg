d5d035a40338fabc1d407f24451d99a5eb9b6e4a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-27 04:01:11 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2499'] = '检查 Piwigo 新版本时，向piwigo.org发起的请求量过多';
+$lang['porg_issue_2501'] = '新增更细粒度的容器检测功能';
+$lang['porg_issue_2503'] = '自动更新按钮未显示';
+$lang['porg_issue_2507'] = '相册通知：发送的邮件中存在模板错误';
+$lang['porg_issue_2508'] = '标准页面使用了默认主题的 header.tpl 模板';
+$lang['porg_issue_2509'] = 'site_update.tpl 中按钮标签错误';
+$lang['porg_issue_2510'] = '[16.x 版本] pwg.comments.php 中 stripslashes () 函数：向类型为字符串的参数 #1（$string）传入 null 已被弃用';
+$lang['porg_issue_2517'] = '标准页面的语言切换控件溢出（显示不全）';
+$lang['porg_issue_2518'] = '标准页面的响应式设计效果不佳';
+$lang['porg_issue_2519'] = '[安全问题] 相册邮件发送功能存在 CSRF 漏洞';
+$lang['porg_issue_2522'] = '[标准页面] 密码重置成功提示语易产生误导';
+$lang['porg_issue_2525'] = '分组选择模式下存在未翻译的文案';
+$lang['porg_issue_2528'] = '已选中的关联标签缺少删除图标';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = '未授权用户可通过 pwg.history.search API 泄露信息';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = '攻击者可通过 filter 参数对 pwg.users.getList API 方法实施 SQL 注入';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = '未授权状态下，攻击者可通过 date 过滤参数对 ws_std_image_sql_filter 实施 SQL 注入';
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'Activity.getList 接口存在 SQL 注入漏洞';
