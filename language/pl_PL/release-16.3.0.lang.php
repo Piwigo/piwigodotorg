4421f810cc7f6925ab8764f6e9f89c381b379c81
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-28 05:59:55 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-16.3.0.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2499'] = 'Sprawdzanie dostępności nowej wersji Piwigo generuje zbyt wiele żądań na piwigo.org';
