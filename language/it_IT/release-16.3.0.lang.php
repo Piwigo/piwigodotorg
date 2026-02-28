aa2016bb62809afb316d7834ee033f66b6d978cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-28 05:59:58 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-16.3.0.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2522'] = '[pagine standard] il messaggio di avvenuto ripristino della password è fuorviante';
+$lang['porg_issue_2525'] = 'Traduzione mancante nella modalità di selezione dei gruppi';
+$lang['porg_issue_2528'] = 'Manca l’icona per rimuovere i tag correlati selezionati';
+$lang['porg_issue_GHSA-397m-gfhm-pmg2'] = 'Divulgazione di informazioni non autenticate tramite API pwg.history.search';
+$lang['porg_issue_GHSA-5jwg-cr5q-vjq2'] = 'SQL Injection nel metodo API pwg.users.getList tramite parametro filtro';
+$lang['porg_issue_GHSA-mgqc-3445-qghq'] = 'SQL Injection precedente l\'autenticazione tramite parametri di filtro data in ws_std_image_sql_filter';
+$lang['porg_issue_GHSA-wfmr-9hg8-jh3m'] = 'SQL Injection in Activity.getList';
+$lang['porg_issue_2499'] = 'il controllo delle nuove versioni di Piwigo genera troppe richieste su piwigo.org';
+$lang['porg_issue_2501'] = 'Add a more granular container detection';
+$lang['porg_issue_2503'] = 'Pulsante di aggiornamento automatico non visibile';
+$lang['porg_issue_2507'] = 'notifica album, errore del modello nell\'e-mail inviata';
+$lang['porg_issue_2508'] = 'Le pagine standard utilizzano il tema predefinito header.tpl';
+$lang['porg_issue_2509'] = 'etichetta del pulsante errata in site_update.tpl';
+$lang['porg_issue_2510'] = '[16.x] stripslashes(): passare di null al parametro #1 ($string) di tipo stringa è deprecato in pwg.comments.php';
+$lang['porg_issue_2517'] = 'Il cambio lingua nelle pagine standard supera lo spazio disponibile';
+$lang['porg_issue_2518'] = 'Il design adattivo delle pagine standard non è sufficiente';
+$lang['porg_issue_2519'] = '[SICUREZZA] Vulnerabilità CSRF nell’invio email dell’album';
