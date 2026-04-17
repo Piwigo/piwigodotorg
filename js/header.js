79d8597b1d7f00805e27b4afb9063f576c9fee3c
Zahra Salou <zahrasalou28@gmail.com>
2026-04-17 09:58:41 +0200

    header version mobile almost finished ( need slight modification about the position of the submenus )

--- js/header.js
+++ js/header.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-   
     $('.nav-btn-wrapper .submenu_button').on('click', function(e) {
         if ($(window).width() <= 800) {
-            e.preventDefault(); 
+            e.preventDefault();
+            e.stopPropagation();
             
-            
-            var subContent = $(this).next('.mobile-sub-content');
-            
-            
-            subContent.slideToggle();
-            
-           
-            $(this).find('.icon-down-open').toggleClass('icon-rotate-180');
+            var $this = $(this);
+            var subContent = $this.next('.mobile-sub-content');
+            var arrow = $this.find('.icon-down-open');
+            subContent.slideToggle(300);
+            arrow.toggleClass('icon-rotate-180');
+
+            $('.mobile-sub-content').not(subContent).slideUp(300);
+            $('.icon-down-open').not(arrow).removeClass('icon-rotate-180');
         }
     });
 });
