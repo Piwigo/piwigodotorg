f93db2737a02b6d89863d5c617a63fed8537e9c4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-04 06:55:52 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/release-16.4.0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2369'] = 'Erro de divisão por zero';
+$lang['porg_issue_2478'] = 'A data de criação da chave da API não está visível.';
+$lang['porg_issue_2531'] = 'A configuração de páginas padrão desaparece.';
+$lang['porg_issue_2538'] = '[mail] diferenciar de e responder a';
+$lang['porg_issue_2539'] = '[API] Defina o pwg_token automaticamente para solicitações de api_key
+';
+$lang['porg_issue_2544'] = '[API] Defina o pwg_token automaticamente para solicitações de api_key
+';
+$lang['porg_issue_2553'] = 'A atualização do plugin através da interface do usuário pode executar a ação de atualização do plugin da versão anterior.';
+$lang['porg_issue_2555'] = '[usuários] O usuário genérico não pode gerar o link de redefinição de senha, mas a interface do usuário sugere o contrário.';
+$lang['porg_issue_796'] = 'A API pwg.images.getInfo retorna URLs incorretas com $conf[\'original_url_protection\']';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'Gerenciador de lotes: verificar entrada do usuário';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'Configuração de páginas padrão: verificação mais robusta do tipo de arquivo de logotipo.';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'nstalação: verificação de integridade do prefixo da tabela durante a instalação
+';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'Envio de fotos: certifique-se de que os arquivos SVG tenham a extensão de nome de arquivo apropriada.';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'Ordem de classificação das fotos do álbum: verificar a entrada do usuário antes de usar';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'Impedir a exibição do conteúdo bruto dos cookies.';
