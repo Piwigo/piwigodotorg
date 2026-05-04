ed45a5fec2cad2a328c6ad59afe7e16b2641b9d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-04 06:55:55 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/release-16.4.0.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'orden de clasificación de las fotos del álbum: verificar la entrada del usuario antes de usarla';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'carga de fotos: asegurarse de que los archivos SVG tengan la extensión de archivo adecuada';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'instalación: comprobación de integridad del prefijo de la tabla durante la instalación';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'configuración de páginas estándar: comprobación más robusta del tipo de archivo del logo';
+$lang['porg_issue_2544'] = '[usuarios] El diálogo modal para añadir usuarios es demasiado grande y algunas traducciones no son correctas';
+$lang['porg_issue_2539'] = '[API] Establecer automáticamente pwg_token para las solicitudes de clave de API';
+$lang['porg_issue_2369'] = 'Error de división por cero';
+$lang['porg_issue_2478'] = 'Fecha de creación de la clave de API no visible';
+$lang['porg_issue_2531'] = 'La configuración de páginas estándar desaparece';
+$lang['porg_issue_2538'] = '[correo] Diferenciar entre remitente y destinatario';
+$lang['porg_issue_2553'] = 'La actualización del plugin a través de la interfaz de usuario puede realizar la acción de actualización de conector correspondiente a la versión anterior';
+$lang['porg_issue_2555'] = '[usuarios] El usuario genérico no puede generar un enlace de restablecimiento de contraseña, pero la interfaz de usuario da a entender lo contrario';
+$lang['porg_issue_796'] = 'La API pwg.images.getInfo devuelve URL incorrectas con $conf[\'original_url_protection\']';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'gestor de lotes: comprobar la entrada del usuario';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'Evitar la visualización del contenido en bruto de las cookies';
