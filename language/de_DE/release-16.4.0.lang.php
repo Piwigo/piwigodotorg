5c7070ce8f66c93ccfd8015a3516bb544aba836a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-04 12:15:18 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/release-16.4.0.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'Foto hochladen: Achten Sie darauf, dass SVG-Dateien die richtige Dateiendung haben';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'Sortierreihenfolge der Albumfotos: Benutzereingabe vor der Verwendung überprüfen';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'Verhindern, dass der Inhalt von RAW-Cookies angezeigt wird';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'Standardseitenkonfiguration: Gründlichere Überprüfung des Dateityps der Logodatei';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'installieren: Überprüfung des Tabellenpräfixes während der Installation';
+$lang['porg_issue_2555'] = '[Benutzer] Ein normaler Benutzer kann keinen Link zum Zurücksetzen des Passworts erstellen, obwohl die Benutzeroberfläche etwas anderes vermuten lässt';
+$lang['porg_issue_796'] = 'Die API-Funktion `pwg.images.getInfo` gibt bei Verwendung von `$conf[‚original_url_protection‘]` falsche URLs zurück';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'Batch-Manager: Benutzereingaben prüfen';
+$lang['porg_issue_2544'] = '[Benutzer] Das Modalfenster zum Hinzufügen von Benutzern ist zu groß und enthält einige fehlerhafte Übersetzungen';
+$lang['porg_issue_2553'] = 'Die Aktualisierung des Plugins über die Benutzeroberfläche führt möglicherweise die Aktualisierungsaktion der vorherigen Version aus';
+$lang['porg_issue_2538'] = '[E-Mail] „Von“ und „Antwort an“ unterscheiden';
+$lang['porg_issue_2539'] = '[API] pwg_token bei Anfragen mit api_key automatisch setzen';
+$lang['porg_issue_2369'] = 'Fehler bei der Division durch Null';
+$lang['porg_issue_2478'] = 'Das Erstellungsdatum des API-Schlüssels ist nicht sichtbar';
+$lang['porg_issue_2531'] = 'Die Standardseiten-Konfigurationsdatei verschwindet';
