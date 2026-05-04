0004e159f1601d78c94dbb09e87afee30c306276
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-04 12:15:22 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/release-16.4.0.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2369'] = 'Errore divisione per zero';
+$lang['porg_issue_2478'] = 'Data creazione chiave API non visibile';
+$lang['porg_issue_2531'] = 'Configurazione delle pagine standard sparisce';
+$lang['porg_issue_2538'] = '[mail] differenziata per i campi Da e Rispondi A';
+$lang['porg_issue_2539'] = 'Imposta automaticamente pwg_token per le richieste api_key';
+$lang['porg_issue_2544'] = '[utenti] la finestra di dialogo per l’aggiunta di un utente è troppo grande e presenta alcune traduzioni errate';
+$lang['porg_issue_2553'] = 'L\'aggiornamento del plugin tramite l\'interfaccia utente potrebbe eseguire l\'azione di aggiornamento del plugin della versione precedente';
+$lang['porg_issue_2555'] = '[utenti] L\'utente generico non può generare il link per il ripristino della password, ma l\'interfaccia utente suggerisce l\'opposto.';
+$lang['porg_issue_796'] = 'L\'API pwg.images.getInfo restituisce URL errati con $conf[\'original_url_protection\']';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'batch manager: verifica l\'input dell\'utente';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'configurazione delle pagine standard: controllo più robusto del tipo di file del logo';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'installazione: controllo di integrità del prefisso della tabella durante l\'installazione';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'Caricamento foto: assicurati che i file SVG abbiano l\'estensione del nome file appropriata';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'Ordine di visualizzazione delle foto dell\'album: verifica l\'input dell\'utente prima dell\'utilizzo';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'impedire la visualizzazione del contenuto dei cookie RAW';
