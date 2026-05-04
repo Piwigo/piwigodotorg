231a90dae174e6ecc5f6dc4b4c8cc2edeaf41987
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-04 12:15:15 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/release-16.4.0.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2539'] = '[API] Automatyczne ustawianie pwg_token dla żądań api_key';
+$lang['porg_issue_2544'] = '[użytkownicy] okno dodawania użytkownika jest zbyt duże i zawiera złe tłumaczenia';
+$lang['porg_issue_2553'] = 'aktualizacja wtyczki przez interfejs użytkownika może wykonać akcję aktualizacji wtyczki poprzedniej wersji';
+$lang['porg_issue_2555'] = '[Użytkownik] użytkownik ogólny nie może wygenerować linku do resetowania hasła, ale interfejs użytkownika sugeruje inaczej';
+$lang['porg_issue_796'] = 'API pwg.images.getInfo zwraca nieprawidłowe adresy URL z $conf[\'original_url_protection\']';
+$lang['porg_issue_GHSA-6wj3-7fhw-gfpm'] = 'aktualizacja: wstrzykiwanie SQL';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'przetwarzanie wsadowe: sprawdzenie wyborów użytkownika';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'standardowa konfiguracja stron: pewniejsze sprawdzenie typu pliku logo';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'instalacja: weryfikacja prefiksu tabeli podczas instalacji';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'przesyłanie zdjęć: upewnienie się, że pliki SVG mają odpowiednie rozszerzenie nazwy pliku';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'Kolejność sortowania zdjęć albumu: sprawdź wybór użytkownika przed użyciem';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'Ciasteczko dla zapobiegania wyświetlania zawartości plików RAW';
+$lang['porg_issue_2369'] = 'Błąd dzielenia przez zero';
+$lang['porg_issue_2478'] = 'Niewidoczna data utworzenia klucza API';
+$lang['porg_issue_2531'] = 'Znikająca standardowa konfiguracja stron';
+$lang['porg_issue_2538'] = '[poczta] rozróżnienie pól „od” i „odpowiedz”';
