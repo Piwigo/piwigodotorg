0acbe733e6886d6d853aadcbacb27d01b2a15479
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-04 06:55:59 +0200

    Update fr_FR, thanks to Katryne

---
+++ language/fr_FR/release-16.4.0.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_796'] = 'L\'API pwg.images.getInfo renvoie des URLs incorrectes avec $conf[\'original_url_protection\']';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'empêcher l\'affichage du contenu brut des cookies';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'Ordre de tri des photos de l\'album : vérification des données saisies par l\'utilisateur avant leur utilisation';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'Téléchargement de photos : vérifier que les fichiers SVG portent l\'extension appropriée';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'installation : vérification de cohérence du préfixe de table lors de l\'installation';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'Configuration des pages standard : vérification plus rigoureuse du type de fichier du logo';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'Gestion par lot : vérifier les données saisies par l\'utilisateur';
+$lang['porg_issue_2555'] = '[utilisateurs] Un utilisateur générique ne peut pas générer de lien de réinitialisation de mot de passe, mais l\'interface utilisateur laisse entendre le contraire';
+$lang['porg_issue_2553'] = 'La mise à jour du plugin via l\'interface utilisateur peut entraîner l\'exécution de l\'action de mise à jour du plugin de la version précédente';
+$lang['porg_issue_2544'] = '[utilisateurs] La fenêtre contextuelle d\'ajout d\'utilisateur est trop grande et comporte des erreurs de traduction';
+$lang['porg_issue_2539'] = '[API] Définir automatiquement pwg_token pour les requêtes api_key';
+$lang['porg_issue_2538'] = '[e-mail] différencier de et réponse à';
+$lang['porg_issue_2531'] = 'La configuration des pages standard disparait';
+$lang['porg_issue_2369'] = 'Erreur de division par zéro';
+$lang['porg_issue_2478'] = 'La date de création de la clé API n\'est pas visible';
