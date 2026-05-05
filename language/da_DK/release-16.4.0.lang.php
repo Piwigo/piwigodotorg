10c726a59231451fbfe59b1337da8ea163c58856
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-05 07:35:41 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/release-16.4.0.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2369'] = 'Division med nul-fejl';
+$lang['porg_issue_2478'] = 'API-keys oprettelsesdato ikke synlig';
+$lang['porg_issue_2531'] = 'Standardsiders opsætning forsvinder';
+$lang['porg_issue_2538'] = '[Mail] gør forskel på "from" og "reply-to"';
+$lang['porg_issue_2539'] = '[API] Opsæt automatisk pwg_token ved api_key-forespørgsler';
+$lang['porg_issue_2544'] = '[Brugere] den modale formular til tilføjelse af brugere er for stor, samt nogle dårlige oversættelser';
+$lang['porg_issue_2553'] = 'Plugin-opdatering gennem brugergrænsefladen kan udføre den tidligere versions plugin-opdateringshandling';
+$lang['porg_issue_2555'] = '[Brugere] generisk bruger kan ikke generere link til nulstilling af adgangskode, men brugergrænsefladen viser noget andet';
+$lang['porg_issue_796'] = 'API\'ets pwg.images.getInfo returnerer ukorrekte URL\'er med  $conf[\'original_url_protection\']';
+$lang['porg_issue_GHSA-6wj3-7fhw-gfpm'] = 'Opgradering: SQL-indsprøjtning';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'Batchhåndtering: Tjek brugers inddata';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'Standardsiders opsætning: Mere robust tjek af logoets filtype';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'Installering: fornuftighedstjek af tabelpræfiks under installering';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'Fotoupload: sikring af at SVG-filer har den korrekte filendelse';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'Albumfotografiers sorteringsrækkefølge: Tjek brugers inddata før anvendelse';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'Visning af RAW-cookies indhold skal forhindres';
