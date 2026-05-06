328e5060e4d83be757a19523d4bd79bd5cc9ae39
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-06 06:34:02 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/release-16.4.0.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2369'] = '除零错误';
+$lang['porg_issue_2478'] = 'API 密钥创建日期不显示';
+$lang['porg_issue_2531'] = '标准页面配置丢失';
+$lang['porg_issue_2538'] = '[邮件] 区分发件人地址与回复地址';
+$lang['porg_issue_2539'] = '[API] 为 api_key 请求自动设置 pwg_token';
+$lang['porg_issue_2544'] = '[用户] 添加用户弹窗过大，且存在多处翻译错误';
+$lang['porg_issue_2553'] = '通过界面更新插件时，可能执行旧版本的插件更新操作';
+$lang['porg_issue_2555'] = '[用户] 普通用户无法生成密码重置链接，但界面显示与之不符';
+$lang['porg_issue_796'] = '启用 $conf [\'original_url_protection\'] 时，API pwg.images.getInfo 返回错误的链接';
+$lang['porg_issue_GHSA-6wj3-7fhw-gfpm'] = '升级功能：存在 SQL 注入漏洞';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = '批量管理器：校验用户输入';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = '标准页面配置：加强 Logo 文件类型校验';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = '安装程序：安装时对数据表前缀进行合法性校验';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = '照片上传：确保 SVG 文件拥有正确的文件扩展名';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = '相册照片排序：使用前校验用户输入';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = '禁止显示原始 Cookie 内容';
