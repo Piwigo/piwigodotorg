1e8274a8c82e2642e7ebf8b08273564d4295fa35
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-17 19:00:29 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/release-16.4.0.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['porg_issue_2369'] = 'SıfıraBölmeHatası';
+$lang['porg_issue_2478'] = 'API anahtarının oluşturulma tarihi görünmüyor';
+$lang['porg_issue_2531'] = 'Standart sayfa conf kaybolur';
+$lang['porg_issue_2538'] = '[mail] farklılık from ve reply-to';
+$lang['porg_issue_2539'] = '[API] api_key istekleri için pwg_token\'ı otomatik olarak ayarla';
+$lang['porg_issue_2544'] = '[users] Kullanıcı ekleme penceresi çok büyük ve bazı çeviri hataları var.';
+$lang['porg_issue_2553'] = 'Kullanıcı arayüzü üzerinden eklenti güncellemesi, önceki sürümün eklenti güncelleme işlemini gerçekleştirebilir.';
+$lang['porg_issue_2555'] = '[users] Genel kullanıcı parola sıfırlama bağlantısı oluşturamıyor ancak kullanıcı arayüzü aksini gösteriyor.';
+$lang['porg_issue_796'] = 'API pwg.images.getInfo, $conf[\'original_url_protection\'] ile hatalı URL\'ler döndürüyor.';
+$lang['porg_issue_GHSA-6wj3-7fhw-gfpm'] = 'yükseltme: SQL enjeksiyonu';
+$lang['porg_issue_GHSA-7r67-9xhq-7p2c'] = 'toplu işlem yöneticisi: kullanıcı girişini kontrol et';
+$lang['porg_issue_GHSA-7w97-5g4p-xqvv'] = 'standart sayfalar yapılandırması: logo dosya türü üzerinde daha sağlam kontrol';
+$lang['porg_issue_GHSA-gphq-34pv-gvf3'] = 'Kurulum: Kurulum sırasında tablo öneki için mantıksal kontrolü';
+$lang['porg_issue_GHSA-j9q6-q52g-g8jw'] = 'Fotoğraf yükleme: SVG dosyalarının uygun dosya adının uzantısınon doğru olduğundan emin ol';
+$lang['porg_issue_GHSA-jhp4-7f82-8f6q'] = 'albüm fotoğrafları sıralama düzeni: kullanmadan önce kullanıcı girişini kontrol edin';
+$lang['porg_issue_GHSA-rr39-mf4j-6594'] = 'Ham çerez içeriğinin görüntülenmesini engelleme';
